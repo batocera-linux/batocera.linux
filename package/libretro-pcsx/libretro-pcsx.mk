d939d7e074a861e92ea0e30c99fad044ebfcbbf7
digitalLumberjack <digitallumberjack@gmail.com>
2015-01-29 12:09:08 +0100

    modif on picodrive and pcsx, aded megadrive system

--- package/libretro-pcsx/libretro-pcsx.mk
+++ package/libretro-pcsx/libretro-pcsx.mk
@@ -5,18 +5,26 @@
 ################################################################################
 LIBRETRO_PCSX_VERSION = master
 LIBRETRO_PCSX_SITE = $(call github,libretro,pcsx_rearmed,master)
+PCSX_INCLUDES=-I$(STAGING_DIR)/usr/include -I$(STAGING_DIR)/usr/include/interface/vcos/pthreads -I$(STAGING_DIR)/usr/include/interface/vmcs_host/linux
 
-#define LIBRETRO_PCSX_CONFIGURE_CMDS
-#	( cd $(@D) && \
-#	CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" CFLAGS="$(TARGET_CFLAGS)" CXXFLAGS="$(TARGET_CXXFLAGS)" ./configure --platform=libretro )
-#endef
+define LIBRETRO_PCSX_CONFIGURE_CMDS
+	$(SED) "s|/opt/vc/include|$(STAGING_DIR)/usr/include|g" $(@D)/configure
+	$(SED) "s|/opt/vc/lib|$(STAGING_DIR)/usr/lib|g" $(@D)/configure
+	$(SED) "s|/opt/vc/include/interface/vcos/pthreads|$(STAGING_DIR)/usr/include/interface/vcos/pthreads|g" $(@D)/configure
+	$(SED) "s|/opt/vc/include/interface/vmcs_host/linux|$(STAGING_DIR)/usr/include/interface/vmcs_host/linux|g" $(@D)/configure
+	$(SED) "s|-lGLESv1_CM|-lGLESv2|g" $(@D)/configure
+	$(SED) "/need_xlib=\"yes\"/d"  $(@D)/configure
+ 	( cd $(@D) && \
+      	LDFLAGS="$(TARGET_LDFLAGS)" AS="$(TARGET_AS)" CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" CFLAGS="$(TARGET_CFLAGS)" CXXFLAGS="$(TARGET_CXXFLAGS)" ./configure --platform=libretro )	
+endef
 
 define LIBRETRO_PCSX_BUILD_CMDS
-	CFLAGS="$(TARGET_CFLAGS)" CXXFLAGS="$(TARGET_CXXFLAGS)" $(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" -C $(@D) -f Makefile.libretro platform="armv6-hardfloat"
+	CFLAGS="$(TARGET_CFLAGS)" CXXFLAGS="$(TARGET_CXXFLAGS)" $(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" AR="$(TARGET_AR)" \
+	-C $(@D)
 endef
 
 define LIBRETRO_PCSX_INSTALL_TARGET_CMDS
-	$(INSTALL) -D $(@D)/pcsx_rearmed_libretro.so \
+	$(INSTALL) -D $(@D)/libretro.so \
 		$(TARGET_DIR)/usr/lib/libretro/pcsx_rearmed_libretro.so
 endef
 
--- package/libretro-picodrive/libretro-picodrive.mk
+++ package/libretro-picodrive/libretro-picodrive.mk
@@ -7,19 +7,20 @@ LIBRETRO_PICODRIVE_VERSION = master
 LIBRETRO_PICODRIVE_SITE = $(call github,libretro,picodrive,$(LIBRETRO_PICODRIVE_VERSION))
 LIBRETRO_PICODRIVE_DEPENDENCIES = libpng sdl
 
-#define LIBRETRO_PICODRIVE_GITHUBHACK
 define LIBRETRO_PICODRIVE_CONFIGURE_CMDS
 	rm -rf $(@D)/picodrive
 	git -C $(@D) clone https://github.com/libretro/picodrive
 	#cp -r $(@D)/../picodrivegithub/.git $(@D)/
 	git -C $(@D)/picodrive submodule update --init
-	( cd $(@D)/picodrive && \
-        CC="$(TARGET_CC)" CXX="$(TARGET_CXX)" CFLAGS="$(TARGET_CFLAGS)" \
-	./configure )
+	##( cd $(@D)/picodrive && \
+        ##CC="$(TARGET_CC)" CXX="$(TARGET_CXX)" CFLAGS="$(TARGET_CFLAGS)" \
+	##./configure )
 endef
+
+
 define LIBRETRO_PICODRIVE_BUILD_CMDS
 	$(MAKE) -C $(@D)/picodrive/cpu/cyclone CONFIG_FILE=$(@D)/picodrive/cpu/cyclone_config.h	
-	CFLAGS="$(TARGET_CFLAGS)" CXXFLAGS="$(TARGET_CXXFLAGS)" $(MAKE) CC="$(TARGET_CC)" CXX="$(TARGET_CXX)" -C  $(@D)/picodrive -f Makefile.libretro platform="armv6-hardfloat"
+	CFLAGS="$(TARGET_CFLAGS)" CXXFLAGS="$(TARGET_CXXFLAGS)" $(MAKE) CC="$(TARGET_CC)" CXX="$(TARGET_CXX)" -C  $(@D)/picodrive -f Makefile.libretro platform="armv6-armasm-hardfloat"
 endef
 
 define LIBRETRO_PICODRIVE_INSTALL_TARGET_CMDS
