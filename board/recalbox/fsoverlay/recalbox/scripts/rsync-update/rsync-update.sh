f459735f24c6e77b595c8314a9bb8ccb44aff59d
digitalLumberjack <digitallumberjack@gmail.com>
2015-02-04 09:29:42 +0100

    changed update system

--- board/recalbox/fsoverlay/recalbox/scripts/rsync-update/rsync-update.sh
+++ board/recalbox/fsoverlay/recalbox/scripts/rsync-update/rsync-update.sh
@@ -13,7 +13,17 @@ fi
 echo "info : will conntect to ${RSYNC_SERVER} with credential : ${RSYNC_USER}" >> /root/updates.log
 
 echo "info : starting root update" >> /root/updates.log
-rsync -aXv --exclude-from=/recalbox/scripts/rsync-update/always-exclude.cfg --exclude-from=/recalbox/scripts/rsync-update/exclude-download.cfg rsync://${RSYNC_USER}@${RSYNC_SERVER}/recalbox / >> /root/updates.log
+rsync -rltXv --exclude-from=/recalbox/scripts/rsync-update/always-exclude.cfg --exclude-from=/recalbox/scripts/rsync-update/exclude-share.cfg rsync://${RSYNC_USER}@${RSYNC_SERVER}/recalbox-share /recalbox/share >> /root/updates.log
+if (($? != 0)); then
+        echo "error : unable to update share" >> /root/updates.log
+        exit 91
+fi
+rsync -rltXv --exclude-from=/recalbox/scripts/rsync-update/always-exclude.cfg --exclude-from=/recalbox/scripts/rsync-update/exclude-boot.cfg rsync://${RSYNC_USER}@${RSYNC_SERVER}/recalbox-boot /boot >> /root/updates.log
+if (($? != 0)); then
+        echo "error : unable to update boot" >> /root/updates.log
+        exit 91
+fi
+rsync -aXv --exclude-from=/recalbox/scripts/rsync-update/always-exclude.cfg --exclude-from=/recalbox/scripts/rsync-update/exclude-root.cfg rsync://${RSYNC_USER}@${RSYNC_SERVER}/recalbox-root / >> /root/updates.log
 if (($? != 0)); then
         echo "error : unable to update root" >> /root/updates.log
         exit 91
