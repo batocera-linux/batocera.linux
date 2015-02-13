ad671890cad111db1ecb9ef7a92082b2c86becc7
digitalLumberjack <digitallumberjack@gmail.com>
2015-02-13 11:26:04 +0100

    coreected catsfc

--- package/libretro-catsfc/libretro-catsfc.mk
+++ package/libretro-catsfc/libretro-catsfc.mk
@@ -6,24 +6,8 @@
 LIBRETRO_CATSFC_VERSION = master
 LIBRETRO_CATSFC_SITE = $(call github,libretro,CATSFC-libretro,master)
 
-PLATFORM =
-ifeq ($(BR2_ARM_CPU_ARMV6),y)
-        PLATFORM = armv6
-endif
-
-ifeq ($(BR2_cortex_a7),y)
-        PLATFORM = armv7
-endif
-
-ifeq ($(BR2_GCC_TARGET_FLOAT_ABI),hard)
-        PLATFORM += hardfloat
-endif
-
-ifeq ($(BR2_ARM_CPU_HAS_NEON),y)
-        PLATFORM += neon
-endif
 define LIBRETRO_CATSFC_BUILD_CMDS
-	CFLAGS="$(TARGET_CFLAGS)" CXXFLAGS="$(TARGET_CXXFLAGS)" $(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" -C $(@D) platform="$(PLATFORM)"
+	CFLAGS="$(TARGET_CFLAGS)" CXXFLAGS="$(TARGET_CXXFLAGS)" $(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" -C $(@D) platform="$(LIBRETRO_PLATFORM)"
 endef
 
 define LIBRETRO_CATSFC_INSTALL_TARGET_CMDS
