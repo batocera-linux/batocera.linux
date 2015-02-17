402bb16523f4048f1ed21c0fcaf19fef5022163e
digitalLumberjack <digitallumberjack@gmail.com>
2015-02-17 09:08:46 +0100

    added scummvm package

--- package/libretro-scummvm/libretro-scummvm.mk
+++ package/libretro-scummvm/libretro-scummvm.mk
@@ -5,10 +5,13 @@
 ################################################################################
 LIBRETRO_SCUMMVM_VERSION = master
 LIBRETRO_SCUMMVM_SITE = $(call github,libretro,scummvm,master)
+#CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" \
+#	LD="$(TARGET_LD)" RANLIB="$(TARGET_RANLIB)" AR="$(TARGET_AR) cru"  \
 
 define LIBRETRO_SCUMMVM_BUILD_CMDS
-	CFLAGS="$(TARGET_CFLAGS)" CXXFLAGS="$(TARGET_CXXFLAGS)" $(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" LD="$(TARGET_LD)" RANLIB="$(TARGET_RANLIB)" AR="$(TARGET_AR) cru"  \
-	 -C $(@D)/backends/platform/libretro/build platform="$(LIBRETRO_PLATFORM)"
+	CFLAGS="$(TARGET_CFLAGS)" CXXFLAGS="$(TARGET_CXXFLAGS)" LDFLAGS="-shared -Wl,--no-undefined" $(MAKE) \
+	TOOLSET="$(TARGET_CROSS)" \
+	-C $(@D)/backends/platform/libretro/build platform="$(LIBRETRO_PLATFORM)"
 endef
 
 define LIBRETRO_SCUMMVM_INSTALL_TARGET_CMDS
