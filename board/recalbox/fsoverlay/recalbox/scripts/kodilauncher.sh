eee12bfc5b8e6e7e65b1957e60daaed8461f8d1b
digitalLumberjack <digitallumberjack@gmail.com>
2015-02-18 08:01:21 +0100

    kodi launcher

--- board/recalbox/fsoverlay/recalbox/scripts/kodilauncher.sh
+++ board/recalbox/fsoverlay/recalbox/scripts/kodilauncher.sh
@@ -1,2 +1,22 @@
 #!/bin/bash
-kodi-standalone 
+# Horrible script
+# will kill kodi when it's thread are < 8
+# because it the number of threads hanging 
+# when you try to quit after seeing a movie
+# and letting the process hang
+
+/usr/lib/kodi/kodi.bin --standalone -fs -n &
+pid=$!
+
+sleep 5
+
+running=1
+while [[ "$running" == "1" ]]; do
+        sleep 3
+        nbThread=`cat /proc/$pid/status | grep Threads: | cut -d":" -f2 | tr -d '[[:space:]]'`
+        if [[ "$nbThread" -lt "8" ]];then
+                kill $pid
+                running=0
+        fi
+done
+echo "exiting"
