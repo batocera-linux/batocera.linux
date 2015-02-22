bcaa3d10bc94ddf91df3e8d655a2741dc9da3774
digitalLumberjack <digitallumberjack@gmail.com>
2015-02-22 06:49:34 +0100

    corrected mk_arcade package mk

--- package/mk_arcade_joystick_rpi/mk_arcade_joystick_rpi.mk
+++ package/mk_arcade_joystick_rpi/mk_arcade_joystick_rpi.mk
@@ -7,12 +7,22 @@ MK_ARCADE_JOYSTICK_RPI_VERSION = master
 MK_ARCADE_JOYSTICK_RPI_SITE = $(call github,digitallumberjack,mk_arcade_joystick_rpi,master)
 MK_ARCADE_JOYSTICK_RPI_DEPENDENCIES = linux
 
+
+# Needed beacause can't pass cflags to cc
+define MK_ARCADE_JOYSTICK_RPI_RPI2_HOOK
+        $(SED) "s/#define PERI_BASE        0x20000000/#define PERI_BASE        0x3F000000/g" $(@D)/mk_arcade_joystick_rpi.c
+endef
+
+ifeq ($(BR2_cortex_a7),y)
+        MK_ARCADE_JOYSTICK_RPI_PRE_CONFIGURE_HOOKS += MK_ARCADE_JOYSTICK_RPI_RPI2_HOOK
+endif
+
 define MK_ARCADE_JOYSTICK_RPI_BUILD_CMDS
-		$(MAKE) -C $(@D) -f Makefile.cross $(LINUX_MAKE_FLAGS) KERNELDIR=$(LINUX_DIR)
+        $(MAKE) -C $(@D) -f Makefile.cross $(LINUX_MAKE_FLAGS) KERNELDIR=$(LINUX_DIR)
 endef
 
 define MK_ARCADE_JOYSTICK_RPI_INSTALL_TARGET_CMDS
-	$(MAKE) -C $(@D) -f Makefile.cross $(LINUX_MAKE_FLAGS) KERNELDIR=$(LINUX_DIR) modules_install
+        $(MAKE) -C $(@D) -f Makefile.cross $(LINUX_MAKE_FLAGS) KERNELDIR=$(LINUX_DIR) modules_install
 endef
 
 $(eval $(generic-package))
