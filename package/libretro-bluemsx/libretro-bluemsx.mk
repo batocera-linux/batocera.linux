ccbcd2ec8b0139921dc3f867500d8cb3b9b2420b
digitalLumberjack <digitallumberjack@gmail.com>
2015-02-24 07:54:48 +0100

    added bluemsx

---
+++ package/libretro-bluemsx/libretro-bluemsx.mk
@@ -0,0 +1,19 @@
+################################################################################
+#
+# BLUEMSX
+#
+################################################################################
+LIBRETRO_BLUEMSX_VERSION = master
+LIBRETRO_BLUEMSX_SITE = $(call github,libretro,blueMSX-libretro,master)
+
+define LIBRETRO_BLUEMSX_BUILD_CMDS
+	CFLAGS="$(TARGET_CFLAGS)" CXXFLAGS="$(TARGET_CXXFLAGS)" \
+        $(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" LD="$(TARGET_LD)" RANLIB="$(TARGET_RANLIB)" AR="$(TARGET_AR)" -C $(@D) -f Makefile.libretro platform="$(LIBRETRO_PLATFORM)"
+endef
+
+define LIBRETRO_BLUEMSX_INSTALL_TARGET_CMDS
+	$(INSTALL) -D $(@D)/bluemsx_libretro.so \
+		$(TARGET_DIR)/usr/lib/libretro/bluemsx_libretro.so
+endef
+
+$(eval $(generic-package))
