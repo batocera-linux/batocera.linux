499429585435a0a7f2583c3792f8d2f0a47d3ea3
digitalLumberjack <digitallumberjack@gmail.com>
2015-02-28 11:38:31 +0100

    added fba liretro and xboxdrv, plus wifi firmwares
    Conflicts:
    	package/Config.in

---
+++ package/libretro-fba/libretro-fba.mk
@@ -0,0 +1,24 @@
+################################################################################
+#
+# FBA
+#
+################################################################################
+LIBRETRO_FBA_VERSION = master
+LIBRETRO_FBA_SITE = $(call github,libretro,libretro-fba,master)
+
+ifeq ($(BR2_cortex_a7),y)
+	LIBRETRO_FBA_PLATFORM=rpi2
+else
+	LIBRETRO_FBA_PLATFORM=$(LIBRETRO_PLATFORM)
+endif
+
+define LIBRETRO_FBA_BUILD_CMDS
+	CFLAGS="$(TARGET_CFLAGS)" CXXFLAGS="$(TARGET_CXXFLAGS)" $(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" -C $(@D) -f makefile.libretro platform="$(LIBRETRO_FBA_PLATFORM)" profile="performance"
+endef
+
+define LIBRETRO_FBA_INSTALL_TARGET_CMDS
+	$(INSTALL) -D $(@D)/fb_alpha_libretro.so \
+		$(TARGET_DIR)/usr/lib/libretro/fba_libretro.so
+endef
+
+$(eval $(generic-package))
---
+++ package/xboxdrv/xboxdrv.mk
@@ -0,0 +1,34 @@
+################################################################################
+#
+# XBOXDRV
+#
+################################################################################
+XBOXDRV_VERSION = fb787abe320741d8c341a9ad62c9a0ec513fc2cb
+XBOXDRV_SITE =  $(call github,Grumbel,xboxdrv,$(XBOXDRV_VERSION))
+XBOXDRV_DEPENDENCIES = libusb
+#opts.Add('CPPPATH', 'Additional preprocessor paths')
+#opts.Add('CPPFLAGS', 'Additional preprocessor flags')
+#opts.Add('CPPDEFINES', 'defined constants')
+#opts.Add('LIBPATH', 'Additional library paths')
+#opts.Add('LIBS', 'Additional libraries')
+#opts.Add('LINKFLAGS', 'Linker Compiler flags')
+#opts.Add('BUILD', 'Build type: release, custom, development')
+
+define XBOXDRV_BUILD_CMDS
+        CFLAGS="$(TARGET_CFLAGS) -I$(STAGING_DIR)/usr/include" CXXFLAGS="$(TARGET_CXXFLAGS) -I$(STAGING_DIR)/usr/include" LD_FLAGS="$(TARGET_LDFLAGS)" $(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" -C $(@D)
+endef
+
+define XBOXDRV_INSTALL_TARGET_CMDS
+        CFLAGS="$(TARGET_CFLAGS)" CXXFLAGS="$(TARGET_CXXFLAGS)" $(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" -C $(@D) install-exec DESTDIR="$(TARGET_DIR)"
+endef
+
+define XBOXDRV_HOOK_SCONS
+	$(SED) "s|dbus-binding-tool|$(HOST_DIR)/usr/bin/dbus-binding-tool|g" $(@D)/SConstruct
+	$(SED) "s|pkg-config|$(HOST_DIR)/usr/bin/pkg-config|g" $(@D)/SConstruct
+	$(SED) "s|scons|$(HOST_DIR)/usr/bin/scons CXX=\"$(TARGET_CXX)\" CC=\"$(TARGET_CC)\" CCFLAGS=\"$(TARGET_CFLAGS)\" CXXFLAGS=\"$(TARGET_CXXFLAGS)\"|g" $(@D)/Makefile
+endef
+
+XBOXDRV_PRE_CONFIGURE_HOOKS += XBOXDRV_HOOK_SCONS
+
+
+$(eval $(generic-package))
