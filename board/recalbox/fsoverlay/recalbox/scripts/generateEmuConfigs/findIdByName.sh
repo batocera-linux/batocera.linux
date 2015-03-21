d69c77628cdce4a38e5373b88d14b466b5c63bdf
digitalLumberjack <digitallumberjack@gmail.com>
2015-03-21 16:31:57 +0100

    corrected indexes of pifba with same name

--- board/recalbox/fsoverlay/recalbox/scripts/generateEmuConfigs/findIdByName.sh
+++ board/recalbox/fsoverlay/recalbox/scripts/generateEmuConfigs/findIdByName.sh
@@ -4,18 +4,19 @@ function findIdByName {
 	name="$1"
 	handlers=`cat /proc/bus/input/devices | grep -A 6 "$name" | grep "H: Handlers"`
 	regex="js([0-9]+)"
-	echo "searching id of joystick '$name' in $handlers"
+	echo "searching id of joystick '$name' in $handlers" >> ~/generateconfig.log
 	IFS=$'\n'
 	for handler in $handlers; do
-		echo "searching in $handler"
+		echo "searching in $handler" >> ~/generateconfig.log
 	    	[[ $handler =~ $regex ]]
 		joystickIndex="${BASH_REMATCH[1]}"
-		echo "values ${BASH_REMATCH['1']} ${usedJoysticks[$joystickIndex]}"
+		echo "values ${BASH_REMATCH['1']} ${usedJoysticks[$joystickIndex]}" >> ~/generateconfig.log
 
 		if [ -n "$joystickIndex" ] && [ -z "${usedJoysticks[$joystickIndex]}" ]; then
-			echo "found joystick index for $1 : $joystickIndex"
+			echo "found joystick index for $1 : $joystickIndex" >> ~/generateconfig.log
 			usedJoysticks[$joystickIndex]=1
 			joysticksystemindex="${BASH_REMATCH[1]}"
+			break
 		fi
 	done
 }
