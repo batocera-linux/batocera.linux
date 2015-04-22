6512778e66d610ac4ee121fb34ef6ab2e850297f
rockaddicted <nirvana_paradize@hotmail.com>
2015-04-22 15:05:08 +0200

    added game and watch system

---
+++ package/libretro-gw/libretro-gw.mk
@@ -0,0 +1,18 @@
+################################################################################
+#
+# GW
+#
+################################################################################
+LIBRETRO_GW_VERSION = 73e19687954bb14466105ac2bba72aa8f66c4500
+LIBRETRO_GW_SITE = $(call github,libretro,gw-libretro,$(LIBRETRO_GW_VERSION))
+
+define LIBRETRO_GW_BUILD_CMDS
+	CFLAGS="$(TARGET_CFLAGS)" CXXFLAGS="$(TARGET_CXXFLAGS)" $(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" -C $(@D)/ -f Makefile.libretro platform="$(LIBRETRO_PLATFORM)"
+endef
+
+define LIBRETRO_GW_INSTALL_TARGET_CMDS
+	$(INSTALL) -D $(@D)/gw_libretro.so \
+		$(TARGET_DIR)/usr/lib/libretro/gw_libretro.so
+endef
+
+$(eval $(generic-package))
