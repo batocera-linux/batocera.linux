ac0e7097747fa62da0fd7bb08913c96d977de3f7
rockaddicted <nirvana_paradize@hotmail.com>
2015-07-25 18:55:43 +0200

    added tgbdual-libretro core

---
+++ package/libretro-tgbdual/libretro-tgbdual.mk
@@ -0,0 +1,20 @@
+################################################################################
+#
+# TGBDUAL
+#
+################################################################################
+LIBRETRO_TGBDUAL_VERSION = ea1364c80e3e26cd49b649526e5db115fa1e1820
+LIBRETRO_TGBDUAL_SITE = $(call github,libretro,tgbdual-libretro,$(LIBRETRO_TGBDUAL_VERSION))
+
+define LIBRETRO_TGBDUAL_BUILD_CMDS
+		CFLAGS="$(TARGET_CFLAGS)" LD="$(TARGET_LD)" \
+		$(MAKE) CC="$(TARGET_CC)" CXX="$(TARGET_CXX)" -C $(@D)
+endef
+
+define LIBRETRO_TGBDUAL_INSTALL_TARGET_CMDS
+	$(INSTALL) -D $(@D)/tgbdual_libretro.so \
+		$(TARGET_DIR)/usr/lib/libretro/tgbdual_libretro.so
+endef
+
+$(eval $(generic-package))
+
