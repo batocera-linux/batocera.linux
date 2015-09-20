64ec4e12d3f7fe6d14e2fde7aca3dece20c092a8
digitalLumberjack <digitallumberjack@gmail.com>
2015-09-20 18:56:21 +0200

    corrected virtual gamepad ioctl version

--- package/virtualgamepads/virtualgamepads.mk
+++ package/virtualgamepads/virtualgamepads.mk
@@ -3,7 +3,7 @@
 # VIRTUALGAMEPADS
 #
 ################################################################################
-VIRTUALGAMEPADS_VERSION = f2c36d9bc668580173dc690baab329fd91a05183
+VIRTUALGAMEPADS_VERSION = e2b4549374ef95c83fa915353d6da75ce86fc7e1
 VIRTUALGAMEPADS_SITE = $(call github,miroof,node-virtual-gamepads,$(VIRTUALGAMEPADS_VERSION))
 VIRTUALGAMEPADS_DEPENDENCIES = nodejs
 
