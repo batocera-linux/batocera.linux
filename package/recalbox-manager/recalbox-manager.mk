d30de83745bcfaa1be798fc1ead8ca81e874bb21
MikaXII <mikael.joubert@supinfo.com>
2015-10-10 20:48:21 +0200

    Add script init

--- package/recalbox-manager/recalbox-manager.mk
+++ package/recalbox-manager/recalbox-manager.mk
@@ -15,4 +15,10 @@ define RECALBOX_MANAGER_INSTALL_TARGET_CMDS
     $(INSTALL) -D $(@D)/* $(TARGET_DIR)/usr/recalbox-manager
 endef
 
+define RECALBOX_INSTALL_INIT_SYSV
+	$(INSTALL) -m 0755 -D package/recalbox-manager/S93manager $(TARGET_DIR)/etc/init.d/S93manager
+    	$(INSTALL) -m 0755 -D package/recalbox-manager/start.sh  $(TARGET_DIR)/usr/recalbox-manager
+endef
+
+
 $(eval $(generic-package))
---
+++ package/recalbox-manager/script/start.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#WDIR=/srv/rhodecode
+VIRTUALENV_DIR=/usr/recalbox-manager
+
+source $VIRTUALENV_DIR/bin/activate
+
+#cd $WDIR
+#paster serve production.ini 1> debug.log 2> error.log
