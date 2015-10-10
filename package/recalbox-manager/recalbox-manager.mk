42ba182811701d274694a473bf309180f2028dac
MikaXII <mikael.joubert@supinfo.com>
2015-10-10 22:35:01 +0200

    rollback version 0.8.4 because no fucking psutil of my balls

--- package/recalbox-manager/recalbox-manager.mk
+++ package/recalbox-manager/recalbox-manager.mk
@@ -3,7 +3,7 @@
 # recalbox-manager
 #
 ################################################################################
-RECALBOX_MANAGER_VERSION = 0.9.0 #6103f9bcee424294cc538c8efa84d90e831b45d1
+RECALBOX_MANAGER_VERSION = 0.8.4 #0.9.0 #6103f9bcee424294cc538c8efa84d90e831b45d1
 RECALBOX_MANAGER_SITE = $(call github,sveetch,recalbox-manager,$(RECALBOX_MANAGER_VERSION))
 RECALBOX_MANAGER_DEPENDENCIES = python
 
@@ -12,12 +12,14 @@ define RECALBOX_MANAGER_BUILD_CMDS
 endef
 
 define RECALBOX_MANAGER_INSTALL_TARGET_CMDS
-    $(INSTALL) -D $(@D)/* $(TARGET_DIR)/usr/recalbox-manager
+	mkdir -p $(TARGET_DIR)/usr/recalbox-manager
+	cp -r $(@D)/* $(TARGET_DIR)/usr/recalbox-manager
+	#$(INSTALL) -D $(@D)/* $(TARGET_DIR)/usr/recalbox-manager
 endef
 
-define RECALBOX_INSTALL_INIT_SYSV
-	$(INSTALL) -m 0755 -D package/recalbox-manager/S93manager $(TARGET_DIR)/etc/init.d/S93manager
-    	$(INSTALL) -m 0755 -D package/recalbox-manager/start.sh  $(TARGET_DIR)/usr/recalbox-manager
+define RECALBOX_MANAGER_INSTALL_INIT_SYSV
+	$(INSTALL) -m 0755 -D package/recalbox-manager/script/S94manager $(TARGET_DIR)/etc/init.d/S94manager
+    	$(INSTALL) -m 0755 -D package/recalbox-manager/script/start.sh  $(TARGET_DIR)/usr/recalbox-manager
 endef
 
 
