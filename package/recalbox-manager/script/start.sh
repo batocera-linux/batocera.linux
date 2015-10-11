059a9199cae288268d0e5c19de4a0fb0cf348119
MikaXII <mikael.joubert@supinfo.com>
2015-10-11 11:29:04 +0200

    update script init

--- package/recalbox-manager/script/start.sh
+++ package/recalbox-manager/script/start.sh
@@ -4,7 +4,7 @@
 VIRTUALENV_DIR=/usr/recalbox-manager
 
 source $VIRTUALENV_DIR/bin/activate
-$VIRTUALENV_DIR/manage.py runserver 0.0.0.0:80 &
+$VIRTUALENV_DIR/bin/python $VIRTUALENV_DIR/manage.py runserver 0.0.0.0:80 &
 
 #cd $WDIR
 #paster serve production.ini 1> debug.log 2> error.log
