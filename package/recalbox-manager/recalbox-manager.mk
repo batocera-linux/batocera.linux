1bed5b53410fd77937547a78331f7a224bdd9658
MikaXII <mikael.joubert@supinfo.com>
2015-10-12 22:06:56 +0200

    update recalbox-manager

--- package/recalbox-manager/recalbox-manager.mk
+++ package/recalbox-manager/recalbox-manager.mk
@@ -5,7 +5,7 @@
 ################################################################################
 RECALBOX_MANAGER_VERSION = 1.0.0 #0.8.4 #0.9.0 #6103f9bcee424294cc538c8efa84d90e831b45d1
 RECALBOX_MANAGER_SITE = $(call github,sveetch,recalbox-manager,$(RECALBOX_MANAGER_VERSION))
-RECALBOX_MANAGER_DEPENDENCIES = python python-psutil
+RECALBOX_MANAGER_DEPENDENCIES = python python-psutil python-django python-autobreadcrumbs
 
 define RECALBOX_MANAGER_BUILD_CMDS
     $(MAKE) CC="$(TARGET_CC)" LD="$(TARGET_LD)" -C $(@D) install
@@ -19,7 +19,6 @@ endef
 
 define RECALBOX_MANAGER_INSTALL_INIT_SYSV
 	$(INSTALL) -m 0755 -D package/recalbox-manager/script/S94manager $(TARGET_DIR)/etc/init.d/S94manager
-    	$(INSTALL) -m 0755 -D package/recalbox-manager/script/start.sh  $(TARGET_DIR)/usr/recalbox-manager
 endef
 
 
