308a68d08c0ab8c12a8b6c9677a0535f1e0b6b45
MikaXII <mikael.joubert@supinfo.com>
2015-10-12 19:32:48 +0200

    update package manager

--- package/recalbox-manager/recalbox-manager.mk
+++ package/recalbox-manager/recalbox-manager.mk
@@ -3,9 +3,9 @@
 # recalbox-manager
 #
 ################################################################################
-RECALBOX_MANAGER_VERSION = 0.8.4 #0.9.0 #6103f9bcee424294cc538c8efa84d90e831b45d1
+RECALBOX_MANAGER_VERSION = 1.0.0 #0.8.4 #0.9.0 #6103f9bcee424294cc538c8efa84d90e831b45d1
 RECALBOX_MANAGER_SITE = $(call github,sveetch,recalbox-manager,$(RECALBOX_MANAGER_VERSION))
-RECALBOX_MANAGER_DEPENDENCIES = python
+RECALBOX_MANAGER_DEPENDENCIES = python python-psutil
 
 define RECALBOX_MANAGER_BUILD_CMDS
     $(MAKE) CC="$(TARGET_CC)" LD="$(TARGET_LD)" -C $(@D) install
