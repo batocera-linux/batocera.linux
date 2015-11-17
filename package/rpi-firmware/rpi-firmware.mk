62a6b3205bd6a52f47047edb6592a25b39157eed
digitalLumberjack <digitallumberjack@gmail.com>
2015-11-17 20:00:05 +0100

    bumped rpi firmware and userland

--- package/rpi-firmware/rpi-firmware.mk
+++ package/rpi-firmware/rpi-firmware.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RPI_FIRMWARE_VERSION = 52f65ac4ad1ef1d2838fa2f921d5bc3ece4c777b
+RPI_FIRMWARE_VERSION = 1f5c0d7c3c86483d7787b1996bc22cb5278e0be8
 RPI_FIRMWARE_SITE = $(call github,raspberrypi,firmware,$(RPI_FIRMWARE_VERSION))
 RPI_FIRMWARE_LICENSE = BSD-3c
 RPI_FIRMWARE_LICENSE_FILES = boot/LICENCE.broadcom
