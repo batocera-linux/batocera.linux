bd9bb0520ad966d1179ad9bbea1ac3a8d9032e6c
DigitalLumberjack <digitallumberjack@gmail.com>
2015-12-05 17:54:22 +0100

    dropbear link to rw partition at build

--- board/recalbox/recalbox-patch-target.sh
+++ board/recalbox/recalbox-patch-target.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 target="output/target"
-
 sed -i "s|root:x:0:0:root:/root:/bin/sh|root:x:0:0:root:/recalbox/share/system:/bin/sh|g" "$target/etc/passwd"
-
- 
+rm -rf "$target/etc/dropbear"
+ln -s "/recalbox/share/system/ssh" "$target/etc/dropbear" 
