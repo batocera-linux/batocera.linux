928fcfd04ccf3c8b0dacfdc1acfd3ffcabf55576
rockaddicted <nirvana_paradize@hotmail.com>
2015-12-09 08:53:29 +0100

    added new libretro cores

--- package/libretro-uae/libretro-uae.mk
+++ package/libretro-uae/libretro-uae.mk
@@ -3,11 +3,11 @@
 # UAE
 #
 ################################################################################
-LIBRETRO_UAE_VERSION = master
+LIBRETRO_UAE_VERSION = 7cbc2304d064cf58455bc4bdf589cb3fd51865a8
 LIBRETRO_UAE_SITE = $(call github,libretro,libretro-uae,master)
 
 define LIBRETRO_UAE_BUILD_CMDS
-	CFLAGS="$(TARGET_CFLAGS)" CXXFLAGS="$(TARGET_CXXFLAGS)" LDFLAGS="$(TARGET_LDFLAGS)" $(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" -C $(@D)/build/ -f Makefile platform="armv7-neon-hardfloat"
+	CFLAGS="$(TARGET_CFLAGS)" CXXFLAGS="$(TARGET_CXXFLAGS)" LDFLAGS="$(TARGET_LDFLAGS)" $(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" -C $(@D)/ -f Makefile platform="rpi"
 endef
 
 define LIBRETRO_UAE_INSTALL_TARGET_CMDS
