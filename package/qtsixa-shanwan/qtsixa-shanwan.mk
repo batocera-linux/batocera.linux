6eb73abe3a0d44ea22074a0cb3fb3db2ed7ff20e
digitallumberjack <digitallumberjack@gmail.com>
2015-12-15 13:58:09 +0000

    added qtsixad unified original and gasia drivers

---
+++ package/qtsixa-shanwan/qtsixa-shanwan.mk
@@ -0,0 +1,39 @@
+################################################################################
+#
+# qtsixa-shanwan
+#
+################################################################################
+QTSIXA_SHANWAN_VERSION = ceef35906f4c894ee0b5b0b3994b956d1f1f643c
+QTSIXA_SHANWAN_SITE = $(call github,yarick123,qtsixa,$(QTSIXA_SHANWAN_VERSION))
+QTSIXA_SHANWAN_DEPENDENCIES = sdl linux-headers qtsixa
+PKGCONFIG_CONFIG=$(STAGING_DIR)/usr/lib/pkgconfig
+
+QTSIXA_SHANWAN_INCLUDES =-I$(STAGING_DIR)/usr/include -I$(STAGING_DIR)/usr/include/interface/vcos/pthreads -I$(STAGING_DIR)/usr/include/interface/vmcs_host/linux
+QTSIXA_SHANWAN_CFLAGS = -D__ARM_PCS_VFP -DARM_ARCH -DRPI_BUILD -Wall $(QTSIXA_SHANWAN_INCLUDES) 
+
+	
+QTSIXA_SHANWAN_LIBS = -ldl -lpthread -lz -L$(STAGING_DIR)/usr/lib -lbcm_host -lvcos -lvchiq_arm -lrt -lvchostif -lusb -lbluetooth
+
+define QTSIXA_SHANWAN_BUILD_CMDS
+	# Make SHANWAN
+	$(SED) "s|/usr/.\+\?/sixad-remote|/usr/sixad/shanwan/sixad-remote|g" $(@D)/sixad/bluetooth.cpp
+	$(SED) "s|/usr/.\+\?/sixad-sixaxis|/usr/sixad/shanwan/sixad-sixaxis|g" $(@D)/sixad/bluetooth.cpp
+	$(MAKE) CXX="$(TARGET_CXX)" \
+		CXXFLAGS="$(TARGET_CFLAGS) $(QTSIXA_SHANWAN_CFLAGS) -DSHANWAN_FAKE_DS3" \
+		LIBS="$(QTSIXA_SHANWAN_LIBS)" INSTALLDIR="shanwan" BINDIR="shanwan"\
+		-C $(@D)/sixad all
+endef
+
+define QTSIXA_SHANWAN_INSTALL_TARGET_CMDS
+	$(MAKE) INSTALLDIR="shanwan" BINDIR="shanwan" DESTDIR=$(TARGET_DIR) -C $(@D)/sixad install 
+endef
+
+define QTSIXA_SHANWAN_RPI_FIXUP
+	$(SED) 's|WANT_JACK = true|WANT_JACK = false|g' $(@D)/utils/Makefile
+	$(SED) 's|`pkg-config --cflags --libs libusb`| $(QTSIXA_SHANWAN_LIBS) |g' $(@D)/utils/Makefile
+	$(SED) 's|`pkg-config --cflags --libs bluez`| $(QTSIXA_SHANWAN_INCLUDES) $(QTSIXA_SHANWAN_LIBS) |g' $(@D)/sixad/Makefile
+endef
+
+QTSIXA_SHANWAN_PRE_CONFIGURE_HOOKS += QTSIXA_SHANWAN_RPI_FIXUP
+
+$(eval $(generic-package))
