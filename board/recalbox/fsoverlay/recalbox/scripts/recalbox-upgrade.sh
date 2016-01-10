14d376070d2824abdf324dc54d1e2c5983317517
Nicolas Adenis-Lamarre <nicolas.adenis-lamarre@gmail.com>
2016-01-10 19:26:00 +0100

    Upgrade: replace the old script
    Signed-off-by: Nicolas Adenis-Lamarre <nicolas.adenis-lamarre@gmail.com>

---
+++ board/recalbox/fsoverlay/recalbox/scripts/recalbox-upgrade.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+version=$(cat /recalbox/recalbox.arch)
+
+if ! mkdir -p /recalbox/share/system/upgrade
+then
+    exit 1
+fi
+
+if ! wget "http://archive2.recalbox.com/4.0.0/last/recalbox-${version}/boot.tar.xz" -O /recalbox/share/system/upgrade/boot.tar.xz.part
+then
+    exit 1
+fi
+
+if ! wget "http://archive2.recalbox.com/4.0.0/last/recalbox-${version}/root.tar.xz" -O /recalbox/share/system/upgrade/root.tar.xz.part
+then
+    rm "/recalbox/share/system/upgrade/boot.tar.xz.part"
+    exit 1
+fi
+
+if ! mv /recalbox/share/system/upgrade/boot.tar.xz.part /recalbox/share/system/upgrade/boot.tar.xz
+then
+    rm /recalbox/share/system/upgrade/boot.tar.xz.part
+    rm /recalbox/share/system/upgrade/root.tar.xz.part
+    exit 1
+fi
+
+if ! mv /recalbox/share/system/upgrade/root.tar.xz.part /recalbox/share/system/upgrade/root.tar.xz
+then
+    rm /recalbox/share/system/upgrade/boot.tar.xz
+    rm /recalbox/share/system/upgrade/root.tar.xz.part
+    exit 1
+fi
+
+exit 0
