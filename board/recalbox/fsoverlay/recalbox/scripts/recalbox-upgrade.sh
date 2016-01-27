6d562818dd87ef67f078911a1041c049b8a2e345
Nicolas Adenis-Lamarre <nicolas.adenis-lamarre@gmail.com>
2016-01-27 22:40:15 +0100

    upgrade : implement the stable/unstable/nightly system
    Signed-off-by: Nicolas Adenis-Lamarre <nicolas.adenis-lamarre@gmail.com>

--- board/recalbox/fsoverlay/recalbox/scripts/recalbox-upgrade.sh
+++ board/recalbox/fsoverlay/recalbox/scripts/recalbox-upgrade.sh
@@ -1,18 +1,28 @@
 #!/bin/bash
 
-version=$(cat /recalbox/recalbox.arch)
+systemsetting="python /usr/lib/python2.7/site-packages/configgen/settings/recalboxSettings.pyc"
+
+arch=$(cat /recalbox/recalbox.arch)
+majorversion=4
+updatetype="`$systemsetting  -command load -key updates.type`"
+
+if test "${updatetype}" != "stable" -a "${updatetype}" != "unstable" -a "${updatetype}" != "nightly"
+then
+    # force a default value in case the value is removed or miswritten
+    updatetype="stable"
+fi
 
 if ! mkdir -p /recalbox/share/system/upgrade
 then
     exit 1
 fi
 
-if ! wget "http://archive2.recalbox.com/4.0.0/last/${version}/boot.tar.xz" -O /recalbox/share/system/upgrade/boot.tar.xz.part
+if ! wget "http://archive2.recalbox.com/${majorversion}/${updatetype}/last/${arch}/boot.tar.xz" -O /recalbox/share/system/upgrade/boot.tar.xz.part
 then
     exit 1
 fi
 
-if ! wget "http://archive2.recalbox.com/4.0.0/last/${version}/root.tar.xz" -O /recalbox/share/system/upgrade/root.tar.xz.part
+if ! wget "http://archive2.recalbox.com/${majorversion}/${updatetype}/last/${arch}/root.tar.xz" -O /recalbox/share/system/upgrade/root.tar.xz.part
 then
     rm "/recalbox/share/system/upgrade/boot.tar.xz.part"
     exit 1
