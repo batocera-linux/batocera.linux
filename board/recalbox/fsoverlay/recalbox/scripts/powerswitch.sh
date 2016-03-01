2b1ab94edc3b22c4cb5a8521abc6f628f8105762
Laurent Marchelli <laurent.marchelli@gmail.com>
2016-03-01 16:46:57 +0100

    Code for 5/6 switch removed.

--- board/recalbox/fsoverlay/recalbox/scripts/powerswitch.sh
+++ board/recalbox/fsoverlay/recalbox/scripts/powerswitch.sh
@@ -151,36 +151,6 @@ msldigital_stop()
     done
 }
 
-# http://raspberrypi.stackexchange.com/questions/13203/creating-halt-wake-button
-raspberry_start()
-{
-    # Init GPIO : 
-    # $1 is the GPIO pin receiving the shut-down signal
-    echo "$1" > /sys/class/gpio/export
-    echo "in" > /sys/class/gpio/gpio$1/direction
-
-    # Wait for switch off signal 
-    power=1
-    while [ "$power" != "0" ]; do
-        sleep 1
-        power=$(cat /sys/class/gpio/gpio$1/value)
-    done
-
-    # Switch off
-    if [ "$?" = "0" ]; then
-        touch "/tmp/poweroff.please"
-        shutdown -h now
-    fi
-}
-
-raspberry_stop()
-{
-    # Cleanup GPIO init
-    for i in $*; do
-        echo "$i" > /sys/class/gpio/unexport
-    done
-}
-
 # First parameter must be start or stop
 if [[ "$1" != "start" && $1 != "stop" ]]; then
     exit 1
@@ -205,13 +175,5 @@ case "$CONFVALUE" in
     ;;
     "REMOTEPIBOARD_2005")
         msldigital_$1 14
-    ;;
-    "RPI_HALTWAKE")
-        # Getting the Halt GPIO pin number (syntax : system.power.switch=RPI_HALTWAKE,$GPIOvalue)
-        # Only GPIO 2 to 27 are supported on the Rasperry Pi, other numbers will be ignored
-        GPIOvalue=$(sed -rn "s/^$CONFPARAM=$CONFVALUE[ \t]*,([2-9]|1[0-9]|2[0-7])\s.*$/\1/p" $CONFFILE | tail -n 1)
-        if [ "" != "$GPIOvalue" ]; then 
-            raspberry_$1 $GPIOvalue; 
-        fi
     ;;
 esac
