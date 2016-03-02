4a6a87f1ab691399c39f9676c2301aaebc97a809
MikaXII <mikael.joubert@supinfo.com>
2016-03-02 12:28:49 +0100

    Update wiringpi.mk

--- package/wiringpi/wiringpi.mk
+++ package/wiringpi/wiringpi.mk
@@ -19,19 +19,28 @@ define WIRINGPI_BUILD_CMDS
      $(MAKE) CC="$(TARGET_CC)" LD="$(TARGET_LD)"  -C $(@D)/gpio all
 endef
 
+define WIRINGPI_INSTALL_STAGING_CMDS
+     $(INSTALL) -D -m 0644  $(@D)/wiringPi/*.h $(STAGING_DIR)/usr/include
+     $(INSTALL) -D -m 0644  $(@D)/devLib/*.h $(STAGING_DIR)/usr/include
+    # $(INSTALL) -D -m 0755 $(@D)/libfoo.a $(STAGING_DIR)/usr/lib/libfoo.a
+    # $(INSTALL) -D -m 0644 $(@D)/foo.h $(STAGING_DIR)/usr/include/foo.h
+    # $(INSTALL) -D -m 0755 $(@D)/libfoo.so* $(STAGING_DIR)/usr/lib
+endef
+
+
 define WIRINGPI_INSTALL_TARGET_CMDS
 	
      cd $(@D)
 	 VERSION=$(shell cat ./VERSION)
 	 
 	 # Install wiringPi lib
-	 $(INSTALL) -D -m 0644  $(@D)/wiringPi/*.h $(TARGET_DIR)/usr/include
-     $(INSTALL) -D -m 0755 $(@D)/wiringPi/libwiringPi.so.$(VERSION) $(TARGET_DIR)/usr/lib
+	 #$(INSTALL) -D -m 0644  $(@D)/wiringPi/*.h $(TARGET_DIR)/usr/include
+     	 $(INSTALL) -D -m 0755 $(@D)/wiringPi/libwiringPi.so.$(VERSION) $(TARGET_DIR)/usr/lib
 	 ln -sf $(TARGET_DIR)/usr/lib/libwiringPi.so.$(VERSION) $(TARGET_DIR)/lib/libwiringPi.so
 	 
 	 # Install device lib
-	 $(INSTALL) -D -m 0644  $(@D)/devLib/*.h $(TARGET_DIR)/usr/include
-     $(INSTALL) -D -m 0755 $(@D)/devLib/libwiringPiDev.so.$(VERSION) $(TARGET_DIR)/usr/lib
+	 #$(INSTALL) -D -m 0644  $(@D)/devLib/*.h $(TARGET_DIR)/usr/include
+         $(INSTALL) -D -m 0755 $(@D)/devLib/libwiringPiDev.so.$(VERSION) $(TARGET_DIR)/usr/lib
 	 ln -sf $(TARGET_DIR)/usr/lib/libwiringPiDev.so.$(VERSION) $(TARGET_DIR)/lib/libwiringPiDev.so
 	 
 	 
