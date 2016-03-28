c6a356e410353288a0677b2817b46da665f5f0d8
rockaddicted <nirvana_paradize@hotmail.com>
2016-03-28 17:23:59 +0200

    fixed libretro-mgba freeze issue

--- package/libretro-mgba/libretro-mgba.mk
+++ package/libretro-mgba/libretro-mgba.mk
@@ -3,11 +3,17 @@
 # MGBA
 #
 ################################################################################
-LIBRETRO_MGBA_VERSION = 3b137e25f2368a57da02bc0489e83a90788fc23a
+LIBRETRO_MGBA_VERSION = 851dbb1b5138b2258168cb3c08a740e04313c409
 LIBRETRO_MGBA_SITE = $(call github,libretro,mgba,$(LIBRETRO_MGBA_VERSION))
 
+ifeq ($(BR2_ARM_CPU_HAS_NEON),y)
+	LIBRETRO_MGBA_NEON += "HAVE_NEON=1"
+else
+	LIBRETRO_MGBA_NEON += "HAVE_NEON=0"
+endif
+
 define LIBRETRO_MGBA_BUILD_CMDS
-	CFLAGS="$(TARGET_CFLAGS)" CXXFLAGS="$(TARGET_CXXFLAGS)" $(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" -C $(@D)/ -f Makefile.libretro platform="$(LIBRETRO_PLATFORM)"
+	CFLAGS="$(TARGET_CFLAGS)" CXXFLAGS="$(TARGET_CXXFLAGS)" $(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" -C $(@D)/ -f Makefile.libretro platform="$(LIBRETRO_PLATFORM)" $(LIBRETRO_MGBA_NEON)
 endef
 
 define LIBRETRO_MGBA_INSTALL_TARGET_CMDS
