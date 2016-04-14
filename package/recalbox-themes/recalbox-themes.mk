aeeaec9c896207f11511de974ef2cd7da450c10c
digitalLumberjack <digitallumberjack@gmail.com>
2016-04-14 13:04:52 +0200

    recalbox-themes package : fixed unexisting directory

--- package/recalbox-themes/recalbox-themes.mk
+++ package/recalbox-themes/recalbox-themes.mk
@@ -8,6 +8,7 @@ RECALBOX_THEMES_VERSION = 4.0.X
 RECALBOX_THEMES_SITE = $(call github,recalbox,recalbox-themes,$(RECALBOX_THEMES_VERSION))
 
 define RECALBOX_THEMES_INSTALL_TARGET_CMDS
+        mkdir -p $(TARGET_DIR)/recalbox/share_init/system/.emulationstation/themes/
 	cp -r $(@D)/themes/* \
 		$(TARGET_DIR)/recalbox/share_init/system/.emulationstation/themes/
 endef
