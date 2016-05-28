66f45e08f51252fddeb23073c35c652c64d92c2f
Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>
2016-05-28 17:16:37 +0200

    qtsixa: remove rpi dependancies when not on rpi
    Signed-off-by: Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>

--- package/qtsixa-shanwan/qtsixa-shanwan.mk
+++ package/qtsixa-shanwan/qtsixa-shanwan.mk
@@ -8,11 +8,16 @@ QTSIXA_SHANWAN_SITE = $(call github,yarick123,qtsixa,$(QTSIXA_SHANWAN_VERSION))
 QTSIXA_SHANWAN_DEPENDENCIES = sdl linux-headers qtsixa
 PKGCONFIG_CONFIG=$(STAGING_DIR)/usr/lib/pkgconfig
 
-QTSIXA_SHANWAN_INCLUDES =-I$(STAGING_DIR)/usr/include -I$(STAGING_DIR)/usr/include/interface/vcos/pthreads -I$(STAGING_DIR)/usr/include/interface/vmcs_host/linux
-QTSIXA_SHANWAN_CFLAGS = -D__ARM_PCS_VFP -DARM_ARCH -DRPI_BUILD -Wall $(QTSIXA_SHANWAN_INCLUDES) 
+QTSIXA_SHANWAN_INCLUDES =-I$(STAGING_DIR)/usr/include
+QTSIXA_SHANWAN_CFLAGS = -D__ARM_PCS_VFP -DARM_ARCH -Wall $(QTSIXA_SHANWAN_INCLUDES)
+QTSIXA_SHANWAN_LIBS = -ldl -lpthread -lz -L$(STAGING_DIR)/usr/lib -lrt -lusb -lbluetooth
 
-	
-QTSIXA_SHANWAN_LIBS = -ldl -lpthread -lz -L$(STAGING_DIR)/usr/lib -lbcm_host -lvcos -lvchiq_arm -lrt -lvchostif -lusb -lbluetooth
+ifeq ($(BR2_PACKAGE_RPI_USERLAND),y)
+	QTSIXA_INCLUDES +=-I$(STAGING_DIR)/usr/include/interface/vcos/pthreads -I$(STAGING_DIR)/usr/include/interface/vmcs_host/linux
+	QTSIXA_CFLAGS += -DRPI_BUILD
+	QTSIXA_LIBS += -lbcm_host -lvcos -lvchiq_arm -lvchostif
+endif
+
 
 define QTSIXA_SHANWAN_BUILD_CMDS
 	# Make SHANWAN
