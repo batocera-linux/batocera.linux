73f4c99ef838e995c4a2dd532d9bb4f9352c228b
Yann Morère <yann.morere@univ-lorraine.fr>
2016-07-11 13:17:49 +0200

    Update mame2003 core to get the mame2003-skip_warnings option^Cn avoid the licence splash screen

--- package/libretro-mame2003/libretro-mame2003.mk
+++ package/libretro-mame2003/libretro-mame2003.mk
@@ -3,7 +3,8 @@
 # MAME2003
 #
 ################################################################################
-LIBRETRO_MAME2003_VERSION = 3ddeba45f648293a4b860f3283762e8a30c34903
+#LIBRETRO_MAME2003_VERSION = 3ddeba45f648293a4b860f3283762e8a30c34903
+LIBRETRO_MAME2003_VERSION = ae84f5d45fa6ff4beded68cfd19a4217f595703c
 LIBRETRO_MAME2003_SITE = $(call github,libretro,mame2003-libretro,$(LIBRETRO_MAME2003_VERSION))
 
 
@@ -15,7 +16,7 @@ define LIBRETRO_MAME2003_BUILD_CMDS
 endef
 
 define LIBRETRO_MAME2003_INSTALL_TARGET_CMDS
-	$(INSTALL) -D $(@D)/mame078_libretro.so \
+	$(INSTALL) -D $(@D)/mame2003_libretro.so \
 		$(TARGET_DIR)/usr/lib/libretro/mame078_libretro.so
 endef
 
