bf561555a68901c5732614363474eaf5991c64a8
Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>
2016-07-13 20:13:36 +0200

    jstest : add to the recalbox-support script
    Signed-off-by: Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>

--- board/recalbox/fsoverlay/recalbox/scripts/recalbox-support.sh
+++ board/recalbox/fsoverlay/recalbox/scripts/recalbox-support.sh
@@ -8,13 +8,13 @@ DHOME="/recalbox/share/system"
 if test $# -eq 1
 then
     REPORTNAME=$(basename "$1" | sed -e s+'^\([^.]*\)\..*$'+'\1'+)
-    OUTPUTFILE="$1"
+    OUTPUTFILE=$1
 else
     REPORTNAME="recalbox-support-"$(date +%Y%m%d%H%M%S)
-    OUTPUTFILE="/recalbox/share/saves/""$REPORTNAME"".tar.gz"
+    OUTPUTFILE="/recalbox/share/saves/${REPORTNAME}.tar.gz"
 fi
 
-TMPDIR="$GTMP""/""$REPORTNAME"
+TMPDIR="${GTMP}/${REPORTNAME}"
 
 f_cp() {
     test -e "$1" && cp "$1" "$2"
@@ -25,9 +25,9 @@ d_cp() {
 }
 
 
-if mkdir "$TMPDIR" && mkdir "$TMPDIR"/{system,joysticks,lirc,kodi}
+if mkdir "${TMPDIR}" && mkdir "${TMPDIR}/"{system,joysticks,lirc,kodi}
 then
-    if ! cd "$TMPDIR"
+    if ! cd "${TMPDIR}"
     then
 	echo "Change directory failed" >&2
 	exit 1
@@ -38,56 +38,58 @@ else
 fi
 
 # SYSTEM
-DSYSTEM="$TMPDIR""/system"
-dmesg 	         	> "$DSYSTEM""/dmesg.txt"
-lsmod 	         	> "$DSYSTEM""/lsmod.txt"
-ps    	         	> "$DSYSTEM""/ps.txt"
-df -h 	         	> "$DSYSTEM""/df.txt"
-netstat -tuan    	> "$DSYSTEM""/netstat.txt"
-lsusb -v         	> "$DSYSTEM""/lsusb.txt" 2>/dev/null
-tvservice -m CEA 	> "$DSYSTEM""/tvservice-CEA.txt"
-tvservice -m DMT 	> "$DSYSTEM""/tvservice-DMT.txt"
-ifconfig -a             > "$DSYSTEM""/ifconfig.txt"
-connmanctl technologies > "$DSYSTEM""/connman-technologies.txt"
-connmanctl services     > "$DSYSTEM""/connman-services.txt"
-f_cp /recalbox/recalbox.version                               "$DSYSTEM"
-f_cp /recalbox/recalbox.arch                                  "$DSYSTEM"
-f_cp /boot/config.txt                                         "$DSYSTEM"
-f_cp /recalbox/share/system/recalbox.conf                     "$DSYSTEM"
-f_cp /recalbox/share/system/logs/recalbox.log                 "$DSYSTEM"
-f_cp /var/log/messages                                        "$DSYSTEM"
-f_cp /recalbox/share/system/.emulationstation/es_settings.cfg "$DSYSTEM"
-f_cp /recalbox/share/system/.emulationstation/es_log.txt      "$DSYSTEM"
-f_cp /recalbox/share/system/.emulationstation/es_input.cfg    "$DSYSTEM"
-f_cp /boot/recalbox-boot.conf                                 "$DSYSTEM"
+DSYSTEM="${TMPDIR}/system"
+dmesg 	         	> "${DSYSTEM}/dmesg.txt"
+lsmod 	         	> "${DSYSTEM}/lsmod.txt"
+ps    	         	> "${DSYSTEM}/ps.txt"
+df -h 	         	> "${DSYSTEM}/df.txt"
+netstat -tuan    	> "${DSYSTEM}/netstat.txt"
+lsusb -v         	> "${DSYSTEM}/lsusb.txt" 2>/dev/null
+tvservice -m CEA 	> "${DSYSTEM}/tvservice-CEA.txt"
+tvservice -m DMT 	> "${DSYSTEM}/tvservice-DMT.txt"
+ifconfig -a             > "${DSYSTEM}/ifconfig.txt"
+connmanctl technologies > "${DSYSTEM}/connman-technologies.txt"
+connmanctl services     > "${DSYSTEM}/connman-services.txt"
+f_cp /recalbox/recalbox.version                               "${DSYSTEM}"
+f_cp /recalbox/recalbox.arch                                  "${DSYSTEM}"
+f_cp /boot/config.txt                                         "${DSYSTEM}"
+f_cp /recalbox/share/system/recalbox.conf                     "${DSYSTEM}"
+f_cp /recalbox/share/system/logs/recalbox.log                 "${DSYSTEM}"
+f_cp /var/log/messages                                        "${DSYSTEM}"
+f_cp /recalbox/share/system/.emulationstation/es_settings.cfg "${DSYSTEM}"
+f_cp /recalbox/share/system/.emulationstation/es_log.txt      "${DSYSTEM}"
+f_cp /recalbox/share/system/.emulationstation/es_input.cfg    "${DSYSTEM}"
+f_cp /boot/recalbox-boot.conf                                 "${DSYSTEM}"
 
 # joysticks
-DJOYS="$TMPDIR""/joysticks"
-find /dev/input > "$DJOYS""/inputs.txt"
+DJOYS="${TMPDIR}/joysticks"
+find /dev/input > "${DJOYS}/inputs.txt"
 for J in /dev/input/event*
 do
-    N=$(basename $J)
-    evtest --info "$J"          > "$DJOYS""/evtest.""$N"".txt"
-    udevadm info -q all -n "$J" > "$DJOYS""/udevadm.""$N"".txt"
+    N=$(basename ${J})
+    evtest --info "${J}"          > "${DJOYS}/evtest.${N}.txt"
+    udevadm info -q all -n "${J}" > "${DJOYS}/udevadm.${N}.txt"
 done
+sdl2-jstest -l > "${DJOYS}/sdl2-jstest.txt"
+
 
 # lirc
-DLIRC="$TMPDIR""/lirc"
-f_cp "$DHOME""/.config/lirc/lircd.conf" "$DLIRC"
+DLIRC="${TMPDIR}/lirc"
+f_cp "${DHOME}/.config/lirc/lircd.conf" "${DLIRC}"
 
 # kodi
-DKODI="$TMPDIR""/kodi"
-f_cp "$DHOME""/.kodi/userdata/Lircmap.xml"          "$DKODI"
-f_cp "$DHOME""/.kodi/userdata/keymaps/recalbox.xml" "$DKODI"
-d_cp "$DHOME""/.kodi/userdata/remotes"              "$DKODI"
-f_cp "$DHOME""/.kodi/temp/kodi.log"                 "$DKODI"
+DKODI="${TMPDIR}/kodi"
+f_cp "${DHOME}/.kodi/userdata/Lircmap.xml"          "${DKODI}"
+f_cp "${DHOME}/.kodi/userdata/keymaps/recalbox.xml" "${DKODI}"
+d_cp "${DHOME}/.kodi/userdata/remotes"              "${DKODI}"
+f_cp "${DHOME}/.kodi/temp/kodi.log"                 "${DKODI}"
 
-if ! (cd "$GTMP" && tar cf -  "$REPORTNAME" | gzip -c > "$OUTPUTFILE")
+if ! (cd "${GTMP}" && tar cf -  "${REPORTNAME}" | gzip -c > "${OUTPUTFILE}")
 then
     echo "Reporting zip creation failed" >&2
     exit 1
 fi
 
-rm -rf "$TMPDIR"
-echo "$OUTPUTFILE"
+rm -rf "${TMPDIR}"
+echo "${OUTPUTFILE}"
 exit 0
