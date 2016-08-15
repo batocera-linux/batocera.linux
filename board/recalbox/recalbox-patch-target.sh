41d27b2ed44747db854f1204e7f745295ffa53eb
Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>
2016-08-15 12:10:28 +0200

    gpu: remove gles packages / add all xorg video drivers
    Signed-off-by: Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>

--- board/recalbox/recalbox-patch-target.sh
+++ board/recalbox/recalbox-patch-target.sh
@@ -22,6 +22,9 @@ ln -sf "/recalbox/share/cheats"                                       "${TARGET_
 # we don't want the kodi startup script
 rm -f "${TARGET_DIR}/etc/init.d/S50kodi" || exit 1
 
+# we don't want default xorg files
+rm -f "${TARGET_DIR}/etc/X11/xorg.conf" || exit 1
+
 # we want an empty boot directory (grub installation copy some files in the target boot directory)
 rm -rf "${TARGET_DIR}/boot/grub" || exit 1
 
