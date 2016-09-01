1166afe05a2f9d0e3ac8eadda4e611b21f6965ac
Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>
2016-09-01 22:13:56 +0200

    recalbox-support : add the xorg.log if it exists
    Signed-off-by: Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>

--- board/recalbox/fsoverlay/recalbox/scripts/recalbox-support.sh
+++ board/recalbox/fsoverlay/recalbox/scripts/recalbox-support.sh
@@ -60,6 +60,7 @@ f_cp /recalbox/share/system/.emulationstation/es_settings.cfg "${DSYSTEM}"
 f_cp /recalbox/share/system/.emulationstation/es_log.txt      "${DSYSTEM}"
 f_cp /recalbox/share/system/.emulationstation/es_input.cfg    "${DSYSTEM}"
 f_cp /boot/recalbox-boot.conf                                 "${DSYSTEM}"
+f_cp /var/log/Xorg.0.log                                      "${DSYSTEM}"
 
 # joysticks
 DJOYS="${TMPDIR}/joysticks"
