a1b30f0f305f67d238854dd2b009d23030249485
Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>
2017-02-17 23:56:19 +0100

    lsblk: add on host, so that the patch works
    Signed-off-by: Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>

--- package/util-linux/util-linux.mk
+++ package/util-linux/util-linux.mk
@@ -144,6 +144,8 @@ else
 HOST_UTIL_LINUX_CONF_OPTS += --disable-all-programs
 endif
 
+HOST_UTIL_LINUX_CONF_OPTS += --enable-libsmartcols
+
 # Install libmount Python bindings
 ifeq ($(BR2_PACKAGE_PYTHON)$(BR2_PACKAGE_PYTHON3),y)
 UTIL_LINUX_CONF_OPTS += --with-python
