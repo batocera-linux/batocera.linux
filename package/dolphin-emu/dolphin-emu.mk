1f1311690c46fc42144ecbe457d56a5ed4194dc3
Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>
2017-03-07 21:42:39 +0100

    dolphin: fix the pad spaces
    Signed-off-by: Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>

--- package/dolphin-emu/dolphin-emu.mk
+++ package/dolphin-emu/dolphin-emu.mk
@@ -4,9 +4,10 @@
 #
 ################################################################################
 
-DOLPHIN_EMU_VERSION = 5.0
+# 5.0 + pad name bug fixed
+DOLPHIN_EMU_VERSION = 7d681f9c931b63f3ecec36c1ef515dc747df7ac4
 DOLPHIN_EMU_SITE = $(call github,dolphin-emu,dolphin,$(DOLPHIN_EMU_VERSION))
-DOLPHIN_EMU_DEPENDENCIES = xserver_xorg-server libevdev ffmpeg zlib libpng lzo libusb libcurl sfml bluez5_utils wxwidgets
+DOLPHIN_EMU_DEPENDENCIES = xserver_xorg-server libevdev ffmpeg zlib libpng lzo libusb libcurl sfml bluez5_utils wxwidgets hidapi
 
 # bluez is disable otherwise a compilation error appears
 DOLPHIN_EMU_CONF_OPTS = -DOPENGL_INCLUDE_DIR=$(STAGING_DIR)/usr/include
