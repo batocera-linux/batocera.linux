ab0b16d9c7c7f49b515602e293a8baa78e293768
Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>
2017-03-08 23:28:40 +0100

    scummvm: add for c2 xu4 x86 x86_64
    Signed-off-by: Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>

--- package/scummvm/scummvm.mk
+++ package/scummvm/scummvm.mk
@@ -13,12 +13,17 @@ SCUMMVM_LICENSE = GPL2
 SCUMMVM_DEPENDENCIES = sdl2 zlib jpeg-turbo libmpeg2 libogg libvorbis flac libmad libpng libtheora \
 	faad2 fluidsynth freetype 
 
-SCUMMVM_ADDITIONAL_FLAGS= -I$(STAGING_DIR)/usr/include -I$(STAGING_DIR)/usr/include/interface/vcos/pthreads -I$(STAGING_DIR)/usr/include/interface/vmcs_host/linux -lpthread -lm -L$(STAGING_DIR)/usr/lib -lbcm_host -lGLESv2 -lEGL -lvchostif 
+SCUMMVM_ADDITIONAL_FLAGS= -I$(STAGING_DIR)/usr/include -I$(STAGING_DIR)/usr/include/interface/vcos/pthreads -I$(STAGING_DIR)/usr/include/interface/vmcs_host/linux -lpthread -lm -L$(STAGING_DIR)/usr/lib -lGLESv2 -lEGL
+
+ifeq ($(BR2_PACKAGE_RECALBOX_TARGET_RPI1)$(BR2_PACKAGE_RECALBOX_TARGET_RPI2)$(BR2_PACKAGE_RECALBOX_TARGET_RPI3),y)
+	SCUMMVM_ADDITIONAL_FLAGS += -lbcm_host -lvchostif
+	SCUMMVM_CONF_OPTS += --host=raspberrypi
+endif
 
 SCUMMVM_CONF_ENV += RANLIB="$(TARGET_RANLIB)" STRIP="$(TARGET_STRIP)" AR="$(TARGET_AR) cru" AS="$(TARGET_AS)"
 SCUMMVM_CONF_OPTS += --enable-opengl --disable-debug --enable-optimizations --disable-mt32emu --enable-flac --enable-mad --enable-vorbis --disable-tremor \
-		--disable-fluidsynth --disable-taskbar --disable-timidity --disable-alsa --enable-vkeybd --enable-keymapper \
-                --prefix=/usr --host=raspberrypi --with-sdl-prefix="$(STAGING_DIR)/usr/bin/" --enable-release \
+		--disable-fluidsynth --disable-taskbar --disable-timidity --disable-alsa --enable-vkeybd --enable-keymapper --disable-eventrecorder \
+                --prefix=/usr --with-sdl-prefix="$(STAGING_DIR)/usr/bin/" --enable-release \
 
 SCUMMVM_MAKE_OPTS += RANLIB="$(TARGET_RANLIB)" STRIP="$(TARGET_STRIP)" AR="$(TARGET_AR) cru" AS="$(TARGET_AS)" LD="$(TARGET_CXX)"
 
