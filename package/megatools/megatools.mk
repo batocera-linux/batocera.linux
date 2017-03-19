1c01198b71736f607cb0e6ca763b88986ab0a3c0
Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>
2017-03-19 21:00:51 +0100

    megatools: requirements
    Signed-off-by: Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>

--- package/megatools/megatools.mk
+++ package/megatools/megatools.mk
@@ -7,6 +7,7 @@ MEGATOOLS_VERSION = 1.9.97
 MEGATOOLS_SOURCE = megatools-$(MEGATOOLS_VERSION).tar.gz
 MEGATOOLS_SITE = https://megatools.megous.com/builds
 MEGATOOLS_LICENCE = GPLv2
+MEGATOOLS_DEPENDENCIES = openssl
 
 MEGATOOLS_CONF_OPTS = \
     --prefix=/usr
