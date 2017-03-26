bb3be7ed3a0de7052aad2a3a86b288765701370d
Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>
2017-03-26 21:12:02 +0200

    wxwidgets: fix the config path
    Signed-off-by: Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>

--- package/wxwidgets/wxwidgets.mk
+++ package/wxwidgets/wxwidgets.mk
@@ -9,4 +9,10 @@ WXWIDGETS_SITE = $(call github,wxWidgets,wxWidgets,$(WXWIDGETS_VERSION))
 WXWIDGETS_DEPENDENCIES = zlib libpng jpeg gdk-pixbuf libgtk2
 WXWIDGETS_INSTALL_STAGING = YES
 
+define WXWIDGETS_FIXUP_WXWIDGET_CONFIG
+	ln -sf $(STAGING_DIR)/usr/lib/wx/config/x86_64-buildroot-linux-gnu-gtk2-unicode-3.1 $(STAGING_DIR)/usr/bin/wx-config
+endef
+
+WXWIDGETS_POST_INSTALL_STAGING_HOOKS += WXWIDGETS_FIXUP_WXWIDGET_CONFIG
+
 $(eval $(autotools-package))
