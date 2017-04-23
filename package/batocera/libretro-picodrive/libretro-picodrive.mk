14178479991789e900922e899c15be536ef984e7
Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>
2017-04-23 21:53:29 +0200

    picodrive: git submodules
    Signed-off-by: Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>

--- package/batocera/libretro-picodrive/libretro-picodrive.mk
+++ package/batocera/libretro-picodrive/libretro-picodrive.mk
@@ -4,19 +4,11 @@
 #
 ################################################################################
 LIBRETRO_PICODRIVE_VERSION = f0a3d0dc4bd9060eb6bfdb16fb20dbb80a9e6ae9
-LIBRETRO_PICODRIVE_SITE = $(call github,libretro,picodrive,$(LIBRETRO_PICODRIVE_VERSION))
+#LIBRETRO_PICODRIVE_SITE = $(call github,libretro,picodrive,$(LIBRETRO_PICODRIVE_VERSION))
+LIBRETRO_PICODRIVE_SITE = https://github.com/libretro/picodrive.git
+LIBRETRO_PICODRIVE_SITE_METHOD=git
+LIBRETRO_PICODRIVE_GIT_SUBMODULES=YES
 LIBRETRO_PICODRIVE_DEPENDENCIES = libpng sdl
-
-define LIBRETRO_PICODRIVE_CONFIGURE_CMDS
-	rm -rf $(@D)/picodrive
-	git -C $(@D) clone https://github.com/libretro/picodrive
-	#git -C $(@D) checkout $(LIBRETRO_PICODRIVE_VERSION)
-	#cp -r $(@D)/../picodrivegithub/.git $(@D)/
-	git -C $(@D)/picodrive submodule update --init
-	##( cd $(@D)/picodrive && \
-        ##CC="$(TARGET_CC)" CXX="$(TARGET_CXX)" CFLAGS="$(TARGET_CFLAGS)" \
-	##./configure )
-endef
 
 PICOPLATFORM=$(LIBRETRO_PLATFORM)
 
@@ -39,15 +31,14 @@ ifeq ($(BR2_cortex_a15),y)
 endif
 
 define LIBRETRO_PICODRIVE_BUILD_CMDS
-	$(MAKE) -C $(@D)/picodrive/cpu/cyclone CONFIG_FILE=$(@D)/picodrive/cpu/cyclone_config.h	
-	CFLAGS="$(TARGET_CFLAGS)" CXXFLAGS="$(TARGET_CXXFLAGS)" $(MAKE) CC="$(TARGET_CC)" CXX="$(TARGET_CXX)" -C  $(@D)/picodrive -f Makefile.libretro platform="$(PICOPLATFORM)"
+	$(MAKE) -C $(@D)/cpu/cyclone CONFIG_FILE=$(@D)/cpu/cyclone_config.h	
+	CFLAGS="$(TARGET_CFLAGS)" CXXFLAGS="$(TARGET_CXXFLAGS)" $(MAKE) CC="$(TARGET_CC)" CXX="$(TARGET_CXX)" -C  $(@D) -f Makefile.libretro platform="$(PICOPLATFORM)"
 endef
 
 define LIBRETRO_PICODRIVE_INSTALL_TARGET_CMDS
-	$(INSTALL) -D $(@D)/picodrive/picodrive_libretro.so \
+	$(INSTALL) -D $(@D)/picodrive_libretro.so \
 		$(TARGET_DIR)/usr/lib/libretro/picodrive_libretro.so
 endef
-#LIBRETRO_PICODRIVE_PRE_CONFIGURE_HOOKS +=  LIBRETRO_PICODRIVE_GITHUBHACK
+
 $(eval $(generic-package))
-#$(eval $(autotools-package))
 
