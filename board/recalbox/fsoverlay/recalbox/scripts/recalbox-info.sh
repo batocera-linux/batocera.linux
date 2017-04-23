57f996ebe385a72798fc4f6ccce247139a3686a3
Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>
2017-04-23 22:07:51 +0200

    recalbox-info.sh: add system
    Signed-off-by: Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>

--- board/recalbox/fsoverlay/recalbox/scripts/recalbox-info.sh
+++ board/recalbox/fsoverlay/recalbox/scripts/recalbox-info.sh
@@ -1,17 +1,9 @@
 #!/bin/bash
 
 V_ARCH=$(cat /recalbox/recalbox.arch)
-V_OPENGLVERSION=$(DISPLAY=:0.0 glxinfo 2>/dev/null | grep -E '^OpenGL version string:' | sed -e s+'^OpenGL version string:[ ]*'++)
 V_CPUNB=$(grep -E $'^processor\t:' /proc/cpuinfo | wc -l)
 V_CPUMODEL1=$(grep -E $'^model name\t:' /proc/cpuinfo | head -1 | sed -e s+'^model name\t: '++)
-
-echo "Architecture: ${V_ARCH}"
-if test "${V_ARCH}" = "x86" -o "${V_ARCH}" = "x86_64"
-then
-    echo "OpenGL version: ${V_OPENGLVERSION}"
-fi
-echo "Cpu number: ${V_CPUNB}"
-echo "Cpu model: ${V_CPUMODEL1}"
+V_SYSTEM=$(uname -rs)
 
 # battery
 if test -e /sys/class/power_supply/BAT0/capacity
@@ -27,3 +19,13 @@ if test -n "${TEMPE}"
 then
     echo "Temperature: ${TEMPE}°"
 fi
+
+echo "Architecture: ${V_ARCH}"
+echo "System: ${V_SYSTEM}"
+if test "${V_ARCH}" = "x86" -o "${V_ARCH}" = "x86_64"
+then
+    V_OPENGLVERSION=$(DISPLAY=:0.0 glxinfo | grep -E '^OpenGL version string:' | sed -e s+'^OpenGL version string:[ ]*'++)
+    echo "OpenGL version: ${V_OPENGLVERSION}"
+fi
+echo "Cpu model: ${V_CPUMODEL1}"
+echo "Cpu number: ${V_CPUNB}"
