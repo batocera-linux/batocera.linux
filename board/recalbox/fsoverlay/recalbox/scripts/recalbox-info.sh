4e7e5370e08f8662aa5fe99b08dc035501e3ae62
Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>
2017-05-02 21:12:42 +0200

    gl version
    Signed-off-by: Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>

--- board/recalbox/fsoverlay/recalbox/scripts/recalbox-info.sh
+++ board/recalbox/fsoverlay/recalbox/scripts/recalbox-info.sh
@@ -24,8 +24,8 @@ echo "Architecture: ${V_ARCH}"
 echo "System: ${V_SYSTEM}"
 if test "${V_ARCH}" = "x86" -o "${V_ARCH}" = "x86_64"
 then
-    V_OPENGLVERSION=$(DISPLAY=:0.0 glxinfo | grep -E '^OpenGL version string:' | sed -e s+'^OpenGL version string:[ ]*'++)
-    echo "OpenGL version: ${V_OPENGLVERSION}"
+    V_OPENGLVERSION=$(DISPLAY=:0.0 glxinfo | grep -E '^OpenGL core profile version string:' | sed -e s+'^OpenGL core profile version string:[ ]*'++)
+    echo "OpenGL: ${V_OPENGLVERSION}"
 fi
 echo "Cpu model: ${V_CPUMODEL1}"
 echo "Cpu number: ${V_CPUNB}"
