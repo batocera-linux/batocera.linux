79dc63e9a3ff99be73b00ab3128d9f1894a29a36
Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>
2017-05-15 22:40:31 +0200

    fix battery & gl informatin
    Signed-off-by: Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>

--- board/recalbox/fsoverlay/recalbox/scripts/recalbox-info.sh
+++ board/recalbox/fsoverlay/recalbox/scripts/recalbox-info.sh
@@ -6,9 +6,8 @@ V_CPUMODEL1=$(grep -E $'^model name\t:' /proc/cpuinfo | head -1 | sed -e s+'^mod
 V_SYSTEM=$(uname -rs)
 
 # battery
-if test -e /sys/class/power_supply/BAT0/uevent
-then
-    BATT=$(grep -E "^POWER_SUPPLY_CAPACITY=" /sys/class/power_supply/BAT0/uevent | sed -e s+'^POWER_SUPPLY_CAPACITY='++)
+BATT=$(grep -E "^POWER_SUPPLY_CAPACITY=" /sys/class/power_supply/BAT*/uevent | sed -e s+'^POWER_SUPPLY_CAPACITY='++ | sort -rn | head -1)
+if test -n "${BATT}"
     echo "Battery: ${BATT}%"
 fi
 
@@ -25,6 +24,10 @@ echo "System: ${V_SYSTEM}"
 if test "${V_ARCH}" = "x86" -o "${V_ARCH}" = "x86_64"
 then
     V_OPENGLVERSION=$(DISPLAY=:0.0 glxinfo | grep -E '^OpenGL core profile version string:' | sed -e s+'^OpenGL core profile version string:[ ]*'++)
+    if test -z "${V_OPENGLVERSION}"
+    then
+	V_OPENGLVERSION=$(DISPLAY=:0.0 glxinfo | grep -E '^OpenGL version string:' | sed -e s+'^OpenGL version string:[ ]*'++)
+    fi
     echo "OpenGL: ${V_OPENGLVERSION}"
 fi
 echo "Cpu model: ${V_CPUMODEL1}"
