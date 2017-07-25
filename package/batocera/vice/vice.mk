f4c296bb4a9cc39deb771d7d43bc4341a2b87500
Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>
2017-07-25 19:45:28 +0200

    vice64: starting rework
    Signed-off-by: Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>

--- package/batocera/vice/vice.mk
+++ package/batocera/vice/vice.mk
@@ -3,20 +3,31 @@
 # Vice Emulation
 #
 ################################################################################
-VICE_VERSION = 2.4.24
+
+VICE_VERSION = 3.1
 VICE_SOURCE = vice-$(VICE_VERSION).tar.gz
-VICE_SITE = https://sourceforge.net/projects/vice-emu/files/development-releases
-VICE_INSTALL_STAGING = NO
-VICE_INSTALL_TARGET = YES
+VICE_SITE = https://freefr.dl.sourceforge.net/project/vice-emu/releases
 
-VICE_CONF_OPTS += \
-   --disable-option-checking \
-   --enable-sdlui \
-   --enable-native-tools=gcc \
-   CFLAGS="-march=armv7-a -mfpu=neon-vfpv4 -mfloat-abi=hard -O2"
+VICE_DEPENDENCIES = ffmpeg sdl2 libpng giflib zlib lame alsa-lib
 
-VICE_DEPENDENCIES = sdl
- 
+VICE_CONF_OPTS += --disable-option-checking
+
+
+# X11
+#ifeq ($(BR2_PACKAGE_XORG7),y)
+#	VICE_DEPENDENCIES += xlib_libXaw
+#endif
+
+# FFMPEG
+VICE_DEPENDENCIES += ffmpeg
+VICE_CONF_OPTS += --enable-external-ffmpeg
+
+VICE_CONF_OPTS += --enable-sdlui2
+VICE_CONF_OPTS += --enable-midi
+VICE_CONF_OPTS += --enable-lame
+VICE_CONF_OPTS += --with-alsa
+VICE_CONF_OPTS += --with-zlib
+VICE_CONF_OPTS += --with-jpeg
+VICE_CONF_OPTS += --with-png
+
 $(eval $(autotools-package))
-
-
