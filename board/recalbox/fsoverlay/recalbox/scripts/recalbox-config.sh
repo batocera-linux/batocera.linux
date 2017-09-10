1401e2472bdb65c05ff3208b69074854e4d97ebd
kryptalivian <kryptalivian@gmail.com>
2017-09-10 10:46:52 -0500

    added pi 3 overclock values

--- board/recalbox/fsoverlay/recalbox/scripts/recalbox-config.sh
+++ board/recalbox/fsoverlay/recalbox/scripts/recalbox-config.sh
@@ -124,6 +124,9 @@ fi
 if [ "$command" == "overclock" ]; then
 
 declare -A arm_freq
+arm_freq["rpi3-extrem"]=1350
+arm_freq["rpi3-turbo"]=1325
+arm_freq["rpi3-high"]=1300
 arm_freq["rpi2-extrem"]=1100
 arm_freq["rpi2-turbo"]=1050
 arm_freq["rpi2-high"]=1050
@@ -132,8 +135,12 @@ arm_freq["turbo"]=1000
 arm_freq["high"]=950
 arm_freq["none"]=700
 arm_freq["none-rpi2"]=900
+arm_freq["none-rpi3"]=1200
 
 declare -A core_freq
+core_freq["rpi3-extrem"]=550
+core_freq["rpi3-turbo"]=525
+core_freq["rpi3-high"]=525
 core_freq["rpi2-extrem"]=550
 core_freq["rpi2-turbo"]=525
 core_freq["rpi2-high"]=525
@@ -142,8 +149,12 @@ core_freq["turbo"]=500
 core_freq["high"]=250
 core_freq["none"]=250
 core_freq["none-rpi2"]=250
+core_freq["none-rpi3"]=400
 
 declare -A sdram_freq
+sdram_freq["rpi3-extrem"]=550
+sdram_freq["rpi3-turbo"]=520
+sdram_freq["rpi3-high"]=500
 sdram_freq["rpi2-extrem"]=480
 sdram_freq["rpi2-turbo"]=480
 sdram_freq["rpi2-high"]=450
@@ -152,8 +163,12 @@ sdram_freq["turbo"]=600
 sdram_freq["high"]=450
 sdram_freq["none"]=400
 sdram_freq["none-rpi2"]=450
+sdram_freq["none-rpi3"]=450
 
 declare -A force_turbo
+force_turbo["rpi3-extrem"]=1
+force_turbo["rpi3-turbo"]=0
+force_turbo["rpi3-high"]=0
 force_turbo["rpi2-extrem"]=1
 force_turbo["rpi2-turbo"]=0
 force_turbo["rpi2-high"]=0
@@ -162,8 +177,12 @@ force_turbo["turbo"]=0
 force_turbo["high"]=0
 force_turbo["none"]=0
 force_turbo["none-rpi2"]=0
+force_turbo["none-rpi3"]=0
 
 declare -A over_voltage
+over_voltage["rpi3-extrem"]=4
+over_voltage["rpi3-turbo"]=4
+over_voltage["rpi3-high"]=4
 over_voltage["rpi2-extrem"]=4
 over_voltage["rpi2-turbo"]=4
 over_voltage["rpi2-high"]=4
@@ -172,8 +191,12 @@ over_voltage["turbo"]=6
 over_voltage["high"]=6
 over_voltage["none"]=0
 over_voltage["none-rpi2"]=0
+over_voltage["none-rpi3"]=0
 
 declare -A over_voltage_sdram
+over_voltage_sdram["rpi3-extrem"]=5
+over_voltage_sdram["rpi3-turbo"]=4
+over_voltage_sdram["rpi3-high"]=4
 over_voltage_sdram["rpi2-extrem"]=4
 over_voltage_sdram["rpi2-turbo"]=2
 over_voltage_sdram["rpi2-high"]=2
@@ -182,8 +205,12 @@ over_voltage_sdram["turbo"]=0
 over_voltage_sdram["high"]=0
 over_voltage_sdram["none"]=0
 over_voltage_sdram["none-rpi2"]=0
+over_voltage_sdram["none-rpi3"]=0
 
 declare -A gpu_freq
+gpu_freq["rpi3-extrem"]=525
+gpu_freq["rpi3-turbo"]=500
+gpu_freq["rpi3-high"]=500
 gpu_freq["rpi2-extrem"]=366
 gpu_freq["rpi2-turbo"]=350
 gpu_freq["rpi2-high"]=350
@@ -192,6 +219,7 @@ gpu_freq["turbo"]=250
 gpu_freq["high"]=250
 gpu_freq["none"]=250
 gpu_freq["none-rpi2"]=250
+gpu_freq["none-rpi3"]=400
 
 if [ -f "$configFile" ];then
         preBootConfig
