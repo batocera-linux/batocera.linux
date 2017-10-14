772230b424beaf45d2f9f270a6761b37bf3413d9
Jérémy <xgenetik57x@gmail.com>
2017-10-14 11:49:56 +0200

    PIN/GPIO Information added

--- board/recalbox/fsoverlay/recalbox/scripts/rpi-pin356-power.py
+++ board/recalbox/fsoverlay/recalbox/scripts/rpi-pin356-power.py
@@ -6,7 +6,7 @@ import RPi.GPIO as GPIO
 import time
 
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(3, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(3, GPIO.IN, pull_up_down=GPIO.PUD_UP)	# GPIO on pin 5 is the GPIO 3 in BCM mode
 
 
 def exitAllBatoceraEmulator(channel):
