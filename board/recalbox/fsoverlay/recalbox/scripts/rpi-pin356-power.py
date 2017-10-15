f2af0d6ef8a41709c33c8b447bb884c955fc0fff
Jérémy <xgenetik57x@gmail.com>
2017-10-15 14:30:50 +0200

    fba2x added

--- board/recalbox/fsoverlay/recalbox/scripts/rpi-pin356-power.py
+++ board/recalbox/fsoverlay/recalbox/scripts/rpi-pin356-power.py
@@ -11,7 +11,7 @@ GPIO.setup(3, GPIO.IN, pull_up_down=GPIO.PUD_UP)	# GPIO on pin 5 is the GPIO 3 i
 
 def exitAllBatoceraEmulator(channel):
     print 'exitAllBatoceraEmulator'
-    os.system('killall retroarch PPSSPPSDL reicast.elf mupen64plus linapple x64')
+    os.system('killall retroarch PPSSPPSDL reicast.elf mupen64plus linapple x64 fba2x')
 
 
 GPIO.add_event_detect(3, GPIO.FALLING, callback=exitAllBatoceraEmulator,
