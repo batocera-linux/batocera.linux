76cc2b15b6adfd4a3abfd6eb0fe50fa7d350b86f
Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>
2017-12-05 23:52:12 +0100

    amiberry
    Signed-off-by: Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>

---
+++ package/batocera/amiberry/amiberry.mk
@@ -0,0 +1,45 @@
+################################################################################
+#
+# AMIBERRY
+#
+################################################################################
+
+AMIBERRY_VERSION = 1f27fdf4f55302068e0419783ff0d6972dff5e1c
+AMIBERRY_SITE = $(call github,midwan,amiberry,$(AMIBERRY_VERSION))
+AMIBERRY_DEPENDENCIES = sdl sdl_image sdl_gfx sdl_ttf mpg123 libxml2 libmpeg2 guichan
+
+ifeq ($(BR2_PACKAGE_RECALBOX_TARGET_RPI3),y)
+	RECALBOX_SYSTEM=rpi3
+else ifeq ($(BR2_PACKAGE_RECALBOX_TARGET_RPI2),y)
+	RECALBOX_SYSTEM=rpi2
+else ifeq ($(BR2_PACKAGE_RECALBOX_TARGET_RPI1),y)
+	RECALBOX_SYSTEM=rpi1
+else ifeq ($(BR2_PACKAGE_RECALBOX_TARGET_XU4),y)
+	RECALBOX_SYSTEM=odroidxu4
+endif
+
+define AMIBERRY_CONFIGURE_PI
+	sed -i "s+/opt/vc+$(STAGING_DIR)/usr+g" $(@D)/Makefile
+endef
+
+AMIBERRY_PRE_CONFIGURE_HOOKS += AMIBERRY_CONFIGURE_PI
+
+define AMIBERRY_BUILD_CMDS
+	$(TARGET_CONFIGURE_OPTS) $(MAKE) \
+		CPP="$(TARGET_CPP)" \
+		CXX="$(TARGET_CXX)" \
+		CC="$(TARGET_CC)" \
+		AS="$(TARGET_CC)" \
+		STRIP="$(TARGET_STRIP)" \
+                SDL_CONFIG=$(STAGING_DIR)/usr/bin/sdl-config \
+		-C $(@D) PLATFORM=$(RECALBOX_SYSTEM)
+endef
+
+define AMIBERRY_INSTALL_TARGET_CMDS
+	$(INSTALL) -D $(@D)/amiberry $(TARGET_DIR)/usr/bin/amiberry
+	rm -rf $(TARGET_DIR)/usr/share/amiberry/data
+	mkdir -p $(TARGET_DIR)/usr/share/amiberry
+	cp -r $(@D)/data $(TARGET_DIR)/usr/share/amiberry
+endef
+
+$(eval $(generic-package))
---
+++ package/batocera/guichan/guichan.mk
@@ -0,0 +1,38 @@
+################################################################################
+#
+# guichan
+#
+################################################################################
+
+GUICHAN_VERSION = 0.8.1
+GUICHAN_SOURCE = guichan-$(GUICHAN_VERSION).tar.gz
+GUICHAN_SITE = http://pkgs.fedoraproject.org/repo/pkgs/guichan/guichan-$(GUICHAN_VERSION).tar.gz/f9ace11cc70d3ba60b62347172334a5f
+
+# broken urls
+#http://guichan.googlecode.com/files/guichan-0.8.1.tar.gz
+#http://guichan.sourceforge.net/wiki/
+
+GUICHAN_LICENSE = BSD
+GUICHAN_DEPENDENCIES = sdl
+
+GUICHAN_INSTALL_STAGING = YES
+
+GUICHAN_CONF_OPTS += HAVE_SDL=yes
+GUICHAN_CPPFLAGS += -I$(STAGING_DIR)/usr/include/SDL
+GUICHAN_CFLAGS += -I$(STAGING_DIR)/usr/include/SDL
+
+define GUICHAN_CONFIGURE_CMDS
+        (cd $(@D); \
+        $(TARGET_CONFIGURE_ARGS) \
+                $(TARGET_CONFIGURE_OPTS) \
+                CFLAGS="$(TARGET_CFLAGS) $(GUICHAN_CFLAGS)" \
+                CXXFLAGS="$(TARGET_CXXFLAGS) $(GUICHAN_CFLAGS)" \
+                CPPFLAGS="$(TARGET_CPPFLAGS) $(GUICHAN_CFLAGS)" \
+                LDFLAGS="$(TARGET_LDFLAGS) $(GUICHAN_LDFLAGS)" \
+                CROSS_COMPILE="$(HOST_DIR)/usr/bin/" \
+                ./configure --host="$(GNU_TARGET_NAME)" \
+	        --prefix=/usr $(GUICHAN_CONF_OPTS) \
+        )
+endef
+
+$(eval $(autotools-package))
