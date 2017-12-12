a788bba3ed3695ed7ff927cda629bd46b8a380e6
ChiaCheng Huang <suzuke@hotmail.com>
2017-12-12 15:49:41 +0800

    Add Amlogic S905 porting

--- board/recalbox/copy-recalbox-archives.sh
+++ board/recalbox/copy-recalbox-archives.sh
@@ -192,6 +192,40 @@ case "${RECALBOX_TARGET}" in
 	sync || exit 1
 	;;
 
+    S905)
+	# boot
+	rm -rf ${BINARIES_DIR}/boot        || exit 1
+	mkdir -p ${BINARIES_DIR}/boot/boot || exit 1
+	cp board/recalbox/s905/boot-logo.bmp.gz ${BINARIES_DIR}/boot   || exit 1
+	cp board/recalbox/s905/s905_autoscript ${BINARIES_DIR}/boot	|| exit 1
+	cp board/recalbox/s905/aml_autoscript ${BINARIES_DIR}/boot     || exit 1
+	cp board/recalbox/s905/aml_autoscript.zip ${BINARIES_DIR}/boot     || exit 1
+	cp "${BINARIES_DIR}/recalbox-boot.conf" "${BINARIES_DIR}/boot/recalbox-boot.conf" || exit 1
+	cp "${BINARIES_DIR}/Image" "${BINARIES_DIR}/boot/boot/Image" || exit 1
+	cp "${BINARIES_DIR}/uInitrd"              "${BINARIES_DIR}/boot/boot" || exit 1
+	cp "${BINARIES_DIR}/rootfs.squashfs" "${BINARIES_DIR}/boot/boot/recalbox.update" || exit 1
+	cp "${BINARIES_DIR}/gxbb_p200_2G.dtb" "${BINARIES_DIR}/boot/boot" || exit 1
+	cp "${BINARIES_DIR}/gxbb_p200.dtb" "${BINARIES_DIR}/boot/boot" || exit 1
+	cp "${BINARIES_DIR}/gxl_p212_1g.dtb" "${BINARIES_DIR}/boot/boot" || exit 1
+	cp "${BINARIES_DIR}/gxl_p212_2g.dtb" "${BINARIES_DIR}/boot/boot" || exit 1
+
+	# boot.tar.xz
+	echo "creating boot.tar.xz"
+	(cd "${BINARIES_DIR}/boot" && tar -cJf "${RECALBOX_BINARIES_DIR}/boot.tar.xz"  boot recalbox-boot.conf boot-logo.bmp.gz) || exit 1
+
+	# batocera.img
+        # rename the squashfs : the .update is the version that will be renamed at boot to replace the old version
+        mv "${BINARIES_DIR}/boot/boot/recalbox.update" "${BINARIES_DIR}/boot/boot/recalbox" || exit 1
+	GENIMAGE_TMP="${BUILD_DIR}/genimage.tmp"
+	RECALBOXIMG="${RECALBOX_BINARIES_DIR}/batocera.img"
+	rm -rf "${GENIMAGE_TMP}" || exit 1
+	cp "board/recalbox/s905/genimage.cfg" "${BINARIES_DIR}" || exit 1
+	echo "generating image"
+	genimage --rootpath="${TARGET_DIR}" --inputpath="${BINARIES_DIR}/boot" --outputpath="${RECALBOX_BINARIES_DIR}" --config="${BINARIES_DIR}/genimage.cfg" --tmppath="${GENIMAGE_TMP}" || exit 1
+	rm -f "${RECALBOX_BINARIES_DIR}/boot.vfat" || exit 1
+	sync || exit 1
+	;;
+
         X86|X86_64)
 	# /boot
 	rm -rf ${BINARIES_DIR}/boot || exit 1
---
+++ package/batocera/gpu-aml/gpu-aml.mk
@@ -0,0 +1,76 @@
+################################################################################
+#
+# amlogic gpu driver
+#
+################################################################################
+LINUX_VERSION_PROBED = `$(MAKE) $(LINUX_MAKE_FLAGS) -C $(LINUX_DIR) --no-print-directory -s kernelrelease 2>/dev/null`
+GPU_AML_VERSION = $(call qstrip,$(BR2_PACKAGE_GPU_AML_VERSION))
+ifneq ($(BR2_PACKAGE_GPU_AML_CUSTOM_TARBALL_LOCATION),"")
+GPU_AML_TARBALL = $(call qstrip,$(BR2_PACKAGE_GPU_AML_CUSTOM_TARBALL_LOCATION))
+GPU_AML_SITE    = $(patsubst %/,%,$(dir $(GPU_AML_TARBALL)))
+GPU_AML_SOURCE  = $(notdir $(GPU_AML_TARBALL))
+else
+GPU_AML_SITE = $(call qstrip,$(BR2_PACKAGE_GPU_AML_GIT_URL))
+GPU_AML_SITE_METHOD = git
+endif
+GPU_AML_MODULE_DIR = kernel/amlogic/gpu
+GPU_AML_INSTALL_DIR = $(TARGET_DIR)/lib/modules/$(LINUX_VERSION_PROBED)/$(GPU_AML_MODULE_DIR)
+ifeq ($(BR2_PACKAGE_GPU_AML_STANDALONE),y)
+GPU_AML_DEPENDENCIES = linux
+endif
+
+ifeq ($(BR2_PACKAGE_GPU_AML_MALI_DRM),y)
+MALI_DRM_BUILD_CMD = \
+	$(TARGET_CONFIGURE_OPTS) $(MAKE) -C $(LINUX_DIR) M=$(LINUX_DIR)/drivers/gpu/drm/mali_drm ARCH=$(KERNEL_ARCH) \
+		CROSS_COMPILE=$(TARGET_CROSS) modules
+MALI_DRM_INSTALL_TARGET_CMDS = \
+	$(INSTALL) -m 0666 $(LINUX_DIR)/drivers/gpu/drm/mali_drm/mali_drm.ko $(GPU_AML_INSTALL_DIR); \
+	echo $(GPU_AML_MODULE_DIR)/mali_drm.ko: >> $(TARGET_DIR)/lib/modules/$(LINUX_VERSION_PROBED)/modules.dep
+endif
+ifeq ($(BR2_PACKAGE_GPU_AML_UMP),y)
+MALI_BUILD_CMD = \
+	$(TARGET_CONFIGURE_OPTS) $(MAKE) -C $(LINUX_DIR) M=$(@D)/mali ARCH=$(KERNEL_ARCH) \
+		CROSS_COMPILE=$(TARGET_CROSS) CONFIG_MALI400=m CONFIG_MALI450=m MALI400_UMP=y modules;
+MALI_UMP_BUILD_CMD = \
+	$(TARGET_CONFIGURE_OPTS) $(MAKE) -C $(LINUX_DIR) M=$(@D)/ump ARCH=$(KERNEL_ARCH) \
+		CROSS_COMPILE=$(TARGET_CROSS) CONFIG_UMP=m modules
+MALI_UMP_INSTALL_TARGET_CMDS = \
+	$(INSTALL) -m 0666 $(@D)/ump/ump.ko $(GPU_AML_INSTALL_DIR); \
+	echo $(GPU_AML_MODULE_DIR)/ump.ko: >> $(TARGET_DIR)/lib/modules/$(LINUX_VERSION_PROBED)/modules.dep;
+else
+ifeq ($(BR2_PACKAGE_GPU_AML_MALI_DMA_SHARED_BUFFER),y)
+MALI_BUILD_CMD = \
+	$(TARGET_CONFIGURE_OPTS) $(MAKE) -C $(LINUX_DIR) M=$(@D)/mali ARCH=$(KERNEL_ARCH) \
+		CROSS_COMPILE=$(TARGET_CROSS) CONFIG_MALI400=m CONFIG_MALI450=m CONFIG_DMA_SHARED_BUFFER=y modules
+else
+MALI_BUILD_CMD = \
+	$(TARGET_CONFIGURE_OPTS) $(MAKE) -C $(LINUX_DIR) M=$(@D)/mali ARCH=$(KERNEL_ARCH) \
+		CROSS_COMPILE=$(TARGET_CROSS) CONFIG_MALI400=m CONFIG_MALI450=m  \
+		EXTRA_CFLAGS="-DCONFIG_MALI400=m -DCONFIG_MALI450=m" modules
+endif
+endif
+MALI_INSTALL_TARGETS_CMDS = \
+	$(INSTALL) -m 0666 $(@D)/mali/mali.ko $(GPU_AML_INSTALL_DIR); \
+	echo $(GPU_AML_MODULE_DIR)/mali.ko: >> $(TARGET_DIR)/lib/modules/$(LINUX_VERSION_PROBED)/modules.dep;
+
+ifeq ($(BR2_PACKAGE_GPU_AML_STANDALONE),y)
+define GPU_AML_BUILD_CMDS
+	$(MALI_BUILD_CMD)
+	$(MALI_UMP_BUILD_CMD) 
+	$(MALI_DRM_BUILD_CMD)
+endef
+define GPU_AML_INSTALL_TARGET_CMDS
+	mkdir -p $(GPU_AML_INSTALL_DIR);
+	$(MALI_INSTALL_TARGETS_CMDS)
+	$(MALI_UMP_INSTALL_TARGET_CMDS) 
+	$(MALI_DRM_INSTALL_TARGET_CMDS)
+endef
+else
+define GPU_AML_BUILD_CMDS
+	mkdir -p  $(LINUX_DIR)/../hardware/arm;
+	ln -sf $(GPU_AML_DIR) $(LINUX_DIR)/../hardware/arm/gpu
+endef
+endif
+
+$(eval $(generic-package))
+
