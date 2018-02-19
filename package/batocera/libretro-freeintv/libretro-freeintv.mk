8cfe7e67286f38bc60ecbb209eb222af2a161ffa
Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>
2018-02-19 22:25:44 +0100

    intellivision: somes fixes
    Signed-off-by: Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>

--- package/batocera/libretro-freeintv/libretro-freeintv.mk
+++ package/batocera/libretro-freeintv/libretro-freeintv.mk
@@ -1,6 +1,6 @@
 ################################################################################
 #
-# FREEINTV
+# LIBRETRO_FREEINTV
 #
 ################################################################################
 LIBRETRO_FREEINTV_VERSION = cc6ef55b4d7c9f62c54595596ba61537e75e881e
@@ -8,11 +8,11 @@ LIBRETRO_FREEINTV_SITE = $(call github,libretro,freeintv,$(LIBRETRO_FREEINTV_VER
 
 define LIBRETRO_FREEINTV_BUILD_CMDS
 	CFLAGS="$(TARGET_CFLAGS)" CXXFLAGS="$(TARGET_CXXFLAGS)" \
-        $(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" LD="$(TARGET_LD)" RANLIB="$(TARGET_RANLIB)" AR="$(TARGET_AR)" -C $(@D)/freeintv -f Makefile platform="$(LIBRETRO_PLATFORM)"
+        $(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" LD="$(TARGET_LD)" RANLIB="$(TARGET_RANLIB)" AR="$(TARGET_AR)" -C $(@D) -f Makefile platform="$(LIBRETRO_PLATFORM)"
 endef
 
 define LIBRETRO_FREEINTV_INSTALL_TARGET_CMDS
-	$(INSTALL) -D $(@D)/freeintv/freeintv_libretro.so \
+	$(INSTALL) -D $(@D)/freeintv_libretro.so \
 		$(TARGET_DIR)/usr/lib/libretro/freeintv_libretro.so
 endef
 
