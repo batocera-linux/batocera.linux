abcbb12186f99dcc3e52b03bff627b58d10371ee
Juliano Dorigão <jdorigao@gmail.com>
2018-02-20 16:25:03 -0400

    libretro-tgdual update

--- package/batocera/libretro-tgbdual/libretro-tgbdual.mk
+++ package/batocera/libretro-tgbdual/libretro-tgbdual.mk
@@ -3,7 +3,7 @@
 # TGBDUAL
 #
 ################################################################################
-LIBRETRO_TGBDUAL_VERSION = 7c5706b5bb2465b2508b4d80015f23f1d8610fc8
+LIBRETRO_TGBDUAL_VERSION = e1f8348eddf0259a2cdf2abfa3e25051acf5ff4f
 LIBRETRO_TGBDUAL_SITE = $(call github,libretro,tgbdual-libretro,$(LIBRETRO_TGBDUAL_VERSION))
 
 define LIBRETRO_TGBDUAL_BUILD_CMDS
