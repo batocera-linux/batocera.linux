c51af1b4501be8bd263af9a345c4f68703eac72c
Jérémy <xgenetik57x@gmail.com>
2018-02-24 17:28:42 +0100

    Some changes
    *Backslash is literal in "\n". Prefer explicit escaping: "\\n" - SC1117
    *Useless cat. Consider 'cmd < file | ..' or 'cmd file | ..' instead - SC2002
    *Use $(STATEMENT) instead of legacy `STATEMENT` - SC2006
    *Check exit code directly with e.g. 'if mycmd;', not indirectly with $? - SC2181
    
    -Genetik57

--- board/recalbox/fsoverlay/recalbox/scripts/systemsetting.sh
+++ board/recalbox/fsoverlay/recalbox/scripts/systemsetting.sh
@@ -7,16 +7,16 @@ argsetting="$2"
 log=/recalbox/share/system/logs/recalbox.log
 
 if [[ "$command" == "get" ]];then
-	echo "`logtime` : systemsetting.sh - searching for $argsetting" >> $log
-	setting=`cat "$system_settings" | sed -n "s/^${argsetting}=\(.*\)/\1/p"`
-	if [[ "$?" != "0" ]]; then
+	echo "$(logtime) : systemsetting.sh - searching for $argsetting" >> $log
+	setting=$(cat < $system_settings | sed -n "s/^${argsetting}=\\(.*\\)/\\1/p")
+	if ! "$?" != "0"
+	then
 		exit 1
 	fi
 	if [[ "$setting" != "" ]]; then
-		echo "`logtime` : systemsetting.sh - $argsetting found : $setting" >> $log
-		echo $setting
+		echo "$(logtime) : systemsetting.sh - $argsetting found : $setting" >> $log
+		echo "$setting"
 		exit 0
 	fi
 fi
 exit 1
-
