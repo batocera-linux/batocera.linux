8d653f762b15813de9f127e5388f3f88698b5337
Jérémy <xgenetik57x@gmail.com>
2018-02-24 17:21:42 +0100

    Little change 2
    -Genetik57

--- board/recalbox/fsoverlay/recalbox/scripts/udev-sixpair.sh
+++ board/recalbox/fsoverlay/recalbox/scripts/udev-sixpair.sh
@@ -2,7 +2,8 @@
 log=/recalbox/share/system/logs/controllers.log
 "ps3 usb controller detected" >> $log
 sixpair >> $log
-if ! "$?" != "0" ; then
+if ! "$?" != "0"
+then
 	bluetoothd --udev &
 	sleep 3
 	sixpair >> $log
