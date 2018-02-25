99aa62ec7d94b634c93c720c3dfba76bc9fc9011
Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>
2018-02-25 11:19:52 +0100

    libretro-cheats: fix
    Signed-off-by: Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>

--- package/batocera/libretro-cheats/libretro-cheats.mk
+++ package/batocera/libretro-cheats/libretro-cheats.mk
@@ -6,11 +6,9 @@
 LIBRETRO_CHEATS_VERSION = master
 LIBRETRO_CHEATS_SITE = $(call github,libretro,libretro-database,$(LIBRETRO_CHEATS_VERSION))
 
-
 define LIBRETRO_CHEATS_INSTALL_TARGET_CMDS
 	mkdir -p $(TARGET_DIR)/recalbox/share/cheats/cht
-	cp -r $(@D)/cht/* \
-		$(TARGET_DIR)/recalbox/share/cheats/cht
+	cp -r $(@D)/cht/* $(TARGET_DIR)/recalbox/share/cheats/cht
 endef
 
 $(eval $(generic-package))
