15494bdc31e0865ef149ab1bb615956a22c04899
Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>
2018-03-02 08:07:50 +0100

    wxwidgets: fix for other plateforms
    Signed-off-by: Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>

--- package/batocera/wxwidgets/wxwidgets.mk
+++ package/batocera/wxwidgets/wxwidgets.mk
@@ -10,7 +10,7 @@ WXWIDGETS_DEPENDENCIES = zlib libpng jpeg gdk-pixbuf libgtk2
 WXWIDGETS_INSTALL_STAGING = YES
 
 define WXWIDGETS_FIXUP_WXWIDGET_CONFIG
-       ln -sf $(STAGING_DIR)/usr/lib/wx/config/x86_64-buildroot-linux-gnu-gtk2-unicode-3.1 $(STAGING_DIR)/usr/bin/wx-config
+       ln -sf $(STAGING_DIR)/usr/lib/wx/config/*-buildroot-linux-gnu-gtk2-unicode-* $(STAGING_DIR)/usr/bin/wx-config
 	$(SED) 's%^prefix=.*%prefix=$(STAGING_DIR)/usr%' \
 		$(STAGING_DIR)/usr/bin/wx-config
 	$(SED) 's%^exec_prefix=.*%exec_prefix=$${prefix}%' \
