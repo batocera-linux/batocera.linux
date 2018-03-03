8f266d9b48eeb6599d68401ae56f7bc3ba95518b
Juliano Dorigão <jdorigao@gmail.com>
2018-03-03 10:05:39 -0400

    libretro-atari800: initial packages

---
+++ package/batocera/libretro-atari800/libretro-atari800.mk
@@ -0,0 +1,18 @@
+################################################################################
+#
+# ATARI800
+#
+################################################################################
+LIBRETRO_ATARI800_VERSION = f892b4437d52a3e1c4b42b18f3edc564a1d1045b
+LIBRETRO_ATARI800_SITE = $(call github,libretro,libretro-atari800,$(LIBRETRO_ATARI800_VERSION))
+
+define LIBRETRO_ATARI800_BUILD_CMDS
+	CFLAGS="$(TARGET_CFLAGS)" CXXFLAGS="$(TARGET_CXXFLAGS)" $(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" -C $(@D)/ -f Makefile platform="$(LIBRETRO_PLATFORM)"
+endef
+
+define LIBRETRO_ATARI800_INSTALL_TARGET_CMDS
+	$(INSTALL) -D $(@D)/atari800_libretro.so \
+		$(TARGET_DIR)/usr/lib/libretro/atari800_libretro.so
+endef
+
+$(eval $(generic-package))
