236ac01a09aee3db773a1cb9b41f5f9c39f55220
suzuke <suzuke@hotmail.com>
2018-03-07 18:29:37 +0800

    device-trees-aml-s9xx: fix gxl_p281_1g isn't exist build error

--- package/batocera/device-trees-aml-s9xx/device-trees-aml-s9xx.mk
+++ package/batocera/device-trees-aml-s9xx/device-trees-aml-s9xx.mk
@@ -21,37 +21,69 @@ endif
 DTS_PATH = $(LINUX_DIR)/arch/$(DEVICE_TREES_AML_S9XX_ARCH)/boot/dts/amlogic/
 
 ifeq ($(BR2_PACKAGE_RECALBOX_TARGET_S912),y)
-	DEVICE_TREES_AML_S9XX_DTS += $(foreach f,$(notdir $(wildcard $(@D)/gxm*.dts)),$f)
+	ifeq ($(BR2_PACKAGE_DEVICE_TREES_AML_S9XX_GXM_KVIM2),y)
+		DEVICE_TREES_AML_S9XX_DTS += $(foreach f,$(notdir $(wildcard $(@D)/gxm_kvim2*.dts)),$f)
+	endif
+
+	ifeq ($(BR2_PACKAGE_DEVICE_TREES_AML_S9XX_GXM_Q20X)),y)
+		DEVICE_TREES_AML_S9XX_DTS += $(foreach f,$(notdir $(wildcard $(@D)/gxm_q20*.dts)),$f)
+	endif
 endif
 
 ifeq ($(BR2_PACKAGE_RECALBOX_TARGET_S905),y)
-	DEVICE_TREES_AML_S9XX_DTS += $(foreach f,$(notdir $(wildcard $(@D)/gxbb*.dts)),$f)
-	DEVICE_TREES_AML_S9XX_DTS += $(foreach f,$(notdir $(wildcard $(@D)/gxl*.dts)),$f)
+	ifeq ($(BR2_PACKAGE_DEVICE_TREES_AML_S9XX_GXBB_P200),y)
+		DEVICE_TREES_AML_S9XX_DTS += $(foreach f,$(notdir $(wildcard $(@D)/gxbb_p200*.dts)),$f)
+	endif
+
+	ifeq ($(BR2_PACKAGE_DEVICE_TREES_AML_S9XX_GXBB_P201),y)
+		DEVICE_TREES_AML_S9XX_DTS += $(foreach f,$(notdir $(wildcard $(@D)/gxbb_p201*.dts)),$f)
+	endif
+
+	ifeq ($(BR2_PACKAGE_DEVICE_TREES_AML_S9XX_GXL_P212),y)
+		DEVICE_TREES_AML_S9XX_DTS += $(foreach f,$(notdir $(wildcard $(@D)/gxl_p212*.dts)),$f)
+	endif
+
+        ifeq ($(BR2_PACKAGE_DEVICE_TREES_AML_S9XX_GXL_P230),y)
+                DEVICE_TREES_AML_S9XX_DTS += $(foreach f,$(notdir $(wildcard $(@D)/gxl_p230*.dts)),$f)
+        endif
+
+        ifeq ($(BR2_PACKAGE_DEVICE_TREES_AML_S9XX_GXL_P231),y)
+                DEVICE_TREES_AML_S9XX_DTS += $(foreach f,$(notdir $(wildcard $(@D)/gxl_p231*.dts)),$f)
+        endif
+
+        ifeq ($(BR2_PACKAGE_DEVICE_TREES_AML_S9XX_GXL_P281),y)
+                DEVICE_TREES_AML_S9XX_DTS += $(foreach f,$(notdir $(wildcard $(@D)/gxl_p281*.dts)),$f)
+        endif
 endif
 
 DEVICE_TREES_AML_S9XX_DTS_NAME =$(basename $(DEVICE_TREES_AML_S9XX_DTS))
 DEVICE_TREES_AML_S9XX_DTBS = $(addprefix amlogic/, $(addsuffix .dtb,$(DEVICE_TREES_AML_S9XX_DTS_NAME)))
 
-define DEVICE_TREES_AML_S9XX_BUILD_CMDS
+PREPARE_DTS_CMD = \
 	for f in $(DEVICE_TREES_AML_S9XX_DTS_NAME) ; do \
-		sed -i '/le-dt-id/d' $(@D)/$$f.dts; \
-		sed -i '/amlogic-dt-id/d' $(@D)/$$f.dts; \
-		echo -e "/ {\n\tamlogic-dt-id = \"$$f\";\n};" >> $(@D)/$$f.dts; \
-	done
-	cp -f $(@D)/* $(DTS_PATH); \
+                sed -i '/le-dt-id/d' $(@D)/$$f.dts \
+                && sed -i '/amlogic-dt-id/d' $(@D)/$$f.dts \
+                && echo -e "/ {\n\tamlogic-dt-id = \"$$f\";\n};" >> $(@D)/$$f.dts; \
+        done
+
+COPY_DTS_CMD = cp -f $(@D)/* $(DTS_PATH)
+
+BUILD_DTS_CMD = \
 	$(MAKE) -C $(LINUX_DIR) ARCH=$(KERNEL_ARCH) \
-                CROSS_COMPILE=$(TARGET_CROSS) $(DEVICE_TREES_AML_S9XX_DTBS) ; \
-	$(HOST_DIR)/bin/dtbTool -o $(LINUX_DIR)/arch/$(DEVICE_TREES_AML_S9XX_ARCH)/boot/dts/dtb.img $(DTS_PATH);
+                CROSS_COMPILE=$(TARGET_CROSS) $(DEVICE_TREES_AML_S9XX_DTBS)
+
+COMBINE_DTS_CMD = \
+	$(HOST_DIR)/bin/dtbTool -o $(LINUX_DIR)/arch/$(DEVICE_TREES_AML_S9XX_ARCH)/boot/dts/dtb.img $(DTS_PATH)
+
+define DEVICE_TREES_AML_S9XX_BUILD_CMDS
+	${PREPARE_DTS_CMD}
+	${COPY_DTS_CMD}
+	${BUILD_DTS_CMD}
+	${COMBINE_DTS_CMD}
 endef
 
 define DEVICE_TREES_AML_S9XX_INSTALL_IMAGES_CMDS
 	cp -f $(LINUX_DIR)/arch/$(DEVICE_TREES_AML_S9XX_ARCH)/boot/dts/dtb.img $(BINARIES_DIR);
 endef
-
-#define DEVICE_TREES_AML_S9XX_INSTALL_IMAGES_CMDS
-#	for f in $(DEVICE_TREES_AML_S9XX_DTBS) ; do \
-#		cp -f $(LINUX_DIR)/arch/$(DEVICE_TREES_AML_S9XX_ARCH)/boot/dts/$$f $(BINARIES_DIR); \
-#	done
-#endef
 
 $(eval $(generic-package))
