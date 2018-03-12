023a60dfd76efd317e1a9be43ee76b5262b909d5
Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>
2018-03-12 21:30:54 +0100

    libretro-reicast: rpi and xu4 plateform
    Signed-off-by: Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>

--- package/batocera/libretro-reicast/libretro-reicast.mk
+++ package/batocera/libretro-reicast/libretro-reicast.mk
@@ -6,8 +6,33 @@
 LIBRETRO_REICAST_VERSION = fff2eb2f92251db82cb763d3ab1d19be4a705f95
 LIBRETRO_REICAST_SITE = $(call github,libretro,reicast-emulator,$(LIBRETRO_REICAST_VERSION))
 
+LIBRETRO_REICAST_PLATFORM = $(LIBRETRO_PLATFORM)
+
+# LIBRETRO_PLATFORM is not good for this core, because
+# for rpi, it contains "unix rpi" and this core do an "if unix elif rpi ..."
+
+# special cases for special plateform then...
+# an other proper way may be to redo the Makefile to do "if rpi elif unix ..." (from specific to general)
+# the Makefile imposes that the platform has gles (or force FORCE_GLES is set) to not link with lGL
+
+ifeq ($(BR2_PACKAGE_RECALBOX_TARGET_RPI1),y)
+	LIBRETRO_REICAST_PLATFORM = rpi-gles
+endif
+
+ifeq ($(BR2_PACKAGE_RECALBOX_TARGET_RPI2),y)
+	LIBRETRO_REICAST_PLATFORM = rpi2-gles
+endif
+
+ifeq ($(BR2_PACKAGE_RECALBOX_TARGET_RPI3),y)
+	LIBRETRO_REICAST_PLATFORM = rpi3-gles
+endif
+
+ifeq ($(BR2_PACKAGE_RECALBOX_TARGET_XU4),y)
+	LIBRETRO_REICAST_PLATFORM = odroid-ODROID-XU3-gles
+endif
+
 define LIBRETRO_REICAST_BUILD_CMDS
-        CFLAGS="$(TARGET_CFLAGS)" CXXFLAGS="$(TARGET_CXXFLAGS)" $(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" AR="$(TARGET_AR)" -C $(@D) -f Makefile platform="$(LIBRETRO_PLATFORM)"
+        CFLAGS="$(TARGET_CFLAGS)" CXXFLAGS="$(TARGET_CXXFLAGS)" $(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" AR="$(TARGET_AR)" -C $(@D) -f Makefile platform="$(LIBRETRO_REICAST_PLATFORM)"
 endef
 
 define LIBRETRO_REICAST_INSTALL_TARGET_CMDS
