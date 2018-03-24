747f7c545e2292898ad7140c796cb7a0c1dd689b
Juliano Dorigão <jdorigao@gmail.com>
2018-03-24 10:40:51 -0400

    patch: fix

--- package/batocera/libretro-bluemsx/libretro-bluemsx.mk
+++ package/batocera/libretro-bluemsx/libretro-bluemsx.mk
@@ -8,7 +8,7 @@ LIBRETRO_BLUEMSX_SITE = $(call github,libretro,blueMSX-libretro,$(LIBRETRO_BLUEM
 
 define LIBRETRO_BLUEMSX_BUILD_CMDS
 	CFLAGS="$(TARGET_CFLAGS)" CXXFLAGS="$(TARGET_CXXFLAGS)" \
-        $(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" LD="$(TARGET_LD)" RANLIB="$(TARGET_RANLIB)" AR="$(TARGET_AR)" -C $(@D) -f Makefile.libretro platform="$(LIBRETRO_PLATFORM)"
+        $(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" LD="$(TARGET_CXX)" RANLIB="$(TARGET_RANLIB)" AR="$(TARGET_AR)" -C $(@D) -f Makefile.libretro platform="$(LIBRETRO_PLATFORM)"
 endef
 
 define LIBRETRO_BLUEMSX_INSTALL_TARGET_CMDS
