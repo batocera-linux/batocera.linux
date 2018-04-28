9b9f72593a98e9ea69c22f364797903beb0b5ccc
Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>
2018-04-28 09:59:13 +0200

    batocera-es-system: first version
    Signed-off-by: Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>

---
+++ package/batocera/batocera-es-system/batocera-es-system.mk
@@ -0,0 +1,26 @@
+################################################################################
+#
+# BATOCERA-ES-SYSTEM
+#
+################################################################################
+
+BATOCERA_ES_SYSTEM_DEPENDENCIES = host-python host-python-pyyaml
+BATOCERA_ES_SYSTEM_SOURCE=
+BATOCERA_ES_SYSTEM_VERSION=1.0
+
+define BATOCERA_ES_SYSTEM_BUILD_CMDS
+	$(HOST_DIR)/bin/python \
+		package/batocera/batocera-es-system/batocera-es-system.py \
+		package/batocera/batocera-es-system/es_systems.yml        \
+		.config \
+		$(@D)/es_systems.cfg \
+		package/batocera/batocera-es-system/roms \
+		$(@D)/roms
+endef
+
+define BATOCERA_ES_SYSTEM_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 0644 -D $(@D)/es_systems.cfg $(TARGET_DIR)/recalbox/share_init/system/.emulationstation/es_systems.cfg
+	cp -pr $(@D)/roms $(TARGET_DIR)/recalbox/share_init/
+endef
+
+$(eval $(generic-package))
