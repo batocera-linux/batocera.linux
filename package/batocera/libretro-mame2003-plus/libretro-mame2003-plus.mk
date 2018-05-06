0b405073ac89eb11167d0e902d329b8851fec509
Jérémy <xgenetik57x@gmail.com>
2018-05-06 22:09:37 +0200

    libretro-mame2003-plus

---
+++ package/batocera/libretro-mame2003-plus/libretro-mame2003-plus.mk
@@ -0,0 +1,31 @@
+################################################################################
+#
+# MAME2003 PLUS
+#
+################################################################################
+#LIBRETRO_MAME2003_PLUS_VERSION = mame078p - Commits on May 6, 2018 
+LIBRETRO_MAME2003_PLUS_VERSION = f880445bcfb637287bab40bb36b31aba936bebe4
+LIBRETRO_MAME2003_PLUS_SITE = $(call github,libretro,mame2003-plus-libretro,$(LIBRETRO_MAME2003_PLUS_VERSION))
+
+
+
+define LIBRETRO_MAME2003_PLUS_BUILD_CMDS
+	mkdir -p $(@D)/obj/mame/cpu/ccpu
+	#CFLAGS="$(TARGET_CFLAGS)" CXXFLAGS="$(TARGET_CXXFLAGS)" $(MAKE) CC="$(TARGET_CC)" LD="$(TARGET_CC)" RANLIB="$(TARGET_RANLIB)" AR="$(TARGET_AR)" -C $(@D)/ -f Makefile ARCH="$(TARGET_CFLAGS) -fsigned-char "
+	CFLAGS="$(TARGET_CFLAGS)" CXXFLAGS="$(TARGET_CXXFLAGS)" $(MAKE) CC="$(TARGET_CC)" LD="$(TARGET_CC)" RANLIB="$(TARGET_RANLIB)" AR="$(TARGET_AR)" -C $(@D)/ -f Makefile platform="$(LIBRETRO_PLATFORM)"
+
+endef
+
+define LIBRETRO_MAME2003_PLUS_INSTALL_TARGET_CMDS
+	$(INSTALL) -D $(@D)/mame2003_plus_libretro.so \
+		$(TARGET_DIR)/usr/lib/libretro/mame078p_libretro.so
+endef
+
+define LIBRETRO_MAME2003_PLUS_NAMCO_QUICK_FIX
+        $(SED) 's|O3|O2|g' $(@D)/Makefile
+        $(SED) 's|to continue|on Keyboard, or Left, Right on Joystick to continue|g' $(@D)/src/ui_text.c 
+endef
+
+LIBRETRO_MAME2003_PLUS_PRE_BUILD_HOOKS += LIBRETRO_MAME2003_PLUS_NAMCO_QUICK_FIX
+
+$(eval $(generic-package))
