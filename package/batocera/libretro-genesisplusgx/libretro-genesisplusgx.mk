cc1db2e4a878cdaf1bb463cfcc89c8ccc65906d8
Ntemis <ierokipides.dem@gmail.com>
2018-05-17 00:39:31 +0300

    Update Libretro Genesis-Plus-GX

--- package/batocera/libretro-genesisplusgx/libretro-genesisplusgx.mk
+++ package/batocera/libretro-genesisplusgx/libretro-genesisplusgx.mk
@@ -3,7 +3,8 @@
 # GENESISPLUSGX
 #
 ################################################################################
-LIBRETRO_GENESISPLUSGX_VERSION = 6ba9e05419be9aac338e152a9e3fd27b6f3e2297
+
+LIBRETRO_GENESISPLUSGX_VERSION = b2667eb1665d03df0fdb83f33afc3a159bf71f72
 LIBRETRO_GENESISPLUSGX_SITE = $(call github,libretro,Genesis-Plus-GX,$(LIBRETRO_GENESISPLUSGX_VERSION))
 
 define LIBRETRO_GENESISPLUSGX_BUILD_CMDS
