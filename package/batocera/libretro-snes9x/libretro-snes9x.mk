35b120056fb3d40b8e6df8bb03b936827a67a45e
ntemis <ierokipides.dem@gmail.com>
2018-05-18 00:38:29 +0300

    Update libretro-snes9x

--- package/batocera/libretro-snes9x/libretro-snes9x.mk
+++ package/batocera/libretro-snes9x/libretro-snes9x.mk
@@ -3,7 +3,7 @@
 # SNES9X
 #
 ################################################################################
-LIBRETRO_SNES9X_VERSION = 2a15fe27f06277a95863c07d47186a28d5aac8ba
+LIBRETRO_SNES9X_VERSION = 4d9f365bb672e691f749beb8a9f8e8388b563b01
 LIBRETRO_SNES9X_SITE = $(call github,libretro,snes9x,$(LIBRETRO_SNES9X_VERSION))
 
 define LIBRETRO_SNES9X_BUILD_CMDS
