6606a4444f2360101f3f462e2886732d1ebed822
Juliano Dorigão <jdorigao@gmail.com>
2018-05-18 22:22:50 -0400

    update: libretro-o2em

--- package/batocera/libretro-o2em/libretro-o2em.mk
+++ package/batocera/libretro-o2em/libretro-o2em.mk
@@ -3,7 +3,8 @@
 # O2EM
 #
 ################################################################################
-LIBRETRO_O2EM_VERSION = 728b63583ca44e526ab5f4f7bd71406aa7b5a1df
+# Version.: Commits on May 2, 2018
+LIBRETRO_O2EM_VERSION = 26715cb218d15f6703bda2e8fa0a51da2d38dfa9
 LIBRETRO_O2EM_SITE = $(call github,libretro,libretro-o2em,$(LIBRETRO_O2EM_VERSION))
 
 define LIBRETRO_O2EM_BUILD_CMDS
