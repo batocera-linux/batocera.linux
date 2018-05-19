69fca9571ed92303bf602347d602e4f57be4a54e
Juliano Dorigão <jdorigao@gmail.com>
2018-05-18 22:23:50 -0400

    update: libretro-imame

--- package/batocera/libretro-imame/libretro-imame.mk
+++ package/batocera/libretro-imame/libretro-imame.mk
@@ -3,8 +3,9 @@
 # IMAME
 #
 ################################################################################
-LIBRETRO_IMAME_VERSION = d93c6483dd31681d404b55c2126e7c2ea86c7d1d
-LIBRETRO_IMAME_SITE = $(call github,libretro,imame4all-libretro,$(LIBRETRO_IMAME_VERSION))
+# Version.: Commits on Apr 20, 2018
+LIBRETRO_IMAME_VERSION = bd3833c41f6894ba1c5d2f3f35b29190658517a0
+LIBRETRO_IMAME_SITE = $(call github,libretro,mame2000-libretro,$(LIBRETRO_IMAME_VERSION))
 
 define LIBRETRO_IMAME_BUILD_CMDS
 	mkdir -p $(@D)/obj_libretro_libretro/cpu
