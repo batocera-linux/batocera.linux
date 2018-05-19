56f1c50fa1e98868be5f20d492bd7c4ae536e00e
Juliano Dorigão <jdorigao@gmail.com>
2018-05-18 22:25:29 -0400

    update: libretro-catsfc

--- package/batocera/libretro-catsfc/libretro-catsfc.mk
+++ package/batocera/libretro-catsfc/libretro-catsfc.mk
@@ -3,8 +3,9 @@
 # CATSFC
 #
 ################################################################################
-LIBRETRO_CATSFC_VERSION = dc596c886e0ec2728dc353dd7862cdf1e5a5ff44
-LIBRETRO_CATSFC_SITE = $(call github,libretro,CATSFC-libretro,$(LIBRETRO_CATSFC_VERSION))
+# Version.: Commits on Apr 11, 2018
+LIBRETRO_CATSFC_VERSION = b83a86f02139b7aae9dc22149ebd5cbeac32faf4
+LIBRETRO_CATSFC_SITE = $(call github,libretro,snes9x2005,$(LIBRETRO_CATSFC_VERSION))
 
 define LIBRETRO_CATSFC_BUILD_CMDS
 	CFLAGS="$(TARGET_CFLAGS)" CXXFLAGS="$(TARGET_CXXFLAGS)" $(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" -C $(@D) platform="$(LIBRETRO_PLATFORM)"
