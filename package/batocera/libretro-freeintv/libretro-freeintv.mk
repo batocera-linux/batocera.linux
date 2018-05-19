d29b946f730d602ad6ca04821ed2a5b1ea8ddab6
Juliano Dorigão <jdorigao@gmail.com>
2018-05-18 22:24:40 -0400

    update: libretro-freeintv

--- package/batocera/libretro-freeintv/libretro-freeintv.mk
+++ package/batocera/libretro-freeintv/libretro-freeintv.mk
@@ -3,12 +3,13 @@
 # LIBRETRO_FREEINTV
 #
 ################################################################################
-LIBRETRO_FREEINTV_VERSION = 1c4959359573619c65d789a89ecd3b5f05d30ca2
+# Version.: Commits on May 3, 2018
+LIBRETRO_FREEINTV_VERSION = 233f362ceea2e7d863b3b40695d8a4793793a414
 LIBRETRO_FREEINTV_SITE = $(call github,libretro,freeintv,$(LIBRETRO_FREEINTV_VERSION))
 
 define LIBRETRO_FREEINTV_BUILD_CMDS
 	CFLAGS="$(TARGET_CFLAGS)" CXXFLAGS="$(TARGET_CXXFLAGS)" \
-        $(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" LD="$(TARGET_LD)" RANLIB="$(TARGET_RANLIB)" AR="$(TARGET_AR)" -C $(@D) -f Makefile platform="unix"
+        $(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" LD="$(TARGET_CXX)" RANLIB="$(TARGET_RANLIB)" AR="$(TARGET_AR)" -C $(@D) -f Makefile platform="unix"
 endef
 
 define LIBRETRO_FREEINTV_INSTALL_TARGET_CMDS
