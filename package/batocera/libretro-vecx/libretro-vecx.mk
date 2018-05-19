36116bcb6fe19a07b0f69c437727b4fa8f280ec9
Juliano Dorigão <jdorigao@gmail.com>
2018-05-18 22:20:34 -0400

    update: libretro-vecx

--- package/batocera/libretro-vecx/libretro-vecx.mk
+++ package/batocera/libretro-vecx/libretro-vecx.mk
@@ -3,7 +3,8 @@
 # VECX
 #
 ################################################################################
-LIBRETRO_VECX_VERSION = b78b3d1f551c3eba0dc2273eeae3047cdce46563
+# Version.: Commits on Apr 12, 2018
+LIBRETRO_VECX_VERSION = 25bfdd94900dcf5fd3d555ef686d857c0f087c2f
 LIBRETRO_VECX_SITE = $(call github,libretro,libretro-vecx,$(LIBRETRO_VECX_VERSION))
 
 define LIBRETRO_VECX_BUILD_CMDS
