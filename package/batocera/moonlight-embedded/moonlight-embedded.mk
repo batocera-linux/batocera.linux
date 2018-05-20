67218e56637172273953f3524cd40a7688457e06
Juliano Dorigão <jdorigao@gmail.com>
2018-05-19 21:20:01 -0400

    fix: moonlight-embedded

--- package/batocera/moonlight-embedded/moonlight-embedded.mk
+++ package/batocera/moonlight-embedded/moonlight-embedded.mk
@@ -9,10 +9,6 @@ MOONLIGHT_EMBEDDED_SOURCE = moonlight-embedded-$(MOONLIGHT_EMBEDDED_VERSION).tar
 MOONLIGHT_EMBEDDED_SITE = https://github.com/irtimmer/moonlight-embedded/releases/download/v$(MOONLIGHT_EMBEDDED_VERSION)
 MOONLIGHT_EMBEDDED_DEPENDENCIES = opus expat libevdev avahi alsa-lib udev libcurl libcec ffmpeg sdl2 libenet
 
-define MOONLIGHT_EMBEDDED_EXTRACT_CMDS
-	$(TAR) -xf $(DL_DIR)/$(MOONLIGHT_EMBEDDED_SOURCE) -C $(BUILD_DIR)
-endef
-
 MOONLIGHT_EMBEDDED_CONF_OPTS = "-DCMAKE_INSTALL_SYSCONFDIR=/etc"
 
 $(eval $(cmake-package))
