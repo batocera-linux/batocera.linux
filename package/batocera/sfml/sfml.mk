fbe35705a43a8db68d920704569e91dd46854f5a
Juliano Dorigão <jdorigao@gmail.com>
2018-05-19 20:53:37 -0400

    fix: sfml

--- package/batocera/sfml/sfml.mk
+++ package/batocera/sfml/sfml.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-SFML_VERSION = 2.4.0
+SFML_VERSION = 2.5.0
 SFML_SOURCE = SFML-$(SFML_VERSION)-sources.zip
 SFML_SITE = http://www.sfml-dev.org/files
 SFML_DEPENDENCIES = xcb-util-image openal jpeg flac
@@ -13,10 +13,10 @@ SFML_INSTALL_STAGING = YES
 
 define SFML_EXTRACT_CMDS
 	mkdir -p $(SFML_DIR)
-	$(UNZIP) -d $(SFML_DIR)/ $(DL_DIR)/$(SFML_SOURCE)
+	$(UNZIP) -d $(SFML_DIR)/ $(DL_DIR)/$(SFML_DL_SUBDIR)/$(SFML_SOURCE)
 	mv $(SFML_DIR)/SFML-$(SFML_VERSION)/* $(SFML_DIR)
 	mv $(SFML_DIR)/SFML-$(SFML_VERSION)/.editorconfig $(SFML_DIR)
-	rmdir $(SFML_DIR)/SFML-$(SFML_VERSION)
+	rm -rf $(SFML_DIR)/SFML-$(SFML_VERSION)
 endef
 
 $(eval $(cmake-package))
