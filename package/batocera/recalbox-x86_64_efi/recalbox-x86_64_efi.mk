e4246321ddaef0159cf75811c5aafc340ac0b536
Juliano Dorigão <jdorigao@gmail.com>
2018-05-19 21:26:00 -0400

    fix: recalbox-x86_64_efi

--- package/batocera/recalbox-x86_64_efi/recalbox-x86_64_efi.mk
+++ package/batocera/recalbox-x86_64_efi/recalbox-x86_64_efi.mk
@@ -9,7 +9,7 @@ RECALBOX_X86_64_EFI_SOURCE = bootx64.efi.gz
 RECALBOX_X86_64_EFI_SITE = https://github.com/batocera-linux/recalbox-x86_64_efi/releases/download/$(RECALBOX_X86_64_EFI_VERSION)
 
 define RECALBOX_X86_64_EFI_EXTRACT_CMDS
-	cp $(DL_DIR)/$(RECALBOX_X86_64_EFI_SOURCE) $(@D)
+	cp $(DL_DIR)/$(RECALBOX_X86_64_EFI_DL_SUBDIR)/$(RECALBOX_X86_64_EFI_SOURCE) $(@D)
 	gunzip $(@D)/$(RECALBOX_X86_64_EFI_SOURCE)
 endef
 
