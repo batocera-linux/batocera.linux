f7eff93e33acd7aa17e215e7ba8a4709f37d57ed
Juliano Dorigão <jdorigao@gmail.com>
2018-05-24 00:09:13 -0400

    Update: dolphin-emu

--- package/batocera/dolphin-emu/dolphin-emu.mk
+++ package/batocera/dolphin-emu/dolphin-emu.mk
@@ -3,14 +3,13 @@
 # DOLPHIN_EMU
 #
 ################################################################################
-
-# 5.0 + pad name bug fixed
-DOLPHIN_EMU_VERSION = 7d681f9c931b63f3ecec36c1ef515dc747df7ac4
+# Version.: Commits on May 23, 2018
+DOLPHIN_EMU_VERSION = a60bba37cd208cf365a6c6cf4f0f17c6fea8e169
 
 DOLPHIN_EMU_SITE = $(call github,dolphin-emu,dolphin,$(DOLPHIN_EMU_VERSION))
 DOLPHIN_EMU_DEPENDENCIES = xserver_xorg-server libevdev ffmpeg zlib libpng lzo libusb libcurl sfml bluez5_utils wxwidgets hidapi
 
 # bluez is disable otherwise a compilation error appears
-DOLPHIN_EMU_CONF_OPTS = -DOPENGL_INCLUDE_DIR=$(STAGING_DIR)/usr/include -DENABLE_QT2=False
+DOLPHIN_EMU_CONF_OPTS = -DBUILD_SHARED_LIBS=OFF -DOPENGL_INCLUDE_DIR=$(STAGING_DIR)/usr/include -DENABLE_QT2=False
 
 $(eval $(cmake-package))
