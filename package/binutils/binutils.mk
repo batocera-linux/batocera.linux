35f4f5de3592df0c307414f2a5ddc42e0f361009
Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>
2018-06-04 21:24:10 +0200

    binutils: strings
    Signed-off-by: Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>

--- package/binutils/binutils.mk
+++ package/binutils/binutils.mk
@@ -133,7 +133,7 @@ endif
 # Hardlinks between binaries in different directories cause a problem
 # with rpath fixup, so we de-hardlink those binaries, and replace them
 # with copies instead.
-BINUTILS_TOOLS = ar as ld ld.bfd nm objcopy objdump ranlib readelf strip
+BINUTILS_TOOLS = ar as ld ld.bfd nm objcopy objdump ranlib readelf strip strings
 define HOST_BINUTILS_FIXUP_HARDLINKS
 	$(foreach tool,$(BINUTILS_TOOLS),\
 		rm -f $(HOST_DIR)/$(GNU_TARGET_NAME)/bin/$(tool) && \
