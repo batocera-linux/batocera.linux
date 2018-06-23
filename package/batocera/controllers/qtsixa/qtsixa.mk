d6bbf78eb4ad33ea21a0f5e0e5b5d3e9785288cd
Juliano Dorigão <jdorigao@gmail.com>
2018-06-23 11:55:03 -0400

    fix: qtsixa dependencies

--- package/batocera/controllers/qtsixa/qtsixa.mk
+++ package/batocera/controllers/qtsixa/qtsixa.mk
@@ -3,16 +3,17 @@
 # qtsixa
 #
 ################################################################################
-QTSIXA_VERSION = gasia 
+QTSIXA_VERSION = gasia
 QTSIXA_SITE = $(call github,batocera-linux,qtsixa,$(QTSIXA_VERSION))
 QTSIXA_DEPENDENCIES = sdl linux-headers libusb-compat bluez5_utils
 PKGCONFIG_CONFIG=$(STAGING_DIR)/usr/lib/pkgconfig
 
 QTSIXA_INCLUDES =-I$(STAGING_DIR)/usr/include
-QTSIXA_CFLAGS = -D__ARM_PCS_VFP -DARM_ARCH -Wall $(QTSIXA_INCLUDES) 
+QTSIXA_CFLAGS = -D__ARM_PCS_VFP -DARM_ARCH -Wall $(QTSIXA_INCLUDES)
 QTSIXA_LIBS = -ldl -lpthread -lz -L$(STAGING_DIR)/usr/lib -lrt -lusb -lbluetooth
 
 ifeq ($(BR2_PACKAGE_RPI_USERLAND),y)
+	QTSIXA_DEPENDENCIES += rpi-userland
 	QTSIXA_INCLUDES +=-I$(STAGING_DIR)/usr/include/interface/vcos/pthreads -I$(STAGING_DIR)/usr/include/interface/vmcs_host/linux
 	QTSIXA_CFLAGS += -DRPI_BUILD
 	QTSIXA_LIBS += -lbcm_host -lvcos -lvchiq_arm -lvchostif
