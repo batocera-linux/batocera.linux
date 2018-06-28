aed293710e711642e5665065a2216ea9853a5f8c
Juliano Dorigão <jdorigao@gmail.com>
2018-06-28 11:12:33 -0400

    update: ppsspp v1.6.3

--- package/batocera/emulators/ppsspp/ppsspp.mk
+++ package/batocera/emulators/ppsspp/ppsspp.mk
@@ -3,23 +3,51 @@
 # PPSSPP
 #
 ################################################################################
-PPSSPP_VERSION = v1.5.4
-#PPSSPP_SITE = $(call github,hrydgard,ppsspp,$(PPSSPP_VERSION))
+# Version.: Commits on Jun 27, 2018 (v1.6.3)
+PPSSPP_VERSION = 5abf324da97597d386255ff5dd6b082c19fb1964
 PPSSPP_SITE = https://github.com/hrydgard/ppsspp.git
 PPSSPP_SITE_METHOD=git
 PPSSPP_GIT_SUBMODULES=YES
 PPSSPP_DEPENDENCIES = sdl2 zlib libzip zip ffmpeg
 
-# required at least on x86
-ifeq ($(BR2_PACKAGE_LIBGLU),y)
-PPSSPP_DEPENDENCIES += libglu
+# x86
+ifeq ($(BR2_PACKAGE_RECALBOX_TARGET_X86),y)
+	PPSSPP_CONF_OPTS += -DX86=ON
 endif
 
-define PPSSPP_CONFIGURE_PI
-	sed -i "s+/opt/vc+$(STAGING_DIR)/usr+g" $(@D)/CMakeLists.txt
-endef
+# x86_64
+ifeq ($(BR2_PACKAGE_RECALBOX_TARGET_X86_64),y)
+	PPSSPP_CONF_OPTS += -DX86_64=ON
+endif
 
-PPSSPP_PRE_CONFIGURE_HOOKS += PPSSPP_CONFIGURE_PI
+# odroid c2 / S905 and variants
+ifeq ($(BR2_aarch64),y)
+	# -DUSE_SYSTEM_FFMPEG=1 is unstable, but at least, games without videos work
+	PPSSPP_CONF_OPTS += -DUSE_FFMPEG=ON -DUSE_SYSTEM_FFMPEG=ON -DARM_NEON=ON
+else
+	PPSSPP_CONF_OPTS += -DUSE_FFMPEG=ON
+endif
+
+# odroid xu4
+ifeq ($(BR2_PACKAGE_SDL2_KMSDRM),y)
+	PPSSPP_CONF_OPTS += -DUSING_EGL=OFF -DUSING_X11_VULKAN=OFF
+endif
+
+# rpi1 / rpi2 /rp3
+ifeq ($(BR2_PACKAGE_RPI_USERLAND),y)
+	PPSSPP_DEPENDENCIES += rpi-userland
+	PPSSPP_CONF_OPTS += -DRASPBIAN=ON -DUSING_FBDEV=ON -DUSING_GLES2=ON -DUSING_EGL=OFF -DUSING_X11_VULKAN=OFF
+endif
+
+# odroid xu4 / rpi3
+ifeq ($(BR2_arm),y)
+	PPSSPP_CONF_OPTS += -DARMV7=ON
+endif
+
+# s912 (libhybris)
+ifeq ($(BR2_PACKAGE_LIBHYBRIS),y)
+	PPSSPP_CONF_OPTS += -DUSING_FBDEV=ON -DUSING_GLES2=ON -DUSING_EGL=OFF -DUSING_X11_VULKAN=OFF
+endif
 
 define PPSSPP_INSTALL_TO_TARGET
 	$(INSTALL) -D -m 0755 $(@D)/PPSSPPSDL $(TARGET_DIR)/usr/bin
@@ -27,43 +55,5 @@ define PPSSPP_INSTALL_TO_TARGET
 endef
 
 PPSSPP_INSTALL_TARGET_CMDS = $(PPSSPP_INSTALL_TO_TARGET)
-
-ifeq ($(BR2_aarch64),y)
-# -DUSE_SYSTEM_FFMPEG=1 is unstable, but at least, games without videos work
-PPSSPP_CONF_OPTS += -DUSE_FFMPEG=1 -DUSE_SYSTEM_FFMPEG=1
-else
-PPSSPP_CONF_OPTS += -DUSE_FFMPEG=1
-endif
-
-ifeq ($(BR2_PACKAGE_MALI_OPENGLES_SDK),y)
-	PPSSPP_CONF_OPTS += -DUSING_FBDEV=1
-	PPSSPP_CONF_OPTS += -DUSING_GLES2=1
-
-	ifeq ($(BR2_PACKAGE_SDL2_KMSDRM),y)
-	PPSSPP_CONF_OPTS += -DUSING_EGL=0
-	endif
-endif
-
-ifeq ($(BR2_PACKAGE_RPI_USERLAND),y)
-	PPSSPP_DEPENDENCIES += rpi-userland
-	PPSSPP_CONF_OPTS += -DUSING_FBDEV=1
-	PPSSPP_CONF_OPTS += -DUSING_GLES2=1
-endif
-
-ifeq ($(BR2_aarch64)$(BR2_ARM_CPU_HAS_NEON),y)
-	PPSSPP_CONF_OPTS += -DARM_NEON=1
-endif
-
-# odroid xu4 / rpi3
-ifeq ($(BR2_arm),y)
-	PPSSPP_CONF_OPTS += -DARMV7=1
-endif
-
-# libhybris
-ifeq ($(BR2_PACKAGE_LIBHYBRIS),y)
-	PPSSPP_CONF_OPTS += -DUSING_FBDEV=1
-	PPSSPP_CONF_OPTS += -DUSING_GLES2=1
-	PPSSPP_CONF_OPTS += -DUSING_EGL=1
-endif
 
 $(eval $(cmake-package))
