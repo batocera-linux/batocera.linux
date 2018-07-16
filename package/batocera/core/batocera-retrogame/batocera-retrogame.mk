a22df4ea217ee901afb949f785247c8f6e3b1aad
Juliano Dorigão <jdorigao@gmail.com>
2018-07-16 01:04:11 -0400

    rename: recalbox-retrogame

--- package/batocera/core/batocera-retrogame/batocera-retrogame.mk
+++ package/batocera/core/batocera-retrogame/batocera-retrogame.mk
@@ -1,16 +1,16 @@
 ################################################################################
 #
-# Retrogame Recalbox https://github.com/ian57/Recalbox-Retrogame-2Players-Pi2
+# Retrogame Batocera https://github.com/batocera-linux/Recalbox-Retrogame-2Players-Pi2
 #
 ################################################################################
-RECALBOX_RETROGAME_VERSION = 8d3e90ed179146d717201b6f4337290100f9ca26
-RECALBOX_RETROGAME_SITE = $(call github,batocera-linux,Recalbox-Retrogame-2Players-Pi2,$(RECALBOX_RETROGAME_VERSION))
+BATOCERA_RETROGAME_VERSION = 8d3e90ed179146d717201b6f4337290100f9ca26
+BATOCERA_RETROGAME_SITE = $(call github,batocera-linux,Recalbox-Retrogame-2Players-Pi2,$(BATOCERA_RETROGAME_VERSION))
 
-define RECALBOX_RETROGAME_BUILD_CMDS
+define BATOCERA_RETROGAME_BUILD_CMDS
 	CFLAGS="$(TARGET_CFLAGS)" CXXFLAGS="$(TARGET_CXXFLAGS)" $(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" -C $(@D) retrogame
 endef
 
-define RECALBOX_RETROGAME_INSTALL_TARGET_CMDS
+define BATOCERA_RETROGAME_INSTALL_TARGET_CMDS
 	$(INSTALL) -D $(@D)/retrogame \
 		$(TARGET_DIR)/usr/bin/recalbox-retrogame
 endef
