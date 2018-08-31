79248411b78e99c812aa014d7f695b01dcefd141
Juliano Dorigão <jdorigao@gmail.com>
2018-08-31 11:24:37 -0400

    update: libretro-pcsx (Retroarch 1.7.4)

--- package/batocera/emulators/retroarch/libretro/libretro-pcsx/libretro-pcsx.mk
+++ package/batocera/emulators/retroarch/libretro/libretro-pcsx/libretro-pcsx.mk
@@ -3,7 +3,8 @@
 # PCSXREARMED
 #
 ################################################################################
-LIBRETRO_PCSX_VERSION = c6e7ce9ef2e39e798dfc0b0fe071064b11b4ca9e
+# Version.: Commits on Aug 28, 2018
+LIBRETRO_PCSX_VERSION = b6169792c70a5ebb2e4baef63af5dc375be89770
 LIBRETRO_PCSX_SITE = $(call github,libretro,pcsx_rearmed,$(LIBRETRO_PCSX_VERSION))
 
 define LIBRETRO_PCSX_BUILD_CMDS
