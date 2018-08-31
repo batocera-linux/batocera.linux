055e901af107384387138e9d6661049fb75a76eb
Juliano Dorigão <jdorigao@gmail.com>
2018-08-31 14:42:44 -0400

    update: libretro-reicast-oit (Retroarch 1.7.4)

--- package/batocera/emulators/retroarch/libretro/libretro-reicast-oit/libretro-reicast-oit.mk
+++ package/batocera/emulators/retroarch/libretro/libretro-reicast-oit/libretro-reicast-oit.mk
@@ -3,12 +3,12 @@
 # LIBRETRO REICAST OIT
 #
 ################################################################################
-# Version.: Commits on Jul 14, 2018
-LIBRETRO_REICAST_OIT_VERSION = fc8eb6db0484def28291466f3d03d305db1098bc
+# Version.: Commits on Aug 31, 2018
+LIBRETRO_REICAST_OIT_VERSION = 6c1f972e793eb7d37edaa67f19f7d03e9f908075
 LIBRETRO_REICAST_OIT_SITE = $(call github,libretro,reicast-emulator,$(LIBRETRO_REICAST_OIT_VERSION))
 
 define LIBRETRO_REICAST_OIT_BUILD_CMDS
-        CFLAGS="$(TARGET_CFLAGS)" CXXFLAGS="$(TARGET_CXXFLAGS)" $(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" AR="$(TARGET_AR)" -C $(@D) -f Makefile platform="$(LIBRETRO_PLATFORM)" HAVE_OIT=1
+        CFLAGS="$(TARGET_CFLAGS)" CXXFLAGS="$(TARGET_CXXFLAGS)" $(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" AR="$(TARGET_AR)" -C $(@D) -f Makefile HAVE_OPENMP=0 HAVE_OIT=1 platform="$(LIBRETRO_PLATFORM)"
 endef
 
 define LIBRETRO_REICAST_OIT_INSTALL_TARGET_CMDS
