6673f9f1a965897820936770e5585559ed7f7319
Juliano Dorigão <jdorigao@gmail.com>
2018-09-05 16:15:22 -0400

    libretro-beetle-psx with hw
    Added the option to choose between rendering by software or hardware in libretro-beetle-psx

--- package/batocera/emulators/retroarch/libretro/libretro-beetle-psx/libretro-beetle-psx.mk
+++ package/batocera/emulators/retroarch/libretro/libretro-beetle-psx/libretro-beetle-psx.mk
@@ -8,11 +8,11 @@ LIBRETRO_BEETLE_PSX_VERSION = 4a75947e0f979c10ab0337d9bbd7b4f485674537
 LIBRETRO_BEETLE_PSX_SITE = $(call github,libretro,beetle-psx-libretro,$(LIBRETRO_BEETLE_PSX_VERSION))
 
 define LIBRETRO_BEETLE_PSX_BUILD_CMDS
-        CFLAGS="$(TARGET_CFLAGS)" CXXFLAGS="$(TARGET_CXXFLAGS)" $(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" AR="$(TARGET_AR)" -C $(@D) -f Makefile platform="$(LIBRETRO_PLATFORM)"
+        CFLAGS="$(TARGET_CFLAGS)" CXXFLAGS="$(TARGET_CXXFLAGS)" $(MAKE) LD="$(TARGET_CXX)" CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" AR="$(TARGET_AR)" -C $(@D) -f Makefile HAVE_HW=1 platform="$(LIBRETRO_PLATFORM)"
 endef
 
 define LIBRETRO_BEETLE_PSX_INSTALL_TARGET_CMDS
-	$(INSTALL) -D $(@D)/mednafen_psx_libretro.so \
+	$(INSTALL) -D $(@D)/mednafen_psx_hw_libretro.so \
 		$(TARGET_DIR)/usr/lib/libretro/mednafen_psx_libretro.so
 endef
 
