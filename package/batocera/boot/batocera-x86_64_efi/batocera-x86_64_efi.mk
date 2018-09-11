a4a2992a172b6d58f4df06186305d85c07ceaf84
Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>
2018-09-11 22:45:38 +0200

    efi prompt
    Signed-off-by: Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>

--- package/batocera/boot/batocera-x86_64_efi/batocera-x86_64_efi.mk
+++ package/batocera/boot/batocera-x86_64_efi/batocera-x86_64_efi.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-BATOCERA_X86_64_EFI_VERSION = 1.0
+BATOCERA_X86_64_EFI_VERSION = 1.1
 BATOCERA_X86_64_EFI_SOURCE = bootx64.efi.gz
-BATOCERA_X86_64_EFI_SITE = https://github.com/batocera-linux/recalbox-x86_64_efi/releases/download/$(BATOCERA_X86_64_EFI_VERSION)
+BATOCERA_X86_64_EFI_SITE = https://github.com/batocera-linux/batocera-x86_64_efi/releases/download/$(BATOCERA_X86_64_EFI_VERSION)
 
 define BATOCERA_X86_64_EFI_EXTRACT_CMDS
 	cp $(DL_DIR)/$(BATOCERA_X86_64_EFI_DL_SUBDIR)/$(BATOCERA_X86_64_EFI_SOURCE) $(@D)
