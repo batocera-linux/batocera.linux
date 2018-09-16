a830625b48d3d72b401017a71694cf0d179c4af2
Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>
2018-09-16 08:53:07 +0200

    pcsx2: fix plugins path
    Signed-off-by: Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>

--- package/batocera/emulators/pcsx2_avx2/pcsx2_avx2.mk
+++ package/batocera/emulators/pcsx2_avx2/pcsx2_avx2.mk
@@ -25,7 +25,7 @@ define PCSX2_AVX2_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 -D $(@D)/pcsx2/PCSX2 $(TARGET_DIR)/usr/PCSX_AVX2/bin/PCSX2
 	cp -pr $(@D)/bin/Langs      $(TARGET_DIR)/usr/PCSX_AVX2/bin
         mkdir -p $(TARGET_DIR)/usr/PCSX_AVX2/bin/plugins
-	cp -pr $(@D)/plugins/*/*.so $(@D)/plugins/*/*/*.so $(TARGET_DIR)/usr/PCSX/bin/plugins
+	cp -pr $(@D)/plugins/*/*.so $(@D)/plugins/*/*/*.so $(TARGET_DIR)/usr/PCSX_AVX2/bin/plugins
 endef
 
 $(eval $(cmake-package))
