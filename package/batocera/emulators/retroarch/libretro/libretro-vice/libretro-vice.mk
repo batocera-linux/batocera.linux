3b7cf684994c9097c5d967f26279c479afdc9353
Juliano Dorigão <jdorigao@gmail.com>
2018-09-20 19:22:07 -0400

    package: libretro-vice

---
+++ package/batocera/emulators/retroarch/libretro/libretro-vice/libretro-vice.mk
@@ -0,0 +1,20 @@
+################################################################################
+#
+# LIBRETRO-VICE
+#
+################################################################################
+# Version.: Commits on Sep 16, 2018
+LIBRETRO_VICE_VERSION = 0fc467f2fd4a0c44f3e94d05f413db72bf2e827a
+LIBRETRO_VICE_SITE = $(call github,libretro,vice-libretro,$(LIBRETRO_VICE_VERSION))
+
+define LIBRETRO_VICE_BUILD_CMDS
+	CFLAGS="$(TARGET_CFLAGS)" CXXFLAGS="$(TARGET_CXXFLAGS)" \
+        $(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" LD="$(TARGET_LD)" RANLIB="$(TARGET_RANLIB)" AR="$(TARGET_AR)" -C $(@D) -f Makefile.libretro platform="$(LIBRETRO_PLATFORM)"
+endef
+
+define LIBRETRO_VICE_INSTALL_TARGET_CMDS
+	$(INSTALL) -D $(@D)/vice_*_libretro.so \
+		$(TARGET_DIR)/usr/lib/libretro/vice_libretro.so
+endef
+
+$(eval $(generic-package))
