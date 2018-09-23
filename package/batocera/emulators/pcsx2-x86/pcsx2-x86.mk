f80f43b2198818e137827789f1723549e40d3c47
Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>
2018-09-23 16:56:09 +0200

    pcsx2: x86_64
    Signed-off-by: Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>

---
+++ package/batocera/emulators/pcsx2-x86/pcsx2-x86.mk
@@ -0,0 +1,20 @@
+################################################################################
+#
+# pcsx2-x86
+#
+################################################################################
+
+# version 5.18 means binary from 5.18 version (or the last built if the version is not yet out)
+PCSX2_X86_VERSION = 5.18
+PCSX2_X86_SOURCE = pcsx2-x86-$(PCSX2_X86_VERSION).tar.gz
+PCSX2_X86_SITE = https://github.com/batocera-linux/pcsx2-x86/releases/download/$(PCSX2_X86_VERSION)
+
+define PCSX2_X86_EXTRACT_CMDS
+	mkdir -p $(@D)/target && cd $(@D)/target && tar xf $(DL_DIR)/$(PCSX2_X86_DL_SUBDIR)/$(PCSX2_X86_SOURCE)
+endef
+
+define PCSX2_X86_INSTALL_TARGET_CMDS
+	cp -pr $(@D)/target/* $(TARGET_DIR)
+endef
+
+$(eval $(generic-package))
