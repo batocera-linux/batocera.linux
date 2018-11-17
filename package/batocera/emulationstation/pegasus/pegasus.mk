c030a14df995901f48d68bf53523ebe0b0f8ea12
Tiago Pierezan Camargo <camargo@discworld.com.br>
2018-11-17 11:00:20 -0200

    pegasus: add missing dependencies

--- package/batocera/emulationstation/pegasus/pegasus.mk
+++ package/batocera/emulationstation/pegasus/pegasus.mk
@@ -9,7 +9,7 @@ PEGASUS_SITE = https://github.com/mmatyas/pegasus-frontend.git
 PEGASUS_SITE_METHOD = git
 PEGASUS_GIT_SUBMODULES = YES
 PEGASUS_LICENSE = GPL3
-PEGASUS_DEPENDENCIES = qt5base qt5multimedia qt5declarative qt5quickcontrols2 qt5graphicaleffects qt5tools qt5imageformats qt5svg qt5gamepad
+PEGASUS_DEPENDENCIES = qt5base qt5multimedia qt5declarative qt5quickcontrols2 qt5graphicaleffects qt5tools qt5imageformats qt5svg qt5gamepad gst1-plugins-good gst1-libav gst1-plugins-base
 
 define PEGASUS_CONFIGURE_CMDS
     (cd $(@D); $(TARGET_MAKE_ENV) $(HOST_DIR)/bin/qmake pegasus.pro)
