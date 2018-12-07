e4ea3c4f500e38d916bdf36e0a94096747b32724
Tiago Pierezan Camargo <camargo@discworld.com.br>
2018-12-06 23:48:34 -0200

    dolphin: update to 4e5f83d23fcee2e744890246360281639bf2ad18

--- package/batocera/emulators/dolphin-emu/dolphin-emu.mk
+++ package/batocera/emulators/dolphin-emu/dolphin-emu.mk
@@ -1,15 +1,16 @@
 ################################################################################
 #
-# DOLPHIN_EMU
+# DOLPHIN EMU
 #
 ################################################################################
-# Version.: Commits on May 23, 2018
-DOLPHIN_EMU_VERSION = a60bba37cd208cf365a6c6cf4f0f17c6fea8e169
-
+# Version.: Commits on December 5, 2018
+DOLPHIN_EMU_VERSION = 4e5f83d23fcee2e744890246360281639bf2ad18
 DOLPHIN_EMU_SITE = $(call github,dolphin-emu,dolphin,$(DOLPHIN_EMU_VERSION))
-DOLPHIN_EMU_DEPENDENCIES = xserver_xorg-server libevdev ffmpeg zlib libpng lzo libusb libcurl sfml bluez5_utils wxwidgets hidapi
+DOLPHIN_EMU_DEPENDENCIES = xserver_xorg-server libevdev ffmpeg zlib libpng lzo libusb libcurl sfml bluez5_utils qt5base hidapi
+DOLPHIN_EMU_SUPPORTS_IN_SOURCE_BUILD = NO
 
-# bluez is disable otherwise a compilation error appears
-DOLPHIN_EMU_CONF_OPTS = -DBUILD_SHARED_LIBS=OFF -DOPENGL_INCLUDE_DIR=$(STAGING_DIR)/usr/include -DENABLE_QT2=False
+DOLPHIN_EMU_CONF_OPTS  = -DTHREADS_PTHREAD_ARG=OFF
+DOLPHIN_EMU_CONF_OPTS += -DUSE_DISCORD_PRESENCE=OFF
+DOLPHIN_EMU_CONF_OPTS += -DBUILD_SHARED_LIBS=OFF
 
 $(eval $(cmake-package))
