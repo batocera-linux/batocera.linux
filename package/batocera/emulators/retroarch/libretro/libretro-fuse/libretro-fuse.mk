6d52666b62bfe1be496fbb0a8433176e1383c341
Juliano Dorigão <jdorigao@gmail.com>
2018-12-08 15:46:11 -0300

    update: libretro-fuse

--- package/batocera/emulators/retroarch/libretro/libretro-fuse/libretro-fuse.mk
+++ package/batocera/emulators/retroarch/libretro/libretro-fuse/libretro-fuse.mk
@@ -3,8 +3,8 @@
 # FUSE
 #
 ################################################################################
-# Version.: Commits on Aug 12, 2018
-LIBRETRO_FUSE_VERSION = 411487a48a158e2c352ba077bd7f00ee1132a3d3
+# Version.: Commits on Nov 11, 2018
+LIBRETRO_FUSE_VERSION = 98b9ba17bad15a6ed60831f53ba34654b1a1bcde
 LIBRETRO_FUSE_SITE = $(call github,libretro,fuse-libretro,$(LIBRETRO_FUSE_VERSION))
 
 define LIBRETRO_FUSE_BUILD_CMDS
