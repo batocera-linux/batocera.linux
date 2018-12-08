ed0e5c930a5deb50632e11880636f2c8b430ea27
Juliano Dorigão <jdorigao@gmail.com>
2018-12-08 15:47:04 -0300

    update: libretro-gw

--- package/batocera/emulators/retroarch/libretro/libretro-gw/libretro-gw.mk
+++ package/batocera/emulators/retroarch/libretro/libretro-gw/libretro-gw.mk
@@ -3,8 +3,8 @@
 # GW
 #
 ################################################################################
-# Version.: Commits on Aug 12, 2018
-LIBRETRO_GW_VERSION = 3235ebacb767ab790efffe9a285b330d036c842c
+# Version.: Commits on Nov 12, 2018
+LIBRETRO_GW_VERSION = 92717c64928274f12ea8db645f962cd1e4a1bc5b
 LIBRETRO_GW_SITE = $(call github,libretro,gw-libretro,$(LIBRETRO_GW_VERSION))
 
 define LIBRETRO_GW_BUILD_CMDS
