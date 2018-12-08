395cbd3df643fab72a033b247bdd3e797fa5806b
Juliano Dorigão <jdorigao@gmail.com>
2018-12-08 15:50:56 -0300

    update: libretro-scummvm

--- package/batocera/emulators/retroarch/libretro/libretro-scummvm/libretro-scummvm.mk
+++ package/batocera/emulators/retroarch/libretro/libretro-scummvm/libretro-scummvm.mk
@@ -3,8 +3,8 @@
 # SCUMMVM
 #
 ################################################################################
-# VERSION.: Commits on May 23, 2018
-LIBRETRO_SCUMMVM_VERSION = 60c47acfcbdd664cd3adeb97688b5466e82d70ea
+# VERSION.: Commits on Oct 9, 2018
+LIBRETRO_SCUMMVM_VERSION = 20d71cd7189ae7fdd453a3041f3103bffabea13e
 LIBRETRO_SCUMMVM_SITE = $(call github,libretro,scummvm,$(LIBRETRO_SCUMMVM_VERSION))
 
 define LIBRETRO_SCUMMVM_BUILD_CMDS
