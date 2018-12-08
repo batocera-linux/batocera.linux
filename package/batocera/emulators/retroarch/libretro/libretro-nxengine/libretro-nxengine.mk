3af1ed08ebdb6e56a9b7d49a57ef612e58a066bc
Juliano Dorigão <jdorigao@gmail.com>
2018-12-08 15:48:54 -0300

    update: libretro-nxengine

--- package/batocera/emulators/retroarch/libretro/libretro-nxengine/libretro-nxengine.mk
+++ package/batocera/emulators/retroarch/libretro/libretro-nxengine/libretro-nxengine.mk
@@ -3,8 +3,8 @@
 # NXENGINE
 #
 ################################################################################
-# Version.: Commits on Aug 29, 2018
-LIBRETRO_NXENGINE_VERSION = 053e28334cf46ce8ea12041b3fe6fd173cc07593
+# Version.: Commits on Nov 6, 2018
+LIBRETRO_NXENGINE_VERSION = b460024c23ef2cb8a70e1ae00b6b425abaaf49a4
 LIBRETRO_NXENGINE_SITE = $(call github,libretro,nxengine-libretro,$(LIBRETRO_NXENGINE_VERSION))
 
 define LIBRETRO_NXENGINE_BUILD_CMDS
