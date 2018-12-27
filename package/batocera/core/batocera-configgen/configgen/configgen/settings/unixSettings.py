fb4b3e833967ca99f0f28a7fc2b03e1d4668f49d
Tiago Pierezan Camargo <camargo@discworld.com.br>
2018-12-26 22:01:17 -0200

    refactor UnixSettings to use ConfigParser as backend

--- package/batocera/core/batocera-configgen/configgen/configgen/settings/unixSettings.py
+++ package/batocera/core/batocera-configgen/configgen/configgen/settings/unixSettings.py
@@ -1,65 +1,71 @@
 #!/usr/bin/env python
 
-import re
+import ConfigParser
+import StringIO
 import os
+import re
+import utils.eslog as eslog
 
+__source__ = os.path.basename(__file__)
 
 class UnixSettings():
+
     def __init__(self, settingsFile, separator='', defaultComment='#'):
         self.settingsFile = settingsFile
-
         self.separator = separator
+        # unused. left for compatibility with previous implementation
         self.comment = defaultComment
 
+        # use ConfigParser as backend.
+        eslog.log("{0}: Creating parser for {1}".format(__source__, self.settingsFile))
+        self.config = ConfigParser.ConfigParser()
+        try:
+            # TODO: remove me when we migrate to Python 3
+            # pretend where have a [DEFAULT] section
+            file = StringIO.StringIO()
+            file.write('[DEFAULT]\n')
+            file.write(open(self.settingsFile).read())
+            file.seek(0, os.SEEK_SET)
+
+            self.config.readfp(file)
+        except IOError, e:
+            eslog.log("{0}: {1}".format(__source__, str(e)))
+
+
     def load(self, name, default=None):
-        separ = self.separator
-        if separ is not '':
-            separ += "?"
-        if not os.path.isfile(self.settingsFile):
-            return default
-        with open(self.settingsFile) as lines:
-            for line in open(self.settingsFile):
-                if name in line:
-                    m = re.match(r"^" + name + separ + "=" + separ + "\"(.+)\"", line)
-                    if m:
-                        return m.group(1)
-                    else:
-                        m = re.match(r"^" + name + separ + "=" + separ + "(.+)", line)
-                        if m:
-                            return m.group(1)
-            return default
+        eslog.log("{0}: Looking for {1} from {2}".format(__source__, name, self.settingsFile))
+        return self.config.get('DEFAULT', name, default)
 
     def save(self, name, value):
-        if os.path.isfile(self.settingsFile):
-            os.system(
-                "sed -i 's|^{}\?{}{}=.*|{}{}={}{}|g' {}".format(self.comment, name, self.separator, name,
-                                                                  self.separator, self.separator,
-                                                                  value, self.settingsFile))
-        if self.load(name) is None:
-            with open(self.settingsFile, "a+") as settings:
-                settings.write("\n{}{}={}{}".format(name, self.separator, self.separator, value))
+        eslog.log("{0}: Writing {1} = {2} to {3}".format(__source__, name, value, self.settingsFile))
+        # TODO: should we call loadAll first?
+        # TODO: do we need proper section support? PSP config is an ini file
+        self.config.set('DEFAULT', name, str(value))
+        fp = open(self.settingsFile, 'w')
+        for (key, value) in self.config.items('DEFAULT'):
+            fp.write("{0}{2}={2}{1}\n".format(key, str(value), self.separator))
+        fp.close()
 
     def disable(self, name):
-        os.system(
-            "sed -i \"s|^.*\({}{}\?=.*\)|{}\\1|g\" {}".format(name, self.separator, self.comment, self.settingsFile))
+        raise Exception
+        # TODO: check if is ok to remove option instead of comment it
+        self.config.remove(name)
 
     def disableAll(self, name):
-        os.system("sed -i \"s|^.*\({}.*\)|{}\\1|g\" {}".format(name, self.comment, self.settingsFile))
+        eslog.log("{0}: Disabling all options from from {1}".format(__source__, self.settingsFile))
+        # TODO: check if is ok to remove whole DEFAULT section
+        self.config.remove_section('DEFAULT')
 
     def remove(self, name):
-        os.system(
-            "sed -i \"\\|^.*\({}{}\?=.*\)|d\" {}".format(name, self.separator, self.settingsFile))
+        raise Exception
+        self.config.remove_option('DEFAULT', name)
 
     def loadAll(self, name):
+        eslog.log("{0}: Looking for {1}.* from {2}".format(__source__, name, self.settingsFile))
         res = dict()
-        with open(self.settingsFile) as lines:
-            for line in lines:
-                if name in line:
-                    m = re.match(r"^" + name + "\.(.+?)=" + self.separator + "\"(.+)\"", line)
-                    if m:
-                        res[m.group(1)] = m.group(2);
-                    else:
-                        m = re.match(r"^" + name + "\.(.+?)=" + self.separator + "(.+)", line)
-                        if m:
-                            res[m.group(1)] = m.group(2);
+        for (key, value) in self.config.items('DEFAULT'):
+            m = re.match(r"^" + name + "\.(.+?)", key)
+            if m:
+                res[m.group(1)] = value;
+
         return res
