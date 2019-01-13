85cf87054ce3b29a02c112208521544a386f5928
Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>
2019-01-13 21:35:18 +0100

    fix psp frameskip
    Signed-off-by: Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>

--- package/batocera/core/batocera-configgen/configgen/configgen/generators/ppsspp/ppssppConfig.py
+++ package/batocera/core/batocera-configgen/configgen/configgen/generators/ppsspp/ppssppConfig.py
@@ -12,15 +12,32 @@ sys.path.append(
 ppssppSettings = UnixSettings(recalboxFiles.ppssppConfig, separator=' ')
 
 def writePPSSPPConfig(system):
+    writePPSSPPConfigDefault()
     writePPSSPPConfigToFile(createPPSSPPConfig(system))
+
+def writePPSSPPConfigDefault():
+    if not os.path.exists(recalboxFiles.ppssppConfig):
+        # write default values template, so that the rest of the config can set values
+        f = open(recalboxFiles.ppssppConfig, "w")
+        f.write("[Graphics]\n")
+        f.write("FrameSkip = 0\n")
+        f.write("ShowFPSCounter = 0\n")
+        f.close()
 
 def createPPSSPPConfig(system):
     ppssppConfig = dict()
+
     # Display FPS
     if system.isOptSet('showFPS') and system.getOptBoolean('showFPS') == True:
         ppssppConfig['ShowFPSCounter'] = '3' # 1 for Speed%, 2 for FPS, 3 for both
     else:
         ppssppConfig['ShowFPSCounter'] = '0'
+
+    # Performances
+    if system.isOptSet('frameskip') and system.getOptBoolean('frameskip') == True:
+        ppssppConfig['FrameSkip'] = '1'
+    else:
+        ppssppConfig['FrameSkip'] = '0'
 
     return ppssppConfig
 
