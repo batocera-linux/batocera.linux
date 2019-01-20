78008bcf99b130f8ec147db4e101ed5f89efeb83
Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>
2019-01-20 17:18:55 +0100

    ppsspp: upgrade
    Signed-off-by: Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>

--- package/batocera/core/batocera-configgen/configgen/configgen/generators/ppsspp/ppssppGenerator.py
+++ package/batocera/core/batocera-configgen/configgen/configgen/generators/ppsspp/ppssppGenerator.py
@@ -32,6 +32,7 @@ class PPSSPPGenerator(Generator):
         # the command to run  
         commandArray = [recalboxFiles.recalboxBins[system.config['emulator']]]
         commandArray.append(rom)
+        commandArray.append("--fullscreen")
         # The next line is a reminder on how to quit PPSSPP with just the HK
         #commandArray = [recalboxFiles.recalboxBins[system.config['emulator']], rom, "--escape-exit"]
         return Command.Command(array=commandArray, env={"XDG_CONFIG_HOME":recalboxFiles.CONF, "SDL_VIDEO_GL_DRIVER": "/usr/lib/libGLESv2.so", "SDL_VIDEO_EGL_DRIVER": "/usr/lib/libGLESv2.so", "PPSSPP_GAME_CONTROLLER_DB_PATH": recalboxFiles.ppssppControls})
