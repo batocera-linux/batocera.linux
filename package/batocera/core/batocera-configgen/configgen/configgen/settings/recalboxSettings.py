04e299c9cad259bf13c60d436c961142d67371db
Tiago Pierezan Camargo <camargo@discworld.com.br>
2019-01-21 17:13:29 -0200

    configgen: refactor recalboxSettings to use UnixSettings

--- package/batocera/core/batocera-configgen/configgen/configgen/settings/recalboxSettings.py
+++ package/batocera/core/batocera-configgen/configgen/configgen/settings/recalboxSettings.py
@@ -1,46 +1,15 @@
 #!/usr/bin/env python
 import sys
-import os
-if __name__ == '__main__':
-    sys.path.append(
-        os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir)))
-
-import re
+# import os
+# if __name__ == '__main__':
+#     sys.path.append(
+#         os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir)))
+#
 import argparse
 import configgen.recalboxFiles as recalboxFiles
+from configgen.settings.unixSettings import UnixSettings
 
 settingsFile = recalboxFiles.recalboxConf
-
-
-def load(name, default=None):
-    for line in open(settingsFile):
-        if name in line:
-            m = re.match(r"^" + name + "=(.+)", line)
-            if m:
-                return m.group(1)
-    return default
-
-
-def save(name, value):
-    os.system("sed -i 's|^.*" + name + "=.*|" + name + "=" + value + "|g' " + settingsFile)
-    if load(name) is None:
-        with open(settingsFile, "a") as settings:
-            settings.write("\n{}={}".format(name, value))
-
-
-def disable(name):
-    # settings=`cat "$es_settings" | sed -n "s/.*name=\"${varname}\" value=\"\(.*\)\".*/\1/p"`
-    os.system("sed -i \"s|^.*\({}=.*\)|;\\1|g\" {}".format(name,settingsFile))
-
-
-def loadAll(name):
-    res = dict()
-    for line in open(settingsFile):
-        if name in line:
-            m = re.match(r"^" + name + "\.(.+?)=(.+)", line)
-            if m:
-                res[m.group(1)] = m.group(2);
-    return res
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='recalbox-config script')
@@ -49,11 +18,19 @@ if __name__ == '__main__':
     parser.add_argument("-value", help="if command = save value to save", type=str, required=False)
     args = parser.parse_args()
 
+    config = UnixSettings(settingsFile)
+
     if args.command == "save" :
-        save(args.key, args.value)
+        # TODO: only load is unused internally. UnixSettings.write() removes file comments.
+        #       we can provide inplace editing for small changes
+        raise "not supported"
+        # save(args.key, args.value)
     if args.command == "load" :
-        loaded = load(args.key)
-        if loaded is not None:
-            sys.stdout.write(loaded)
-    if args.command == "disable" :
-        disable(args.key)
+        v = config.load(args.key)
+        if v is not None:
+            sys.stdout.write(v)
+    if args.command == "disable":
+        # TODO: only load is unused internally. UnixSettings.write() removes file comments.
+        #       we can provide inplace editing for small changes
+        raise "not supported"
+        # disable(args.key)
