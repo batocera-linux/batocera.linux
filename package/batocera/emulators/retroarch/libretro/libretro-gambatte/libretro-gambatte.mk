269dde25b0f435dec988c31b94e47c446ff7a822
Juliano Dorigão <jdorigao@gmail.com>
2019-02-04 14:30:41 -0300

    update: libretro-gambatte

--- package/batocera/emulators/retroarch/libretro/libretro-gambatte/libretro-gambatte.mk
+++ package/batocera/emulators/retroarch/libretro/libretro-gambatte/libretro-gambatte.mk
@@ -3,9 +3,10 @@
 # GAMBATTE
 #
 ################################################################################
-# Version.: Commits on Nov 11, 2018
-LIBRETRO_GAMBATTE_VERSION = 85edb5b900dcdc12df7d734f0cedcdcfbf84d70f
+# Version.: Commits on Dec 29, 2018
+LIBRETRO_GAMBATTE_VERSION = ffce1f0e93111bec77b3778273297536fcfb612a
 LIBRETRO_GAMBATTE_SITE = $(call github,libretro,gambatte-libretro,$(LIBRETRO_GAMBATTE_VERSION))
+LIBRETRO_GAMBATTE_LICENSE="GPLv2"
 
 define LIBRETRO_GAMBATTE_BUILD_CMDS
 	CFLAGS="$(TARGET_CFLAGS)" CXXFLAGS="$(TARGET_CXXFLAGS)" $(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" -C $(@D) -f Makefile platform="$(LIBRETRO_PLATFORM)"
