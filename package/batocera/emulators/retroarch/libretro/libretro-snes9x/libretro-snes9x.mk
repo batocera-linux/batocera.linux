5586985ba76e89fe7c093bc9da008765ad4d77a4
Juliano Dorigão <jdorigao@gmail.com>
2019-02-04 15:59:44 -0300

    update: libretro-snes9x

--- package/batocera/emulators/retroarch/libretro/libretro-snes9x/libretro-snes9x.mk
+++ package/batocera/emulators/retroarch/libretro/libretro-snes9x/libretro-snes9x.mk
@@ -3,9 +3,10 @@
 # SNES9X
 #
 ################################################################################
-# Version: 1.58 - 85e0a748e49b913ceb3b3a7950b970c245b24bcc
-LIBRETRO_SNES9X_VERSION = 85e0a748e49b913ceb3b3a7950b970c245b24bcc
+# Version.: Commits on Jan 26, 2019
+LIBRETRO_SNES9X_VERSION = a425ee94707648db676d91414680ff5482c82bcf
 LIBRETRO_SNES9X_SITE = $(call github,libretro,snes9x,$(LIBRETRO_SNES9X_VERSION))
+LIBRETRO_SNES9X_LICENSE="Non-commercial"
 
 define LIBRETRO_SNES9X_BUILD_CMDS
 	CFLAGS="$(TARGET_CFLAGS)" CXXFLAGS="$(TARGET_CXXFLAGS)" $(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" -C $(@D)/libretro -f Makefile platform="$(LIBRETRO_PLATFORM)"
