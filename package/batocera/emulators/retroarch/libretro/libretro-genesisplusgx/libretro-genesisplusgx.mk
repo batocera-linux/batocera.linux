78e6e3bff355534a6ce1e593fa972c93b0c946c8
Juliano Dorigão <jdorigao@gmail.com>
2019-02-04 14:33:05 -0300

    update: libretro-genesisplusgx

--- package/batocera/emulators/retroarch/libretro/libretro-genesisplusgx/libretro-genesisplusgx.mk
+++ package/batocera/emulators/retroarch/libretro/libretro-genesisplusgx/libretro-genesisplusgx.mk
@@ -3,9 +3,10 @@
 # GENESISPLUSGX
 #
 ################################################################################
-# Version.: Commits on Nov 7, 2018
-LIBRETRO_GENESISPLUSGX_VERSION = 5993fd8295bafe7b7b132375f6b9dcdda0129e54
+# Version.: Commits on Jan 26, 2019
+LIBRETRO_GENESISPLUSGX_VERSION = f5eed5133101999e1f6f04d2240ae77bb891adb9
 LIBRETRO_GENESISPLUSGX_SITE = $(call github,libretro,Genesis-Plus-GX,$(LIBRETRO_GENESISPLUSGX_VERSION))
+LIBRETRO_GENESISPLUSGX_LICENSE="Non-commercial"
 
 define LIBRETRO_GENESISPLUSGX_BUILD_CMDS
 	CFLAGS="$(TARGET_CFLAGS)" CXXFLAGS="$(TARGET_CXXFLAGS)" $(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" -C $(@D) -f Makefile.libretro platform="$(LIBRETRO_PLATFORM)"
