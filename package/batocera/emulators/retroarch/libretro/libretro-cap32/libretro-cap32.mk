a15339c45e13ce0de98e372fdec8d766f25ee5f9
Juliano Dorigão <jdorigao@gmail.com>
2019-02-04 12:10:38 -0300

    update: libretro-cap32

--- package/batocera/emulators/retroarch/libretro/libretro-cap32/libretro-cap32.mk
+++ package/batocera/emulators/retroarch/libretro/libretro-cap32/libretro-cap32.mk
@@ -3,9 +3,10 @@
 # CAP32
 #
 ################################################################################
-# Version.: Commits on Aug 27, 2018
-LIBRETRO_CAP32_VERSION = ac8608b149e96b5b48ad07909edfb423481f365a
+# Version.: Commits on Jan 28, 2019
+LIBRETRO_CAP32_VERSION = 1a8bcf4d68b633bb8e92e20da460ac7618e8b950
 LIBRETRO_CAP32_SITE = $(call github,libretro,libretro-cap32,$(LIBRETRO_CAP32_VERSION))
+LIBRETRO_CAP32_LICENSE="GPLv2"
 
 define LIBRETRO_CAP32_BUILD_CMDS
 	CFLAGS="$(TARGET_CFLAGS)" CXXFLAGS="$(TARGET_CXXFLAGS)" LDFLAGS="$(TARGET_LDFLAGS)" $(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" -C $(@D) -f Makefile
