b5dc498fc3426f1fb44171eb567c794342ca36ac
Juliano Dorigão <jdorigao@gmail.com>
2019-02-04 12:08:31 -0300

    update: libretro-bluemsx

--- package/batocera/emulators/retroarch/libretro/libretro-bluemsx/libretro-bluemsx.mk
+++ package/batocera/emulators/retroarch/libretro/libretro-bluemsx/libretro-bluemsx.mk
@@ -3,9 +3,10 @@
 # BLUEMSX
 #
 ################################################################################
-# Version.: Commits on Nov 11, 2018
-LIBRETRO_BLUEMSX_VERSION = b75da4e3394fd3c956a09958a97b67bc6db79f4d
+# Version.: Commits on Feb 2, 2019
+LIBRETRO_BLUEMSX_VERSION = d0fd4a7bf3f76329ab078e4e1e2db13ea20c106b
 LIBRETRO_BLUEMSX_SITE = $(call github,libretro,blueMSX-libretro,$(LIBRETRO_BLUEMSX_VERSION))
+LIBRETRO_BLUEMSX_LICENSE="GPLv2"
 
 define LIBRETRO_BLUEMSX_BUILD_CMDS
 	CFLAGS="$(TARGET_CFLAGS)" CXXFLAGS="$(TARGET_CXXFLAGS)" \
