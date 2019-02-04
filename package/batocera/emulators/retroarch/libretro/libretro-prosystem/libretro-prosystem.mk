a788ad07dec98efd2f75b5915167d775789e5327
Juliano Dorigão <jdorigao@gmail.com>
2019-02-04 15:39:50 -0300

    update: libretro-prosystem

--- package/batocera/emulators/retroarch/libretro/libretro-prosystem/libretro-prosystem.mk
+++ package/batocera/emulators/retroarch/libretro/libretro-prosystem/libretro-prosystem.mk
@@ -3,9 +3,10 @@
 # PROSYSTEM
 #
 ################################################################################
-# Version.: Commits on Sep 29, 2018
-LIBRETRO_PROSYSTEM_VERSION = 247d5f2dccd748495c5310081e6a9c4ff33a377f
+# Version.: Commits on Jan 4, 2019
+LIBRETRO_PROSYSTEM_VERSION = d3c4796983d0758c7bb19249b3088f1973332d36
 LIBRETRO_PROSYSTEM_SITE = $(call github,libretro,prosystem-libretro,$(LIBRETRO_PROSYSTEM_VERSION))
+LIBRETRO_PROSYSTEM_LICENSE="GPLv2"
 
 define LIBRETRO_PROSYSTEM_BUILD_CMDS
 	CFLAGS="$(TARGET_CFLAGS)" CXXFLAGS="$(TARGET_CXXFLAGS)" $(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" -C $(@D)/ -f Makefile platform="$(LIBRETRO_PLATFORM)"
