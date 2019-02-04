73deeb7f4661853393d87a887b4de95108f2bfdc
Juliano Dorigão <jdorigao@gmail.com>
2019-02-04 11:48:25 -0300

    update: libretro-beetle-ngp

--- package/batocera/emulators/retroarch/libretro/libretro-beetle-ngp/libretro-beetle-ngp.mk
+++ package/batocera/emulators/retroarch/libretro/libretro-beetle-ngp/libretro-beetle-ngp.mk
@@ -3,9 +3,10 @@
 # BEETLE_NGP
 #
 ################################################################################
-# Version.: Commits on Nov 11, 2018
-LIBRETRO_BEETLE_NGP_VERSION = b9fff92d68bed67e44a4d07fb8c43af5d61b0021
+# Version.: Commits on Feb 3, 2019
+LIBRETRO_BEETLE_NGP_VERSION = 78eb883d0e9efbb8a66be20f3938f1616453cea6
 LIBRETRO_BEETLE_NGP_SITE = $(call github,libretro,beetle-ngp-libretro,$(LIBRETRO_BEETLE_NGP_VERSION))
+LIBRETRO_BEETLE_NGP_LICENSE="GPLv2"
 
 define LIBRETRO_BEETLE_NGP_BUILD_CMDS
 	CFLAGS="$(TARGET_CFLAGS)" CXXFLAGS="$(TARGET_CXXFLAGS)" $(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" -C $(@D)/ -f Makefile platform="$(LIBRETRO_PLATFORM)"
