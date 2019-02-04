e9ccf24ee305e94bc8a320705b06350a8b2d499c
Juliano Dorigão <jdorigao@gmail.com>
2019-02-04 12:17:14 -0300

    update: libretro-cheats

--- package/batocera/emulators/retroarch/libretro/libretro-cheats/libretro-cheats.mk
+++ package/batocera/emulators/retroarch/libretro/libretro-cheats/libretro-cheats.mk
@@ -3,7 +3,8 @@
 # CHEATS
 #
 ################################################################################
-LIBRETRO_CHEATS_VERSION = master
+# Version.: Commits on Feb 4, 2019
+LIBRETRO_CHEATS_VERSION = b2853574592aa6fce1adaaf2bdc5f16eb7609ee9
 LIBRETRO_CHEATS_SITE = $(call github,libretro,libretro-database,$(LIBRETRO_CHEATS_VERSION))
 
 define LIBRETRO_CHEATS_INSTALL_TARGET_CMDS
