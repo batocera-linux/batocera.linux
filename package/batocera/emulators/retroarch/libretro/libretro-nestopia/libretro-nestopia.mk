778b1c77392781b9e5dcfad03dabd93b6864bd18
Juliano Dorigão <jdorigao@gmail.com>
2019-02-04 15:20:44 -0300

    update: libretro-nestopia

--- package/batocera/emulators/retroarch/libretro/libretro-nestopia/libretro-nestopia.mk
+++ package/batocera/emulators/retroarch/libretro/libretro-nestopia/libretro-nestopia.mk
@@ -3,9 +3,10 @@
 # NESTOPIA
 #
 ################################################################################
-# Version.: Commits on Oct 22, 2018
-LIBRETRO_NESTOPIA_VERSION = 28aa27ccd6a44cf8e1e406b58356a8d0bb646834
+# Version.: Commits on Jan 3, 2019
+LIBRETRO_NESTOPIA_VERSION = 75b2d1b4b08282e578eb44461c6bc007d6f8c486
 LIBRETRO_NESTOPIA_SITE = $(call github,libretro,nestopia,$(LIBRETRO_NESTOPIA_VERSION))
+LIBRETRO_NESTOPIA_LICENSE="GPLv2"
 
 define LIBRETRO_NESTOPIA_BUILD_CMDS
 	CFLAGS="$(TARGET_CFLAGS)" CXXFLAGS="$(TARGET_CXXFLAGS)" $(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" -C $(@D)/libretro/ platform="$(LIBRETRO_PLATFORM)"
