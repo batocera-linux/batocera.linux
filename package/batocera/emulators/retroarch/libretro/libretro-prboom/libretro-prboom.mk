1083d03e95494539e45acc5fee9ab584d95d082b
Juliano Dorigão <jdorigao@gmail.com>
2019-02-04 15:37:53 -0300

    update: libretro-prboom

--- package/batocera/emulators/retroarch/libretro/libretro-prboom/libretro-prboom.mk
+++ package/batocera/emulators/retroarch/libretro/libretro-prboom/libretro-prboom.mk
@@ -3,9 +3,10 @@
 # PRBOOM
 #
 ################################################################################
-# Version.: Commits on Sep 29, 2018
-LIBRETRO_PRBOOM_VERSION = b516a666f7a63f421a89de1a819aa738697574d8
+# Version.: Commits on Feb 3, 2019
+LIBRETRO_PRBOOM_VERSION = 25d5cb47c3fff64e3ee9ea8c0f9571c20bd31577
 LIBRETRO_PRBOOM_SITE = $(call github,libretro,libretro-prboom,$(LIBRETRO_PRBOOM_VERSION))
+LIBRETRO_PRBOOM_LICENSE="GPLv2"
 
 define LIBRETRO_PRBOOM_BUILD_CMDS
 	CFLAGS="$(TARGET_CFLAGS)" CXXFLAGS="$(TARGET_CXXFLAGS)" $(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" -C $(@D)/ -f Makefile platform="$(LIBRETRO_PLATFORM)"
