f629d38a25a634b4b4b313cd9d7dfbcb8f972dff
Juliano Dorigão <jdorigao@gmail.com>
2019-02-04 15:25:13 -0300

    update: libretro-o2em

--- package/batocera/emulators/retroarch/libretro/libretro-o2em/libretro-o2em.mk
+++ package/batocera/emulators/retroarch/libretro/libretro-o2em/libretro-o2em.mk
@@ -3,9 +3,10 @@
 # O2EM
 #
 ################################################################################
-# Version.: Commits on Sep 29, 2018
-LIBRETRO_O2EM_VERSION = 8df6bdddbfc551a47b3f0fed7bb5dfd50d2ce55d
+# Version.: Commits on Jan 4, 2019
+LIBRETRO_O2EM_VERSION = 32182d2b2d2a8c2455db3b06b65aa7e01505c020
 LIBRETRO_O2EM_SITE = $(call github,libretro,libretro-o2em,$(LIBRETRO_O2EM_VERSION))
+LIBRETRO_O2EM_LICENSE="Artistic License"
 
 define LIBRETRO_O2EM_BUILD_CMDS
 	CFLAGS="$(TARGET_CFLAGS)" CXXFLAGS="$(TARGET_CXXFLAGS)" $(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" -C $(@D)/ -f Makefile platform="$(LIBRETRO_PLATFORM)"
