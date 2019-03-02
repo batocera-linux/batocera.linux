77de6a6300df6719f3dee181de46de6d9065da60
Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>
2019-03-02 22:01:36 +0100

    ppsspp: fix when the .ini is invalid
    Signed-off-by: Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>

--- package/batocera/core/batocera-configgen/configgen/configgen/generators/ppsspp/ppssppConfig.py
+++ package/batocera/core/batocera-configgen/configgen/configgen/generators/ppsspp/ppssppConfig.py
@@ -13,8 +13,11 @@ def writePPSSPPConfig(system):
     # To prevent ConfigParser from converting to lower case
     iniConfig.optionxform = str
     if os.path.exists(recalboxFiles.ppssppConfig):
-        with io.open(recalboxFiles.ppssppConfig, 'r', encoding='utf_8_sig') as fp:
-            iniConfig.readfp(fp)
+        try:
+            with io.open(recalboxFiles.ppssppConfig, 'r', encoding='utf_8_sig') as fp:
+                iniConfig.readfp(fp)
+        except:
+            pass
 
     createPPSSPPConfig(iniConfig, system)
     # save the ini file
