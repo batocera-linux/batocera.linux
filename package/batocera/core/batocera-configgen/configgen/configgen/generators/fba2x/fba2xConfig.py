d924f625f0c66eac4ddde0abd928638965bc0369
Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>
2019-03-03 21:36:15 +0100

    fba2x: fix configgen
    Signed-off-by: Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>

--- package/batocera/core/batocera-configgen/configgen/configgen/generators/fba2x/fba2xConfig.py
+++ package/batocera/core/batocera-configgen/configgen/configgen/generators/fba2x/fba2xConfig.py
@@ -1,47 +1,29 @@
 #!/usr/bin/env python
-import sys
-import os
-import recalboxFiles
-from Emulator import Emulator
-from settings.unixSettings import UnixSettings
-
-sys.path.append(
-    os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
-
-fbaSettings = UnixSettings(recalboxFiles.fbaCustom)
 
 # return true if the option is considered defined
 def defined(key, dict):
     return key in dict and isinstance(dict[key], str) and len(dict[key]) > 0
 
-ratioIndexes = {'16/9': '0', '4/3': '1'}
+def updateFBAConfig(iniConfig, system):
+    ratioIndexes = {'16/9': '0', '4/3': '1'}
 
-def writeFBAConfig(system):
-    writeFBAConfigToFile(createFBAConfig(system))
+    if not iniConfig.has_section("Graphics"):
+        iniConfig.add_section("Graphics")
 
-
-# take a system, and returns a dict of retroarch.cfg compatible parameters
-def createFBAConfig(system):
-    fbaConfig = dict()
-    recalboxConfig = system.config
-    if system.isOptSet('smooth') and system.getOptBoolean('smooth') == True:
-        fbaConfig['DisplaySmoothStretch'] = '1'
+    if system.isOptSet("smooth") and system.getOptBoolean("smooth") == True:
+        iniConfig.set("Graphics", "DisplaySmoothStretch", "1")
     else:
-        fbaConfig['DisplaySmoothStretch'] = '0'
+        iniConfig.set("Graphics", "DisplaySmoothStretch", "0")
 
-    if defined('ratio', recalboxConfig) and recalboxConfig['ratio'] in ratioIndexes:
-        fbaConfig['MaintainAspectRatio'] = ratioIndexes[recalboxConfig['ratio']]
+    if defined("ratio", system.config) and system.config["ratio"] in ratioIndexes:
+        iniConfig.set("Graphics", "MaintainAspectRatio", ratioIndexes[system.config["ratio"]])
     else:
-        fbaConfig['MaintainAspectRatio'] = '1'
-
-    if defined('shaders', recalboxConfig) and recalboxConfig['shaders'] == 'scanlines':
-        fbaConfig['DisplayEffect'] = '1'
+        iniConfig.set("Graphics", "MaintainAspectRatio", "1")
+    
+    if defined("shaders", system.config) and system.config["shaders"] == 'scanlines':
+        iniConfig.set("Graphics", "DisplayEffect", "1")
     else :
-        fbaConfig['DisplayEffect'] = '0'
+        iniConfig.set("Graphics", "DisplayEffect", "0")
 
-    return fbaConfig
-
-
-def writeFBAConfigToFile(config):
-    for setting in config:
-        fbaSettings.save(setting, config[setting])
+    iniConfig.set("Graphics", "RotateScreen",  "0")
+    iniConfig.set("Graphics", "DisplayBorder", "0")
--- package/batocera/core/batocera-configgen/configgen/configgen/generators/fba2x/fba2xGenerator.py
+++ package/batocera/core/batocera-configgen/configgen/configgen/generators/fba2x/fba2xGenerator.py
@@ -1,29 +1,32 @@
 #!/usr/bin/env python
+
 import Command
 import fba2xControllers
 import recalboxFiles
 import fba2xConfig
-import shutil
 from generators.Generator import Generator
 import os.path
-
+import ConfigParser
+import io
 
 class Fba2xGenerator(Generator):
-    
-    # Main entry of the module
-    # Configure fba and return a command
     def generate(self, system, rom, playersControllers, gameResolution):
-        # Settings recalbox default config file if no user defined one
-        if not 'configfile' in system.config:
-            # Using recalbox config file
-            system.config['configfile'] = recalboxFiles.fbaCustom
-            # Copy original fba2x.cfg
-            shutil.copyfile(recalboxFiles.fbaCustomOrigin, recalboxFiles.fbaCustom)
-            #  Write controllers configuration files
-            fba2xControllers.writeControllersConfig(system, rom, playersControllers)
-            # Write configuration to retroarchcustom.cfg
-            fba2xConfig.writeFBAConfig(system)
+        iniConfig = ConfigParser.ConfigParser()
+        # To prevent ConfigParser from converting to lower case
+        iniConfig.optionxform = str
+        if os.path.exists(recalboxFiles.fbaCustom):
+            with io.open(recalboxFiles.fbaCustom, 'r', encoding='utf_8_sig') as fp:
+                iniConfig.readfp(fp)
 
-        commandArray = [recalboxFiles.recalboxBins[system.config['emulator']], "--configfile", system.config['configfile'], '--logfile', recalboxFiles.logdir+"/fba2x.log"]
+        fba2xConfig.updateFBAConfig(iniConfig, system)
+        fba2xControllers.updateControllersConfig(iniConfig, system, rom, playersControllers)
+
+        # save the ini file
+        if not os.path.exists(os.path.dirname(recalboxFiles.fbaCustom)):
+            os.makedirs(os.path.dirname(recalboxFiles.fbaCustom))
+        with open(recalboxFiles.fbaCustom, 'w') as configfile:
+            iniConfig.write(configfile)
+
+        commandArray = [recalboxFiles.recalboxBins[system.config['emulator']], "--configfile", recalboxFiles.fbaCustom, '--logfile', recalboxFiles.logdir+"/fba2x.log"]
         commandArray.append(rom)
         return Command.Command(array=commandArray)
