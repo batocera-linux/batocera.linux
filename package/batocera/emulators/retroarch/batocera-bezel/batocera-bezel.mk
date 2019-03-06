be23e0b7bfc0eab1d0146b9a27951e56369554c5
Juliano Dorigão <jdorigao@gmail.com>
2019-03-06 17:45:12 -0400

    bezel bump

--- package/batocera/emulators/retroarch/batocera-bezel/batocera-bezel.mk
+++ package/batocera/emulators/retroarch/batocera-bezel/batocera-bezel.mk
@@ -3,8 +3,8 @@
 # batocera bezel
 #
 ################################################################################
-
-BATOCERA_BEZEL_VERSION = master
+# Version.: Commits on Mar 6, 2019
+BATOCERA_BEZEL_VERSION = e3ecfc4c0f3310dd63fbb4c49da1c95ae4c1b2f0
 BATOCERA_BEZEL_SITE = $(call github,batocera-linux,batocera-bezel,$(BATOCERA_BEZEL_VERSION))
 
 define BATOCERA_BEZEL_INSTALL_TARGET_CMDS
