a72f8cefc2c7201ec76acc1f5cf766b74a5de1cd
Juliano Dorigão <jdorigao@gmail.com>
2019-03-13 17:54:09 -0400

    desktopapps: renamed and added icons

--- package/batocera/core/batocera-desktopapps/batocera-desktopapps.mk
+++ package/batocera/core/batocera-desktopapps/batocera-desktopapps.mk
@@ -7,24 +7,28 @@ BATOCERA_DESKTOPAPPS_VERSION = 1.0
 BATOCERA_DESKTOPAPPS_SOURCE=  
 
 BATOCERA_DESKTOPAPPS_SCRIPTS = filemanagerlauncher.sh
-BATOCERA_DESKTOPAPPS_APPS = 
+BATOCERA_DESKTOPAPPS_APPS  = 
+BATOCERA_DESKTOPAPPS_ICONS =
 
 # pcsx2
 ifneq ($(BR2_PACKAGE_PCSX2_X86)$(BR2_PACKAGE_PCSX2)$(BR2_PACKAGE_PCSX2_AVX2),)
-  BATOCERA_DESKTOPAPPS_SCRIPTS += recalbox-config-pcsx2.sh
+  BATOCERA_DESKTOPAPPS_SCRIPTS += batocera-config-pcsx2.sh
   BATOCERA_DESKTOPAPPS_APPS    += pcsx2-config.desktop
+	BATOCERA_DESKTOPAPPS_ICONS   += pcsx2.png
 endif
 
 # dolphin
 ifeq ($(BR2_PACKAGE_DOLPHIN_EMU),y)
-  BATOCERA_DESKTOPAPPS_SCRIPTS += recalbox-config-dolphin.sh
+  BATOCERA_DESKTOPAPPS_SCRIPTS += batocera-config-dolphin.sh
   BATOCERA_DESKTOPAPPS_APPS    += dolphin-config.desktop
+	BATOCERA_DESKTOPAPPS_ICONS   += dolphin.png
 endif
 
 # retroarch
 ifeq ($(BR2_PACKAGE_RETROARCH),y)
-  BATOCERA_DESKTOPAPPS_SCRIPTS += recalbox-config-retroarch.sh
+  BATOCERA_DESKTOPAPPS_SCRIPTS += batocera-config-retroarch.sh
   BATOCERA_DESKTOPAPPS_APPS    += retroarch-config.desktop
+	BATOCERA_DESKTOPAPPS_ICONS   += retroarch.png
 endif
 
 define BATOCERA_DESKTOPAPPS_INSTALL_TARGET_CMDS
@@ -35,6 +39,10 @@ define BATOCERA_DESKTOPAPPS_INSTALL_TARGET_CMDS
 	# apps
 	mkdir -p $(TARGET_DIR)/usr/share/applications
 	$(foreach f,$(BATOCERA_DESKTOPAPPS_APPS), cp package/batocera/core/batocera-desktopapps/apps/$(f) $(TARGET_DIR)/usr/share/applications/$(f)$(sep))
+
+	# icons
+	mkdir -p $(TARGET_DIR)/usr/share/icons/batocera
+	$(foreach f,$(BATOCERA_DESKTOPAPPS_ICONS), cp package/batocera/core/batocera-desktopapps/icons/$(f) $(TARGET_DIR)/usr/share/icons/batocera/$(f)$(sep))
 
 	# menu
 	mkdir -p $(TARGET_DIR)/etc/xdg/menus
