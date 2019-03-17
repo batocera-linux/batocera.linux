478519a655a373132557467d3ceaed1d4d24937f
Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>
2019-03-17 18:52:15 +0100

    bezel: fix gamegear
    Signed-off-by: Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>

--- package/batocera/emulators/retroarch/batocera-bezel/batocera-bezel.mk
+++ package/batocera/emulators/retroarch/batocera-bezel/batocera-bezel.mk
@@ -3,8 +3,8 @@
 # batocera bezel
 #
 ################################################################################
-# Version.: Commits on Mar 15, 2019
-BATOCERA_BEZEL_VERSION = 1e0699e0fb2e143087cfdf60c57940e20f36ee8c
+# Version.: Commits on Mar 17, 2019
+BATOCERA_BEZEL_VERSION = cef4616c8c0f0ca891f4880f94b18f98c59a515a
 BATOCERA_BEZEL_SITE = $(call github,batocera-linux,batocera-bezel,$(BATOCERA_BEZEL_VERSION))
 
 define BATOCERA_BEZEL_INSTALL_TARGET_CMDS
