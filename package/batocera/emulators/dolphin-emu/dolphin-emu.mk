89aecc31cdb69de6abc7a50fdb7a1f1c206d69c8
Tiago Pierezan Camargo <camargo@discworld.com.br>
2019-04-04 20:09:24 -0300

    dolphin: version bump (5.0-9896)

--- package/batocera/emulators/dolphin-emu/dolphin-emu.mk
+++ package/batocera/emulators/dolphin-emu/dolphin-emu.mk
@@ -3,8 +3,8 @@
 # DOLPHIN EMU
 #
 ################################################################################
-# Version.: 5.0-9208
-DOLPHIN_EMU_VERSION = f510f6ef0d72764cae23a1c662ffdc92c45acd9e
+# Version.: 5.0-9896
+DOLPHIN_EMU_VERSION = bfde5b931e542ec2b8a5eee72d4794515955c7e8
 DOLPHIN_EMU_SITE = $(call github,dolphin-emu,dolphin,$(DOLPHIN_EMU_VERSION))
 DOLPHIN_EMU_DEPENDENCIES = xserver_xorg-server libevdev ffmpeg zlib libpng lzo libusb libcurl sfml bluez5_utils qt5base hidapi
 DOLPHIN_EMU_SUPPORTS_IN_SOURCE_BUILD = NO
