3cc116fb07f3ff03bcc7b43b779c4a46a26a9115
Juliano Dorigão <jdorigao@gmail.com>
2019-04-26 23:18:17 -0400

    fix: name of the game without line break.
    Signed-off-by: Juliano Dorigão <jdorigao@gmail.com>

--- board/batocera/fsoverlay/recalbox/scripts/moonlight/Moonlight.sh
+++ board/batocera/fsoverlay/recalbox/scripts/moonlight/Moonlight.sh
@@ -24,7 +24,7 @@ createRomLinks () {
   listGames | while read line
   do
     filename=$(echo $line | sed 's/[^ A-Za-z0-9._-]/-/g')
-    echo -e " $filename$SEPERATOR$line" >> $moonlight_gamesnames
+    echo "$filename$SEPERATOR$line" >> $moonlight_gamesnames
     touch "$moonlight_romsdir/${filename}.moonlight"
   done
 }
