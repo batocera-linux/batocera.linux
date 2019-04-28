bd27de53c52258772c19c9b4c56cd47d67d49221
Juliano Dorigão <jdorigao@gmail.com>
2019-04-27 21:06:38 -0400

    bump: moonlight-embedded
    Signed-off-by: Juliano Dorigão <jdorigao@gmail.com>

--- package/batocera/emulators/moonlight-embedded/moonlight-embedded.mk
+++ package/batocera/emulators/moonlight-embedded/moonlight-embedded.mk
@@ -3,10 +3,10 @@
 # moonlight-embedded
 #
 ################################################################################
-# Version.: Commits on Aug 11, 2018
-MOONLIGHT_EMBEDDED_VERSION = 2.4.7
-MOONLIGHT_EMBEDDED_SOURCE = moonlight-embedded-$(MOONLIGHT_EMBEDDED_VERSION).tar.xz
-MOONLIGHT_EMBEDDED_SITE = https://github.com/irtimmer/moonlight-embedded/releases/download/v$(MOONLIGHT_EMBEDDED_VERSION)
+# Version.: Commits on Apr 18, 2019 (v2.4.7)
+MOONLIGHT_EMBEDDED_VERSION = 7b46b4b2ea4e51bc758a02d18cf80313ba442161
+MOONLIGHT_EMBEDDED_SITE = git://github.com/irtimmer/moonlight-embedded.git
+MOONLIGHT_EMBEDDED_GIT_SUBMODULES=y
 MOONLIGHT_EMBEDDED_DEPENDENCIES = opus expat libevdev avahi alsa-lib udev libcurl libcec ffmpeg sdl2 libenet
 
 MOONLIGHT_EMBEDDED_CONF_OPTS = "-DCMAKE_INSTALL_SYSCONFDIR=/etc"
