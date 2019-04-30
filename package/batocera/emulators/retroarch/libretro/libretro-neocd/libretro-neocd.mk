a214c36b3b2c634f27a596c7b8a533bb5ad0f33e
Juliano Dorigão <jdorigao@gmail.com>
2019-04-29 23:52:00 -0400

    fix: license
    Signed-off-by: Juliano Dorigão <jdorigao@gmail.com>

--- package/batocera/emulators/retroarch/libretro/libretro-neocd/libretro-neocd.mk
+++ package/batocera/emulators/retroarch/libretro/libretro-neocd/libretro-neocd.mk
@@ -6,7 +6,7 @@
 # Version.: Commits on Apr 5, 2019
 LIBRETRO_NEOCD_VERSION = fd77de0d3f557f9d92a78c7bdda44c207f992ab5
 LIBRETRO_NEOCD_SITE = $(call github,libretro,neocd_libretro,$(LIBRETRO_NEOCD_VERSION))
-LIBRETRO_NEOCD_LICENSE="GPLv3"
+LIBRETRO_NEOCD_LICENSE = GPLv3
 
 define LIBRETRO_NEOCD_INSTALL_TARGET_CMDS
 	$(INSTALL) -D $(@D)/cmake-build-Release/output/libneocd_libretro.so \
