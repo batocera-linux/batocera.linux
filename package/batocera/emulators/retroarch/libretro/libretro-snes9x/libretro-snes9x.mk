c8a9ea39849b751c1eae18583cad4a26c6cf915c
Juliano Dorigão <jdorigao@gmail.com>
2019-04-29 23:52:00 -0400

    fix: license
    Signed-off-by: Juliano Dorigão <jdorigao@gmail.com>

--- package/batocera/emulators/retroarch/libretro/libretro-snes9x/libretro-snes9x.mk
+++ package/batocera/emulators/retroarch/libretro/libretro-snes9x/libretro-snes9x.mk
@@ -6,7 +6,7 @@
 # Version.: Commits on Mar 20, 2019
 LIBRETRO_SNES9X_VERSION = 5dc1bde625efba1e92e9a84ad7c4da102cfeb2c7
 LIBRETRO_SNES9X_SITE = $(call github,libretro,snes9x,$(LIBRETRO_SNES9X_VERSION))
-LIBRETRO_SNES9X_LICENSE="Non-commercial"
+LIBRETRO_SNES9X_LICENSE = Non-commercial
 
 define LIBRETRO_SNES9X_BUILD_CMDS
 	CFLAGS="$(TARGET_CFLAGS)" CXXFLAGS="$(TARGET_CXXFLAGS)" $(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" -C $(@D)/libretro -f Makefile platform="$(LIBRETRO_PLATFORM)"
