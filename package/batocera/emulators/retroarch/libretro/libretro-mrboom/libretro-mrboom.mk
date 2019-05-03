9b89d3b91200c14b694a9d5d96877ff0bb5a9825
CJ <kryptalivian@gmail.com>
2019-05-03 09:36:09 -0500

    libretro-mrboom
    new mr boom core

---
+++ package/batocera/emulators/retroarch/libretro/libretro-mrboom/libretro-mrboom.mk
@@ -0,0 +1,17 @@
+################################################################################
+#
+# MRBOOM
+#
+################################################################################
+# Version.: Commits on Feb 28, 2019
+LIBRETRO_MRBOOM_VERSION = 67b1b65633f67e79e0ff552f4571cbcf0969a535
+LIBRETRO_MRBOOM_SITE = $(call github,libretro,mrboom-libretro,$(LIBRETRO_MRBOOM$
+LIBRETRO_MRBOOM_LICENSE="GPLv2"
+define LIBRETRO_MRBOOM_BUILD_CMDS
+    CFLAGS="$(TARGET_CFLAGS)" CXXFLAGS="$(TARGET_CXXFLAGS)" $(MAKE) CXX="$(TARG$
+endef
+define LIBRETRO_MRBOOM_INSTALL_TARGET_CMDS
+    $(INSTALL) -D $(@D)/mrboom_libretro.so \
+        $(TARGET_DIR)/usr/lib/libretro/mrboom_libretro.so
+endef
+$(eval $(generic-package))
