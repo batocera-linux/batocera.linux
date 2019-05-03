a5995abf9f33acad92b473ed2cb8d69769e03594
kryptalivian <kryptalivian@gmail.com>
2019-05-03 10:53:59 -0500

    Update libretro-mrboom.mk

--- package/batocera/emulators/retroarch/libretro/libretro-mrboom/libretro-mrboom.mk
+++ package/batocera/emulators/retroarch/libretro/libretro-mrboom/libretro-mrboom.mk
@@ -5,13 +5,16 @@
 ################################################################################
 # Version.: Commits on Feb 28, 2019
 LIBRETRO_MRBOOM_VERSION = 67b1b65633f67e79e0ff552f4571cbcf0969a535
-LIBRETRO_MRBOOM_SITE = $(call github,libretro,mrboom-libretro,$(LIBRETRO_MRBOOM$
+LIBRETRO_MRBOOM_SITE = $(call github,libretro,libretro-mrboom,$(LIBRETRO_MRBOOM_VERSION))
 LIBRETRO_MRBOOM_LICENSE="GPLv2"
+
 define LIBRETRO_MRBOOM_BUILD_CMDS
-    CFLAGS="$(TARGET_CFLAGS)" CXXFLAGS="$(TARGET_CXXFLAGS)" $(MAKE) CXX="$(TARG$
+	CFLAGS="$(TARGET_CFLAGS)" CXXFLAGS="$(TARGET_CXXFLAGS)" $(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" -C $(@D)/ -f Makefile platform="$(LIBRETRO_PLATFORM)"
 endef
+
 define LIBRETRO_MRBOOM_INSTALL_TARGET_CMDS
-    $(INSTALL) -D $(@D)/mrboom_libretro.so \
-        $(TARGET_DIR)/usr/lib/libretro/mrboom_libretro.so
+	$(INSTALL) -D $(@D)/mrboom_libretro.so \
+		$(TARGET_DIR)/usr/lib/libretro/mrboom_libretro.so
 endef
+
 $(eval $(generic-package))
