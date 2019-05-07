231ca14af41083d011bedfd2cbf3dea7f785ece8
Juliano Dorigão <jdorigao@gmail.com>
2019-05-07 18:31:15 -0400

    carbon: emulators missing
    Signed-off-by: Juliano Dorigão <jdorigao@gmail.com>

--- package/batocera/emulationstation/es-theme-carbon/es-theme-carbon.mk
+++ package/batocera/emulationstation/es-theme-carbon/es-theme-carbon.mk
@@ -10,6 +10,9 @@ ES_THEME_CARBON_SITE = $(call github,RetroPie,es-theme-carbon,$(ES_THEME_CARBON_
 define ES_THEME_CARBON_INSTALL_TARGET_CMDS
     mkdir -p $(TARGET_DIR)/usr/share/emulationstation/themes/es-theme-carbon
     cp -r $(@D)/* $(TARGET_DIR)/usr/share/emulationstation/themes/es-theme-carbon
+    
+    cp -r package/batocera/emulationstation/es-theme-carbon/theme/* \
+        $(TARGET_DIR)/usr/share/emulationstation/themes/es-theme-carbon
 endef
 
 $(eval $(generic-package))
