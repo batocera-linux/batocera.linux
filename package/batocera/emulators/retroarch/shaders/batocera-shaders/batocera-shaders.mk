0046fa6dfea31ee226fa813a8a0dff2db55aa05a
Juliano Dorigão <jdorigao@gmail.com>
2019-05-14 09:00:06 -0400

    batocera-shaders: removed shaders folder
    Signed-off-by: Juliano Dorigão <jdorigao@gmail.com>

--- package/batocera/emulators/retroarch/shaders/batocera-shaders/batocera-shaders.mk
+++ package/batocera/emulators/retroarch/shaders/batocera-shaders/batocera-shaders.mk
@@ -6,10 +6,10 @@
 
 BATOCERA_SHADERS_VERSION = 1.0
 BATOCERA_SHADERS_SOURCE=
+BATOCERA_SHADERS_DEPENDENCIES= glsl-shaders slang-shaders
 
 define BATOCERA_SHADERS_INSTALL_TARGET_CMDS
         mkdir -p $(TARGET_DIR)/usr/share/batocera/shaders
-	cp -r package/batocera/emulators/retroarch/batocera-shaders/shaders/* $(TARGET_DIR)/usr/share/batocera/shaders
 	cp -r package/batocera/emulators/retroarch/batocera-shaders/configs $(TARGET_DIR)/usr/share/batocera/shaders
 endef
 
