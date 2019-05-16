e133143b6d5a013f617ca8c807d84db1eead3051
Juliano Dorigão <jdorigao@gmail.com>
2019-05-16 16:53:46 -0400

    bump: libretro-snes9x
    Signed-off-by: Juliano Dorigão <jdorigao@gmail.com>

--- package/batocera/emulators/retroarch/libretro/libretro-snes9x/libretro-snes9x.mk
+++ package/batocera/emulators/retroarch/libretro/libretro-snes9x/libretro-snes9x.mk
@@ -3,8 +3,8 @@
 # SNES9X
 #
 ################################################################################
-# Version.: Commits on Mar 20, 2019
-LIBRETRO_SNES9X_VERSION = 5dc1bde625efba1e92e9a84ad7c4da102cfeb2c7
+# Version.: Commits on May 11, 2019
+LIBRETRO_SNES9X_VERSION = 8b20cb5f544626924521f338f28c135df8b9c692
 LIBRETRO_SNES9X_SITE = $(call github,libretro,snes9x,$(LIBRETRO_SNES9X_VERSION))
 LIBRETRO_SNES9X_LICENSE = Non-commercial
 
