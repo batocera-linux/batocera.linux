22669a38fe606cb29a5b207709c150482a3bba24
Juliano Dorigão <jdorigao@gmail.com>
2019-05-16 16:45:18 -0400

    bump: libretro-beetle-ngp
    Signed-off-by: Juliano Dorigão <jdorigao@gmail.com>

--- package/batocera/emulators/retroarch/libretro/libretro-beetle-ngp/libretro-beetle-ngp.mk
+++ package/batocera/emulators/retroarch/libretro/libretro-beetle-ngp/libretro-beetle-ngp.mk
@@ -3,8 +3,8 @@
 # BEETLE_NGP
 #
 ################################################################################
-# Version.: Commits on Feb 3, 2019
-LIBRETRO_BEETLE_NGP_VERSION = 78eb883d0e9efbb8a66be20f3938f1616453cea6
+# Version.: Commits on May 9, 2019
+LIBRETRO_BEETLE_NGP_VERSION = 8088946b1958b565d5d2177a53f75c8243a8e172
 LIBRETRO_BEETLE_NGP_SITE = $(call github,libretro,beetle-ngp-libretro,$(LIBRETRO_BEETLE_NGP_VERSION))
 LIBRETRO_BEETLE_NGP_LICENSE = GPLv2
 
