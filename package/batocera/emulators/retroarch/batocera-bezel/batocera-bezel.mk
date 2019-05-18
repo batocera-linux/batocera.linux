0cb5e7afb5b45aefc6b0132b0eb6d0c6a0662a0b
Jérémy <xgenetik57x@gmail.com>
2019-05-18 20:01:31 +0200

    Bump Bezel

--- package/batocera/emulators/retroarch/batocera-bezel/batocera-bezel.mk
+++ package/batocera/emulators/retroarch/batocera-bezel/batocera-bezel.mk
@@ -3,8 +3,8 @@
 # batocera bezel
 #
 ################################################################################
-# Version.: Commits on May 16, 2019
-BATOCERA_BEZEL_VERSION = afb4e9af1789373c876fb3c2dce67beb10096c2c
+# Version.: Commits on May 18, 2019
+BATOCERA_BEZEL_VERSION = e0ae21fd4be804c081efa381fb5c0f811d52547e
 BATOCERA_BEZEL_SITE = $(call github,batocera-linux,batocera-bezel,$(BATOCERA_BEZEL_VERSION))
 
 define BATOCERA_BEZEL_INSTALL_TARGET_CMDS
