64846da0fd86758e2a8b6cea28bf873122f39acd
Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>
2019-05-27 18:35:52 +0200

    update bezel
    Signed-off-by: Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>

--- package/batocera/emulators/retroarch/batocera-bezel/batocera-bezel.mk
+++ package/batocera/emulators/retroarch/batocera-bezel/batocera-bezel.mk
@@ -3,8 +3,8 @@
 # batocera bezel
 #
 ################################################################################
-# Version.: Commits on May 24, 2019
-BATOCERA_BEZEL_VERSION = a5bbbe2ff5c13c9949a71f4db24c0252c95ed56c
+# Version.: Commits on May 27, 2019
+BATOCERA_BEZEL_VERSION = f6836aed583b7ac1ba3438f32ce5a0b276cc197d
 BATOCERA_BEZEL_SITE = $(call github,batocera-linux,batocera-bezel,$(BATOCERA_BEZEL_VERSION))
 
 define BATOCERA_BEZEL_INSTALL_TARGET_CMDS
