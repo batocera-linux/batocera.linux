af10bc521f34fa9cc97218ae8eac1a762e924ea3
Juliano Dorigão <jdorigao@gmail.com>
2019-07-09 13:54:57 -0400

    bump: capsimg
    Signed-off-by: Juliano Dorigão <jdorigao@gmail.com>

--- package/batocera/libraries/capsimg/capsimg.mk
+++ package/batocera/libraries/capsimg/capsimg.mk
@@ -3,16 +3,25 @@
 # CAPSIMG
 #
 ################################################################################
-# Version.: Commits on Dec 8, 2016
-CAPSIMG_VERSION = 067db4cc6bfdcd1ab684b812da6fedbb8f96e04a
+# Version.: Commits on May 3, 2019
+CAPSIMG_VERSION = 5d306bb19bc4382367a1e489fb36768fd224b5e6
 CAPSIMG_SITE = $(call github,FrodeSolheim,capsimg,$(CAPSIMG_VERSION))
 CAPSIMG_LICENSE = Non-commercial
 
 define CAPSIMG_HOOK_BOOTSTRAP
-  cd $(@D) && ./bootstrap
+	cd $(@D) && ./bootstrap.fs
 endef
 
 CAPSIMG_PRE_CONFIGURE_HOOKS += CAPSIMG_HOOK_BOOTSTRAP
+
+define CAPSIMG_CONFIGURE_CMDS
+	cd $(@D) && ./configure.fs
+endef
+
+define CAPSIMG_BUILD_CMDS
+	CFLAGS="$(TARGET_CFLAGS)" CXXFLAGS="$(TARGET_CXXFLAGS)" \
+		$(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" -C $(@D)/ -f Makefile.fs
+endef
 
 define CAPSIMG_INSTALL_TARGET_CMDS
 	$(INSTALL) -D $(@D)/capsimg.so \
