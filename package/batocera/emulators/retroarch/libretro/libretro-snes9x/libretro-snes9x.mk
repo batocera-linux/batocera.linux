a4542005be1eea788dbeaf35dad35dbe4d8f2194
Juliano Dorigão <jdorigao@gmail.com>
2019-07-31 11:53:54 -0400

    snes9x: replacing repository
    Signed-off-by: Juliano Dorigão <jdorigao@gmail.com>

--- package/batocera/emulators/retroarch/libretro/libretro-snes9x/libretro-snes9x.mk
+++ package/batocera/emulators/retroarch/libretro/libretro-snes9x/libretro-snes9x.mk
@@ -3,13 +3,14 @@
 # SNES9X
 #
 ################################################################################
-# Version.: Commits on May 11, 2019
-LIBRETRO_SNES9X_VERSION = 8b20cb5f544626924521f338f28c135df8b9c692
-LIBRETRO_SNES9X_SITE = $(call github,libretro,snes9x,$(LIBRETRO_SNES9X_VERSION))
+# Version.: Commits on Jul 17, 2019
+LIBRETRO_SNES9X_VERSION = 27314dc6bcd79637d66f654343ec7926818eed54
+LIBRETRO_SNES9X_SITE = $(call github,snes9xgit,snes9x,$(LIBRETRO_SNES9X_VERSION))
 LIBRETRO_SNES9X_LICENSE = Non-commercial
 
 define LIBRETRO_SNES9X_BUILD_CMDS
-	CFLAGS="$(TARGET_CFLAGS)" CXXFLAGS="$(TARGET_CXXFLAGS)" $(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" -C $(@D)/libretro -f Makefile platform="$(LIBRETRO_PLATFORM)"
+	CFLAGS="$(TARGET_CFLAGS)" CXXFLAGS="$(TARGET_CXXFLAGS)" $(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" \
+		-C $(@D)/libretro -f Makefile platform="$(LIBRETRO_PLATFORM)"
 endef
 
 define LIBRETRO_SNES9X_INSTALL_TARGET_CMDS
