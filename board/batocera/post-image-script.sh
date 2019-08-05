277284f1b637dbf25827361a51aa9cf6a2703f5e
Ntemis <ierokipides.dem@gmail.com>
2019-08-05 03:33:25 +0300

    ROCK960: Batocera Support https://www.96boards.org/product/rock960/
    Signed-off-by: Ntemis <ierokipides.dem@gmail.com>

--- board/batocera/post-image-script.sh
+++ board/batocera/post-image-script.sh
@@ -394,6 +394,40 @@ case "${BATOCERA_TARGET}" in
 	sync || exit 1
 	;;
 
+    ROCK960)
+    # /boot
+    rm -rf "${BINARIES_DIR}/boot"            || exit 1
+    mkdir -p "${BINARIES_DIR}/boot/boot"     || exit 1
+	mkdir -p "${BINARIES_DIR}/boot/extlinux" || exit 1
+    cp "${BINARIES_DIR}/Image"                 "${BINARIES_DIR}/boot/boot/linux"                || exit 1
+    cp "${BINARIES_DIR}/initrd.gz"             "${BINARIES_DIR}/boot/boot/initrd.gz"            || exit 1
+    cp "${BINARIES_DIR}/rootfs.squashfs"       "${BINARIES_DIR}/boot/boot/batocera.update"      || exit 1
+    cp "${BINARIES_DIR}/rk3399-rock960.dtb"  "${BINARIES_DIR}/boot/boot/rk3399-rock960.dtb" || exit 1
+    cp "${BINARIES_DIR}/batocera-boot.conf"    "${BINARIES_DIR}/boot/batocera-boot.conf"        || exit 1
+	cp "board/batocera/rock960/boot/extlinux.conf" ${BINARIES_DIR}/boot/extlinux                   || exit 1
+    # boot.tar.xz
+    echo "creating boot.tar.xz"
+    (cd "${BINARIES_DIR}/boot" && tar -cJf "${BATOCERA_BINARIES_DIR}/boot.tar.xz" extlinux boot batocera-boot.conf) || exit 1
+
+    # blobs
+    for F in idbloader.img trust.img uboot.img
+    do
+    cp "${BINARIES_DIR}/${F}" "${BINARIES_DIR}/boot/${F}" || exit 1
+    done
+
+    # batocera.img
+    # rename the squashfs : the .update is the version that will be renamed at boot to replace the old version
+    mv "${BINARIES_DIR}/boot/boot/batocera.update" "${BINARIES_DIR}/boot/boot/batocera" || exit 1
+    GENIMAGE_TMP="${BUILD_DIR}/genimage.tmp"
+    BATOCERAIMG="${BATOCERA_BINARIES_DIR}/batocera.img"
+    rm -rf "${GENIMAGE_TMP}" || exit 1
+    cp "board/batocera/rock960/genimage.cfg" "${BINARIES_DIR}" || exit 1
+    echo "generating image"
+    genimage --rootpath="${TARGET_DIR}" --inputpath="${BINARIES_DIR}/boot" --outputpath="${BATOCERA_BINARIES_DIR}" --config="${BINARIES_DIR}/genimage.cfg" --tmppath="${GENIMAGE_TMP}" || exit 1
+    rm -f "${BATOCERA_BINARIES_DIR}/boot.vfat" || exit 1
+    sync || exit 1
+    ;;
+
 	ODROIDN2)
 	touch ${TARGET_DIR}/userdata/.please_resize_me
 	# /boot
