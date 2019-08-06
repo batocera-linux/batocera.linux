56ddee3f6dd61db0388fdf3d6d39a9fdf55ab52c
Ntemis <ierokipides.dem@gmail.com>
2019-08-07 01:10:22 +0300

    ROCK960: Provide ready to flash uboot binaries

---
+++ package/batocera/boot/rock960-blobs/rock960-blobs.mk
@@ -0,0 +1,17 @@
+################################################################################
+#
+# rock960-blobs
+#
+################################################################################
+
+ROCK960_BLOBS_VERSION = 74754e2
+ROCK960_BLOBS_SITE = https://github.com/Multi-Retropie/rock960-blobs.git
+ROCK960_BLOBS_SITE_METHOD=git
+
+define ROCK960_BLOBS_INSTALL_TARGET_CMDS
+	cp $(@D)/idbloader.img $(BINARIES_DIR)/
+	cp $(@D)/trust.img     $(BINARIES_DIR)/
+	cp $(@D)/uboot.img     $(BINARIES_DIR)/
+endef
+
+$(eval $(generic-package))
