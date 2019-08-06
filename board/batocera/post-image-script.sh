c75f81fd4ad9e5ecb44de188137a484ae1014f64
Ntemis <ierokipides.dem@gmail.com>
2019-08-07 01:15:29 +0300

    ROCK960:Adjust script for new blobs

--- board/batocera/post-image-script.sh
+++ board/batocera/post-image-script.sh
@@ -388,12 +388,9 @@ case "${BATOCERA_TARGET}" in
         # boot.tar.xz
         echo "creating boot.tar.xz"
         (cd "${BINARIES_DIR}/boot" && tar -cJf "${BATOCERA_BINARIES_DIR}/boot.tar.xz" extlinux boot batocera-boot.conf) || exit 1
+
 	    # blobs
-	    MKIMAGE=$HOST_DIR/bin/mkimage
-	    BOARD_DIR="board/batocera/rock960"
-	    $MKIMAGE -n rk3399 -T rksd -d $BINARIES_DIR/u-boot-spl-dtb.bin $BINARIES_DIR/u-boot-spl-dtb.img
-	    cat $BINARIES_DIR/u-boot-dtb.bin >> $BINARIES_DIR/u-boot-spl-dtb.img
-	    for F in idbloader.img trust.img u-boot-spl-dtb.img
+	    for F in idbloader.img trust.img uboot.img
         do
         cp "${BINARIES_DIR}/${F}" "${BINARIES_DIR}/boot/${F}" || exit 1
         done
