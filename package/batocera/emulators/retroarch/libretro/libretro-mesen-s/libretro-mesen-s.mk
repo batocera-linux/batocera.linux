3f27115b7cdd82c0ce841f73b7b117ad510d22bf
Ntemis <ierokipides.dem@gmail.com>
2019-09-04 00:38:46 +0300

    Add Libretro Mesen-S Core - Mesen-S is a cross-platform (Windows & Linux) SNES emulator built in C++ and C#
    Signed-off-by: Ntemis <ierokipides.dem@gmail.com>

---
+++ package/batocera/emulators/retroarch/libretro/libretro-mesen-s/libretro-mesen-s.mk
@@ -0,0 +1,18 @@
+################################################################################
+#
+# MESEN-S
+#
+################################################################################
+LIBRETRO_MESEN-S_VERSION = 5da37974fc6a8efc651c834e42162dd8fe8af99e
+LIBRETRO_MESEN-S_SITE = $(call github,SourMesen,Mesen-S,$(LIBRETRO_MESEN-S_VERSION))
+
+define LIBRETRO_MESEN-S_BUILD_CMDS
+	CFLAGS="$(TARGET_CFLAGS)" CXXFLAGS="$(TARGET_CXXFLAGS)" $(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" -C $(@D)/Libretro
+endef
+
+define LIBRETRO_MESEN-S_INSTALL_TARGET_CMDS
+	$(INSTALL) -D $(@D)/Libretro/mesen-s_libretro.so \
+		$(TARGET_DIR)/usr/lib/libretro/mesen-s_libretro.so
+endef
+
+$(eval $(generic-package))
