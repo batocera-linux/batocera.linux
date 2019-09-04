084ca22652bc432bc72f02703fe4130f9a16e20d
Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>
2019-09-04 20:42:51 +0200

    update to es-carbon caruso
    Signed-off-by: Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>

--- package/batocera/emulationstation/es-theme-carbon/es-theme-carbon.mk
+++ package/batocera/emulationstation/es-theme-carbon/es-theme-carbon.mk
@@ -3,9 +3,9 @@
 # EmulationStation theme "Carbon"
 #
 ################################################################################
-# Version.: Commits on Jan 29, 2019
-ES_THEME_CARBON_VERSION = 37413452b33497385c314b4d630d1b1ccafbb7d6
-ES_THEME_CARBON_SITE = $(call github,RetroPie,es-theme-carbon,$(ES_THEME_CARBON_VERSION))
+# Version.: Commits on Sep 04, 2019
+ES_THEME_CARBON_VERSION = 62509737c2f732b81ce7bf37f6c4c3b82dafae28
+ES_THEME_CARBON_SITE = $(call github,fabricecaruso,es-theme-carbon,$(ES_THEME_CARBON_VERSION))
 
 define ES_THEME_CARBON_INSTALL_TARGET_CMDS
     mkdir -p $(TARGET_DIR)/usr/share/emulationstation/themes/es-theme-carbon
