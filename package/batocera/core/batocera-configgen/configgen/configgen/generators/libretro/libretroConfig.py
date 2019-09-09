703b975692ce78612898c747b520ea8d37265735
Jérémy <xgenetik57x@gmail.com>
2019-09-10 00:03:44 +0200

    Retroachievements: Add PSX

--- package/batocera/core/batocera-configgen/configgen/configgen/generators/libretro/libretroConfig.py
+++ package/batocera/core/batocera-configgen/configgen/configgen/generators/libretro/libretroConfig.py
@@ -28,7 +28,7 @@ coreToP1Device = {'cap32': '513', '81': '257', 'fuse': '513'};
 coreToP2Device = {'fuse': '513'};
 
 # Define systems compatible with retroachievements
-systemToRetroachievements = {'snes', 'nes', 'gba', 'gb', 'gbc', 'megadrive', 'mastersystem', 'pcengine', 'lynx', 'ngp', 'atari2600', 'virtualboy', 'neogeo', 'neogeocd'};
+systemToRetroachievements = {'snes', 'nes', 'gba', 'gb', 'gbc', 'megadrive', 'mastersystem', 'pcengine', 'lynx', 'ngp', 'atari2600', 'virtualboy', 'neogeo', 'neogeocd', 'psx'};
 
 # Define systems not compatible with rewind option
 systemNoRewind = {'sega32x', 'psx', 'zxspectrum', 'odyssey2', 'mame', 'n64', 'dreamcast', 'atomiswave', 'naomi', 'neogeocd', 'saturn'};
