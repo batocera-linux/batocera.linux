fb272cae49402020a833ef5aca24baa71ae5886f
Ntemis <ierokipides.dem@gmail.com>
2019-09-11 18:34:46 +0300

    Upgrade to Retroarch v1.7.8 (v3) - Remove hash requirment

--- package/batocera/emulators/retroarch/shaders/glsl-shaders/glsl-shaders.mk
+++ package/batocera/emulators/retroarch/shaders/glsl-shaders/glsl-shaders.mk
@@ -3,8 +3,8 @@
 # GLSL SHADERS
 #
 ################################################################################
-# Version.: Commits on May 7, 2019
-GLSL_SHADERS_VERSION = 257a5bb0fdffd4e62ead54ab1919ef0ff0cf0b79
+# Version.: Commits on Sep 11, 2019
+GLSL_SHADERS_VERSION = 38b09e4e202ba7aea1384724adcc993ead236f7d
 GLSL_SHADERS_SITE = $(call github,libretro,glsl-shaders,$(GLSL_SHADERS_VERSION))
 GLSL_SHADERS_LICENSE = GPL
 
--- package/batocera/emulators/retroarch/shaders/slang-shaders/slang-shaders.mk
+++ package/batocera/emulators/retroarch/shaders/slang-shaders/slang-shaders.mk
@@ -3,8 +3,8 @@
 # SLANG SHADERS
 #
 ################################################################################
-# Version.: Commits on May 12, 2019
-SLANG_SHADERS_VERSION = 2730bc6b31ceff6369070bb8df87bfcf4b194558
+# Version.: Commits on Sep 11, 2019
+SLANG_SHADERS_VERSION = ad8b06f747b5fe661c6440691d67aa21ca3167ab
 SLANG_SHADERS_SITE = $(call github,libretro,slang-shaders,$(SLANG_SHADERS_VERSION))
 SLANG_SHADERS_LICENSE = GPL
 
