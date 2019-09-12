d7b2316aa992da1bbd6d8a5ac6eaa2ac2579ef96
Jérémy <xgenetik57x@gmail.com>
2019-09-12 23:16:06 +0200

    Update RetroFlag Safe Shutdown script

--- package/batocera/utils/retroflag/retroflag.mk
+++ package/batocera/utils/retroflag/retroflag.mk
@@ -1,9 +1,10 @@
 ################################################################################
 #
-# retroflag 
+# RetroFlag 
 #
 ################################################################################
-RETROFLAG_VERSION = b43b53dbc88696c42e47a634381a7ad94410f9a9
+# Version.: Commits on May 18, 2018
+RETROFLAG_VERSION = ac0b5a6718c543ce7f10bb70b829dc24d8534e9f
 RETROFLAG_SITE = $(call github,RetroFlag,retroflag-picase,$(RETROFLAG_VERSION))
 
 define RETROFLAG_INSTALL_TARGET_CMDS
