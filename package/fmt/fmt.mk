f99abb4791b628222f4f49611adfe9b88efb4abd
Juliano Dorigão <jdorigao@gmail.com>
2019-09-24 13:31:40 -0300

    bump: fmt to v6.0.0
    Signed-off-by: Juliano Dorigão <jdorigao@gmail.com>

--- package/fmt/fmt.mk
+++ package/fmt/fmt.mk
@@ -3,8 +3,8 @@
 # fmt
 #
 ################################################################################
-
-FMT_VERSION = 5.3.0
+# Batocera: Newer versions of Citra-emu Requires Version 6.0.0
+FMT_VERSION = 6.0.0
 FMT_SITE = $(call github,fmtlib,fmt,$(FMT_VERSION))
 FMT_LICENSE = BSD-2-Clause
 FMT_LICENSE_FILES = LICENSE.rst
