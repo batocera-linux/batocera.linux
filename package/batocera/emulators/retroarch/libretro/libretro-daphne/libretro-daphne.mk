934014cfad90d96dd2a6e4114c301e48c07afb17
Juliano Dorigão <jdorigao@gmail.com>
2019-10-23 08:35:51 -0300

    bump: libretro-daphne
    Signed-off-by: Juliano Dorigão <jdorigao@gmail.com>

--- package/batocera/emulators/retroarch/libretro/libretro-daphne/libretro-daphne.mk
+++ package/batocera/emulators/retroarch/libretro/libretro-daphne/libretro-daphne.mk
@@ -3,8 +3,8 @@
 # DAPHNE
 #
 ################################################################################
-# Version.: Commits on Aug 17, 2019
-LIBRETRO_DAPHNE_VERSION = c1f7d09c8b3a9bf17b28bd5a123635fb6784f8ba
+# Version.: Commits on Sep 14, 2019
+LIBRETRO_DAPHNE_VERSION = 7e5cac88d0509c6f4722100c5b8a9b5ee91f404a
 LIBRETRO_DAPHNE_SITE = $(call github,libretro,daphne,$(LIBRETRO_DAPHNE_VERSION))
 
 define LIBRETRO_DAPHNE_BUILD_CMDS
