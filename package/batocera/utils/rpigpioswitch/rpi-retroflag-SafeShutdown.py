93db72e688fcaa959a20e5aaacc67b843f0840e3
cyperghost <crcerror@users.noreply.github.com>
2019-11-07 21:56:23 +0100

    Update rpi-retroflag-SafeShutdown.py

--- package/batocera/utils/rpigpioswitch/rpi-retroflag-SafeShutdown.py
+++ package/batocera/utils/rpigpioswitch/rpi-retroflag-SafeShutdown.py
@@ -8,9 +8,9 @@ from multiprocessing import Process
 
 #initialize pins
 powerPin = 3 #pin 5
-ledPin = 14 #TXD
-resetPin = 2 #pin 13
-powerenPin = 4 #pin 5
+ledPin = 14 #TXD - pin 8
+resetPin = 2 #pin 3
+powerenPin = 4 #pin 7
 
 #initialize GPIO settings
 def init():
