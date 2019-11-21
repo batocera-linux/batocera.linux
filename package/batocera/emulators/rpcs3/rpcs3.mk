9c578dc72b961bc9c01e4e7853479946ba76cf2f
Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>
2019-11-21 07:06:08 +0000

    rpcs3 : next
    Signed-off-by: Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>

--- package/batocera/emulators/rpcs3/rpcs3.mk
+++ package/batocera/emulators/rpcs3/rpcs3.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RPCS3_VERSION = 997e3046e303662e73cc595997d4776e09100165
+RPCS3_VERSION = cb66d05693504f4e453999af489786d05d2a8e51
 RPCS3_SITE = https://github.com/RPCS3/rpcs3.git
 RPCS3_SITE_METHOD=git
 RPCS3_GIT_SUBMODULES=YES
@@ -14,8 +14,22 @@ RPCS3_DEPENDENCIES = qt5declarative libxml2 mesa3d libglu openal alsa-lib libevd
 RPCS3_CONF_OPTS += -DUSE_PULSE=OFF
 RPCS3_CONF_OPTS += -DUSE_SYSTEM_FFMPEG=ON
 RPCS3_CONF_OPTS += -DUSE_SYSTEM_LIBPNG=ON
+RPCS3_CONF_OPTS += -DUSE_DISCORD_RPC=OFF
 
-#RPCS3_CONF_OPTS += -DBUILD_LLVM_SUBMODULE=OFF
-RPCS3_CONF_OPTS += -DWITH_LLVM=OFF
+RPCS3_CONF_OPTS += -DCMAKE_CROSSCOMPILING=OFF
+define RPCS3_BUILD_CMDS
+        LD_LIBRARY_PATH=$(HOST_DIR)/lib $(TARGET_MAKE_ENV) $(MAKE) -C $(@D)
+endef
+
+define RPCS3_INSTALL_LIBS
+        cp $(@D)/rpcs3/rpcs3qt/librpcs3_ui.so $(TARGET_DIR)/usr/lib/
+        cp $(@D)/rpcs3/Emu/librpcs3_emu.so $(TARGET_DIR)/usr/lib/
+        cp $(@D)/asmjitsrc/libasmjit.so $(TARGET_DIR)/usr/lib/
+        cp $(@D)/3rdparty/yaml-cpp/libyaml-cpp.so.0.6 $(TARGET_DIR)/usr/lib/
+        cp $(@D)/3rdparty/xxHash/cmake_unofficial/libxxhash.so.0.6.5 $(TARGET_DIR)/usr/lib/
+        cp $(@D)/3rdparty/llvm_build/lib/libLLVM*.so.9svn $(TARGET_DIR)/usr/lib/
+endef
+
+RPCS3_POST_INSTALL_TARGET_HOOKS += RPCS3_INSTALL_LIBS
 
 $(eval $(cmake-package))
