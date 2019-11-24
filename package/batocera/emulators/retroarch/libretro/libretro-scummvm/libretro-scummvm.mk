e2c0365aa8a14c35bb26f82f7d1f38a3c600b209
Juliano Dorigão <jdorigao@gmail.com>
2019-11-24 18:16:35 -0300

    experimental: libretro-scummvm
    Signed-off-by: Juliano Dorigão <jdorigao@gmail.com>

---
+++ package/batocera/emulators/retroarch/libretro/libretro-scummvm/libretro-scummvm.mk
@@ -0,0 +1,21 @@
+################################################################################
+#
+# SCUMMVM
+#
+################################################################################
+# VERSION.: Commits on Nov 24, 2019
+LIBRETRO_SCUMMVM_VERSION = 0c777c88c556e21acb9a0772790c7519acf499c6
+LIBRETRO_SCUMMVM_SITE = $(call github,libretro,scummvm,$(LIBRETRO_SCUMMVM_VERSION))
+LIBRETRO_SCUMMVM_LICENSE = GPLv2
+
+define LIBRETRO_SCUMMVM_BUILD_CMDS
+	CFLAGS="$(TARGET_CFLAGS)" CXXFLAGS="$(TARGET_CXXFLAGS)" LDFLAGS="-shared -Wl,--no-undefined" $(MAKE) \
+		TOOLSET="$(TARGET_CROSS)" -C $(@D)/backends/platform/libretro/build platform="$(LIBRETRO_PLATFORM)"
+endef
+
+define LIBRETRO_SCUMMVM_INSTALL_TARGET_CMDS
+	$(INSTALL) -D $(@D)/backends/platform/libretro/build/scummvm_libretro.so \
+		$(TARGET_DIR)/usr/lib/libretro/scummvm_libretro.so
+endef
+
+$(eval $(generic-package))
