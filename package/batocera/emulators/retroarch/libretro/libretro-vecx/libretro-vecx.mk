33bd61c99e82190f841c70bea934edfef3b52ee6
Juliano Dorigão <jdorigao@gmail.com>
2019-12-22 20:34:55 -0400

    update: libretro-vecx
    Signed-off-by: Juliano Dorigão <jdorigao@gmail.com>

--- package/batocera/emulators/retroarch/libretro/libretro-vecx/libretro-vecx.mk
+++ package/batocera/emulators/retroarch/libretro/libretro-vecx/libretro-vecx.mk
@@ -3,8 +3,8 @@
 # VECX
 #
 ################################################################################
-# Version.: Commits on Aug 17, 2019
-LIBRETRO_VECX_VERSION = 26585ee701499550e484c11f005db18e926827d9
+# Version.: Commits on Nov 29, 2019
+LIBRETRO_VECX_VERSION = 4c6d2f86d2af9172618d4b77471cfb4f4ca6ee05
 LIBRETRO_VECX_SITE = $(call github,libretro,libretro-vecx,$(LIBRETRO_VECX_VERSION))
 LIBRETRO_VECX_LICENSE = GPLv2|LGPLv2.1
 
