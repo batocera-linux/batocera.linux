c7aba0cbbfd2285be775122f9cbaa1d0e4db1446
Juliano Dorigão <jdorigao@gmail.com>
2019-12-22 20:30:55 -0400

    update: libretro-fceumm
    Signed-off-by: Juliano Dorigão <jdorigao@gmail.com>

--- package/batocera/emulators/retroarch/libretro/libretro-fceumm/libretro-fceumm.mk
+++ package/batocera/emulators/retroarch/libretro/libretro-fceumm/libretro-fceumm.mk
@@ -3,8 +3,8 @@
 # FCEUMM
 #
 ################################################################################
-# Version.: Commits on Nov 7, 2019
-LIBRETRO_FCEUMM_VERSION = cfa6350035811801d7c795092153299838ab8a4a
+# Version.: Commits on Dez 11, 2019
+LIBRETRO_FCEUMM_VERSION = 58030a374d362b10f6c5af3d1d8fdfeb513dba2b
 LIBRETRO_FCEUMM_SITE = $(call github,libretro,libretro-fceumm,$(LIBRETRO_FCEUMM_VERSION))
 LIBRETRO_FCEUMM_LICENSE = GPLv2
 
