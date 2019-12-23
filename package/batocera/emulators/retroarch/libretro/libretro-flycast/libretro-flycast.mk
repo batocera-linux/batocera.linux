e0a85a3241d0f64897a6684eb0497f6ed18fe623
Juliano Dorigão <jdorigao@gmail.com>
2019-12-22 20:31:03 -0400

    update: libretro-flycast
    Signed-off-by: Juliano Dorigão <jdorigao@gmail.com>

--- package/batocera/emulators/retroarch/libretro/libretro-flycast/libretro-flycast.mk
+++ package/batocera/emulators/retroarch/libretro/libretro-flycast/libretro-flycast.mk
@@ -3,8 +3,8 @@
 # LIBRETRO-FLYCAST
 #
 ################################################################################
-# Version.: Commits on Nov 14, 2019
-LIBRETRO_FLYCAST_VERSION = 64cd7e1d0a372ce904eef2b21ae793c241fc1477
+# Version.: Commits on Dez 22, 2019
+LIBRETRO_FLYCAST_VERSION = ea8528a7de5318991328f0fb037ab3cee4c56196
 LIBRETRO_FLYCAST_SITE = $(call github,libretro,flycast,$(LIBRETRO_FLYCAST_VERSION))
 LIBRETRO_FLYCAST_LICENSE = GPLv2
 
