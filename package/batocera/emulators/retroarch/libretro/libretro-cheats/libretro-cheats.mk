dfbabc863d585f0f35bf92725b69def570b58050
Juliano Dorigão <jdorigao@gmail.com>
2019-12-22 20:30:37 -0400

    update: libretro-cheats
    Signed-off-by: Juliano Dorigão <jdorigao@gmail.com>

--- package/batocera/emulators/retroarch/libretro/libretro-cheats/libretro-cheats.mk
+++ package/batocera/emulators/retroarch/libretro/libretro-cheats/libretro-cheats.mk
@@ -3,8 +3,8 @@
 # CHEATS
 #
 ################################################################################
-# Version.: Commits on Nov 24, 2019
-LIBRETRO_CHEATS_VERSION = 9bd7f8f7a2a4e8e062c997e813e79432c487635d
+# Version.: Commits on Dez 04, 2019
+LIBRETRO_CHEATS_VERSION = a4ab7c4fb53e047e3a420b098922ce16e3e1d3cf
 LIBRETRO_CHEATS_SITE = $(call github,libretro,libretro-database,$(LIBRETRO_CHEATS_VERSION))
 
 define LIBRETRO_CHEATS_INSTALL_TARGET_CMDS
