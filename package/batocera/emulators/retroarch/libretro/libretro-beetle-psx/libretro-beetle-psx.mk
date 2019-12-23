13034e9dcac8824dbc7bc06b6d58af8d5fc9e870
Juliano Dorigão <jdorigao@gmail.com>
2019-12-22 20:30:11 -0400

    update: libretro-psx
    Signed-off-by: Juliano Dorigão <jdorigao@gmail.com>

--- package/batocera/emulators/retroarch/libretro/libretro-beetle-psx/libretro-beetle-psx.mk
+++ package/batocera/emulators/retroarch/libretro/libretro-beetle-psx/libretro-beetle-psx.mk
@@ -3,8 +3,8 @@
 # LIBRETRO_BEETLE_PSX
 #
 ################################################################################
-# Version.: Commits on Sep 11, 2019
-LIBRETRO_BEETLE_PSX_VERSION = ca36c044e4deec4812ae20a35b8df129d61917ee
+# Version.: Commits on Dez 19, 2019
+LIBRETRO_BEETLE_PSX_VERSION = 8cdfd50b1026085c5974bc1ec11ad9c2b53593cf
 LIBRETRO_BEETLE_PSX_SITE = $(call github,libretro,beetle-psx-libretro,$(LIBRETRO_BEETLE_PSX_VERSION))
 LIBRETRO_BEETLE_PSX_LICENSE = GPLv2
 
