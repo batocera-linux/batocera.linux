463bd02c8707362284053fac52209eaaa72e09a7
Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>
2019-12-30 22:52:26 +0100

    file untabify
    Signed-off-by: Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>

--- package/batocera/core/batocera-configgen/configgen/configgen/generators/mupen/mupenControllers.py
+++ package/batocera/core/batocera-configgen/configgen/configgen/generators/mupen/mupenControllers.py
@@ -11,7 +11,8 @@ import batoceraFiles
 # http://mupen64plus.org/wiki/index.php?title=Mupen64Plus_Plugin_Parameters
 
 # Mupen doesn't like to have 2 buttons mapped for N64 pad entry. That's why r2 is commented for now. 1 axis and 1 button is ok
-mupenHatToAxis = {'1': 'Up', '2': 'Right', '4': 'Down', '8': 'Left'}
+mupenHatToAxis        = {'1': 'Up',   '2': 'Right', '4': 'Down', '8': 'Left'}
+mupenHatToReverseAxis = {'1': 'Down', '2': 'Left',  '4': 'Up',   '8': 'Right'}
 mupenDoubleAxis = {0:'X Axis', 1:'Y Axis'}
 
 def getMupenMapping():
@@ -31,9 +32,9 @@ def getMupenMapping():
 def setControllersConfig(iniConfig, controllers, systemconfig):
     nplayer = 1
     for playercontroller, pad in sorted(controllers.items()):
-	# Dynamic controller bindings
-	config = defineControllerKeys(pad, systemconfig)
-	fillIniPlayer(nplayer, iniConfig, pad, config)
+        # Dynamic controller bindings
+        config = defineControllerKeys(pad, systemconfig)
+        fillIniPlayer(nplayer, iniConfig, pad, config)
         nplayer += 1
 
     # remove section with no player
@@ -45,14 +46,14 @@ def setControllersConfig(iniConfig, controllers, systemconfig):
 def defineControllerKeys(controller, systemconfig):
         mupenmapping = getMupenMapping()
 
-	# config holds the final pad configuration in the mupen style
-	# ex: config['DPad U'] = "button(1)"
-	config = dict()
+        # config holds the final pad configuration in the mupen style
+        # ex: config['DPad U'] = "button(1)"
+        config = dict()
 
         # deadzone and peak from config files
-	config['AnalogDeadzone'] = mupenmapping['AnalogDeadzone']
+        config['AnalogDeadzone'] = mupenmapping['AnalogDeadzone']
         config['AnalogPeak']     = mupenmapping['AnalogPeak']
-	if 'analogdeadzone' in systemconfig:
+        if 'analogdeadzone' in systemconfig:
             config['AnalogDeadzone'] = systemconfig['analogdeadzone']
         if 'analogpeak' in systemconfig:
             config['AnalogPeak']     = systemconfig['analogpeak']
@@ -60,111 +61,116 @@ def defineControllerKeys(controller, systemconfig):
         # z is important, in case l2 is not available for this pad, use l1
         # assume that l2 is for "Z Trig" in the mapping
         if 'l2' not in controller.inputs:
-            controller.inputs['l1'] = controller.inputs['l2']
+            mupenmapping['pageup'] = mupenmapping['l2']
 
         # if joystick1up is not available, use up/left while these keys are more used
         if 'joystick1up' not in controller.inputs:
-            controller.inputs['up']    = controller.inputs['joystick1up']
-            controller.inputs['down']  = controller.inputs['joystick1down']
-            controller.inputs['left']  = controller.inputs['joystick1left']
-            controller.inputs['right'] = controller.inputs['joystick1right']
+            mupenmapping['up']    = mupenmapping['joystick1up']
+            mupenmapping['down']  = mupenmapping['joystick1down']
+            mupenmapping['left']  = mupenmapping['joystick1left']
+            mupenmapping['right'] = mupenmapping['joystick1right']
 
-	# the input.xml adds 2 directions per joystick, ES handles just 1
-	fakeSticks = { 'joystick2up' : 'joystick2down', 'joystick2left' : 'joystick2right'}
-	# Cheat on the controller
-	for realStick, fakeStick in fakeSticks.iteritems():
-		if realStick in controller.inputs:
-			print fakeStick + "-> " + realStick
-			inputVar =  Input(fakeStick
-					, controller.inputs[realStick].type
-					, controller.inputs[realStick].id
-					, str(-int(controller.inputs[realStick].value))
-					, controller.inputs[realStick].code)
-			controller.inputs[fakeStick] = inputVar
-	
-	for inputIdx in controller.inputs:
-		input = controller.inputs[inputIdx]
-		if input.name in mupenmapping and mupenmapping[input.name] != "":
+        # the input.xml adds 2 directions per joystick, ES handles just 1
+        fakeSticks = { 'joystick2up' : 'joystick2down', 'joystick2left' : 'joystick2right'}
+        # Cheat on the controller
+        for realStick, fakeStick in fakeSticks.iteritems():
+                if realStick in controller.inputs:
+                        print fakeStick + "-> " + realStick
+                        inputVar =  Input(fakeStick
+                                        , controller.inputs[realStick].type
+                                        , controller.inputs[realStick].id
+                                        , str(-int(controller.inputs[realStick].value))
+                                        , controller.inputs[realStick].code)
+                        controller.inputs[fakeStick] = inputVar
+
+        for inputIdx in controller.inputs:
+                input = controller.inputs[inputIdx]
+                if input.name in mupenmapping and mupenmapping[input.name] != "":
                         value=setControllerLine(mupenmapping, input, mupenmapping[input.name])
                         # Handle multiple inputs for a single N64 Pad input
-                        if mupenmapping[input.name] not in config :
+                        if value != "":
+                            if mupenmapping[input.name] not in config :
                                 config[mupenmapping[input.name]] = value
-                        else:
+                            else:
                                 config[mupenmapping[input.name]] += " " + value
-	return config
+        return config
 
 
 def setControllerLine(mupenmapping, input, mupenSettingName):
-	value = ''
-	inputType = input.type
-	if inputType == 'button':
-		value = "button({})".format(input.id)
-	elif inputType == 'hat':
-		value = "hat({} {})".format(input.id, mupenHatToAxis[input.value])
-	elif inputType == 'axis':
-		# Generic case for joystick1up and joystick1left
-		if mupenSettingName in mupenDoubleAxis.values():
-			# X axis : value = -1 for left, +1 for right
-			# Y axis : value = -1 for up, +1 for down
-			if input.value == "-1":				
-				value = "axis({}-,{}+)".format(input.id, input.id)
-			else:
-				value = "axis({}+,{}-)".format(input.id, input.id)
-		else:
+        value = ''
+        inputType = input.type
+        if inputType == 'button':
+                value = "button({})".format(input.id)
+        elif inputType == 'hat':
+                if mupenSettingName in ["X Axis", "Y Axis"]: # special case for these 2 axis...
+                    if input.value == "1" or input.value == "8": # only for the lower value to avoid duplicate
+                        value = "hat({} {} {})".format(input.id, mupenHatToAxis[input.value], mupenHatToReverseAxis[input.value])
+                else:
+                    value = "hat({} {})".format(input.id, mupenHatToAxis[input.value])
+        elif inputType == 'axis':
+                # Generic case for joystick1up and joystick1left
+                if mupenSettingName in mupenDoubleAxis.values():
+                        # X axis : value = -1 for left, +1 for right
+                        # Y axis : value = -1 for up, +1 for down
+                        if input.value == "-1":
+                                value = "axis({}-,{}+)".format(input.id, input.id)
+                        else:
+                                value = "axis({}+,{}-)".format(input.id, input.id)
+                else:
                         if input.value == "1":
                                 value = "axis({}+)".format(input.id)
                         else:
                                 value = "axis({}-)".format(input.id)
-	return value
+        return value
 
 def fillIniPlayer(nplayer, iniConfig, controller, config):
-	section = "Input-SDL-Control"+str(nplayer)
+        section = "Input-SDL-Control"+str(nplayer)
 
-	# set static config
+        # set static config
         if not iniConfig.has_section(section):
-	    iniConfig.add_section(section)
+            iniConfig.add_section(section)
         iniConfig.set(section, 'Version', '2')
         iniConfig.set(section, 'mode', 0)
-	iniConfig.set(section, 'device', controller.index)
+        iniConfig.set(section, 'device', controller.index)
         iniConfig.set(section, 'name', controller.realName)
-	iniConfig.set(section, 'plugged', True)
-	iniConfig.set(section, 'plugin', 2)
-	iniConfig.set(section, 'AnalogDeadzone', config['AnalogDeadzone'])
-	iniConfig.set(section, 'AnalogPeak', config['AnalogPeak'])
-	iniConfig.set(section, 'mouse', "False")
+        iniConfig.set(section, 'plugged', True)
+        iniConfig.set(section, 'plugin', 2)
+        iniConfig.set(section, 'AnalogDeadzone', config['AnalogDeadzone'])
+        iniConfig.set(section, 'AnalogPeak', config['AnalogPeak'])
+        iniConfig.set(section, 'mouse', "False")
 
-	# set dynamic config - clear all keys then fill
-	iniConfig.set(section, "Mempak switch", "")
-	iniConfig.set(section, "Rumblepak switch", "")
-	iniConfig.set(section, "C Button R", "")
-	iniConfig.set(section, "A Button", "")
-	iniConfig.set(section, "C Button U", "")
-	iniConfig.set(section, "B Button", "")
-	iniConfig.set(section, "Start", "")
-	iniConfig.set(section, "L Trig", "")
-	iniConfig.set(section, "R Trig", "")
-	iniConfig.set(section, "Z Trig", "")
-	iniConfig.set(section, "DPad U", "")
-	iniConfig.set(section, "DPad D", "")
-	iniConfig.set(section, "DPad R", "")
-	iniConfig.set(section, "DPad L", "")
-	iniConfig.set(section, "Y Axis", "")
-	iniConfig.set(section, "Y Axis", "")
-	iniConfig.set(section, "X Axis", "")
-	iniConfig.set(section, "X Axis", "")
-	iniConfig.set(section, "C Button U", "")
-	iniConfig.set(section, "C Button D", "")
-	iniConfig.set(section, "C Button L", "")
-	iniConfig.set(section, "C Button R", "")
-	for inputName in sorted(config):
-		iniConfig.set(section, inputName, config[inputName])
+        # set dynamic config - clear all keys then fill
+        iniConfig.set(section, "Mempak switch", "")
+        iniConfig.set(section, "Rumblepak switch", "")
+        iniConfig.set(section, "C Button R", "")
+        iniConfig.set(section, "A Button", "")
+        iniConfig.set(section, "C Button U", "")
+        iniConfig.set(section, "B Button", "")
+        iniConfig.set(section, "Start", "")
+        iniConfig.set(section, "L Trig", "")
+        iniConfig.set(section, "R Trig", "")
+        iniConfig.set(section, "Z Trig", "")
+        iniConfig.set(section, "DPad U", "")
+        iniConfig.set(section, "DPad D", "")
+        iniConfig.set(section, "DPad R", "")
+        iniConfig.set(section, "DPad L", "")
+        iniConfig.set(section, "Y Axis", "")
+        iniConfig.set(section, "Y Axis", "")
+        iniConfig.set(section, "X Axis", "")
+        iniConfig.set(section, "X Axis", "")
+        iniConfig.set(section, "C Button U", "")
+        iniConfig.set(section, "C Button D", "")
+        iniConfig.set(section, "C Button L", "")
+        iniConfig.set(section, "C Button R", "")
+        for inputName in sorted(config):
+                iniConfig.set(section, inputName, config[inputName])
 
 def cleanPlayer(nplayer, iniConfig):
-	section = "Input-SDL-Control"+str(nplayer)
+        section = "Input-SDL-Control"+str(nplayer)
 
-	# set static config
+        # set static config
         if not iniConfig.has_section(section):
-	    iniConfig.add_section(section)
+            iniConfig.add_section(section)
         iniConfig.set(section, 'Version', '2')
         iniConfig.set(section, 'plugged', False)
 
