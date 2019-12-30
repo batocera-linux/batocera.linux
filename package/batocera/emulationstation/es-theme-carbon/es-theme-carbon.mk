06aecd5da1916e02977c38c12ac0202e202e9a33
Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>
2019-12-30 20:36:04 +0000

    bump es-theme-carbon
    Signed-off-by: Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>

--- package/batocera/emulationstation/es-theme-carbon/es-theme-carbon.mk
+++ package/batocera/emulationstation/es-theme-carbon/es-theme-carbon.mk
@@ -3,8 +3,8 @@
 # EmulationStation theme "Carbon"
 #
 ################################################################################
-# Version.: Commits on Nov 30, 2019
-ES_THEME_CARBON_VERSION = 63e692a307f1c3246a50f53ae570f9aaf1b24f7e
+# Version.: Commits on Dec 30, 2019
+ES_THEME_CARBON_VERSION = bbb619607cc88ffdd8961e493e5408dd5e9f26fb
 ES_THEME_CARBON_SITE = $(call github,fabricecaruso,es-theme-carbon,$(ES_THEME_CARBON_VERSION))
 
 define ES_THEME_CARBON_INSTALL_TARGET_CMDS
