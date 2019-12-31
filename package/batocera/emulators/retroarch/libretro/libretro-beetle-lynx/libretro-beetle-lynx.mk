cf8e41cb26952100fbb9990d4100a262a94cd79a
Juliano Dorigão <jdorigao@gmail.com>
2019-12-31 16:44:17 -0400

    bump: libretro-beetle-lynx
    Signed-off-by: Juliano Dorigão <jdorigao@gmail.com>

--- package/batocera/emulators/retroarch/libretro/libretro-beetle-lynx/libretro-beetle-lynx.mk
+++ package/batocera/emulators/retroarch/libretro/libretro-beetle-lynx/libretro-beetle-lynx.mk
@@ -3,8 +3,8 @@
 # BEETLE_LYNX
 #
 ################################################################################
-# Version.: Commits on Nov 18, 2019
-LIBRETRO_BEETLE_LYNX_VERSION = fe8dc90931de95b90dc1ff76681a95910511f087
+# Version.: Commits on Dec 26, 2019
+LIBRETRO_BEETLE_LYNX_VERSION = 74dde204c0ec6c4bc4cd7821c14548387fbd9ce8
 LIBRETRO_BEETLE_LYNX_SITE = $(call github,libretro,beetle-lynx-libretro,$(LIBRETRO_BEETLE_LYNX_VERSION))
 LIBRETRO_BEETLE_LYNX_LICENSE = GPLv2
 
