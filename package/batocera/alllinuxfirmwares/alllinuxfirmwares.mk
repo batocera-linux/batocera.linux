db347aecdea704b0411ac2ae889a3a080b833bd3
Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>
2020-01-03 20:35:02 +0000

    batocera br merge fixes
    Signed-off-by: Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>

--- package/batocera/alllinuxfirmwares/alllinuxfirmwares.mk
+++ package/batocera/alllinuxfirmwares/alllinuxfirmwares.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 # Version from 2019-04-29 08:50:27 -0500 - amdgpu: update vega20 to the latest 19.10 firmware
-ALLLINUXFIRMWARES_VERSION = 92e17d0dd2437140fab044ae62baf69b35d7d1fa
+ALLLINUXFIRMWARES_VERSION = 20190717
 ALLLINUXFIRMWARES_SITE = http://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git
 ALLLINUXFIRMWARES_SITE_METHOD = git
 
