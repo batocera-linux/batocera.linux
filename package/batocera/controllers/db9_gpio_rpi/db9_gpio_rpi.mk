1d88caaafa23d223684631602c475786e633367a
Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>
2020-01-05 21:05:30 +0000

    br merge: cleaning db9_gpio
    Signed-off-by: Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>

--- package/batocera/controllers/db9_gpio_rpi/db9_gpio_rpi.mk
+++ package/batocera/controllers/db9_gpio_rpi/db9_gpio_rpi.mk
@@ -5,12 +5,16 @@
 ################################################################################
 DB9_GPIO_RPI_VERSION = c21faee29e42696722a953a8eee9093a223f1408
 DB9_GPIO_RPI_SITE = $(call github,marqs85,db9_gpio_rpi,$(DB9_GPIO_RPI_VERSION))
-DB9_GPIO_RPI_DEPENDENCIES = linux
 
 # Needed because can't pass cflags to cc
 define DB9_GPIO_RPI_RPI2_HOOK
-        $(SED) "s/#define BCM2708_PERI_BASE 0x20000000/#define BCM2708_PERI_BASE 0x3F000000/g" $(@D)/db9_gpio_rpi-1.2/db9_gpio_rpi.c
+        $(SED) "s/#define BCM2708_PERI_BASE 0x20000000/#define BCM2708_PERI_BASE 0x3F000000/g" $(@D)/db9_gpio_rpi.c
 endef
+
+define DB9_GPIO_RPI_FIX_EXTRACT
+	mv $(@D)/db9_gpio_rpi-1.2/* $(@D)/
+endef
+DB9_GPIO_RPI_POST_EXTRACT_HOOKS += DB9_GPIO_RPI_FIX_EXTRACT
 
 ifeq ($(BR2_cortex_a7),y)
         DB9_GPIO_RPI_PRE_CONFIGURE_HOOKS += DB9_GPIO_RPI_RPI2_HOOK
@@ -19,12 +23,5 @@ ifeq ($(BR2_cortex_a53),y)
         DB9_GPIO_RPI_PRE_CONFIGURE_HOOKS += DB9_GPIO_RPI_RPI2_HOOK
 endif
 
-define DB9_GPIO_RPI_BUILD_CMDS
-        $(MAKE) -C $(@D)/db9_gpio_rpi-1.2 $(LINUX_MAKE_FLAGS) KERNELDIR=$(LINUX_DIR)
-endef
-
-define DB9_GPIO_RPI_INSTALL_TARGET_CMDS
-        $(MAKE) -C $(@D)/db9_gpio_rpi-1.2 $(LINUX_MAKE_FLAGS) KERNELDIR=$(LINUX_DIR) modules_install
-endef
-
+$(eval $(kernel-module))
 $(eval $(generic-package))
