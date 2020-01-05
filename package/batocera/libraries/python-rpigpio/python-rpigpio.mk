df5b4251898778aca600a713e00560f8c9d31c85
Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>
2020-01-05 16:56:37 +0000

    bump python-rpigpio
    Signed-off-by: Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>

--- package/batocera/libraries/python-rpigpio/python-rpigpio.mk
+++ package/batocera/libraries/python-rpigpio/python-rpigpio.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_RPIGPIO_VERSION = 0.6.2
+PYTHON_RPIGPIO_VERSION = 0.7.0
 PYTHON_RPIGPIO_SOURCE = RPi.GPIO-$(PYTHON_RPIGPIO_VERSION).tar.gz
 PYTHON_RPIGPIO_SETUP_TYPE = distutils
 PYTHON_RPIGPIO_SITE = https://pypi.python.org/packages/source/R/RPi.GPIO
