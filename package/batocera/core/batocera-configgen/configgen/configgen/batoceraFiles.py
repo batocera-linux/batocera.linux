eba01aca66be0438cb731631bfaa7e3db1302b25
Juliano Dorigão <jdorigao@gmail.com>
2020-01-14 10:51:31 -0400

    daphne: added folders
    Signed-off-by: Juliano Dorigão <jdorigao@gmail.com>

--- package/batocera/core/batocera-configgen/configgen/configgen/batoceraFiles.py
+++ package/batocera/core/batocera-configgen/configgen/configgen/batoceraFiles.py
@@ -113,7 +113,7 @@ amiberryRetroarchCustom = amiberryRoot + '/conf/retroarch/retroarchcustom.cfg'
 
 hatariConf = CONF + '/hatari/hatari.cfg'
 
-daphneConfig = CONF + '/hypseus/hypinput.ini'
-daphneHomedir = CONF + '/hypseus'
-daphneDatadir = ROMS + '/daphne'
+daphneConfig = CONF + '/daphne/hypinput.ini'
+daphneHomedir = ROMS + '/daphne'
+daphneDatadir = CONF + '/daphne'
 daphneSaves = SAVES + '/daphne'
