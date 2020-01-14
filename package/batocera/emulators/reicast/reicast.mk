fabd97da8a0a836e95df3aeb31e726fc29bb3dff
Juliano Dorigão <jdorigao@gmail.com>
2020-01-13 21:38:45 -0400

    bump: reicast
    Signed-off-by: Juliano Dorigão <jdorigao@gmail.com>

--- package/batocera/emulators/reicast/reicast.mk
+++ package/batocera/emulators/reicast/reicast.mk
@@ -3,15 +3,14 @@
 # REICAST
 #
 ################################################################################
-# Version.: Commits on Sep 12, 2018
-REICAST_VERSION = r8.1
+# Version.: Commits on Jan 13, 2020
+REICAST_VERSION = 08579eea658657059afa29a446b160b1d34c7502
 REICAST_SITE = $(call github,reicast,reicast-emulator,$(REICAST_VERSION))
 REICAST_LICENSE = GPLv2
 REICAST_DEPENDENCIES = sdl2 libpng
 
 define REICAST_UPDATE_INCLUDES
-	sed -i "s+/opt/vc+$(STAGING_DIR)/usr+g" $(@D)/shell/linux/Makefile
-	sed -i "s+sdl2-config+$(STAGING_DIR)/usr/bin/sdl2-config+g" $(@D)/shell/linux/Makefile
+	sed -i "s+sdl2-config+$(STAGING_DIR)/usr/bin/sdl2-config+g" $(@D)/reicast/linux/Makefile
 endef
 
 REICAST_PRE_CONFIGURE_HOOKS += REICAST_UPDATE_INCLUDES
@@ -37,23 +36,17 @@ else ifeq ($(BR2_PACKAGE_BATOCERA_TARGET_TINKERBOARD)$(BR2_PACKAGE_BATOCERA_TARG
 endif
 
 ifeq ($(BR2_PACKAGE_SDL2_KMSDRM),y)
-	REICAST_EXTRA_ARGS="USE_SDL=1"
+	REICAST_EXTRA_ARGS = USE_SDL=1
 endif
 
-# Sadly the NEON optimizations in the PNG library don't work yet, so disable them
 define REICAST_BUILD_CMDS
-	$(TARGET_CONFIGURE_OPTS) $(MAKE) \
-		CPP="$(TARGET_CPP)" \
-		CXX="$(TARGET_CXX) -D_GLIBCXX_USE_CXX11_ABI=0" \
-		CC="$(TARGET_CC) -DPNG_ARM_NEON_OPT=0" \
-		AS="$(TARGET_CC)" \
-		STRIP="$(TARGET_STRIP)" \
-		-C $(@D)/shell/linux -f Makefile platform=$(BATOCERA_SYSTEM) $(REICAST_EXTRA_ARGS)
+	$(TARGET_CONFIGURE_OPTS) CFLAGS="$(TARGET_CFLAGS)" CXXFLAGS="$(TARGET_CXXFLAGS)" CPPFLAGS="$(TARGET_CPPFLAGS)" $(MAKE) \
+		CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" LD="$(TARGET_CC)" RANLIB="$(TARGET_RANLIB)" AR="$(TARGET_AR)" AS="$(TARGET_AS)" \
+		STRIP="$(TARGET_STRIP)" -C $(@D)/reicast/linux -f Makefile platform=$(BATOCERA_SYSTEM) $(REICAST_EXTRA_ARGS)
 endef
 
 define REICAST_INSTALL_TARGET_CMDS
-	$(INSTALL) -D -m 0755 $(@D)/shell/linux/reicast.elf $(TARGET_DIR)/usr/bin/reicast.elf
+	$(INSTALL) -D -m 0755 $(@D)/reicast/linux/reicast.elf $(TARGET_DIR)/usr/bin/reicast.elf
 endef
 
 $(eval $(generic-package))
-
