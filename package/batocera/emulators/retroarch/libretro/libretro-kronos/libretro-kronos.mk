64dc81c8980ced07158cff39668199cbbee61b3f
Juliano Dorigão <jdorigao@gmail.com>
2020-01-14 23:31:40 -0400

    bump: libretro-kronos
    Signed-off-by: Juliano Dorigão <jdorigao@gmail.com>

--- package/batocera/emulators/retroarch/libretro/libretro-kronos/libretro-kronos.mk
+++ package/batocera/emulators/retroarch/libretro/libretro-kronos/libretro-kronos.mk
@@ -3,8 +3,8 @@
 # LIBRETRO-KRONOS
 #
 ################################################################################
-# Version.: Commits on Dec 30, 2019
-LIBRETRO_KRONOS_VERSION = 3937ab74ee61ecda2b77b9a1626f47fb8766a02e
+# Version.: Commits on Jan 11, 2020
+LIBRETRO_KRONOS_VERSION = 1049c8ffb7ee9696a3e4b4061164f7949b8de811
 LIBRETRO_KRONOS_SITE = $(call github,libretro,yabause,$(LIBRETRO_KRONOS_VERSION))
 LIBRETRO_KRONOS_LICENSE = BSD-3-Clause
 
