3b3ac237a18ebc2e5c554152f6d2c789a6c54ca0
Juliano Dorigão <jdorigao@gmail.com>
2020-01-15 00:30:57 -0400

    bump: libretro-neocd
    Signed-off-by: Juliano Dorigão <jdorigao@gmail.com>

--- package/batocera/emulators/retroarch/libretro/libretro-neocd/libretro-neocd.mk
+++ package/batocera/emulators/retroarch/libretro/libretro-neocd/libretro-neocd.mk
@@ -4,8 +4,8 @@
 # NEOCD
 #
 ################################################################################
-# Version.: Commits on Nov 16, 2019
-LIBRETRO_NEOCD_VERSION = 25f5fefd1aa51eb7146be37dbd191f287ebd62a1
+# Version.: Commits on Dec 18, 2019
+LIBRETRO_NEOCD_VERSION = 7346ab58a0c0e96f35b39086b03439c10343b81c
 LIBRETRO_NEOCD_SITE = https://github.com/libretro/neocd_libretro.git
 LIBRETRO_NEOCD_SITE_METHOD=git
 LIBRETRO_NEOCD_GIT_SUBMODULES=YES
