9754e75a13fc68388a085b9b36a3c81a59449a06
Juliano Dorigão <jdorigao@gmail.com>
2020-01-14 23:42:34 -0400

    bump: libretro-prosystem
    Signed-off-by: Juliano Dorigão <jdorigao@gmail.com>

--- package/batocera/emulators/retroarch/libretro/libretro-prosystem/libretro-prosystem.mk
+++ package/batocera/emulators/retroarch/libretro/libretro-prosystem/libretro-prosystem.mk
@@ -3,8 +3,8 @@
 # PROSYSTEM
 #
 ################################################################################
-# Version.: Commits on Sep 14, 2019
-LIBRETRO_PROSYSTEM_VERSION = cb4aa3ee72f98b0891a7bac5c9dac458cdba4d34
+# Version.: Commits on Jan 06, 2020
+LIBRETRO_PROSYSTEM_VERSION = 6f7e34aea89db5ba2fbf674e5ff0ad6fc68a198e
 LIBRETRO_PROSYSTEM_SITE = $(call github,libretro,prosystem-libretro,$(LIBRETRO_PROSYSTEM_VERSION))
 LIBRETRO_PROSYSTEM_LICENSE = GPLv2
 
