c52d412e667514e7e18be0fc2cad802acbf23faa
Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>
2020-01-25 13:38:45 +0100

    batocera-brightness
    Signed-off-by: Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>

--- package/batocera/core/batocera-scripts/batocera-scripts.mk
+++ package/batocera/core/batocera-scripts/batocera-scripts.mk
@@ -41,6 +41,7 @@ define BATOCERA_SCRIPTS_INSTALL_TARGET_CMDS
 	install -m 0755 package/batocera/core/batocera-scripts/scripts/batocera-cores                  $(TARGET_DIR)/usr/bin/
 	install -m 0755 package/batocera/core/batocera-scripts/scripts/batocera-hybrid-nvidia          $(TARGET_DIR)/usr/bin/
 	install -m 0755 package/batocera/core/batocera-scripts/scripts/batocera-wifi                   $(TARGET_DIR)/usr/bin/
+	install -m 0755 package/batocera/core/batocera-scripts/scripts/batocera-brightness             $(TARGET_DIR)/usr/bin/
 	install -m 0755 package/batocera/core/batocera-scripts/scripts/batocera-es-swissknife          $(TARGET_DIR)/usr/bin/
 
 endef
