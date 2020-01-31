649419386342a147c6294f1fe6d4a947310bbd3e
Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>
2020-01-31 06:30:35 +0000

    fix: batocera scripts
    Signed-off-by: Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>

--- package/batocera/core/batocera-scripts/batocera-scripts.mk
+++ package/batocera/core/batocera-scripts/batocera-scripts.mk
@@ -16,7 +16,6 @@ define BATOCERA_SCRIPTS_INSTALL_TARGET_CMDS
 	install -m 0755 package/batocera/core/batocera-scripts/scripts/bluetooth/bluezutils.py            $(TARGET_DIR)/usr/lib/python2.7/ # any variable ?
 	install -m 0755 package/batocera/core/batocera-scripts/scripts/bluetooth/batocera-bluetooth-agent $(TARGET_DIR)/usr/bin/
 	install -m 0755 package/batocera/core/batocera-scripts/scripts/bluetooth/batocera-bt-list-devices $(TARGET_DIR)/usr/bin/
-	install -m 0755 package/batocera/core/batocera-scripts/scripts/bluetooth/batocera-bt-pair-device  $(TARGET_DIR)/usr/bin/
 	install -m 0755 package/batocera/core/batocera-scripts/scripts/bluetooth/batocera-bt-test-device  $(TARGET_DIR)/usr/bin/
 
 	install -m 0755 package/batocera/core/batocera-scripts/scripts/batocera-settings               $(TARGET_DIR)/usr/bin/
