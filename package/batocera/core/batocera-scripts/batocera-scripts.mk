72fd7a7153cc3fac0eb02eeaf71bb33ceb11c52c
Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>
2020-01-31 22:22:11 +0100

    fix bt scripts
    Signed-off-by: Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>

--- package/batocera/core/batocera-scripts/batocera-scripts.mk
+++ package/batocera/core/batocera-scripts/batocera-scripts.mk
@@ -14,10 +14,8 @@ define BATOCERA_SCRIPTS_INSTALL_TARGET_CMDS
 	cp package/batocera/core/batocera-scripts/Mallet.wav           $(TARGET_DIR)/usr/share/sounds
 
 	install -m 0755 package/batocera/core/batocera-scripts/scripts/bluetooth/bluezutils.py            $(TARGET_DIR)/usr/lib/python2.7/ # any variable ?
+	install -m 0755 package/batocera/core/batocera-scripts/scripts/bluetooth/batocera-bluetooth       $(TARGET_DIR)/usr/bin/
 	install -m 0755 package/batocera/core/batocera-scripts/scripts/bluetooth/batocera-bluetooth-agent $(TARGET_DIR)/usr/bin/
-	install -m 0755 package/batocera/core/batocera-scripts/scripts/bluetooth/batocera-bt-list-devices $(TARGET_DIR)/usr/bin/
-	install -m 0755 package/batocera/core/batocera-scripts/scripts/bluetooth/batocera-bt-test-device  $(TARGET_DIR)/usr/bin/
-
 	install -m 0755 package/batocera/core/batocera-scripts/scripts/batocera-settings               $(TARGET_DIR)/usr/bin/
 	install -m 0755 package/batocera/core/batocera-scripts/scripts/batocera-save-overlay           $(TARGET_DIR)/usr/bin/
 	install -m 0755 package/batocera/core/batocera-scripts/scripts/batocera-es-theme               $(TARGET_DIR)/usr/bin/
