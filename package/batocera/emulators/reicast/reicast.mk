7a5ec5d91b985ecc39d36a59425f4edff8ef673f
Juliano Dorigão <jdorigao@gmail.com>
2020-02-09 18:12:33 -0400

    reicast: extra arguments
    Signed-off-by: Juliano Dorigão <jdorigao@gmail.com>

--- package/batocera/emulators/reicast/reicast.mk
+++ package/batocera/emulators/reicast/reicast.mk
@@ -9,13 +9,6 @@ REICAST_SITE = $(call github,reicast,reicast-emulator,$(REICAST_VERSION))
 REICAST_LICENSE = GPLv2
 REICAST_DEPENDENCIES = sdl2 libpng
 
-define REICAST_UPDATE_INCLUDES
-	sed -i "s+/opt/vc+$(STAGING_DIR)/usr+g" $(@D)/shell/linux/Makefile
-	sed -i "s+sdl2-config+$(STAGING_DIR)/usr/bin/sdl2-config+g" $(@D)/shell/linux/Makefile
-endef
-
-REICAST_PRE_CONFIGURE_HOOKS += REICAST_UPDATE_INCLUDES
-
 ifeq ($(BR2_PACKAGE_BATOCERA_TARGET_RPI3),y)
 	BATOCERA_SYSTEM=rpi3
 else ifeq ($(BR2_PACKAGE_BATOCERA_TARGET_RPI2),y)
@@ -39,10 +32,36 @@ else ifeq ($(BR2_PACKAGE_BATOCERA_TARGET_TINKERBOARD)$(BR2_PACKAGE_BATOCERA_TARG
 endif
 
 ifeq ($(BR2_PACKAGE_SDL2_KMSDRM),y)
-	REICAST_EXTRA_ARGS="USE_SDL=1"
+	REICAST_EXTRA_ARGS += USE_SDL=1
 endif
 
-# Sadly the NEON optimizations in the PNG library don't work yet, so disable them
+ifeq ($(BR2_PACKAGE_XORG7),y)
+	REICAST_EXTRA_ARGS += USE_X11=1
+endif
+
+ifeq ($(BR2_PACKAGE_ALSA_LIB),y)
+	REICAST_EXTRA_ARGS += USE_ALSA=1
+endif
+
+ifeq ($(BR2_PACKAGE_PULSEAUDIO),y)
+	REICAST_EXTRA_ARGS += USE_PULSEAUDIO=1
+endif
+
+ifeq ($(BR2_PACKAGE_HAS_LIBGLES),y)
+	REICAST_EXTRA_ARGS += SUPPORT_EGL=1
+endif
+
+ifeq ($(BR2_PACKAGE_HAS_LIBEGL),y)
+	REICAST_EXTRA_ARGS += SUPPORT_EGL=1
+endif
+
+define REICAST_UPDATE_INCLUDES
+	sed -i "s+/opt/vc+$(STAGING_DIR)/usr+g" $(@D)/shell/linux/Makefile
+	sed -i "s+sdl2-config+$(STAGING_DIR)/usr/bin/sdl2-config+g" $(@D)/shell/linux/Makefile
+endef
+
+REICAST_PRE_CONFIGURE_HOOKS += REICAST_UPDATE_INCLUDES
+
 define REICAST_BUILD_CMDS
 	$(TARGET_CONFIGURE_OPTS) $(MAKE) CPP="$(TARGET_CPP)" CXX="$(TARGET_CXX)" \
 		CC="$(TARGET_CC)" AS="$(TARGET_CC)" LD="$(TARGET_CC)" STRIP="$(TARGET_STRIP)" \
