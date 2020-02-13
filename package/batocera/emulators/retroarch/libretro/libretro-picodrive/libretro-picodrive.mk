0199a5909dea03460171f638b75a5ad45e5cf107
Juliano Dorigão <jdorigao@gmail.com>
2020-02-12 21:37:27 -0400

    lr-picodrive: updated package
    Signed-off-by: Juliano Dorigão <jdorigao@gmail.com>

--- package/batocera/emulators/retroarch/libretro/libretro-picodrive/libretro-picodrive.mk
+++ package/batocera/emulators/retroarch/libretro/libretro-picodrive/libretro-picodrive.mk
@@ -11,33 +11,16 @@ LIBRETRO_PICODRIVE_GIT_SUBMODULES=YES
 LIBRETRO_PICODRIVE_DEPENDENCIES = libpng sdl
 LIBRETRO_PICODRIVE_LICENSE = MAME
 
-PICOPLATFORM=$(LIBRETRO_PLATFORM)
-
-ifeq ($(BR2_arm),y)
-  # RPI 0 and 1
-  ifeq ($(BR2_arm1176jzf_s),y)
+ifeq ($(BR2_arm)$(BR2_aarch64),y)
     PICOPLATFORM=$(LIBRETRO_PLATFORM) armasm
-  endif
-
-  # RPI 2 and 3
-  ifeq ($(BR2_cortex_a7),y)
-    PICOPLATFORM=$(LIBRETRO_PLATFORM) armasm
-  endif
-
-  ifeq ($(BR2_cortex_a53),y)
-    PICOPLATFORM=$(LIBRETRO_PLATFORM) armasm
-  endif
-
-  # odroid xu4
-  ifeq ($(BR2_cortex_a15),y)
-    PICOPLATFORM=$(LIBRETRO_PLATFORM) armasm
-  endif
+else
+    PICOPLATFORM=$(LIBRETRO_PLATFORM)
 endif
 
 define LIBRETRO_PICODRIVE_BUILD_CMDS
 	$(MAKE) -C $(@D)/cpu/cyclone CONFIG_FILE=$(@D)/cpu/cyclone_config.h	
-	CFLAGS="$(TARGET_CFLAGS)" CXXFLAGS="$(TARGET_CXXFLAGS)" $(MAKE) CC="$(TARGET_CC)" CXX="$(TARGET_CXX)" \
-    -C  $(@D) -f Makefile.libretro platform="$(PICOPLATFORM)"
+    CFLAGS="$(TARGET_CFLAGS)" CXXFLAGS="$(TARGET_CXXFLAGS)" $(MAKE) CC="$(TARGET_CC)" CXX="$(TARGET_CXX)" \
+        -C  $(@D) -f Makefile.libretro platform="$(PICOPLATFORM)"
 endef
 
 define LIBRETRO_PICODRIVE_INSTALL_TARGET_CMDS
