f0e7ffb20cc7bbee355c6340232b694ac532464e
Juliano Dorigão <jdorigao@gmail.com>
2020-03-01 20:05:59 -0400

    rename: linapple-pie to linapple
    Signed-off-by: Juliano Dorigão <jdorigao@gmail.com>

--- package/batocera/emulators/linapple/linapple.mk
+++ package/batocera/emulators/linapple/linapple.mk
@@ -1,26 +1,26 @@
 ################################################################################
 #
-# lineapple-pie
+# lineapple
 #
 ################################################################################
 # Version.: Commits on Feb 27, 2020
-LINAPPLE_PIE_VERSION = 8ddc3f61dbbaf984ec11e4577544a1fd86f9e7c9
-LINAPPLE_PIE_SITE = $(call github,linappleii,linapple,$(LINAPPLE_PIE_VERSION))
-LINAPPLE_PIE_LICENSE = GPLv2
-LINAPPLE_PIE_DEPENDENCIES = sdl sdl_image libcurl zlib libzip
+LINAPPLE_VERSION = 8ddc3f61dbbaf984ec11e4577544a1fd86f9e7c9
+LINAPPLE_SITE = $(call github,linappleii,linapple,$(LINAPPLE_VERSION))
+LINAPPLE_LICENSE = GPLv2
+LINAPPLE_DEPENDENCIES = sdl sdl_image libcurl zlib libzip
 
 ifeq ($(BR2_PACKAGE_RPI_USERLAND),y)
-	LINAPPLE_PIE_DEPENDENCIES += rpi-userland
+	LINAPPLE_DEPENDENCIES += rpi-userland
 endif
 
-define LINAPPLE_PIE_BUILD_CMDS
+define LINAPPLE_BUILD_CMDS
 	$(SED) "s+/usr/local+$(STAGING_DIR)/usr+g" $(@D)/Makefile	
 	$(TARGET_CONFIGURE_OPTS) $(MAKE) CC="$(TARGET_CXX)" -C $(@D) \
 		SDL_CONFIG=$(STAGING_DIR)/usr/bin/sdl-config \
 		CURL_CONFIG=$(STAGING_DIR)/usr/bin/curl-config
 endef
 
-define LINAPPLE_PIE_INSTALL_TARGET_CMDS
+define LINAPPLE_INSTALL_TARGET_CMDS
 	$(INSTALL) -D $(@D)/bin/linapple $(TARGET_DIR)/usr/bin/
 endef
 
