5a28b168856585204d86a26a6e7ad05145a826df
Juliano Dorigão <jdorigao@gmail.com>
2020-03-11 08:15:57 -0400

    bump: fsuae to 3.0.2

--- package/batocera/emulators/fsuae/fsuae.mk
+++ package/batocera/emulators/fsuae/fsuae.mk
@@ -3,8 +3,8 @@
 # FSUAE
 #
 ################################################################################
-# Version.: Commits on Jul 21, 2019 (3.0.0)
-FSUAE_VERSION = 31239a79b097728aba9ceeef74d2c135cea49e1c
+# Version.: Commits on Mar 09, 2020 (3.0.2)
+FSUAE_VERSION = fb7514f1446000285377b16bba089eeafad98c67
 FSUAE_SITE = $(call github,FrodeSolheim,fs-uae,$(FSUAE_VERSION))
 FSUAE_LICENSE = GPLv2
 FSUAE_DEPENDENCIES = xserver_xorg-server openal libpng sdl2 zlib libmpeg2 libglib2 capsimg
