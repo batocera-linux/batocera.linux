79cc9d014af53bfbb85abc53634e6d904d398f32
Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>
2020-03-16 17:24:47 +0000

    bump rpcs3
    Signed-off-by: Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>

--- package/batocera/emulators/rpcs3/rpcs3.mk
+++ package/batocera/emulators/rpcs3/rpcs3.mk
@@ -4,7 +4,18 @@
 #
 ################################################################################
 
-RPCS3_VERSION = cb66d05693504f4e453999af489786d05d2a8e51
+# last version without gcc-9 and qt bump
+#RPCS3_VERSION = 491526b42185c864883176f5000e144b9ac3c83e
+
+# original
+#RPCS3_VERSION = cb66d05693504f4e453999af489786d05d2a8e51
+
+# 1er january
+#RPCS3_VERSION = 4c20881f8f40f4dfd45ffb3eca94ac206a56e7f9
+
+# 1er december
+RPCS3_VERSION = 1a6e8e20dca9fd259e03c607d4c9d93ae5375298
+
 RPCS3_SITE = https://github.com/RPCS3/rpcs3.git
 RPCS3_SITE_METHOD=git
 RPCS3_GIT_SUBMODULES=YES
