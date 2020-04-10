38c776ba262007a1c116c8cc13b66519e754d958
Juliano Dorigão <jdorigao@gmail.com>
2020-04-10 00:34:02 -0400

    bump: amiberry to v3.1.3

--- package/batocera/emulators/amiberry/amiberry.mk
+++ package/batocera/emulators/amiberry/amiberry.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 # Version.: Commits on Feb 18, 2020
-AMIBERRY_VERSION = v3.1.2
+AMIBERRY_VERSION = v3.1.3
 AMIBERRY_SITE = $(call github,midwan,amiberry,$(AMIBERRY_VERSION))
 AMIBERRY_LICENSE = GPLv3
 AMIBERRY_DEPENDENCIES = sdl2 sdl2_image sdl2_ttf mpg123 libxml2 libmpeg2 flac
