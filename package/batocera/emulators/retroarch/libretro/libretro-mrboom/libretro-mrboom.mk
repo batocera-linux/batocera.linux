8f5d5f8d22befa207bf02bd656daff6471dacd22
Juliano Dorigão <jdorigao@gmail.com>
2020-04-12 12:50:46 -0400

    bump: libretro-mrboom

--- package/batocera/emulators/retroarch/libretro/libretro-mrboom/libretro-mrboom.mk
+++ package/batocera/emulators/retroarch/libretro/libretro-mrboom/libretro-mrboom.mk
@@ -3,14 +3,18 @@
 # MRBOOM
 #
 ################################################################################
-# Version.: Commits on Jan 09, 2020
-LIBRETRO_MRBOOM_VERSION = c777f1059c9a4b3fcefe6e2a19cfe9f81a13740b
-LIBRETRO_MRBOOM_SITE = $(call github,libretro,libretro-mrboom,$(LIBRETRO_MRBOOM_VERSION))
+# Version.: Commits on Apr 2, 2020
+LIBRETRO_MRBOOM_VERSION = b974ce0ae13a8a85835351670d64ca7cb16e7bbc
+LIBRETRO_MRBOOM_SITE = $(call github,libretro,mrboom-libretro,$(LIBRETRO_MRBOOM_VERSION))
 LIBRETRO_MRBOOM_LICENSE="GPLv2"
 
+ifeq ($(BR2_ARM_FPU_NEON_VFPV4)$(BR2_ARM_FPU_NEON)$(BR2_ARM_FPU_NEON_FP_ARMV8),y)
+    LIBRETRO_MRBOOM_EXTRA_ARGS = HAVE_NEON=1
+endif
+
 define LIBRETRO_MRBOOM_BUILD_CMDS
-	CFLAGS="$(TARGET_CFLAGS)" CXXFLAGS="$(TARGET_CXXFLAGS)" $(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" \
-		-C $(@D)/ -f Makefile platform="$(LIBRETRO_PLATFORM)"
+	$(TARGET_CONFIGURE_OPTS) $(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" \
+		-C $(@D)/ -f Makefile platform=unix $(LIBRETRO_MRBOOM_EXTRA_ARGS)
 endef
 
 define LIBRETRO_MRBOOM_INSTALL_TARGET_CMDS
