80c41ad9a4daaf8f226c9abadbf79b3dac899827
Juliano Dorigão <jdorigao@gmail.com>
2020-04-15 01:25:33 -0400

    bios: nec pc-9800

--- package/batocera/emulators/retroarch/libretro/libretro-pc98/libretro-pc98.mk
+++ package/batocera/emulators/retroarch/libretro/libretro-pc98/libretro-pc98.mk
@@ -13,6 +13,7 @@ define LIBRETRO_PC98_BUILD_CMDS
 endef
 
 define LIBRETRO_PC98_INSTALL_TARGET_CMDS
+	mkdir -p $(TARGET_DIR)/usr/share/batocera/datainit/bios/np2kai
 	$(INSTALL) -D $(@D)/sdl2/np2kai_libretro.so \
 		$(TARGET_DIR)/usr/lib/libretro/np2kai_libretro.so
 endef
