b1c42b2f11c3ff6be6269fb5a59dfaf06f6a896e
mistepien <mistepien@wp.pl>
2020-04-17 23:36:45 +0200

    Create libretro-beetle-pce-fast.mk

---
+++ package/batocera/emulators/retroarch/libretro/libretro-beetle-pce-fast/libretro-beetle-pce-fast.mk
@@ -0,0 +1,21 @@
+################################################################################
+#
+# BEETLE_PCE_FAST
+#
+################################################################################
+# Version.: Commits on Jan 30, 2020
+LIBRETRO_BEETLE_PCE_FAST_VERSION = 40a42b7f43f029760c92bf0b2097e7d4b90ed29c
+LIBRETRO_BEETLE_PCE_FAST_SITE = $(call github,libretro,beetle-pce-fast-libretro,$(LIBRETRO_BEETLE_PCE_FAST_VERSION))
+LIBRETRO_BEETLE_PCE_FAST_LICENSE = GPLv2
+
+define LIBRETRO_BEETLE_PCE_FAST_BUILD_CMDS
+        CFLAGS="$(TARGET_CFLAGS)" CXXFLAGS="$(TARGET_CXXFLAGS)" $(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" \
+                LD="$(TARGET_CXX)" AR="$(TARGET_AR)" RANLIB="$(TARGET_RANLIB)" -C $(@D) platform="$(LIBRETRO_PLATFORM)"
+endef
+
+define LIBRETRO_BEETLE_PCE_FAST_INSTALL_TARGET_CMDS
+        $(INSTALL) -D $(@D)/mednafen_pce_fast_libretro.so \
+                $(TARGET_DIR)/usr/lib/libretro/pce_fast_libretro.so
+endef
+
+$(eval $(generic-package))
