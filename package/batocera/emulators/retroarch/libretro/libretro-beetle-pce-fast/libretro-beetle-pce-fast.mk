a4f96a912f3d73187c1464c1859ba9075f80f673
Juliano Dorigão <jdorigao@gmail.com>
2020-04-17 23:20:59 -0400

    lr-beetle-pce: small adjustment

--- package/batocera/emulators/retroarch/libretro/libretro-beetle-pce-fast/libretro-beetle-pce-fast.mk
+++ package/batocera/emulators/retroarch/libretro/libretro-beetle-pce-fast/libretro-beetle-pce-fast.mk
@@ -9,13 +9,12 @@ LIBRETRO_BEETLE_PCE_FAST_SITE = $(call github,libretro,beetle-pce-fast-libretro,
 LIBRETRO_BEETLE_PCE_FAST_LICENSE = GPLv2
 
 define LIBRETRO_BEETLE_PCE_FAST_BUILD_CMDS
-        CFLAGS="$(TARGET_CFLAGS)" CXXFLAGS="$(TARGET_CXXFLAGS)" $(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" \
-                LD="$(TARGET_CXX)" AR="$(TARGET_AR)" RANLIB="$(TARGET_RANLIB)" -C $(@D) platform="$(LIBRETRO_PLATFORM)"
+    $(TARGET_CONFIGURE_OPTS) $(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" -C $(@D) platform="$(LIBRETRO_PLATFORM)"
 endef
 
 define LIBRETRO_BEETLE_PCE_FAST_INSTALL_TARGET_CMDS
-        $(INSTALL) -D $(@D)/mednafen_pce_fast_libretro.so \
-                $(TARGET_DIR)/usr/lib/libretro/pce_fast_libretro.so
+	$(INSTALL) -D $(@D)/mednafen_pce_fast_libretro.so \
+		$(TARGET_DIR)/usr/lib/libretro/pce_fast_libretro.so
 endef
 
 $(eval $(generic-package))
