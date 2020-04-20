2bb6a9328808ca91ac0aa17d91888fd1643c50ab
Juliano Dorigão <jdorigao@gmail.com>
2020-04-20 10:48:23 -0400

    bump: libretro-hatari

--- package/batocera/emulators/retroarch/libretro/libretro-hatari/libretro-hatari.mk
+++ package/batocera/emulators/retroarch/libretro/libretro-hatari/libretro-hatari.mk
@@ -3,8 +3,8 @@
 # HATARI
 #
 ################################################################################
-# Version.: Commits on Sep 23, 2019
-LIBRETRO_HATARI_VERSION = ec1b59c4b6c7ca7d0d23d60cfe2cb61911b11173
+# Version.: Commits on Apr 2, 2020
+LIBRETRO_HATARI_VERSION = f8c35958ec1c93c32d41d61fa4903b2f5daec78c
 LIBRETRO_HATARI_SITE = $(call github,libretro,hatari,$(LIBRETRO_HATARI_VERSION))
 LIBRETRO_HATARI_DEPENDENCIES = libcapsimage
 LIBRETRO_HATARI_LICENSE = GPLv2
@@ -17,11 +17,5 @@ define LIBRETRO_HATARI_INSTALL_TARGET_CMDS
 	$(INSTALL) -D $(@D)/hatari_libretro.so \
 		$(TARGET_DIR)/usr/lib/libretro/hatari_libretro.so
 endef
-
-define LIBRETRO_HATARI_PRE_PATCH_FIXUP
-	$(SED) 's/\r//g' $(@D)/Makefile.libretro
-endef
-
-LIBRETRO_HATARI_PRE_PATCH_HOOKS += LIBRETRO_HATARI_PRE_PATCH_FIXUP
 
 $(eval $(generic-package))
