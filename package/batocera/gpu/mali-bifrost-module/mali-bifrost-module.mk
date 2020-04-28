bc1a4b5ee5e15b499a3b8e7e38ca38e5dd6cb9db
Tiago Pierezan Camargo <camargo@discworld.com.br>
2020-04-28 18:27:00 -0300

    mail-bifrost-module: version bump (r24p0)

--- package/batocera/gpu/mali-bifrost-module/mali-bifrost-module.mk
+++ package/batocera/gpu/mali-bifrost-module/mali-bifrost-module.mk
@@ -3,9 +3,11 @@
 # mali-bifrost-module
 #
 ################################################################################
-# Version.: Commits on Feb 26, 2020
-MALI_BIFROST_MODULE_VERSION = af7c8d8bcdedd792a8d101d3a11876bb8bcbe3da
-MALI_BIFROST_MODULE_SITE = $(call github,LibreELEC,mali-bifrost,$(MALI_BIFROST_MODULE_VERSION))
+# Version: Commits on Apr 14, 2020
+MALI_BIFROST_MODULE_VERSION = 41b59d836a46db69798681674258b512a120b9d1
+# MALI_BIFROST_MODULE_SITE = $(call github,LibreELEC,mali-bifrost,$(MALI_BIFROST_MODULE_VERSION))
+MALI_BIFROST_MODULE_SITE = $(call github,batocera-linux,mali-bifrost,$(MALI_BIFROST_MODULE_VERSION))
+# MALI_BIFROST_MODULE_OVERRIDE_SRCDIR = /sources/mali-bifrost
 
 MALI_BIFROST_MODULE_MODULE_MAKE_OPTS = \
 	CONFIG_MALI_MIDGARD=m \
@@ -13,7 +15,7 @@ MALI_BIFROST_MODULE_MODULE_MAKE_OPTS = \
 	CONFIG_MALI_PLATFORM_POWER_DOWN_ONLY=y \
 	KVER=$(LINUX_VERSION_PROBED) \
 	KSRC=$(LINUX_DIR) \
-        NOSTDINC_FLAGS=-I$(@D)/product/kernel/include
+		NOSTDINC_FLAGS=-I$(@D)/driver/product/kernel/include
 
 MALI_BIFROST_MODULE_MODULE_SUBDIRS = driver/product/kernel/drivers/gpu/arm
 
