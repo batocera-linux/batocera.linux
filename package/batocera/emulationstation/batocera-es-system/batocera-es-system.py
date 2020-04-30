4f6e86684d94ca34950bca4c994186fe36e9a366
lbrpdx <lionel.bringuier@gmail.com>
2020-04-30 10:48:36 -0700

    Add ".sh" into Ports (for 3rd party games/apps)

--- package/batocera/emulationstation/batocera-es-system/batocera-es-system.py
+++ package/batocera/emulationstation/batocera-es-system/batocera-es-system.py
@@ -103,7 +103,7 @@ class EsSystemConf:
         platformValue  = EsSystemConf.systemPlatform(system, data)
         listExtensions = EsSystemConf.listExtension(data, True)
         groupValue     = EsSystemConf.systemGroup(system, data)
-        command        = EsSystemConf.default_command
+        command        = EsSystemConf.commandName(data)
 
         systemTxt =  "  <system>\n"
         systemTxt += "        <fullname>%s</fullname>\n" % (data["name"])
@@ -163,6 +163,13 @@ class EsSystemConf:
         if "theme" in data:
           return data["theme"]
         return system
+
+    # In case you need to specify a different command line 
+    @staticmethod
+    def commandName(data):
+        if "command" in data:
+          return data["command"]
+        return EsSystemConf.default_command
 
     # Create the folders of the consoles in the roms folder
     @staticmethod
