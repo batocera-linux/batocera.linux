0598b39911ab007488fee97aaaba491324179e3a
liberodark <liberodark@gmail.com>
2020-05-05 10:05:48 +0200

    Update post-image-script.sh

--- board/batocera/post-image-script.sh
+++ board/batocera/post-image-script.sh
@@ -233,8 +233,8 @@ case "${BATOCERA_TARGET}" in
 	rm -rf "${BINARIES_DIR:?}/boot"      || exit 1
 	mkdir -p "${BINARIES_DIR}/boot/boot" || exit 1
 	cp "${BOARD_DIR}"/boot/boot-logo.bmp.gz "${BINARIES_DIR}/boot"   || exit 1
-	$MKIMAGE -C none -A arm64 -T script -d "${BOARD_DIR}"/boot/s905_autoscript.txt "${BINARIES_DIR}/boot"/s905_autoscript
-	$MKIMAGE -C none -A arm64 -T script -d "${BOARD_DIR}"/boot/aml_autoscript.txt "${BINARIES_DIR}/boot"/aml_autoscript
+	$MKIMAGE -C none -A arm64 -T script -d "${BOARD_DIR}/boot/s905_autoscript.txt" "${BINARIES_DIR}/boot"/s905_autoscript
+	$MKIMAGE -C none -A arm64 -T script -d "${BOARD_DIR}/boot/aml_autoscript.txt" "${BINARIES_DIR}/boot"/aml_autoscript
 	cp "${BOARD_DIR}"/boot/aml_autoscript.zip "${BINARIES_DIR}/boot"     || exit 1
 	cp "${BINARIES_DIR}/batocera-boot.conf" "${BINARIES_DIR}/boot/batocera-boot.conf" || exit 1
 	cp "${BOARD_DIR}/boot/README.txt" "${BINARIES_DIR}/boot/README.txt" || exit 1
@@ -274,8 +274,8 @@ case "${BATOCERA_TARGET}" in
 	rm -rf "${BINARIES_DIR:?}/boot"      || exit 1
 	mkdir -p "${BINARIES_DIR}/boot/boot" || exit 1
 	cp "${BOARD_DIR}"/boot/boot-logo.bmp.gz "${BINARIES_DIR}/boot"   || exit 1
-	$MKIMAGE -C none -A arm64 -T script -d "${BOARD_DIR}"/boot/s905_autoscript.txt "${BINARIES_DIR}/boot"/s905_autoscript
-	$MKIMAGE -C none -A arm64 -T script -d "${BOARD_DIR}"/boot/aml_autoscript.txt "${BINARIES_DIR}/boot"/aml_autoscript
+	$MKIMAGE -C none -A arm64 -T script -d "${BOARD_DIR}/boot/s905_autoscript.txt" "${BINARIES_DIR}/boot"/s905_autoscript
+	$MKIMAGE -C none -A arm64 -T script -d "${BOARD_DIR}/boot/aml_autoscript.txt" "${BINARIES_DIR}/boot"/aml_autoscript
 	cp "${BOARD_DIR}"/boot/aml_autoscript.zip "${BINARIES_DIR}/boot"     || exit 1
 	cp "${BINARIES_DIR}/batocera-boot.conf" "${BINARIES_DIR}/boot/batocera-boot.conf" || exit 1
 	cp "${BINARIES_DIR}/all_merged.dtb" "${BINARIES_DIR}/dtb.img" || exit 1
