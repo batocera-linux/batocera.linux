aac249141186b439f58d3e6745d974c4f144de6c
Jérémy <xgenetik57x@gmail.com>
2020-05-08 23:31:44 +0200

    GPIO: Update to 1.7.0
    Signed-off-by: Jérémy Del Grosso <genetik57@batocera.org>

--- package/batocera/controllers/mk_arcade_joystick_rpi/mk_arcade_joystick_rpi.mk
+++ package/batocera/controllers/mk_arcade_joystick_rpi/mk_arcade_joystick_rpi.mk
@@ -3,8 +3,8 @@
 # MK_ARCADE_JOYSTICK_RPI
 #
 ################################################################################
-# Version: 1.6.0 Commits on March 09, 2020
-MK_ARCADE_JOYSTICK_RPI_VERSION = e2e10c4bb4088abc8d7cad06dd27ccf8a133af1c
+# Version: 1.7.0 Commits on May 08, 2020
+MK_ARCADE_JOYSTICK_RPI_VERSION = 910dd090459b14d7d1cf186a3dadfd4ff3ec877b
 MK_ARCADE_JOYSTICK_RPI_SITE = $(call github,batocera-linux,mk_arcade_joystick_rpi,$(MK_ARCADE_JOYSTICK_RPI_VERSION))
 MK_ARCADE_JOYSTICK_RPI_DEPENDENCIES = linux
 
