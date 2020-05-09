01457e920009d5f68ac5f40f65b5b93b33eee9fc
Tiago Pierezan Camargo <camargo@discworld.com.br>
2020-05-09 18:00:02 -0300

    vim3: remove unecessary call to mkimage

--- board/batocera/post-image-script.sh
+++ board/batocera/post-image-script.sh
@@ -575,10 +575,9 @@ case "${BATOCERA_TARGET}" in
 	# /boot
 	rm -rf "${BINARIES_DIR}/boot"            || exit 1
 	mkdir -p "${BINARIES_DIR}/boot/boot"     || exit 1
-	"${HOST_DIR}/bin/mkimage" -A arm64 -O linux -T kernel -C none -a 0x1080000 -e 0x1080000  -n batocera.linux -d "${BINARIES_DIR}/Image" "${BINARIES_DIR}/uImage" || exit 1
-	cp "${BINARIES_DIR}/uImage"                "${BINARIES_DIR}/boot/boot/linux"                || exit 1
-	cp "${BINARIES_DIR}/uInitrd"             "${BINARIES_DIR}/boot/boot/uInitrd"                || exit 1
-	cp "${BINARIES_DIR}/rootfs.squashfs"       "${BINARIES_DIR}/boot/boot/batocera.update"      || exit 1
+	cp "${BINARIES_DIR}/Image"                "${BINARIES_DIR}/boot/boot/linux"                || exit 1
+	cp "${BINARIES_DIR}/uInitrd"              "${BINARIES_DIR}/boot/boot/uInitrd"                || exit 1
+	cp "${BINARIES_DIR}/rootfs.squashfs"      "${BINARIES_DIR}/boot/boot/batocera.update"      || exit 1
 	cp "${BINARIES_DIR}/meson-g12b-a311d-khadas-vim3.dtb"  "${BINARIES_DIR}/boot/boot/meson-g12b-a311d-khadas-vim3.dtb" || exit 1
 	cp "${BINARIES_DIR}/batocera-boot.conf"    "${BINARIES_DIR}/boot/batocera-boot.conf"        || exit 1
 	cp "${BR2_EXTERNAL_BATOCERA_PATH}/board/batocera/vim3/boot/extlinux.conf" ${BINARIES_DIR}/boot/boot/extlinux.conf || exit 1
