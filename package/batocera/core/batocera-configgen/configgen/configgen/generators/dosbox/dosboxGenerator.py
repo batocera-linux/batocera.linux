fe115d9d1de1eca2abcb1610d21382f2684cf61d
Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>
2020-05-10 22:20:00 +0200

    fix dosbox-x
    Signed-off-by: Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>

--- package/batocera/core/batocera-configgen/configgen/configgen/generators/dosbox/dosboxGenerator.py
+++ package/batocera/core/batocera-configgen/configgen/configgen/generators/dosbox/dosboxGenerator.py
@@ -4,31 +4,43 @@ import batoceraFiles
 from generators.Generator import Generator
 import os.path
 import glob
-
+import ConfigParser
 
 class DosBoxGenerator(Generator):
 
-    def getResolutionMode(self, config):
-        return 'default'
-    
-    # Main entry of the module
-    # Return command
     def generate(self, system, rom, playersControllers, gameResolution):
         # Find rom path
         gameDir = rom
         batFile = gameDir + "/dosbox.bat"
         gameConfFile = gameDir + "/dosbox.cfg"
-           
-        commandArray = [batoceraFiles.batoceraBins[system.config['emulator']], 
-			"-userconf", 
-			"-exit", 
-			"""{}""".format(batFile),
-			"-c", """set ROOT={}""".format(gameDir)]
+
+        configFile = batoceraFiles.dosboxConfig
         if os.path.isfile(gameConfFile):
-            commandArray.append("-conf")
-            commandArray.append("""{}""".format(gameConfFile))
-        else:
-            commandArray.append("-conf")
-            commandArray.append("""{}""".format(batoceraFiles.dosboxConfig))
+            configFile = gameConfFile
+
+        # configuration file
+        iniSettings = ConfigParser.ConfigParser()
+        # To prevent ConfigParser from converting to lower case
+        iniSettings.optionxform = str
+        if os.path.exists(configFile):
+            iniSettings.read(configFile)
+
+        # sections
+        if not iniSettings.has_section("sdl"):
+            iniSettings.add_section("sdl")
+        iniSettings.set("sdl", "output", "opengl")
+
+        # save
+        with open(configFile, 'w') as config:
+            iniSettings.write(config)
+
+        commandArray = ["/usr/bin/dosbox-x",
+			"-exit", 
+			"-c", """mount c {}""".format(gameDir),
+                        "-c", "c:",
+                        "-c", "dosbox.bat",
+                        "-fastbioslogo",
+                        "-fullscreen",
+                        "-conf {}".format(configFile)]
 
         return Command.Command(array=commandArray)
