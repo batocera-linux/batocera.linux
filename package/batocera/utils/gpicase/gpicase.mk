f12d29782b1db5874793334cb335e8b7de527328
Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>
2020-05-12 11:04:01 +0000

    fix gpicase
    Signed-off-by: Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>

--- package/batocera/utils/gpicase/gpicase.mk
+++ package/batocera/utils/gpicase/gpicase.mk
@@ -11,8 +11,8 @@ define GPICASE_INSTALL_TARGET_CMDS
 	mkdir -p $(BINARIES_DIR)/rpi-firmware/overlays
 	cp $(@D)/GPi_Case_patch/patch_files/overlays/dpi24.dtbo             $(BINARIES_DIR)/rpi-firmware/overlays/dpi24_gpicase.dtbo
 	cp $(@D)/GPi_Case_patch/patch_files/overlays/pwm-audio-pi-zero.dtbo $(BINARIES_DIR)/rpi-firmware/overlays/pwm-audio-pi-zero_gpicase.dtbo
-	cp package/batocera/utils/gpicase/99-gpicase.rules                  $(TARGET_DIR)/etc/udev/rules.d
-	cp package/batocera/utils/gpicase/batocera-gpicase-install          $(TARGET_DIR)/usr/bin/batocera-gpicase-install
+	cp $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/utils/gpicase/99-gpicase.rules                  $(TARGET_DIR)/etc/udev/rules.d
+	cp $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/utils/gpicase/batocera-gpicase-install          $(TARGET_DIR)/usr/bin/batocera-gpicase-install
 endef
 
 define GPICASE_EXTRACT_CMDS
