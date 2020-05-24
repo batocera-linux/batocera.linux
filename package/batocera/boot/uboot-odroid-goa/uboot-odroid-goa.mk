1a448d617284538062aa547dd1f495902d83e4b4
Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>
2020-05-24 20:15:26 +0000

    uboot for v11
    Signed-off-by: Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>

--- package/batocera/boot/uboot-odroid-goa/uboot-odroid-goa.mk
+++ package/batocera/boot/uboot-odroid-goa/uboot-odroid-goa.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-UBOOT_ODROID_GOA_VERSION = travis/odroidgo2-116
+UBOOT_ODROID_GOA_VERSION = e7b255b54c42c8a805dee7a9409a8838cfa13586
 UBOOT_ODROID_GOA_SITE = https://github.com/hardkernel/u-boot.git
 UBOOT_ODROID_GOA_SITE_METHOD=git
 
