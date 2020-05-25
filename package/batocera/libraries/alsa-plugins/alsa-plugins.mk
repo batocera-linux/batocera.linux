8b4a1daf969be0a4dc17b3904d4b1bf945527282
Tiago Pierezan Camargo <camargo@discworld.com.br>
2020-05-25 08:10:32 -0300

    alsa-plugins: new package
    Needed for pulseaudio support.

---
+++ package/batocera/libraries/alsa-plugins/alsa-plugins.mk
@@ -0,0 +1,19 @@
+#############################################################
+#
+# alsa-plugins
+#
+#############################################################
+ALSA_PLUGINS_VERSION = 1.2.1
+ALSA_PLUGINS_SOURCE = alsa-plugins-$(ALSA_PLUGINS_VERSION).tar.bz2
+ALSA_PLUGINS_SITE = ftp://ftp.alsa-project.org/pub/plugins
+ALSA_PLUGINS_INSTALL_STAGING = NO
+ALSA_PLUGINS_DEPENDENCIES = alsa-lib libsamplerate
+
+ALSA_PLUGINS_CONF_OPTS += --with-plugindir=/usr/lib/alsa \
+	--localstatedir=/var \
+	--disable-jack \
+	--enable-samplerate \
+	--disable-avcodec \
+	--with-speex=builtin
+
+$(eval $(autotools-package))
