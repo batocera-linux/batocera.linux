d58a3f2791cc7f7be127fe03d72ebfb90eadd35a
Tiago Pierezan Camargo <camargo@discworld.com.br>
2020-06-10 13:35:22 -0300

    batocera-audio: rework pulseaudio init scripts

--- package/batocera/core/batocera-audio/batocera-audio.mk
+++ package/batocera/core/batocera-audio/batocera-audio.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-BATOCERA_AUDIO_VERSION = 1.4
+BATOCERA_AUDIO_VERSION = 1.5
 BATOCERA_AUDIO_LICENSE = GPL
 BATOCERA_AUDIO_DEPENDENCIES = alsa-lib
 BATOCERA_AUDIO_SOURCE=
@@ -23,14 +23,14 @@ else ifeq ($(BR2_PACKAGE_BATOCERA_AUDIO_NONE),y)
 	BATOCERA_AUDIO_SCRIPT=none
 else
 	# pulseaudio
-	BATOCERA_AUDIO_DEPENDENCIES += pulseaudio
-	BATOCERA_AUDIO_SCRIPT=pulseaudio alsa-plugins
+	BATOCERA_AUDIO_DEPENDENCIES += pulseaudio alsa-plugins
+	BATOCERA_AUDIO_SCRIPT=pulseaudio
 	BATOCERA_AUDIO_POST_INSTALL_TARGET_HOOKS += BATOCERA_AUDIO_INSTALL_AUDIO_PULSEAUDIO
 endif
 
 define BATOCERA_AUDIO_INSTALL_TARGET_CMDS
 	mkdir -p $(TARGET_DIR)/usr/lib/python2.7 $(TARGET_DIR)/usr/bin $(TARGET_DIR)/usr/share/sounds
-	cp $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/core/batocera-audio/Mallet.wav           $(TARGET_DIR)/usr/share/sounds
+	cp $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/core/batocera-audio/Mallet.wav $(TARGET_DIR)/usr/share/sounds
 	install -m 0755 $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/core/batocera-audio/batocera-audio-$(BATOCERA_AUDIO_SCRIPT) $(TARGET_DIR)/usr/bin/batocera-audio
 endef
 
@@ -39,7 +39,11 @@ define BATOCERA_AUDIO_INSTALL_AUDIO_DMIX
 endef
 
 define BATOCERA_AUDIO_INSTALL_AUDIO_PULSEAUDIO
-	install -m 0755 $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/core/batocera-audio/S15audio $(TARGET_DIR)/etc/init.d/S15audio
+	# init script
+	install -m 0755 $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/core/batocera-audio/pulseaudio/S15audio    $(TARGET_DIR)/etc/init.d/S15audio
+	# override  conf files
+	install -m 0644 $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/core/batocera-audio/pulseaudio/client.conf $(TARGET_DIR)/etc/pulse/client.conf
+	install -m 0644 $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/core/batocera-audio/pulseaudio/default.pa  $(TARGET_DIR)/etc/pulse/default.pa
 endef
 
 $(eval $(generic-package))
