06faa366f5f3b5557cd6d467ab8b9e63cf8f3ae7
Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>
2020-06-15 20:36:27 +0000

    bump firmwares
    Signed-off-by: Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>

--- package/batocera/alllinuxfirmwares/alllinuxfirmwares.mk
+++ package/batocera/alllinuxfirmwares/alllinuxfirmwares.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 # Version from 2020-04-14 10:37 - amdgpu: update vega20 to the latest 19.50 firmware
-ALLLINUXFIRMWARES_VERSION = 64dba0fedb22eae32f76dcd4534b3f416db178de
+ALLLINUXFIRMWARES_VERSION = 78c0348458c035cf1de6093555db5431cc8c1268
 ALLLINUXFIRMWARES_SITE = http://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git
 ALLLINUXFIRMWARES_SITE_METHOD = git
 
