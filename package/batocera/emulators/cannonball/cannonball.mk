3037dd93c8ef4e88d3feaad6f5d705777571a7ea
Romain Tisserand <romain.tisserand@gmail.com>
2020-06-17 14:50:24 +0200

    Working cannonball package

--- package/batocera/emulators/cannonball/cannonball.mk
+++ package/batocera/emulators/cannonball/cannonball.mk
@@ -16,8 +16,20 @@ CANNONBALL_CONF_OPTS += -DCMAKE_BUILD_TYPE=Release -DTARGET=$(CANNONBALL_TARGET)
 CANNONBALL_SUPPORTS_IN_SOURCE_BUILD = NO
 
 define CANNONBALL_SETUP_CMAKE
-	cd $(@D)
+	#cd $(@D)
 	cp $(@D)/cmake/* $(@D)/
+	#mkdir -p $(@D)/buildroot-build
+	#cd $(@D)/buildroot-build/
+	#cp $(@D)/res/* $(@D)/buildroot-build/res/
+	#$(SED) "s+../res+\${CMAKE_CURRENT_BUILD_DIR}/res+g" $(@D)/CMakeLists.txt
+	$(SED) "s+../res/config+\./res/config+g" $(@D)/CMakeLists.txt
+	$(SED) "s+../src/main+\./src/main+g" $(@D)/CMakeLists.txt
+	$(SED) "s+../res/tilemap.bin+\./res/tilemap.bin +g" $(@D)/CMakeLists.txt
+	$(SED) "s+../res/tilepatch.bin+\./res/tilepatch.bin +g" $(@D)/CMakeLists.txt
+        $(SED) "s+/usr+$(STAGING_DIR)/usr+g" $(@D)/CMakeLists.txt
+        $(SED) "s+/usr+$(STAGING_DIR)/usr+g" $(@D)/sdl2.cmake
+        $(SED) "s+/usr+$(STAGING_DIR)/usr+g" $(@D)/sdl2gl.cmake
+        $(SED) "s+/usr+$(STAGING_DIR)/usr+g" $(@D)/sdl2gles.cmake
 endef
 
 CANNONBALL_PRE_CONFIGURE_HOOKS += CANNONBALL_SETUP_CMAKE
@@ -26,7 +38,6 @@ CANNONBALL_PRE_CONFIGURE_HOOKS += CANNONBALL_SETUP_CMAKE
 	#cd $(@D)
 	#cp cmake/* .
 	#$(TARGET_CONFIGURE_OPTS) $(MAKE) -G "Unix Makefiles" $(CANNONBALL_CONF_OPTS)
-	#$(SED) "s+/usr/local+$(STAGING_DIR)/usr+g" $(@D)/Makefile
 	#$(TARGET_CONFIGURE_OPTS) $(MAKE) CC="$(TARGET_CXX)" -C $(@D) \
 	#	SDL_CONFIG=$(STAGING_DIR)/usr/bin/sdl-config \
 	#	CURL_CONFIG=$(STAGING_DIR)/usr/bin/curl-config \
@@ -34,7 +45,11 @@ CANNONBALL_PRE_CONFIGURE_HOOKS += CANNONBALL_SETUP_CMAKE
 #endef
 
 define CANNONBALL_INSTALL_TARGET_CMDS
-	$(INSTALL) -D $(@D)/build/cannonball $(TARGET_DIR)/usr/bin/
+	$(INSTALL) -D $(@D)/buildroot-build/cannonball $(TARGET_DIR)/usr/bin/
+	mkdir -p $(TARGET_DIR)/usr/share/cannonball/res/
+	$(INSTALL) -D $(@D)/buildroot-build/config.xml $(TARGET_DIR)/usr/share/cannonball/
+	$(INSTALL) -D $(@D)/buildroot-build/res/tilemap.bin $(TARGET_DIR)/usr/share/cannonball/res/
+	$(INSTALL) -D $(@D)/buildroot-build/res/tilepatch.bin $(TARGET_DIR)/usr/share/cannonball/res/
 endef
 
 $(eval $(cmake-package))
