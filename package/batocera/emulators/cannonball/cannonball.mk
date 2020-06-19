6601b00644e5bd3097aa6984f844a8ac18b12070
Romain Tisserand <romain.tisserand@gmail.com>
2020-06-19 15:26:30 +0200

    Set proper folders for cannonball

--- package/batocera/emulators/cannonball/cannonball.mk
+++ package/batocera/emulators/cannonball/cannonball.mk
@@ -3,9 +3,9 @@
 # cannonball
 #
 ################################################################################
-# Version.: Commits on Oct 19, 2019
-CANNONBALL_VERSION = b6aa525ddd552f96b43b3b3a6f69326a277206bd
-CANNONBALL_SITE = $(call github,djyt,cannonball,$(CANNONBALL_VERSION))
+# Version.: Commits on Jun 19, 2020
+CANNONBALL_VERSION = 61351c3393e1cd30c995d899469fbb90eacb5d6a
+CANNONBALL_SITE = $(call github,rtissera,cannonball,$(CANNONBALL_VERSION))
 CANNONBALL_LICENSE = GPLv2
 CANNONBALL_DEPENDENCIES = sdl2 boost
 
@@ -21,17 +21,18 @@ else ifeq ($(BR2_PACKAGE_BATOCERA_TARGET_X86_64),y)
         CANNONBALL_TARGET = sdl2gl
 endif
 
+# Build as release with proper target and paths
+CANNONBALL_CONF_OPTS += -DCMAKE_BUILD_TYPE=Release -DTARGET=$(CANNONBALL_TARGET)
+CANNONBALL_CONF_OPTS += -Droms_directory=/userdata/roms/cannonball/ -Dxml_directory=/userdata/system/configs/cannonball/
+
 # Enabling LTO as hires mode tends to be slow, it does help video rendering loops
-CANNONBALL_CONF_OPTS += -DCMAKE_BUILD_TYPE=Release -DTARGET=$(CANNONBALL_TARGET) -DCMAKE_CXX_FLAGS=-flto -DCMAKE_EXE_LINKER_FLAGS=-flto -DCMAKE_SHARED_LINKER_FLAGS=-flto
+CANNONBALL_CONF_OPTS += -DCMAKE_CXX_FLAGS=-flto -DCMAKE_EXE_LINKER_FLAGS=-flto -DCMAKE_SHARED_LINKER_FLAGS=-flto
+
+# We need to build out-of-tree
 CANNONBALL_SUPPORTS_IN_SOURCE_BUILD = NO
 
 define CANNONBALL_SETUP_CMAKE
-	#cd $(@D)
 	cp $(@D)/cmake/* $(@D)/
-	#mkdir -p $(@D)/buildroot-build
-	#cd $(@D)/buildroot-build/
-	#cp $(@D)/res/* $(@D)/buildroot-build/res/
-	#$(SED) "s+../res+\${CMAKE_CURRENT_BUILD_DIR}/res+g" $(@D)/CMakeLists.txt
 	$(SED) "s+../res/config+\./res/config+g" $(@D)/CMakeLists.txt
 	$(SED) "s+../src/main+\./src/main+g" $(@D)/CMakeLists.txt
 	$(SED) "s+../res/tilemap.bin+\./res/tilemap.bin +g" $(@D)/CMakeLists.txt
