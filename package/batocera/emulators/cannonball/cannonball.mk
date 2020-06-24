e75f36bc3f7be4bb7c7fdb15b1df203cb76b0647
Romain Tisserand <romain.tisserand@gmail.com>
2020-06-24 22:37:35 +0200

    Make cannonball paths more configurable

--- package/batocera/emulators/cannonball/cannonball.mk
+++ package/batocera/emulators/cannonball/cannonball.mk
@@ -3,8 +3,8 @@
 # cannonball
 #
 ################################################################################
-# Version.: Commits on Jun 19, 2020
-CANNONBALL_VERSION = 61351c3393e1cd30c995d899469fbb90eacb5d6a
+# Version.: Commits on Jun 24, 2020
+CANNONBALL_VERSION = 8aa7322c2a23d50def7613a2e55c683dc78418b9
 CANNONBALL_SITE = $(call github,rtissera,cannonball,$(CANNONBALL_VERSION))
 CANNONBALL_LICENSE = GPLv2
 CANNONBALL_DEPENDENCIES = sdl2 boost
@@ -23,7 +23,7 @@ endif
 
 # Build as release with proper target and paths
 CANNONBALL_CONF_OPTS += -DCMAKE_BUILD_TYPE=Release -DTARGET=$(CANNONBALL_TARGET)
-CANNONBALL_CONF_OPTS += -Droms_directory=/userdata/roms/cannonball/ -Dxml_directory=/userdata/system/configs/cannonball/
+CANNONBALL_CONF_OPTS += -Droms_directory=/userdata/roms/cannonball/ -Dxml_directory=/userdata/system/configs/cannonball/ -Dres_directory=/userdata/system/configs/cannonball/
 
 # Enabling LTO as hires mode tends to be slow, it does help video rendering loops
 CANNONBALL_CONF_OPTS += -DCMAKE_CXX_FLAGS=-flto -DCMAKE_EXE_LINKER_FLAGS=-flto -DCMAKE_SHARED_LINKER_FLAGS=-flto
@@ -42,10 +42,10 @@ define CANNONBALL_SETUP_CMAKE
         $(SED) "s+/usr+$(STAGING_DIR)/usr+g" $(@D)/sdl2gl.cmake
         $(SED) "s+/usr+$(STAGING_DIR)/usr+g" $(@D)/sdl2gles.cmake
         $(SED) "s+/usr+$(STAGING_DIR)/usr+g" $(@D)/sdl2gles_rpi.cmake
-        $(SED) "s+set(xml+#set(xml+g" $(@D)/sdl2.cmake
-        $(SED) "s+set(xml+#set(xml+g" $(@D)/sdl2gl.cmake
-        $(SED) "s+set(xml+#set(xml+g" $(@D)/sdl2gles.cmake
-        $(SED) "s+set(xml+#set(xml+g" $(@D)/sdl2gles_rpi.cmake
+        #$(SED) "s+set(xml+#set(xml+g" $(@D)/sdl2.cmake
+        #$(SED) "s+set(xml+#set(xml+g" $(@D)/sdl2gl.cmake
+        #$(SED) "s+set(xml+#set(xml+g" $(@D)/sdl2gles.cmake
+        #$(SED) "s+set(xml+#set(xml+g" $(@D)/sdl2gles_rpi.cmake
 endef
 
 CANNONBALL_PRE_CONFIGURE_HOOKS += CANNONBALL_SETUP_CMAKE
