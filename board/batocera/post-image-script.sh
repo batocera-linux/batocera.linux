26277d7680a56d9d9c253c766346e4804b706ab6
Ntemis <ierokipides.dem@gmail.com>
2020-06-24 23:15:36 +0300

    H5: add batocera post-image-script and fix remaining issues

--- board/batocera/post-image-script.sh
+++ board/batocera/post-image-script.sh
@@ -611,6 +611,45 @@ case "${BATOCERA_TARGET}" in
 	sync || exit 1
 	;;
 
+	LIBRETECH_H5)
+	MKIMAGE=${HOST_DIR}/bin/mkimage
+	BOARD_DIR="${BR2_EXTERNAL_BATOCERA_PATH}/board/batocera/libretech-h5"
+	# boot
+	rm -rf "${BINARIES_DIR}/boot"            || exit 1
+	mkdir -p "${BINARIES_DIR}/boot/boot"     || exit 1
+	mkdir -p "${BINARIES_DIR}/boot/extlinux" || exit 1
+	#$MKIMAGE -C none -A arm64 -T script -d "${BOARD_DIR}/boot/boot.cmd" "${BINARIES_DIR}/boot/boot/boot.scr"
+    cp "${BINARIES_DIR}/Image"                 "${BINARIES_DIR}/boot/boot/linux"                || exit 1
+    cp "${BINARIES_DIR}/initrd.gz"             "${BINARIES_DIR}/boot/boot/initrd.gz"            || exit 1
+    cp "${BINARIES_DIR}/rootfs.squashfs"       "${BINARIES_DIR}/boot/boot/batocera.update"      || exit 1
+    cp "${BINARIES_DIR}/sun50i-h5-libretech-all-h5-cc.dtb"  "${BINARIES_DIR}/boot/boot/sun50i-h5-libretech-all-h5-cc.dtb" || exit 1
+    cp "${BINARIES_DIR}/batocera-boot.conf"    "${BINARIES_DIR}/boot/batocera-boot.conf"        || exit 1
+    cp "${BOARD_DIR}/boot/extlinux.conf" "${BINARIES_DIR}/boot/extlinux"                   || exit 1
+	cp -pr "${BINARIES_DIR}/tools"       "${BINARIES_DIR}/boot/"                || exit 1
+
+	# boot.tar.xz
+	echo "creating boot.tar.xz"
+	(cd "${BINARIES_DIR}/boot" && tar -I "xz -T0" -cf "${BATOCERA_BINARIES_DIR}/boot.tar.xz" extlinux tools boot batocera-boot.conf) || exit 1
+
+	# blobs
+	for F in u-boot-sunxi-with-spl.bin
+	do
+		cp "${BINARIES_DIR}/${F}" "${BINARIES_DIR}/boot/${F}" || exit 1
+	done
+
+	# batocera.img
+	mv "${BINARIES_DIR}/boot/boot/batocera.update" "${BINARIES_DIR}/boot/boot/batocera" || exit 1
+	GENIMAGE_TMP="${BUILD_DIR}/genimage.tmp"
+	BATOCERAIMG="${BATOCERA_BINARIES_DIR}/batocera.img"
+	rm -rf "${GENIMAGE_TMP}" || exit 1
+	cp "${BR2_EXTERNAL_BATOCERA_PATH}/board/batocera/libretech-h5/genimage.cfg" "${BINARIES_DIR}" || exit 1
+	echo "generating image"
+	genimage --rootpath="${TARGET_DIR}" --inputpath="${BINARIES_DIR}/boot" --outputpath="${BATOCERA_BINARIES_DIR}" --config="${BINARIES_DIR}/genimage.cfg" --tmppath="${GENIMAGE_TMP}" || exit 1
+	rm -f "${BATOCERA_BINARIES_DIR}/boot.vfat" || exit 1
+	rm -f "${BATOCERA_BINARIES_DIR}/userdata.ext4" || exit 1
+	sync || exit 1
+	;;
+
 	*)
 	echo "Outch. Unknown target ${BATOCERA_TARGET} (see copy-batocera-archives.sh)" >&2
 	bash
