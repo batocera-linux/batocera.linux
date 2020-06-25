c59dbc127f5bde209e5e8dbe358ff0107b6d839e
Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>
2020-06-25 17:56:23 +0000

    fix rpi bt wifi
    Signed-off-by: Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>

--- package/batocera/alllinuxfirmwares/alllinuxfirmwares.mk
+++ package/batocera/alllinuxfirmwares/alllinuxfirmwares.mk
@@ -10,7 +10,9 @@ ALLLINUXFIRMWARES_SITE_METHOD = git
 
 define ALLLINUXFIRMWARES_INSTALL_TARGET_CMDS
 	mkdir -p $(TARGET_DIR)/lib/firmware
-	cp -pr $(@D)/* $(TARGET_DIR)/lib/firmware/
+	# -n is mandatory while some other packages provides firmwares too
+	# this is not ideal, but i don't know how to tell to buildroot to install this package first (and not worry about all packages installing firmwares)
+	cp -prn $(@D)/* $(TARGET_DIR)/lib/firmware/
 endef
 
 $(eval $(generic-package))
