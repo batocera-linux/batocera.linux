34e5b7e846527a5ea71351e3400044c684f7a7ef
MrFixIt2001 <mrfixit2001@gmail.com>
2020-06-28 21:40:22 -0400

    mupen64plus rockchip defaults Improve default settings for mupen64plus standalone Also minor adjustments to mupen64plus core package

--- package/batocera/emulators/mupen64plus/mupen64plus-core/mupen64plus-core.mk
+++ package/batocera/emulators/mupen64plus/mupen64plus-core/mupen64plus-core.mk
@@ -27,7 +27,11 @@ ifeq ($(BR2_PACKAGE_RPI_USERLAND),y)
 endif
 
 ifeq ($(BR2_arm),y)
-	MUPEN64PLUS_HOST_CPU = armv7
+	ifeq ($(BR2_ARM_CPU_ARMV8A),y)
+		MUPEN64PLUS_HOST_CPU = armv8
+	else
+		MUPEN64PLUS_HOST_CPU = armv7
+	endif
 	MUPEN64PLUS_PARAMS += VFP_HARD=1
 endif
 
@@ -41,13 +45,13 @@ ifeq ($(BR2_ARM_CPU_HAS_NEON),y)
 	MUPEN64PLUS_GL_CFLAGS += -D__ARM_NEON__ -D__NEON_OPT -ftree-vectorize -mvectorize-with-neon-quad -ftree-vectorizer-verbose=2 -funsafe-math-optimizations -fno-finite-math-only
 
 	ifeq ($(BR2_ARM_CPU_HAS_VFPV4),y)
-	MUPEN64PLUS_CORE_CPUFLAGS += -mfpu=neon-vfpv4 
+		MUPEN64PLUS_CORE_CPUFLAGS += -mfpu=neon-vfpv4 
 	else
-	MUPEN64PLUS_CORE_CPUFLAGS += -mfpu=neon
+		MUPEN64PLUS_CORE_CPUFLAGS += -mfpu=neon
 	endif
 
 	ifeq ($(BR2_GCC_TARGET_FLOAT_ABI),"hard")
-	MUPEN64PLUS_CORE_CPUFLAGS += -mfloat-abi=hard 
+		MUPEN64PLUS_CORE_CPUFLAGS += -mfloat-abi=hard 
 	endif
 
 	MUPEN64PLUS_PARAMS += NEON=1 CPUFLAGS="$(MUPEN64PLUS_CORE_CPUFLAGS)"
@@ -62,14 +66,14 @@ ifeq ($(BR2_PACKAGE_BATOCERA_TARGET_X86_64),y)
 endif
 
 ifeq ($(BR2_PACKAGE_BATOCERA_TARGET_ROCKCHIP_ANY),y)
-MUPEN64PLUS_PARAMS += ARCH=arm NEW_DYNAREC=1 VFP=1 CRC_OPT=1 TRIBUFFER_OPT=1 NO_SSE=1 PIC=1 USE_FRAMESKIPPER=1
-MUPEN64PLUS_GL_CFLAGS += -I$(STAGING_DIR)/usr/include/libdrm -ldrm -fpermissive -DEGL_NO_X11=1 -DMESA_EGL_NO_X11_HEADERS=1
+	MUPEN64PLUS_PARAMS += ARCH=arm NEW_DYNAREC=1 VFP=1 CRC_OPT=1 TRIBUFFER_OPT=1 NO_SSE=1 PIC=1 USE_FRAMESKIPPER=1
+	MUPEN64PLUS_GL_CFLAGS += -I$(STAGING_DIR)/usr/include/libdrm -ldrm -fpermissive -DEGL_NO_X11=1 -DMESA_EGL_NO_X11_HEADERS=1
 endif
 
 define MUPEN64PLUS_CORE_BUILD_CMDS
 	$(TARGET_CONFIGURE_OPTS) $(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" \
 		CROSS_COMPILE="$(STAGING_DIR)/usr/bin/" \
-        AS="$(HOST_DIR)/bin/nasm" \
+		AS="$(HOST_DIR)/bin/nasm" \
 		PREFIX="$(STAGING_DIR)/usr" \
 		PKG_CONFIG="$(HOST_DIR)/usr/bin/pkg-config" \
 		HOST_CPU="$(MUPEN64PLUS_HOST_CPU)" \
@@ -105,7 +109,6 @@ define MUPEN64PLUS_CORE_CROSS_FIXUP
 	$(SED) 's|/opt/vc/include|$(STAGING_DIR)/usr/include|g' $(@D)/projects/unix/Makefile
 	$(SED) 's|/opt/vc/lib|$(STAGING_DIR)/usr/lib|g' $(@D)/projects/unix/Makefile
 endef
-
 MUPEN64PLUS_CORE_PRE_CONFIGURE_HOOKS += MUPEN64PLUS_CORE_CROSS_FIXUP
 
 $(eval $(generic-package))
