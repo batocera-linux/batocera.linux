3442627550fe931863446abd32f23e0d08179086
Romain Tisserand <romain.tisserand@gmail.com>
2020-07-02 14:03:07 +0200

    Use batocera-linux github for wine-x86

--- package/batocera/emulators/wine-x86/wine-x86.mk
+++ package/batocera/emulators/wine-x86/wine-x86.mk
@@ -7,7 +7,7 @@
 # version 5.18 means binary from 5.18 version (or the last built if the version is not yet out)
 WINE_X86_VERSION = $(BATOCERA_SYSTEM_VERSION)
 WINE_X86_SOURCE = wine-x86-$(WINE_X86_VERSION).tar.gz
-WINE_X86_SITE = https://github.com/rtissera/wine-x86/releases/download/$(WINE_X86_VERSION)
+WINE_X86_SITE = https://github.com/batocera-linux/wine-x86/releases/download/$(WINE_X86_VERSION)
 
 # Wine Mono addon (required)
 WINE_MONO_VERSION = 4.9.4
