a3b866a8a37297c6a2c4a49f0f40a288afbd1eeb
Tiago Pierezan Camargo <camargo@discworld.com.br>
2020-07-04 20:27:10 -0300

    batocera-configgen: bump

--- package/batocera/core/batocera-configgen/batocera-configgen.mk
+++ package/batocera/core/batocera-configgen/batocera-configgen.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-BATOCERA_CONFIGGEN_VERSION = 1.2
+BATOCERA_CONFIGGEN_VERSION = 1.3
 BATOCERA_CONFIGGEN_LICENSE = GPL
 BATOCERA_CONFIGGEN_SOURCE=
 BATOCERA_CONFIGGEN_DEPENDENCIES = python python-pyyaml
