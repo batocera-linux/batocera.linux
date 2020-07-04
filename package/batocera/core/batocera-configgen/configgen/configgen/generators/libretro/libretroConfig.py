9be2d8d3a913479d6e874e79f130589fb30b4d23
Tiago Pierezan Camargo <camargo@discworld.com.br>
2020-07-04 20:24:10 -0300

    batocera-configgen: force alsa

--- package/batocera/core/batocera-configgen/configgen/configgen/generators/libretro/libretroConfig.py
+++ package/batocera/core/batocera-configgen/configgen/configgen/generators/libretro/libretroConfig.py
@@ -68,6 +68,8 @@ def createLibretroConfig(system, controllers, rom, bezel, gameResolution):
     retroarchConfig['video_driver'] = '"gl"'                 # needed for the ozone menu
     retroarchConfig['video_black_frame_insertion'] = 'false' # don't use anymore this value while it doesn't allow the shaders to work
     retroarchConfig['pause_nonactive'] = 'false'             # required at least on x86 x86_64 otherwise, the game is paused at launch
+    retroarchConfig['audio_driver'] = 'alsa'                 # force ALSA. TODO: check audio.backend
+    retroarchConfig['midi_driver'] = 'alsa'
     retroarchConfig['cache_directory'] = '/userdata/system/.cache'
 
     # fs is required at least for x86* and odroidn2
