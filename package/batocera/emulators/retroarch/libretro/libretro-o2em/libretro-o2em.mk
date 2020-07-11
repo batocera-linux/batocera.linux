fe93a84da4350439f9c0ce968b31ccdbd51ad3bc
Juliano Dorigão <jdorigao@gmail.com>
2020-07-11 19:34:28 -0400

    bump: libretro-o2em

--- package/batocera/emulators/retroarch/libretro/libretro-o2em/libretro-o2em.mk
+++ package/batocera/emulators/retroarch/libretro/libretro-o2em/libretro-o2em.mk
@@ -3,8 +3,8 @@
 # O2EM
 #
 ################################################################################
-# Version.: Commits on May 22, 2020
-LIBRETRO_O2EM_VERSION = 31a60bb551e9d72b66dab0c373a61e185cd25459
+# Version.: Commits on Jun 09, 2020
+LIBRETRO_O2EM_VERSION = 88592cfcf1d9e940f87b99216473b2154cba7450
 LIBRETRO_O2EM_SITE = $(call github,libretro,libretro-o2em,$(LIBRETRO_O2EM_VERSION))
 LIBRETRO_O2EM_LICENSE = Artistic License
 
