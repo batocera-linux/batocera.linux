18197f52df9fe7bc59894b85886f48be7e4b5943
Juliano Dorigão <jdorigao@gmail.com>
2020-07-11 19:12:24 -0400

    bump: libretro-beetle-pcfx

--- package/batocera/emulators/retroarch/libretro/libretro-beetle-pcfx/libretro-beetle-pcfx.mk
+++ package/batocera/emulators/retroarch/libretro/libretro-beetle-pcfx/libretro-beetle-pcfx.mk
@@ -3,8 +3,8 @@
 # BEETLE_PCFX
 #
 ################################################################################
-# Version.: Commits on May 19, 2020
-LIBRETRO_BEETLE_PCFX_VERSION = f0d284854a1973196c36590505f70c3404cc37b4
+# Version.: Commits on Jul 05, 2020
+LIBRETRO_BEETLE_PCFX_VERSION = 5b05b198c95a838798dbb57a25a4b8046b8ff34b
 LIBRETRO_BEETLE_PCFX_SITE = $(call github,libretro,beetle-pcfx-libretro,$(LIBRETRO_BEETLE_PCFX_VERSION))
 LIBRETRO_BEETLE_PCFX_LICENSE = GPLv2
 
