9a8988fcf9d1afc05f533f0aad4c85bb94b0c290
Romain Tisserand <romain.tisserand@gmail.com>
2020-07-19 14:40:58 +0200

    Add glslang (ok) and vulkan-tools (not compiling yet)

--- package/batocera/gpu/vulkan-samples/vulkan-samples.mk
+++ package/batocera/gpu/vulkan-samples/vulkan-samples.mk
@@ -15,4 +15,14 @@ VULKAN_SAMPLES_SUPPORTS_IN_SOURCE_BUILD = NO
 VULKAN_SAMPLES_CONF_OPTS += -DCMAKE_BUILD_TYPE=Release
 VULKAN_SAMPLES_CONF_ENV += LDFLAGS="--lpthread -ldl"
 
+define VULKAN_SAMPLES_INSTALL_TARGET_CMDS
+        $(INSTALL) -D -m 0755 $(@D)/buildroot-build/third_party/lib/Release/x86_64/libktx.so $(TARGET_DIR)/usr/lib/libktx.so
+        $(INSTALL) -D -m 0755 $(@D)/buildroot-build/third_party/lib/Release/x86_64/libglslang-default-resource-limits.so $(TARGET_DIR)/usr/lib/libglslang-default-resource-limits.so
+        $(INSTALL) -D -m 0755 $(@D)/buildroot-build/third_party/lib/Release/x86_64/libastc.so $(TARGET_DIR)/usr/lib/libastc.so
+        $(INSTALL) -D -m 0755 $(@D)/buildroot-build/third_party/glfw/src/lib/Release/x86_64/libglfw.so.3.3 $(TARGET_DIR)/usr/lib/libglfw.so.3.3
+        $(INSTALL) -D -m 0755 $(@D)/buildroot-build/third_party/glfw/src/lib/Release/x86_64/libglfw.so.3 $(TARGET_DIR)/usr/lib/libglfw.so.3
+        $(INSTALL) -D -m 0755 $(@D)/buildroot-build/third_party/glfw/src/lib/Release/x86_64/libglfw.so $(TARGET_DIR)/usr/lib/libglfw.so
+	$(INSTALL) -D -m 0755 $(@D)/buildroot-build/app/bin/Release/x86_64/vulkan_samples $(TARGET_DIR)/usr/bin/vulkan_samples
+endef
+
 $(eval $(cmake-package))
