28f610026e7fd1e037a1d4596ecf66f09135101d
Romain Tisserand <romain.tisserand@gmail.com>
2020-07-23 12:58:34 +0200

    Cleanup yuzu and build statically

--- package/batocera/emulators/yuzu/yuzu.mk
+++ package/batocera/emulators/yuzu/yuzu.mk
@@ -9,7 +9,7 @@ YUZU_SITE = https://github.com/yuzu-emu/yuzu-mainline.git
 YUZU_SITE_METHOD=git
 YUZU_GIT_SUBMODULES=YES
 YUZU_LICENSE = GPLv2
-YUZU_DEPENDENCIES = qt5base qt5tools qt5multimedia fmt boost ffmpeg zstd
+YUZU_DEPENDENCIES = qt5base qt5tools qt5multimedia fmt boost ffmpeg zstd catch2
 
 # Should be set when the package cannot be built inside the source tree but needs a separate build directory.
 YUZU_SUPPORTS_IN_SOURCE_BUILD = NO
@@ -17,6 +17,7 @@ YUZU_SUPPORTS_IN_SOURCE_BUILD = NO
 YUZU_CONF_OPTS += -DCMAKE_BUILD_TYPE=Release
 YUZU_CONF_OPTS += -DENABLE_SDL2=OFF
 YUZU_CONF_OPTS += -DARCHITECTURE_x86_64=ON
+YUZU_CONF_OPTS += -DBUILD_SHARED_LIBS=OFF
 
 YUZU_CONF_ENV += LDFLAGS=-lpthread ARCHITECTURE_x86_64=1
 
@@ -24,32 +25,7 @@ define YUZU_INSTALL_TARGET_CMDS
         mkdir -p $(TARGET_DIR)/usr/bin
         mkdir -p $(TARGET_DIR)/usr/lib/yuzu
 
-	$(INSTALL) -D $(@D)/buildroot-build/bin/yuzu \
-		$(TARGET_DIR)/usr/bin/
-
-	cp -pr $(@D)/buildroot-build/externals/sirit/src/*.so* \
-		$(TARGET_DIR)/usr/lib/yuzu/
-
-	cp -pr $(@D)/buildroot-build/externals/lurlparser/*.so* \
-		$(TARGET_DIR)/usr/lib/yuzu/
-
-	cp -pr $(@D)/buildroot-build/externals/dynarmic/src/*.so* \
-		$(TARGET_DIR)/usr/lib/yuzu/
-
-	cp -pr $(@D)/buildroot-build/externals/libressl/ssl/*.so* \
-		$(TARGET_DIR)/usr/lib/yuzu/
-
-	cp -pr $(@D)/buildroot-build/externals/libressl/crypto/*.so* \
-		$(TARGET_DIR)/usr/lib/yuzu/
-
-	cp -pr $(@D)/buildroot-build/externals/libressl/tls/*.so* \
-		$(TARGET_DIR)/usr/lib/yuzu/
-
-	cp -pr $(@D)/buildroot-build/externals/cubeb/*.so* \
-		$(TARGET_DIR)/usr/lib/yuzu/
-
-	cp -pr $(@D)/buildroot-build/externals/inih/*.so* \
-		$(TARGET_DIR)/usr/lib/yuzu/
+	$(INSTALL) -D $(@D)/buildroot-build/bin/yuzu $(TARGET_DIR)/usr/bin/
 endef
 
 $(eval $(cmake-package))
