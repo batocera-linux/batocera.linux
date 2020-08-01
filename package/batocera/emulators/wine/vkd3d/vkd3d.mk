1f7c59c2fb66c2c6f3b4b36fda8ba23a4b70ed3f
Romain Tisserand <romain.tisserand@gmail.com>
2020-08-01 12:39:18 +0200

    Fix vkd3d missing vk-headers and vk-loader dependency

--- package/batocera/emulators/wine/vkd3d/vkd3d.mk
+++ package/batocera/emulators/wine/vkd3d/vkd3d.mk
@@ -9,7 +9,7 @@ VKD3D_VERSION = 29287d3368f70b57d05c4ae2e2c937ebbb539236
 VKD3D_SITE = git://source.winehq.org/git/vkd3d
 VKD3D_LICENSE = LGPL-2.1+
 VKD3D_LICENSE_FILES = COPYING.LIB LICENSE
-VKD3D_DEPENDENCIES = host-bison host-flex host-wine spirv-headers host-libtool
+VKD3D_DEPENDENCIES = host-bison host-flex host-wine spirv-headers host-libtool vulkan-headers vulkan-loader
 VKD3D_CONF_ENV += WIDL="$(BUILD_DIR)/host-wine-$(WINE_VERSION)/tools/widl/widl"
 
 VKD3D_CONF_OPTS = --disable-tests --with-sysroot=$(STAGING_DIR)
