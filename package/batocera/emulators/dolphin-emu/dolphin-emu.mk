5b3926acd98028cbb3ec408e1df366f941beb68c
Romain TISSERAND <romain.tisserand@gmail.com>
2020-08-08 06:21:01 +0200

    Fix rebase

--- package/batocera/emulators/dolphin-emu/dolphin-emu.mk
+++ package/batocera/emulators/dolphin-emu/dolphin-emu.mk
@@ -7,9 +7,6 @@
 DOLPHIN_EMU_VERSION = 0dbe8fb2eaa608a6540df3d269648a596c29cf4b
 DOLPHIN_EMU_SITE = $(call github,dolphin-emu,dolphin,$(DOLPHIN_EMU_VERSION))
 DOLPHIN_EMU_LICENSE = GPLv2+
-<<<<<<< HEAD
-DOLPHIN_EMU_DEPENDENCIES = xserver_xorg-server libevdev ffmpeg zlib libpng lzo libusb libcurl bluez5_utils qt5base hidapi
-=======
 DOLPHIN_EMU_DEPENDENCIES = libevdev ffmpeg zlib libpng lzo libusb libcurl bluez5_utils qt5base hidapi
 
 ifeq ($(BR2_PACKAGE_BATOCERA_TARGET_X86_64),y)
@@ -19,7 +16,6 @@ ifeq ($(BR2_PACKAGE_BATOCERA_TARGET_ODROIDN2)$(BR2_PACKAGE_BATOCERA_TARGET_VIM3)
 DOLPHIN_EMU_DEPENDENCIES += libdrm
 endif
 
->>>>>>> Enable Dolphin on N2 and VIM3
 DOLPHIN_EMU_SUPPORTS_IN_SOURCE_BUILD = NO
 
 DOLPHIN_EMU_CONF_OPTS  = -DTHREADS_PTHREAD_ARG=OFF
