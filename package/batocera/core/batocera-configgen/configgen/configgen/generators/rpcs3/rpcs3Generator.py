8ab0370861a1c3e7040d0bfbb0a47eb92132b7f2
ordovice <dw@trugeek.us>
2020-08-10 12:30:34 -0400

    Update rpcs3Generator.py
    Initial rom launching

--- package/batocera/core/batocera-configgen/configgen/configgen/generators/rpcs3/rpcs3Generator.py
+++ package/batocera/core/batocera-configgen/configgen/configgen/generators/rpcs3/rpcs3Generator.py
@@ -1,14 +1,19 @@
 #!/usr/bin/env python
 
 from generators.Generator import Generator
+import batoceraFiles
 import Command
+import shutil
 import os
+from utils.logger import eslog
 from os import path
+from os import environ
+import ConfigParser
 
 class Rpcs3Generator(Generator):
 
     def generate(self, system, rom, playersControllers, gameResolution):
         romBasename = path.basename(rom)
-
-        commandArray = [batoceraFiles.batoceraBins[system.config['emulator']], rom]
+        romName = rom + '/PS3_GAME/USRDIR/EBOOT.BIN'
+        commandArray = [batoceraFiles.batoceraBins[system.config['emulator']], romName]
         return Command.Command(array=commandArray, env={"XDG_CONFIG_HOME":batoceraFiles.CONF, "XDG_CACHE_HOME":batoceraFiles.SAVES, "QT_QPA_PLATFORM":"xcb"})
