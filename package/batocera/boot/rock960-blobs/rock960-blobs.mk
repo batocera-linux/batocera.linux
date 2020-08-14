93469be2f24263f7de1530a65cae02992c04d35e
Ntemis <ierokipides.dem@gmail.com>
2020-08-15 00:08:17 +0300

    Rock960: Restructure rk3399 u-boot packages

--- package/batocera/boot/rock960-blobs/rock960-blobs.mk
+++ package/batocera/boot/rock960-blobs/rock960-blobs.mk
@@ -4,14 +4,13 @@
 #
 ################################################################################
 
-ROCK960_BLOBS_VERSION = 25a39c5
+ROCK960_BLOBS_VERSION = 338f4e8
 ROCK960_BLOBS_SITE = https://github.com/Multi-Retropie/rock960-blobs.git
 ROCK960_BLOBS_SITE_METHOD=git
 
 define ROCK960_BLOBS_INSTALL_TARGET_CMDS
-	cp $(@D)/idbloader.img $(BINARIES_DIR)/
-	cp $(@D)/trust.img     $(BINARIES_DIR)/
-	cp $(@D)/uboot.img     $(BINARIES_DIR)/
+	cp $(@D)/idbspl.img $(BINARIES_DIR)/
+	cp $(@D)/u-boot.itb $(BINARIES_DIR)/
 endef
 
 $(eval $(generic-package))
