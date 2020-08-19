8751c86dc44a2bf98a0b313229a4e5b8ea11bab3
Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>
2020-08-19 17:06:37 +0000

    bump dolphin
    Signed-off-by: Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>

--- package/batocera/emulators/dolphin-emu/dolphin-emu.mk
+++ package/batocera/emulators/dolphin-emu/dolphin-emu.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 # Version: Commits on Jul 5, 2020 (5.0-12257)
-DOLPHIN_EMU_VERSION = 0dbe8fb2eaa608a6540df3d269648a596c29cf4b
+DOLPHIN_EMU_VERSION = 905df6756b56ddfd7b4ead1f858c84f9e9608900
 DOLPHIN_EMU_SITE = $(call github,dolphin-emu,dolphin,$(DOLPHIN_EMU_VERSION))
 DOLPHIN_EMU_LICENSE = GPLv2+
 DOLPHIN_EMU_DEPENDENCIES = libevdev ffmpeg zlib libpng lzo libusb libcurl bluez5_utils qt5base hidapi
