fba051542acf7d9fde0c59a8ba1a9baece44cb89
Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>
2020-08-23 21:07:24 +0200

    remove unglazed
    Signed-off-by: Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>

--- package/batocera/emulators/retroarch/batocera-bezel/batocera-bezel.mk
+++ package/batocera/emulators/retroarch/batocera-bezel/batocera-bezel.mk
@@ -9,7 +9,34 @@ BATOCERA_BEZEL_SITE = $(call github,batocera-linux,batocera-bezel,$(BATOCERA_BEZ
 
 define BATOCERA_BEZEL_INSTALL_TARGET_CMDS
 	mkdir -p $(TARGET_DIR)/usr/share/batocera/datainit/decorations
-	cp -r $(@D)/* $(TARGET_DIR)/usr/share/batocera/datainit/decorations
+	cp -r $(@D)/ambiance_01 	      $(TARGET_DIR)/usr/share/batocera/datainit/decorations
+	cp -r $(@D)/ambiance_02 	      $(TARGET_DIR)/usr/share/batocera/datainit/decorations
+	cp -r $(@D)/ambiance_03 	      $(TARGET_DIR)/usr/share/batocera/datainit/decorations
+	cp -r $(@D)/ambiance_04 	      $(TARGET_DIR)/usr/share/batocera/datainit/decorations
+	cp -r $(@D)/ambiance_05 	      $(TARGET_DIR)/usr/share/batocera/datainit/decorations
+	cp -r $(@D)/ambiance_06 	      $(TARGET_DIR)/usr/share/batocera/datainit/decorations
+	cp -r $(@D)/ambiance_07 	      $(TARGET_DIR)/usr/share/batocera/datainit/decorations
+	cp -r $(@D)/ambiance_08 	      $(TARGET_DIR)/usr/share/batocera/datainit/decorations
+	cp -r $(@D)/ambiance_09 	      $(TARGET_DIR)/usr/share/batocera/datainit/decorations
+	cp -r $(@D)/ambiance_10 	      $(TARGET_DIR)/usr/share/batocera/datainit/decorations
+	cp -r $(@D)/ambiance_11 	      $(TARGET_DIR)/usr/share/batocera/datainit/decorations
+	cp -r $(@D)/ambiance_12 	      $(TARGET_DIR)/usr/share/batocera/datainit/decorations
+	cp -r $(@D)/ambiance_13 	      $(TARGET_DIR)/usr/share/batocera/datainit/decorations
+	cp -r $(@D)/ambiance_14 	      $(TARGET_DIR)/usr/share/batocera/datainit/decorations
+	cp -r $(@D)/ambiance_15 	      $(TARGET_DIR)/usr/share/batocera/datainit/decorations
+	cp -r $(@D)/ambiance_16 	      $(TARGET_DIR)/usr/share/batocera/datainit/decorations
+	cp -r $(@D)/ambiance_17 	      $(TARGET_DIR)/usr/share/batocera/datainit/decorations
+	cp -r $(@D)/ambiance_18 	      $(TARGET_DIR)/usr/share/batocera/datainit/decorations
+	cp -r $(@D)/ambiance_19 	      $(TARGET_DIR)/usr/share/batocera/datainit/decorations
+	cp -r $(@D)/ambiance_20 	      $(TARGET_DIR)/usr/share/batocera/datainit/decorations
+	cp -r $(@D)/arcade_01   	      $(TARGET_DIR)/usr/share/batocera/datainit/decorations
+	cp -r $(@D)/arcade_02   	      $(TARGET_DIR)/usr/share/batocera/datainit/decorations
+	cp -r $(@D)/arcade_vertical_01 	      $(TARGET_DIR)/usr/share/batocera/datainit/decorations
+	cp -r $(@D)/arcade_vertical_02 	      $(TARGET_DIR)/usr/share/batocera/datainit/decorations
+	cp -r $(@D)/arcade_vertical_default   $(TARGET_DIR)/usr/share/batocera/datainit/decorations
+	cp -r $(@D)/atomiswave_naomi_vertical $(TARGET_DIR)/usr/share/batocera/datainit/decorations
+	cp -r $(@D)/default_unglazed          $(TARGET_DIR)/usr/share/batocera/datainit/decorations
+
 	(cd $(TARGET_DIR)/usr/share/batocera/datainit/decorations && ln -sf default_unglazed default) # default bezel
 
 	echo -e "You can find help here to find how to customize decorations: \n" \
