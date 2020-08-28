452bc40b6c2850795a00fc270d3600359df5609d
Romain TISSERAND <romain.tisserand@gmail.com>
2020-08-28 13:29:49 +0200

    Bump DosBox-X to 0.83.4 and fix associated patches

--- package/batocera/emulators/dosbox-x/dosbox-x.mk
+++ package/batocera/emulators/dosbox-x/dosbox-x.mk
@@ -3,8 +3,8 @@
 # DosBox-X
 #
 ################################################################################
-# Version.: Commits on May 02, 2020
-DOSBOX_X_VERSION = dosbox-x-v0.83.1
+# Version.: Commits on Aug 02, 2020
+DOSBOX_X_VERSION = dosbox-x-v0.83.4
 DOSBOX_X_SITE = $(call github,joncampbell123,dosbox-x,$(DOSBOX_X_VERSION))
 DOSBOX_X_DEPENDENCIES = sdl2 sdl2_net sdl_sound zlib libpng libogg libvorbis
 DOSBOX_X_LICENSE = GPLv2
