7ac73b0c9ce06b59304d19592bd79f4e433cbc16
Romain TISSERAND <romain.tisserand@gmail.com>
2020-08-29 19:51:29 +0200

    Basic lr-dolphin integration (flagged as EXPERIMENTAL)

--- package/batocera/emulators/retroarch/libretro/libretro-dolphin/libretro-dolphin.mk
+++ package/batocera/emulators/retroarch/libretro/libretro-dolphin/libretro-dolphin.mk
@@ -16,18 +16,14 @@ LIBRETRO_DOLPHIN_CONF_OPTS += -DENABLE_NOGUI=OFF
 LIBRETRO_DOLPHIN_CONF_OPTS += -DENABLE_QT=OFF
 LIBRETRO_DOLPHIN_CONF_OPTS += -DENABLE_TESTS=OFF
 LIBRETRO_DOLPHIN_CONF_OPTS += -DUSE_DISCORD_PRESENCE=OFF
-LIBRETRO_DOLPHIN_CONF_OPTS += -DDISTRIBUTOR='batocera.linux'
 LIBRETRO_DOLPHIN_CONF_OPTS += -DBUILD_SHARED_LIBS=OFF
-
-#option(USE_SHARED_ENET "Use shared libenet if found rather than Dolphin's soon-to-compatibly-diverge version" OFF)
-#option(USE_UPNP "Enables UPnP port mapping support" ON)
-#option(ENABLE_LTO "Enables Link Time Optimization" OFF)
-#option(ENABLE_GENERIC "Enables generic build that should run on any little-endian host" OFF)
-#option(ENABLE_HEADLESS "Enables running Dolphin as a headless variant" OFF)
-#option(ENABLE_ALSA "Enables ALSA sound backend" ON)
-#option(ENABLE_PULSEAUDIO "Enables PulseAudio sound backend" ON)
-#option(ENABLE_LLVM "Enables LLVM support, for disassembly" ON)
+LIBRETRO_DOLPHIN_CONF_OPTS += -DDISTRIBUTOR='batocera.linux'
 
 LIBRETRO_DOLPHIN_PLATFORM = $(LIBRETRO_PLATFORM)
+
+define LIBRETRO_DOLPHIN_INSTALL_TARGET_CMDS
+        $(INSTALL) -D $(@D)/buildroot-build/dolphin_libretro.so \
+                $(TARGET_DIR)/usr/lib/libretro/dolphin_libretro.so
+endef
 
 $(eval $(cmake-package))
