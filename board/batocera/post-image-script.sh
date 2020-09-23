c45efe8c28561669dcc60392e47217de2b2af8fd
Romain TISSERAND <romain.tisserand@gmail.com>
2020-09-23 23:25:58 +0200

    Huge amlogic rework and cleanup => S912/VIM2 OK mainline 5.8 kernel and panfrost

--- board/batocera/post-image-script.sh
+++ board/batocera/post-image-script.sh
@@ -155,7 +155,7 @@ case "${BATOCERA_TARGET}" in
 
 	# boot.tar.xz
 	echo "creating boot.tar.xz"
-	(cd "${BINARIES_DIR}/boot" && tar -I "xz -T0" -cf "${BATOCERA_BINARIES_DIR}/boot.tar.xz" tools boot batocera-boot.conf boot-logo.bmp.gz aml_autoscript.zip aml_autoscript s905_autoscript) || exit 1
+	(cd "${BINARIES_DIR}/boot" && tar -I "xz -T0" -cf "${BATOCERA_BINARIES_DIR}/boot.tar.xz" tools boot batocera-boot.conf boot-logo.bmp.gz) || exit 1
 
 	# batocera.img
 	# rename the squashfs : the .update is the version that will be renamed at boot to replace the old version
@@ -178,26 +178,27 @@ case "${BATOCERA_TARGET}" in
 	# boot
 	rm -rf "${BINARIES_DIR:?}/boot"      || exit 1
 	mkdir -p "${BINARIES_DIR}/boot/boot" || exit 1
-	cp "${BOARD_DIR}/boot/boot-logo.bmp.gz" "${BINARIES_DIR}/boot"   || exit 1
-	$MKIMAGE -C none -A arm64 -T script -d "${BOARD_DIR}/boot/s905_autoscript.txt" "${BINARIES_DIR}/boot/s905_autoscript"
-	$MKIMAGE -C none -A arm64 -T script -d "${BOARD_DIR}/boot/aml_autoscript.txt" "${BINARIES_DIR}/boot/aml_autoscript"
-	cp "${BOARD_DIR}/boot/aml_autoscript.zip" "${BINARIES_DIR}/boot"     || exit 1
-	cp "${BINARIES_DIR}/batocera-boot.conf" "${BINARIES_DIR}/boot/batocera-boot.conf" || exit 1
-	#cp "${BINARIES_DIR}/all_merged.dtb" "${BINARIES_DIR}/dtb.img" || exit 1
+        mkdir -p "${BINARIES_DIR}/boot/extlinux" || exit 1
+        "${HOST_DIR}/bin/mkimage" -A arm64 -O linux -T kernel -C none -a 0x1080000 -e 0x1080000 -n 5.x -d "${BINARIES_DIR}/Image" "${BINARIES_DIR}/uImage" || exit 1
+	cp "${BOARD_DIR}/boot/boot-logo.bmp.gz" "${BINARIES_DIR}/boot"                      || exit 1
+	cp "${BINARIES_DIR}/batocera-boot.conf" "${BINARIES_DIR}/boot/batocera-boot.conf"   || exit 1
+	cp "${BINARIES_DIR}/uImage"             "${BINARIES_DIR}/boot/boot/linux"           || exit 1
+	cp "${BINARIES_DIR}/rootfs.squashfs"    "${BINARIES_DIR}/boot/boot/batocera.update" || exit 1
+	cp "${BINARIES_DIR}/initrd.gz"          "${BINARIES_DIR}/boot/boot"                 || exit 1
+        cp "${BOARD_DIR}/boot/extlinux.conf"    "${BINARIES_DIR}/boot/extlinux"             || exit 1
+	cp "${BOARD_DIR}/boot/logo.bmp"         "${BINARIES_DIR}/boot/boot/logo.bmp"        || exit 1
+	cp "${BOARD_DIR}/boot/boot.cmd"         "${BINARIES_DIR}/boot/boot.cmd"             || exit 1
+
 	for DTB in meson-gxm-khadas-vim2 meson-gxm-nexbox-a1 meson-gxm-q200 meson-gxm-q201 meson-gxm-rbox-pro meson-gxm-vega-s96 meson-gxm-s912-libretech-pc
         do
                 cp "${BINARIES_DIR}/${DTB}.dtb" "${BINARIES_DIR}/boot/boot" || exit 1
         done
-        "${HOST_DIR}/bin/mkimage" -A arm64 -O linux -T kernel -C none -a 0x1080000 -e 0x1080000 -n 5.x -d "${BINARIES_DIR}/Image" "${BINARIES_DIR}/uImage" || exit 1
-	#$MKBOOTIMAGE --kernel "${BINARIES_DIR}/Image" --ramdisk "${BINARIES_DIR}/initrd" --second "${BINARIES_DIR}/meson-gxm-khadas-vim2.dtb" --output "${BINARIES_DIR}/linux" || exit 1
-	cp "${BINARIES_DIR}/uImage" "${BINARIES_DIR}/boot/boot/linux" || exit 1
-
-	cp "${BINARIES_DIR}/rootfs.squashfs" "${BINARIES_DIR}/boot/boot/batocera.update" || exit 1
-	cp -pr "${BINARIES_DIR}/tools"       "${BINARIES_DIR}/boot/"                || exit 1
+	cp -pr "${BINARIES_DIR}/tools"          "${BINARIES_DIR}/boot/"                || exit 1
+        cp "${BINARIES_DIR}/u-boot.bin.sd.bin"  "${BINARIES_DIR}/boot/"                || exit 1
 
 	# boot.tar.xz
 	echo "creating boot.tar.xz"
-	(cd "${BINARIES_DIR}/boot" && tar -I "xz -T0" -cf "${BATOCERA_BINARIES_DIR}/boot.tar.xz" tools boot batocera-boot.conf boot-logo.bmp.gz aml_autoscript.zip aml_autoscript s905_autoscript) || exit 1
+	(cd "${BINARIES_DIR}/boot" && tar -I "xz -T0" -cf "${BATOCERA_BINARIES_DIR}/boot.tar.xz" extlinux tools boot batocera-boot.conf boot-logo.bmp.gz) || exit 1
 
 	# batocera.img
 	# rename the squashfs : the .update is the version that will be renamed at boot to replace the old version
