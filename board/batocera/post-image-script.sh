878482194b799b2b5492d9a89cddd7c28eb39ee3
Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>
2020-09-28 17:06:48 +0000

    fix rpi3 config.txt
    Signed-off-by: Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>

--- board/batocera/post-image-script.sh
+++ board/batocera/post-image-script.sh
@@ -121,7 +121,43 @@ case "${BATOCERA_TARGET}" in
 	sync || exit 1
 	;;
 
-	RPI3|RPI4)
+	RPI3)
+	BOARD_DIR="${BATO_DIR}/rpi"
+	# boot.tar.xz
+	cp -f "${BINARIES_DIR}/"*.dtb "${BINARIES_DIR}/rpi-firmware"
+	rm -rf "${BINARIES_DIR}/rpi-firmware/boot"   || exit 1
+	mkdir -p "${BINARIES_DIR}/rpi-firmware/boot" || exit 1
+	cp "${BOARD_DIR}/boot/config.txt" "${BINARIES_DIR}/rpi-firmware/config.txt"   || exit 1
+	cp "${BOARD_DIR}/boot/cmdline.txt" "${BINARIES_DIR}/rpi-firmware/cmdline.txt" || exit 1
+
+	KERNEL_VERSION=$(grep -E "^BR2_LINUX_KERNEL_VERSION=" "${BR2_CONFIG}" | sed -e s+'^BR2_LINUX_KERNEL_VERSION="\(.*\)"$'+'\1'+)
+	cp "${BINARIES_DIR}/zImage" "${BINARIES_DIR}/rpi-firmware/boot/linux" || exit 1
+	cp "${BINARIES_DIR}/initrd.gz" "${BINARIES_DIR}/rpi-firmware/boot" || exit 1
+	cp "${BINARIES_DIR}/rootfs.squashfs" "${BINARIES_DIR}/rpi-firmware/boot/batocera.update" || exit 1
+	cp -pr "${BINARIES_DIR}/tools"       "${BINARIES_DIR}/rpi-firmware/"                     || exit 1
+
+	echo "creating boot.tar.xz"
+	tar -I "xz -T0" -cf "${BATOCERA_BINARIES_DIR}/boot.tar.xz" -C "${BINARIES_DIR}/rpi-firmware" "." ||
+	{ echo "ERROR : unable to create boot.tar.xz" && exit 1 ;}
+
+	# batocera.img
+	# rename the squashfs : the .update is the version that will be renamed at boot to replace the old version
+	mv "${BINARIES_DIR}/rpi-firmware/boot/batocera.update" "${BINARIES_DIR}/rpi-firmware/boot/batocera" || exit 1
+	GENIMAGE_TMP="${BUILD_DIR}/genimage.tmp"
+	BATOCERAIMG="${BATOCERA_BINARIES_DIR}/batocera.img"
+	rm -rf "${GENIMAGE_TMP}" || exit 1
+	cp "${BOARD_DIR}/genimage.cfg" "${BINARIES_DIR}/genimage.cfg.tmp" || exit 1
+	FILES=$(find "${BINARIES_DIR}/rpi-firmware" -type f | sed -e s+"^${BINARIES_DIR}/rpi-firmware/\(.*\)$"+"file \1 \{ image = 'rpi-firmware/\1' }"+ | tr '\n' '@')
+	cat "${BINARIES_DIR}/genimage.cfg.tmp" | sed -e s+'@files'+"${FILES}"+ | tr '@' '\n' > "${BINARIES_DIR}/genimage.cfg" || exit 1
+	rm -f "${BINARIES_DIR}/genimage.cfg.tmp" || exit 1
+	echo "generating image"
+	genimage --rootpath="${TARGET_DIR}" --inputpath="${BINARIES_DIR}" --outputpath="${BATOCERA_BINARIES_DIR}" --config="${BINARIES_DIR}/genimage.cfg" --tmppath="${GENIMAGE_TMP}" || exit 1
+	rm -f "${BATOCERA_BINARIES_DIR}/boot.vfat" || exit 1
+	rm -f "${BATOCERA_BINARIES_DIR}/userdata.ext4" || exit 1
+	sync || exit 1
+	;;
+
+	RPI4)
 	BOARD_DIR="${BATO_DIR}/rpi4"
 	# boot.tar.xz
 	cp -f "${BINARIES_DIR}/"*.dtb "${BINARIES_DIR}/rpi-firmware"
