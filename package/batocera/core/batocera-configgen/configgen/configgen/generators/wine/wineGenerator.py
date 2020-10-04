2276267812fda11daa4e990a2bb9159ed564cd67
Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>
2020-10-04 18:30:23 +0000

    wine4 integration
    Signed-off-by: Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>

--- package/batocera/core/batocera-configgen/configgen/configgen/generators/wine/wineGenerator.py
+++ package/batocera/core/batocera-configgen/configgen/configgen/generators/wine/wineGenerator.py
@@ -11,14 +11,16 @@ class WineGenerator(Generator):
         romBasename = path.basename(rom)
 
         if system.name == "windows32_installers":
-            commandArray = ["batocera-wine", "install", 32, romBasename]
+            commandArray = ["batocera-wine", "install", "32", romBasename]
             return Command.Command(array=commandArray)
         elif system.name == "windows32":
-            commandArray = ["batocera-wine", "play", 32, romBasename]
+            commandArray = ["batocera-wine", "play", "32", romBasename]
             return Command.Command(array=commandArray)
         elif system.name == "windows64_installers":
-            commandArray = ["batocera-wine", "install", 64, romBasename]
+            commandArray = ["batocera-wine", "install", "64", romBasename]
             return Command.Command(array=commandArray)
         elif system.name == "windows64":
-            commandArray = ["batocera-wine", "play", 64, romBasename]
+            commandArray = ["batocera-wine", "play", "64", romBasename]
             return Command.Command(array=commandArray)
+
+        raise Exception("invalid system " + system.name)
