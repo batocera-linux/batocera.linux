aa77ba4a11fdbda7aff0ed7fa6f29547a4ada037
Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>
2020-10-04 09:38:12 +0000

    wine4
    Signed-off-by: Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>

--- package/batocera/core/batocera-configgen/configgen/configgen/generators/wine/wineGenerator.py
+++ package/batocera/core/batocera-configgen/configgen/configgen/generators/wine/wineGenerator.py
@@ -10,9 +10,15 @@ class WineGenerator(Generator):
     def generate(self, system, rom, playersControllers, gameResolution):
         romBasename = path.basename(rom)
 
-        if system.name == "windows_installers":
-            commandArray = ["batocera-wine", "install", romBasename]
+        if system.name == "windows32_installers":
+            commandArray = ["batocera-wine", "install", 32, romBasename]
             return Command.Command(array=commandArray)
-        else:
-            commandArray = ["batocera-wine", "play", romBasename]
+        elif system.name == "windows32":
+            commandArray = ["batocera-wine", "play", 32, romBasename]
+            return Command.Command(array=commandArray)
+        elif system.name == "windows64_installers":
+            commandArray = ["batocera-wine", "install", 64, romBasename]
+            return Command.Command(array=commandArray)
+        elif system.name == "windows64":
+            commandArray = ["batocera-wine", "play", 64, romBasename]
             return Command.Command(array=commandArray)
