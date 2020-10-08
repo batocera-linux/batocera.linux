b26f34b8410c6f20cda14dbe2ba74b584073a233
Romain TISSERAND <romain.tisserand@gmail.com>
2020-10-08 22:06:20 +0200

    Initial U-Boot 2020.10 + Kernel 5.8 XU4 reworking (boots to ES)

--- package/batocera/gpu/mali-midgard-module/mali-midgard-module.mk
+++ package/batocera/gpu/mali-midgard-module/mali-midgard-module.mk
@@ -4,9 +4,8 @@
 #
 ################################################################################
 
-MALI_MIDGARD_MODULE_VERSION = r29
-MALI_MIDGARD_MODULE_SOURCE = TX041-SW-99002-$(MALI_MIDGARD_MODULE_VERSION)p0-01rel0.tar
-MALI_MIDGARD_MODULE_SITE = https://armkeil.blob.core.windows.net/developer/Files/downloads/mali-drivers/kernel/mali-midgard-gpu
+MALI_MIDGARD_MODULE_VERSION = 2815552cc83e928d3ca3811f18e2c60d8c9eba0d
+MALI_MIDGARD_MODULE_SITE = $(call github,LibreELEC,mali-midgard,$(MALI_MIDGARD_MODULE_VERSION))
 
 MALI_MIDGARD_MODULE_MODULE_MAKE_OPTS = \
 	CONFIG_MALI_MIDGARD=m \
@@ -16,7 +15,11 @@ MALI_MIDGARD_MODULE_MODULE_MAKE_OPTS = \
 	KSRC=$(LINUX_DIR) \
 		NOSTDINC_FLAGS=-I$(@D)/product/kernel/include
 
-MALI_MIDGARD_MODULE_MODULE_SUBDIRS = product/kernel/drivers/gpu/arm
+MALI_MIDGARD_MODULE_MODULE_SUBDIRS = driver/product/kernel/drivers/gpu/arm
 
 $(eval $(kernel-module))
 $(eval $(generic-package))
+
+
+
+
