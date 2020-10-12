31c8153a56c03fee3dd71887e75b7fe6e8fa7709
liberodark <liberodark@gmail.com>
2020-10-13 00:18:31 +0200

    Update dxvk.mk

--- package/batocera/emulators/wine/dxvk/dxvk.mk
+++ package/batocera/emulators/wine/dxvk/dxvk.mk
@@ -14,7 +14,7 @@ define DXVK_EXTRACT_CMDS
 endef
 
 define DXVK_INSTALL_TARGET_CMDS
-	mkdir -p $(TARGET_DIR)/usr/share/dvxk
+	mkdir -p $(TARGET_DIR)/usr/share/dxvk
 	cp -pr $(@D)/target/dxvk-$(DXVK_VERSION)/x32 $(TARGET_DIR)/usr/share/dxvk/
 	cp -pr $(@D)/target/dxvk-$(DXVK_VERSION)/x64 $(TARGET_DIR)/usr/share/dxvk/
 endef
