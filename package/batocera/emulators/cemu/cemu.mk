59be53ce5b50c1f7d0020e06872d8d580fa82a52
liberodark <liberodark@gmail.com>
2020-11-10 14:47:33 +0100

    Update cemu.mk

--- package/batocera/emulators/cemu/cemu.mk
+++ package/batocera/emulators/cemu/cemu.mk
@@ -14,24 +14,12 @@ define CEMU_EXTRACT_CMDS
 endef
 
 define CEMU_INSTALL_TARGET_CMDS
-	mkdir -p $(TARGET_DIR)/userdata/system/cemu/
-	cp -prn $(@D)/cemu_$(CEMU_VERSION)/{Cemu.exe,gameProfiles,resources} $(TARGET_DIR)/userdata/system/cemu/
-
-	# settings
-	ln -sf /userdata/system/configs/cemu/settings.xml $(TARGET_DIR)/userdata/system/cemu/settings.xml
+	mkdir -p $(TARGET_DIR)/userdata/system/configs/cemu/
+	cp -prn $(@D)/cemu_$(CEMU_VERSION)/{Cemu.exe,gameProfiles,resources} $(TARGET_DIR)/userdata/system/configs/cemu/
 
 	# keys.txt
 	mkdir -p $(TARGET_DIR)/usr/share/batocera/datainit/bios/cemu
 	touch $(TARGET_DIR)/usr/share/batocera/datainit/bios/cemu/keys.txt
-	ln -sf /userdata/bios/cemu/keys.txt $(TARGET_DIR)/userdata/system/cemu/keys.txt
-
-	# logs
-	ln -sf /userdata/system/configs/cemu/log.txt $(TARGET_DIR)/userdata/system/cemu/log.txt
-
-	# subdirs config
-	ln -sf /userdata/system/configs/cemu/shaderCache $(TARGET_DIR)/userdata/system/cemu/shaderCache
-	ln -sf /userdata/system/configs/cemu/controllerProfiles $(TARGET_DIR)/userdata/system/cemu/controllerProfiles
-	ln -sf /userdata/system/configs/cemu/graphicPacks $(TARGET_DIR)/userdata/system/cemu/graphicPacks
 	
 	#evmap config
 	mkdir -p $(TARGET_DIR)/usr/share/evmapy
