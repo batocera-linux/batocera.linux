d477cfcd66c02eed1961a9e157102bf879e53f57
Juliano Dorigão <jdorigao@gmail.com>
2020-11-17 15:09:12 -0400

    bump: libretro-mesen-s

--- package/batocera/emulators/retroarch/libretro/libretro-mesen-s/libretro-mesen-s.mk
+++ package/batocera/emulators/retroarch/libretro/libretro-mesen-s/libretro-mesen-s.mk
@@ -3,9 +3,9 @@
 # MESEN-S
 #
 ################################################################################
-# Version.: Commits on Apr 10, 2020
-LIBRETRO_MESEN_S_VERSION = 2d0b0c47bbb29440a502c975f39233cb0c9f8001
-LIBRETRO_MESEN_S_SITE = $(call github,SourMesen,Mesen-S,$(LIBRETRO_MESEN_S_VERSION))
+# Version.: Commits on Oct 09, 2020
+LIBRETRO_MESEN_S_VERSION = 56989d162671cfb8fe1720b72eec315ec9e4f844
+LIBRETRO_MESEN_S_SITE = $(call github,libretro,Mesen-S,$(LIBRETRO_MESEN_S_VERSION))
 
 define LIBRETRO_MESEN_S_BUILD_CMDS
 	$(TARGET_CONFIGURE_OPTS) $(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" -C $(@D)/Libretro
