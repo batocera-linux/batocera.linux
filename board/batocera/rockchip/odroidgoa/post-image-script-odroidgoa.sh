b02c97c90f82fc2019e5043247a479733a995cfc
Romain TISSERAND <romain.tisserand@gmail.com>
2020-11-19 15:04:13 +0100

    Fix OGA build

--- board/batocera/rockchip/odroidgoa/post-image-script-odroidgoa.sh
+++ board/batocera/rockchip/odroidgoa/post-image-script-odroidgoa.sh
@@ -23,9 +23,9 @@ mkdir -p "${BINARIES_DIR}/boot/boot"     || exit 1
 cp "${BINARIES_DIR}/uImage"                "${BINARIES_DIR}/boot/boot/linux"                || exit 1
 cp "${BINARIES_DIR}/uInitrd"             "${BINARIES_DIR}/boot/boot/uInitrd"                || exit 1
 cp "${BINARIES_DIR}/rootfs.squashfs"       "${BINARIES_DIR}/boot/boot/batocera.update"      || exit 1
-cp "${BINARIES_DIR}/rk3326-rg351p-linux.dtb"     "${BINARIES_DIR}/boot/boot/rk3326-rg351p-linux.dtb" || exit 1
-cp "${BINARIES_DIR}/rk3326-odroidgo2-linux.dtb"  "${BINARIES_DIR}/boot/boot/rk3326-odroidgo2-linux.dtb" || exit 1
-cp "${BINARIES_DIR}/rk3326-odroidgo2-linux-v11.dtb"  "${BINARIES_DIR}/boot/boot/rk3326-odroidgo2-linux-v11.dtb" || exit 1
+#TODO cp "${BINARIES_DIR}/rk3326-rg351p-linux.dtb"     "${BINARIES_DIR}/boot/boot/rk3326-rg351p-linux.dtb" || exit 1
+cp "${BINARIES_DIR}/rk3326-odroid-go2.dtb"  "${BINARIES_DIR}/boot/boot/rk3326-odroid-go2.dtb" || exit 1
+#TODO cp "${BINARIES_DIR}/rk3326-odroidgo2-linux-v11.dtb"  "${BINARIES_DIR}/boot/boot/rk3326-odroidgo2-linux-v11.dtb" || exit 1
 cp "${BINARIES_DIR}/batocera-boot.conf"    "${BINARIES_DIR}/boot/batocera-boot.conf"        || exit 1
 cp "${BOARD_DIR}/boot/boot.ini"      "${BINARIES_DIR}/boot/boot.ini"                  || exit 1
 cp -pr "${BINARIES_DIR}/tools"             "${BINARIES_DIR}/boot/"                     || exit 1
