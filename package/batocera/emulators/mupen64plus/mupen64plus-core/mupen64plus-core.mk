fd8636aef8e76971d63524148fdabd8f3c50a73b
Juliano Dorigão <jdorigao@gmail.com>
2020-11-19 09:47:25 -0400

    bump: mupen64plus-core

--- package/batocera/emulators/mupen64plus/mupen64plus-core/mupen64plus-core.mk
+++ package/batocera/emulators/mupen64plus/mupen64plus-core/mupen64plus-core.mk
@@ -3,8 +3,8 @@
 # mupen64plus core
 #
 ################################################################################
-# Version.: Commits on May 25, 2020
-MUPEN64PLUS_CORE_VERSION = b7b56fea513e8734b982347d5b965c4a8e6f2074
+# Version.: Commits on Oct 29, 2020
+MUPEN64PLUS_CORE_VERSION = 91b909414ddf9d535192abd34432fa0c8baedf18
 MUPEN64PLUS_CORE_SITE = $(call github,mupen64plus,mupen64plus-core,$(MUPEN64PLUS_CORE_VERSION))
 MUPEN64PLUS_CORE_LICENSE = GPLv2
 MUPEN64PLUS_CORE_DEPENDENCIES = sdl2 alsa-lib freetype dejavu
