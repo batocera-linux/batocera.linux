ab6f93c3150d9f09d445303620f6a7a84fc6d17a
Romain TISSERAND <romain.tisserand@gmail.com>
2020-12-01 15:29:34 +0100

    Start to split up MAME generation and comment heavily what we do

--- package/batocera/core/batocera-configgen/configgen/configgen/generators/mame/mameGenerator.py
+++ package/batocera/core/batocera-configgen/configgen/configgen/generators/mame/mameGenerator.py
@@ -14,10 +14,8 @@ class MameGenerator(Generator):
 
     def generate(self, system, rom, playersControllers, gameResolution):
 
+	# Extract "<romfile.zip>"
         romBasename = path.basename(rom)
-
-	# Right now, hack to get into the proper MAME distro directory
-	# os.chdir("/usr/bin/mame")
 
 	# Generate userdata folders if needed
 	if not os.path.exists("/userdata/saves/mame/"):
@@ -37,23 +35,24 @@ class MameGenerator(Generator):
 	if not os.path.exists("/userdata/saves/mame/comments/"):
 	    os.makedirs("/userdata/saves/mame/comments/")
 
-	# MAME options quick explanation
-	# -skip_gameinfo to skip info screen
-	# -rompath for the ROMs path
-	# -video bgfx for shaders (can be soft accel opengl bgfx)
-	# -autoror / -autorol is TATE mode for vertical games (should be a core option)
-	# -bgfx_screen_chains specifies the shader can be crt-geom crt-geom-deluxe hlsl eagle hq2x hq3x hq4x
+	# MAME options used here are explained as it's not always straightforward
 	# A lot more options can be configured, just run mame -showusage and have a look
         commandArray = [batoceraFiles.batoceraBins[system.config['emulator']],
-			"-skip_gameinfo",
-			"-rompath",      "/userdata/roms/mame/",
-			"-bgfx_path",    "/usr/bin/mame/bgfx/",
-			"-fontpath",     "/usr/bin/mame/",
-			"-languagepath", "/usr/bin/mame/language/",
-			"-cheatpath",    "/userdata/cheats/mame/",
-			"-samplepath",   "/userdata/bios/mame/samples/",
-			"-artpath",	 "/usr/bin/mame/artwork/", # This should be on /userdata but we need to copy "base" files onto /userdata partition
 
+			# -skip_gameinfo to skip info screen
+			"-skip_gameinfo",
+			# -rompath for the ROMs path
+			"-rompath",      "/userdata/roms/mame/",
+
+			# MAME various paths we can probably do better
+			"-bgfx_path",    "/usr/bin/mame/bgfx/",          # Core bgfx files can be left on ROM filesystem
+			"-fontpath",     "/usr/bin/mame/",	         # Fonts can be left on ROM filesystem
+			"-languagepath", "/usr/bin/mame/language/",      # Translations can be left on ROM filesystem
+			"-cheatpath",    "/userdata/cheats/mame/",       # Should this point to path or cheat.7z file ?
+			"-samplepath",   "/userdata/bios/mame/samples/", # Current batocera storage location for MAME samples
+			"-artpath",	 "/usr/bin/mame/artwork/",       # This should be on /userdata but we need to copy "base" files onto /userdata partition
+
+			# MAME saves a lot of stuff, we need to map this on /userdata/saves/mame/<subfolder> for each one
 			"-nvram_directory" ,    "/userdata/saves/mame/nvram/",
 			"-cfg_directory"   ,    "/userdata/saves/mame/cfg/",
 			"-input_directory" ,    "/userdata/saves/mame/input/",
@@ -61,17 +60,45 @@ class MameGenerator(Generator):
 			"-snapshot_directory" , "/userdata/saves/mame/snap/",
 			"-diff_directory" ,     "/userdata/saves/mame/diff/",
 			"-comment_directory",   "/userdata/saves/mame/comments/",
+			]
 
-# TODO -homepath            path to base folder for plugin data (read/write)
-# TODO -ctrlrpath           path to controller definitions
-# TODO -inipath             path to ini files
-# TODO -crosshairpath       path to crosshair files
-# TODO -pluginspath         path to plugin files
-# TODO -swpath              path to loose software
+			# TODO These paths are not handled yet
+			# TODO -homepath            path to base folder for plugin data (read/write)
+			# TODO -ctrlrpath           path to controller definitions
+			# TODO -inipath             path to ini files
+			# TODO -crosshairpath       path to crosshair files
+			# TODO -pluginspath         path to plugin files
+			# TODO -swpath              path to loose software
 
+			# Rendering options
+			# Video can be : soft (slow, useless), accel (SDL2), opengl, or bgfx (shaders/effects)
+			# SHOULD BE A CORE OPTION EXPOSED IN ES
+
+	# If BGFX is selected
+	commandArray += [
 			"-video", "bgfx",
+			# -bgfx_screen_chains specifies the shader can be crt-geom crt-geom-deluxe hlsl eagle hq2x hq3x hq4x
+			# SHOULD BE A CORE OPTION EXPOSED IN ES
 			"-bgfx_screen_chains",  "crt-geom-deluxe",
-			"-autoror",
-			romBasename]
+			# -bgfx_backend can be opengl gles vulkan
+			# SHOULD BE A CORE OPTION EXPOSED IN ES
+			"-bgfx_backend", "auto",
+			]
+
+	# If accel is selected
+	#commandArray += [
+	#		"-video", "accel",
+	#		]
+
+	# If opengl is selected
+	#commandArray += [
+	#		"-video", "opengl",
+	#		]
+
+	# Rotation / TATE options : autoror (CW 90), autorol (CCW 90) depending on system configuration
+	commandArray += [ "-autoror" ] # "-autorol"
+
+	# Finally we pass game name
+	commandArray += [ romBasename ]
 
         return Command.Command(array=commandArray, env={"PWD":"/usr/bin/mame/","XDG_CONFIG_HOME":batoceraFiles.CONF, "XDG_CACHE_HOME":batoceraFiles.SAVES})
