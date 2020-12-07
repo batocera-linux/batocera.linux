9b68d243f1d1613597225ff8ea4ef73e4d93b3b1
Romain TISSERAND <romain.tisserand@gmail.com>
2020-12-07 12:13:05 +0100

    More libretro fixes for rpi4 64 bit....

--- package/batocera/emulators/retroarch/libretro/libretro-scummvm/libretro-scummvm.mk
+++ package/batocera/emulators/retroarch/libretro/libretro-scummvm/libretro-scummvm.mk
@@ -14,6 +14,10 @@ ifeq ($(BR2_PACKAGE_BATOCERA_TARGET_RPI3),y)
         LIBRETRO_SCUMMVM_PLATFORM = rpi3
 endif
 
+ifeq ($(BR2_PACKAGE_BATOCERA_TARGET_RPI4),y)
+        LIBRETRO_SCUMMVM_PLATFORM = rpi4
+endif
+
 define LIBRETRO_SCUMMVM_BUILD_CMDS
 	$(TARGET_CONFIGURE_OPTS) $(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" -C $(@D)/backends/platform/libretro/build platform="$(LIBRETRO_SCUMMVM_PLATFORM)"
 endef
