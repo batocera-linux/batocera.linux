d23dd013d764b951113d8b9879f82603ef8b3bbb
liberodark <liberodark@gmail.com>
2020-12-07 22:05:16 +0100

    change name

--- package/batocera/firmware_nonfree_rpi/firmware_nonfree_rpi.mk
+++ package/batocera/firmware_nonfree_rpi/firmware_nonfree_rpi.mk
@@ -1,14 +1,14 @@
 ################################################################################
 #
-# firmware-nonfree
+# firmware-nonfree_rpi
 #
 ################################################################################
 # Version from 1:20190114-1+rpt10 release
-FIRMWARE_NONFREE_VERSION = b66ab26cebff689d0d3257f56912b9bb03c20567
-FIRMWARE_NONFREE_SITE = https://github.com/RPi-Distro/firmware-nonfree.git
-FIRMWARE_NONFREE_SITE_METHOD = git
+FIRMWARE_NONFREE_RPI_VERSION = b66ab26cebff689d0d3257f56912b9bb03c20567
+FIRMWARE_NONFREE_RPI_SITE = https://github.com/RPi-Distro/firmware-nonfree.git
+FIRMWARE_NONFREE_RPI_SITE_METHOD = git
 
-define FIRMWARE_NONFREE_INSTALL_TARGET_CMDS
+define FIRMWARE_NONFREE_RPI_INSTALL_TARGET_CMDS
 	mkdir -p $(TARGET_DIR)/lib/firmware
 	# -n is mandatory while some other packages provides firmwares too
 	# this is not ideal, but i don't know how to tell to buildroot to install this package first (and not worry about all packages installing firmwares)
