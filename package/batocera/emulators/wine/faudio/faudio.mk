a773993e2613af9cb9d9899a1d151e2286c37771
Romain TISSERAND <romain.tisserand@gmail.com>
2020-12-18 10:39:17 +0100

    Bump FAudio Wine to 20.12

--- package/batocera/emulators/wine/faudio/faudio.mk
+++ package/batocera/emulators/wine/faudio/faudio.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-FAUDIO_VERSION = 20.08
+FAUDIO_VERSION = 20.12
 FAUDIO_SITE = $(call github,FNA-XNA,FAudio,$(FAUDIO_VERSION))
 
 FAUDIO_LICENSE = ZLIB
