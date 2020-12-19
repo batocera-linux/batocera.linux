91642ee341c331147411e330b9ff4785bfe1b70d
Romain TISSERAND <romain.tisserand@gmail.com>
2020-12-19 12:41:42 +0100

    Move N2, VIM3 on mainline 5.10 with working mali bifrost kernel module

--- package/batocera/gpu/mali-bifrost-module/mali-bifrost-module.mk
+++ package/batocera/gpu/mali-bifrost-module/mali-bifrost-module.mk
@@ -3,8 +3,8 @@
 # mali-bifrost-module
 #
 ################################################################################
-# Version: Commits on Sep 27, 2020
-MALI_BIFROST_MODULE_VERSION = 6755fc9382fc6f31cf9f434a1ec89789020f858b
+# Version: Commits on Dec 19, 2020
+MALI_BIFROST_MODULE_VERSION = b8222dc2df414afbea4270e7b382c6a96a46c4ed
 # MALI_BIFROST_MODULE_SITE = $(call github,LibreELEC,mali-bifrost,$(MALI_BIFROST_MODULE_VERSION))
 MALI_BIFROST_MODULE_SITE = $(call github,batocera-linux,mali-bifrost,$(MALI_BIFROST_MODULE_VERSION))
 # MALI_BIFROST_MODULE_OVERRIDE_SRCDIR = /sources/mali-bifrost
@@ -13,6 +13,8 @@ MALI_BIFROST_MODULE_MODULE_MAKE_OPTS = \
 	CONFIG_MALI_MIDGARD=m \
 	CONFIG_MALI_PLATFORM_NAME=meson \
 	CONFIG_MALI_PLATFORM_POWER_DOWN_ONLY=y \
+	CONFIG_MALI_DEVFREQ=y \
+	CONFIG_DEVFREQ_THERMAL=y \
 	KVER=$(LINUX_VERSION_PROBED) \
 	KSRC=$(LINUX_DIR) \
 		NOSTDINC_FLAGS=-I$(@D)/driver/product/kernel/include
