940a498ef2296cd96d9e875e18dd1491e6f2d6b3
Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>
2020-12-21 17:45:52 +0000

    fix rockpro64 boot
    Signed-off-by: Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>

--- board/batocera/rockchip/rk3399/rockpro64/create-boot-script.sh
+++ board/batocera/rockchip/rk3399/rockpro64/create-boot-script.sh
@@ -14,8 +14,8 @@ BINARIES_DIR=$4
 TARGET_DIR=$5
 BATOCERA_BINARIES_DIR=$6
 
-mkdir -p "${BINARIES_DIR}/boot/boot"     || exit 1
-mkdir -p "${BINARIES_DIR}/boot/extlinux" || exit 1
+mkdir -p "${BATOCERA_BINARIES_DIR}/boot/boot"     || exit 1
+mkdir -p "${BATOCERA_BINARIES_DIR}/boot/extlinux" || exit 1
 
 cp "${BINARIES_DIR}/Image"                 "${BATOCERA_BINARIES_DIR}/boot/boot/linux"                || exit 1
 cp "${BINARIES_DIR}/initrd.gz"             "${BATOCERA_BINARIES_DIR}/boot/boot/initrd.gz"            || exit 1
