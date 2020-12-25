45368a799403356e19747f35b68f97aabb2f013b
Romain TISSERAND <romain.tisserand@gmail.com>
2020-12-25 11:50:38 +0100

    Bump Vice to 3.5

--- package/batocera/emulators/vice/vice.mk
+++ package/batocera/emulators/vice/vice.mk
@@ -3,14 +3,14 @@
 # Vice Emulation
 #
 ################################################################################
-# Version.: Dec 24, 2019
-VICE_VERSION = 3.4
+# Version.: Dec 24, 2020
+VICE_VERSION = 3.5
 VICE_SOURCE = vice-$(VICE_VERSION).tar.gz
 VICE_SITE = https://freefr.dl.sourceforge.net/project/vice-emu/releases
 VICE_LICENSE = GPLv2
-VICE_DEPENDENCIES = ffmpeg sdl2 libpng giflib zlib lame alsa-lib jpeg host-xa
+VICE_DEPENDENCIES = ffmpeg sdl2 libpng giflib zlib lame alsa-lib jpeg host-xa host-dos2unix
 
-VICE_CONF_OPTS += --disable-option-checking
+VICE_CONF_OPTS += --disable-option-checking --disable-pdf-docs
 
 # FFMPEG
 VICE_DEPENDENCIES += ffmpeg
@@ -22,6 +22,7 @@ VICE_CONF_OPTS += --with-alsa
 VICE_CONF_OPTS += --with-zlib
 VICE_CONF_OPTS += --with-jpeg
 VICE_CONF_OPTS += --with-png
+VICE_CONF_OPTS += --without-pulse
 VICE_CONF_OPTS += --enable-x64
 
 VICE_CONF_ENV += LDFLAGS=-lSDL2
