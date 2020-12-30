841f40285d436decdd1fbb7975053b4306f8ab2e
Romain TISSERAND <romain.tisserand@gmail.com>
2020-12-30 20:07:00 +0100

    WIP PCSX2 master x86_64 native build (needs GTK3, breaks leafpad, replace with l3afpad but not compiling yet) Includes both variants (pure x86_64 and AVX2-enabled build), without the 32-bit subsystem mess

--- package/batocera/emulators/pcsx2/pcsx2.mk
+++ package/batocera/emulators/pcsx2/pcsx2.mk
@@ -4,10 +4,10 @@
 #
 ################################################################################
 
-PCSX2_VERSION = fc1d88cb516236fda5e6c678bedbb0839a474be3
+PCSX2_VERSION = c998a51f71f6a81a2f4f0c9d27c85714d74ccd57
 PCSX2_SITE = $(call github,pcsx2,pcsx2,$(PCSX2_VERSION))
 PCSX2_LICENSE = GPLv2 GPLv3 LGPLv2.1 LGPLv3
-PCSX2_DEPENDENCIES = xserver_xorg-server alsa-lib freetype zlib libpng wxwidgets libaio portaudio libsoundtouch sdl2
+PCSX2_DEPENDENCIES = xserver_xorg-server alsa-lib freetype zlib libpng wxwidgets libaio portaudio libsoundtouch sdl2 libpcap yaml-cpp libgtk3
 
 PCSX2_CONF_OPTS += -DCMAKE_BUILD_TYPE=Release
 PCSX2_CONF_OPTS += -DXDG_STD=TRUE
@@ -15,21 +15,24 @@ PCSX2_CONF_OPTS += -DSDL2_API=TRUE
 PCSX2_CONF_OPTS += -DDISABLE_PCSX2_WRAPPER=1
 PCSX2_CONF_OPTS += -DPACKAGE_MODE=FALSE
 PCSX2_CONF_OPTS += -DwxWidgets_CONFIG_EXECUTABLE="$(STAGING_DIR)/usr/bin/wx-config"
-PCSX2_CONF_OPTS += -DPCSX2_TARGET_ARCHITECTURES=i386
+PCSX2_CONF_OPTS += -DPCSX2_TARGET_ARCHITECTURES=x86_64
+PCSX2_CONF_OPTS += -DENABLE_TESTS=OFF
+PCSX2_CONF_OPTS += -DUSE_SYSTEM_YAML=ON
 PCSX2_CONF_OPTS += -DEXTRA_PLUGINS=TRUE
 #PCSX2_CONF_OPTS += -DwxUSE_UNICODE=0
 #PCSX2_CONF_OPTS += -DwxUSE_UNICODE_UTF8=0
-
+PCSX2_CONF_OPTS += -DBUILD_SHARED_LIBS=ON
 PCSX2_CONF_OPTS += -DDISABLE_ADVANCE_SIMD=ON
+PCSX2_CONF_OPTS += -DUSE_VTUNE=OFF
 
 # https://github.com/PCSX2/pcsx2/blob/51ceec74a351bd25a1066ec2c02c2aa3f8c813f4/cmake/BuildParameters.cmake#L215
-PCSX2_CONF_OPTS += -DARCH_FLAG="-msse -msse2 -mfxsr -mxsave -march=i686"
+# DITCH PCSX2_CONF_OPTS += -DARCH_FLAG="-msse -msse2 -mfxsr -mxsave -march=x86_64"
 
 define PCSX2_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 -D $(@D)/pcsx2/PCSX2 $(TARGET_DIR)/usr/PCSX/bin/PCSX2
 	cp -pr $(@D)/bin/Langs      $(TARGET_DIR)/usr/PCSX/bin
         mkdir -p $(TARGET_DIR)/usr/PCSX/bin/plugins
-	cp -pr $(@D)/plugins/*/*.so $(@D)/plugins/*/*/*.so $(TARGET_DIR)/usr/PCSX/bin/plugins
+	cp -pr $(@D)/plugins/*/*.so $(TARGET_DIR)/usr/PCSX/bin/plugins
 endef
 
 $(eval $(cmake-package))
