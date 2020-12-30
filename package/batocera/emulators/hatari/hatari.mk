2382c8c96e060ca9b96ef9d675c651fe5721ce82
Romain TISSERAND <romain.tisserand@gmail.com>
2020-12-30 02:25:09 +0100

    Better Hatari CMake config (static libs, release)

--- package/batocera/emulators/hatari/hatari.mk
+++ package/batocera/emulators/hatari/hatari.mk
@@ -6,13 +6,15 @@
 # Version.: Release on Sep 17, 2020
 HATARI_VERSION = v2.3.1
 HATARI_SOURCE = hatari-$(HATARI_VERSION).tar.gz
-HATARI_SITE = https://git.tuxfamily.org/hatari/hatari.git/snapshot/
-
+HATARI_SITE = https://git.tuxfamily.org/hatari/hatari.git/snapshot
 HATARI_LICENSE = GPLv3
-HATARI_DEPENDENCIES = sdl2
+HATARI_DEPENDENCIES = sdl2 zlib libpng capsimg
+
+HATARI_CONF_OPTS += -DBUILD_SHARED_LIBS=OFF
+HATARI_CONF_OPTS += -DCMAKE_BUILD_TYPE=Release
 
 define HATARI_INSTALL_TARGET_CMDS
-	$(INSTALL) -D $(@D)/hatari $(TARGET_DIR)/usr/bin/hatari
+	$(INSTALL) -D $(@D)/src/hatari $(TARGET_DIR)/usr/bin/hatari
         mkdir -p $(TARGET_DIR)/usr/share/hatari
 endef
 
