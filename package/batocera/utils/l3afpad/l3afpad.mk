c99b22786928b7af1a8f6022dabdc02385337fae
Romain TISSERAND <romain.tisserand@gmail.com>
2020-12-31 17:03:52 +0100

    Fix l3afpad (leafpad GTK3 replacement)

--- package/batocera/utils/l3afpad/l3afpad.mk
+++ package/batocera/utils/l3afpad/l3afpad.mk
@@ -7,10 +7,13 @@
 L3AFPAD_VERSION = v0.8.18.1.11
 L3AFPAD_SITE = $(call github,stevenhoneyman,l3afpad,$(L3AFPAD_VERSION))
 
-L3AFPAD_DEPENDENCIES = libgtk3 host-intltool
+L3AFPAD_DEPENDENCIES = libgtk3 host-intltool harfbuzz
 L3AFPAD_LICENSE = GPL-2.0+
 L3AFPAD_LICENSE_FILES = COPYING
 
-L3AFPAD_AUTORECONF = YES
+define L3AFPAD_CONFIGURE_CMDS
+    cd $(@D); PATH=$(HOST_DIR)/bin:$(PATH) ./autogen.sh ; \
+    $(TARGET_CONFIGURE_OPTS) PREFIX=$(STAGING_DIR) CFLAGS="-I$(STAGING_DIR)/usr/include" LDFLAGS="-L$(STAGING_DIR)/usr/lib" ./configure --disable-nls
+endef
 
 $(eval $(autotools-package))
