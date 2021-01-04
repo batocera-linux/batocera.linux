f68233fb83ce747594e2508da8f2498f6304d4ee
Romain TISSERAND <romain.tisserand@gmail.com>
2021-01-04 18:34:03 +0100

    EasyRPG needs pixman to build (standalone or libretro core)

--- package/batocera/emulators/easyrpg/easyrpg-player/easyrpg-player.mk
+++ package/batocera/emulators/easyrpg/easyrpg-player/easyrpg-player.mk
@@ -5,7 +5,7 @@
 ################################################################################
 # Version.: Release on May 22, 2020
 EASYRPG_PLAYER_VERSION = 0.6.2.1
-EASYRPG_PLAYER_DEPENDENCIES = sdl2 zlib fmt libpng freetype mpg123 libvorbis opusfile sdl2_mixer liblcf
+EASYRPG_PLAYER_DEPENDENCIES = sdl2 zlib fmt libpng freetype mpg123 libvorbis opusfile sdl2_mixer liblcf pixman
 EASYRPG_PLAYER_LICENSE = MIT
 EASYRPG_PLAYER_SITE = $(call github,EasyRPG,Player,$(EASYRPG_PLAYER_VERSION))
 
