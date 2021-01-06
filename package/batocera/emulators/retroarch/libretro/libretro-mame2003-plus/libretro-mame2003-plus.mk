0a5aa6cac483c2db458d0040860801c9ed5af79d
Romain TISSERAND <romain.tisserand@gmail.com>
2021-01-06 23:10:27 +0100

    bump lr-puae, lr-vice, lr-mame2003-plus, lr-gambatte

--- package/batocera/emulators/retroarch/libretro/libretro-mame2003-plus/libretro-mame2003-plus.mk
+++ package/batocera/emulators/retroarch/libretro/libretro-mame2003-plus/libretro-mame2003-plus.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 # Version.: Commits on Jan 1, 2020
-LIBRETRO_MAME2003_PLUS_VERSION = 6ed89ab8194d44d69c4d1f9c56b5510560fd9fcb
+LIBRETRO_MAME2003_PLUS_VERSION = e362ae09ef283f452c0c994ca619e921be2468b6
 LIBRETRO_MAME2003_PLUS_SITE = $(call github,libretro,mame2003-plus-libretro,$(LIBRETRO_MAME2003_PLUS_VERSION))
 LIBRETRO_MAME2003_PLUS_LICENSE = MAME
 
