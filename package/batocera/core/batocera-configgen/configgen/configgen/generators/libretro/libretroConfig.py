524bb39f76596246de2629e86728b7ac49f13d4b
Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>
2021-01-10 20:19:46 +0000

    fix: ra rotation
    Signed-off-by: Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>

--- package/batocera/core/batocera-configgen/configgen/configgen/generators/libretro/libretroConfig.py
+++ package/batocera/core/batocera-configgen/configgen/configgen/generators/libretro/libretroConfig.py
@@ -100,17 +100,17 @@ def createLibretroConfig(system, controllers, rom, bezel, gameResolution):
     retroarchConfig['video_driver'] = '"gl"'                    # needed for the ozone menu
 
     if system.isOptSet("display.rotate"):
-        # 0 => 90 ; 1 => 0 ; 2 => 270 ; 3 => 180
+        # 0 => 0 ; 1 => 270; 2 => 180 ; 3 => 90
         if system.config["display.rotate"] == "0":
-            retroarchConfig['video_rotation'] = "1"
-        elif system.config["display.rotate"] == "1":
             retroarchConfig['video_rotation'] = "0"
-        elif system.config["display.rotate"] == "2":
+        elif system.config["display.rotate"] == "1":
             retroarchConfig['video_rotation'] = "3"
-        elif system.config["display.rotate"] == "3":
+        elif system.config["display.rotate"] == "2":
             retroarchConfig['video_rotation'] = "2"
+        elif system.config["display.rotate"] == "3":
+            retroarchConfig['video_rotation'] = "1"
     else:
-        retroarchConfig['video_rotation'] = '1'
+        retroarchConfig['video_rotation'] = '0'
 
     if system.isOptSet("gfxbackend") and system.config["gfxbackend"] == "vulkan":
         retroarchConfig['video_driver'] = '"vulkan"'
