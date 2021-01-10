3a68ed44febbaa350c608318165c494be97da938
Daniel Martin <dmanlfc@gmail.com>
2021-01-10 23:58:35 +1000

    Launch with -legacy3d for now

--- package/batocera/core/batocera-configgen/configgen/configgen/generators/supermodel/supermodelGenerator.py
+++ package/batocera/core/batocera-configgen/configgen/configgen/generators/supermodel/supermodelGenerator.py
@@ -9,5 +9,8 @@ import os.path
 class SupermodelGenerator(Generator):
 
     def generate(self, system, rom, playersControllers, gameResolution):
-        commandArray = ["supermodel", "-fullscreen", "-res=1024,768", rom]
+        commandArray = ["supermodel", "-legacy3d", "-fullscreen", "-res=1024,768", rom]
         return Command.Command(array=commandArray, env={'SDL_GAMECONTROLLERCONFIG': controllersConfig.generateSdlGameControllerConfig(playersControllers)})
+        
+# Set resolution manually currently - 1024x768 should be fine for most systems.
+# Set -legacy3d for older GPU's - will need to look at an option here too in future.
