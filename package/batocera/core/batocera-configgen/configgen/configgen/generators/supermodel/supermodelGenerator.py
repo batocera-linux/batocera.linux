6f6ca1971fed56e5e91ee12ea3f193d9725c9d0c
Daniel Martin <dmanlfc@gmail.com>
2021-01-10 23:42:22 +1000

    Add the bloody __init__.py file! Little fixes

--- package/batocera/core/batocera-configgen/configgen/configgen/generators/supermodel/supermodelGenerator.py
+++ package/batocera/core/batocera-configgen/configgen/configgen/generators/supermodel/supermodelGenerator.py
@@ -1,16 +1,13 @@
 #!/usr/bin/env python
-
 import Command
+import batoceraFiles
 from generators.Generator import Generator
 import controllersConfig
-
+import shutil
+import os.path
 
 class SupermodelGenerator(Generator):
 
     def generate(self, system, rom, playersControllers, gameResolution):
         commandArray = ["supermodel", "-fullscreen", "-res=1024,768", rom]
-        return Command.Command(
-            array=commandArray,
-            env={
-                'SDL_GAMECONTROLLERCONFIG': controllersConfig.generateSdlGameControllerConfig(playersControllers)
-            })
+        return Command.Command(array=commandArray, env={'SDL_GAMECONTROLLERCONFIG': controllersConfig.generateSdlGameControllerConfig(playersControllers)})
--- package/batocera/emulators/supermodel/supermodel.mk
+++ package/batocera/emulators/supermodel/supermodel.mk
@@ -12,20 +12,18 @@ SUPERMODEL_LICENSE = GPLv3
 
 define SUPERMODEL_BUILD_CMDS
 	cp $(@D)/Makefiles/Makefile.UNIX $(@D)/Makefile
-	$(SED) "s|-O2|-O3|g" $(@D)/Makefile
 	$(SED) "s|CC = gcc|CC = $(TARGET_CC)|g" $(@D)/Makefile
 	$(SED) "s|CXX = g++|CXX = $(TARGET_CXX)|g" $(@D)/Makefile
 	$(SED) "s|LD = gcc|LD = $(TARGET_CC)|g" $(@D)/Makefile
 	$(SED) "s|sdl2-config|$(STAGING_DIR)/usr/bin/sdl2-config|g" $(@D)/Makefile
-	$(SED) "s+-march=native -mfpmath=sse++g" $(@D)/Makefiles/Rules.inc
 	$(TARGET_CONFIGURE_OPTS) $(MAKE) -C $(@D) -f Makefile VERBOSE=1
 endef
 
 define SUPERMODEL_INSTALL_TARGET_CMDS
 	$(INSTALL) -D -m 0755 $(@D)/bin/supermodel $(TARGET_DIR)/usr/bin/supermodel
-	$(INSTALL) -D -m 0644 $(@D)/Config/Games.xml $(TARGET_DIR)/usr/share/batocera/datainit/system/configs/model3/Games.xml
-	$(INSTALL) -D -m 0644 $(@D)/Config/Supermodel.ini $(TARGET_DIR)/usr/share/batocera/datainit/system/configs/model3/Supermodel.ini
-	mkdir -p $(TARGET_DIR)/usr/share/batocera/datainit/system/configs/model3/NVRAM/
+	$(INSTALL) -D -m 0644 $(@D)/Config/Games.xml $(TARGET_DIR)/userdata/system/configs/model3/Games.xml
+	$(INSTALL) -D -m 0644 $(@D)/Config/Supermodel.ini $(TARGET_DIR)/userdata/configs/model3/Supermodel.ini
+	mkdir -p $(TARGET_DIR)/userdata/system/configs/model3/NVRAM/
 	mkdir -p $(TARGET_DIR)/userdata/saves/model3/
 	mkdir -p $(TARGET_DIR)/userdata/screenshots/model3/
 endef
