5af0fb692420b95631d29864814c204dd4a5b5d5
Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>
2021-01-13 21:02:00 +0000

    move solarus from br to ba
    Signed-off-by: Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>

---
+++ package/batocera/emulators/solarus-engine/solarus-engine.mk
@@ -0,0 +1,44 @@
+################################################################################
+#
+# solarus-engine
+#
+################################################################################
+
+SOLARUS_ENGINE_VERSION = 1.6.4
+SOLARUS_ENGINE_SITE = \
+	https://gitlab.com/solarus-games/solarus/-/archive/v$(SOLARUS_ENGINE_VERSION)
+SOLARUS_ENGINE_SOURCE = solarus-v$(SOLARUS_ENGINE_VERSION).tar.bz2
+
+SOLARUS_ENGINE_LICENSE = GPL-3.0 (code), CC-BY-SA-4.0 (Solarus logos and icons), \
+	CC-BY-SA-3.0 (GUI icons)
+SOLARUS_ENGINE_LICENSE_FILES = license.txt
+
+# Install libsolarus.so
+SOLARUS_ENGINE_INSTALL_STAGING = YES
+
+SOLARUS_ENGINE_DEPENDENCIES = glm libmodplug libogg libvorbis openal physfs \
+	sdl2 sdl2_image sdl2_ttf
+
+# Disable launcher GUI (requires Qt5)
+SOLARUS_ENGINE_CONF_OPTS = \
+	-DSOLARUS_GUI=OFF \
+	-DSOLARUS_TESTS=OFF
+
+ifeq ($(BR2_PACKAGE_HAS_LIBGL),y)
+SOLARUS_ENGINE_DEPENDENCIES += libgl
+else
+ifeq ($(BR2_PACKAGE_HAS_LIBGLES),y)
+SOLARUS_ENGINE_DEPENDENCIES += libgles
+SOLARUS_ENGINE_CONF_OPTS += -DSOLARUS_GL_ES=ON
+endif
+endif
+
+ifeq ($(BR2_PACKAGE_LUAJIT),y)
+SOLARUS_ENGINE_CONF_OPTS += -DSOLARUS_USE_LUAJIT=ON
+SOLARUS_ENGINE_DEPENDENCIES += luajit
+else
+SOLARUS_ENGINE_CONF_OPTS += -DSOLARUS_USE_LUAJIT=OFF
+SOLARUS_ENGINE_DEPENDENCIES += lua
+endif
+
+$(eval $(cmake-package))
