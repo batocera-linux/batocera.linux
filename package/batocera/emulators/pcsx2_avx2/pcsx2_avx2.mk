7559c3a7594fd2a8d07ea093c048e27ebb51a321
Romain TISSERAND <romain.tisserand@gmail.com>
2021-01-24 12:58:25 +0100

    Bump pcsx2 and lr-vba-m

--- package/batocera/emulators/pcsx2_avx2/pcsx2_avx2.mk
+++ package/batocera/emulators/pcsx2_avx2/pcsx2_avx2.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PCSX2_AVX2_VERSION = 2ffc6b3acddc9500106da73175a2604f98476493
+PCSX2_AVX2_VERSION = ff0c9334e6f8cfed46a163f1c0b750bab4c06c7e
 PCSX2_AVX2_SITE = $(call github,pcsx2,pcsx2,$(PCSX2_AVX2_VERSION))
 PCSX2_AVX2_LICENSE = GPLv2 GPLv3 LGPLv2.1 LGPLv3
 PCSX2_AVX2_DEPENDENCIES = xserver_xorg-server alsa-lib freetype zlib libpng wxwidgets libaio portaudio libsoundtouch sdl2 libpcap yaml-cpp libgtk3
