31427f73342d6567f85a1015715d0e2386194a41
Gleb Mazovetskiy <glex.spb@gmail.com>
2021-01-25 21:05:43 +0000

    batocera-emulationstation: Bump version
    For https://github.com/batocera-linux/batocera-emulationstation/pull/742
    
    Signed-off-by: Gleb Mazovetskiy <glex.spb@gmail.com>

--- package/batocera/emulationstation/batocera-emulationstation/batocera-emulationstation.mk
+++ package/batocera/emulationstation/batocera-emulationstation/batocera-emulationstation.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-BATOCERA_EMULATIONSTATION_VERSION = 0538b105efb93546214b5888bc700b05674d53e8
+BATOCERA_EMULATIONSTATION_VERSION = bad71f067202c415da0e3dbf94939aa104f7ff3b
 BATOCERA_EMULATIONSTATION_SITE = https://github.com/batocera-linux/batocera-emulationstation
 BATOCERA_EMULATIONSTATION_SITE_METHOD = git
 BATOCERA_EMULATIONSTATION_LICENSE = MIT
