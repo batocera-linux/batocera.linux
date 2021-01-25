22cd7e1dcf5250cdd0e9c91e807a9da8ec67ec19
Romain TISSERAND <romain.tisserand@gmail.com>
2021-01-25 17:00:59 +0100

    WIP flycast bump going bad (GCC10 OK, GCC9 KO)

--- package/batocera/emulators/flycast/flycast.mk
+++ package/batocera/emulators/flycast/flycast.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 # Version.: Commits on Oct 10, 2020
-FLYCAST_VERSION = 170ae3477c5b69975540aa382ac9ce3442d5c40e
+FLYCAST_VERSION = 7697d37ec69a7f00920955a5fc3e70bf246b2e22
 FLYCAST_SITE = $(call github,flyinghead,flycast,$(FLYCAST_VERSION))
 FLYCAST_LICENSE = GPLv2
 FLYCAST_DEPENDENCIES = sdl2 libpng
