af5e08bcf8278d0dd89e568c4c2517e5878b04ad
Ntemis <ierokipides.dem@gmail.com>
2021-01-27 23:08:45 +0100

    RK3288: Fix rk3288W variant r1p0 libmali version

--- package/batocera/gpu/mali-T760/mali-T760.mk
+++ package/batocera/gpu/mali-T760/mali-T760.mk
@@ -9,6 +9,10 @@ MALI_T760_SITE = $(call github,rockchip-linux,libmali,$(MALI_T760_VERSION))
 MALI_T760_INSTALL_STAGING = YES
 MALI_T760_PROVIDES = libegl libgles
 
+ifeq ($(BR2_PACKAGE_BATOCERA_TARGET_MIQI),y)
+MALI_T760_CONF_OPTS += -Darch=auto -Dgpu=midgard-t76x -Dversion=r18p0-r1p0 -Dsubversion=all -Dplatform=gbm
+else
 MALI_T760_CONF_OPTS += -Darch=auto -Dgpu=midgard-t76x -Dplatform=gbm -Dsubversion=all
+endif
 
 $(eval $(meson-package))
