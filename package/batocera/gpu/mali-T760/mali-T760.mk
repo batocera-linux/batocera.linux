7afc37b1feeae7665dc27ae2eb11653fe0b795de
Ntemis <ierokipides.dem@gmail.com>
2021-01-27 02:03:31 +0100

    RK3288 & S812 RK:Fix libmali and co S812: Sound config changes

--- package/batocera/gpu/mali-T760/mali-T760.mk
+++ package/batocera/gpu/mali-T760/mali-T760.mk
@@ -4,50 +4,11 @@
 #
 ################################################################################
 
-MALI_T760_VERSION = 43b24f4a2c7cda2144210e6ca6c62eaaf8a29497
+MALI_T760_VERSION = 23dbb929bd339a6c9d8f6dc5ce348f60244cc040
 MALI_T760_SITE = $(call github,rockchip-linux,libmali,$(MALI_T760_VERSION))
 MALI_T760_INSTALL_STAGING = YES
 MALI_T760_PROVIDES = libegl libgles
-MALI_T760_TARGET_DIR=$(TARGET_DIR)
-MALI_T760_STAGING_DIR=$(STAGING_DIR)
-ifeq ($(BR2_PACKAGE_BATOCERA_TARGET_MIQI),y)
-MALI_T760_LIBFILE=libmali-midgard-t76x-r18p0-r1p0-gbm.so
-else
-MALI_T760_LIBFILE=libmali-midgard-t76x-r18p0-r0p0-gbm.so
-endif
 
 MALI_T760_CONF_OPTS += -Darch=auto -Dgpu=midgard-t76x -Dplatform=gbm -Dsubversion=all
-
-define MALI_T760_INSTALL_STAGING_CMDS
-	mkdir -p $(MALI_T760_STAGING_DIR)
-	(cd $(MALI_T760_STAGING_DIR)/usr/lib && ln -sf $(MALI_T760_LIBFILE) libmali.so)
-	(cd $(MALI_T760_STAGING_DIR)/usr/lib && ln -sf libmali.so libEGL.so)
-	(cd $(MALI_T760_STAGING_DIR)/usr/lib && ln -sf libmali.so libEGL.so.1)
-	(cd $(MALI_T760_STAGING_DIR)/usr/lib && ln -sf libmali.so libgbm.so)
-	(cd $(MALI_T760_STAGING_DIR)/usr/lib && ln -sf libmali.so libGLESv2.so)
-        (cd $(MALI_T760_STAGING_DIR)/usr/lib && ln -sf libmali.so libGLESv2.so.1)
-	(cd $(MALI_T760_STAGING_DIR)/usr/lib && ln -sf libmali.so libGLESv2.so.2)
-	(cd $(MALI_T760_STAGING_DIR)/usr/lib && ln -sf libmali.so libMali.so)
-	(cd $(MALI_T760_STAGING_DIR)/usr/lib && ln -sf libmali.so libGLESv1.so)
-	(cd $(MALI_T760_STAGING_DIR)/usr/lib && ln -sf libmali.so libGLESv1_CM.so)
-	(cd $(MALI_T760_STAGING_DIR)/usr/lib && ln -sf libmali.so libGLESv1_CM.so.1)
-	(cd $(MALI_T760_STAGING_DIR)/usr/lib && ln -sf libmali.so libbrcmGLESv2.so)
-endef
-
-define MALI_T760_INSTALL_TARGET_CMDS
-	mkdir -p $(MALI_T760_TARGET_DIR)
-	(cd $(MALI_T760_TARGET_DIR)/usr/lib && ln -sf $(MALI_T760_LIBFILE) libmali.so)
-	(cd $(MALI_T760_TARGET_DIR)/usr/lib && ln -sf libmali.so libEGL.so)
-	(cd $(MALI_T760_TARGET_DIR)/usr/lib && ln -sf libmali.so libEGL.so.1)
-	(cd $(MALI_T760_TARGET_DIR)/usr/lib && ln -sf libmali.so libGLESv1.so)
-	(cd $(MALI_T760_TARGET_DIR)/usr/lib && ln -sf libmali.so libGLESv1_CM.so)
-	(cd $(MALI_T760_TARGET_DIR)/usr/lib && ln -sf libmali.so libGLESv1_CM.so.1)
-	(cd $(MALI_T760_TARGET_DIR)/usr/lib && ln -sf libmali.so libbrcmGLESv2.so)
-	(cd $(MALI_T760_TARGET_DIR)/usr/lib && ln -sf libmali.so libgbm.so)
-	(cd $(MALI_T760_TARGET_DIR)/usr/lib && ln -sf libmali.so libGLESv2.so)
-        (cd $(MALI_T760_TARGET_DIR)/usr/lib && ln -sf libmali.so libGLESv2.so.1)
-	(cd $(MALI_T760_TARGET_DIR)/usr/lib && ln -sf libmali.so libGLESv2.so.2)
-	(cd $(MALI_T760_TARGET_DIR)/usr/lib && ln -sf libmali.so libMali.so)
-endef
 
 $(eval $(meson-package))
