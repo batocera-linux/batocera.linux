d1d55b91a666c13f6e1da358038688d1fe03499f
Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>
2021-01-30 09:17:11 +0100

    update es
    Signed-off-by: Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>

--- package/batocera/emulationstation/batocera-emulationstation/batocera-emulationstation.mk
+++ package/batocera/emulationstation/batocera-emulationstation/batocera-emulationstation.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-BATOCERA_EMULATIONSTATION_VERSION = e576f6f0a21359d4e26e30db796ba9b0aebb701e
+BATOCERA_EMULATIONSTATION_VERSION = 80a91e81826a4f01879d57f37bc0e962bc91d7e4
 BATOCERA_EMULATIONSTATION_SITE = https://github.com/batocera-linux/batocera-emulationstation
 BATOCERA_EMULATIONSTATION_SITE_METHOD = git
 BATOCERA_EMULATIONSTATION_LICENSE = MIT
--- package/batocera/emulationstation/es-theme-carbon/es-theme-carbon.mk
+++ package/batocera/emulationstation/es-theme-carbon/es-theme-carbon.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 # Version.: Commits on Jan 8, 2021
-ES_THEME_CARBON_VERSION = 499630eee9b70cfacea2f09dc3bda03168cea5f6
+ES_THEME_CARBON_VERSION = e7c79b08d0dba599d128b186c644300d0ca8d067
 ES_THEME_CARBON_SITE = $(call github,fabricecaruso,es-theme-carbon,$(ES_THEME_CARBON_VERSION))
 
 define ES_THEME_CARBON_INSTALL_TARGET_CMDS
