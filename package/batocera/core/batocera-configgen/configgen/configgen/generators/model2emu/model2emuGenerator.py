5da0b8bc77d9eb1840d8187ce6eb596cb9bd1e49
Daniel Martin <dmanlfc@gmail.com>
2021-01-30 16:51:27 +1000

    update generator

--- package/batocera/core/batocera-configgen/configgen/configgen/generators/model2emu/model2emuGenerator.py
+++ package/batocera/core/batocera-configgen/configgen/configgen/generators/model2emu/model2emuGenerator.py
@@ -10,13 +10,6 @@ class Model2EmuGenerator(Generator):
 
     def generate(self, system, rom, playersControllers, gameResolution):
 
-        #configdir = "/userdata/system/configs/model2emu"
-        #if not os.path.exists(configdir):
-        #    os.makedirs(configdir)
-
-        #copyfile("/usr/bin/model2emu/EMULATOR.INI", configdir + "/EMULATOR.INI")
-        #os.chmod(configdir + "/EMULATOR.INI", 0775)
-
         commandArray = ["/usr/wine/lutris/bin/wine64", "/usr/model2emu/emulator_multicpu.exe", rom]
         return Command.Command(
             array=commandArray,
