3d980bfb009f0eebaac3f00ef693329451b7b4f4
Romain TISSERAND <romain.tisserand@gmail.com>
2021-02-08 13:44:25 +0100

    Tidy up switchres.mk

--- package/batocera/utils/switchres/switchres.mk
+++ package/batocera/utils/switchres/switchres.mk
@@ -29,17 +29,9 @@ define SWITCHRES_INSTALL_STAGING_CMDS
 	PREFIX="$(STAGING_DIR)/usr" \
 	PKG_CONFIG_PATH="$(STAGING_DIR)/usr/lib/pkgconfig" \
 	$(MAKE) install
-	#$(INSTALL) -D -m 0644 $(@D)/libswitchres.a $(STAGING_DIR)/usr/lib/libswitchres.a
 endef
 
 define SWITCHRES_INSTALL_TARGET_CMDS
-	# Copy to target (dirty...)
-	#cd $(@D) && \
-	#CC="$(TARGET_CC)" \
-	#CXX="$(TARGET_CXX)" \
-	#PREFIX="$(STAGING_DIR)/usr" \
-	#PKG_CONFIG_PATH="$(STAGING_DIR)/usr/lib/pkgconfig" \
-	#$(MAKE) install
 	$(INSTALL) -D -m 0644 $(@D)/libswitchres.so $(TARGET_DIR)/usr/lib/libswitchres.so
 	$(INSTALL) -D -m 0755 $(@D)/switchres $(TARGET_DIR)/usr/bin/switchres
 endef
