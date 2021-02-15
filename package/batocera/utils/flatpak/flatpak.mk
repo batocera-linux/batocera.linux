0e2bb5a321da648aa598772ed3f0388856188548
Romain TISSERAND <romain.tisserand@gmail.com>
2021-02-15 17:21:41 +0100

    Some flatpak fixes to get downloading/installing packages okay

--- package/batocera/utils/flatpak/flatpak.mk
+++ package/batocera/utils/flatpak/flatpak.mk
@@ -8,11 +8,11 @@ FLATPAK_VERSION = 1.10.1
 FLATPAK_SOURCE = flatpak-$(FLATPAK_VERSION).tar.xz
 FLATPAK_SITE = https://github.com/flatpak/flatpak/releases/download/$(FLATPAK_VERSION)
 
-FLATPAK_DEPENDENCIES += pkgconf host-pkgconf libcap libarchive libglib2 libsoup libgpgme polkit libostree json-glib appstream-glib yaml-cpp python-pyparsing host-python-pyparsing python3-pyparsing host-python3-pyparsing
+FLATPAK_DEPENDENCIES += pkgconf host-pkgconf libcap libarchive libglib2 libsoup libgpgme polkit libostree json-glib appstream-glib yaml-cpp python3-pyparsing host-python3-pyparsing glib-networking
 
-FLATPAK_CONF_OPTS += --with-sysroot=$(STAGING_DIR) --with-gpgme-prefix=$(STAGING_DIR)/usr --disable-selinux-module --disable-seccomp
+FLATPAK_CONF_OPTS += --with-sysroot=$(STAGING_DIR) --with-gpgme-prefix=$(STAGING_DIR)/usr --disable-selinux-module --disable-seccomp 
 
-FLATPAK_CONF_ENV += LDFLAGS=-lpthread PKG_CONFIG_PATH=$(STAGING_DIR)/usr/lib/pkgconfig
+FLATPAK_CONF_ENV += LDFLAGS=-lpthread 
 
 $(eval $(autotools-package))
 $(eval $(host-autotools-package))
