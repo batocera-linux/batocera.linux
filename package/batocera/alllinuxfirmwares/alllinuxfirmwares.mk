54283f684094875feabb6cd150537ab7863a209e
Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>
2021-02-24 17:06:30 +0000

    rpi alllinux firmware fix
    Signed-off-by: Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>

--- package/batocera/alllinuxfirmwares/alllinuxfirmwares.mk
+++ package/batocera/alllinuxfirmwares/alllinuxfirmwares.mk
@@ -10,13 +10,23 @@ ALLLINUXFIRMWARES_SITE_METHOD = git
 
 define ALLLINUXFIRMWARES_INSTALL_TARGET_CMDS
 	mkdir -p $(TARGET_DIR)/lib/firmware
+
+	# exclude some dirs not required on batocera
+	rm -rf $(@D)/liquidio
+	rm -rf $(@D)/netronome
+
 	# -n is mandatory while some other packages provides firmwares too
 	# this is not ideal, but i don't know how to tell to buildroot to install this package first (and not worry about all packages installing firmwares)
 	cp -prn $(@D)/* $(TARGET_DIR)/lib/firmware/
-
-	# exclude some dirs not required on batocera
-	rm -rf $(TARGET_DIR)/lib/firmware/liquidio
-	rm -rf $(TARGET_DIR)/lib/firmware/netronome
 endef
+
+# because it adds so non required files on the rpi ; we prefer the specific rpi firmware packages
+define ALLLINUXFIRMWARES_DELETE_BRCM
+	rm -rf $(@D)/brcm
+endef
+
+ifeq ($(BR2_PACKAGE_BATOCERA_RPI_VCORE)$(BR2_PACKAGE_BATOCERA_RPI_MESA3D),y)
+ALLLINUXFIRMWARES_PRE_INSTALL_TARGET_HOOKS += ALLLINUXFIRMWARES_DELETE_BRCM
+endif
 
 $(eval $(generic-package))
