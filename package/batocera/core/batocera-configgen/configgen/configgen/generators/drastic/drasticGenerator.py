93f0013043e9dc280795753a8a12f0ff108bd145
liberodark <liberodark@gmail.com>
2021-02-26 01:17:30 +0100

    Add Drastic

---
+++ package/batocera/core/batocera-configgen/configgen/configgen/generators/drastic/drasticGenerator.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+import Command
+from generators.Generator import Generator
+import controllersConfig
+from shutil import copyfile
+import os
+
+class DrasticGenerator(Generator):
+
+    def generate(self, system, rom, playersControllers, gameResolution):
+
+        configdir = "/userdata/system/configs/drastic"
+        if not os.path.exists(configdir):
+            os.makedirs(configdir)
+
+        copyfile("/usr/bin/drastic", configdir + "/drastic")
+        os.chmod(configdir + "/drastic", 0775)
+
+        commandArray = [configdir + "/drastic", rom]
+        return Command.Command(
+            array=commandArray,
+            env={
+                'SDL_GAMECONTROLLERCONFIG': controllersConfig.generateSdlGameControllerConfig(playersControllers)
+            })
