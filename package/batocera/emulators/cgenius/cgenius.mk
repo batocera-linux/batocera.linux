f27207ead213af64d08eeb1b6b1a1652d324fc38
Romain TISSERAND <romain.tisserand@gmail.com>
2021-03-15 17:23:30 +0100

    Copy cgenius shared libs

--- package/batocera/emulators/cgenius/cgenius.mk
+++ package/batocera/emulators/cgenius/cgenius.mk
@@ -18,4 +18,19 @@ endif
 
 CGENIUS_CONF_OPTS += -DCMAKE_BUILD_TYPE=Release
 
+define CGENIUS_INSTALL_TARGET_SHARED_LIBRARIES
+	mkdir -p $(TARGET_DIR)/usr/lib
+	cp $(@D)/GsKit/base/lua/libGsKit_base_lua.so $(TARGET_DIR)/usr/lib
+	cp $(@D)/GsKit/base/utils/property_tree/libGsKit_base_utils_ptree.so $(TARGET_DIR)/usr/lib
+	cp $(@D)/GsKit/base/utils/libGsKit_base_utils.so $(TARGET_DIR)/usr/lib
+	cp $(@D)/GsKit/base/video/libGsKit_base_video.so $(TARGET_DIR)/usr/lib
+	cp $(@D)/GsKit/base/libGsKit_base.so $(TARGET_DIR)/usr/lib
+	cp $(@D)/GsKit/fileio/libGsKit_fileio.so $(TARGET_DIR)/usr/lib
+	cp $(@D)/GsKit/graphics/libGsKit_graphics.so $(TARGET_DIR)/usr/lib
+	cp $(@D)/GsKit/widgets/libGsKit_widgets.so $(TARGET_DIR)/usr/lib
+	cp $(@D)/GsKit/libGsKit.so $(TARGET_DIR)/usr/lib
+endef
+
+CGENIUS_POST_INSTALL_TARGET_HOOKS += CGENIUS_INSTALL_TARGET_SHARED_LIBRARIES
+
 $(eval $(cmake-package))
