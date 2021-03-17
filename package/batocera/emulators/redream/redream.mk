5de8855148ae56c054356d9b74f3dfba393ec4b0
liberodark <liberodark@gmail.com>
2021-03-18 00:10:43 +0100

    Bump Redream

--- package/batocera/emulators/redream/redream.mk
+++ package/batocera/emulators/redream/redream.mk
@@ -4,9 +4,11 @@
 #
 ################################################################################
 
-REDREAM_VERSION = 1.5.0-682-gd2be0f3
+REDREAM_VERSION = 1.5.0-785-g4e66e91
 REDREAM_SOURCE = redream.x86_64-linux-v$(REDREAM_VERSION).tar.gz
-REDREAM_SITE = https://redream.io/download
+REDREAM_SITE = https://github.com/liberodark/redream-repo/releases/download/1.0
+# REDREAM_SITE = https://redream.io/download
+# temporarly link to a private repo untile the stable is realeased
 
 define REDREAM_EXTRACT_CMDS
 	mkdir -p $(@D)/target && cd $(@D)/target && tar xf $(DL_DIR)/$(REDREAM_DL_SUBDIR)/$(REDREAM_SOURCE)
