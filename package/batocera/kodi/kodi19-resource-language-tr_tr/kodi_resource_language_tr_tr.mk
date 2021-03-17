ae25d41c143e7af5bdcc94cbc5342e0c6310cfd4
Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>
2021-03-17 12:31:52 +0000

    fix kodi languages 19
    Signed-off-by: Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>

---
+++ package/batocera/kodi/kodi19-resource-language-tr_tr/kodi_resource_language_tr_tr.mk
@@ -0,0 +1,23 @@
+################################################################################
+#
+# kodi tr_tr language resource
+#
+################################################################################
+
+KODI19_RESOURCE_LANGUAGE_TR_TR_VERSION = 9.0.22
+KODI19_RESOURCE_LANGUAGE_TR_TR_SOURCE = resource.language.tr_tr-$(KODI19_RESOURCE_LANGUAGE_TR_TR_VERSION).zip
+KODI19_RESOURCE_LANGUAGE_TR_TR_SITE = http://mirrors.kodi.tv/addons/matrix/resource.language.tr_tr
+KODI19_RESOURCE_LANGUAGE_TR_TR_PLUGINNAME=resource.language.tr_tr
+
+KODI19_PLUGIN_TARGET_DIR=$(TARGET_DIR)/usr/share/kodi/addons
+
+define KODI19_RESOURCE_LANGUAGE_TR_TR_EXTRACT_CMDS
+	@unzip -q -o $(DL_DIR)/$(KODI19_RESOURCE_LANGUAGE_TR_TR_DL_SUBDIR)/$(KODI19_RESOURCE_LANGUAGE_TR_TR_SOURCE) -d $(@D)
+endef
+
+define KODI19_RESOURCE_LANGUAGE_TR_TR_INSTALL_TARGET_CMDS
+	@mkdir -p $(KODI19_PLUGIN_TARGET_DIR)
+	@cp -r $(@D)/$(KODI19_RESOURCE_LANGUAGE_TR_TR_PLUGINNAME) $(KODI19_PLUGIN_TARGET_DIR)
+endef
+
+$(eval $(generic-package))
