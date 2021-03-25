cb08c06ce5176f29b4bbcab949a261c89521b10d
Tiago Pierezan Camargo <camargo@discworld.com.br>
2021-03-25 12:08:14 -0300

    alsa-plugins: fix deps

--- package/batocera/libraries/alsa-plugins/alsa-plugins.mk
+++ package/batocera/libraries/alsa-plugins/alsa-plugins.mk
@@ -7,14 +7,14 @@ ALSA_PLUGINS_VERSION = 1.2.2
 ALSA_PLUGINS_SOURCE = alsa-plugins-$(ALSA_PLUGINS_VERSION).tar.bz2
 ALSA_PLUGINS_SITE = ftp://ftp.alsa-project.org/pub/plugins
 ALSA_PLUGINS_INSTALL_STAGING = NO
-ALSA_PLUGINS_DEPENDENCIES = alsa-lib
+ALSA_PLUGINS_DEPENDENCIES = alsa-lib host-pkgconf
 ALSA_PLUGINS_POST_INSTALL_TARGET_HOOKS = ALSA_PLUGINS_PULSEAUDIO_CONF
 
 ALSA_PLUGINS_CONF_OPTS += --with-plugindir=/usr/lib/alsa-lib \
 	--localstatedir=/var \
 	--disable-jack \
 	--disable-avcodec \
-	--with-speex=builtin
+	--with-speex=builtin 
 
 # define ALSA_PLUGINS_PULSEAUDIO_CONF
 # 	mv $(TARGET_DIR)/etc/alsa/conf.d/99-pulseaudio-default.conf.example \
