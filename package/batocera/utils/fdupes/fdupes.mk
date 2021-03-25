20e38b16bf495b05ca00add5f7fc31aee1de6a61
Tiago Pierezan Camargo <camargo@discworld.com.br>
2021-03-25 09:50:08 -0300

    fdupes: fix deps

--- package/batocera/utils/fdupes/fdupes.mk
+++ package/batocera/utils/fdupes/fdupes.mk
@@ -3,17 +3,11 @@
 # fdupes
 #
 ################################################################################
-
 FDUPES_VERSION = 791fc12a093b77e8e897241bff731d6ec1e8f5b9
 FDUPES_SITE = $(call github,adrianlopezroche,fdupes,$(FDUPES_VERSION))
-
-FDUPES_DEPENDENCIES = host-autoconf host-automake host-libtool
+FDUPES_AUTORECONF = YES
+# fdupes needs curses.h but full ncurses support is disabled
+FDUPES_DEPENDENCIES = ncurses
 FDUPES_CONF_OPTS = --without-ncurses
-
-define FDUPES_RUN_AUTOCONF
-        (cd $(@D); $(HOST_DIR)/bin/autoreconf --install)
-endef
-
-FDUPES_PRE_CONFIGURE_HOOKS += FDUPES_RUN_AUTOCONF
 
 $(eval $(autotools-package))
