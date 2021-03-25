ffff75bf0e35838dedb38fc3a077dd4e13fef720
Tiago Pierezan Camargo <camargo@discworld.com.br>
2021-03-25 11:22:35 -0300

    batocera-bluetooth: make sure init.d/ exists

--- package/batocera/core/batocera-bluetooth/batocera-bluetooth.mk
+++ package/batocera/core/batocera-bluetooth/batocera-bluetooth.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-BATOCERA_BLUETOOTH_VERSION = 1.1
+BATOCERA_BLUETOOTH_VERSION = 2
 BATOCERA_BLUETOOTH_LICENSE = GPL
 BATOCERA_BLUETOOTH_SOURCE=
 
@@ -19,6 +19,7 @@ else ifeq ($(BR2_PACKAGE_BATOCERA_TARGET_RK3399),y)
 endif
 
 define BATOCERA_BLUETOOTH_INSTALL_TARGET_CMDS
+	mkdir -p $(TARGET_DIR)/etc/init.d/
 	cp $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/core/batocera-bluetooth/S32bluetooth.template $(TARGET_DIR)/etc/init.d/S32bluetooth
 	sed -i -e s+"@INTERNAL_BLUETOOTH_STACK@"+"$(BATOCERA_BLUETOOTH_STACK)"+ $(TARGET_DIR)/etc/init.d/S32bluetooth
 endef
