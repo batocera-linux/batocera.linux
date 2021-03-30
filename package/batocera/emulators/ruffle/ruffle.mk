d780a81ca28179c1510c71823ba40c1158832e14
Tiago Pierezan Camargo <camargo@discworld.com.br>
2021-03-30 13:16:32 -0300

    ruffle: fix deps

--- package/batocera/emulators/ruffle/ruffle.mk
+++ package/batocera/emulators/ruffle/ruffle.mk
@@ -7,27 +7,27 @@
 RUFFLE_VERSION = 0063f40af9cd6dd39038d67880db1d996176c309
 RUFFLE_SITE = $(call github,ruffle-rs,ruffle,$(RUFFLE_VERSION))
 RUFFLE_LICENSE = GPLv2
-RUFFLE_DEPENDENCIES = host-rustc openssl
+RUFFLE_DEPENDENCIES = host-rustc openssl alsa-lib libxcb
 
 RUFFLE_CARGO_ENV = \
-    CARGO_HOME=$(HOST_DIR)/usr/share/cargo \
-    RUST_TARGET_PATH=$(HOST_DIR)/etc/rustc \
-    PKG_CONFIG_SYSROOT_DIR=$(STAGING_DIR) \
-    PKG_CONFIG_PATH=$(STAGING_DIR)/usr/lib/pkgconfig \
-    TARGET_CC=$(TARGET_CC) \
-    TARGET_CXX=$(TARGET_CXX) \
-    TARGET_LD=$(TARGET_LD)
+	CARGO_HOME=$(HOST_DIR)/usr/share/cargo \
+	RUST_TARGET_PATH=$(HOST_DIR)/etc/rustc \
+	PKG_CONFIG_SYSROOT_DIR=$(STAGING_DIR) \
+	PKG_CONFIG_PATH=$(STAGING_DIR)/usr/lib/pkgconfig \
+	TARGET_CC=$(TARGET_CC) \
+	TARGET_CXX=$(TARGET_CXX) \
+	TARGET_LD=$(TARGET_LD)
 
 RUFFLE_CARGO_OPTS = \
-    --target=$(BR2_ARCH)-unknown-linux-gnu \
-    --manifest-path=$(@D)/Cargo.toml
+	--target=$(BR2_ARCH)-unknown-linux-gnu \
+	--manifest-path=$(@D)/Cargo.toml
 
 RUFFLE_CARGO_MODE = release
 RUFFLE_CARGO_OPTS += --$(RUFFLE_CARGO_MODE)
 
 define RUFFLE_BUILD_CMDS
-    $(TARGET_MAKE_ENV) $(RUFFLE_CARGO_ENV) \
-            cargo build $(RUFFLE_CARGO_OPTS)
+	$(TARGET_MAKE_ENV) $(RUFFLE_CARGO_ENV) \
+		cargo build $(RUFFLE_CARGO_OPTS)
 endef
 
 define RUFFLE_INSTALL_TARGET_CMDS
