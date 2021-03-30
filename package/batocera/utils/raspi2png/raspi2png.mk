edccbcccee3ff1e8dd2cafc9c57aa978ef38a62b
Juliano Dorigão <jdorigao@gmail.com>
2021-03-30 16:29:05 -0400

    raspi2png: fix dependency

--- package/batocera/utils/raspi2png/raspi2png.mk
+++ package/batocera/utils/raspi2png/raspi2png.mk
@@ -5,6 +5,9 @@
 ################################################################################
 RASPI2PNG_VERSION = caa4068e9879f09afa045ee89f219bf6f8a3560d
 RASPI2PNG_SITE = $(call github,AndrewFromMelbourne,raspi2png,$(RASPI2PNG_VERSION))
+RASPI2PNG_LICENSE = MIT
+RASPI2PNG_DEPENDENCIES = libpng
+
 RASPI2PNG_LDFLAGS = -L$(STAGING_DIR)/usr/lib -lbcm_host -lpng -lm -lvchostif
 RASPI2PNG_INCLUDES = -I$(STAGING_DIR)/usr/include/ -I$(STAGING_DIR)/usr/include/interface/vcos/pthreads -I$(STAGING_DIR)/usr/include/interface/vmcs_host/linux
 
