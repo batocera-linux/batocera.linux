f5111f98f8f212632513832bee5bfa3e966a5968
Tiago Pierezan Camargo <camargo@discworld.com.br>
2021-03-30 15:25:27 -0300

    batocera-wine: create $(TARGET_DIR)/etc/X11/xorg.conf.d

--- package/batocera/utils/batocera-wine/batocera-wine.mk
+++ package/batocera/utils/batocera-wine/batocera-wine.mk
@@ -4,13 +4,14 @@
 #
 ################################################################################
 
-BATOCERA_WINE_VERSION = 1.0
+BATOCERA_WINE_VERSION = 2
 BATOCERA_WINE_LICENSE = GPL
 BATOCERA_WINE_SOURCE=
 
 define BATOCERA_WINE_INSTALL_TARGET_CMDS
 	install -m 0755 $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/utils/batocera-wine/batocera-wine $(TARGET_DIR)/usr/bin/batocera-wine
 	install -m 0755 $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/utils/batocera-wine/bsod.py       $(TARGET_DIR)/usr/bin/bsod-wine
+	mkdir -p $(TARGET_DIR)/etc/X11/xorg.conf.d
 	ln -fs /userdata/system/99-nvidia.conf $(TARGET_DIR)/etc/X11/xorg.conf.d/99-nvidia.conf
 
 	mkdir -p $(TARGET_DIR)/usr/share/evmapy
