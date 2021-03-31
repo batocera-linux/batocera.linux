6ae54fc287251bab7f739d9c129c196293b6b2f2
Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>
2021-03-31 20:05:38 +0000

    s922x merge
    Signed-off-by: Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>

--- package/batocera/boot/uboot-odroid-n2/uboot-odroid-n2.mk
+++ package/batocera/boot/uboot-odroid-n2/uboot-odroid-n2.mk
@@ -9,7 +9,7 @@ UBOOT_ODROID_N2_SOURCE = u-boot-odroidn2-$(UBOOT_ODROID_N2_VERSION).tar.gz
 UBOOT_ODROID_N2_SITE = https://github.com/hardkernel/u-boot/releases/download/travis%2Fodroidn2-$(UBOOT_ODROID_N2_VERSION)
 
 define UBOOT_ODROID_N2_INSTALL_TARGET_CMDS
-	cp $(@D)/u-boot.bin $(BINARIES_DIR)/u-boot.bin
+	cp $(@D)/u-boot.bin $(BINARIES_DIR)/u-boot-odroidn2.bin
 endef
 
 $(eval $(generic-package))
