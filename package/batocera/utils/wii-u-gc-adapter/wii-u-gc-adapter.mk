d679e0a09e1aaddd0f14e7f003ae1fac5c8eb61f
Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>
2021-04-02 11:36:25 +0000

    wii-u-gc-adapter daemon
    Signed-off-by: Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>

---
+++ package/batocera/utils/wii-u-gc-adapter/wii-u-gc-adapter.mk
@@ -0,0 +1,19 @@
+################################################################################
+#
+# wii-u-gc-adapter
+#
+################################################################################
+WII_U_GC_ADAPTER_VERSION = 64d7ddc511adb12f2eb05bd97294bc51cf51bfab
+WII_U_GC_ADAPTER_SITE = $(call github,ToadKing,wii-u-gc-adapter,$(WII_U_GC_ADAPTER_VERSION))
+
+define WII_U_GC_ADAPTER_BUILD_CMDS
+	$(TARGET_CONFIGURE_OPTS) $(MAKE) CC="$(TARGET_CC)" -C  $(@D)
+endef
+
+define WII_U_GC_ADAPTER_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 0755 -D $(@D)/wii-u-gc-adapter $(TARGET_DIR)/usr/bin/wii-u-gc-adapter
+	$(INSTALL) -m 0755 -D $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/utils/wii-u-gc-adapter/wii-u-gc-adapter-daemon $(TARGET_DIR)/usr/bin/wii-u-gc-adapter-daemon
+	cp $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/utils/wii-u-gc-adapter/99-wii-u-gc-adapter.rules $(TARGET_DIR)/etc/udev/rules.d
+endef
+
+$(eval $(generic-package))
