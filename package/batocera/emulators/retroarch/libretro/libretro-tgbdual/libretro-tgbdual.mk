944534b0643c905b793826659c9eb3c78836f21f
Juliano Dorigão <jdorigao@gmail.com>
2021-04-05 09:19:46 -0400

    bump: libretro-tgdual

--- package/batocera/emulators/retroarch/libretro/libretro-tgbdual/libretro-tgbdual.mk
+++ package/batocera/emulators/retroarch/libretro/libretro-tgbdual/libretro-tgbdual.mk
@@ -3,8 +3,8 @@
 # TGBDUAL
 #
 ################################################################################
-# Version.: Commits on Oct 19, 2020
-LIBRETRO_TGBDUAL_VERSION = 18fd6092faf2c1f3a534c778a3850433c4962808
+# Version.: Commits on Mar 12, 2021
+LIBRETRO_TGBDUAL_VERSION = fb6d2f6fca1956b389b3e9a8282df5b0cd465a6f
 LIBRETRO_TGBDUAL_SITE = $(call github,libretro,tgbdual-libretro,$(LIBRETRO_TGBDUAL_VERSION))
 LIBRETRO_TGBDUAL_LICENSE = GPLv2
 
