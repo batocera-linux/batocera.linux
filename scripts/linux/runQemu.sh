7db3dbe057c5f1931273462dcff39ad4894da8c0
Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>
2021-04-16 14:14:23 +0200

    update runQemu.sh instructions
    Signed-off-by: Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>

--- scripts/linux/runQemu.sh
+++ scripts/linux/runQemu.sh
@@ -14,8 +14,12 @@ if test $# -lt 1
 then
     echo "${0} <batocera.img>" >&2
     echo "${0} <batocera.img> <share image>" >&2
+    echo "" >&2
+    echo "1. create an image" >&2
     echo "You can create a share image with : # dd if=/dev/zero of=share.img count=5 bs=1G" >&2
+    echo "" >&2
     echo "You can format it from batocera" >&2
+    echo "" >&2
     echo "Then you can mount it on linux with : " >&2
     echo 'X=$(sudo losetup -f)' >&2
     echo 'mkdir -p BATOCERA' >&2
@@ -23,6 +27,15 @@ then
     echo 'sudo mount ${X}p1 BATOCERA' >&2
     echo 'sudo umount BATOCERA' >&2
     echo 'sudo losetup -D $X' >&2
+    echo "" >&2
+    echo "Or you can access it via network : ssh root@localhost -p 5555" >&2
+    echo "" >&2
+    echo "2. Add pads :" >&2
+    echo "On your host machine, run evtest to find your pad event name" >&2
+    echo "then for the event15, get the ID_MODEL udevadm info -q all -n /dev/input/event15 | grep MODEL=" >&2
+    echo "and finally add an udev rule in /etc/udev/rules.d/99-joystick.rules" >&2
+    echo 'SUBSYSTEM=="usb", ENV{ID_MODEL}=="Bigben_Game_Pad", MODE="0666"' >&2
+    echo "Reload udev : sudo /etc/init.d/udev restart and replug your pad." >&2
     exit 1
 fi
 
