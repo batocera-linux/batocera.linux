fb11fb3c014a112ef3cff310b949d26d4a31f95b
Romain TISSERAND <romain.tisserand@gmail.com>
2021-04-20 23:42:54 +0200

    Bump mGBA (libretro) to 0.9.1

--- package/batocera/emulators/retroarch/libretro/libretro-mgba/libretro-mgba.mk
+++ package/batocera/emulators/retroarch/libretro/libretro-mgba/libretro-mgba.mk
@@ -3,8 +3,8 @@
 # MGBA
 #
 ################################################################################
-# Version.: Release on Mar 28, 2021
-LIBRETRO_MGBA_VERSION = 0.9.0
+# Version.: Release on Apr 18, 2021
+LIBRETRO_MGBA_VERSION = 0.9.1
 LIBRETRO_MGBA_SITE = $(call github,mgba-emu,mgba,$(LIBRETRO_MGBA_VERSION))
 LIBRETRO_MGBA_LICENSE = MPLv2.0
 
