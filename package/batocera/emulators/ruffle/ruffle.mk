0945bb73654d0cae21af810c5d4495cdf5e91e16
liberodark <liberodark@gmail.com>
2021-04-25 16:07:49 +0200

    Fix Ruffle
    That revert on old commit new commits make several compile errors.

--- package/batocera/emulators/ruffle/ruffle.mk
+++ package/batocera/emulators/ruffle/ruffle.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 # Version.: Commits on Mar 27, 2021
-RUFFLE_VERSION = c422a66aa64dc25bb7313f2f305a0a3513aab17a
+RUFFLE_VERSION = 5cefaa16d2c29dac0e573967477126f31bb69075
 RUFFLE_SITE = $(call github,ruffle-rs,ruffle,$(RUFFLE_VERSION))
 RUFFLE_LICENSE = GPLv2
 RUFFLE_DEPENDENCIES = host-rustc openssl
