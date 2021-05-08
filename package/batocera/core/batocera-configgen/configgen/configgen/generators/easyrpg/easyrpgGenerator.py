8ddece4893cde778b7a65dc79801db1931e6044b
soaresden <54243866+soaresden@users.noreply.github.com>
2021-05-09 00:05:54 +0200

    Fixing the Generator
    After many tries, finally got it
    Can it be simpler ?

--- package/batocera/core/batocera-configgen/configgen/configgen/generators/easyrpg/easyrpgGenerator.py
+++ package/batocera/core/batocera-configgen/configgen/configgen/generators/easyrpg/easyrpgGenerator.py
@@ -5,6 +5,7 @@ from generators.Generator import Generator
 import controllersConfig
 import os
 import codecs
+import logging
 
 class EasyRPGGenerator(Generator):
 
@@ -40,65 +41,65 @@ class EasyRPGGenerator(Generator):
 
         # Retrieving ES Values 
         mappings = {
-        "easyrpg_action":   {"key": "button_action"   ,"default" : "a"}
-        "easyrpg_cancel":   {"key": "button_cancel"   ,"default" : "b"}
-        "easyrpg_shift":    {"key": "button_shift"    ,"default" : "pageup"}
-        "easyrpg_n0":       {"key": "button_n0"       ,"default" : None}
-        "easyrpg_n1":       {"key": "button_n1"       ,"default" : None}
-        "easyrpg_n2":       {"key": "button_n2"       ,"default" : None}
-        "easyrpg_n3":       {"key": "button_n3"       ,"default" : None}
-        "easyrpg_n4":       {"key": "button_n4"       ,"default" : None}
-        "easyrpg_n5":       {"key": "button_n5"       ,"default" : None}
-        "easyrpg_n6":       {"key": "button_n6"       ,"default" : None}
-        "easyrpg_n7":       {"key": "button_n7"       ,"default" : None}
-        "easyrpg_n8":       {"key": "button_n8"       ,"default" : None}
-        "easyrpg_n9":       {"key": "button_n9"       ,"default" : None}
-        "easyrpg_plus":     {"key": "button_plus"     ,"default" : None}
-        "easyrpg_minus":    {"key": "button_minus"    ,"default" : None}
-        "easyrpg_multiply": {"key": "button_multiply" ,"default" : None}
-        "easyrpg_divide":   {"key": "button_divide"   ,"default" : None}
+        "easyrpg_action":   {"key": "button_action"   ,"default" : "a"},
+        "easyrpg_cancel":   {"key": "button_cancel"   ,"default" : "b"},
+        "easyrpg_shift":    {"key": "button_shift"    ,"default" : "pageup"},
+        "easyrpg_n0":       {"key": "button_n0"       ,"default" : None},
+        "easyrpg_n1":       {"key": "button_n1"       ,"default" : None},
+        "easyrpg_n2":       {"key": "button_n2"       ,"default" : None},
+        "easyrpg_n3":       {"key": "button_n3"       ,"default" : None},
+        "easyrpg_n4":       {"key": "button_n4"       ,"default" : None},
+        "easyrpg_n5":       {"key": "button_n5"       ,"default" : None},
+        "easyrpg_n6":       {"key": "button_n6"       ,"default" : None},
+        "easyrpg_n7":       {"key": "button_n7"       ,"default" : None},
+        "easyrpg_n8":       {"key": "button_n8"       ,"default" : None},
+        "easyrpg_n9":       {"key": "button_n9"       ,"default" : None},
+        "easyrpg_plus":     {"key": "button_plus"     ,"default" : None},
+        "easyrpg_minus":    {"key": "button_minus"    ,"default" : None},
+        "easyrpg_multiply": {"key": "button_multiply" ,"default" : None},
+        "easyrpg_divide":   {"key": "button_divide"   ,"default" : None},
         "easyrpg_period":   {"key": "button_period"   ,"default" : None}
         }
         # Loop every keys to assign 
-        
-        for mapping in mappings:
-        if system.isOptSet(mapping):
-           esbt[mappings[mapping]["key"]] = system.config[mapping]
-        else:
-           esbt[mappings[mapping]["key"]] = mappings[mapping]["default"]
-
+        esbuttontable = {} 
+        for entry in mappings:
+            esbutton = "es"+str(mappings[entry]["key"]) 
+            if system.isOptSet(entry):
+                entryvalue = system.config[entry]
+                esbuttontable[esbutton] = entryvalue
+            else:
+                entrydefault = mappings[entry]["default"]
+                esbuttontable[esbutton] = entrydefault
         # Configuring Pads
-        EasyRPGGenerator.padConfig(configdir, playersControllers, esbtaction, esbtcancel,esbtshift, esbt0, esbt1, esbt2, esbt3, esbt4, esbt5, esbt6, esbt7, esbt8, esbt9, esbtplus, esbtminus, esbtmulti, esbtdiv, esbtperiod)
-           
-        
+        EasyRPGGenerator.padConfig(configdir, playersControllers, esbuttontable)
         # Launching EasyRPG
         return Command.Command(array=commandArray)
 
     @staticmethod
-    def padConfig(configdir, playersControllers, esbtaction, esbtcancel, esbtshift, esbt0, esbt1, esbt2, esbt3, esbt4, esbt5, esbt6, esbt7, esbt8, esbt9, esbtplus, esbtminus, esbtmulti, esbtdiv, esbtperiod):
+    def padConfig(configdir, playersControllers, esbuttontable):
         keymapping = {
             "button_up":                 None,
             "button_down":               None,
             "button_left":               None,
             "button_right":              None,
-            "button_action":             esbtaction,
-            "button_cancel":             esbtcancel,
-            "button_shift":              esbtshift,
-            "button_n0":                 esbt0,
-            "button_n1":                 esbt1,
-            "button_n2":                 esbt2,
-            "button_n3":                 esbt3,
-            "button_n4":                 esbt4,
-            "button_n5":                 esbt5,
-            "button_n6":                 esbt6,
-            "button_n7":                 esbt7,
-            "button_n8":                 esbt8,
-            "button_n9":                 esbt9,
-            "button_plus":               esbtplus,
-            "button_minus":              esbtminus,
-            "button_multiply":           esbtmulti,
-            "button_divide":             esbtdiv,
-            "button_period":             esbtperiod,
+            "button_action":             esbuttontable['esbutton_action'],
+            "button_cancel":             esbuttontable['esbutton_cancel'],
+            "button_shift":              esbuttontable['esbutton_shift'],
+            "button_n0":                 esbuttontable['esbutton_n0'],
+            "button_n1":                 esbuttontable['esbutton_n1'],
+            "button_n2":                 esbuttontable['esbutton_n2'],
+            "button_n3":                 esbuttontable['esbutton_n3'],
+            "button_n4":                 esbuttontable['esbutton_n4'],
+            "button_n5":                 esbuttontable['esbutton_n5'],
+            "button_n6":                 esbuttontable['esbutton_n6'],
+            "button_n7":                 esbuttontable['esbutton_n7'],
+            "button_n8":                 esbuttontable['esbutton_n8'],
+            "button_n9":                 esbuttontable['esbutton_n9'],
+            "button_plus":               esbuttontable['esbutton_plus'],
+            "button_minus":              esbuttontable['esbutton_minus'],
+            "button_multiply":           esbuttontable['esbutton_multiply'],
+            "button_divide":             esbuttontable['esbutton_divide'],
+            "button_period":             esbuttontable['esbutton_period'],
             "button_debug_menu":         None,
             "button_debug_through":      None
         }
