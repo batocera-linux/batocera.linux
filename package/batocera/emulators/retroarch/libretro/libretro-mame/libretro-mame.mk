b49744ce17a5890da1777e85cd8cce5e87c66d52
Juliano Dorigão <jdorigao@gmail.com>
2021-05-29 12:14:02 -0400

    Revert libretro-mame

--- package/batocera/emulators/retroarch/libretro/libretro-mame/libretro-mame.mk
+++ package/batocera/emulators/retroarch/libretro/libretro-mame/libretro-mame.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 # Version: Commits on Jan 31, 2021 (0.228)
-LIBRETRO_MAME_VERSION =  98b0ba18a9109339c2ebf4f5945f6c3575301ba9
+LIBRETRO_MAME_VERSION =  4b1001771c0394b6cb9f54c19f7d11aeaf0821dd
 LIBRETRO_MAME_SITE = $(call github,libretro,mame,$(LIBRETRO_MAME_VERSION))
 LIBRETRO_MAME_LICENSE = MAME
 LIBRETRO_MAME_DEPENDENCIES = retroarch
@@ -20,7 +20,7 @@ ifeq ($(BR2_i386),y)
 	LIBRETRO_MAME_EXTRA_ARGS += PTR64=0 LIBRETRO_CPU=x86 PLATFORM=x86
 endif
 
-ifeq ($(BR2_arm),y) 
+ifeq ($(BR2_arm),y)
 	LIBRETRO_MAME_EXTRA_ARGS += PTR64=0 LIBRETRO_CPU=arm PLATFORM=arm
 	# workaround for asmjit broken build system (arm backend is not public)
 	LIBRETRO_MAME_ARCHOPTS += -D__arm__ -DASMJIT_BUILD_X86
