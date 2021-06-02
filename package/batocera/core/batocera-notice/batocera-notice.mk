d326bf2b6d3b4ced862d355a04739db2b0744baa
Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>
2021-06-02 07:31:12 +0200

    batocera notice
    Signed-off-by: Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>

--- package/batocera/core/batocera-notice/batocera-notice.mk
+++ package/batocera/core/batocera-notice/batocera-notice.mk
@@ -3,7 +3,7 @@
 # Batocera notice
 #
 ################################################################################
-BATOCERA_NOTICE_VERSION = 456fbde1ef4462ea72f23eb8836fcfa9514c6ca9
+BATOCERA_NOTICE_VERSION = e6241340d292dee20406a0567262a78ff34d45f7
 BATOCERA_NOTICE_SITE = $(call github,batocera-linux,batocera-notice,$(BATOCERA_NOTICE_VERSION))
 
 define BATOCERA_NOTICE_INSTALL_TARGET_CMDS
