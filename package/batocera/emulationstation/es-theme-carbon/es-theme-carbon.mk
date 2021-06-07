850f3601b5e0e27a93108e6f0e7327c7c11ebf21
Juliano Dorigão <jdorigao@gmail.com>
2021-06-07 08:14:09 -0400

    update es

--- package/batocera/emulationstation/es-theme-carbon/es-theme-carbon.mk
+++ package/batocera/emulationstation/es-theme-carbon/es-theme-carbon.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 # Version.: Commits on Apr 18, 2021
-ES_THEME_CARBON_VERSION = a07467ca2fd110ae17295f6a10cc651de315ca7c
+ES_THEME_CARBON_VERSION = 69b9478e10200d27541229eda05e138daa5bb077
 ES_THEME_CARBON_SITE = $(call github,fabricecaruso,es-theme-carbon,$(ES_THEME_CARBON_VERSION))
 
 define ES_THEME_CARBON_INSTALL_TARGET_CMDS
