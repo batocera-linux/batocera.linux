d3a311029666273f753caa2cb7d3a13ceba49ddb
Romain TISSERAND <romain.tisserand@gmail.com>
2021-06-07 19:35:50 +0200

    Bump Daphne (Hypseus-Singe fork) to 2.5.4

--- package/batocera/emulators/daphne/daphne.mk
+++ package/batocera/emulators/daphne/daphne.mk
@@ -3,8 +3,8 @@
 # Hypseus + Singe (a fork of Daphne)
 #
 ################################################################################
-# Version.: Commits on May 19, 2021
-DAPHNE_VERSION = v2.5.2
+# Version.: Commits on Jun 03, 2021
+DAPHNE_VERSION = v2.5.4
 DAPHNE_SITE = https://github.com/DirtBagXon/hypseus-singe
 DAPHNE_SITE_METHOD=git
 DAPHNE_LICENSE = GPLv3
