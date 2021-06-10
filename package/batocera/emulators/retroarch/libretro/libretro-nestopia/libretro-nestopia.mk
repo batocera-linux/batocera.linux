35ec8fbff9e0c168ea2cb5e1adf51aeb68160819
Juliano Dorigão <jdorigao@gmail.com>
2021-06-10 10:54:18 -0400

    rpi3_64: libretro-nestopia

--- package/batocera/emulators/retroarch/libretro/libretro-nestopia/libretro-nestopia.mk
+++ package/batocera/emulators/retroarch/libretro/libretro-nestopia/libretro-nestopia.mk
@@ -10,15 +10,11 @@ LIBRETRO_NESTOPIA_LICENSE = GPLv2
 
 LIBRETRO_NESTOPIA_PLATFORM = $(LIBRETRO_PLATFORM)
 
-ifeq ($(BR2_PACKAGE_BATOCERA_TARGET_RPI3),y)
-	LIBRETRO_NESTOPIA_PLATFORM = rpi3
-endif
-
 ifeq ($(BR2_PACKAGE_BATOCERA_TARGET_S812),y)
-        LIBRETRO_NESTOPIA_PLATFORM = armv
-endif
-
-ifeq ($(BR2_PACKAGE_BATOCERA_TARGET_RPI4),y)
+LIBRETRO_NESTOPIA_PLATFORM = armv
+else ifeq ($(BR2_PACKAGE_BATOCERA_TARGET_RPI3),y)
+LIBRETRO_NESTOPIA_PLATFORM = rpi3_64
+else ifeq ($(BR2_PACKAGE_BATOCERA_TARGET_RPI4),y)
 LIBRETRO_NESTOPIA_PLATFORM = rpi4_64
 else ifeq ($(BR2_aarch64),y)
 LIBRETRO_NESTOPIA_PLATFORM = unix
@@ -31,7 +27,7 @@ endef
 define LIBRETRO_NESTOPIA_INSTALL_TARGET_CMDS
 	$(INSTALL) -D $(@D)/libretro/nestopia_libretro.so \
 		$(TARGET_DIR)/usr/lib/libretro/nestopia_libretro.so
-	
+
 	# Bios
 	mkdir -p $(TARGET_DIR)/usr/share/batocera/datainit/bios
 	$(INSTALL) -D $(@D)/NstDatabase.xml \
