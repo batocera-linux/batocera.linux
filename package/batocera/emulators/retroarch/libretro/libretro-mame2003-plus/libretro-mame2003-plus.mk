0de76bcf598b4603438c118a6ade25b28fe5b292
Demetris Ierokipides <ierokipides.dem@gmail.com>
2021-06-12 04:38:33 +0300

    Mame2003+: Update

--- package/batocera/emulators/retroarch/libretro/libretro-mame2003-plus/libretro-mame2003-plus.mk
+++ package/batocera/emulators/retroarch/libretro/libretro-mame2003-plus/libretro-mame2003-plus.mk
@@ -3,8 +3,8 @@
 # MAME2003 PLUS
 #
 ################################################################################
-# Version.: Commits on May 29, 2021
-LIBRETRO_MAME2003_PLUS_VERSION = c0ef2163bd951abd75a88ebfa826f5b4c5a29a6f
+# Version.: Commits on June 12, 2021
+LIBRETRO_MAME2003_PLUS_VERSION = 0f11bc7439878710f00f58a540f20f83fd71d33c
 LIBRETRO_MAME2003_PLUS_SITE = $(call github,libretro,mame2003-plus-libretro,$(LIBRETRO_MAME2003_PLUS_VERSION))
 LIBRETRO_MAME2003_PLUS_LICENSE = MAME
 
