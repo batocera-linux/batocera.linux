0aebd7c64b25ea42479e872f648d39a4010398ad
Juliano Dorigão <jdorigao@gmail.com>
2021-07-01 15:38:23 -0400

    fix: ecwolfGenerator

--- package/batocera/core/batocera-configgen/configgen/configgen/generators/ecwolf/ecwolfGenerator.py
+++ package/batocera/core/batocera-configgen/configgen/configgen/generators/ecwolf/ecwolfGenerator.py
@@ -1,23 +1,21 @@
 #!/usr/bin/env python
 
 import Command
+import batoceraFiles
 from generators.Generator import Generator
 import controllersConfig
 import os
-
-ecwolfConfig  = batoceraFiles.CONF + "/ecwolf"
-ecwolfSaves   = batoceraFiles.SAVES + "/ecwolf"
 
 class ECWolfGenerator(Generator):
 
     def generate(self, system, rom, playersControllers, gameResolution):
         # Create config folder
-        if not path.isdir(ecwolfConfig):
-            os.mkdir(ecwolfConfig)
+        if not path.isdir(batoceraFiles.ecwolfConfig):
+            os.mkdir(batoceraFiles.ecwolfConfig)
 
         # Create save folder
-        if not path.isdir(ecwolfSaves):
-            os.mkdir(ecwolfSaves)
+        if not path.isdir(batoceraFiles.ecwolfSaves):
+            os.mkdir(batoceraFiles.ecwolfSaves)
 
         os.chdir(rom)
         commandArray = ["/usr/share/ecwolf/ecwolf", "--fullscreen", "--joystick", "--savedir /userdata/saves/ecwolf", "--config /userdata/system/configs/ecwolf/ecwolf.cfg"]
