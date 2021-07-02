90c610c8d5d50a98119e501f777f08e8d77fe306
Romain TISSERAND <romain.tisserand@gmail.com>
2021-07-02 14:33:59 +0200

    Move dosbox-staging to package system

--- package/retrolx/emulators/dosbox-staging/dosbox-staging.mk
+++ package/retrolx/emulators/dosbox-staging/dosbox-staging.mk
@@ -13,6 +13,8 @@ DOSBOX_STAGING_CPPFLAGS = -DNDEBUG
 DOSBOX_STAGING_CFLAGS   = -O3 -fstrict-aliasing -fno-signed-zeros -fno-trapping-math -fassociative-math -frename-registers -ffunction-sections -fdata-sections
 DOSBOX_STAGING_CXXFLAGS = -O3 -fstrict-aliasing -fno-signed-zeros -fno-trapping-math -fassociative-math -frename-registers -ffunction-sections -fdata-sections
 
+DOSBOX_STAGING_PKG_DIR = $(TARGET_DIR)/opt/retrolx/dosbox-staging
+
 ifeq ($(BR2_PACKAGE_BATOCERA_TARGET_RPI1),y)
 DOSBOX_STAGING_CFLAGS   += -mcpu=arm1176jzf-s -mfpu=vfp -mfloat-abi=hard
 DOSBOX_STAGING_CXXFLAGS += -mcpu=arm1176jzf-s -mfpu=vfp -mfloat-abi=hard
@@ -21,25 +23,25 @@ ifeq ($(BR2_PACKAGE_BATOCERA_TARGET_RPI2),y)
 DOSBOX_STAGING_CFLAGS   += -mcpu=cortex-a7 -mfpu=neon-vfpv4 -mfloat-abi=hard
 DOSBOX_STAGING_CXXFLAGS += -mcpu=cortex-a7 -mfpu=neon-vfpv4 -mfloat-abi=hard
 endif
-ifeq ($(BR2_PACKAGE_BATOCERA_TARGET_RPI3),y)
-DOSBOX_STAGING_CFLAGS   += -march=armv8-a+crc -mcpu=cortex-a53 -mtune=cortex-a53
-DOSBOX_STAGING_CXXFLAGS += -march=armv8-a+crc -mcpu=cortex-a53 -mtune=cortex-a53
-endif
 ifeq ($(BR2_PACKAGE_BATOCERA_TARGET_RPI4),y)
 DOSBOX_STAGING_CFLAGS   += -march=armv8-a+crc -mcpu=cortex-a72 -mtune=cortex-a72
 DOSBOX_STAGING_CXXFLAGS += -march=armv8-a+crc -mcpu=cortex-a72 -mtune=cortex-a72
 endif
-ifeq ($(BR2_PACKAGE_BATOCERA_TARGET_TRITIUM_H5)$(BR2_PACKAGE_BATOCERA_TARGET_S905)$(BR2_PACKAGE_BATOCERA_TARGET_S912),y)
-DOSBOX_STAGING_CFLAGS   += -march=armv8-a+crc -mtune=cortex-a53
-DOSBOX_STAGING_CXXFLAGS += -march=armv8-a+crc -mtune=cortex-a53
+ifeq ($(BR2_cortex_a35),y)
+DOSBOX_STAGING_CFLAGS   += -mcpu=cortex-a35 -mtune=cortex-a35
+DOSBOX_STAGING_CXXFLAGS += -mcpu=cortex-a35 -mtune=cortex-a35
 endif
-ifeq ($(BR2_PACKAGE_BATOCERA_TARGET_S905GEN3),y)
-DOSBOX_STAGING_CFLAGS   += -march=armv8-a+crc -mtune=cortex-a55
-DOSBOX_STAGING_CXXFLAGS += -march=armv8-a+crc -mtune=cortex-a55
+ifeq ($(BR2_cortex_a53),y)
+DOSBOX_STAGING_CFLAGS   += -mcpu=cortex-a53 -mtune=cortex-a53
+DOSBOX_STAGING_CXXFLAGS += -mcpu=cortex-a53 -mtune=cortex-a53
+endif
+ifeq ($(BR2_cortex_a55),y)
+DOSBOX_STAGING_CFLAGS   += -mcpu=cortex-a55 -mtune=cortex-a55
+DOSBOX_STAGING_CXXFLAGS += -mcpu=cortex-a55 -mtune=cortex-a55
 endif
 ifeq ($(BR2_PACKAGE_BATOCERA_TARGET_S922X),y)
-DOSBOX_STAGING_CFLAGS   += -march=armv8-a+crc -mcpu=cortex-a73 -mtune=cortex-a73.cortex-a53
-DOSBOX_STAGING_CXXFLAGS += -march=armv8-a+crc -mcpu=cortex-a73 -mtune=cortex-a73.cortex-a53
+DOSBOX_STAGING_CFLAGS   += -mcpu=cortex-a73.cortex-a53 -mtune=cortex-a73.cortex-a53
+DOSBOX_STAGING_CXXFLAGS += -mcpu=cortex-a73.cortex-a53 -mtune=cortex-a73.cortex-a53
 endif
 ifeq ($(BR2_PACKAGE_BATOCERA_TARGET_EXYNOS5422),y)
 DOSBOX_STAGING_CFLAGS   += -mcpu=cortex-a15 -mfpu=neon-vfpv4 -mfpu=neon-vfpv4 -mfloat-abi=hard
@@ -56,12 +58,22 @@ define DOSBOX_STAGING_CONFIGURE_CMDS
                     --enable-core-inline \
                     --enable-dynrec \
                     --enable-unaligned_memory \
-                    --prefix=/usr \
+                    --prefix=/opt/retrolx/dosbox-staging \
                     --with-sdl-prefix="$(STAGING_DIR)/usr";
 endef
 
 define DOSBOX_STAGING_INSTALL_TARGET_CMDS
-        $(INSTALL) -D $(@D)/src/dosbox $(TARGET_DIR)/usr/bin/dosbox-staging
 endef
+
+define DOSBOX_STAGING_MAKE_PKG
+        $(INSTALL) -D $(@D)/src/dosbox $(DOSBOX_STAGING_PKG_DIR)/dosbox-staging
+
+	# Build Pacman package
+	cd $(DOSBOX_STAGING_PKG_DIR) && $(BR2_EXTERNAL_BATOCERA_PATH)/scripts/retrolx-makepkg \
+	$(BR2_EXTERNAL_BATOCERA_PATH)/package/retrolx/emulators/dosbox-staging/PKGINFO \
+	$(BATOCERA_SYSTEM_ARCH) $(HOST_DIR)
+endef
+
+DOSBOX_STAGING_POST_INSTALL_TARGET_HOOKS += DOSBOX_STAGING_MAKE_PKG
 
 $(eval $(autotools-package))
