06ac0e9c5a2ab7b87e00a29d82e81a351857becf
Romain TISSERAND <romain.tisserand@gmail.com>
2021-07-06 19:15:47 +0200

    Drop ogg/mp3 bundled music, switch to modarchive.org public domain XM files

---
+++ package/retrolx/frontends/emulationstation/es-background-musics/es-background-musics.mk
@@ -0,0 +1,17 @@
+################################################################################
+#
+# es-background-musics
+#
+################################################################################
+
+ES_BACKGROUND_MUSICS_VERSION = 2.0
+ES_BACKGROUND_MUSICS_LICENSE = Public Domain
+ES_BACKGROUND_MUSICS_SOURCE=
+
+define ES_BACKGROUND_MUSICS_INSTALL_TARGET_CMDS
+	mkdir -p $(TARGET_DIR)/usr/share/batocera/music
+
+	cp -R $(BR2_EXTERNAL_BATOCERA_PATH)/package/retrolx/frontends/emulationstation/es-background-musics/music/* $(TARGET_DIR)/usr/share/batocera/music/
+endef
+
+$(eval $(generic-package))
---
+++ package/retrolx/frontends/emulationstation/es-background-musics/music/licence.md
@@ -0,0 +1,4 @@
+Public domain modules downloaded from modarchive.org
+
+See here for details:
+https://modarchive.org/index.php?request=view_by_license&query=publicdomain
