5cf2193042ce89af8fea2bbd851d522132bf8e6d
Juliano Dorigão <jdorigao@gmail.com>
2021-07-06 08:51:52 -0400

    bump: libretro-vba-m

--- package/batocera/emulators/retroarch/libretro/libretro-vba-m/libretro-vba-m.mk
+++ package/batocera/emulators/retroarch/libretro/libretro-vba-m/libretro-vba-m.mk
@@ -3,8 +3,7 @@
 # VBA-M
 #
 ################################################################################
-# Version.: Commits on Fab 27, 2021
-LIBRETRO_VBA_M_VERSION = e55ed5c0f49757652fbe0ee82e74da42e8e4d856
+LIBRETRO_VBA_M_VERSION = c08bb6bdb0c8576ea60aa41a22f96eaaeac6adea
 LIBRETRO_VBA_M_SITE = $(call github,visualboyadvance-m,visualboyadvance-m,$(LIBRETRO_VBA_M_VERSION))
 
 define LIBRETRO_VBA_M_BUILD_CMDS
