de6690772616825a6b5acb9a092f7a1466e0ea65
Romain TISSERAND <romain.tisserand@gmail.com>
2021-07-06 16:42:13 +0200

    Move lr-mame2003-plus, lr-mame2010 and lr-scumm to package system. Reduce rootfs

--- package/retrolx/emulators/libretro/libretro-mame2003-plus/libretro-mame2003-plus.mk
+++ package/retrolx/emulators/libretro/libretro-mame2003-plus/libretro-mame2003-plus.mk
@@ -3,12 +3,15 @@
 # MAME2003 PLUS
 #
 ################################################################################
-# Version.: Commits on June 23, 2021
-LIBRETRO_MAME2003_PLUS_VERSION = 2b5e3c5527ba3db98e66f028ceebf2d47a825031
+# Version.: Commits on July 05, 2021
+LIBRETRO_MAME2003_PLUS_VERSION = 4cd65b604b52f3b622e7b45519f476526b2b39ce
 LIBRETRO_MAME2003_PLUS_SITE = $(call github,libretro,mame2003-plus-libretro,$(LIBRETRO_MAME2003_PLUS_VERSION))
 LIBRETRO_MAME2003_PLUS_LICENSE = MAME
 
 LIBRETRO_MAME2003_PLUS_PLATFORM = $(LIBRETRO_PLATFORM)
+
+LIBRETRO_MAME2003_PLUS_PKG_DIR = $(TARGET_DIR)/opt/retrolx/libretro
+LIBRETRO_MAME2003_PLUS_PKG_INSTALL_DIR = /userdata/packages/$(BATOCERA_SYSTEM_ARCH)/lr-mame2003-plus
 
 ifeq ($(BR2_PACKAGE_BATOCERA_TARGET_RPI3),y)
 LIBRETRO_MAME2003_PLUS_PLATFORM = rpi3_64
@@ -36,15 +39,6 @@ define LIBRETRO_MAME2003_PLUS_BUILD_CMDS
 endef
 
 define LIBRETRO_MAME2003_PLUS_INSTALL_TARGET_CMDS
-	$(INSTALL) -D $(@D)/mame2003_plus_libretro.so \
-		$(TARGET_DIR)/usr/lib/libretro/mame078plus_libretro.so
-
-	# Bios
-    # Need to think of another way to use these files.
-    # They take up a lot of space on tmpfs.
-	mkdir -p $(TARGET_DIR)/usr/share/batocera/datainit/bios/mame2003-plus/samples
-	cp -r $(@D)/metadata/* \
-		$(TARGET_DIR)/usr/share/batocera/datainit/bios/mame2003-plus
 endef
 
 define LIBRETRO_MAME2003_PLUS_NAMCO_QUICK_FIX
@@ -52,6 +46,24 @@ define LIBRETRO_MAME2003_PLUS_NAMCO_QUICK_FIX
 	$(SED) 's|to continue|on Keyboard, or Left, Right on Joystick to continue|g' $(@D)/src/ui_text.c
 endef
 
-LIBRETRO_MAME2003_PLUS_PRE_BUILD_HOOKS += LIBRETRO_MAME2003_PLUS_NAMCO_QUICK_FIX
+define LIBRETRO_MAME2003_PLUS_MAKEPKG
+	# Create directories
+	mkdir -p $(LIBRETRO_MAME2003_PLUS_PKG_DIR)$(LIBRETRO_MAME2003_PLUS_PKG_INSTALL_DIR)/bios/samples
 
+	# Copy package files
+	cp -pr $(@D)/mame2003_plus_libretro.so $(LIBRETRO_MAME2003_PLUS_PKG_DIR)$(LIBRETRO_MAME2003_PLUS_PKG_INSTALL_DIR)
+	cp -pr $(@D)/metadata/* $(LIBRETRO_MAME2003_PLUS_PKG_DIR)$(LIBRETRO_MAME2003_PLUS_PKG_INSTALL_DIR)/bios/
+
+	# Build Pacman package
+	cd $(LIBRETRO_MAME2003_PLUS_PKG_DIR) && $(BR2_EXTERNAL_BATOCERA_PATH)/scripts/retrolx-makepkg \
+	$(BR2_EXTERNAL_BATOCERA_PATH)/package/retrolx/emulators/libretro/libretro-mame2003-plus/PKGINFO \
+	$(BATOCERA_SYSTEM_ARCH) $(HOST_DIR)
+	mv $(TARGET_DIR)/opt/retrolx/*.zst $(BR2_EXTERNAL_BATOCERA_PATH)/repo/$(BATOCERA_SYSTEM_ARCH)/
+
+	# Cleanup
+	rm -Rf $(TARGET_DIR)/opt/retrolx/*
+endef
+
+LIBRETRO_MAME2003_PLUS_PRE_BUILD_HOOKS += LIBRETRO_MAME2003_PLUS_NAMCO_QUICK_FIX
+LIBRETRO_MAME2003_PLUS_POST_INSTALL_TARGET_HOOKS = LIBRETRO_MAME2003_PLUS_MAKEPKG
 $(eval $(generic-package))
--- package/retrolx/emulators/libretro/libretro-scummvm/libretro-scummvm.mk
+++ package/retrolx/emulators/libretro/libretro-scummvm/libretro-scummvm.mk
@@ -10,6 +10,10 @@ LIBRETRO_SCUMMVM_LICENSE = GPLv2
 
 LIBRETRO_SCUMMVM_PLATFORM = $(LIBRETRO_PLATFORM)
 
+LIBRETRO_SCUMMVM_PKG_DIR = $(TARGET_DIR)/opt/retrolx/libretro
+LIBRETRO_SCUMMVM_PKG_INSTALL_DIR = /userdata/packages/$(BATOCERA_SYSTEM_ARCH)/lr-scummvm
+
+
 ifeq ($(BR2_PACKAGE_BATOCERA_TARGET_S812),y)
 LIBRETRO_SCUMMVM_PLATFORM = armv cortexa9 neon hardfloat
 else ifeq ($(BR2_PACKAGE_BATOCERA_TARGET_RPI3),y)
@@ -26,8 +30,26 @@ define LIBRETRO_SCUMMVM_BUILD_CMDS
 endef
 
 define LIBRETRO_SCUMMVM_INSTALL_TARGET_CMDS
-	$(INSTALL) -D $(@D)/backends/platform/libretro/build/scummvm_libretro.so \
-		$(TARGET_DIR)/usr/lib/libretro/scummvm_libretro.so
 endef
+
+define LIBRETRO_SCUMMVM_MAKEPKG
+	# Create directories
+	mkdir -p $(LIBRETRO_SCUMMVM_PKG_DIR)$(LIBRETRO_SCUMMVM_PKG_INSTALL_DIR)/
+
+	# Copy package files
+	cp -pr $(@D)/backends/platform/libretro/build/scummvm_libretro.so \
+	$(LIBRETRO_SCUMMVM_PKG_DIR)$(LIBRETRO_SCUMMVM_PKG_INSTALL_DIR)
+
+	# Build Pacman package
+	cd $(LIBRETRO_SCUMMVM_PKG_DIR) && $(BR2_EXTERNAL_BATOCERA_PATH)/scripts/retrolx-makepkg \
+	$(BR2_EXTERNAL_BATOCERA_PATH)/package/retrolx/emulators/libretro/libretro-scummvm/PKGINFO \
+	$(BATOCERA_SYSTEM_ARCH) $(HOST_DIR)
+	mv $(TARGET_DIR)/opt/retrolx/*.zst $(BR2_EXTERNAL_BATOCERA_PATH)/repo/$(BATOCERA_SYSTEM_ARCH)/
+
+	# Cleanup
+	rm -Rf $(TARGET_DIR)/opt/retrolx/*
+endef
+
+LIBRETRO_SCUMMVM_POST_INSTALL_TARGET_HOOKS = LIBRETRO_SCUMMVM_MAKEPKG
 
 $(eval $(generic-package))
