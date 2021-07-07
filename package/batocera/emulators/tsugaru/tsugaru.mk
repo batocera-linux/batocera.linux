f0db18fb523bfc122a51b5580cf5f137f1429882
Lio <lionel.bringuier@gmail.com>
2021-07-07 09:54:57 -0700

    Fixed FMTowns. Added support for CD-based games.

--- package/batocera/emulators/tsugaru/tsugaru.mk
+++ package/batocera/emulators/tsugaru/tsugaru.mk
@@ -3,8 +3,8 @@
 # TSUGARU
 #
 ################################################################################
-# Version.: Commits on May 19, 2021
-TSUGARU_VERSION = v20210611
+# Version.: Commits on July 5, 2021
+TSUGARU_VERSION = 3c824b1b3ff971ea0f1180b3adeeb5e470286c36
 TSUGARU_SITE = $(call github,captainys,TOWNSEMU,$(TSUGARU_VERSION))
 TSUGARU_DEPENDENCIES = libglu
 TSUGARU_LICENSE = GPLv2
@@ -22,9 +22,11 @@ TSUGARU_CONF_ENV += LDFLAGS=-lpthread
 
 define TSUGARU_INSTALL_TARGET_CMDS
         mkdir -p $(TARGET_DIR)/usr/bin
-
         $(INSTALL) -D $(@D)/src/buildroot-build/main_cui/Tsugaru_CUI \
                 $(TARGET_DIR)/usr/bin/
+
+	mkdir -p $(TARGET_DIR)/usr/share/evmapy
+	cp $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/emulators/tsugaru/fmtowns.keys $(TARGET_DIR)/usr/share/evmapy
 endef
 
 $(eval $(cmake-package))
