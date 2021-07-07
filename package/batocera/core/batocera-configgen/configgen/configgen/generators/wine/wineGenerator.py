eae25b81b8163986c9875367460bbcb0c8985c65
Ian Murray <tvsian@gmail.com>
2021-07-07 19:39:53 -0400

    Revert WINE changes
    To be moved to batocera-wine

--- package/batocera/core/batocera-configgen/configgen/configgen/generators/wine/wineGenerator.py
+++ package/batocera/core/batocera-configgen/configgen/configgen/generators/wine/wineGenerator.py
@@ -6,7 +6,6 @@ import os
 from os import path
 import controllersConfig
 
-
 class WineGenerator(Generator):
 
     def generate(self, system, rom, playersControllers, gameResolution):
@@ -14,14 +13,6 @@ class WineGenerator(Generator):
             commandArray = ["batocera-wine", "windows", "install", rom]
             return Command.Command(array=commandArray)
         elif system.name == "windows":
-            if os.path.exists(rom + "/autorun.cmd"):
-                with open(rom + "/autorun.cmd") as fp:
-                    for line in fp:
-                        if line.startswith("LANG="):
-                            newLang = (line.rstrip()).split("=")
-                            os.environ["LANG"] = newLang[1]
-                            os.environ["LANGUAGE"] = newLang[1]
-                            os.environ["LC_ALL"] = newLang[1]
             commandArray = ["batocera-wine", "windows", "play", rom]
             return Command.Command(array=commandArray,env={
                 "SDL_GAMECONTROLLERCONFIG": controllersConfig.generateSdlGameControllerConfig(playersControllers),
