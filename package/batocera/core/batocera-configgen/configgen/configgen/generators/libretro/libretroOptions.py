79e5421272a91195fad8c44b131dd58e4833e37e
soaresden <soaresden@gmail.com>
2021-08-06 20:34:59 +0200

    Smart Coloring and Disabled
    Add Smart Coloring and Disabled

--- package/batocera/core/batocera-configgen/configgen/configgen/generators/libretro/libretroOptions.py
+++ package/batocera/core/batocera-configgen/configgen/configgen/generators/libretro/libretroOptions.py
@@ -778,11 +778,22 @@ def generateCoreSettings(coreSettings, system, rom):
 
         if (system.name == 'gb'):
             # GB: Colorization of GB games
-            if system.isOptSet('gb_colorization') and system.config['gb_colorization'] != 'none':
-                coreSettings.save('gambatte_gb_colorization',     '"internal"')
-                coreSettings.save('gambatte_gb_internal_palette', '"' + system.config['gb_colorization'] + '"')
+            if system.isOptSet('gb_colorization'):
+                if system.config['gb_colorization'] == 'none':                           #No Selection --> Classic Green
+                    coreSettings.save('gambatte_gb_colorization',     '"internal"')
+                    coreSettings.save('gambatte_gb_internal_palette', '"Special 1"')
+                elif system.config['gb_colorization'] == 'GB - Disabled':                #Disabled --> Black and White Color
+                    coreSettings.save('gambatte_gb_colorization',     '"disabled"')
+                    coreSettings.save('gambatte_gb_internal_palette', '"Special 1"')
+                elif system.config['gb_colorization'] == 'GB - SmartColor':              #Smart Coloring --> Gambatte's most colorful/appropriate color
+                    coreSettings.save('gambatte_gb_colorization',     '"auto"')
+                    coreSettings.save('gambatte_gb_internal_palette', '"Special 1"')
+                else:                                                                    #User Selection
+                    coreSettings.save('gambatte_gb_colorization',     '"internal"')           
+                    coreSettings.save('gambatte_gb_internal_palette', '"' + system.config['gb_colorization'] + '"')
             else:
-                coreSettings.save('gambatte_gb_colorization',     '"Special 1"')
+                coreSettings.save('gambatte_gb_colorization',         '"internal"')      #It's an empty file, set to Classic Green
+                coreSettings.save('gambatte_gb_internal_palette',     '"Special 1"')
 
     if (system.config['core'] == 'mgba'):
         # Skip BIOS intro
