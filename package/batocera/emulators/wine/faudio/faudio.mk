cc0dc122b62a049bcb4bc6e97575b4e180b55dbe
liberodark <liberodark@gmail.com>
2021-08-10 17:02:55 +0200

    Bump faudio

--- package/batocera/emulators/wine/faudio/faudio.mk
+++ package/batocera/emulators/wine/faudio/faudio.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-FAUDIO_VERSION = 21.06
+FAUDIO_VERSION = 21.08
 FAUDIO_SITE = $(call github,FNA-XNA,FAudio,$(FAUDIO_VERSION))
 
 FAUDIO_LICENSE = ZLIB
