92c03e79dad48d410be2f4ae6e46502c9d2b1e4e
liberodark <liberodark@gmail.com>
2021-08-12 01:55:38 +0200

    Bump Lutris Again...

--- package/batocera/emulators/wine/wine-lutris/wine-lutris.mk
+++ package/batocera/emulators/wine/wine-lutris/wine-lutris.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-WINE_LUTRIS_VERSION = lutris-6.14-2
+WINE_LUTRIS_VERSION = lutris-6.14-3
 WINE_LUTRIS_SITE = $(call github,lutris,wine,$(WINE_LUTRIS_VERSION))
 WINE_LUTRIS_LICENSE = LGPL-2.1+
 WINE_LUTRIS_DEPENDENCIES = host-bison host-flex host-wine-lutris
