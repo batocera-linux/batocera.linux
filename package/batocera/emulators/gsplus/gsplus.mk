176710f21253433bec60e56c968d26affd939963
Lio <lionel.bringuier@gmail.com>
2021-08-16 06:12:07 -0700

    Clean up makefile

--- package/batocera/emulators/gsplus/gsplus.mk
+++ package/batocera/emulators/gsplus/gsplus.mk
@@ -4,22 +4,14 @@
 #
 ################################################################################
 # Version.: Commits on Aug 16, 2019
-
 GSPLUS_VERSION = 480572054518112647c8fae5d7ea7046a6d6ecfb
 GSPLUS_SITE = $(call github,digarok,gsplus,$(GSPLUS_VERSION))
 GSPLUS_LICENSE = GPLv2
 GSPLUS_DEPENDENCIES = sdl2 libpcap
 
-define GSPLUS_SETUP_CMAKE
-	mkdir -p $(@D)/build
-endef
-
-GSPLUS_PRE_CONFIGURE_HOOKS += GSPLUS_SETUP_CMAKE
-
 define GSPLUS_INSTALL_TARGET_CMDS
 	$(INSTALL) -D $(@D)/bin/GSplus $(TARGET_DIR)/usr/bin/
 	$(INSTALL) -D $(@D)/bin/libx_readline.so $(TARGET_DIR)/usr/lib/
-        mkdir -p $(TARGET_DIR)/usr/share/apple2gs
 	cp -f $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/emulators/gsplus/apple2gs.keys $(TARGET_DIR)/usr/share/evmapy
 endef
 
