bc7c1c09906f4b16d0272c7d7c1890f067a70b61
liberodark <liberodark@gmail.com>
2021-08-20 22:59:37 +0200

    Bump VK

--- package/batocera/gpu/vulkan/vulkan-headers/vulkan-headers.mk
+++ package/batocera/gpu/vulkan/vulkan-headers/vulkan-headers.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-VULKAN_HEADERS_VERSION = v1.2.180
+VULKAN_HEADERS_VERSION = v1.2.189
 
 VULKAN_HEADERS_SITE =  $(call github,KhronosGroup,Vulkan-Headers,$(VULKAN_HEADERS_VERSION))
 VULKAN_HEADERS_DEPENDENCIES = 
