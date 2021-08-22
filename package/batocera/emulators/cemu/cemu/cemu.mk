7c2162fdcab0543bc102c1e84588bc8908d6ad9c
liberodark <liberodark@gmail.com>
2021-08-22 16:52:36 +0200

    Bump Cemu

--- package/batocera/emulators/cemu/cemu/cemu.mk
+++ package/batocera/emulators/cemu/cemu/cemu.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-# version 1.25.1
-CEMU_VERSION = 1.25.1
+# version 1.25.2
+CEMU_VERSION = 1.25.2
 CEMU_SOURCE = cemu_$(CEMU_VERSION).zip
 CEMU_SITE = https://cemu.info/releases
 
