1bb7e22a3c68c8a5fd72e75a1263c820c8ad0f19
Lio <lionel.bringuier@gmail.com>
2021-08-24 06:06:08 -0700

    Update Retrotrivia (fix for non-latin games + new questions)

--- package/batocera/utils/batocera-pygame/batocera-pygame.mk
+++ package/batocera/utils/batocera-pygame/batocera-pygame.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-BATOCERA_PYGAME_VERSION = 1.1
-RETROTRIVIA_VERSION = 642cd6035427948ca28088be5dbb398a4d6c2185
+BATOCERA_PYGAME_VERSION = 1.2
+RETROTRIVIA_VERSION = ff695c62cf1d0203d6cb68a6922b80d93c9a6d14
 BATOCERA_PYGAME_LICENSE = GPLv3
 BATOCERA_PYGAME_SITE = $(call github,lbrpdx,retrotrivia,$(RETROTRIVIA_VERSION))
 
@@ -25,8 +25,8 @@ define BATOCERA_PYGAME_INSTALL_SAMPLE
 	$(INSTALL) -D -m 0644 $(@D)/gamelists.py       $(TARGET_DIR)/usr/share/batocera/datainit/roms/pygame/retrotrivia/
 	$(INSTALL) -D -m 0644 $(@D)/assets/*           $(TARGET_DIR)/usr/share/batocera/datainit/roms/pygame/retrotrivia/assets/
 	$(INSTALL) -D -m 0644 $(@D)/README.md          $(TARGET_DIR)/usr/share/batocera/datainit/roms/pygame/retrotrivia/
-	$(INSTALL) -D -m 0644 $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/utils/batocera-pygame/retrotrivia.png        $(TARGET_DIR)/usr/share/batocera/datainit/roms/pygame/images
-	$(INSTALL) -D -m 0644 $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/utils/batocera-pygame/retrotrivia-manual.pdf $(TARGET_DIR)/usr/share/batocera/datainit/roms/pygame/manuals
+	$(INSTALL) -D -m 0644 $(@D)/retrotrivia.png        $(TARGET_DIR)/usr/share/batocera/datainit/roms/pygame/images
+	$(INSTALL) -D -m 0644 $(@D)/retrotrivia-manual.pdf $(TARGET_DIR)/usr/share/batocera/datainit/roms/pygame/manuals
 	$(INSTALL) -D -m 0644 $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/utils/batocera-pygame/retrotrivia.xml        $(TARGET_DIR)/usr/share/batocera/datainit/roms/pygame/gamelist.xml
 
 	# create an alias for pygame to be able to kill it with killall and evmapy
