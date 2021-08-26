6e23e6dc7d7477b3e4bdcca5c1a1836a068815e2
Juliano Dorigão <jdorigao@gmail.com>
2021-08-26 07:33:46 -0400

    bump es-theme-carbon (rpi low performance))

--- package/batocera/emulationstation/es-theme-carbon/es-theme-carbon.mk
+++ package/batocera/emulationstation/es-theme-carbon/es-theme-carbon.mk
@@ -3,8 +3,8 @@
 # EmulationStation theme "Carbon"
 #
 ################################################################################
-# Version.: Commits on Aug 23, 2021
-ES_THEME_CARBON_VERSION = febc0be5e2529db4e4b0d11a9c5a34a4d1347d75
+# Version.: Commits on Aug 26, 2021
+ES_THEME_CARBON_VERSION = 3ec77d70fa0ab7fc74c8f963e37fa69ff6ef591d
 ES_THEME_CARBON_SITE = $(call github,fabricecaruso,es-theme-carbon,$(ES_THEME_CARBON_VERSION))
 
 define ES_THEME_CARBON_INSTALL_TARGET_CMDS
