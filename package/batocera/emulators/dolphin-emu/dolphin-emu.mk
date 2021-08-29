f884a5803dead86bdb915eb84d2f99f93a029b7b
liberodark <liberodark@gmail.com>
2021-08-29 15:20:34 +0200

    Bump Dolphin

--- package/batocera/emulators/dolphin-emu/dolphin-emu.mk
+++ package/batocera/emulators/dolphin-emu/dolphin-emu.mk
@@ -3,8 +3,8 @@
 # DOLPHIN EMU
 #
 ################################################################################
-# Version: Commits on Jun 5, 2021 - 5.0-14344
-DOLPHIN_EMU_VERSION = acc7d3710d60552769f61f4b44bc8533a940df36
+# Version: Commits on Jul 31, 2021 - 5.0-14790
+DOLPHIN_EMU_VERSION = 3cc274880f47d340bd508dba91aaf37c48acd367
 DOLPHIN_EMU_SITE = $(call github,dolphin-emu,dolphin,$(DOLPHIN_EMU_VERSION))
 DOLPHIN_EMU_LICENSE = GPLv2+
 DOLPHIN_EMU_DEPENDENCIES = libevdev ffmpeg zlib libpng lzo libusb libcurl bluez5_utils hidapi xz host-xz
@@ -15,6 +15,7 @@ DOLPHIN_EMU_CONF_OPTS  = -DTHREADS_PTHREAD_ARG=OFF
 DOLPHIN_EMU_CONF_OPTS += -DUSE_DISCORD_PRESENCE=OFF
 DOLPHIN_EMU_CONF_OPTS += -DBUILD_SHARED_LIBS=OFF
 DOLPHIN_EMU_CONF_OPTS += -DDISTRIBUTOR='batocera.linux'
+DOLPHIN_EMU_CONF_OPTS += -DUSE_MGBA=OFF
 
 ifeq ($(BR2_PACKAGE_BATOCERA_TARGET_X86_64),y)
 DOLPHIN_EMU_DEPENDENCIES += xserver_xorg-server qt5base
