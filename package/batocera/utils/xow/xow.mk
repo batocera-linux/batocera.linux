37ba063c45cb8ef945c9be90b9c00a491e96f7f9
not43s <78993338+not43s@users.noreply.github.com>
2021-08-30 11:46:37 -0700

    Improve xbox one usb dongle support

--- package/batocera/utils/xow/xow.mk
+++ package/batocera/utils/xow/xow.mk
@@ -3,7 +3,7 @@
 # xow
 #
 ################################################################################
-XOW_VERSION = cd271644ead198dcbfeafd3c3411092850adfd3a
+XOW_VERSION = 700529b2517df7d17ad5a2fa0bd679143ac48666
 XOW_SITE = $(call github,medusalix,xow,$(XOW_VERSION))
 XOW_DEPENDENCIES = host-libcurl host-cabextract libusb
 
@@ -13,6 +13,7 @@ endef
 
 define XOW_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 -D $(@D)/xow $(TARGET_DIR)/usr/bin/xow
+	$(INSTALL) -m 0755 -D $(@D)/install/modprobe.conf $(TARGET_DIR)/etc/modprobe.d/xow-blacklist.conf
 	$(INSTALL) -m 0755 -D $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/utils/xow/xow-daemon $(TARGET_DIR)/usr/bin/xow-daemon
 	cp $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/utils/xow/99-xow.rules $(TARGET_DIR)/etc/udev/rules.d
 endef
