59a12a123c711e68b105847d8c72af7344a14ebb
Daniel Martin <dmanlfc@gmail.com>
2021-08-30 20:40:22 +1000

    update xow for xbox one controllers

--- package/batocera/utils/xow/xow.mk
+++ package/batocera/utils/xow/xow.mk
@@ -3,7 +3,7 @@
 # xow
 #
 ################################################################################
-XOW_VERSION = cd271644ead198dcbfeafd3c3411092850adfd3a
+XOW_VERSION = 700529b2517df7d17ad5a2fa0bd679143ac48666
 XOW_SITE = $(call github,medusalix,xow,$(XOW_VERSION))
 XOW_DEPENDENCIES = host-libcurl host-cabextract libusb
 
@@ -13,8 +13,10 @@ endef
 
 define XOW_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 -D $(@D)/xow $(TARGET_DIR)/usr/bin/xow
+	$(INSTALL) -D -m 0644 $(@D)/install/udev.rules $(TARGET_DIR)/etc/udev/rules.d/99-xow.rules
+	$(INSTALL) -D -m 0644 $(@D)/install/modules.conf $(TARGET_DIR)/etc/modules-load.d/xow-uinput.conf
+	$(INSTALL) -D -m 0644 $(@D)/install/modprobe.conf $(TARGET_DIR)/etc/modprobe.d/xow-blacklist.conf
 	$(INSTALL) -m 0755 -D $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/utils/xow/xow-daemon $(TARGET_DIR)/usr/bin/xow-daemon
-	cp $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/utils/xow/99-xow.rules $(TARGET_DIR)/etc/udev/rules.d
 endef
 
 $(eval $(generic-package))
