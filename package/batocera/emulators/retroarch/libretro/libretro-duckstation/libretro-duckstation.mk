ec7966f8f522c4ef64bb3e1de8d6b38e3cc4d257
Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>
2021-09-04 06:46:58 +0000

    fix lr duck
    Signed-off-by: Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>

--- package/batocera/emulators/retroarch/libretro/libretro-duckstation/libretro-duckstation.mk
+++ package/batocera/emulators/retroarch/libretro/libretro-duckstation/libretro-duckstation.mk
@@ -9,15 +9,12 @@ LIBRETRO_DUCKSTATION_VERSION = c26eca7d0d9e6559f3a6a7af94dfa2a850223a19
 LIBRETRO_DUCKSTATION_SITE = $(call github,batocera-linux,lr-duckstation,$(LIBRETRO_DUCKSTATION_VERSION))
 LIBRETRO_DUCKSTATION_LICENSE = non-commercial
 
-ifeq ($(BR2_PACKAGE_BATOCERA_TARGET_X86_64),y)
+LIBRETRO_DUCKSTATION_PK = unknown
+ifeq ($(BR2_x86_64),y)
 LIBRETRO_DUCKSTATION_PK = duckstation_libretro_linux_x64.zip
-else ifeq ($(BR2_PACKAGE_BATOCERA_TARGET_RK3326_ANY),y)
+else ifeq ($(BR2_aarch64),y)
 LIBRETRO_DUCKSTATION_PK = duckstation_libretro_linux_aarch64.zip
-else ifeq ($(BR2_PACKAGE_BATOCERA_TARGET_RPI4),y)
-LIBRETRO_DUCKSTATION_PK = duckstation_libretro_linux_aarch64.zip
-else ifeq ($(BR2_PACKAGE_BATOCERA_TARGET_RPI3),y)
-LIBRETRO_DUCKSTATION_PK = duckstation_libretro_linux_aarch64.zip
-else # armv7 32-bit version
+else ifeq ($(BR2_arm),y)
 LIBRETRO_DUCKSTATION_PK = duckstation_libretro_linux_armv7.zip
 endif
 
