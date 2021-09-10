34ca2d96b27839fdd6a054a565924cc08bf090d0
Lio <lionel.bringuier@gmail.com>
2021-09-10 04:47:04 -0700

    Fix switchres for CRT, with missing grid

--- package/batocera/utils/switchres/switchres.mk
+++ package/batocera/utils/switchres/switchres.mk
@@ -3,8 +3,8 @@
 # SwitchRes
 #
 ################################################################################
-# Version: Commits from Feb 20th, 2021
-SWITCHRES_VERSION = a73fe3e3587a91327ead48e77f1852376ea07738
+# Version: Commits from Jun 28th, 2021
+SWITCHRES_VERSION = a8af8e8f7f54d24414b4a8595a84c0f0f23e14a1
 SWITCHRES_SITE = $(call github,antonioginer,switchres,$(SWITCHRES_VERSION))
 
 SWITCHRES_DEPENDENCIES = libdrm xserver_xorg-server
@@ -17,8 +17,8 @@ define SWITCHRES_BUILD_CMDS
 	CXX="$(TARGET_CXX)" \
 	PREFIX="$(STAGING_DIR)/usr" \
 	PKG_CONFIG_PATH="$(STAGING_DIR)/usr/lib/pkgconfig" \
-	CPPFLAGS="-I$(STAGING_DIR)/usr/include" \
-	$(MAKE) all
+	CPPFLAGS="-I$(STAGING_DIR)/usr/include -I$(STAGING_DIR)/usr/include/libdrm" \
+	$(MAKE) all grid
 endef
 
 define SWITCHRES_INSTALL_STAGING_CMDS
@@ -34,6 +34,7 @@ endef
 define SWITCHRES_INSTALL_TARGET_CMDS
 	$(INSTALL) -D -m 0644 $(@D)/libswitchres.so $(TARGET_DIR)/usr/lib/libswitchres.so
 	$(INSTALL) -D -m 0755 $(@D)/switchres $(TARGET_DIR)/usr/bin/switchres
+	$(INSTALL) -D -m 0755 $(@D)/grid $(TARGET_DIR)/usr/bin/grid
 endef
 
 $(eval $(generic-package))
