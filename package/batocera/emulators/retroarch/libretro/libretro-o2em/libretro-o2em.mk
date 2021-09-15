d67e15459d937bf0f6834a04faf06cfe9c6615b4
Juliano Dorigão <jdorigao@gmail.com>
2021-09-15 08:28:35 -0400

    rpi3: cleaning BR_arm

--- package/batocera/emulators/retroarch/libretro/libretro-o2em/libretro-o2em.mk
+++ package/batocera/emulators/retroarch/libretro/libretro-o2em/libretro-o2em.mk
@@ -15,9 +15,6 @@ LIBRETRO_O2EM_PLATFORM = armv neon
 
 else ifeq ($(BR2_aarch64),y)
 LIBRETRO_O2EM_PLATFORM = unix
-
-else ifeq ($(BR2_PACKAGE_BATOCERA_TARGET_RPI3),y)
-LIBRETRO_O2EM_PLATFORM = armv neon
 endif
 
 define LIBRETRO_O2EM_BUILD_CMDS
