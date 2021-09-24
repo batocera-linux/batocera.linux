7040a1f1a974412aab532bebf391955c9c398102
Juliano Dorigão <jdorigao@gmail.com>
2021-09-23 22:42:58 -0400

    rpi1: rpi_firmware

--- board/batocera/raspberrypi/rpi/create-boot-script.sh
+++ board/batocera/raspberrypi/rpi/create-boot-script.sh
@@ -14,16 +14,15 @@ BINARIES_DIR=$4
 TARGET_DIR=$5
 BATOCERA_BINARIES_DIR=$6
 
-mkdir -p "${BATOCERA_BINARIES_DIR}/boot/boot"         || exit 1
+mkdir -p "${BATOCERA_BINARIES_DIR}/boot/boot" || exit 1
 
 cp -pr "${BINARIES_DIR}/rpi-firmware/"* "${BATOCERA_BINARIES_DIR}/boot/" || exit 1
-cp -f "${BINARIES_DIR}/"*.dtb      	"${BATOCERA_BINARIES_DIR}/boot/" || exit 1
-cp "${BOARD_DIR}/boot/config.txt"  	"${BATOCERA_BINARIES_DIR}/boot/" || exit 1
-cp "${BOARD_DIR}/boot/cmdline.txt" 	"${BATOCERA_BINARIES_DIR}/boot/" || exit 1
+cp -f  "${BINARIES_DIR}/"*.dtb          "${BATOCERA_BINARIES_DIR}/boot/" || exit 1
+cp     "${BOARD_DIR}/boot/config.txt"   "${BATOCERA_BINARIES_DIR}/boot/" || exit 1
+cp     "${BOARD_DIR}/boot/cmdline.txt"  "${BATOCERA_BINARIES_DIR}/boot/" || exit 1
 
-KERNEL_VERSION=$(grep -E "^BR2_LINUX_KERNEL_VERSION=" "${BR2_CONFIG}" | sed -e s+'^BR2_LINUX_KERNEL_VERSION="\(.*\)"$'+'\1'+)
-cp "${BINARIES_DIR}/zImage"          "${BATOCERA_BINARIES_DIR}/boot/boot/linux"           || exit 1
-cp "${BINARIES_DIR}/initrd.gz"       "${BATOCERA_BINARIES_DIR}/boot/boot"                 || exit 1
+cp "${BINARIES_DIR}/Image"           "${BATOCERA_BINARIES_DIR}/boot/boot/linux"           || exit 1
+cp "${BINARIES_DIR}/initrd.gz"       "${BATOCERA_BINARIES_DIR}/boot/boot/"                || exit 1
 cp "${BINARIES_DIR}/rootfs.squashfs" "${BATOCERA_BINARIES_DIR}/boot/boot/batocera.update" || exit 1
 
 exit 0
