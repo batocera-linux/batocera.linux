b39c229eef27ca1a7c06300af01b781c9a0f9258
Juliano Dorigão <jdorigao@gmail.com>
2021-09-26 18:40:41 -0400

    bump: mupen64plus-video-rice

--- package/batocera/emulators/mupen64plus/mupen64plus-video-rice/mupen64plus-video-rice.mk
+++ package/batocera/emulators/mupen64plus/mupen64plus-video-rice/mupen64plus-video-rice.mk
@@ -3,8 +3,8 @@
 # mupen64plus video video-rice
 #
 ################################################################################
-# Version.: Commits on Feb 11, 2021
-MUPEN64PLUS_VIDEO_RICE_VERSION = 8cd53f0272cd3cd44510a01fd0aedcb7e5df0790
+# Version.: Commits on Sep 14, 2021
+MUPEN64PLUS_VIDEO_RICE_VERSION = 3da7d800989717111da5992a50a8ac410cac038f
 MUPEN64PLUS_VIDEO_RICE_SITE = $(call github,mupen64plus,mupen64plus-video-rice,$(MUPEN64PLUS_VIDEO_RICE_VERSION))
 MUPEN64PLUS_VIDEO_RICE_LICENSE = GPLv2
 MUPEN64PLUS_VIDEO_RICE_DEPENDENCIES = sdl2 alsa-lib mupen64plus-core boost
