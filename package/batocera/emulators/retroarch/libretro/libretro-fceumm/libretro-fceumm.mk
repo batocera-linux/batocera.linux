1f5f16b6c5c6f65b0c7784f2e6875de9ce0be291
Juliano Dorigão <jdorigao@gmail.com>
2021-09-27 09:55:18 -0400

    bump: libretro-fceumm

--- package/batocera/emulators/retroarch/libretro/libretro-fceumm/libretro-fceumm.mk
+++ package/batocera/emulators/retroarch/libretro/libretro-fceumm/libretro-fceumm.mk
@@ -3,8 +3,8 @@
 # FCEUMM
 #
 ################################################################################
-# Last commit: July 30, 2021
-LIBRETRO_FCEUMM_VERSION = afe2b4d9650d9e468b20636a3eab6bac2b8f9844
+# Version.: commit: Sep 24, 2021
+LIBRETRO_FCEUMM_VERSION = 6881c1fd2a2e268c50ceac8c1605eaa6f12528f9
 LIBRETRO_FCEUMM_SITE = $(call github,libretro,libretro-fceumm,$(LIBRETRO_FCEUMM_VERSION))
 LIBRETRO_FCEUMM_LICENSE = GPLv2
 
@@ -18,10 +18,9 @@ LIBRETRO_FCEUMM_PLATFORM = rpi4
 
 else ifeq ($(BR2_aarch64),y)
 LIBRETRO_FCEUMM_PLATFORM = unix
-endif
 
-ifeq ($(BR2_PACKAGE_BATOCERA_TARGET_S812),y)
-	LIBRETRO_FCEUMM_PLATFORM = armv
+else ifeq ($(BR2_PACKAGE_BATOCERA_TARGET_S812),y)
+LIBRETRO_FCEUMM_PLATFORM = armv
 endif
 
 define LIBRETRO_FCEUMM_BUILD_CMDS
