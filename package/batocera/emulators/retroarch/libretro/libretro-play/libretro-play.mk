011cc936597803f417d0c2c47bc3fe7e0a695ede
liberodark <liberodark@gmail.com>
2021-09-27 16:23:55 +0200

    Bump Play

--- package/batocera/emulators/retroarch/libretro/libretro-play/libretro-play.mk
+++ package/batocera/emulators/retroarch/libretro/libretro-play/libretro-play.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBRETRO_PLAY_VERSION = 67a826fed6d96c3acc0c5a3d1d75b05a12fb865d
+LIBRETRO_PLAY_VERSION = 06b4753d6d2b1f2bfb25096eecb3dd7bdb6d81b9
 LIBRETRO_PLAY_SITE = https://github.com/jpd002/Play-.git
 LIBRETRO_PLAY_LICENSE = BSD
 LIBRETRO_PLAY_DEPENDENCIES = qt5base qt5x11extras xserver_xorg-server libglew
