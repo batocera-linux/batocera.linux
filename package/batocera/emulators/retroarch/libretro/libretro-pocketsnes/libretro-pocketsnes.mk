adf08b2f3fa98bbe793163b38f13c2dc6ceba493
Juliano Dorigão <jdorigao@gmail.com>
2021-09-27 11:32:25 -0400

    bump: libretro-pocketsnes

--- package/batocera/emulators/retroarch/libretro/libretro-pocketsnes/libretro-pocketsnes.mk
+++ package/batocera/emulators/retroarch/libretro/libretro-pocketsnes/libretro-pocketsnes.mk
@@ -3,7 +3,7 @@
 # POCKETSNESS
 #
 ################################################################################
-# Version.: Commits on Mar 12, 2021
+# Version.: Commits on Jun 07, 2021
 LIBRETRO_POCKETSNES_VERSION = 48b67ee60cf32d587b2d3e9f4cd37c84c647666e
 LIBRETRO_POCKETSNES_SITE = $(call github,libretro,snes9x2002,$(LIBRETRO_POCKETSNES_VERSION))
 LIBRETRO_POCKETSNES_LICENSE = Non-commercial
