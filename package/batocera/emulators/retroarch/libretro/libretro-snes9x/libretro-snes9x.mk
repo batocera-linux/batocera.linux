ff4d9b3f8f823b2fbc96881f43f4bc8af8983c4e
Juliano Dorigão <jdorigao@gmail.com>
2021-09-28 08:36:45 -0400

    lr-snes9x: patch removed

--- package/batocera/emulators/retroarch/libretro/libretro-snes9x/libretro-snes9x.mk
+++ package/batocera/emulators/retroarch/libretro/libretro-snes9x/libretro-snes9x.mk
@@ -3,8 +3,8 @@
 # SNES9X
 #
 ################################################################################
-# Version.: Commits on Aug 17, 2021
-LIBRETRO_SNES9X_VERSION = 74257ec252951bb8615ec8412f03d2203cb12dc3
+# Version.: Commits on Sep 28, 2021
+LIBRETRO_SNES9X_VERSION = 714ec560bc5f8d4301acef417b77a3656f353771
 LIBRETRO_SNES9X_SITE = $(call github,libretro,snes9x,$(LIBRETRO_SNES9X_VERSION))
 LIBRETRO_SNES9X_LICENSE = Non-commercial
 
@@ -12,10 +12,13 @@ LIBRETRO_SNES9X_PLATFORM = $(LIBRETRO_PLATFORM)
 
 ifeq ($(BR2_PACKAGE_BATOCERA_TARGET_S922X),y)
 LIBRETRO_SNES9X_PLATFORM = CortexA73_G12B
+
 else ifeq ($(BR2_PACKAGE_BATOCERA_TARGET_RPI3),y)
 LIBRETRO_SNES9X_PLATFORM = rpi3_64
+
 else ifeq ($(BR2_PACKAGE_BATOCERA_TARGET_RPI4),y)
 LIBRETRO_SNES9X_PLATFORM = rpi4_64
+
 else ifeq ($(BR2_aarch64),y)
 LIBRETRO_SNES9X_PLATFORM = unix
 endif
