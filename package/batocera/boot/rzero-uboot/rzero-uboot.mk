eadebbfe630bbaebef6fd15d9d690bafea9db682
Demetris Ierokipides <ierokipides.dem@gmail.com>
2021-09-29 15:31:38 +0300

    Raxda Zero: use emmc u-boot binary

--- package/batocera/boot/rzero-uboot/rzero-uboot.mk
+++ package/batocera/boot/rzero-uboot/rzero-uboot.mk
@@ -4,11 +4,12 @@
 #
 ################################################################################
 
-RZERO_UBOOT_VERSION = 895299bd84b0a5782851d0f142cc3aa686dc5a95
+RZERO_UBOOT_VERSION = 0008019abdd487e75c094b01a09d128c3e2358fb
 RZERO_UBOOT_SITE = https://github.com/Multi-Retropie/rzero-uboot.git
 RZERO_UBOOT_SITE_METHOD=git
 
 define RZERO_UBOOT_INSTALL_TARGET_CMDS
+	cp $(@D)/u-boot.bin $(BINARIES_DIR)/u-boot.bin
 	cp $(@D)/u-boot.bin.sd.bin $(BINARIES_DIR)/u-boot.bin.sd.bin
 endef
 
