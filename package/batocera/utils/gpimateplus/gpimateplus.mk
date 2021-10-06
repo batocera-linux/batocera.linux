2ea3693a0a292aaec95b8970b5abe894e185e827
Nathan Hand <nathanh@manu.com.au>
2021-10-06 16:53:13 +1100

    fix squashed retroarch with aspect_ratio_index = 21

--- package/batocera/utils/gpimateplus/gpimateplus.mk
+++ package/batocera/utils/gpimateplus/gpimateplus.mk
@@ -10,7 +10,7 @@ GPIMATEPLUS_SITE = https://github.com/martinx72/GPiMatePlusHowTo/raw/main
 define GPIMATEPLUS_INSTALL_TARGET_CMDS
 	mkdir -p $(BINARIES_DIR)/rpi-firmware/overlays
 	cp $(@D)/disable-pcie.dtbo $(BINARIES_DIR)/rpi-firmware/overlays/disable-pcie.dtbo
-	$(INSTALL) -D -m 0755 $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/utils/gpimateplus/S92rotate $(TARGET_DIR)/etc/init.d/S92rotate
+	$(INSTALL) -D -m 0755 $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/utils/gpimateplus/S92gpimateplus $(TARGET_DIR)/etc/init.d/S92gpimateplus
 endef
 
 define GPIMATEPLUS_EXTRACT_CMDS
