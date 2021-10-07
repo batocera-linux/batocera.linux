513e22a0548c7cd47e4fd3874f0d3566bd26d630
Juliano Dorigão <jdorigao@gmail.com>
2021-10-07 08:29:05 -0400

    bump: retroarch to v1.9.10

--- package/batocera/emulators/retroarch/retroarch/retroarch.mk
+++ package/batocera/emulators/retroarch/retroarch/retroarch.mk
@@ -3,8 +3,8 @@
 # retroarch
 #
 ################################################################################
-# Version.: Release on Sept 4, 2021
-RETROARCH_VERSION = v1.9.9
+# Version.: Release on Sept 18, 2021
+RETROARCH_VERSION = v1.9.10
 RETROARCH_SITE = $(call github,libretro,RetroArch,$(RETROARCH_VERSION))
 RETROARCH_LICENSE = GPLv3+
 RETROARCH_DEPENDENCIES = host-pkgconf dejavu retroarch-assets flac
