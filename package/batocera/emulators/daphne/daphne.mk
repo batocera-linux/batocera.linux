3c55829de4e7f022c7fcb0c2bf79e4866d986d8d
Juliano Dorigão <jdorigao@gmail.com>
2021-10-08 23:52:18 -0400

    bump: daphne to v2.6.16

--- package/batocera/emulators/daphne/daphne.mk
+++ package/batocera/emulators/daphne/daphne.mk
@@ -3,8 +3,8 @@
 # Hypseus + Singe (a fork of Daphne)
 #
 ################################################################################
-# Version.: Commits on Jun 03, 2021
-DAPHNE_VERSION = v2.6.12
+# Version.: Commits on Oct 05, 2021
+DAPHNE_VERSION = v2.6.16
 DAPHNE_SITE = https://github.com/DirtBagXon/hypseus-singe
 DAPHNE_SITE_METHOD=git
 DAPHNE_LICENSE = GPLv3
