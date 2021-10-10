9005e8340dd47ae730f934c72077cac5a58f1942
Demetris Ierokipides <ierokipides.dem@gmail.com>
2021-10-10 21:31:13 +0300

    S922x: Add support for Beelink GT-King & GT-King-Pro   -  SoC – Amlogic S922X hexa-core big.LITTLE processor with 4x Arm Cortex A73 cores @ up to 1.8 GHz, 2x Arm Cortex A53 cores @ 1.9 GHz, Arm Mali-G52MP4 GPU @ 846MHz; 12nm manufacturing process   -  System Memory – 4GB LPDDR4 RAM   -  Storage – 64GB eMMC flash, microSD card slot   -  Video Output – HDMI 2.1 up to 4K @ 75 Hz, AV port (composite)   -  Video – AVE-10 engine with 4K UHD H.265/VP9/AVS2 video decoding up to 60 fps, Dolby Vision, Advanced HDR10, HDR10+, HLG, and PRIME HDR high-dynamic range support   -  Audio – Digital audio via HDMI, stereo analog audio via 3.5mm AV jack, optical S/PDIF   -  Connectivity – Gigabit Ethernet, dual-band 802.11 b/g/n/ac 2×2 MIMO WiFi 5, Bluetooth 4.1   -  USB – 2x USB 3.0 ports, 1x USB 2.0 OTG port   -  Misc – Upgrade button, green LED   -  Power Supply – 12V DC / 1.5A power barrel jack   -  Dimensions – 108 x 108 x 15.3 mm Signed-off-by: Demetris Ierokipides <ierokipides.dem@gmail.com>

---
+++ board/batocera/amlogic/s922x/gtking/create-boot-script.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# HOST_DIR = host dir
+# BOARD_DIR = board specific dir
+# BUILD_DIR = base dir/build
+# BINARIES_DIR = images dir
+# TARGET_DIR = target dir
+# BATOCERA_BINARIES_DIR = batocera binaries sub directory
+
+HOST_DIR=$1
+BOARD_DIR=$2
+BUILD_DIR=$3
+BINARIES_DIR=$4
+TARGET_DIR=$5
+BATOCERA_BINARIES_DIR=$6
+
+mkdir -p "${BATOCERA_BINARIES_DIR}/build-uboot-gtking"     || exit 1
+cp "${BOARD_DIR}/build-uboot.sh"          "${BATOCERA_BINARIES_DIR}/build-uboot-gtking/" || exit 1
+cd "${BATOCERA_BINARIES_DIR}/build-uboot-gtking/" && ./build-uboot.sh "${HOST_DIR}" "${BOARD_DIR}" "${BINARIES_DIR}" || exit 1
+
+mkdir -p "${BATOCERA_BINARIES_DIR}/boot/boot"     || exit 1
+mkdir -p "${BATOCERA_BINARIES_DIR}/boot/extlinux" || exit 1
+
+cp "${BINARIES_DIR}/Image"           "${BATOCERA_BINARIES_DIR}/boot/boot/linux"           || exit 1
+cp "${BINARIES_DIR}/initrd.gz"       "${BATOCERA_BINARIES_DIR}/boot/boot/initrd.gz"       || exit 1
+cp "${BINARIES_DIR}/rootfs.squashfs" "${BATOCERA_BINARIES_DIR}/boot/boot/batocera.update" || exit 1
+
+cp "${BINARIES_DIR}/meson-g12b-gtking.dtb" "${BATOCERA_BINARIES_DIR}/boot/boot/"          || exit 1
+cp "${BOARD_DIR}/boot/extlinux.conf"       "${BATOCERA_BINARIES_DIR}/boot/extlinux/" || exit 1
+# cp "${BINARIES_DIR}/boot.scr"                          "${BATOCERA_BINARIES_DIR}/boot/"               || exit 1
+# cp "${BOARD_DIR}/boot/logo.bmp"                        "${BATOCERA_BINARIES_DIR}/boot/boot/"          || exit 1
+
+
+# dd if="${BINARIES_DIR}/u-boot.bin.sd-amlogic.bin" of="${BINARIES_DIR}/u-boot1.bin" bs=1   count=444 || exit 1
+# dd if="${BINARIES_DIR}/u-boot.bin.sd-amlogic.bin" of="${BINARIES_DIR}/u-boot2.bin" bs=512 skip=1    || exit 1
+
+exit 0
