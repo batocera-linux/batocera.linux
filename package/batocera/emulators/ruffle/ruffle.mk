72a8dedb72a4e781182a7baeda73e7cd4c45c418
liberodark <liberodark@gmail.com>
2021-10-11 09:08:16 +0200

    Bump Flash

--- package/batocera/emulators/ruffle/ruffle.mk
+++ package/batocera/emulators/ruffle/ruffle.mk
@@ -3,8 +3,8 @@
 # RUFFLE
 #
 ################################################################################
-# Version.: Commits on Sep 26, 2021
-RUFFLE_VERSION = d5b223c1b34db3db7b151ffcdc6c83787fcb4480
+# Version.: Commits on Oct 11, 2021
+RUFFLE_VERSION = 37e3f0903ec45ae2d946b63bcfb0b1c294641cc9
 RUFFLE_SITE = $(call github,ruffle-rs,ruffle,$(RUFFLE_VERSION))
 RUFFLE_LICENSE = GPLv2
 RUFFLE_DEPENDENCIES = host-rustc host-rust-bin openssl
