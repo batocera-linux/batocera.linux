71da0e908cf686ecd301ee7414cd2a11fe92ce4c
Demetris Ierokipides <ierokipides.dem@gmail.com>
2021-10-14 20:58:07 +0300

    S922x: Small fixes and cleaning

--- board/batocera/amlogic/s922x/gtkingpro/create-boot-script.sh
+++ board/batocera/amlogic/s922x/gtkingpro/create-boot-script.sh
@@ -25,13 +25,7 @@ cp "${BINARIES_DIR}/Image"           "${BATOCERA_BINARIES_DIR}/boot/boot/linux" 
 cp "${BINARIES_DIR}/initrd.gz"       "${BATOCERA_BINARIES_DIR}/boot/boot/initrd.gz"       || exit 1
 cp "${BINARIES_DIR}/rootfs.squashfs" "${BATOCERA_BINARIES_DIR}/boot/boot/batocera.update" || exit 1
 
-cp "${BINARIES_DIR}/meson-g12b-gtking-pro.dtb" "${BATOCERA_BINARIES_DIR}/boot/boot/"          || exit 1
-cp "${BOARD_DIR}/boot/extlinux.conf"       "${BATOCERA_BINARIES_DIR}/boot/extlinux/" || exit 1
-# cp "${BINARIES_DIR}/boot.scr"                          "${BATOCERA_BINARIES_DIR}/boot/"               || exit 1
-# cp "${BOARD_DIR}/boot/logo.bmp"                        "${BATOCERA_BINARIES_DIR}/boot/boot/"          || exit 1
-
-
-# dd if="${BINARIES_DIR}/u-boot.bin.sd-amlogic.bin" of="${BINARIES_DIR}/u-boot1.bin" bs=1   count=444 || exit 1
-# dd if="${BINARIES_DIR}/u-boot.bin.sd-amlogic.bin" of="${BINARIES_DIR}/u-boot2.bin" bs=512 skip=1    || exit 1
+cp "${BINARIES_DIR}/meson-g12b-gtking-pro.dtb" "${BATOCERA_BINARIES_DIR}/boot/boot/"     || exit 1
+cp "${BOARD_DIR}/boot/extlinux.conf"                   "${BATOCERA_BINARIES_DIR}/boot/extlinux/" || exit 1
 
 exit 0
