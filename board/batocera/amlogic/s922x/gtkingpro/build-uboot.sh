8b52d41f26807249723f341c92d25a18302f1b7b
Demetris Ierokipides <ierokipides.dem@gmail.com>
2021-10-14 21:44:04 +0300

    Beelink-s922: Add uboot patch needed to remove a hardcoded amlogic path

--- board/batocera/amlogic/s922x/gtkingpro/build-uboot.sh
+++ board/batocera/amlogic/s922x/gtkingpro/build-uboot.sh
@@ -9,13 +9,13 @@ wget "https://ftp.denx.de/pub/u-boot/u-boot-2021.10.tar.bz2"
 tar xf u-boot-2021.10.tar.bz2
 cd u-boot-2021.10
 
-# Apply patches
-# PATCHES="${BOARD_DIR}/patches/uboot/*.patch"
-# for patch in $PATCHES
-# do
-# echo "Applying patch: $patch"
-# patch -p1 < $patch
-# done
+Apply patches
+PATCHES="${BOARD_DIR}/patches/uboot/*.patch"
+for patch in $PATCHES
+do
+echo "Applying patch: $patch"
+patch -p1 < $patch
+done
 
 # Make config
 make beelink-gtkingpro_defconfig
