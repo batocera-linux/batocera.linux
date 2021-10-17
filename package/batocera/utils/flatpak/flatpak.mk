651a544f504d8af327aec71fdfa2492f3ca432da
liberodark <liberodark@gmail.com>
2021-10-17 23:03:35 +0200

    Add Padtokey for Steam

--- package/batocera/utils/flatpak/flatpak.mk
+++ package/batocera/utils/flatpak/flatpak.mk
@@ -26,6 +26,10 @@ define FLATPAK_INSTALL_SCRIPTS
 	mkdir -p $(TARGET_DIR)/usr/share/emulationstation/hooks
 	ln -sf /usr/bin/batocera-flatpak-update $(TARGET_DIR)/usr/share/emulationstation/hooks/preupdate-gamelists-flatpak
 	ln -sf /usr/bin/batocera-steam-update   $(TARGET_DIR)/usr/share/emulationstation/hooks/preupdate-gamelists-steam
+
+	#evmap config
+	mkdir -p $(TARGET_DIR)/usr/share/evmapy
+	cp -prn $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/utils/flatpak/steam.keys $(TARGET_DIR)/usr/share/evmapy
 endef
 
 FLATPAK_POST_INSTALL_TARGET_HOOKS += FLATPAK_INSTALL_SCRIPTS
