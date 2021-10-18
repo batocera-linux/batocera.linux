ec47cf6eb3773bbdc8b694038b6b53e3a8da0638
Juliano Dorigão <jdorigao@gmail.com>
2021-10-18 12:55:06 -0400

    bump: libretro-px68k

--- package/batocera/emulators/retroarch/libretro/libretro-px68k/libretro-px68k.mk
+++ package/batocera/emulators/retroarch/libretro/libretro-px68k/libretro-px68k.mk
@@ -3,13 +3,29 @@
 # PX68K
 #
 ################################################################################
-# Last commit: July 12, 2021
-LIBRETRO_PX68K_VERSION = 979a4a9fa33a309f12432b69fe5dc2bccd0aab8a
+# Version.: Commits on Oct 03, 2021
+LIBRETRO_PX68K_VERSION = a34e55f21393b28b85b49d3d4a5fbe97052c7a68
 LIBRETRO_PX68K_SITE = $(call github,libretro,px68k-libretro,$(LIBRETRO_PX68K_VERSION))
 LIBRETRO_PX68K_LICENSE = Unknown
 
+LIBRETRO_PX68K_PLATFORM = $(LIBRETRO_PLATFORM)
+
+ifeq ($(BR2_PACKAGE_BATOCERA_TARGET_RPI1),y)
+LIBRETRO_PX68K_PLATFORM = rpi1
+
+else ifeq ($(BR2_PACKAGE_BATOCERA_TARGET_RPI2),y)
+LIBRETRO_PX68K_PLATFORM = rpi2
+
+else ifeq ($(BR2_PACKAGE_BATOCERA_TARGET_RPI3),y)
+LIBRETRO_PX68K_PLATFORM = rpi3_64
+
+else ifeq ($(BR2_PACKAGE_BATOCERA_TARGET_RPI4),y)
+LIBRETRO_PX68K_PLATFORM = rpi4
+endif
+
 define LIBRETRO_PX68K_BUILD_CMDS
-	$(TARGET_CONFIGURE_OPTS) $(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" -C $(@D)/ -f Makefile.libretro platform="$(LIBRETRO_PLATFORM)"
+	$(TARGET_CONFIGURE_OPTS) $(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" -C $(@D)/ -f Makefile.libretro platform="$(LIBRETRO_PX68K_PLATFORM)" \
+        GIT_VERSION="_$(shell echo $(LIBRETRO_PX68K_VERSION) | cut -c 1-7)"
 endef
 
 define LIBRETRO_PX68K_INSTALL_TARGET_CMDS
