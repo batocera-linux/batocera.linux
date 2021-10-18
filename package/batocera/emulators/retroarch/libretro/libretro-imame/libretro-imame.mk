6ef944a1df3d5a9b855416901a425f312db8f8e3
Juliano Dorigão <jdorigao@gmail.com>
2021-10-18 11:05:51 -0400

    bump: libretro-imame

--- package/batocera/emulators/retroarch/libretro/libretro-imame/libretro-imame.mk
+++ package/batocera/emulators/retroarch/libretro/libretro-imame/libretro-imame.mk
@@ -3,14 +3,20 @@
 # IMAME
 #
 ################################################################################
-# Version.: Commits on May 29, 2021
-LIBRETRO_IMAME_VERSION = 49671d509bd370a1e92b972eb021149fcdfb1a0d
+# Version.: Commits on Oct 11, 2021
+LIBRETRO_IMAME_VERSION = 4793742b457945afb74053c8a895e6ff0b36b033
 LIBRETRO_IMAME_SITE = $(call github,libretro,mame2000-libretro,$(LIBRETRO_IMAME_VERSION))
 LIBRETRO_IMAME_LICENSE = MAME
 
+LIBRETRO_IMAME_PLATFORM=$(LIBRETRO_PLATFORM)
+
+ifeq ($(BR2_PACKAGE_BATOCERA_TARGET_RPI1),y)
+LIBRETRO_IMAME_PLATFORM = rpi1
+endif
+
 define LIBRETRO_IMAME_BUILD_CMDS
 	mkdir -p $(@D)/obj_libretro_libretro/cpu
-	$(TARGET_CONFIGURE_OPTS) $(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" -C $(@D) -f Makefile ARM=1
+	$(TARGET_CONFIGURE_OPTS) $(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" -C $(@D) -f Makefile platform="$(LIBRETRO_IMAME_PLATFORM)"
 endef
 
 define LIBRETRO_IMAME_INSTALL_TARGET_CMDS
