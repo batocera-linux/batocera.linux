cfd757bfd65e64fbc0f8ad46afa2d7995ce5d1ed
liberodark <liberodark@gmail.com>
2021-10-19 18:29:05 +0200

    Fix Pad2Key

--- package/batocera/core/batocera-configgen/configgen/configgen/generators/play/playGenerator.py
+++ package/batocera/core/batocera-configgen/configgen/configgen/generators/play/playGenerator.py
@@ -7,13 +7,27 @@ import controllersConfig
 import os
 from os import path
 
+playConfig = batoceraFiles.CONF + '/play'
+playSaves = batoceraFiles.SAVES + '/play'
+playHome = batoceraFiles.CONF
+
 class PlayGenerator(Generator):
 
     def generate(self, system, rom, playersControllers, gameResolution):
 
-        commandArray = ["play", "--disc", rom]
+        # Create config folder
+        if not path.isdir(playConfig):
+            os.mkdir(playConfig)
+
+        # Create save folder
+        if not path.isdir(playSaves):
+            os.mkdir(playSaves)
+
+        commandArray = ["/usr/bin/play-emu", "--disc", rom]
         return Command.Command(
-            array=commandArray)
+            array=commandArray,
+            env={"XDG_CONFIG_HOME":playConfig, "XDG_DATA_HOME":playConfig, "XDG_CACHE_HOME":batoceraFiles.CACHE, "QT_QPA_PLATFORM":"xcb"}
+            )
 
     def getMouseMode(self, config):
         return True
