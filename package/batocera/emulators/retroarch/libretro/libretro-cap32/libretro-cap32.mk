1827ac511fd8b665827b5f1c60657a69a68d6c58
Juliano Dorigão <jdorigao@gmail.com>
2021-10-21 21:17:19 -0400

    rpi-optimization: libretro-cap32

--- package/batocera/emulators/retroarch/libretro/libretro-cap32/libretro-cap32.mk
+++ package/batocera/emulators/retroarch/libretro/libretro-cap32/libretro-cap32.mk
@@ -3,15 +3,24 @@
 # CAP32
 #
 ################################################################################
-# Version.: Commits on Sep 13, 2021
-LIBRETRO_CAP32_VERSION = c0dc3a5b1f5fa04d2f5c263445de14f8c8d5b1ed
+# Version.: Commits on Oct 16, 2021
+LIBRETRO_CAP32_VERSION = b647a3ab1e5528050cf04b0b5114719d212e31a1
 LIBRETRO_CAP32_SITE = $(call github,libretro,libretro-cap32,$(LIBRETRO_CAP32_VERSION))
 LIBRETRO_CAP32_LICENSE = GPLv2
 
 LIBRETRO_CAP32_PLATFORM = $(LIBRETRO_PLATFORM)
 
 ifeq ($(BR2_PACKAGE_BATOCERA_TARGET_RPI1),y)
-LIBRETRO_CAP32_PLATFORM = armv
+LIBRETRO_CAP32_PLATFORM = rpi1
+
+else ifeq ($(BR2_PACKAGE_BATOCERA_TARGET_RPI2),y)
+LIBRETRO_CAP32_PLATFORM = rpi2
+
+else ifeq ($(BR2_PACKAGE_BATOCERA_TARGET_RPI3),y)
+LIBRETRO_CAP32_PLATFORM = rpi3_64
+
+else ifeq ($(BR2_PACKAGE_BATOCERA_TARGET_RPI4),y)
+LIBRETRO_CAP32_PLATFORM = rpi4_64
 
 else ifeq ($(BR2_cortex_a35)$(BR2_cortex_a53)$(BR2_arm),yy)
 LIBRETRO_CAP32_PLATFORM = armv neon
