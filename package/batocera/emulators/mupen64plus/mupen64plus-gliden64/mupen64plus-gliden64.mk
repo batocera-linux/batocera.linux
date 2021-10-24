263bc01097867fec163fdf5db3fdf6f7e592e8da
Juliano Dorigão <jdorigao@gmail.com>
2021-10-24 01:13:32 -0400

    bump: mupen64plus-gliden64

--- package/batocera/emulators/mupen64plus/mupen64plus-gliden64/mupen64plus-gliden64.mk
+++ package/batocera/emulators/mupen64plus/mupen64plus-gliden64/mupen64plus-gliden64.mk
@@ -3,8 +3,8 @@
 # mupen64plus video GLIDEN64
 #
 ################################################################################
-# Version.: Commits on Sep 26, 2021
-MUPEN64PLUS_GLIDEN64_VERSION = bfe3c36e7367619e0470209fed26e1d8c021a221
+# Version.: Commits on Oct 17, 2021
+MUPEN64PLUS_GLIDEN64_VERSION = 4c7b17db04d09dc0443219270c2b20e510c5c83e
 MUPEN64PLUS_GLIDEN64_SITE = $(call github,gonetz,GLideN64,$(MUPEN64PLUS_GLIDEN64_VERSION))
 MUPEN64PLUS_GLIDEN64_LICENSE = GPLv2
 MUPEN64PLUS_GLIDEN64_DEPENDENCIES = sdl2 alsa-lib mupen64plus-core
