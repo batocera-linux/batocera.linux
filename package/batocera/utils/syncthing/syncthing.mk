bdd7b98287964d625839d198e262b6d9fbe2befa
liberodark <liberodark@gmail.com>
2021-10-28 02:04:36 +0200

    Add Syncthing

---
+++ package/batocera/utils/syncthing/syncthing.mk
@@ -0,0 +1,28 @@
+################################################################################
+#
+# syncthing
+#
+################################################################################
+
+SYNCTHING_VERSION = v1.18.3
+SYNCTHING_SITE = $(call github,syncthing,syncthing,$(SYNCTHING_VERSION))
+SYNCTHING_LICENSE = MPLv2
+SYNCTHING_LICENSE_FILES = LICENSE
+
+SYNCTHING_TARGET_ENV = \
+	PATH=$(BR_PATH) \
+	GOROOT="$(HOST_GO_ROOT)" \
+	GOPATH="$(HOST_GO_GOPATH)" \
+	GOCACHE="$(HOST_GO_TARGET_CACHE)"
+
+define SYNCTHING_BUILD_CMDS
+	cd $(@D) && $(SYNCTHING_TARGET_ENV) $(GO_BIN) run build.go
+endef
+
+define SYNCTHING_INSTALL_TARGET_CMDS
+	mkdir -p $(TARGET_DIR)/usr/bin
+
+	$(INSTALL) -D $(@D)/bin/syncthing $(TARGET_DIR)/usr/bin/syncthing
+endef
+
+$(eval $(golang-package))
