7b023767621f889ad5516d7a29ed078d37b8f751
Juliano Dorigão <jdorigao@gmail.com>
2021-10-28 21:47:19 -0400

    bump: allinuxfirmware to 20211027

--- package/batocera/alllinuxfirmwares/alllinuxfirmwares.mk
+++ package/batocera/alllinuxfirmwares/alllinuxfirmwares.mk
@@ -3,8 +3,8 @@
 # alllinuxfirmwares
 #
 ################################################################################
-# Version from 2021-09-19
-ALLLINUXFIRMWARES_VERSION = 20210919
+# Version from 2021-10-27
+ALLLINUXFIRMWARES_VERSION = 20211027
 ALLLINUXFIRMWARES_SITE = http://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git
 ALLLINUXFIRMWARES_SITE_METHOD = git
 
