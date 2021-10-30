7ea23a88bbb716d8b823a94e1901375db01ee904
liberodark <liberodark@gmail.com>
2021-10-30 18:04:55 +0200

    Update libretro-pcsx2.mk

--- package/batocera/emulators/retroarch/libretro/libretro-pcsx2/libretro-pcsx2.mk
+++ package/batocera/emulators/retroarch/libretro/libretro-pcsx2/libretro-pcsx2.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-# Version.: Commits on Oct 11, 2021
-LIBRETRO_PCSX2_VERSION = afdbc84bf67efaeddf9d169948705c0119de3153
+# Version.: Commits on Oct 30, 2021
+LIBRETRO_PCSX2_VERSION = 282fce6ce6d72ab2e61d13aecb81ed428a83dee9
 LIBRETRO_PCSX2_SITE = https://github.com/libretro/pcsx2.git
 LIBRETRO_PCSX2_SITE_METHOD = git
 LIBRETRO_PCSX2_GIT_SUBMODULES = YES
