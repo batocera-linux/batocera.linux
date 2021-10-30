f1c483f3c970e457b14f400cf97dee177beb39da
Ian Murray <tvsian@gmail.com>
2021-10-29 20:55:44 -0400

    Bump Sonic Retro Engine ports
    Bump to the latest commit, which includes the mobile menus for Sonic 1 & 2.

--- package/batocera/ports/sonic2013/sonic2013.mk
+++ package/batocera/ports/sonic2013/sonic2013.mk
@@ -1,10 +1,19 @@
 
-SONIC2013_VERSION = 'c3be493616ada254f52dfd1f0842df1de5704c3c'
+SONIC2013_VERSION = '759d8459638a4fa97fb7c8729029369b90dcbe87'
 SONIC2013_SITE = $(call github,Rubberduckycooly,Sonic-1-2-2013-Decompilation,$(SONIC2013_VERSION))
 SONIC2013_DEPENDENCIES = sdl2 libogg libvorbis
 SONIC2013_LICENSE = Custom
 
 define SONIC2013_BUILD_CMDS
+	rm -rf $(@D)/tmp
+	rm -rf $(@D)/dependencies/all/asio/
+	git clone https://github.com/chriskohlhoff/asio.git $(@D)/tmp
+	mv $(@D)/tmp/* $(@D)/dependencies/all/
+	rm -rf $(@D)/tmp
+	rm -rf $(@D)/dependencies/all/tinyxml2
+	git clone https://github.com/leethomason/tinyxml2.git $(@D)/dependencies/all/tinyxml2
+	rm -rf $(@D)/dependencies/all/stb-image
+	git clone https://github.com/nothings/stb.git $(@D)/dependencies/all/stb-image
 	$(TARGET_CONFIGURE_OPTS) $(MAKE) -C $(@D) -f Makefile VERBOSE=1
 endef
 
--- package/batocera/ports/soniccd/soniccd.mk
+++ package/batocera/ports/soniccd/soniccd.mk
@@ -1,15 +1,17 @@
 
-SONICCD_VERSION = '129cd86fbd0d6af58ae50f9c111021f0b4ef4f9c'
+SONICCD_VERSION = 'b99fed54b22821a3512cf5b09de9e826935668bd'
 SONICCD_SITE = $(call github,Rubberduckycooly,Sonic-CD-11-Decompilation,$(SONICCD_VERSION))
 SONICCD_DEPENDENCIES = sdl2 libvorbis libogg libtheora
 SONICCD_LICENSE = Custom
 
 define SONICCD_BUILD_CMDS
+	rm -rf $(@D)/dependencies/all/tinyxml2
+	git clone https://github.com/leethomason/tinyxml2.git $(@D)/dependencies/all/tinyxml2
 	$(TARGET_CONFIGURE_OPTS) $(MAKE) -C $(@D) -f Makefile VERBOSE=1
 endef
 
 define SONICCD_INSTALL_TARGET_CMDS
-	$(INSTALL) -D -m 0755 $(@D)/bin/soniccd $(TARGET_DIR)/usr/bin/soniccd
+	$(INSTALL) -D -m 0755 $(@D)/bin/RSDKv3 $(TARGET_DIR)/usr/bin/soniccd
 endef
 
 define SONICCD_POST_PROCESS
