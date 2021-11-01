ad0da20f8063406af169469f7c3d7625379d73f8
Juliano Dorigão <jdorigao@gmail.com>
2021-11-01 08:14:08 -0400

    bump: scummvm to 2.5.1pre

--- package/batocera/emulators/scummvm/scummvm.mk
+++ package/batocera/emulators/scummvm/scummvm.mk
@@ -3,8 +3,8 @@
 # Scummvm
 #
 ################################################################################
-# Version.: Commits on Oct 14, 2021
-SCUMMVM_VERSION = v2.5.0
+# Version.: Commits on Oct 31, 2021 (2.5.1pre)
+SCUMMVM_VERSION = 57bad5b424475558957aadbb7a2161f6a8f1ff9e
 SCUMMVM_SITE = $(call github,scummvm,scummvm,$(SCUMMVM_VERSION))
 SCUMMVM_LICENSE = GPLv2
 SCUMMVM_DEPENDENCIES = sdl2 zlib jpeg libmpeg2 libogg libvorbis flac libmad libpng libtheora faad2 freetype
