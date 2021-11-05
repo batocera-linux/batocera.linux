e9d09d35546fe9291d7ceb9f47cf797cd0670515
Juliano Dorigão <jdorigao@gmail.com>
2021-11-05 10:32:53 -0400

    updated libretro-duckstation package

--- package/batocera/emulators/retroarch/libretro/libretro-duckstation/libretro-duckstation.mk
+++ package/batocera/emulators/retroarch/libretro/libretro-duckstation/libretro-duckstation.mk
@@ -5,6 +5,7 @@
 ################################################################################
 # libretro cores can be downloaded in binary form,
 # but not built from the Github Duckstation sources
+# Version.: 0.1-4795-g4a017bd1
 LIBRETRO_DUCKSTATION_VERSION = f0630f5ab57a8b63dec0bef7539a834f0249009e
 LIBRETRO_DUCKSTATION_SITE = $(call github,batocera-linux,lr-duckstation,$(LIBRETRO_DUCKSTATION_VERSION))
 LIBRETRO_DUCKSTATION_LICENSE = non-commercial
@@ -19,9 +20,10 @@ LIBRETRO_DUCKSTATION_PK = duckstation_libretro_linux_armv7.zip
 endif
 
 define LIBRETRO_DUCKSTATION_INSTALL_TARGET_CMDS
-        cd $(@D) && unzip $(LIBRETRO_DUCKSTATION_PK)
-        mkdir -p $(TARGET_DIR)/usr/lib/libretro/
-        cp -prn $(@D)/duckstation_libretro.so $(TARGET_DIR)/usr/lib/libretro
+    cd $(@D) && unzip $(LIBRETRO_DUCKSTATION_PK)
+
+    $(INSTALL) -D $(@D)/duckstation_libretro.so \
+        $(TARGET_DIR)/usr/lib/libretro/duckstation_libretro.so
 endef
 
 $(eval $(generic-package))
