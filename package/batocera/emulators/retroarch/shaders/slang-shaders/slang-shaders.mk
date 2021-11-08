6c8f98324041ee6434fef64fb8c0e42b508fb744
Juliano Dorigão <jdorigao@gmail.com>
2021-11-08 08:14:08 -0400

    bump: slang-shaders

--- package/batocera/emulators/retroarch/shaders/slang-shaders/slang-shaders.mk
+++ package/batocera/emulators/retroarch/shaders/slang-shaders/slang-shaders.mk
@@ -3,8 +3,8 @@
 # SLANG SHADERS
 #
 ################################################################################
-# Version.: Commits on Oct 21, 2021
-SLANG_SHADERS_VERSION = b93526756836e812bfd1900c82bfa4d730a4aae5
+# Version.: Commits on Oct 28, 2021
+SLANG_SHADERS_VERSION = 72eb5c1547758a225faca365d5aa467a33d4ab46
 SLANG_SHADERS_SITE = $(call github,libretro,slang-shaders,$(SLANG_SHADERS_VERSION))
 SLANG_SHADERS_LICENSE = GPL
 
