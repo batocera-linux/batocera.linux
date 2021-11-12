1e77291216908f12b7d61f2c5865e9ca67e0ef79
liberodark <liberodark@gmail.com>
2021-11-12 15:41:13 +0100

    Improve Piboy Install

--- package/batocera/utils/piboy/piboy.mk
+++ package/batocera/utils/piboy/piboy.mk
@@ -13,13 +13,6 @@ define PIBOY_BUILD_CMDS
         $(MAKE) -C $(@D) $(LINUX_MAKE_FLAGS) KERNELDIR=$(LINUX_DIR) KVERSION=$(PIBOY_LINUX_VER)
 endef
 
-ifeq ($(BR2_PACKAGE_BATOCERA_TARGET_RPI3),y)
-CONFIG_RPI = config_rpi3.txt
-
-else ifeq ($(BR2_PACKAGE_BATOCERA_TARGET_RPI4),y)
-CONFIG_RPI = config_rpi4.txt
-endif
-
 define PIBOY_INSTALL_TARGET_CMDS
 	mkdir -p $(TARGET_DIR)/usr/bin
 	mkdir -p $(TARGET_DIR)/usr/share/piboy
@@ -27,7 +20,6 @@ define PIBOY_INSTALL_TARGET_CMDS
 	$(INSTALL) -D -m 0755 $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/utils/piboy/piboy_fan_ctrl.py      $(TARGET_DIR)/usr/bin/piboy_fan_ctrl.py
 	$(INSTALL) -D -m 0755 $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/utils/piboy/piboy_aud_ctrl.py      $(TARGET_DIR)/usr/bin/piboy_aud_ctrl.py
 	$(INSTALL) -D -m 0755 $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/utils/piboy/piboy_power_ctrl.py    $(TARGET_DIR)/usr/bin/piboy_power_ctrl.py
-	cp -a $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/utils/piboy/$(CONFIG_RPI)                          $(TARGET_DIR)/usr/share/piboy/config_rpi.txt
 	cp -a $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/utils/piboy/fan.ini                                $(TARGET_DIR)/usr/share/piboy/fan.ini
 endef
 
