88d29225148ef68c98ae2f1077295f53f2eab7fb
liberodark <liberodark@gmail.com>
2021-11-14 02:27:38 +0100

    Bump Lutris

--- package/batocera/emulators/wine/wine-lutris-wow64_32/wine-lutris-wow64_32.mk
+++ package/batocera/emulators/wine/wine-lutris-wow64_32/wine-lutris-wow64_32.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-WINE_LUTRIS_WOW64_32_VERSION = lutris-6.14-4
+WINE_LUTRIS_WOW64_32_VERSION = lutris-6.21
 WINE_LUTRIS_WOW64_32_SITE = $(call github,lutris,wine,$(WINE_LUTRIS_WOW64_32_VERSION))
 WINE_LUTRIS_WOW64_32_LICENSE = LGPL-2.1+
 WINE_LUTRIS_WOW64_32_DEPENDENCIES = host-bison host-flex host-wine-lutris
