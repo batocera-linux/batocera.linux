ef3e5023d70d76d1a4d6440b500b3179648d48bb
Juliano Dorigão <jdorigao@gmail.com>
2021-11-16 11:16:12 -0400

    bump: libretro-picodrive to v1.99

--- package/batocera/emulators/retroarch/libretro/libretro-picodrive/libretro-picodrive.mk
+++ package/batocera/emulators/retroarch/libretro/libretro-picodrive/libretro-picodrive.mk
@@ -3,8 +3,8 @@
 # libretro-picodrive
 #
 ################################################################################
-# Version.: Commits on Oct 16, 2021
-LIBRETRO_PICODRIVE_VERSION = d2e3f475ffa184787de033ddff80fda8506de76b
+# Version.: Commits on Nov 14, 2021
+LIBRETRO_PICODRIVE_VERSION = v1.99
 LIBRETRO_PICODRIVE_SITE = https://github.com/irixxxx/picodrive.git
 LIBRETRO_PICODRIVE_SITE_METHOD=git
 LIBRETRO_PICODRIVE_GIT_SUBMODULES=YES
