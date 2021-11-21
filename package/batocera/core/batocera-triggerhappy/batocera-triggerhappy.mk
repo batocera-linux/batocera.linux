664d3e453c2528564d31edf354a918de25fe183c
Juliano Dorigão <jdorigao@gmail.com>
2021-11-21 18:18:02 -0400

    rk33266: batocera-triggerhappy

--- package/batocera/core/batocera-triggerhappy/batocera-triggerhappy.mk
+++ package/batocera/core/batocera-triggerhappy/batocera-triggerhappy.mk
@@ -17,26 +17,17 @@ define BATOCERA_TRIGGERHAPPY_INSTALL_CONFIG
 	install -m 0755 $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/core/batocera-triggerhappy/triggerhappy.service  $(TARGET_DIR)/etc/init.d/S50triggerhappy
 endef
 
-define BATOCERA_TRIGGERHAPPY_INSTALL_ODROIDGOA_CONFIG
-	cp $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/core/batocera-triggerhappy/conf/multimedia_keys_Hardkernel_ODROID_GO3.conf $(TARGET_DIR)/etc/triggerhappy/triggers.d
-	cp $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/core/batocera-triggerhappy/conf/multimedia_keys_Anbernic_RG351P.conf       $(TARGET_DIR)/etc/triggerhappy/triggers.d
-
-	# erase because some models are missing
-	cp $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/core/batocera-triggerhappy/conf/multimedia_keys_odroidgoadvance.conf       $(TARGET_DIR)/etc/triggerhappy/triggers.d/multimedia_keys.conf
-endef
-
-define BATOCERA_TRIGGERHAPPY_INSTALL_GAMEFORCE_CONFIG
-	cp $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/core/batocera-triggerhappy/conf/multimedia_keys_gameforce.conf       $(TARGET_DIR)/etc/triggerhappy/triggers.d/multimedia_keys.conf
+define BATOCERA_TRIGGERHAPPY_INSTALL_RK3326_CONFIG
+    cp $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/core/batocera-triggerhappy/conf/rk3326/multimedia_keys_Hardkernel_ODROID_GO2.conf    $(TARGET_DIR)/etc/triggerhappy/triggers.d
+    cp $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/core/batocera-triggerhappy/conf/rk3326/multimedia_keys_Hardkernel_ODROID_GO3.conf    $(TARGET_DIR)/etc/triggerhappy/triggers.d
+    cp $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/core/batocera-triggerhappy/conf/rk3326/multimedia_keys_Anbernic_RG351P.conf          $(TARGET_DIR)/etc/triggerhappy/triggers.d
+    cp $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/core/batocera-triggerhappy/conf/rk3326/multimedia_keys_Golden_GameForce.conf         $(TARGET_DIR)/etc/triggerhappy/triggers.d
 endef
 
 BATOCERA_TRIGGERHAPPY_POST_INSTALL_TARGET_HOOKS += BATOCERA_TRIGGERHAPPY_INSTALL_CONFIG
 
-ifeq ($(BR2_PACKAGE_BATOCERA_TARGET_ODROIDGOA),y)
-	BATOCERA_TRIGGERHAPPY_POST_INSTALL_TARGET_HOOKS += BATOCERA_TRIGGERHAPPY_INSTALL_ODROIDGOA_CONFIG
-endif
-
-ifeq ($(BR2_PACKAGE_BATOCERA_TARGET_GAMEFORCE),y)
-	BATOCERA_TRIGGERHAPPY_POST_INSTALL_TARGET_HOOKS += BATOCERA_TRIGGERHAPPY_INSTALL_GAMEFORCE_CONFIG
+ifeq ($(BR2_PACKAGE_BATOCERA_TARGET_RK3326_ANY),y)
+	BATOCERA_TRIGGERHAPPY_POST_INSTALL_TARGET_HOOKS += BATOCERA_TRIGGERHAPPY_INSTALL_RK3326_CONFIG
 endif
 
 $(eval $(generic-package))
