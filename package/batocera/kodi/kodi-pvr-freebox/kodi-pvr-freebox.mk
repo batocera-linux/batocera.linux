12f817c8ae238b721d7ad590e91f2db96130b565
Juliano Dorigão <jdorigao@gmail.com>
2021-12-27 18:21:03 -0400

    kodi: bump pvr-freebox and language (FR, BR))

--- package/batocera/kodi/kodi-pvr-freebox/kodi-pvr-freebox.mk
+++ package/batocera/kodi/kodi-pvr-freebox/kodi-pvr-freebox.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-KODI_PVR_FREEBOX_VERSION = 6.1.4-Matrix
+KODI_PVR_FREEBOX_VERSION = 19.0.0-Matrix
 KODI_PVR_FREEBOX_SITE = $(call github,aassif,pvr.freebox,$(KODI_PVR_FREEBOX_VERSION))
 KODI_PVR_FREEBOX_LICENSE = MIT
 KODI_PVR_FREEBOX_DEPENDENCIES = kodi
