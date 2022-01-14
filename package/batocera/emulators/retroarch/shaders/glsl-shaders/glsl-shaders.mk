60f109dfc420fd4da1b23f79976a409922ceb805
Juliano Dorigão <jdorigao@gmail.com>
2022-01-14 13:04:50 -0400

    bump: retroarch-glsl-shaders

--- package/batocera/emulators/retroarch/shaders/glsl-shaders/glsl-shaders.mk
+++ package/batocera/emulators/retroarch/shaders/glsl-shaders/glsl-shaders.mk
@@ -3,8 +3,8 @@
 # GLSL SHADERS
 #
 ################################################################################
-# Version.: Commits on Nov 07, 2021
-GLSL_SHADERS_VERSION = 3b66160795221bf73d51d63d6d86a93c0a0a21c7
+# Version.: Commits on Jan 11, 2022
+GLSL_SHADERS_VERSION = ec69b2543d067e0950f3e0f3c6142575a2b0700a
 GLSL_SHADERS_SITE = $(call github,libretro,glsl-shaders,$(GLSL_SHADERS_VERSION))
 GLSL_SHADERS_LICENSE = GPL
 
