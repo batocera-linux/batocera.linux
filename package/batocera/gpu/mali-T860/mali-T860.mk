1d138354ceaf632c7e004d63e01ba7b168fcfbb6
Juliano Dorigão <jdorigao@gmail.com>
2022-01-17 12:55:11 -0400

    mali-t860: khrplatform.h

--- package/batocera/gpu/mali-T860/mali-T860.mk
+++ package/batocera/gpu/mali-T860/mali-T860.mk
@@ -9,6 +9,6 @@ MALI_T860_SITE = $(call github,rockchip-linux,libmali,$(MALI_T860_VERSION))
 MALI_T860_INSTALL_STAGING = YES
 MALI_T860_PROVIDES = libegl libgles libmali
 
-MALI_T860_CONF_OPTS += -Darch=auto -Dgpu=midgard-t86x -Dversion=r18p0 -Dplatform=gbm
+MALI_T860_CONF_OPTS += -Darch=auto -Dgpu=midgard-t86x -Dversion=r18p0 -Dplatform=gbm -Dkhr-header=true
 
 $(eval $(meson-package))
