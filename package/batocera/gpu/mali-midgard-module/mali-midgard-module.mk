106e58dcaa3f8158f863e7f4ec1f7e9f58090975
Juliano Dorigão <jdorigao@gmail.com>
2022-01-19 08:59:11 -0400

    bump: mali-midgard-module

--- package/batocera/gpu/mali-midgard-module/mali-midgard-module.mk
+++ package/batocera/gpu/mali-midgard-module/mali-midgard-module.mk
@@ -3,8 +3,8 @@
 # mali-midgard-module
 #
 ################################################################################
-# Version.: Commits on Nov 6, 2021
-MALI_MIDGARD_MODULE_VERSION = 4d72a50cd76e2cdeef25fec113e3f6b3396f6e3c
+# Version.: Commits on Jan 10, 2022
+MALI_MIDGARD_MODULE_VERSION = 2c2accf67356463ee661627b8705429256011dcb
 MALI_MIDGARD_MODULE_SITE = $(call github,LibreELEC,mali-midgard,$(MALI_MIDGARD_MODULE_VERSION))
 
 MALI_MIDGARD_MODULE_MODULE_MAKE_OPTS = \
