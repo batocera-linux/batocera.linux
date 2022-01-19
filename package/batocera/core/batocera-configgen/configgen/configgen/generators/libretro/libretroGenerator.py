09fc317d007b7ace542ab8d9cec67faf41200d90
Ian Murray <tvsian@gmail.com>
2022-01-18 23:39:46 -0500

    Remove debug code

--- package/batocera/core/batocera-configgen/configgen/configgen/generators/libretro/libretroGenerator.py
+++ package/batocera/core/batocera-configgen/configgen/configgen/generators/libretro/libretroGenerator.py
@@ -204,8 +204,6 @@ class LibretroGenerator(Generator):
         # RetroArch 1.7.8 (Batocera 5.24) now requires the shaders to be passed as command line argument
         renderConfig = system.renderconfig
         gameSpecial = videoMode.getGameSpecial(system.name, rom)
-        print("Special game bezel/shader = " + str(gameSpecial))
-        print("Will be looking for " + ('shader-' + str(gameSpecial)))
         if gameSpecial == "0":
             gameShader = renderConfig['shader']
         else:
