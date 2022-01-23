26017e5b660b8e832cef9af23c75dcd5fa1573e8
Daniel Martin <dmanlfc@gmail.com>
2022-01-23 11:22:56 +1000

    add some redream options etc

--- package/batocera/emulators/redream/redream.mk
+++ package/batocera/emulators/redream/redream.mk
@@ -4,11 +4,9 @@
 #
 ################################################################################
 
-REDREAM_VERSION = 1.5.0-986-g499d5a0
+REDREAM_VERSION = 1.5.0-993-g6956a4e
 REDREAM_SOURCE = redream.x86_64-linux-v$(REDREAM_VERSION).tar.gz
-REDREAM_SITE = https://github.com/liberodark/redream-repo/releases/download/1.0
-# REDREAM_SITE = https://redream.io/download
-# temporarly link to a private repo untile the stable is realeased
+REDREAM_SITE = https://redream.io/download
 
 define REDREAM_EXTRACT_CMDS
 	mkdir -p $(@D)/target && cd $(@D)/target && tar xf $(DL_DIR)/$(REDREAM_DL_SUBDIR)/$(REDREAM_SOURCE)
@@ -16,7 +14,6 @@ endef
 
 define REDREAM_INSTALL_TARGET_CMDS
 	mkdir -p $(TARGET_DIR)/usr/bin
-
 	cp -pr $(@D)/target/redream $(TARGET_DIR)/usr/bin/redream
 
 	# evmap config
