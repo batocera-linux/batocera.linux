dab2e3a3fa578cdae8bf56960c3f68bb8848f794
Ian Murray <tvsian@gmail.com>
2022-02-02 17:48:40 -0500

    Duckstation Software Renderer

--- package/batocera/core/batocera-configgen/configgen/configgen/generators/libretro/libretroGenerator.py
+++ package/batocera/core/batocera-configgen/configgen/configgen/generators/libretro/libretroGenerator.py
@@ -262,6 +262,8 @@ def getGFXBackend(system):
         # Pick glcore or gl based on drivers if not selected
         if system.isOptSet("gfxbackend"):
             backend = system.config["gfxbackend"]
+        elif system.core == 'duckstation' and system.isOptSet("gpu_software") and system.getOptBoolean("gpu_software"):
+            return "software"
         else:
             if videoMode.getGLVersion() >= 3.1 and videoMode.getGLVendor() in ["nvidia", "amd"]:  
                 backend = "glcore"
