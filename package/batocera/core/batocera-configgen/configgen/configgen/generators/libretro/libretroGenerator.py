b91b65f35baeef1b5a53ccffda81c702a51dffa2
Ian Murray <tvsian@gmail.com>
2022-02-02 19:11:06 -0500

    Actual fix
    Turns out the case was wrong and some quotes were missing.

--- package/batocera/core/batocera-configgen/configgen/configgen/generators/libretro/libretroGenerator.py
+++ package/batocera/core/batocera-configgen/configgen/configgen/generators/libretro/libretroGenerator.py
@@ -262,8 +262,6 @@ def getGFXBackend(system):
         # Pick glcore or gl based on drivers if not selected
         if system.isOptSet("gfxbackend"):
             backend = system.config["gfxbackend"]
-        elif core == 'duckstation' and system.isOptSet("gpu_software") and system.getOptBoolean("gpu_software"):
-            return "software"
         else:
             if videoMode.getGLVersion() >= 3.1 and videoMode.getGLVendor() in ["nvidia", "amd"]:  
                 backend = "glcore"
