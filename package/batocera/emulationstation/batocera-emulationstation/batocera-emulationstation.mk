fa58d1cc4f9db4b91491fcd3fdcffb2f6cc3f62a
Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>
2022-02-16 07:08:58 +0000

    update po
    Signed-off-by: Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>

--- package/batocera/emulationstation/batocera-emulationstation/batocera-emulationstation.mk
+++ package/batocera/emulationstation/batocera-emulationstation/batocera-emulationstation.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-BATOCERA_EMULATIONSTATION_VERSION = 51e75029d00b126cba02ea0d9b4611bec298afc3
+BATOCERA_EMULATIONSTATION_VERSION = 2ce8700903f1623ed1b10b0c5d6efc7fecc14187
 BATOCERA_EMULATIONSTATION_SITE = https://github.com/batocera-linux/batocera-emulationstation
 BATOCERA_EMULATIONSTATION_SITE_METHOD = git
 BATOCERA_EMULATIONSTATION_LICENSE = MIT
