0be74046ace340e91e4982e3a80d229253d10876
Ian Murray <tvsian@gmail.com>
2022-02-22 11:22:09 -0500

    Libretro Flycast Fix

--- package/batocera/core/batocera-configgen/configgen/configgen/generators/libretro/libretroGenerator.py
+++ package/batocera/core/batocera-configgen/configgen/configgen/generators/libretro/libretroGenerator.py
@@ -264,14 +264,17 @@ def getGFXBackend(system):
         # Pick glcore or gl based on drivers if not selected
         if system.isOptSet("gfxbackend"):
             backend = system.config["gfxbackend"]
+            setManually = True
         else:
+            setManually = False
             if videoMode.getGLVersion() >= 3.1 and videoMode.getGLVendor() in ["nvidia", "amd"]:  
                 backend = "glcore"
             else:
-                backend = "gl"
+                backend = "opengl"
         # If set to glcore or gl, override setting for certain cores that require one or the other
-        if backend == "gl" and core in [ 'kronos', 'citra', 'mupen64plus-next', 'melonds', 'beetle-psx-hw' ]:
-            backend = "glcore"
-        if backend == "glcore" and core in [ 'parallel_n64', 'yabasanshiro', 'openlara', 'boom3' ]:
-            backend = "gl"
+        if not setManually:
+            if backend == "opengl" and core in [ 'kronos', 'citra', 'mupen64plus-next', 'melonds', 'beetle-psx-hw' ]:
+                backend = "glcore"
+            if backend == "glcore" and core in [ 'parallel_n64', 'yabasanshiro', 'openlara', 'boom3', 'flycast' ]:
+                backend = "opengl"
         return backend
