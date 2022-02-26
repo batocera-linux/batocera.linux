cbd5f248497222dc10b5ff6de73b07354d18f078
Daniel Martin <dmanlfc@gmail.com>
2022-02-26 13:12:00 +1000

    bump mupen64plus core & plugins

--- package/batocera/emulators/mupen64plus/mupen64plus-core/mupen64plus-core.mk
+++ package/batocera/emulators/mupen64plus/mupen64plus-core/mupen64plus-core.mk
@@ -1,10 +1,10 @@
 ################################################################################
 #
-# mupen64plus core
+# mupen64plus-core
 #
 ################################################################################
-# Version.: Commits on Nov 30, 2021
-MUPEN64PLUS_CORE_VERSION = 6860d134afd16f40262fe335cfee04967265fbac
+# Version.: Commits on Feb 19, 2022
+MUPEN64PLUS_CORE_VERSION = 49dd0bfc8f48afd04ddd1762ea9d9e89a85fc8a2
 MUPEN64PLUS_CORE_SITE = $(call github,mupen64plus,mupen64plus-core,$(MUPEN64PLUS_CORE_VERSION))
 MUPEN64PLUS_CORE_LICENSE = GPLv2
 MUPEN64PLUS_CORE_DEPENDENCIES = sdl2 alsa-lib freetype dejavu
