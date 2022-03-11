8dfa0ee4d298df6ccaa8026e189939581130435e
Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>
2022-03-11 20:37:18 +0000

    fix deps
    Signed-off-by: Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>

--- package/batocera/gpu/rockchip-rga/rockchip-rga.mk
+++ package/batocera/gpu/rockchip-rga/rockchip-rga.mk
@@ -5,7 +5,11 @@
 ################################################################################
 # Version.: Commits on Aug 5, 2021
 ROCKCHIP_RGA_VERSION = df26244eb0c3991df319d4276ab80e8c0bf91f64
-ROCKCHIP_RGA_SITE =  $(call github,rockchip-linux,linux-rga,$(ROCKCHIP_RGA_VERSION))
+
+ROCKCHIP_RGA_SOURCE = rockchip-rga-$(ROCKCHIP_RGA_VERSION).tar.gz
+ROCKCHIP_RGA_SITE = https://github.com/batocera-linux/rockchip-packages/releases/download/20220303
+#ROCKCHIP_RGA_SITE =  $(call github,rockchip-linux,linux-rga,$(ROCKCHIP_RGA_VERSION))
+
 ROCKCHIP_RGA_LICENSE = Apache License 2.0
 ROCKCHIP_RGA_DEPENDENCIES = libdrm
 
