7137e356917246ded1578c5b1256f6b7bb716c14
Atari <67527064+Hew-ux@users.noreply.github.com>
2022-03-20 10:20:02 +1100

    compilation fix
    thanks dmanlfc

--- package/batocera/utils/picade/picade.mk
+++ package/batocera/utils/picade/picade.mk
@@ -7,16 +7,13 @@
 PICADE_VERSION = df02844c0cd773af5b908f47eac5fb1f7f361531
 PICADE_SITE = $(call github,pimoroni,picade-hat,$(PICADE_VERSION))
 
-# These commands do not build correctly. Working around by manually copying and pasting the file for now.
-#define PICADE_BUILD_CMDS
-#    cd $(@D)
-#    $(MAKE)
-#endef
+define PICADE_BUILD_CMDS
+    $(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)
+endef
 
 define PICADE_INSTALL_TARGET_CMDS
     mkdir -p $(BINARIES_DIR)/rpi-firmware/overlays
-    #cp $(@D)/picade.dtbo $(BINARIES_DIR)/rpi-firmware/overlays/picade.dtbo
-    cp $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/utils/picade/picade.dtbo $(BINARIES_DIR)/rpi-firmware/overlays/picade.dtbo
+    cp $(@D)/picade.dtbo $(BINARIES_DIR)/rpi-firmware/overlays/picade.dtbo
 endef
 
 $(eval $(generic-package))
