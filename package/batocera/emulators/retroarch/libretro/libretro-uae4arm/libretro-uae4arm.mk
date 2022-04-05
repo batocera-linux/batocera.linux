1793d85028c6180437dbf1dc6cbd83f120a7bac8
Chips-fr <darcelf@gmail.com>
2022-04-05 18:39:03 +0200

    add uae4arm core

---
+++ package/batocera/emulators/retroarch/libretro/libretro-uae4arm/libretro-uae4arm.mk
@@ -0,0 +1,42 @@
+################################################################################
+#
+# libretro-uae4arm
+#
+################################################################################
+# Last commit: April 22, 2022
+LIBRETRO_UAE4ARM_VERSION = 177c2f0e892adf2603ada9b150e31beffe0f76c3
+LIBRETRO_UAE4ARM_SITE = $(call github,chips-fr,uae4arm-rpi,$(LIBRETRO_UAE4ARM_VERSION))
+LIBRETRO_UAE4ARM__LICENSE = GPLv2
+
+LIBRETRO_UAE4ARM_PLATFORM=$(LIBRETRO_PLATFORM)
+
+ifeq ($(BR2_PACKAGE_BATOCERA_TARGET_RPI1),y)
+LIBRETRO_UAE4ARM_PLATFORM = rpi
+
+else ifeq ($(BR2_PACKAGE_BATOCERA_TARGET_RPI2),y)
+LIBRETRO_UAE4ARM_PLATFORM = rpi2
+
+else ifeq ($(BR2_PACKAGE_BATOCERA_TARGET_RPI3)$(BR2_PACKAGE_BATOCERA_TARGET_RPIZERO2),y)
+    ifeq ($(BR2_arm),y)
+        LIBRETRO_UAE4ARM_PLATFORM = rpi3
+    else
+        LIBRETRO_UAE4ARM_PLATFORM = rpi3-aarch64
+    endif
+else ifeq ($(BR2_PACKAGE_BATOCERA_TARGET_RPI4),y)
+    ifeq ($(BR2_arm),y)
+        LIBRETRO_UAE4ARM_PLATFORM = rpi4
+    else
+        LIBRETRO_UAE4ARM_PLATFORM = rpi4-aarch64
+    endif
+endif
+
+define LIBRETRO_UAE4ARM_BUILD_CMDS
+	$(TARGET_CONFIGURE_OPTS) $(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" -C $(@D)/ -f Makefile.libretro platform="$(LIBRETRO_UAE4ARM_PLATFORM)"
+endef
+
+define LIBRETRO_UAE4ARM_INSTALL_TARGET_CMDS
+	$(INSTALL) -D $(@D)/uae4arm_libretro.so \
+		$(TARGET_DIR)/usr/lib/libretro/uae4arm_libretro.so
+endef
+
+$(eval $(generic-package))
