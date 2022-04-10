0d368fa0c0c9f0cb067c16f92e715a2a6ffa7668
Lio <lionel.bringuier@gmail.com>
2022-04-10 11:39:36 -0700

    Removed the joypad #1 code

--- package/batocera/core/batocera-configgen/configgen/configgen/generators/odcommander/odcommanderGenerator.py
+++ package/batocera/core/batocera-configgen/configgen/configgen/generators/odcommander/odcommanderGenerator.py
@@ -9,13 +9,6 @@ class OdcommanderGenerator(Generator):
     def generate(self, system, rom, playersControllers, gameResolution):
         commandArray = ["od-commander"]
 
-        # pad number
-        nplayer = 1
-        for playercontroller, pad in sorted(playersControllers.items()):
-            if nplayer == 1:
-                commandArray.append("joynum={}".format(pad.index))
-            nplayer += 1
-
         return Command.Command(array=commandArray,env={
             "SDL_GAMECONTROLLERCONFIG": controllersConfig.generateSdlGameControllerConfig(playersControllers)
         })
