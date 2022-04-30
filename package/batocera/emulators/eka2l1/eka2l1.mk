71e05fff10aeb513b41900b03843835621673e56
Atari <67527064+Hew-ux@users.noreply.github.com>
2022-04-30 13:33:03 +1000

    successful build

--- package/batocera/emulators/eka2l1/eka2l1.mk
+++ package/batocera/emulators/eka2l1/eka2l1.mk
@@ -11,15 +11,13 @@ EKA2L1_GIT_SUBMODULES=YES
 EKA2L1_LICENSE = GPL-3.0
 EKA2L1_DEPENDENCIES = qt5base qt5tools qt5multimedia
 # Seem to be included as external packages, maybe not needed to be included here?
-# fmt boost ffmpeg sdl2
+#fmt boost ffmpeg sdl2
 
 # May be needed for translations.
 #EKA2L1_CONF_OPTS = -DENABLE_QT_TRANSLATION=ON
 
-#EKA2L1_CONF_OPTS = -DCMAKE_INSTALL_PREFIX= $(@D)/buildroot-build/eka2l1 $(TARGET_DIR)/usr/eka2l1
-
-# EKA2L1 compiles most of its stuff from its submodules, and errors out if attempting to use "non-static" libraries.
-EKA2L1_CONF_OPTS = -DBUILD_SHARED_LIBS=OFF
+# EKA2L1 compiles most of its stuff from its submodules, and errors out if attempting to use "non-static" libraries. The build test also fails so it must be forced to off for now (binary seems unaffected by this).
+EKA2L1_CONF_OPTS = -DBUILD_SHARED_LIBS=OFF -DEKA2L1_BUILD_TESTS=OFF
 
 # Does this matter? The readme says to do it if not using Qt5Creator but I'm not seeing where this would actually be in Batocera.
 #EKA2L1_CONF_OPT += -DCMAKE_PREFIX_PATH="$(STAGING_DIR)"
@@ -27,18 +25,17 @@ EKA2L1_CONF_OPTS = -DBUILD_SHARED_LIBS=OFF
 # Should be set when the package cannot be built inside the source tree but needs a separate build directory.
 EKA2L1_SUPPORTS_IN_SOURCE_BUILD = NO
 
-# Maybe necessary to create a new folder and run cmake from it in order to build.
-#define EKA2L1_BUILD_STAGING_CMDS
-#    mkdir newfolder
-#    cmake ..
-#endef
+# Grab the .git folder to ensure git variables apply during the build. Should be fixed upstream in the future.
+define EKA2L1_FIXGIT
+    cp -r $(BR2_DL_DIR)/eka2l1/git/.git $(@D)
+endef
 
-# Sort this out once we have the binary.
-#define EKA2L1_INSTALL_TARGET_CMDS
-#    mkdir -p $(TARGET_DIR)/usr/bin
-#    mkdir -p $(TARGET_DIR)/usr/lib
-#	$(INSTALL) -D $(@D)/buildroot-build/bin/Release/citra-qt \
-#		$(TARGET_DIR)/usr/bin/
-#endef
+EKA2L1_PRE_CONFIGURE_HOOKS += EKA2L1_FIXGIT
+
+define EKA2L1_INSTALL_TARGET_CMDS
+    mkdir -p $(TARGET_DIR)/usr/bin
+    $(INSTALL) -D $(@D)/buildroot-build/bin/eka2l1_qt \
+        $(TARGET_DIR)/usr/bin/
+endef
 
 $(eval $(cmake-package))
