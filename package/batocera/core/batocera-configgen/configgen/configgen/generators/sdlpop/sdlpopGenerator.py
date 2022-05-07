2d945e49d041f39cf662c33c92962ca1ddf96313
Daniel Martin <dmanlfc@gmail.com>
2022-05-07 16:42:00 +1000

    fix sdlpop config file locations

--- package/batocera/core/batocera-configgen/configgen/configgen/generators/sdlpop/sdlpopGenerator.py
+++ package/batocera/core/batocera-configgen/configgen/configgen/generators/sdlpop/sdlpopGenerator.py
@@ -3,12 +3,32 @@
 from generators.Generator import Generator
 import Command
 import controllersConfig
+import batoceraFiles
+import os
+import shutil
 
 class SdlPopGenerator(Generator):
 
     def generate(self, system, rom, playersControllers, gameResolution):
         commandArray = ["SDLPoP"]
 
+        # create sdlpop config directory
+        if not os.path.exists(batoceraFiles.sdlpopConfigDir):
+            os.makedirs(batoceraFiles.sdlpopConfigDir)
+        if not os.path.exists(batoceraFiles.sdlpopSrcCfg):
+            shutil.copyfile('/usr/share/sdlpop/cfg/SDLPoP.cfg', batoceraFiles.sdlpopSrcCfg)
+        if not os.path.exists(batoceraFiles.sdlpopSrcIni):
+            shutil.copyfile('/usr/share/sdlpop/cfg/SDLPoP.ini', batoceraFiles.sdlpopSrcIni)       
+        # symbolic link cfg files
+        if not os.path.exists(batoceraFiles.sdlpopDestCfg):
+            os.symlink(batoceraFiles.sdlpopSrcCfg, batoceraFiles.sdlpopDestCfg)
+        if not os.path.exists(batoceraFiles.sdlpopDestIni):
+            os.symlink(batoceraFiles.sdlpopSrcIni, batoceraFiles.sdlpopDestIni)
+        # symbolic link screenshot folder too
+        if not os.path.exists('/userdata/screenshots/sdlpop'):
+            os.makedirs('/userdata/screenshots/sdlpop')
+            os.symlink('/userdata/screenshots/sdlpop', '/usr/share/sdlpop/screenshots', target_is_directory = True)
+        
         # pad number
         nplayer = 1
         for playercontroller, pad in sorted(playersControllers.items()):
