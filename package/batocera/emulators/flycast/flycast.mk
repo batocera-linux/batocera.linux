f77c9845f0ff7a5a2a6d2331a4d5a785a1179874
Daniel Martin <dmanlfc@gmail.com>
2022-05-15 18:05:36 +1000

    move flycast controller mapping to SDL2

--- package/batocera/emulators/flycast/flycast.mk
+++ package/batocera/emulators/flycast/flycast.mk
@@ -3,8 +3,8 @@
 # flycast
 #
 ################################################################################
-# Version: Commits on May 6, 2022
-FLYCAST_VERSION = 6c6b447ea1d2d251e5f7b96d57b0e243cdbd236d
+# Version: Commits on May 16, 2022
+FLYCAST_VERSION = 221060cc707c66326efca7df9af229f6ac24d1ea
 FLYCAST_SITE = https://github.com/flyinghead/flycast.git
 FLYCAST_SITE_METHOD=git
 FLYCAST_GIT_SUBMODULES=YES
@@ -13,8 +13,15 @@ FLYCAST_DEPENDENCIES = sdl2 libpng libzip libao pulseaudio-utils
 
 FLYCAST_CONF_OPTS += -DLIBRETRO=OFF
 
-# determine the best GLES version to use - prefer GLES3
-ifeq ($(BR2_PACKAGE_BATOCERA_GLES3),y)
+# determine the best OpenGL version to use
+ifeq ($(BR2_PACKAGE_HAS_LIBGL),y)
+  # Batocera - RPi4 prefer GLES
+  ifneq ($(BR2_PACKAGE_BATOCERA_RPI4_WITH_XORG),y)
+    FLYCAST_CONF_OPTS += -DUSE_OPENGL=ON
+  else
+    FLYCAST_CONF_OPTS += -DUSE_GLES=ON -DUSE_GLES2=OFF
+  endif
+else ifeq ($(BR2_PACKAGE_BATOCERA_GLES3),y)
     FLYCAST_CONF_OPTS += -DUSE_GLES=ON -DUSE_GLES2=OFF
 else ifeq ($(BR2_PACKAGE_BATOCERA_GLES2),y)
     FLYCAST_CONF_OPTS += -DUSE_GLES2=ON -DUSE_GLES=OFF
