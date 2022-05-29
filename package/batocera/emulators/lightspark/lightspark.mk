6d5ada17e70c5bdfe7713bcd583f495d672e73ec
liberodark <liberodark@gmail.com>
2022-05-29 11:01:18 +0200

    Bump Flash

--- package/batocera/emulators/lightspark/lightspark.mk
+++ package/batocera/emulators/lightspark/lightspark.mk
@@ -3,8 +3,8 @@
 # LIGHTSPARK
 #
 ################################################################################
-# Version.: Commits on Apr 11, 2022
-LIGHTSPARK_VERSION = a25235ee3092252de421de53da1458e512471148
+# Version.: Commits on May 28, 2022
+LIGHTSPARK_VERSION = 890493f40e7db56833021b7f9daf777d1eb576f7
 LIGHTSPARK_SITE = $(call github,lightspark,lightspark,$(LIGHTSPARK_VERSION))
 LIGHTSPARK_LICENSE = LGPLv3
 LIGHTSPARK_DEPENDENCIES = sdl2 sdl2_mixer freetype pcre jpeg libpng cairo ffmpeg libcurl
