f5cb2d0c6ccf0fb07a8103a5d46806256d800fea
Juliano Dorigão <jdorigao@gmail.com>
2022-06-06 16:31:09 -0400

    bump: firmware-wlan-aml

--- package/batocera/arch/firmware-wlan-aml/firmware-wlan-aml.mk
+++ package/batocera/arch/firmware-wlan-aml/firmware-wlan-aml.mk
@@ -3,8 +3,8 @@
 # firmware-wlan-aml
 #
 ################################################################################
-# Version.: Commits on Dec 16, 2021
-FIRMWARE_WLAN_AML_VERSION = 371e416064e9bf8f0f173ce94de17981b6503f71
+# Version.: Commits on Apr 18, 2022
+FIRMWARE_WLAN_AML_VERSION = 1da9185bddb6ec3c975f5739335e7e23b7e2b6d0
 FIRMWARE_WLAN_AML_SITE = $(call github,LibreELEC,brcmfmac_sdio-firmware,$(FIRMWARE_WLAN_AML_VERSION))
 
 FIRMWARE_WLAN_AML_TARGET_DIR=$(TARGET_DIR)/lib/firmware/brcm
