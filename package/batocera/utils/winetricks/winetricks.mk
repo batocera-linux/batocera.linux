03fcc4947898caf3452d6b9b59766b9e86ddaedf
liberodark <liberodark@gmail.com>
2022-06-14 12:14:35 +0200

    Bump Winetricks
    Build OK

--- package/batocera/utils/winetricks/winetricks.mk
+++ package/batocera/utils/winetricks/winetricks.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 # 20210825
-WINETRICKS_VERSION = 20210825
+WINETRICKS_VERSION = 20220411
 WINETRICKS_SITE = $(call github,Winetricks,winetricks,$(WINETRICKS_VERSION))
 
 define WINETRICKS_INSTALL_TARGET_CMDS
