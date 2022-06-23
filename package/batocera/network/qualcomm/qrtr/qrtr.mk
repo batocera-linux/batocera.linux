702b23ca9f315babc1ff43556295ee6dc4d531ec
Teguh Sobirin <teguh@sobir.in>
2022-06-23 23:54:53 +0700

    Update qrtr.mk

--- package/batocera/network/qualcomm/qrtr/qrtr.mk
+++ package/batocera/network/qualcomm/qrtr/qrtr.mk
@@ -20,8 +20,8 @@ define QRTR_INSTALL_TARGET_CMDS
     $(INSTALL) -D -m 0755 $(@D)/qrtr-cfg $(TARGET_DIR)/usr/bin
     $(INSTALL) -D -m 0755 $(@D)/qrtr-lookup $(TARGET_DIR)/usr/bin
     cp $(@D)/libqrtr.so $(TARGET_DIR)/usr/lib
-	ln -sf $(TARGET_DIR)/usr/lib/libqrtr.so $(TARGET_DIR)/usr/lib/libqrtr.so.1
-    ln -sf $(TARGET_DIR)/usr/lib/libqrtr.so.1 $(TARGET_DIR)/usr/lib/libqrtr.so.1.0
+    ln -sf libqrtr.so $(TARGET_DIR)/usr/lib/libqrtr.so.1
+    ln -sf libqrtr.so.1 $(TARGET_DIR)/usr/lib/libqrtr.so.1.0
 endef
 
 define HOST_QRTR_BUILD_CMDS
