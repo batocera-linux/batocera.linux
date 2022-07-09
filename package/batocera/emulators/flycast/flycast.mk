c32e0efb80c31fa1fb9f67dcc4918c1a01d327f9
Juliano Dorigão <jdorigao@gmail.com>
2022-07-09 09:31:15 -0400

    bump: Flycast

--- package/batocera/emulators/flycast/flycast.mk
+++ package/batocera/emulators/flycast/flycast.mk
@@ -3,8 +3,8 @@
 # flycast
 #
 ################################################################################
-# Version: Commits on Jun 24, 2022
-FLYCAST_VERSION = 907131b7f5c51fa1cdcf453a900e41c999504a25
+# Version: Commits on Jul 9, 2022
+FLYCAST_VERSION = c77c3ab015ae0b7bf6e411aa15c52bd3a71e99ef
 FLYCAST_SITE = https://github.com/flyinghead/flycast.git
 FLYCAST_SITE_METHOD=git
 FLYCAST_GIT_SUBMODULES=YES
