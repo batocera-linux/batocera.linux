952acf80a556e43ef63cd6539ae6db28a81def04
dmanlfc <dmanlfc@gmail.com>
2022-07-29 10:29:14 +1000

    bump libretro-fceumm

--- package/batocera/emulators/retroarch/libretro/libretro-fceumm/libretro-fceumm.mk
+++ package/batocera/emulators/retroarch/libretro/libretro-fceumm/libretro-fceumm.mk
@@ -3,8 +3,8 @@
 # libretro-fceumm
 #
 ################################################################################
-# Version: Commits on Apr 7, 2022
-LIBRETRO_FCEUMM_VERSION = b3c35b6515b2b6a789c589f976a4a323ebebe3eb
+# Version: Commits on Jul 28, 2022
+LIBRETRO_FCEUMM_VERSION = c9ca9204b5f4d02f3f67e14d077714e41632520a
 LIBRETRO_FCEUMM_SITE = $(call github,libretro,libretro-fceumm,$(LIBRETRO_FCEUMM_VERSION))
 LIBRETRO_FCEUMM_LICENSE = GPLv2
 
