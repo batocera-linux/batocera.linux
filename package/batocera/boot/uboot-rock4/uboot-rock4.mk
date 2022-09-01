180fc8c9d1cf1eb3064a4fc7acc60de3e02067ea
Demetris Ierokipides <ierokipides.dem@gmail.com>
2022-09-01 22:35:04 +0300

    Place Rockpi-4 uboot files in its own directory - makes it cleaner to flash

--- package/batocera/boot/uboot-rock4/uboot-rock4.mk
+++ package/batocera/boot/uboot-rock4/uboot-rock4.mk
@@ -5,9 +5,11 @@
 ################################################################################
 UBOOT_ROCK4_VERSION = 1c17801dc72cbc932ee6742aea25e4ba52ec6b2a
 UBOOT_ROCK4_SITE = https://github.com/Multi-Retropie/rock4-uboot
+UBOOT_ROCK4_SITE_METHOD=git
 
 define UBOOT_ROCK4_INSTALL_TARGET_CMDS
-	cp $(@D)/rock4-*   $(BINARIES_DIR)/
+	mkdir -p   $(BINARIES_DIR)/rockpi4/
+	cp $(@D)/rock4-*   $(BINARIES_DIR)/rockpi4/
 endef
 
 $(eval $(generic-package))
