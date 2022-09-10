be248409b4ab9119abb48bef7ff9732927b66371
Atari <67527064+Hew-ux@users.noreply.github.com>
2022-09-10 11:52:04 +1000

    exclude "light" folder from bios/fbneo
    The `bios/fbneo` on a fresh install now includes a `light/` folder. According to its readme:
    ```
    Those "light" dat files are meant for n3ds builds, which can't load a full FBNeo build due to memory limitations.
    ```
    
    They should be removed, they do nothing for Batocera.
    
    There may be a better way to do this "copy everything but exclude the light folder" but this is the one I know. Will draft for a while so others can think of better ideas.

--- package/batocera/emulators/retroarch/libretro/libretro-fbneo/libretro-fbneo.mk
+++ package/batocera/emulators/retroarch/libretro/libretro-fbneo/libretro-fbneo.mk
@@ -63,8 +63,8 @@ define LIBRETRO_FBNEO_INSTALL_TARGET_CMDS
 
     # Need to think of another way to use these files.
     # They take up a lot of space on tmpfs.
-	cp -r $(@D)/dats/* \
-		$(TARGET_DIR)/usr/share/batocera/datainit/bios/fbneo
+	rsync -a $(@D)/dats/* \
+		$(TARGET_DIR)/usr/share/batocera/datainit/bios/fbneo --exclude light
 endef
 
 $(eval $(generic-package))
