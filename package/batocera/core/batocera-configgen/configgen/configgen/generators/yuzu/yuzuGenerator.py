d7f884117ae5e8dcfba7fcd193b2b761c915fb9b
Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>
2022-09-26 20:42:50 +0000

    fix pathes
    Signed-off-by: Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>

--- package/batocera/core/batocera-configgen/configgen/configgen/generators/yuzu/yuzuGenerator.py
+++ package/batocera/core/batocera-configgen/configgen/configgen/generators/yuzu/yuzuGenerator.py
@@ -3,10 +3,18 @@
 from generators.Generator import Generator
 import Command
 import os
+import batoceraFiles # GLOBAL VARIABLES
 
 class YuzuGenerator(Generator):
 
     def generate(self, system, rom, playersControllers, guns, gameResolution):
 
+        if not os.path.exists(batoceraFiles.CONF+"/yuzu"):
+            os.makedirs(batoceraFiles.CONF+"/yuzu")
+
         commandArray = ["/usr/bin/yuzu", "-f", "-g", rom ]
-        return Command.Command(array=commandArray, env={"QT_QPA_PLATFORM":"xcb"})
+        return Command.Command(array=commandArray, env={
+            "XDG_CONFIG_HOME":batoceraFiles.CONF, \
+            "XDG_DATA_HOME":batoceraFiles.SAVES + "/switch", \
+            "XDG_CACHE_HOME":batoceraFiles.CACHE, \
+            "QT_QPA_PLATFORM":"xcb"})
