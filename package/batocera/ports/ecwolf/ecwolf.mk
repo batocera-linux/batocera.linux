05cb89c3de865cbe9131e768eac91cedfe774ffd
dmanlfc <dmanlfc@gmail.com>
2022-09-27 16:27:37 +1000

    bump & fix ecwolf for v36

--- package/batocera/ports/ecwolf/ecwolf.mk
+++ package/batocera/ports/ecwolf/ecwolf.mk
@@ -3,23 +3,26 @@
 # ecwolf
 #
 ################################################################################
-# Version.: Commits on Aug 1, 2021
-ECWOLF_VERSION = ac4a22947240db067021e55f437c0ea29844760c
+# Version.: Commits on Sep 24, 2022
+ECWOLF_VERSION = db154c482943e89c16b8d4de23120e66f5312042
 ECWOLF_SITE = https://bitbucket.org/ecwolf/ecwolf.git
 ECWOLF_SITE_METHOD=git
 ECWOLF_GIT_SUBMODULES=YES
 ECWOLF_LICENSE = Non-commercial
 ECWOLF_DEPENDENCIES = sdl2
 
-ECWOLF_CONF_OPTS += -DCMAKE_BUILD_TYPE=Release -DGPL=ON
+ECWOLF_SUPPORTS_IN_SOURCE_BUILD = NO
+
+ECWOLF_CONF_OPTS += -DCMAKE_BUILD_TYPE=Release -DGPL=ON -DINTERNAL_SDL=ON \
+                    -DINTERNAL_SDL_MIXER=ON -DINTERNAL_SDL_NET=ON
 
 ECWOLF_CONF_ENV += LDFLAGS="-lpthread -lvorbisfile -lopusfile -lFLAC -lmodplug -lfluidsynth"
 
 define ECWOLF_INSTALL_TARGET_CMDS
-		mkdir -p $(TARGET_DIR)/usr/bin
-		mkdir -p $(TARGET_DIR)/usr/share/ecwolf
-	$(INSTALL) -D -m 0755 $(@D)/ecwolf $(TARGET_DIR)/usr/share/ecwolf/ecwolf
-	cp -a $(@D)/ecwolf.pk3 $(TARGET_DIR)/usr/share/ecwolf/
+	mkdir -p $(TARGET_DIR)/usr/bin
+	mkdir -p $(TARGET_DIR)/usr/share/ecwolf
+	$(INSTALL) -D -m 0755 $(@D)/buildroot-build/ecwolf $(TARGET_DIR)/usr/share/ecwolf/ecwolf
+	cp -a $(@D)/buildroot-build/ecwolf.pk3 $(TARGET_DIR)/usr/share/ecwolf/
 	ln -sf /usr/share/ecwolf/ecwolf $(TARGET_DIR)/usr/bin/ecwolf
 
 	# evmap config
