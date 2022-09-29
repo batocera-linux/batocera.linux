6920443e1c9d45882b11d548cb7dd5ed5248d0ad
Juliano Dorigão <jdorigao@gmail.com>
2022-09-29 09:46:27 -0400

    firmwares: batocera/firmwares
    Merging the alllinuxfirmwares and arch folders into one, "firmwares" and adding alllinuxfirmwares as a dependency to ensure it is always built before the platform-specific firmware package.

--- package/batocera/firmwares/firmware-wlan-aml/firmware-wlan-aml.mk
+++ package/batocera/firmwares/firmware-wlan-aml/firmware-wlan-aml.mk
@@ -6,6 +6,7 @@
 # Version.: Commits on Apr 18, 2022
 FIRMWARE_WLAN_AML_VERSION = 1da9185bddb6ec3c975f5739335e7e23b7e2b6d0
 FIRMWARE_WLAN_AML_SITE = $(call github,LibreELEC,brcmfmac_sdio-firmware,$(FIRMWARE_WLAN_AML_VERSION))
+FIRMWARE_WLAN_AML_DEPENDENCIES = alllinuxfirmwares
 
 FIRMWARE_WLAN_AML_TARGET_DIR=$(TARGET_DIR)/lib/firmware/brcm
 
