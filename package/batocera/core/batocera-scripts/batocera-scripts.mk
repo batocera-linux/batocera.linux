5fb2e201d84946012b367266101212d994552f3e
Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>
2022-10-01 07:12:39 +0000

    fix missing switch bins
    Signed-off-by: Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>

--- package/batocera/core/batocera-scripts/batocera-scripts.mk
+++ package/batocera/core/batocera-scripts/batocera-scripts.mk
@@ -64,6 +64,9 @@ define BATOCERA_SCRIPTS_INSTALL_TARGET_CMDS
 	install -m 0755 $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/core/batocera-scripts/scripts/batocera-timezone               $(TARGET_DIR)/usr/bin/
 	install -m 0755 $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/core/batocera-scripts/scripts/batocera-gameforce              $(TARGET_DIR)/usr/bin/
 	install -m 0755 $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/core/batocera-scripts/scripts/batocera-shutdown               $(TARGET_DIR)/usr/bin/
+
+	install -m 0755 $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/core/batocera-scripts/scripts/batocera-switch-screen-checker  $(TARGET_DIR)/usr/bin/
+	install -m 0644 $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/core/batocera-scripts/rules/80-switch-screen.rules            $(TARGET_DIR)/etc/udev/rules.d
 endef
 
 define BATOCERA_SCRIPTS_INSTALL_RG552
