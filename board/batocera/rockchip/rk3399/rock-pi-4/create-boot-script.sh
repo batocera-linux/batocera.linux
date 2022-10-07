3e7d199cc77bb2bdab2e38f3e9241084bbcc0945
Juliano Dorigão <jdorigao@gmail.com>
2022-10-06 21:22:43 -0400

    clean: linux-defconfig

--- board/batocera/rockchip/rk3399/rock-pi-4/create-boot-script.sh
+++ board/batocera/rockchip/rk3399/rock-pi-4/create-boot-script.sh
@@ -17,13 +17,13 @@ BATOCERA_BINARIES_DIR=$6
 mkdir -p "${BATOCERA_BINARIES_DIR}/boot/boot"     || exit 1
 mkdir -p "${BATOCERA_BINARIES_DIR}/boot/extlinux" || exit 1
 
-cp "${BINARIES_DIR}/Image"                 "${BATOCERA_BINARIES_DIR}/boot/boot/linux"                || exit 1
-cp "${BINARIES_DIR}/initrd.gz"             "${BATOCERA_BINARIES_DIR}/boot/boot/initrd.gz"            || exit 1
-cp "${BINARIES_DIR}/rootfs.squashfs"       "${BATOCERA_BINARIES_DIR}/boot/boot/batocera.update"      || exit 1
+cp "${BINARIES_DIR}/Image"                  "${BATOCERA_BINARIES_DIR}/boot/boot/linux"                || exit 1
+cp "${BINARIES_DIR}/initrd.gz"              "${BATOCERA_BINARIES_DIR}/boot/boot/initrd.gz"            || exit 1
+cp "${BINARIES_DIR}/rootfs.squashfs"        "${BATOCERA_BINARIES_DIR}/boot/boot/batocera.update"      || exit 1
 
-cp "${BINARIES_DIR}/rk3399-rock-pi-4a.dtb"    "${BATOCERA_BINARIES_DIR}/boot/boot/"     || exit 1
-cp "${BINARIES_DIR}/rk3399-rock-pi-4b.dtb"    "${BATOCERA_BINARIES_DIR}/boot/boot/"     || exit 1
-cp "${BINARIES_DIR}/rk3399-rock-pi-4c.dtb"    "${BATOCERA_BINARIES_DIR}/boot/boot/"     || exit 1
-cp "${BOARD_DIR}/boot/extlinux.conf"       "${BATOCERA_BINARIES_DIR}/boot/extlinux/" || exit 1
+cp "${BINARIES_DIR}/rk3399-rock-pi-4a.dtb"  "${BATOCERA_BINARIES_DIR}/boot/boot/"     || exit 1
+cp "${BINARIES_DIR}/rk3399-rock-pi-4b.dtb"  "${BATOCERA_BINARIES_DIR}/boot/boot/"     || exit 1
+cp "${BINARIES_DIR}/rk3399-rock-pi-4c.dtb"  "${BATOCERA_BINARIES_DIR}/boot/boot/"     || exit 1
+cp "${BOARD_DIR}/boot/extlinux.conf"        "${BATOCERA_BINARIES_DIR}/boot/extlinux/" || exit 1
 
 exit 0
