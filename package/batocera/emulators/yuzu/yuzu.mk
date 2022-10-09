72ad41386e2a1790d24b6a383c73fb8557587813
liberodark <liberodark@gmail.com>
2022-10-09 13:39:44 +0200

    Add Room
    Add Room for MP

--- package/batocera/emulators/yuzu/yuzu.mk
+++ package/batocera/emulators/yuzu/yuzu.mk
@@ -3,8 +3,8 @@
 # YUZU
 #
 ################################################################################
-# Version.: Commits on Apr 24, 2022
-YUZU_VERSION = 465c46387d38c9fd0c14ad856fb22edb7c2858d7
+# Version.: Commits on Oct 09, 2022
+YUZU_VERSION = 55e6d0dae003378ca8ed9a10b6fbb8626a1fd25f
 YUZU_SITE = https://github.com/yuzu-emu/yuzu.git
 YUZU_SITE_METHOD=git
 YUZU_GIT_SUBMODULES=YES
@@ -35,6 +35,7 @@ define YUZU_INSTALL_TARGET_CMDS
 
         $(INSTALL) -D $(@D)/buildroot-build/bin/yuzu $(TARGET_DIR)/usr/bin/
         $(INSTALL) -D $(@D)/buildroot-build/bin/yuzu-cmd $(TARGET_DIR)/usr/bin/
+        $(INSTALL) -D $(@D)/buildroot-build/bin/yuzu-room $(TARGET_DIR)/usr/bin/
 
         #evmap config
         mkdir -p $(TARGET_DIR)/usr/share/evmapy
