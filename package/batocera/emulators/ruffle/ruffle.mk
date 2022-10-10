3be74396e2dda0f3baaf12e7c3dfb48ceadbbad0
liberodark <liberodark@gmail.com>
2022-10-10 14:33:06 +0200

    Update ruffle.mk

--- package/batocera/emulators/ruffle/ruffle.mk
+++ package/batocera/emulators/ruffle/ruffle.mk
@@ -3,8 +3,8 @@
 # ruffle
 #
 ################################################################################
-# Version: Commits on Aug 19, 2022
-RUFFLE_VERSION = 8a1e297e262350088ea81ec8e0509188dd668f18
+# Version: Commits on Oct 09, 2022
+RUFFLE_VERSION = 10c19fef5751b3fd648fb997864f953de9ce9a72
 RUFFLE_SITE = $(call github,ruffle-rs,ruffle,$(RUFFLE_VERSION))
 RUFFLE_LICENSE = GPLv2
 RUFFLE_DEPENDENCIES = host-rustc host-rust-bin openssl
