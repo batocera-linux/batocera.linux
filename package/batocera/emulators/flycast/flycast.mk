cc2e7caa8332dfe692bb1d52ae1f051282d4e4dd
dmanlfc <dmanlfc@gmail.com>
2022-10-14 22:16:48 +1000

    update & optimise flycast(s)

--- package/batocera/emulators/flycast/flycast.mk
+++ package/batocera/emulators/flycast/flycast.mk
@@ -3,8 +3,8 @@
 # flycast
 #
 ################################################################################
-# Version.: Release on Aug 29, 2022
-FLYCAST_VERSION = v2.0
+# Version: Commits on Oct 13, 2022
+FLYCAST_VERSION = 02d4b821d755087d2db0fcb3bc002e198ac0fa22
 FLYCAST_SITE = https://github.com/flyinghead/flycast.git
 FLYCAST_SITE_METHOD=git
 FLYCAST_GIT_SUBMODULES=YES
@@ -30,6 +30,20 @@ endif
 # RPI: use the legacy Broadcom GLES libraries
 ifeq ($(BR2_PACKAGE_BATOCERA_RPI_VCORE),y)
     FLYCAST_CONF_OPTS += -DUSE_VIDEOCORE=ON
+endif
+
+ifeq ($(BR2_PACKAGE_HAS_LIBMALI),y)
+    FLYCAST_CONF_OPTS += -DUSE_MALI=ON
+endif
+
+ifeq ($(BR2_PACKAGE_BATOCERA_TARGET_RG552),y)
+    FLYCAST_CONF_OPTS += -DRG552=ON
+else ifeq ($(BR2_PACKAGE_BATOCERA_TARGET_BCM2711),y)
+    FLYCAST_CONF_OPTS += -DRPI4=ON
+else ifeq ($(BR2_PACKAGE_BATOCERA_TARGET_RK3326),y)
+    FLYCAST_CONF_OPTS += -DRK3326=ON
+else ifeq ($(BR2_PACKAGE_BATOCERA_TARGET_S922X),y)
+    FLYCAST_CONF_OPTS += -DS922X=ON
 endif
 
 define FLYCAST_INSTALL_TARGET_CMDS
