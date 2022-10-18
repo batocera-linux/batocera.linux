012096d94db8d9090072d5d931948417bffe08fe
dmanlfc <dmanlfc@gmail.com>
2022-10-18 18:33:30 +1000

    add the rtl 8812bu & 8822bu support

---
+++ package/batocera/network/rtl88x2bu/rtl88x2bu.mk
@@ -0,0 +1,24 @@
+################################################################################
+#
+# rtl88x2bu
+#
+################################################################################
+
+RTL88X2BU_VERSION = 6a2916db1a01625b9f2fc77d466f07e498735c44
+RTL88X2BU_SITE = $(call github,morrownr,88x2bu-20210702,$(RTL88X2BU_VERSION))
+RTL88X2BU_LICENSE = GPL-2.0
+RTL88X2BU_LICENSE_FILES = LICENSE
+
+RTL88X2BU_MODULE_MAKE_OPTS = \
+	CONFIG_RTL8822BU=m \
+	USER_EXTRA_CFLAGS="-DCONFIG_$(call qstrip,$(BR2_ENDIAN))_ENDIAN \
+		-Wno-error"
+
+define RTL88X2BU_MAKE_SUBDIR
+        (cd $(@D); ln -s . rtl88x2bu)
+endef
+
+RTL88X2BU_PRE_CONFIGURE_HOOKS += RTL88X2BU_MAKE_SUBDIR
+
+$(eval $(kernel-module))
+$(eval $(generic-package))
