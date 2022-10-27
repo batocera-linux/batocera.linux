8a3d3b309c5b5a319df9773415a1c8076bbfb818
dmanlfc <dmanlfc@gmail.com>
2022-10-27 23:47:38 +1000

    fix od-commander font

--- package/batocera/utils/od-commander/od-commander.mk
+++ package/batocera/utils/od-commander/od-commander.mk
@@ -47,8 +47,11 @@ endif
 
 define OD_COMMANDER_INSTALL_TARGET_CMDS
 	mkdir -p $(TARGET_DIR)$(OD_COMMANDER_RESOURCES_DIR)
+	mkdir -p $(TARGET_DIR)/usr/share/fonts/truetype/droid
 	$(INSTALL) -m 0644 $(@D)/res/Fiery_Turk.ttf \
 	  $(TARGET_DIR)$(OD_COMMANDER_RESOURCES_DIR)
+	$(INSTALL) -m 0644 $(@D)/res/DroidSansFallback.ttf \
+	  $(TARGET_DIR)/usr/share/fonts/truetype/droid/
 	$(INSTALL) -m 0644 $(@D)/res/*.png \
 	  $(TARGET_DIR)$(OD_COMMANDER_RESOURCES_DIR)
 	$(INSTALL) -m 0755 -D $(OD_COMMANDER_BUILDDIR)commander \
