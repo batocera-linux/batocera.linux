3893080266ac20e0e1a81b11384eaaf30eba4d73
Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>
2022-10-28 06:00:13 +0000

    guncon buttons
    Signed-off-by: Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>

--- package/batocera/controllers/guncon/guncon.mk
+++ package/batocera/controllers/guncon/guncon.mk
@@ -8,6 +8,7 @@ GUNCON_SITE = $(call github,beardypig,guncon2,$(GUNCON_VERSION))
 
 define GUNCON_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0644 -D $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/controllers/guncon/99-guncon.rules $(TARGET_DIR)/etc/udev/rules.d/99-guncon.rules
+	$(INSTALL) -m 0755 -D $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/controllers/guncon/guncon-add      $(TARGET_DIR)/usr/bin/guncon-add
 endef
 
 
