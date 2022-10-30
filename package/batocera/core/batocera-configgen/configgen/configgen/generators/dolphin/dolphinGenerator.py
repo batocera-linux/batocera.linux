134f8d24f0f239df23147e8359061727bf65df78
Ian Murray <tvsian@gmail.com>
2022-10-30 18:17:33 -0400

    GBA ROM loading modification
    Missing Option

--- package/batocera/core/batocera-configgen/configgen/configgen/generators/dolphin/dolphinGenerator.py
+++ package/batocera/core/batocera-configgen/configgen/configgen/generators/dolphin/dolphinGenerator.py
@@ -18,6 +18,27 @@ class DolphinGenerator(Generator):
         gbaMode = False
         gbaSlots = 0
         gbaPort = {}
+        gbaROMs = []
+        if os.path.splitext(rom)[1] == ".gbl":
+            openFile = open(rom, 'r')
+            fileInput = openFile.readlines()
+            lineCount = 0
+            for line in fileInput:
+                if lineCount == 0:
+                    gcROM = line.strip()
+                    if os.path.exists(gcROM):
+                        rom = gcROM
+                    elif os.path.exists(f"/userdata/roms/gamecube/{gcROM}"):
+                        rom = f"/userdata/roms/gamecube/{gcROM}"
+                    elif os.path.exists(f"/userdata/roms/gamecube{gcROM}"):
+                        rom = f"/userdata/roms/gamecube{gcROM}"
+                    else:
+                        eslog.error(f"GameCube ROM {gcROM} in {rom} not found, check path or filename")
+                else:
+                    gbaROMs.append(line.strip())
+                lineCount = lineCount + 1
+            openFile.close()
+            eslog.debug(f"Loaded .gbl file. GC ROM: {rom}, GBA ROM(s): {gbaROMs}")
 
         if not os.path.exists(os.path.dirname(batoceraFiles.dolphinIni)):
             os.makedirs(os.path.dirname(batoceraFiles.dolphinIni))
@@ -144,12 +165,14 @@ class DolphinGenerator(Generator):
         if not os.path.exists(gbaSavePath):
             os.makedirs(gbaSavePath)
         dolphinSettings.set("GBA", "SavesPath", gbaSavePath)
+        assignedROM = 0
         for i in range(1,5):
             if gbaPort[i]:
-                gbaROM = getGBAROM(rom, i)
-                dolphinSettings.set("GBA", f"Rom{str(i)}", gbaROM)
-            else:
-                dolphinSettings.set("GBA", f"Rom{str(i)}", "")
+                if len(gbaROMs) <= assignedROM:
+                    dolphinSettings.set("GBA", f"Rom{str(i)}", prepGBAROM(gbaROMs[assignedROM], i))
+                    assignedROM = assignedROM + 1
+                else:
+                    dolphinSettings.set("GBA", f"Rom{str(i)}", prepGBAROM(gbaROMs[len(gbaROMs)-1], i))
 
         # Change discs automatically
         dolphinSettings.set("Core", "AutoDiscChange", "True")
@@ -335,8 +358,9 @@ class DolphinGenerator(Generator):
 
         #GBA Mode
         for i in range(1,5):
-            if system.isOptSet("dolphin_port_" + str(i) + "_type") and system.config["dolphin_port_" + str(i) + "_type"] == "13":
-               return 16/9
+            if ("dolphin_port_" + str(i) + "_type") in config and config["dolphin_port_" + str(i) + "_type"] == "13":
+                eslog.debug("Decorations disabled - GBA Mode")
+                return 16/9
 
         # Auto
         if dolphin_aspect_ratio == "0":
@@ -438,19 +462,28 @@ def createGBAFiles(rom, slots, mode):
     gbaFile.close()
     makeExecutble(dolphinLauncher)
 
-def getGBAROM(rom, slot):
+def prepGBAROM(rom, slot):
     baseFileName = os.path.splitext(os.path.basename(rom))[0]
     baseFilePath = os.path.dirname(rom)
 
-    for FileExt in ["zip", "7z", "gba", "gb", "gbc", "agb", "mb", "rom", "bin"]:
-        gbaROMFile = f"{baseFilePath}/{baseFileName}-{slot}.{FileExt}"
-        if os.path.exists(gbaROMFile):
-            eslog.debug(f"Found {gbaROMFile} for GBA #{slot}")
-            return gbaROMFile
-
-    eslog.debug(f"No ROM found for GBA #{slot}")
-    return ""
-
+    # By default, we symlink the save file if it doesn't exist.
+    if os.path.exists(f"/userdata/saves/gba/{baseFileName}.sav"):
+        if not os.path.exists(f"/userdata/saves/gamecube/gba/{baseFileName}-{slot}.sav"):
+            os.symlink(f"/userdata/saves/gba/{baseFileName}.sav", f"/userdata/saves/gamecube/gba/{baseFileName}-{slot}.sav")
+            eslog.debug(f"Symlinked /userdata/saves/gba/{baseFileName}.sav to /userdata/saves/gamecube/gba/{baseFileName}-{slot}.sav")
+        else:
+            eslog.debug(f"Save file /userdata/saves/gamecube/gba/{baseFileName}-{slot}.sav exists, not overwriting.")
+    else:
+        eslog.debug(f"No save file found, no link created.")
+    if os.path.exists(rom):
+        return rom
+    elif os.path.exists(f"/userdata/roms/gba/{rom}"):
+        return f"/userdata/roms/gba/{rom}"
+    elif os.path.exists(f"/userdata/roms/gba{rom}"):
+        return f"/userdata/roms/gba{rom}"
+    else:
+        eslog.error(f"GBA ROM {rom} not found, check path or filename")
+        return ""
 
 def removeExisting(fileName):
     if os.path.exists(fileName):
