fd8277ace364942a7260ffb2c2ffb25a30903f10
Lio <lionel.bringuier@gmail.com>
2022-11-03 09:29:25 -0700

    Update following @dmanlfc 's comments

--- package/batocera/emulators/ikemen/ikemen.mk
+++ package/batocera/emulators/ikemen/ikemen.mk
@@ -7,30 +7,30 @@
 IKEMEN_VERSION = f586e49413c26646642dc3b5039478bd764d386a
 IKEMEN_SITE = https://github.com/ikemen-engine/Ikemen-GO
 IKEMEN_LICENSE = MIT
-IKEMEN_DEPENDENCIES = host-go
+IKEMEN_DEPENDENCIES = host-go libgtk3 mesa3d openal libglfw
 
 IKEMEN_SITE_METHOD = git
 IKEMEN_GIT_SUBMODULES = YES
 
-IKEMEN_TARGET_ENV = \
-	GOROOT="$(HOST_GO_ROOT)" \
-	GOPATH="$(HOST_GO_GOPATH)" \
-	PATH=$(BR_PATH) \
-    GOCACHE="$(HOST_GO_TARGET_CACHE)" \
-	GOMODCACHE="$(@D)" \
-	GOFLAGS="-modcacherw" \
+HOST_GO_COMMON_ENV = GOFLAGS=-mod=mod \
+		     GO111MODULE=on \
+		     GOROOT="$(HOST_GO_ROOT)" \
+		     GOPATH="$(HOST_GO_GOPATH)" \
+		     GOCACHE="$(HOST_GO_TARGET_CACHE)" \
+		     GOMODCACHE="$(@D)" \
+		     GOFLAGS="-modcacherw" \
+		     PATH=$(BR_PATH) \
+		     GOBIN= \
+		     CGO_ENABLED=$(HOST_GO_CGO_ENABLED)
 
 define IKEMEN_BUILD_CMDS
-	$(IKEMEN_TARGET_ENV) $(MAKE) \
-		CPP="$(TARGET_CPP)" CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" \
-		AS="$(TARGET_CC)" LD="$(TARGET_LD)" STRIP="$(TARGET_STRIP)" \
-		-C $(@D) -f Makefile Ikemen_GO_Linux
+	$(HOST_GO_TARGET_ENV) $(MAKE) -C $(@D) -f Makefile Ikemen_GO_Linux
 endef
 
 define IKEMEN_INSTALL_TARGET_CMDS
 	mkdir -p $(TARGET_DIR)/usr/bin
-
 	$(INSTALL) -D $(@D)/bin/Ikemen_GO_Linux $(TARGET_DIR)/usr/bin/ikemen
+	# evmapuy
 	mkdir -p $(TARGET_DIR)/usr/share/evmapy
 	cp $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/emulators/ikemen/ikemen.keys $(TARGET_DIR)/usr/share/evmapy
 endef
