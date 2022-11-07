d65287b9f4179b16a76ba6058f6e2596c48f69cc
Juliano Dorigão <jdorigao@gmail.com>
2022-11-07 18:29:23 -0400

    rtl8192eu: Update to Nov 5, 2022

--- package/batocera/network/rtl8192eu/rtl8192eu.mk
+++ package/batocera/network/rtl8192eu/rtl8192eu.mk
@@ -3,8 +3,8 @@
 # rtl8192eu
 #
 ################################################################################
-
-RTL8192EU_VERSION = e39c4e7a66b05fab6eceb2bb251d399c003fb544
+# Version.: Commits on Nov 5, 2022
+RTL8192EU_VERSION = 23e06000d82a54788ba0c90a4d4caf4663de6b7a
 RTL8192EU_SITE = $(call github,Mange,rtl8192eu-linux-driver,$(RTL8192EU_VERSION))
 RTL8192EU_LICENSE = GPL-2.0
 RTL8192EU_LICENSE_FILES = LICENSE
