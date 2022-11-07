afba69ce7a55f997bbb1b67754dc51a64a9a6c98
Lio <lionel.bringuier@gmail.com>
2022-11-07 11:59:35 -0800

    Bump pcsx2

--- package/batocera/emulators/pcsx2-avx2/pcsx2-avx2.mk
+++ package/batocera/emulators/pcsx2-avx2/pcsx2-avx2.mk
@@ -3,8 +3,8 @@
 # pcsx2-avx2
 #
 ################################################################################
-#Version: Commits on Oct 5, 2022
-PCSX2_AVX2_VERSION = v1.7.3371
+#Version: Commits on Nov 5, 2022
+PCSX2_AVX2_VERSION = v1.7.3527
 PCSX2_AVX2_SITE = https://github.com/pcsx2/pcsx2.git
 PCSX2_AVX2_SITE_METHOD = git
 PCSX2_AVX2_GIT_SUBMODULES = YES
