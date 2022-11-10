a7255ca0656c48f13656e2ec852376a7e56b165f
dmanlfc <dmanlfc@gmail.com>
2022-11-10 22:10:12 +1000

    nothing to see here...

--- package/batocera/core/batocera-configgen/configgen/configgen/generators/ryujinx/ryujinxGenerator.py
+++ package/batocera/core/batocera-configgen/configgen/configgen/generators/ryujinx/ryujinxGenerator.py
@@ -3,16 +3,221 @@
 from generators.Generator import Generator
 import Command
 import os
+from os import environ
 import batoceraFiles
+import controllersConfig
+import filecmp
+import json
+import shutil
+import evdev
+from evdev import InputDevice
 
-ryujinxHome = batoceraFiles.CONF
+ryujinxConf = batoceraFiles.CONF + "/Ryujinx"
+ryujinxConfFile = ryujinxConf + "/Config.json"
+ryujinxKeys = batoceraFiles.BIOS + "/switch/prod.keys"
+ryujinxExec = ryujinxConf + "/ryujinx"
+
+ryujinxCtrl = {
+        "left_joycon_stick": {
+        "joystick": "Left",
+        "invert_stick_x": False,
+        "invert_stick_y": False,
+        "rotate90_cw": False,
+        "stick_button": "LeftStick"
+      },
+      "right_joycon_stick": {
+        "joystick": "Right",
+        "invert_stick_x": False,
+        "invert_stick_y": False,
+        "rotate90_cw": False,
+        "stick_button": "RightStick"
+      },
+      "deadzone_left": 0,
+      "deadzone_right": 0,
+      "range_left": 1,
+      "range_right": 1,
+      "trigger_threshold": 0,
+      "motion": {
+        "motion_backend": "GamepadDriver",
+        "sensitivity": 100,
+        "gyro_deadzone": 1,
+        "enable_motion": True
+      },
+      "rumble": {
+        "strong_rumble": 8,
+        "weak_rumble": 2,
+        "enable_rumble": True
+      },
+      "left_joycon": {
+        "button_minus": "Minus",
+        "button_l": "LeftShoulder",
+        "button_zl": "LeftTrigger",
+        "button_sl": "Unbound",
+        "button_sr": "Unbound",
+        "dpad_up": "DpadUp",
+        "dpad_down": "DpadDown",
+        "dpad_left": "DpadLeft",
+        "dpad_right": "DpadRight"
+      },
+      "right_joycon": {
+        "button_plus": "Plus",
+        "button_r": "RightShoulder",
+        "button_zr": "RightTrigger",
+        "button_sl": "Unbound",
+        "button_sr": "Unbound",
+        "button_x": "Y",
+        "button_b": "A",
+        "button_y": "X",
+        "button_a": "B"
+    },
+    "version": 1,
+    "backend": "GamepadSDL2",
+}
 
 class RyujinxGenerator(Generator):
 
     def generate(self, system, rom, playersControllers, guns, gameResolution):
+        if not os.path.exists(ryujinxConf):
+            os.makedirs(ryujinxConf)
+        if not os.path.exists(ryujinxConf + "/system"):
+            os.makedirs(ryujinxConf + "/system")
 
-        #commandArray = ["/usr/ryujinx/Ryujinx", rom ]
+        # Copy file & make executable (workaround)
+        if not os.path.exists(ryujinxExec) or not filecmp.cmp("/usr/ryujinx/Ryujinx", ryujinxExec):
+            shutil.copyfile("/usr/ryujinx/Ryujinx", ryujinxExec)
+            os.chmod(ryujinxExec, 0o0775)
+
+        # Copy the prod.keys file to where ryujinx reads it
+        if os.path.exists(ryujinxKeys):
+            shutil.copyfile(ryujinxKeys, ryujinxConf + "/system/prod.keys")
+
+        # [Configuration]
+        if not os.path.exists(os.path.dirname(ryujinxConfFile)):
+            os.makedirs(os.path.dirname(ryujinxConfFile))
+        try:
+            conf = json.load(open(ryujinxConfFile, "r"))
+        except:
+            conf = {}
+
+        # Set defaults
+        conf["enable_discord_integration"] = False
+        conf["check_updates_on_start"] = False
+        conf["show_confirm_exit"] = False
+        conf["hide_cursor_on_idle"] = True
+        conf["game_dirs"] = ["/userdata/roms/switch"]
+        conf["start_fullscreen"] = True
+        conf["docked_mode"] = True
+        # set ryujinx app language
+        conf["language_code"] = str(getLangFromEnvironment())
+
+        # Console language, time & date
+        if system.isOptSet("ryujinx_language"):
+            conf["system_language"] = system.config["ryujinx_language"]
+        else:
+            conf["system_language"] = "AmericanEnglish"
+        
+        if system.isOptSet("ryujinx_region"):
+            conf["system_region"] = system.config["ryujinx_region"]
+        else:
+            conf["system_region"] = "USA"        
+        
+        conf["system_time_zone"] = "UTC"
+        if system.isOptSet("ryujinx_timeoffset"):
+            conf["system_time_offset"] = int(system.config["ryujinx_timeoffset"])
+        else:
+            conf["system_time_offset"]= 0
+
+        # Graphics
+        if system.isOptSet("ryujinx_api"):
+            conf["graphics_backend"] = system.config["ryujinx_api"]
+        else:
+            conf["graphics_backend"] = "Vulkan"
+
+        if system.isOptSet("ryujinx_scale"):
+            conf["res_scale"] = int(system.config["ryujinx_scale"])
+        else:
+            conf["res_scale"] = 1
+
+        if system.isOptSet("ryujinx_ratio"):
+            conf["aspect_ratio"] = system.config["ryujinx_ratio"]
+        else:
+            conf["aspect_ratio"] = "Fixed16x9"
+        
+        if system.isOptSet("ryujinx_filtering"):
+            conf["max_anisotropy"] = int(system.config["ryujinx_filtering"])
+        else:
+            conf["max_anisotropy"] = -1
+
+        conf["input_config"] = []
+
+        # write / update the config file
+        js_out = json.dumps(conf, indent=2)
+        with open(ryujinxConfFile, "w") as jout:
+            jout.write(js_out)
+        
+        # Now add Controllers
+        nplayer = 1
+        for controller, pad in sorted(playersControllers.items()):
+            if nplayer <= 8:
+                ctrlConf = ryujinxCtrl
+                # we need to get the uuid for ryujinx controllers
+                # example xbox 360 - "id": "0-00000003-045e-0000-8e02-000014010000"
+                devices = [InputDevice(fn) for fn in evdev.list_devices()]
+                for dev in devices:
+                    if dev.path == pad.dev:
+                        bustype = "%x" % dev.info.bustype
+                        bustype = bustype.zfill(8)
+                        vendor = "%x" % dev.info.vendor
+                        vendor = vendor.zfill(4)
+                        product = "%x" % dev.info.product
+                        product = product.zfill(4)
+                        # reverse the poduct id, so 028e becomes 8e02
+                        product1 = (product)[-2::]
+                        product2 = (product)[:-2]
+                        product = product1 + product2
+                        # reverse the version id also
+                        version = "%x" % dev.info.version
+                        version = version.zfill(4)
+                        version1 = (version)[-2::]
+                        version2 = (version)[:-2]
+                        version = version1 + version2
+                        ctrlUUID = (f"{pad.index}-{bustype}-{vendor}-0000-{product}-0000{version}0000")
+                        ctrlConf["id"] = ctrlUUID
+                        # always configure a pro controller for now
+                        ctrlConf["controller_type"] = "ProController"
+                        playerNum = (f"Player{nplayer}")
+                        ctrlConf["player_index"] = playerNum
+                        # write the controller to the file
+                        writeControllerIntoJson(ctrlConf)
+                        break
+            nplayer += 1
+
+        if rom == "config":
+            commandArray = [ryujinxExec]
+        else:
+            commandArray = [ryujinxExec, rom]
+
         return Command.Command(
             array=commandArray,
-            env={"XDG_CONFIG_HOME":ryujinxHome, "XDG_DATA_HOME":ryujinxHome, "XDG_CACHE_HOME":batoceraFiles.CACHE, "QT_QPA_PLATFORM":"xcb"}
-            )
+            env={"XDG_CONFIG_HOME":batoceraFiles.CONF, \
+            "XDG_DATA_HOME":batoceraFiles.SAVES + "/switch", \
+            "XDG_CACHE_HOME":batoceraFiles.CACHE, \
+            "QT_QPA_PLATFORM":"xcb", \
+            "SDL_GAMECONTROLLERCONFIG": controllersConfig.generateSdlGameControllerConfig(playersControllers)})
+
+def writeControllerIntoJson(new_controller, filename=ryujinxConfFile):
+    with open(filename,'r+') as file:
+        file_data = json.load(file)
+        file_data["input_config"].append(new_controller)
+        file.seek(0)
+        json.dump(file_data, file, indent=2)
+ 
+def getLangFromEnvironment():
+    lang = environ['LANG'][:5]
+    availableLanguages = { "jp_JP": 0, "en_US": 1, "de_DE": 2,
+                           "fr_FR": 3, "es_ES": 4, "it_IT": 5,
+                           "nl_NL": 6, "zh_CN": 7, "zh_TW": 8, "ko_KR": 9 }
+    if lang in availableLanguages:
+        return availableLanguages[lang]
+    else:
+        return availableLanguages["en_US"]
