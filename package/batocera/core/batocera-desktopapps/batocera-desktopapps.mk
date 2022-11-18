9e5014ce040c164ccc3c315de4a129a6d8663860
dmanlfc <dmanlfc@gmail.com>
2022-11-18 11:42:16 +1000

    add the xenia canary builds as an emulator option

--- package/batocera/core/batocera-desktopapps/batocera-desktopapps.mk
+++ package/batocera/core/batocera-desktopapps/batocera-desktopapps.mk
@@ -141,6 +141,13 @@ ifeq ($(BR2_PACKAGE_XENIA),y)
   BATOCERA_DESKTOPAPPS_ICONS   += xenia.png
 endif
 
+# xenia-canary
+ifeq ($(BR2_PACKAGE_XENIA_CANARY),y)
+  BATOCERA_DESKTOPAPPS_SCRIPTS += batocera-config-xenia-canary
+  BATOCERA_DESKTOPAPPS_APPS    += xenia-canary-config.desktop
+  BATOCERA_DESKTOPAPPS_ICONS   += xenia-canary.png
+endif
+
 # vita3k
 ifeq ($(BR2_PACKAGE_VITA3K),y)
   BATOCERA_DESKTOPAPPS_SCRIPTS += batocera-config-vita3k
