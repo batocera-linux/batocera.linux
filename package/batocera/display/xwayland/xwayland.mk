1c3209ea70bc1161dada67745f2ec3b12f9c5f6e
dmanlfc <dmanlfc@gmail.com>
2022-11-25 14:32:36 +1000

    fix some minor build issues

--- package/batocera/display/xwayland/xwayland.mk
+++ package/batocera/display/xwayland/xwayland.mk
@@ -16,9 +16,21 @@ XWAYLAND_DEPENDENCIES = xorgproto pixman xlib_xtrans xlib_libxkbfile \
                         libdrm libepoxy xfont_font-util xapp_xkbcomp mesa3d \
                         xlib_libxcvt
 
-XWAYLAND_CONF_OPTS += -Dxvfb=false -Ddocs=false -Ddri3=true -Dxwayland-path=/usr/bin
+XWAYLAND_CONF_OPTS += -Dxvfb=false -Ddocs=false -Ddri3=auto -Dxwayland-path=/usr/bin
 XWAYLAND_CONF_OPTS += -Dxkb_dir=/usr/share/X11/xkb -Dxkb_output_dir=/var/lib/xkb
 XWAYLAND_CONF_OPTS += -Dxwayland_eglstream=auto -Dglamor=true
+
+ifeq ($(BR2_PACKAGE_HAS_LIBGL),y)
+    XWAYLAND_CONF_OPTS += -Dglx=true
+else
+    XWAYLAND_CONF_OPTS += -Dglx=false
+endif
+
+ifeq ($(BR2_PACKAGE_LIBDRM),y)
+    XWAYLAND_CONF_OPTS += -Ddrm=true
+else
+    XWAYLAND_CONF_OPTS += -Ddrm=false
+endif
 
 define XWAYLAND_INSTALL_TARGET_CMDS
     mkdir -p $(TARGET_DIR)/usr/bin
