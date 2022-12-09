5f75426efb87d4893946ee9f26b017261a2e284d
Lio <lionel.bringuier@gmail.com>
2022-12-08 16:39:57 -0800

    Bump emulationstation

--- package/batocera/emulationstation/batocera-emulationstation/batocera-emulationstation.mk
+++ package/batocera/emulationstation/batocera-emulationstation/batocera-emulationstation.mk
@@ -3,8 +3,8 @@
 # Batocera EmulationStation
 #
 ################################################################################
-# Last update: Nov 9, 2022
-BATOCERA_EMULATIONSTATION_VERSION = 445036bee6f914b2dc8afc555ccfe8162b628444
+# Last update: Dec 8, 2022
+BATOCERA_EMULATIONSTATION_VERSION = cb617457c028ff0f68f5212bfbd52f961478a25d
 BATOCERA_EMULATIONSTATION_SITE = https://github.com/batocera-linux/batocera-emulationstation
 BATOCERA_EMULATIONSTATION_SITE_METHOD = git
 BATOCERA_EMULATIONSTATION_LICENSE = MIT
