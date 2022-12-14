c1fc36861e042eef8c91c0f6ed17f1576da513c3
dmanlfc <dmanlfc@gmail.com>
2022-12-14 19:01:52 +1000

    fix canary download

--- package/batocera/emulators/xenia-canary/xenia-canary.mk
+++ package/batocera/emulators/xenia-canary/xenia-canary.mk
@@ -3,10 +3,10 @@
 # xenia-canary
 #
 ################################################################################
-
+# Version: b2dd489
 XENIA_CANARY_SOURCE = xenia_canary.zip
-XENIA_CANARY_VERSION = 6e541536dd604ba3ddeb4b61f6bc5f48af4b7db7
-XENIA_CANARY_SITE = https://github.com/xenia-canary/xenia-canary/releases/download/experimental
+XENIA_CANARY_VERSION = experimental
+XENIA_CANARY_SITE = https://github.com/xenia-canary/xenia-canary/releases/download/$(XENIA_CANARY_VERSION)
 XENIA_CANARY_LICENSE = BSD
 XENIA_CANARY_LICENSE_FILE = LICENSE
 
