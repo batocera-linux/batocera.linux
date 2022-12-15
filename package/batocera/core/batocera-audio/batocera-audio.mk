51b645f6f837da12f8c139e0989baa58d39b2663
Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>
2022-12-15 19:47:50 +0000

    fix x86_wow64 build
    Signed-off-by: Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>

--- package/batocera/core/batocera-audio/batocera-audio.mk
+++ package/batocera/core/batocera-audio/batocera-audio.mk
@@ -41,6 +41,7 @@ define BATOCERA_AUDIO_INSTALL_TARGET_CMDS
 	# init script
 	install -m 0755 $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/core/batocera-audio/Saudio \
 		$(TARGET_DIR)/etc/init.d/S06audio
+	mkdir -p $(TARGET_DIR)/etc/modprobe.d
 	ln -sf /var/run/modprobe/intel-dsp.conf $(TARGET_DIR)/etc/modprobe.d/intel-dsp.conf
 	install -m 0755 $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/core/batocera-audio/S27audioconfig \
 		$(TARGET_DIR)/etc/init.d/S27audioconfig
