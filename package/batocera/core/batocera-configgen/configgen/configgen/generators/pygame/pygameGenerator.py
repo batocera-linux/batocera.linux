13fbd9e6c3751a62aaad735118968ffff7c00d5e
Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>
2022-12-18 16:32:11 +0000

    pygame cleaning
    Signed-off-by: Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>

--- package/batocera/core/batocera-configgen/configgen/configgen/generators/pygame/pygameGenerator.py
+++ package/batocera/core/batocera-configgen/configgen/configgen/generators/pygame/pygameGenerator.py
@@ -2,9 +2,13 @@
 
 import Command
 from generators.Generator import Generator
+import os
 
 class PygameGenerator(Generator):
 
     def generate(self, system, rom, playersControllers, guns, gameResolution):
-        commandArray = ["batocera-pygame", rom]
+        commandArray = ["pygame", rom]
         return Command.Command(array=commandArray)
+
+    def executionDirectory(self, config, rom):
+        return os.path.dirname(rom)
