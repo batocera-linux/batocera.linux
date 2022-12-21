a26bf54c345aada2fdc53c69d280f806c248422a
DirtBag Xon <github@pani.cx>
2022-12-21 23:26:32 +0000

    Add new Singe overlay options for hypseus v2.10.1
      - Also removed long obsolete argument from daphne arguments

--- package/batocera/core/batocera-configgen/configgen/configgen/generators/daphne/daphneGenerator.py
+++ package/batocera/core/batocera-configgen/configgen/configgen/generators/daphne/daphneGenerator.py
@@ -56,7 +56,7 @@ class DaphneGenerator(Generator):
                             "-gamepad", "-datadir", batoceraFiles.daphneDatadir, "-homedir", batoceraFiles.daphneDatadir]
         else:
             commandArray = [batoceraFiles.batoceraBins[system.config['emulator']],
-                            romName, "vldp", "-framefile", frameFile, "-useoverlaysb", "2", "-fullscreen",
+                            romName, "vldp", "-framefile", frameFile, "-fullscreen",
                             "-fastboot", "-gamepad", "-datadir", batoceraFiles.daphneDatadir, "-homedir", batoceraFiles.daphneHomedir]
         
         # controller config file
@@ -103,9 +103,13 @@ class DaphneGenerator(Generator):
                     if system.isOptSet('abs_mouse_input') and system.getOptBoolean("abs_mouse_input"):
                         commandArray.extend(["-manymouse"]) # this is causing issues on some "non-gun" games
 
-            # Oversize Overlay (Singe) for HD lightgun games
-            if system.isOptSet('lightgun_hd') and system.getOptBoolean("lightgun_hd"):
+            # Overlay sizes (Singe) for HD lightgun and Singe 2 games
+            if system.isOptSet('overlay_size') and system.config['overlay_size'] == 'oversize':
                 commandArray.append("-set_overlay", "oversize")
+            elif system.isOptSet('overlay_size') and system.config['overlay_size'] == 'full':
+                commandArray.append("-set_overlay", "full")
+            elif system.isOptSet('overlay_size') and system.config['overlay_size'] == 'half':
+                commandArray.append("-set_overlay", "half")
             
             # crosshair
             if system.isOptSet('daphne_crosshair'):
