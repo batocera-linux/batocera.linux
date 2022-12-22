ad420b2011a3c150849e808dccddeced7f3e7a1d
Demetris Ierokipides <ierokipides.dem@gmail.com>
2022-12-22 14:22:45 +0200

    Amiberry: add libpng support See https://github.com/BlitterStudio/amiberry/blob/master/CMakeLists.txt#L297

--- package/batocera/emulators/amiberry/amiberry.mk
+++ package/batocera/emulators/amiberry/amiberry.mk
@@ -7,7 +7,7 @@
 AMIBERRY_VERSION = v5.4
 AMIBERRY_SITE = $(call github,BlitterStudio,amiberry,$(AMIBERRY_VERSION))
 AMIBERRY_LICENSE = GPLv3
-AMIBERRY_DEPENDENCIES = sdl2 sdl2_image sdl2_ttf mpg123 libxml2 libmpeg2 flac
+AMIBERRY_DEPENDENCIES = sdl2 sdl2_image sdl2_ttf mpg123 libxml2 libmpeg2 flac libpng
 
 ifeq ($(BR2_PACKAGE_RPI_USERLAND),y)
 	AMIBERRY_DEPENDENCIES += rpi-userland
