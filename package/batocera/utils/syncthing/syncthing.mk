8f13f8b03619bcc46dbdcf171a319a8b88129f43
dmanlfc <dmanlfc@gmail.com>
2023-01-03 18:08:31 +1000

    fixes build issues with previous version

--- package/batocera/utils/syncthing/syncthing.mk
+++ package/batocera/utils/syncthing/syncthing.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-SYNCTHING_VERSION = v1.20.1
+SYNCTHING_VERSION = v1.22.2
 SYNCTHING_SITE = $(call github,syncthing,syncthing,$(SYNCTHING_VERSION))
 SYNCTHING_LICENSE = MPLv2
 SYNCTHING_LICENSE_FILES = LICENSE
