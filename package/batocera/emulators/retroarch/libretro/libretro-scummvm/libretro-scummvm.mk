e09a3fad1d617919671ad1420d5cf4b57fa568d9
dmanlfc <dmanlfc@gmail.com>
2023-01-06 16:40:31 +1000

    fix scummvm build

--- package/batocera/emulators/retroarch/libretro/libretro-scummvm/libretro-scummvm.mk
+++ package/batocera/emulators/retroarch/libretro/libretro-scummvm/libretro-scummvm.mk
@@ -3,9 +3,11 @@
 # libretro-scummvm
 #
 ################################################################################
-# Version: Commits on Apr 7, 2022
-LIBRETRO_SCUMMVM_VERSION = 2fb2e4c551c9c1510c56f6e890ee0300b7b3fca3
-LIBRETRO_SCUMMVM_SITE = $(call github,libretro,scummvm,$(LIBRETRO_SCUMMVM_VERSION))
+# Version: Commits on Jan 6, 2023
+LIBRETRO_SCUMMVM_VERSION = 5ce8142a0339ff7eda78f515b6d12d72a1732a31
+LIBRETRO_SCUMMVM_SITE = https://github.com/libretro/scummvm
+LIBRETRO_SCUMMVM_SITE_METHOD=git
+LIBRETRO_SCUMMVM_GIT_SUBMODULES=YES
 LIBRETRO_SCUMMVM_LICENSE = GPLv2
 
 LIBRETRO_SCUMMVM_PLATFORM = $(LIBRETRO_PLATFORM)
@@ -34,11 +36,11 @@ LIBRETRO_SCUMMVM_MAKE_OPTS += TARGET_64BIT=1
 endif
 
 define LIBRETRO_SCUMMVM_BUILD_CMDS
-	$(TARGET_CONFIGURE_OPTS) $(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" -C $(@D)/backends/platform/libretro/build platform="$(LIBRETRO_SCUMMVM_PLATFORM)" $(LIBRETRO_SCUMMVM_MAKE_OPTS)
+	$(TARGET_CONFIGURE_OPTS) $(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" -C $(@D)/ platform="$(LIBRETRO_SCUMMVM_PLATFORM)" $(LIBRETRO_SCUMMVM_MAKE_OPTS)
 endef
 
 define LIBRETRO_SCUMMVM_INSTALL_TARGET_CMDS
-	$(INSTALL) -D $(@D)/backends/platform/libretro/build/scummvm_libretro.so \
+	$(INSTALL) -D $(@D)/scummvm_libretro.so \
 		$(TARGET_DIR)/usr/lib/libretro/scummvm_libretro.so
 endef
 
