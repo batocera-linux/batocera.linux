431110c2b8b5b181f54c8d8839f83300bfd899a9
Demetris Ierokipides <ierokipides.dem@gmail.com>
2023-02-02 16:44:34 +0200

    S905: Odroid-C2 switch to mainline u-boot - Clean no-op packages

--- board/batocera/amlogic/s905/odroidc2/create-boot-script.sh
+++ board/batocera/amlogic/s905/odroidc2/create-boot-script.sh
@@ -6,7 +6,6 @@
 # BINARIES_DIR = images dir
 # TARGET_DIR = target dir
 # BATOCERA_BINARIES_DIR = batocera binaries sub directory
-# BATOCERA_TARGET_DIR = batocera target sub directory
 
 HOST_DIR=$1
 BOARD_DIR=$2
@@ -15,14 +14,17 @@ BINARIES_DIR=$4
 TARGET_DIR=$5
 BATOCERA_BINARIES_DIR=$6
 
+mkdir -p "${BATOCERA_BINARIES_DIR}/build-uboot-odroidc2"     || exit 1
+cp "${BOARD_DIR}/build-uboot.sh"          "${BATOCERA_BINARIES_DIR}/build-uboot-odroidc2/" || exit 1
+cd "${BATOCERA_BINARIES_DIR}/build-uboot-odroidc2/" && ./build-uboot.sh "${HOST_DIR}" "${BOARD_DIR}" "${BINARIES_DIR}" || exit 1
+
 mkdir -p "${BATOCERA_BINARIES_DIR}/boot/boot"     || exit 1
 mkdir -p "${BATOCERA_BINARIES_DIR}/boot/extlinux" || exit 1
 
-"${HOST_DIR}/bin/mkimage" -A arm64 -O linux -T kernel -C none -a 0x1080000 -e 0x1080000 -n linux -d "${BINARIES_DIR}/Image" "${BATOCERA_BINARIES_DIR}/boot/boot/linux" || exit 1
-cp "${BINARIES_DIR}/initrd.lz4"       "${BATOCERA_BINARIES_DIR}/boot/boot/initrd.lz4"       || exit 1
+cp "${BINARIES_DIR}/Image"           "${BATOCERA_BINARIES_DIR}/boot/boot/linux"           || exit 1
+cp "${BINARIES_DIR}/initrd.lz4"      "${BATOCERA_BINARIES_DIR}/boot/boot/initrd.lz4"       || exit 1
 cp "${BINARIES_DIR}/rootfs.squashfs" "${BATOCERA_BINARIES_DIR}/boot/boot/batocera.update" || exit 1
 
-cp "${BOARD_DIR}/boot/boot-logo.bmp.gz"      "${BATOCERA_BINARIES_DIR}/boot/"          || exit 1
 cp "${BINARIES_DIR}/meson-gxbb-odroidc2.dtb" "${BATOCERA_BINARIES_DIR}/boot/boot/"     || exit 1
 cp "${BOARD_DIR}/boot/extlinux.conf"         "${BATOCERA_BINARIES_DIR}/boot/extlinux/" || exit 1
 
