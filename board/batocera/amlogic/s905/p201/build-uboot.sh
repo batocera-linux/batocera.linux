50b52589f136135d558959ec59ded48ca16041a7
Demetris Ierokipides <ierokipides.dem@gmail.com>
2023-02-05 02:32:32 +0200

    S905: Add Khadas Vim & P201 variant

---
+++ board/batocera/amlogic/s905/p201/build-uboot.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+HOST_DIR=$1
+BOARD_DIR=$2
+IMAGES_DIR=$3
+
+# Download U-Boot mainline
+wget "https://ftp.denx.de/pub/u-boot/u-boot-2023.01.tar.bz2"
+tar xf u-boot-2023.01.tar.bz2
+cd u-boot-2023.01
+
+# Apply patches
+PATCHES="${BR2_EXTERNAL_BATOCERA_PATH}/board/batocera/amlogic/s905/patches/uboot/*.patch"
+for patch in $PATCHES
+do
+echo "Applying patch: $patch"
+patch -p1 < $patch
+done
+
+# Make config
+make p201_defconfig
+
+# Build it
+ARCH=aarch64 CROSS_COMPILE="${HOST_DIR}/bin/aarch64-buildroot-linux-gnu-" make -j$(nproc)
+mkdir -p ../../uboot-p201
+
+# Clone LibreElec Amlogic FIP
+git clone --depth 1 https://github.com/LibreELEC/amlogic-boot-fip
+
+# Build and put to appropriate place
+cd amlogic-boot-fip && ./build-fip.sh p201 ../u-boot.bin ../../../uboot-p201/
