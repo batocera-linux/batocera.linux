c53ac94b57fa153abb648ded0e3c25e336c94648
Steve Hay <me@stevenhay.com>
2023-02-07 04:13:04 +0100

    s922x / add odroid-n2l
    This patch adds support for the odroid-n2l board. This board is
    a variant of the odroid-n2 (s922x) board.
    
    - A patch from u-boot/master is brought in to enable the bootloader
    - A patch from linux/linux-next is brought in to enable the kernel.
    - EMMC does not always initialize correctly, so a temporary workaround
      is put that will retry booting until EMMC initializes properly. In
      practice this could cause delayed boot and booting out of expected
      order.
    
    Signed-off-by: Steve Hay <me@stevenhay.com>

---
+++ board/batocera/amlogic/s922x/odroidn2l/build-uboot.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+HOST_DIR=$1
+BOARD_DIR=$2
+IMAGES_DIR=$3
+
+make_uboot () {
+    board=$1
+    build_target=$2
+
+    ## terrible hack / sometimes the EMMC does not initialize. try the boot sequence until it succeeds
+    echo 'CONFIG_BOOTCOMMAND="until false; do run distro_bootcmd; sleep 5; done"' >> configs/${build_target}_defconfig
+
+    # Make config
+    make mrproper || exit 1
+    make ${build_target}_defconfig || exit 1
+
+    # Build it
+    ARCH=aarch64 CROSS_COMPILE="${HOST_DIR}/bin/aarch64-buildroot-linux-gnu-" make -j$(nproc) || exit 1
+    mkdir -p "../../$board" || exit 1
+
+    # Build and put to appropriate place
+    pushd amlogic-boot-fip
+        ./build-fip.sh $build_target ../u-boot.bin "../../../$board/" || { popd; exit 1; }
+    popd
+}
+
+# Download U-Boot mainline
+uboot="u-boot-2023.01"
+wget "https://ftp.denx.de/pub/u-boot/${uboot}.tar.bz2"
+tar xf "${uboot}.tar.bz2" || exit 1
+pushd "${uboot}"
+    # Clone LibreElec Amlogic FIP
+    git clone --depth 1 https://github.com/LibreELEC/amlogic-boot-fip || exit 1
+
+    # Apply patches
+    for patch in ${BR2_EXTERNAL_BATOCERA_PATH}/board/batocera/amlogic/s922x/patches/uboot/*.patch ; do
+        echo "Applying patch: $patch"
+        patch -p1 < $patch || exit 1
+    done
+
+    make_uboot uboot-odroidn2l odroid-n2l
+popd
