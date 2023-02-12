f062755d1ca5ded4ff8b379c3e6aeb1fa4fc2a69
Lio <lionel.bringuier@gmail.com>
2023-02-11 18:19:35 -0800

    Bump BigPEmu - fix Jaguar CD

--- package/batocera/emulators/bigpemu/bigpemu.mk
+++ package/batocera/emulators/bigpemu/bigpemu.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-BIGPEMU_VERSION = v104
+BIGPEMU_VERSION = v1051
 BIGPEMU_SOURCE = BigPEmu_$(BIGPEMU_VERSION).zip
 BIGPEMU_SITE = https://www.richwhitehouse.com/jaguar/builds
 
