7e800c0c07e8f67a78c5021ff96ec810e68b518c
Juliano Dorigão <jdorigao@gmail.com>
2023-02-26 11:12:47 -0400

    bump: firmware-orangepi

--- package/batocera/firmwares/firmware-orangepi/firmware-orangepi.mk
+++ package/batocera/firmwares/firmware-orangepi/firmware-orangepi.mk
@@ -3,8 +3,8 @@
 # firmware-orangepi
 #
 ################################################################################
-# Version.: Commits on Jan 13, 2023
-FIRMWARE_ORANGEPI_VERSION = 75747c7034b1136b4674269e248b69bf1a5e4039
+# Version.: Commits on Feb 20, 2023
+FIRMWARE_ORANGEPI_VERSION = 79186949b2fbd01c52d55f085106b96dfd670ff6
 FIRMWARE_ORANGEPI_SITE = $(call github,orangepi-xunlong,firmware,$(FIRMWARE_ORANGEPI_VERSION))
 FIRMWARE_ORANGEPI_DEPENDENCIES = alllinuxfirmwares
 
