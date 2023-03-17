fcd4eaff60d0bf7eee084dd51683b45cd8aacbfc
Lbrpdx <lionel.bringuier@gmail.com>
2023-03-16 17:46:44 -0700

    Added Zelda Classic 2.10 to ARM

--- package/batocera/emulators/retroarch/libretro/libretro-zc210/libretro-zc210.mk
+++ package/batocera/emulators/retroarch/libretro/libretro-zc210/libretro-zc210.mk
@@ -3,8 +3,8 @@
 # libretro-zc210
 #
 ################################################################################
-# Version: Commits on Feb 28, 2022
-LIBRETRO_ZC210_VERSION = 4d0997606b9ea1eba0222b8d287b12171729395c
+# Version: Commits on April 25, 2022
+LIBRETRO_ZC210_VERSION = b6426da40b074245fda097fd345fa7c9cdbf152a
 LIBRETRO_ZC210_SITE = https://github.com/netux79/zc210-libretro.git
 LIBRETRO_ZC210_SITE_METHOD=git
 LIBRETRO_ZC210_GIT_SUBMODULES=YES
@@ -39,8 +39,10 @@ LIBRETRO_ZC210_PLATFORM = unix
 LIBRETRO_ZC210_EXTRA_ARGS +=
 endif
 
+ZC210_CONFIGURE_OPTS = platform=$(LIBRETRO_ZC210_PLATFORM)
+
 define LIBRETRO_ZC210_BUILD_CMDS
-	$(TARGET_CONFIGURE_OPTS) $(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" -C $(@D)/ -f Makefile platform="$(LIBRETRO_ZC210_PLATFORM)" $(LIBRETRO_ZC210_EXTRA_ARGS) \
+	$(ZC210_CONFIGURE_OPTS) $(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" -C $(@D)/ -f Makefile $(LIBRETRO_ZC210_EXTRA_ARGS) \
         GIT_VERSION="-$(shell echo $(LIBRETRO_ZC210_VERSION) | cut -c 1-7)"
 endef
 
