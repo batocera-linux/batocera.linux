98c3e8bd5b7441f99117de73f30d9b1e8abd6683
Lbrpdx <lionel.bringuier@gmail.com>
2023-03-18 11:19:02 -0700

    Abuse: bump to 0.9.1, add to ARM SBC, data in Content Downloader

--- package/batocera/core/batocera-configgen/configgen/configgen/generators/abuse/abuseGenerator.py
+++ package/batocera/core/batocera-configgen/configgen/configgen/generators/abuse/abuseGenerator.py
@@ -8,7 +8,7 @@ import controllersConfig
 class AbuseGenerator(Generator):
 
     def generate(self, system, rom, playersControllers, guns, gameResolution):
-        commandArray = ["abuse", "-datadir", "/usr/share/abuse"]
+        commandArray = ["abuse", "-datadir", "/userdata/roms/abuse/abuse_data"]
 
         return Command.Command(
             array=commandArray,
