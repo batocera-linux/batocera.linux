ecf2e44c41bfa4ba3d8511eda120017b21c69878
Juliano Dorigão <jdorigao@gmail.com>
2023-03-26 09:15:11 -0400

    libretro-arduous: bump to May 24, 2022 build

--- package/batocera/emulators/retroarch/libretro/libretro-arduous/libretro-arduous.mk
+++ package/batocera/emulators/retroarch/libretro/libretro-arduous/libretro-arduous.mk
@@ -3,8 +3,8 @@
 # libretro-arduous
 #
 ################################################################################
-
-LIBRETRO_ARDUOUS_VERSION = aed5050
+# Version.: Commits on May 24, 2022
+LIBRETRO_ARDUOUS_VERSION = aed50506962df6f965748e888b3fe7027ddb410d
 LIBRETRO_ARDUOUS_SITE = https://github.com/libretro/arduous.git
 LIBRETRO_ARDUOUS_SITE_METHOD=git
 LIBRETRO_ARDUOUS_GIT_SUBMODULES=YES
