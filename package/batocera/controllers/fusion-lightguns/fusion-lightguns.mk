727a2632983d6b0e49c2d2c74e5d21dfb7138971
Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>
2023-04-09 14:03:56 +0000

    adding fusion-lightguns
    Signed-off-by: Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>

---
+++ package/batocera/controllers/fusion-lightguns/fusion-lightguns.mk
@@ -0,0 +1,15 @@
+################################################################################
+#
+# fusion-lightguns
+#
+################################################################################
+FUSION_LIGHTGUNS_VERSION = 1
+FUSION_LIGHTGUNS_LICENSE = GPL
+FUSION_LIGHTGUNS_SOURCE=
+
+define FUSION_LIGHTGUNS_INSTALL_TARGET_CMDS
+	$(INSTALL) -m 0644 -D $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/controllers/fusion-lightguns/99-fusion-lightguns.rules $(TARGET_DIR)/etc/udev/rules.d/99-fusion-lightguns.rules
+	$(INSTALL) -m 0755 -D $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/controllers/fusion-lightguns/fusion-lightguns-add $(TARGET_DIR)/usr/bin/fusion-lightguns-add
+endef
+
+$(eval $(generic-package))
