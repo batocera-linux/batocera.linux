49d72d86c123171815e0b384191d30672f50d457
dmanlfc@gmail.com <dmanlfc@gmail.com>
2023-04-14 10:41:48 +1000

    fix vice sdl2 compilation

--- package/batocera/emulators/vice/vice.mk
+++ package/batocera/emulators/vice/vice.mk
@@ -10,12 +10,9 @@ VICE_SITE = https://sourceforge.net/projects/vice-emu/files/releases
 VICE_LICENSE = GPLv2
 VICE_DEPENDENCIES = ffmpeg sdl2 libpng giflib zlib lame alsa-lib jpeg host-xa host-dos2unix sdl2_image
 
-VICE_CONF_OPTS += --disable-option-checking --disable-pdf-docs
-
-# FFMPEG
-VICE_DEPENDENCIES += ffmpeg
+VICE_CONF_OPTS += --disable-option-checking
+VICE_CONF_OPTS += --disable-pdf-docs
 VICE_CONF_OPTS += --enable-external-ffmpeg
-
 VICE_CONF_OPTS += --enable-midi
 VICE_CONF_OPTS += --enable-lame
 VICE_CONF_OPTS += --with-alsa
@@ -25,12 +22,19 @@ VICE_CONF_OPTS += --with-png
 VICE_CONF_OPTS += --with-fastsid
 VICE_CONF_OPTS += --without-pulse
 VICE_CONF_OPTS += --enable-x64
-
-VICE_CONF_OPTS += --enable-arch=sdl
-VICE_CONF_OPTS += --enable-sdlui2
+VICE_CONF_OPTS += --enable-arch=yes
+VICE_CONF_OPTS += --enable-sdl2ui
+VICE_CONF_OPTS += --with-sdlsound 
 VICE_CONF_OPTS += --disable-debug-gtk3ui
-VICE_CONF_OPTS += --disable-native-gtk3ui
 
 VICE_CONF_ENV += LDFLAGS=-lSDL2
+
+define VICE_POST_PROCESS
+	mkdir -p $(TARGET_DIR)/usr/share/evmapy
+	cp -f $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/emulators/vice/c64.vice.keys \
+        $(TARGET_DIR)/usr/share/evmapy
+endef
+
+VICE_POST_INSTALL_TARGET_HOOKS += VICE_POST_PROCESS
 
 $(eval $(autotools-package))
