a86a505058535ed67319e295406fdadaa17106c3
dmanlfc@gmail.com <dmanlfc@gmail.com>
2023-04-15 12:08:15 +1000

    bump xpadneo

--- package/batocera/controllers/xpadneo/xpadneo.mk
+++ package/batocera/controllers/xpadneo/xpadneo.mk
@@ -3,8 +3,8 @@
 # xpadneo
 #
 ################################################################################
-
-XPADNEO_VERSION = v0.9.5
+# Version: Commits on Apr 15, 2023
+XPADNEO_VERSION = 5df12b21e24f9f8691a815d5f71a7d2a97f1247d
 XPADNEO_SITE = $(call github,atar-axis,xpadneo,$(XPADNEO_VERSION))
 XPADNEO_DEPENDENCIES = host-libcurl host-cabextract bluez5_utils
 XPADNEO_MODULE_SUBDIRS = hid-xpadneo/src
