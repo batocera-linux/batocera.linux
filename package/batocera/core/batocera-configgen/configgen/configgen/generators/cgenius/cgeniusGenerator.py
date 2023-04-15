1803b4f47323fc4c7214aa3edeb01a5e066fb355
dmanlfc@gmail.com <dmanlfc@gmail.com>
2023-04-15 15:07:01 +1000

    bump & resurrect commander genius

--- package/batocera/core/batocera-configgen/configgen/configgen/generators/cgenius/cgeniusGenerator.py
+++ package/batocera/core/batocera-configgen/configgen/configgen/generators/cgenius/cgeniusGenerator.py
@@ -1,20 +1,129 @@
 #!/usr/bin/env python
 
-import Command
+import os
+from configobj import ConfigObj
+import batoceraFiles
 from generators.Generator import Generator
 import controllersConfig
-import batoceraFiles
-import codecs
-import os
+import Command
+import shutil
 
 class CGeniusGenerator(Generator):
-
     def generate(self, system, rom, playersControllers, guns, gameResolution):
 
-        # basis
+        cgeniusCtrl = {
+            "a":              "Fire",
+            "b":              "Jump",
+            "pageup":         "Camlead",
+            "x":              "Status",
+            "y":              "Pogo",
+            "pagedown":       "Run",
+            "up":             "Up",
+            "down":           "Down",
+            "left":           "Left",
+            "right":          "Right"
+        }
+
+        # Define the directory and file name for the config file
+        config_dir = batoceraFiles.CONF + "/cgenius"
+        alt_config_dir = "/userdata/roms/cgenius"
+        config_file = "cgenius.cfg"
+        config_path = os.path.join(config_dir, config_file)
+
+        # Create the config directory if it doesn't exist
+        if not os.path.exists(config_dir):
+            os.makedirs(config_dir)
+       
+        if not os.path.exists(config_path):
+            config = ConfigObj()
+            config.filename = config_path
+        else:
+            config = ConfigObj(infile=config_path)
+
+        # Now setup the options we want...
+        if "FileHandling" not in config:
+            config["FileHandling"] = {}
+        config["FileHandling"]["EnableLogfile"] = "false"
+        config["FileHandling"]["SearchPath1"] = "/userdata/roms/cgenius"
+        config["FileHandling"]["SearchPath2"] = "/userdata/roms/cgenius/games"
+
+        if "Video" not in config:
+            config["Video"] = {}
+        # aspect
+        if system.isOptSet("cgenius_aspect"):
+            config["Video"]["aspect"] = system.config["cgenius_aspect"]
+        else:
+            config["Video"]["aspect"] = "4:3"
+        # we always want fullscreen
+        config["Video"]["fullscreen"] = "true"
+        # filter
+        if system.isOptSet("cgenius_filter"):
+            config["Video"]["filter"] = system.config["cgenius_filter"]
+        else:
+            config["Video"]["filter"] = "none"
+        # quality
+        if system.isOptSet("cgenius_quality"):
+            config["Video"]["OGLfilter"] = system.config["cgenius_quality"]
+        else:
+            config["Video"]["OGLfilter"] = "nearest"
+        # render resolution
+        if system.isOptSet("cgenius_render"):
+            if system.config["cgenius_render"] == "200":
+                config["Video"]["gameHeight"] = "200"
+                config["Video"]["gameWidth"] = "320"
+            if system.config["cgenius_render"] == "240":
+                config["Video"]["gameHeight"] = "240"
+                config["Video"]["gameWidth"] = "320"
+            if system.config["cgenius_render"] == "360":
+                config["Video"]["gameHeight"] = "360"
+                config["Video"]["gameWidth"] = "640"
+            if system.config["cgenius_render"] == "480":
+                config["Video"]["gameHeight"] = "480"
+                config["Video"]["gameWidth"] = "640"
+        else:
+            config["Video"]["gameHeight"] = "200"
+            config["Video"]["gameWidth"] = "320"
+        # mouse
+        if system.isOptSet("cgenius_cursor"):
+            config["Video"]["ShowCursor"] = system.config["cgenius_cursor"]
+        else:
+            config["Video"]["ShowCursor"] = "false"
+
+        # -= Controllers =-
+        # Configure the first four controllers
+        nplayer = 1
+        for playercontroller, pad in sorted(playersControllers.items()):
+            if nplayer <= 4:
+                input_num = "input" + str(pad.index)
+                if input_num not in config:
+                    config[input_num] = {}
+                for x in pad.inputs:
+                    input = pad.inputs[x]
+                    if input.name in cgeniusCtrl:
+                        if input.type == "hat":
+                            config[input_num][cgeniusCtrl[input.name]] = "Joy" + str(pad.index) + "-" + input.type[0].upper() + str(input.value)
+                        else:
+                            config[input_num][cgeniusCtrl[input.name]] = "Joy" + str(pad.index) + "-" + input.type[0].upper() + str(input.id)
+                nplayer += 1
+        
+        # Write the config file
+        config.write()
+        # need to copy to roms folder too
+        shutil.copy(config_path, alt_config_dir)
+
+        # now setup to run the rom
         commandArray = ["CGeniusExe"]
+        # get rom path
+        rom_path = os.path.dirname(rom)
+        rom_path = rom_path.replace("/userdata/roms/cgenius/", "")
+        dir_string = "dir=\"" + rom_path + "\""
+        commandArray.append(dir_string)
 
-        # rom
-        commandArray.append(rom)
+        return Command.Command(
+            array=commandArray,
+            env={"SDL_GAMECONTROLLERCONFIG": controllersConfig.generateSdlGameControllerConfig(playersControllers)}
+        )
 
-        return Command.Command(array=commandArray)
+    # Show mouse on screen for the Config Screen
+    def getMouseMode(self, config):
+        return True
--- package/batocera/emulators/cgenius/cgenius.mk
+++ package/batocera/emulators/cgenius/cgenius.mk
@@ -1,36 +1,39 @@
 ################################################################################
 #
-# Commander Genius
+# cgenius
 #
 ################################################################################
 
-CGENIUS_VERSION = v2.8.2
+CGENIUS_VERSION = v3.3.0
 CGENIUS_SITE = $(call github,gerstrong,Commander-Genius,$(CGENIUS_VERSION))
-
-CGENIUS_DEPENDENCIES = sdl2 sdl2_mixer sdl2_image sdl2_ttf boost libcurl
-
-# No OpenGL ES support
-ifeq ($(BR2_PACKAGE_BATOCERA_TARGET_X86_ANY),y)
-CGENIUS_CONF_OPTS += -DUSE_OPENGL=ON
-else	
-CGENIUS_CONF_OPTS += -DUSE_OPENGL=OFF
-endif
+CGENIUS_CONF_LICENSE = GPL-2.0
+CGENIUS_CONF_LICENSE_FILES = LICENSE
+CGENIUS_DEPENDENCIES += sdl2 sdl2_mixer sdl2_image sdl2_ttf
+CGENIUS_DEPENDENCIES += boost libcurl host-xxd python3-configobj
 
 CGENIUS_CONF_OPTS += -DCMAKE_BUILD_TYPE=Release
+CGENIUS_CONF_OPTS += -DBUILD_SHARED_LIBS=OFF
+# No OpenGL ES support
+#ifeq ($(BR2_PACKAGE_BATOCERA_TARGET_X86_ANY),y)
+#CGENIUS_CONF_OPTS += -DUSE_OPENGL=ON
+#else	
+#CGENIUS_CONF_OPTS += -DUSE_OPENGL=OFF
+#endif
+# compile the cosmos engine too
+CGENIUS_CONF_OPTS += -DBUILD_COSMOS=1
 
-define CGENIUS_INSTALL_TARGET_SHARED_LIBRARIES
-	mkdir -p $(TARGET_DIR)/usr/lib
-	cp $(@D)/GsKit/base/lua/libGsKit_base_lua.so $(TARGET_DIR)/usr/lib
-	cp $(@D)/GsKit/base/utils/property_tree/libGsKit_base_utils_ptree.so $(TARGET_DIR)/usr/lib
-	cp $(@D)/GsKit/base/utils/libGsKit_base_utils.so $(TARGET_DIR)/usr/lib
-	cp $(@D)/GsKit/base/video/libGsKit_base_video.so $(TARGET_DIR)/usr/lib
-	cp $(@D)/GsKit/base/libGsKit_base.so $(TARGET_DIR)/usr/lib
-	cp $(@D)/GsKit/fileio/libGsKit_fileio.so $(TARGET_DIR)/usr/lib
-	cp $(@D)/GsKit/graphics/libGsKit_graphics.so $(TARGET_DIR)/usr/lib
-	cp $(@D)/GsKit/widgets/libGsKit_widgets.so $(TARGET_DIR)/usr/lib
-	cp $(@D)/GsKit/libGsKit.so $(TARGET_DIR)/usr/lib
+define CGENIUS_GET_COSMOS
+    cd $(@D); \
+	git clone https://gitlab.com/Dringgstein/cosmos.git src/engine/cosmos
 endef
 
-CGENIUS_POST_INSTALL_TARGET_HOOKS += CGENIUS_INSTALL_TARGET_SHARED_LIBRARIES
+define CGENIUS_POST_PROCESS
+	mkdir -p $(TARGET_DIR)/usr/share/evmapy
+	cp -f $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/emulators/cgenius/cgenius.cgenius.keys \
+        $(TARGET_DIR)/usr/share/evmapy
+endef
+
+CGENIUS_PRE_CONFIGURE_HOOKS += CGENIUS_GET_COSMOS
+CGENIUS_POST_INSTALL_TARGET_HOOKS += CGENIUS_POST_PROCESS
 
 $(eval $(cmake-package))
