6e963a1466dbeef9a8e1c7a5c37ca4981bfd4905
Juliano Dorigão <jdorigao@gmail.com>
2023-04-28 10:08:54 -0400

    bump: lr-dolphin to Dec 17, 2022 build

--- package/batocera/emulators/retroarch/libretro/libretro-dolphin/libretro-dolphin.mk
+++ package/batocera/emulators/retroarch/libretro/libretro-dolphin/libretro-dolphin.mk
@@ -3,31 +3,30 @@
 # libretro-dolphin
 #
 ################################################################################
-# Version: Commits on Jun 23, 2022
-LIBRETRO_DOLPHIN_VERSION = 6ebaa94082f516cd19f31432ff4779ff3685076c
-LIBRETRO_DOLPHIN_SITE = https://github.com/libretro/dolphin
-LIBRETRO_DOLPHIN_SITE_METHOD=git
-LIBRETRO_DOLPHIN_GIT_SUBMODULES=YES
+# Version: Commits on Dec 17, 2022
+LIBRETRO_DOLPHIN_VERSION = 2f4b0f7902257d40a054f60b2c670d6e314f2a04
+LIBRETRO_DOLPHIN_SITE = $(call github,libretro,dolphin,$(LIBRETRO_DOLPHIN_VERSION))
 LIBRETRO_DOLPHIN_LICENSE = GPLv2
 LIBRETRO_DOLPHIN_DEPENDENCIES = libevdev fmt bluez5_utils
 
 LIBRETRO_DOLPHIN_PLATFORM = $(LIBRETRO_PLATFORM)
 
-LIBRETRO_DOLPHIN_CONF_OPTS = -DUSE_DISCORD_PRESENCE=OFF -DLIBRETRO=ON \
-    -DBUILD_SHARED_LIBS=OFF -DCMAKE_BUILD_TYPE=Release
+LIBRETRO_DOLPHIN_CONF_OPTS = -DLIBRETRO=ON \
+                             -DENABLE_NOGUI=OFF \
+                             -DENABLE_QT=OFF \
+                             -DENABLE_TESTS=OFF \
+                             -DUSE_DISCORD_PRESENCE=OFF \
+                             -DBUILD_SHARED_LIBS=OFF \
+                             -DCMAKE_BUILD_TYPE=Release
 
 ifeq ($(BR2_PACKAGE_XSERVER_XORG_SERVER),y)
 LIBRETRO_DOLPHIN_DEPENDENCIES += xserver_xorg-server
 LIBRETRO_DOLPHIN_CONF_OPTS += -DENABLE_X11=ON
-endif
-
-ifeq ($(BR2_PACKAGE_BATOCERA_TARGET_S922X),y)
-LIBRETRO_DOLPHIN_CONF_OPTS += -DENABLE_EGL=OFF
+else
 LIBRETRO_DOLPHIN_CONF_OPTS += -DENABLE_X11=OFF
 endif
 
 ifeq ($(BR2_PACKAGE_BATOCERA_TARGET_RK3588),y)
-LIBRETRO_DOLPHIN_CONF_OPTS += -DENABLE_X11=OFF
 LIBRETRO_DOLPHIN_CONF_OPTS += -DCMAKE_SHARED_LINKER_FLAGS="-lmali_hook -Wl,--whole-archive -lmali_hook_injector -Wl,--no-whole-archive -lmali"
 LIBRETRO_DOLPHIN_CONF_OPTS += -DCMAKE_EXE_LINKER_FLAGS="-lmali_hook -Wl,--whole-archive -lmali_hook_injector -Wl,--no-whole-archive -lmali"
 endif
