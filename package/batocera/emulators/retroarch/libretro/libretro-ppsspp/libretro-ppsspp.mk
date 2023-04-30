8d603fd6e65d6944daa71439620c14cc2963042b
Juliano Dorigão <jdorigao@gmail.com>
2023-04-30 16:44:59 -0400

    bump: libretro-ppsspp to v1.15

--- package/batocera/emulators/retroarch/libretro/libretro-ppsspp/libretro-ppsspp.mk
+++ package/batocera/emulators/retroarch/libretro/libretro-ppsspp/libretro-ppsspp.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBRETRO_PPSSPP_VERSION = v1.14.4
+LIBRETRO_PPSSPP_VERSION = v1.15
 LIBRETRO_PPSSPP_SITE = https://github.com/hrydgard/ppsspp.git
 LIBRETRO_PPSSPP_SITE_METHOD=git
 LIBRETRO_PPSSPP_GIT_SUBMODULES=YES
