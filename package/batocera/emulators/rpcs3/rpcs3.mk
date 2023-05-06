958608a0f935ec341a019b37230ad0711311c0f7
dmanlfc@gmail.com <dmanlfc@gmail.com>
2023-05-06 18:14:12 +1000

    bump rpcs3

--- package/batocera/emulators/rpcs3/rpcs3.mk
+++ package/batocera/emulators/rpcs3/rpcs3.mk
@@ -3,28 +3,44 @@
 # rpcs3
 #
 ################################################################################
-
-RPCS3_VERSION = v0.0.27
+# Version: Commits on May 6, 2023
+RPCS3_VERSION = f1150320950eb015ab2c610a13fdd291eb857b0a
 RPCS3_SITE = https://github.com/RPCS3/rpcs3.git
 RPCS3_SITE_METHOD=git
 RPCS3_GIT_SUBMODULES=YES
 RPCS3_LICENSE = GPLv2
-RPCS3_DEPENDENCIES += qt5declarative libxml2 mesa3d libglu openal alsa-lib 
-RPCS3_DEPENDENCIES += libevdev libglew libusb ffmpeg faudio wolfssl
+RPCS3_DEPENDENCIES += alsa-lib batocera-llvm faudio ffmpeg libevdev
+RPCS3_DEPENDENCIES += libglew libglu libpng libusb mesa3d ncurses openal 
+RPCS3_DEPENDENCIES += qt5base qt5declarative qt5multimedia qt5svg
+RPCS3_DEPENDENCIES += wolfssl libxml2
 
 RPCS3_SUPPORTS_IN_SOURCE_BUILD = NO
 
-RPCS3_CONF_OPTS = -DCMAKE_BUILD_TYPE=Release \
-    -DCMAKE_CROSSCOMPILING=ON -DBUILD_SHARED_LIBS=OFF \
-    -DUSE_SYSTEM_FFMPEG=ON -DUSE_NATIVE_INSTRUCTIONS=OFF \
-	-DBUILD_LLVM_SUBMODULE=OFF -DLLVM_DIR=$(@D)/llvmlibs/lib/cmake/llvm/ \
-	-DUSE_PRECOMPILED_HEADERS=OFF -DUSE_SYSTEM_CURL=ON
+RPCS3_CONF_OPTS += -DCMAKE_BUILD_TYPE=Release
+RPCS3_CONF_OPTS += -DCMAKE_INSTALL_PREFIX=/usr
+RPCS3_CONF_OPTS += -DCMAKE_CROSSCOMPILING=ON
+RPCS3_CONF_OPTS += -DBUILD_SHARED_LIBS=OFF
+RPCS3_CONF_OPTS += -DUSE_NATIVE_INSTRUCTIONS=ON #compile with -march=native
+RPCS3_CONF_OPTS += -DBUILD_LLVM_SUBMODULE=OFF
+RPCS3_CONF_OPTS += -DLLVM_DIR=$(STAGING_DIR)/usr/lib/cmake/llvm/
+RPCS3_CONF_OPTS += -DUSE_PRECOMPILED_HEADERS=OFF
+RPCS3_CONF_OPTS += -DSTATIC_LINK_LLVM=OFF
+RPCS3_CONF_OPTS += -DUSE_SYSTEM_FFMPEG=ON
+RPCS3_CONF_OPTS += -DUSE_SYSTEM_CURL=ON
 
-define RPCS3_LLVM_LIBS
-	mkdir -p $(@D)/llvmlibs
-    $(HOST_DIR)/bin/curl -L https://github.com/RPCS3/llvm-mirror/releases/download/custom-build/llvmlibs-linux.tar.gz -o $(@D)/llvmlibs-linux.tar.gz
-	cd $(@D) && tar -xf llvmlibs-linux.tar.gz -C $(@D)/llvmlibs
-endef
+RPCS3_CONF_ENV = LIBS="-ncurses -ltinfo"
+
+ifeq ($(BR2_PACKAGE_VULKAN_HEADERS)$(BR2_PACKAGE_VULKAN_LOADER),yy)
+    RPCS3_CONF_OPTS += -DUSE_VULKAN=ON
+else
+    RPCS3_CONF_OPTS += -DUSE_VULKAN=OFF
+endif
+
+ifeq ($(BR2_PACKAGE_SDL2),y)
+    RPCS3_CONF_OPTS += -DUSE_SDL=ON
+else
+    RPCS3_CONF_OPTS += -DUSE_SDL=ON
+endif
 
 define RPCS3_BUILD_CMDS
 	$(TARGET_CONFIGURE_OPTS) \
@@ -33,11 +49,11 @@ endef
 
 define RPCS3_INSTALL_EVMAPY
 	mkdir -p $(TARGET_DIR)/usr/share/evmapy
-	$(INSTALL) -D -m 0644 $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/emulators/rpcs3/evmapy.keys \
+	$(INSTALL) -D -m 0644 \
+	    $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/emulators/rpcs3/evmapy.keys \
 	    $(TARGET_DIR)/usr/share/evmapy/ps3.keys
 endef
 
-RPCS3_PRE_CONFIGURE_HOOKS = RPCS3_LLVM_LIBS
 RPCS3_POST_INSTALL_TARGET_HOOKS = RPCS3_INSTALL_EVMAPY
 
 $(eval $(cmake-package))
