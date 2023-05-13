97a91e5532c13143b135739b284326a8901bbb3d
dmanlfc@gmail.com <dmanlfc@gmail.com>
2023-05-14 09:06:15 +1000

    fix the rk3588 build with xpadneo

--- package/batocera/controllers/xpadneo/xpadneo.mk
+++ package/batocera/controllers/xpadneo/xpadneo.mk
@@ -9,6 +9,11 @@ XPADNEO_SITE = $(call github,atar-axis,xpadneo,$(XPADNEO_VERSION))
 XPADNEO_DEPENDENCIES = host-libcurl host-cabextract bluez5_utils
 XPADNEO_MODULE_SUBDIRS = hid-xpadneo/src
 
+XPADNEO_USER_EXTRA_CFLAGS = -w -Wno-error=unused-function
+
+XPADNEO_MODULE_MAKE_OPTS = \
+	KCFLAGS="$$KCFLAGS $(XPADNEO_USER_EXTRA_CFLAGS)"
+
 define XPADNEO_INSTALL_TARGET_CMDS
 	cp -v $(@D)/hid-xpadneo/etc-udev-rules.d/*.rules $(TARGET_DIR)/etc/udev/rules.d/
 	cp -v $(@D)/hid-xpadneo/etc-modprobe.d/*.conf $(TARGET_DIR)/etc/modprobe.d/
