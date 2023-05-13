8fefd6e8c2d07ccb66c16095205163a31a8dea98
dmanlfc@gmail.com <dmanlfc@gmail.com>
2023-05-13 17:27:03 +1000

    bump Xenia's

--- package/batocera/emulators/xenia/xenia.mk
+++ package/batocera/emulators/xenia/xenia.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 XENIA_SOURCE = xenia_master.zip
-XENIA_VERSION = v1.0.2791-master
+XENIA_VERSION = v1.0.2798-master
 XENIA_SITE = https://github.com/xenia-project/release-builds-windows/releases/download/$(XENIA_VERSION)
 XENIA_LICENSE = BSD
 XENIA_LICENSE_FILE = LICENSE
