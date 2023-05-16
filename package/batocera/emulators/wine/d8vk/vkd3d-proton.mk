e0ca8c150d4b13426ca11a423dd12a5625779a40
dmanlfc@gmail.com <dmanlfc@gmail.com>
2023-05-16 14:08:45 +1000

    bring in direct3d 8 to vulkan support for windows peeps

---
+++ package/batocera/emulators/wine/d8vk/vkd3d-proton.mk
@@ -0,0 +1,22 @@
+################################################################################
+#
+# d8vk
+#
+################################################################################
+
+D8VK_VERSION = 1.0
+D8VK_SOURCE = d8vk-v$(D8VK_VERSION).tar.gz
+D8VK_SITE = https://github.com/AlpyneDreams/d8vk/releases/download/d8vk-v$(D8VK_VERSION)
+D8VK_LICENSE = zlib
+D8VK_LICENSE_FILES = LICENSE
+
+D8VK_DEPENDENCIES = dxvk
+
+define D8VK_INSTALL_TARGET_CMDS
+    mkdir -p $(TARGET_DIR)/usr/wine/dxvk/x32
+    mkdir -p $(TARGET_DIR)/usr/wine/dxvk/x64
+    cp -a $(@D)/d3d8.dll $(TARGET_DIR)/usr/wine/dxvk/x32
+    cp -a $(@D)/d3d8.dll $(TARGET_DIR)/usr/wine/dxvk/x64
+endef
+
+$(eval $(generic-package))
