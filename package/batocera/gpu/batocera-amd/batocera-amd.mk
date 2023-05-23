3ddbb99b64ac27516f5fabd17d287dc01fd917bf
dmanlfc@gmail.com <dmanlfc@gmail.com>
2023-05-23 16:59:37 +1000

    modify amd detection & additions

--- package/batocera/gpu/batocera-amd/batocera-amd.mk
+++ package/batocera/gpu/batocera-amd/batocera-amd.mk
@@ -14,8 +14,9 @@ define BATOCERA_AMD_INSTALL_TARGET_CMDS
 	mkdir -p $(TARGET_DIR)/usr/share/amd
 	install -m 0644 $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/gpu/batocera-amd/islands.list \
 	    $(TARGET_DIR)/usr/share/amd
-	# Module
+	# Modules
 	ln -sf /var/run/amd/modprobe/amdgpu.conf $(TARGET_DIR)/etc/modprobe.d/amdgpu.conf
+	ln -sf /var/run/amd/modprobe/radeon.conf $(TARGET_DIR)/etc/modprobe.d/radeon.conf
 endef
 
 $(eval $(generic-package))
