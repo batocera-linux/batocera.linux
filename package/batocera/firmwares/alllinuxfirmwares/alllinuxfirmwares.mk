5c0634921b69c19d84e622db42d408477f22205b
Romain TISSERAND <romain.tisserand@gmail.com>
2023-05-27 01:46:08 +0200

    Inital H3 work: - Merge CHA, OPI-PC, add other H3 OrangePi boards and BananaPi M2 Zero - Adjust CONFIG_SND_SEQUENCER in kernel config - Adjust uboot-multiboard for single H3 build - Remove Kodi on H3 platforms (to fit with CHA constraints) - Filter more useless firmware in alllinuxfirmwares.mk

--- package/batocera/firmwares/alllinuxfirmwares/alllinuxfirmwares.mk
+++ package/batocera/firmwares/alllinuxfirmwares/alllinuxfirmwares.mk
@@ -9,10 +9,10 @@ ALLLINUXFIRMWARES_SOURCE = linux-firmware-$(ALLLINUXFIRMWARES_VERSION).tar.gz
 ALLLINUXFIRMWARES_SITE = https://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git/snapshot
 
 # exclude some dirs not required on batocera
-ALLLINUXFIRMWARES_REMOVE_DIRS = $(@D)/liquidio $(@D)/netronome
+ALLLINUXFIRMWARES_REMOVE_DIRS = $(@D)/liquidio $(@D)/netronome $(@D)/mellanox $(@D)/dpaa2
 
 ifeq ($(BR2_arm)$(BR2_aarch64),y)
-    ALLLINUXFIRMWARES_REMOVE_DIRS += $(@D)/amd $(@D)/amdgpu $(@D)/i915 $(@D)/nvidia $(@D)/radeon $(@D)/s5p-* $(@D)/qat_* $(@D)/ql2*
+    ALLLINUXFIRMWARES_REMOVE_DIRS += $(@D)/amd $(@D)/amdgpu $(@D)/intel $(@D)/i915 $(@D)/nvidia $(@D)/radeon $(@D)/s5p-* $(@D)/qat_* $(@D)/ql2*
 endif
 
 ifeq ($(BR2_PACKAGE_BRCMFMAC_SDIO_FIRMWARE_RPI)$(BR2_PACKAGE_EXTRALINUXFIRMWARES),y)
