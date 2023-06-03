5cec84527edc00239321695778970a165c3b42f2
Romain Tisserand <romain.tisserand@gmail.com>
2023-06-03 21:33:38 +0200

    Remove patch merged upstream, update lr-yabasanshiro package

--- package/batocera/emulators/retroarch/libretro/libretro-yabasanshiro/libretro-yabasanshiro.mk
+++ package/batocera/emulators/retroarch/libretro/libretro-yabasanshiro/libretro-yabasanshiro.mk
@@ -3,9 +3,9 @@
 # libretro-yabasanshiro
 #
 ################################################################################
-# Version: Commits on Sep 28, 2022
+# Version: Commits on Jun 3, 2023
 # Need to use this branch : https://github.com/libretro/yabause/tree/yabasanshiro
-LIBRETRO_YABASANSHIRO_VERSION = fd459968aae4a251d839174404a346b1f428912a
+LIBRETRO_YABASANSHIRO_VERSION = 7513c8459034a0fe415d3cae4d692a54d0f90296
 LIBRETRO_YABASANSHIRO_SITE = $(call github,libretro,yabause,$(LIBRETRO_YABASANSHIRO_VERSION))
 LIBRETRO_YABASANSHIRO_LICENSE = GPLv2
 
