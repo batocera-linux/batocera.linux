fd12a5fbf44ac3af26c47d39a31c3cfe01bd7b43
dmanlfc@gmail.com <dmanlfc@gmail.com>
2023-06-04 09:47:57 +1000

    make cache folder

--- package/batocera/core/batocera-configgen/configgen/configgen/generators/pcsx2/pcsx2Generator.py
+++ package/batocera/core/batocera-configgen/configgen/configgen/generators/pcsx2/pcsx2Generator.py
@@ -156,6 +156,10 @@ def configureINI(config_directory, bios_directory, system, controllers, guns):
     pcsx2INIConfig.set("Folders", "InputProfiles", "inputprofiles")
     pcsx2INIConfig.set("Folders", "Videos", "../../../saves/ps2/pcsx2/videos")
 
+    # create cache folder
+    if not os.path.exists("/userdata/cache/ps2"):
+        os.makedirs("/userdata/cache/ps2")
+    
     ## [EmuCore]
     if not pcsx2INIConfig.has_section("EmuCore"):
         pcsx2INIConfig.add_section("EmuCore")
