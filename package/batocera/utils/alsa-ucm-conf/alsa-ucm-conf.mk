f876528c9b71d7b15cd80b75d22f41f748f93c9c
Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>
2023-06-06 20:20:18 +0000

    fix audio on steam deck after standby
    Signed-off-by: Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>

--- package/batocera/utils/alsa-ucm-conf/alsa-ucm-conf.mk
+++ package/batocera/utils/alsa-ucm-conf/alsa-ucm-conf.mk
@@ -10,7 +10,9 @@ ALSA_UCM_CONF_LICENSE = BSD-3-Clause
 ALSA_UCM_CONF_LICENSE_FILES = LICENSE
 
 define ALSA_UCM_CONF_INSTALL_TARGET_CMDS
-	rsync -arv $(@D)/ucm* $(TARGET_DIR)/usr/share/alsa/
+	rsync -arv $(@D)/ucm* $(TARGET_DIR)/usr/share/alsa/ --exclude acp5x
+	# exclude the acp5x specific to the steam deck. it causes audio Source to DSP after standby.
+	# we have our own file in batocera-audio packages - todo : analyze to get the best of the two files
 endef
 
 $(eval $(generic-package))
