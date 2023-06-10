f2109faab03971cde69341be22ec90997f78a922
Lbrpdx <lionel.bringuier@gmail.com>
2023-06-10 07:29:52 -0700

    Update with more team members coordinates

--- team.js
+++ team.js
@@ -2,7 +2,7 @@
     "susan34": {
         "latitude": 44.8448769,
         "longitude": -0.656358,
-	"town": "Mérignac, France",
+	"town": "Mérignac",
 	"last_contribution" : 2023
     },
     "lbrpdx": {
@@ -34,5 +34,53 @@
         "longitude": -51.150283,
 	"town": "São Leopoldo",
 	"last_contribution" : 2021
+    },
+    "dmanlfc": {
+        "latitude": -27.470125,
+        "longitude": 153.021072,
+	"town": "Brisbane",
+	"last_contribution" : 2023
+    },
+    "demetris": {
+        "latitude": 34.707130,
+        "longitude": 33.022617,
+	"town": "Cyprus",
+	"last_contribution" : 2023
+    },
+    "tvsian": {
+        "latitude": 39.103119,
+        "longitude": -84.512016,
+	"town": "Cincinnati",
+	"last_contribution" : 2023
+    },
+    "niroku": {
+        "latitude": -37.840935,
+        "longitude": 144.946457,
+	"town": "Melbourne",
+	"last_contribution" : 2023
+    },
+    "stvhay": {
+        "latitude": 38.889805,
+        "longitude": -77.009056,
+	"town": "Washington",
+	"last_contribution" : 2023
+    },
+    "rtissera": {
+        "latitude": 48.692055,
+        "longitude": 6.184417,
+	"town": "Nancy",
+	"last_contribution" : 2023
+    },
+    "modhack": {
+        "latitude": 48.864716,
+        "longitude": 2.349014,
+	"town": "Paris",
+	"last_contribution" : 2023
+    },
+    "tovarichtch": {
+        "latitude": 45.508888,
+        "longitude": -73.561668,
+	"town": "Montreal",
+	"last_contribution" : 2023
     }
 }
