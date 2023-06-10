c8d14ffbf787f60617eec593fef740579a1cebe4
Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>
2023-06-10 15:34:05 +0200

    team people
    Signed-off-by: Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>

--- team.js
+++ team.js
@@ -1,38 +1,38 @@
 {
     "susan34": {
-        latitude: 44.8448769,
-        longitude: -0.656358,
-	town: "Mérignac, France",
-	last_contribution : 2023
+        "latitude": 44.8448769,
+        "longitude": -0.656358,
+	"town": "Mérignac, France",
+	"last_contribution" : 2023
     },
     "lbrpdx": {
-        latitude: 45.5230622,
-        longitude: -122.6764816,
-	town: "Portland",
-	last_contribution : 2023
+        "latitude": 45.5230622,
+        "longitude": -122.6764816,
+	"town": "Portland",
+	"last_contribution" : 2023
     },
     "juliano": {
-        latitude: -21.61389,
-        longitude: -55.16778,
-	town: "Maracaju",
-	last_contribution : 2023
+        "latitude": -21.61389,
+        "longitude": -55.16778,
+	"town": "Maracaju",
+	"last_contribution" : 2023
     },
     "f.caruso": {
-        latitude: 47.950001,
-        longitude: 1.88333 ,
-	town: "Saran",
-	last_contribution : 2023
+        "latitude": 47.950001,
+        "longitude": 1.88333 ,
+	"town": "Saran",
+	"last_contribution" : 2023
     },
     "joinski": {
-        latitude: 53.551085,
-        longitude: 9.993682,
-	town: "Hamburg",
-	last_contribution : 2023
+        "latitude": 53.551085,
+        "longitude": 9.993682,
+	"town": "Hamburg",
+	"last_contribution" : 2023
     },
     "poke": {
-        latitude: -29.754994,
-        longitude: -51.150283,
-	town: "São Leopoldo",
-	last_contribution : 2021
+        "latitude": -29.754994,
+        "longitude": -51.150283,
+	"town": "São Leopoldo",
+	"last_contribution" : 2021
     }
 }
