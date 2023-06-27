c53a27b0eeb62c382bcdbf1cada28d94ceed8ba8
Romain Tisserand <romain.tisserand@gmail.com>
2023-06-28 01:20:11 +0200

    Preliminary Odroid Go Ultra support

--- board/batocera/amlogic/s922x/ogu/create-boot-script.sh
+++ board/batocera/amlogic/s922x/ogu/create-boot-script.sh
@@ -25,7 +25,7 @@ cp "${BINARIES_DIR}/Image"           "${BATOCERA_BINARIES_DIR}/boot/boot/linux" 
 cp "${BINARIES_DIR}/initrd.lz4"       "${BATOCERA_BINARIES_DIR}/boot/boot/initrd.lz4"       || exit 1
 cp "${BINARIES_DIR}/rootfs.squashfs" "${BATOCERA_BINARIES_DIR}/boot/boot/batocera.update" || exit 1
 
-cp "${BINARIES_DIR}/meson-g12b-odroid-ogu.dtb" "${BATOCERA_BINARIES_DIR}/boot/boot/"     || exit 1
+cp "${BINARIES_DIR}/meson-g12b-odroid-go-ultra.dtb" "${BATOCERA_BINARIES_DIR}/boot/boot/"     || exit 1
 cp "${BOARD_DIR}/boot/extlinux.conf"                   "${BATOCERA_BINARIES_DIR}/boot/extlinux/" || exit 1
 
 exit 0
