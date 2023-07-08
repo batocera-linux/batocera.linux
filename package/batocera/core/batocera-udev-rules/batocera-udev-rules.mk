bca958bdd8a3b2f1f1f631f22e3cf0315493366f
Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>
2023-07-08 06:21:09 +0000

    make a package with specfic batocera udev rules
    Signed-off-by: Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>

---
+++ package/batocera/core/batocera-udev-rules/batocera-udev-rules.mk
@@ -0,0 +1,16 @@
+################################################################################
+#
+# batocera udev extra rules
+#
+################################################################################
+
+BATOCERA_UDEV_RULES_VERSION = 1.0
+BATOCERA_UDEV_RULES_LICENSE = GPL
+BATOCERA_UDEV_RULES_SOURCE=
+
+define BATOCERA_UDEV_RULES_INSTALL_TARGET_CMDS
+	mkdir -p $(TARGET_DIR)/etc/udev/rules.d
+	$(INSTALL) -m 0644 -D $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/core/batocera-udev-rules/rules/*.rules $(TARGET_DIR)/etc/udev/rules.d/
+endef
+
+$(eval $(generic-package))
