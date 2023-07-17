aa6c1419aea59b97a71b76fac48ef5ba4a3b65bc
Romain Tisserand <romain.tisserand@gmail.com>
2023-07-17 23:19:33 +0200

    Enable ioquake3 on many ARM/AArch64 boards through gl4es shim

--- package/batocera/ports/ioquake3/ioquake3.mk
+++ package/batocera/ports/ioquake3/ioquake3.mk
@@ -18,11 +18,18 @@ IOQUAKE3_BUILD_ARGS += BUILD_MISSIONPACK=1
 IOQUAKE3_BUILD_ARGS += BUILD_GAME_SO=0
 IOQUAKE3_BUILD_ARGS += BUILD_GAME_QVM=0
 IOQUAKE3_BUILD_ARGS += CROSS_COMPILING=1
-IOQUAKE3_BUILD_ARGS += USE_RENDERER_DLOPEN=0
+IOQUAKE3_BUILD_ARGS += USE_RENDERER_DLOPEN=1
+
+ifeq ($(BR2_PACKAGE_GL4ES),y)
+    IOQUAKE3_DEPENDENCIES += gl4es
+endif
 
 ifeq ($(BR2_aarch64),y)
     IOQUAKE3_BUILD_ARGS += COMPILE_ARCH=arm64
     IOQUAKE3_ARCH = arm64
+else ifeq ($(BR2_arm),y)
+    IOQUAKE3_BUILD_ARGS += COMPILE_ARCH=armv7l
+    IOQUAKE3_ARCH = armv7l
 else ifeq ($(BR2_x86_64),y)
     IOQUAKE3_BUILD_ARGS += COMPILE_ARCH=x86_64
     IOQUAKE3_ARCH = x86_64
@@ -30,13 +37,17 @@ endif
 
 define IOQUAKE3_BUILD_CMDS
 	$(TARGET_CONFIGURE_OPTS) $(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" \
-	    $(IOQUAKE3_BUILD_ARGS) -C $(@D) -f Makefile
+	    $(IOQUAKE3_BUILD_ARGS) SYSROOT=$(STAGING_DIR) -C $(@D) -f Makefile
 endef
 
 define IOQUAKE3_INSTALL_TARGET_CMDS
 	mkdir -p $(TARGET_DIR)/usr/bin/ioquake3
-	$(INSTALL) -D $(@D)/build/release-linux-$(IOQUAKE3_ARCH)/ioquake3_opengl2.$(IOQUAKE3_ARCH) \
+	$(INSTALL) -D $(@D)/build/release-linux-$(IOQUAKE3_ARCH)/ioquake3.$(IOQUAKE3_ARCH) \
 		$(TARGET_DIR)/usr/bin/ioquake3/ioquake3
+	$(INSTALL) -D $(@D)/build/release-linux-$(IOQUAKE3_ARCH)/renderer_opengl1_$(IOQUAKE3_ARCH).so \
+		$(TARGET_DIR)/usr/bin/ioquake3/
+	$(INSTALL) -D $(@D)/build/release-linux-$(IOQUAKE3_ARCH)/renderer_opengl2_$(IOQUAKE3_ARCH).so \
+		$(TARGET_DIR)/usr/bin/ioquake3/
 endef
 
 define IOQUAKE3_EVMAPY
