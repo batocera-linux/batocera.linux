fbf71899d80bcc510e77c548d0db43feaf55f59a
dmanlfc@gmail.com <dmanlfc@gmail.com>
2023-07-21 09:39:52 +1000

    bump xpad neo for sdl 2.28

--- package/batocera/controllers/xpadneo/xpadneo.mk
+++ package/batocera/controllers/xpadneo/xpadneo.mk
@@ -3,8 +3,8 @@
 # xpadneo
 #
 ################################################################################
-# Version: Commits on Apr 15, 2023
-XPADNEO_VERSION = 5df12b21e24f9f8691a815d5f71a7d2a97f1247d
+# Version: Commits on Jul 18, 2023
+XPADNEO_VERSION = 9b3b6968304d75faca00d1cead63f89e8895195f
 XPADNEO_SITE = $(call github,atar-axis,xpadneo,$(XPADNEO_VERSION))
 XPADNEO_DEPENDENCIES = host-libcurl host-cabextract bluez5_utils
 XPADNEO_MODULE_SUBDIRS = hid-xpadneo/src
