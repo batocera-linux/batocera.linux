53029a08ad0be784a87fe8a8d306f9bf4fd57fa4
NullPopPo <sylph@reset.jp>
2023-08-05 08:11:47 +0000

    add locale select on Wine

--- package/batocera/core/batocera-configgen/configgen/configgen/generators/wine/wineGenerator.py
+++ package/batocera/core/batocera-configgen/configgen/configgen/generators/wine/wineGenerator.py
@@ -9,16 +9,21 @@ import controllersConfig
 class WineGenerator(Generator):
 
     def generate(self, system, rom, playersControllers, guns, gameResolution):
+        cmd=None
         if system.name == "windows_installers":
             commandArray = ["batocera-wine", "windows", "install", rom]
-            return Command.Command(array=commandArray)
+            cmd=Command.Command(array=commandArray)
         elif system.name == "windows":
             commandArray = ["batocera-wine", "windows", "play", rom]
-            return Command.Command(array=commandArray,env={
-                "SDL_GAMECONTROLLERCONFIG": controllersConfig.generateSdlGameControllerConfig(playersControllers),
-            })
+            cmd=Command.Command(array=commandArray)
+        else: raise Exception("invalid system " + system.name)
 
-        raise Exception("invalid system " + system.name)
+        cmd.env['SDL_GAMECONTROLLERCONFIG']=controllersConfig.generateSdlGameControllerConfig(playersControllers)
+
+        if 'lang' in system.config and system.config['lang'] != '':
+            cmd.env['LANG']=cmd.env['LC_ALL']=system.config['lang']+'.UTF-8'
+
+        return cmd
 
     def getMouseMode(self, config):
         if "force_mouse" in config and config["force_mouse"] == "0":
