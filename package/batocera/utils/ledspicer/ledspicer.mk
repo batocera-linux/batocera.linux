a48f8e2ad25447d150ab40f1c4780e16a6bc6233
MizterB <5458030+MizterB@users.noreply.github.com>
2023-08-07 09:58:17 -0400

    Externalize configuration

--- package/batocera/utils/ledspicer/ledspicer.mk
+++ package/batocera/utils/ledspicer/ledspicer.mk
@@ -38,12 +38,38 @@ define LEDSPICER_APPLY_UDEV_RULES
 	cp $(@D)/data/21-ledspicer.rules $(TARGET_DIR)/etc/udev/rules.d/99-ledspicer.rules
 endef
 
-LEDSPICER_POST_INSTALL_TARGET_HOOKS += define LEDSPICER_APPLY_UDEV_RULES
-
 define LEDSPICER_ENABLE_SERVICE
 	install -m 0755 $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/utils/ledspicer/ledspicer.service $(TARGET_DIR)/etc/init.d/S90ledspicer
 endef
 
+define LEDSPICER_EXTERNALIZE_CONFIG
+    mkdir -p $(TARGET_DIR)/usr/share/batocera/datainit/system/configs/ledspicer
+
+    cp $(TARGET_DIR)/usr/share/ledspicer/doc/examples/ledspicer.conf $(TARGET_DIR)/usr/share/batocera/datainit/system/configs/ledspicer/ledspicer.conf
+    ln -sf /userdata/system/configs/ledspicer/ledspicer.conf $(TARGET_DIR)/etc/ledspicer.conf
+
+    mv $(TARGET_DIR)/usr/share/ledspicer/basicColors.xml $(TARGET_DIR)/usr/share/batocera/datainit/system/configs/ledspicer/basicColors.xml
+    ln -sf /userdata/system/configs/ledspicer/basicColors.xml $(TARGET_DIR)/usr/share/ledspicer/basicColors.xml
+
+    mv $(TARGET_DIR)/usr/share/ledspicer/gameData.xml $(TARGET_DIR)/usr/share/batocera/datainit/system/configs/ledspicer/gameData.xml
+    ln -sf /userdata/system/configs/ledspicer/gameData.xml $(TARGET_DIR)/usr/share/ledspicer/gameData.xml
+
+    mv $(TARGET_DIR)/usr/share/ledspicer/animations $(TARGET_DIR)/usr/share/batocera/datainit/system/configs/ledspicer/animations
+    ln -sf /userdata/system/configs/ledspicer/animations $(TARGET_DIR)/usr/share/ledspicer/animations
+
+    mv $(TARGET_DIR)/usr/share/ledspicer/inputs $(TARGET_DIR)/usr/share/batocera/datainit/system/configs/ledspicer/inputs
+    ln -sf /userdata/system/configs/ledspicer/inputs $(TARGET_DIR)/usr/share/ledspicer/inputs
+
+    mv $(TARGET_DIR)/usr/share/ledspicer/profiles $(TARGET_DIR)/usr/share/batocera/datainit/system/configs/ledspicer/profiles
+    ln -sf /userdata/system/configs/ledspicer/profiles $(TARGET_DIR)/usr/share/ledspicer/profiles
+
+    mv $(TARGET_DIR)/usr/share/ledspicer/umaps $(TARGET_DIR)/usr/share/batocera/datainit/system/configs/ledspicer/umaps
+    ln -sf /userdata/system/configs/ledspicer/umaps $(TARGET_DIR)/usr/share/ledspicer/umaps
+
+endef
+
+LEDSPICER_POST_INSTALL_TARGET_HOOKS += LEDSPICER_APPLY_UDEV_RULES
 LEDSPICER_POST_INSTALL_TARGET_HOOKS += LEDSPICER_ENABLE_SERVICE
+LEDSPICER_POST_INSTALL_TARGET_HOOKS += LEDSPICER_EXTERNALIZE_CONFIG
 
 $(eval $(autotools-package))
