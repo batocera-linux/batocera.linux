03e3fd48b6706f18e257e644d5651c3e203efb2b
dmanlfc@gmail.com <dmanlfc@gmail.com>
2023-08-09 08:12:27 +1000

    update latest production list

--- package/batocera/gpu/batocera-nvidia-lists/batocera-nvidia-lists.mk
+++ package/batocera/gpu/batocera-nvidia-lists/batocera-nvidia-lists.mk
@@ -3,8 +3,8 @@
 # batocera-nvidia-lists
 #
 ################################################################################
-# Version: Commits on Jul 20, 2023
-BATOCERA_NVIDIA_LISTS_VERSION = df6a1dd48bdcac8a37293005746faef02c36d687
+# Version: Commits on Aug 9, 2023
+BATOCERA_NVIDIA_LISTS_VERSION = d3992a821caaf8aba189fac6890eb490286d7694
 BATOCERA_NVIDIA_LISTS_SITE = $(call github,batocera-linux,nvidia-lists,$(BATOCERA_NVIDIA_LISTS_VERSION))
 BATOCERA_NVIDIA_LISTS_LICENSE = GPL-3.0+
 BATOCERA_NVIDIA_LISTS_LICENSE_FILES = LICENSE
