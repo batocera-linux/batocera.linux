6c64d95f1b7b7e251e1371517be17028d56c168d
Lbrpdx <lionel.bringuier@gmail.com>
2023-08-07 17:43:38 -0700

    Hurrican: update to latest, prepare for content downloader

--- package/batocera/ports/hurrican/hurrican.mk
+++ package/batocera/ports/hurrican/hurrican.mk
@@ -3,9 +3,9 @@
 # HURRICAN
 #
 ################################################################################
-# Version.: Commits on Jul 30, 2021
-HURRICAN_VERSION = f2cce99acebd1b155cbb445c26e77785a14287d6
-HURRICAN_SITE = https://github.com/drfiemost/Hurrican.git
+# Version.: Commits on Aug, 2023
+HURRICAN_VERSION = 67da195a2fea7cb43202626a18f13357ac6adac2
+HURRICAN_SITE = https://github.com/HurricanGame/Hurrican
 
 HURRICAN_DEPENDENCIES = sdl2 sdl2_mixer sdl2_image libepoxy
 HURRICAN_SITE_METHOD=git
@@ -20,9 +20,6 @@ define HURRICAN_INSTALL_TARGET_CMDS
 	mkdir -p $(TARGET_DIR)/usr/share/hurrican/
 	cp -pvr $(@D)/buildroot-build/hurrican $(TARGET_DIR)/usr/bin
 	chmod 0755 $(TARGET_DIR)/usr/bin/hurrican
-    cp -avr $(@D)/Hurrican/data $(TARGET_DIR)/usr/share/hurrican/
-    cp -avr $(@D)/Hurrican/lang $(TARGET_DIR)/usr/share/hurrican/
-
 	# evmap config
 	mkdir -p $(TARGET_DIR)/usr/share/evmapy
 	cp $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/ports/hurrican/hurrican.keys $(TARGET_DIR)/usr/share/evmapy
