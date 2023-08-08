20d11f4f5821030ede9e5cfc64a91e12a01ba3bd
dmanlfc@gmail.com <dmanlfc@gmail.com>
2023-08-08 21:57:47 +1000

    add some extra configs for wine

--- package/batocera/core/batocera-configgen/configgen/configgen/generators/wine/wineGenerator.py
+++ package/batocera/core/batocera-configgen/configgen/configgen/generators/wine/wineGenerator.py
@@ -5,6 +5,7 @@ import Command
 import os
 from os import path
 import controllersConfig
+import subprocess
 
 class WineGenerator(Generator):
 
@@ -14,14 +15,25 @@ class WineGenerator(Generator):
             return Command.Command(array=commandArray)
         elif system.name == "windows":
             commandArray = ["batocera-wine", "windows", "play", rom]
-            return Command.Command(
-                array=commandArray,
-                env={
+            
+            environment = {}
+            #system.language
+            language = subprocess.check_output("batocera-settings-get system.language", shell=True, text=True).strip()
+            if language:
+                environment.update({
+                    "LANG": language + ".UTF-8",
+                    "LC_ALL": language + ".UTF-8"
+                    }
+                )
+            # sdl controller option
+            if system.isOptSet("sdl_config") and system.getOptBoolean("sdl_config"):
+                environment.update({
                     "SDL_GAMECONTROLLERCONFIG": controllersConfig.generateSdlGameControllerConfig(playersControllers),
                     "SDL_JOYSTICK_HIDAPI": "0"
-                }
-            )
-
+                    }
+                )
+            return Command.Command(array=commandArray, env=environment)
+        
         raise Exception("invalid system " + system.name)
 
     def getMouseMode(self, config):
