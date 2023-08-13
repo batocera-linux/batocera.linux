14021355c4ecd7a66bf8839a74338df21ea1e629
Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>
2023-08-13 13:17:24 +0000

    pixelcade: first init at boot
    Signed-off-by: Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>

--- package/batocera/core/batocera-udev-rules/batocera-udev-rules.mk
+++ package/batocera/core/batocera-udev-rules/batocera-udev-rules.mk
@@ -10,7 +10,8 @@ BATOCERA_UDEV_RULES_SOURCE=
 
 define BATOCERA_UDEV_RULES_INSTALL_TARGET_CMDS
 	mkdir -p $(TARGET_DIR)/etc/udev/rules.d
-	$(INSTALL) -m 0644 -D $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/core/batocera-udev-rules/rules/*.rules $(TARGET_DIR)/etc/udev/rules.d/
+	$(INSTALL) -m 0644 -D $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/core/batocera-udev-rules/rules/*.rules    $(TARGET_DIR)/etc/udev/rules.d/
+	$(INSTALL) -m 0755 -D $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/core/batocera-udev-rules/S15virtualevents $(TARGET_DIR)/etc/init.d/
 endef
 
 $(eval $(generic-package))
