8d5b6e3d8d304331f12e05d2b5322bc44960a709
dmanlfc@gmail.com <dmanlfc@gmail.com>
2023-08-14 21:19:45 +1000

    static link to the exe to avoid - `no version information available` when not cleanbuilding. - remove ini update since this is resolved in code.

--- package/batocera/core/batocera-configgen/configgen/configgen/generators/vpinball/vpinballGenerator.py
+++ package/batocera/core/batocera-configgen/configgen/configgen/generators/vpinball/vpinballGenerator.py
@@ -7,7 +7,6 @@ from generators.Generator import Generator
 import batoceraFiles
 
 vpinballConfigPath = batoceraFiles.CONF + "/vpinball"
-vpinballConfigFile = vpinballConfigPath + "/VPinballX.ini"
 vpinballMusicPath = vpinballConfigPath + "/music"
 
 class VPinballGenerator(Generator):
@@ -20,21 +19,6 @@ class VPinballGenerator(Generator):
         # create vpinball music directory
         if not os.path.exists(vpinballMusicPath):
             os.makedirs(vpinballMusicPath)
-        
-        config = configparser.ConfigParser()
-
-        # ensure we set disableesc=1 to allow graceful shitdown
-        if not os.path.exists(vpinballConfigFile):    
-            config["Player"] = {"disableesc": "1"}    
-        else:
-            config.read(vpinballConfigFile)
-            if not config.has_section("Player"):
-                config["Player"] = {"disableesc": "1"}
-            else:
-                config["Player"]["disableesc"] = "1"
-        
-        with open(vpinballConfigFile, "w") as f:
-                config.write(f)
         
         # set the config path to be sure
         commandArray = [
--- package/batocera/emulators/vpinball/vpinball.mk
+++ package/batocera/emulators/vpinball/vpinball.mk
@@ -48,6 +48,7 @@ define VPINBALL_CMAKE_HACKS
 endef
 
 VPINBALL_CONF_OPTS += -DCMAKE_BUILD_TYPE=Release
+VPINBALL_CONF_OPTS += -DBUILD_SHARED_LIBS=OFF
 
 define VPINBALL_INSTALL_TARGET_CMDS
     mkdir -p $(TARGET_DIR)/usr/bin/vpinball
@@ -59,9 +60,6 @@ define VPINBALL_INSTALL_TARGET_CMDS
     cp -R $(@D)/buildroot-build/res $(TARGET_DIR)/usr/bin/vpinball/
     cp -R $(@D)/buildroot-build/scripts $(TARGET_DIR)/usr/bin/vpinball/
     cp -R $(@D)/buildroot-build/shader $(TARGET_DIR)/usr/bin/vpinball/
-    # install last library
-    $(INSTALL) -D -m 0755 $(@D)/buildroot-build/libglad.so \
-        $(TARGET_DIR)/usr/lib
 endef
 
 define VPINBALL_EVMAPY
