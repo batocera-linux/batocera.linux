93c1b5cc24936f8dd350b98e70045943a97621a4
Romain Tisserand <romain.tisserand@gmail.com>
2023-08-18 20:34:29 +0200

    Experimental standalone Sugarbox Amstrad CPC/CPC+ emulator

---
+++ package/batocera/libraries/qt6/qt6websockets/qt6websockets.mk
@@ -0,0 +1,44 @@
+################################################################################
+#
+# qt6websockets
+#
+################################################################################
+
+QT6WEBSOCKETS_VERSION = $(QT6_VERSION)
+QT6WEBSOCKETS_SITE = $(QT6_SITE)
+QT6WEBSOCKETS_SOURCE = qtwebsockets-$(QT6_SOURCE_TARBALL_PREFIX)-$(QT6WEBSOCKETS_VERSION).tar.xz
+QT6WEBSOCKETS_INSTALL_STAGING = YES
+QT6WEBSOCKETS_SUPPORTS_IN_SOURCE_BUILD = NO
+
+QT6WEBSOCKETS_LICENSE = \
+    GPL-2.0+ or LGPL-3.0, \
+    GPL-3.0, GFDL-1.3 no invariants (docs)
+
+QT6WEBSOCKETS_LICENSE_FILES = \
+    LICENSES/GPL-2.0-only.txt \
+    LICENSES/GPL-3.0-only.txt \
+    LICENSES/LGPL-3.0-only.txt \
+    LICENSES/GFDL-1.3-no-invariants-only.txt
+
+QT6WEBSOCKETS_CONF_OPTS = \
+    -GNinja \
+    -DQT_HOST_PATH=$(HOST_DIR) \
+    -DBUILD_WITH_PCH=OFF \
+    -DQT_BUILD_EXAMPLES=OFF \
+    -DQT_BUILD_TESTS=OFF
+
+QT6WEBSOCKETS_DEPENDENCIES = qt6base
+
+define QT6WEBSOCKETS_BUILD_CMDS
+    $(TARGET_MAKE_ENV) $(BR2_CMAKE) --build $(QT6WEBSOCKETS_BUILDDIR)
+endef
+
+define QT6WEBSOCKETS_INSTALL_STAGING_CMDS
+    $(TARGET_MAKE_ENV) DESTDIR=$(STAGING_DIR) $(BR2_CMAKE) --install $(QT6WEBSOCKETS_BUILDDIR)
+endef
+
+define QT6WEBSOCKETS_INSTALL_TARGET_CMDS
+    $(TARGET_MAKE_ENV) DESTDIR=$(TARGET_DIR) $(BR2_CMAKE) --install $(QT6WEBSOCKETS_BUILDDIR)
+endef
+
+$(eval $(cmake-package))
