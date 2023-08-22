bc2dca0040678f7927842cd22f920b648c5f3c43
dmanlfc@gmail.com <dmanlfc@gmail.com>
2023-08-22 18:23:55 +1000

    pcsx2 vulkan fixes

--- package/batocera/emulators/pcsx2/pcsx2.mk
+++ package/batocera/emulators/pcsx2/pcsx2.mk
@@ -3,8 +3,8 @@
 # pcsx2
 #
 ################################################################################
-#Version: Commits on Aug 5, 2023
-PCSX2_VERSION = v1.7.4871
+#Version: Commits on Aug 20, 2023
+PCSX2_VERSION = v1.7.4933
 PCSX2_SITE = https://github.com/pcsx2/pcsx2.git
 PCSX2_SITE_METHOD = git
 PCSX2_GIT_SUBMODULES = YES
