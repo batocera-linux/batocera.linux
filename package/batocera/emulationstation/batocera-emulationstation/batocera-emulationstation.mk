2433f70b1cf4b5d900073bf23df17c670855c514
dmanlfc@gmail.com <dmanlfc@gmail.com>
2023-08-27 21:13:21 +1000

    ES update for gcc 13

--- package/batocera/emulationstation/batocera-emulationstation/batocera-emulationstation.mk
+++ package/batocera/emulationstation/batocera-emulationstation/batocera-emulationstation.mk
@@ -1,10 +1,10 @@
 ################################################################################
 #
-# batocera-emulationStation
+# batocera-emulationstation
 #
 ################################################################################
-# Last update: Commits on Aug 7, 2023
-BATOCERA_EMULATIONSTATION_VERSION = edcf873ad65344fb3677cd64264048100e0174a4
+# Last update: Commits on Aug 27, 2023
+BATOCERA_EMULATIONSTATION_VERSION = 50eac5161c91ff8c0fb5a55030915cd2a53bc103
 BATOCERA_EMULATIONSTATION_SITE = https://github.com/batocera-linux/batocera-emulationstation
 BATOCERA_EMULATIONSTATION_SITE_METHOD = git
 BATOCERA_EMULATIONSTATION_LICENSE = MIT
