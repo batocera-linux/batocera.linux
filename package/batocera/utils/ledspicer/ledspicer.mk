824a09292ebbe8d831883ac4418158b775a7e137
MizterB <5458030+MizterB@users.noreply.github.com>
2023-08-29 14:04:01 -0400

    Bump rev and create service

--- package/batocera/utils/ledspicer/ledspicer.mk
+++ package/batocera/utils/ledspicer/ledspicer.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LEDSPICER_VERSION = 4add69ba78068a48066f3c5e8ebe72f0ef2e27ad
+LEDSPICER_VERSION = 7e8957edebe9fd6e209ac824dfe109edb3de36d9
 LEDSPICER_SITE = $(call github,meduzapat,LEDSpicer,$(LEDSPICER_VERSION))
 LEDSPICER_LICENSE = GPLv3
 LEDSPICER_DEPENDENCIES = tinyxml2 libusb libtool udev
@@ -34,42 +34,26 @@ else
 	LEDSPICER_CONF_OPTS += --disable-raspberrypi
 endif
 
-define LEDSPICER_APPLY_UDEV_RULES
-	cp $(@D)/data/21-ledspicer.rules $(TARGET_DIR)/etc/udev/rules.d/99-ledspicer.rules
+# Stage files in an 'init' directory to be used by the LEDSpicer service
+# Default contents of /usr/share/ledspicer (except 'doc') move to '/usr/share/ledspicer/init/share'
+# udev rules move to '/usr/share/ledspicer/init/udev'
+define LEDSPICER_SERVICE_INIT
+    mkdir -p $(TARGET_DIR)/usr/share/ledspicer/init/share
+    for f in $(TARGET_DIR)/usr/share/ledspicer/*; do \
+        test "$$f" == "$(TARGET_DIR)/usr/share/ledspicer/doc" && continue; \
+        test "$$f" == "$(TARGET_DIR)/usr/share/ledspicer/init" && continue; \
+        mv "$$f" "$(TARGET_DIR)/usr/share/ledspicer/init/share/"; \
+    done
+    mkdir -p $(TARGET_DIR)/usr/share/ledspicer/init/udev
+    cp $(@D)/data/21-ledspicer.rules $(TARGET_DIR)/usr/share/ledspicer/init/udev/99-ledspicer.rules
 endef
 
-define LEDSPICER_ENABLE_SERVICE
-	install -m 0755 $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/utils/ledspicer/ledspicer.service $(TARGET_DIR)/etc/init.d/S90ledspicer
+define LEDSPICER_SERVICE_INSTALL
+    mkdir -p $(TARGET_DIR)/usr/share/batocera/services
+	install -m 0755 $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/utils/ledspicer/ledspicer $(TARGET_DIR)/usr/share/batocera/services/
 endef
 
-define LEDSPICER_EXTERNALIZE_CONFIG
-    mkdir -p $(TARGET_DIR)/usr/share/batocera/datainit/system/configs/ledspicer
-
-    cp $(TARGET_DIR)/usr/share/ledspicer/doc/examples/ledspicer.conf $(TARGET_DIR)/usr/share/batocera/datainit/system/configs/ledspicer/ledspicer.conf
-    ln -sf /userdata/system/configs/ledspicer/ledspicer.conf $(TARGET_DIR)/etc/ledspicer.conf
-
-    mv $(TARGET_DIR)/usr/share/ledspicer/basicColors.xml $(TARGET_DIR)/usr/share/batocera/datainit/system/configs/ledspicer/basicColors.xml
-    ln -sf /userdata/system/configs/ledspicer/basicColors.xml $(TARGET_DIR)/usr/share/ledspicer/basicColors.xml
-
-    mv $(TARGET_DIR)/usr/share/ledspicer/gameData.xml $(TARGET_DIR)/usr/share/batocera/datainit/system/configs/ledspicer/gameData.xml
-    ln -sf /userdata/system/configs/ledspicer/gameData.xml $(TARGET_DIR)/usr/share/ledspicer/gameData.xml
-
-    mv $(TARGET_DIR)/usr/share/ledspicer/animations $(TARGET_DIR)/usr/share/batocera/datainit/system/configs/ledspicer/animations
-    ln -sf /userdata/system/configs/ledspicer/animations $(TARGET_DIR)/usr/share/ledspicer/animations
-
-    mv $(TARGET_DIR)/usr/share/ledspicer/inputs $(TARGET_DIR)/usr/share/batocera/datainit/system/configs/ledspicer/inputs
-    ln -sf /userdata/system/configs/ledspicer/inputs $(TARGET_DIR)/usr/share/ledspicer/inputs
-
-    mv $(TARGET_DIR)/usr/share/ledspicer/profiles $(TARGET_DIR)/usr/share/batocera/datainit/system/configs/ledspicer/profiles
-    ln -sf /userdata/system/configs/ledspicer/profiles $(TARGET_DIR)/usr/share/ledspicer/profiles
-
-    mv $(TARGET_DIR)/usr/share/ledspicer/umaps $(TARGET_DIR)/usr/share/batocera/datainit/system/configs/ledspicer/umaps
-    ln -sf /userdata/system/configs/ledspicer/umaps $(TARGET_DIR)/usr/share/ledspicer/umaps
-
-endef
-
-LEDSPICER_POST_INSTALL_TARGET_HOOKS += LEDSPICER_APPLY_UDEV_RULES
-LEDSPICER_POST_INSTALL_TARGET_HOOKS += LEDSPICER_ENABLE_SERVICE
-LEDSPICER_POST_INSTALL_TARGET_HOOKS += LEDSPICER_EXTERNALIZE_CONFIG
+LEDSPICER_POST_INSTALL_TARGET_HOOKS += LEDSPICER_SERVICE_INIT
+LEDSPICER_POST_INSTALL_TARGET_HOOKS += LEDSPICER_SERVICE_INSTALL
 
 $(eval $(autotools-package))
