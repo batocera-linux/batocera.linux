c0e806dbc73d57be9d45092679734a5be3199822
MizterB <5458030+MizterB@users.noreply.github.com>
2023-09-07 20:37:57 -0400

    Patch to read configs from userdata

--- package/batocera/utils/ledspicer/ledspicer.mk
+++ package/batocera/utils/ledspicer/ledspicer.mk
@@ -10,8 +10,6 @@ LEDSPICER_LICENSE = GPLv3
 LEDSPICER_DEPENDENCIES = tinyxml2 libusb libtool udev
 LEDSPICER_AUTORECONF = YES
 LEDSPICER_CONF_OPTS = CXXFLAGS='-g0 -O3' --enable-nanoled --enable-pacdrive --enable-pacled64 --enable-ultimateio --enable-ledwiz32 --enable-howler --enable-adalight
-
-# Move doc from /usr/share/doc/ledspicer to /usr/share/ledspicer/doc to conform to Batocera
 LEDSPICER_CONF_OPTS += --docdir=/usr/share/ledspicer/doc
 
 ifeq ($(BR2_PACKAGE_PULSEAUDIO),y)
@@ -34,15 +32,8 @@ else
 	LEDSPICER_CONF_OPTS += --disable-raspberrypi
 endif
 
-# Stage files in an 'init' directory to be used by the LEDSpicer service
-# Default contents of /usr/share/ledspicer (except 'doc') move to '/usr/share/ledspicer/init'
-define LEDSPICER_SERVICE_INIT
-    mkdir -p $(TARGET_DIR)/usr/share/ledspicer/init
-    for f in $(TARGET_DIR)/usr/share/ledspicer/*; do \
-        test "$$f" == "$(TARGET_DIR)/usr/share/ledspicer/doc" && continue; \
-        test "$$f" == "$(TARGET_DIR)/usr/share/ledspicer/init" && continue; \
-        mv "$$f" "$(TARGET_DIR)/usr/share/ledspicer/init/"; \
-    done
+define LEDSPICER_UDEV_RULE
+    cp $(@D)/data/21-ledspicer.rules $(TARGET_DIR)/etc/udev/rules.d/99-ledspicer.rules
 endef
 
 define LEDSPICER_SERVICE_INSTALL
@@ -50,7 +41,7 @@ define LEDSPICER_SERVICE_INSTALL
 	install -m 0755 $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/utils/ledspicer/ledspicer $(TARGET_DIR)/usr/share/batocera/services/
 endef
 
-LEDSPICER_POST_INSTALL_TARGET_HOOKS += LEDSPICER_SERVICE_INIT
+LEDSPICER_POST_INSTALL_TARGET_HOOKS += LEDSPICER_UDEV_RULE
 LEDSPICER_POST_INSTALL_TARGET_HOOKS += LEDSPICER_SERVICE_INSTALL
 
 $(eval $(autotools-package))
