48612bdc8b95ef7ec45f63e54deb8c99c3ebec3b
Romain Tisserand <romain.tisserand@gmail.com>
2023-09-09 12:17:11 +0200

    Add Qt6 Wayland package

---
+++ package/batocera/libraries/qt6/qt6wayland/qt6wayland.mk
@@ -0,0 +1,45 @@
+################################################################################
+#
+# qt6wayland
+#
+################################################################################
+
+QT6WAYLAND_VERSION = $(QT6_VERSION)
+QT6WAYLAND_SITE = $(QT6_SITE)
+QT6WAYLAND_SOURCE = qtwayland-$(QT6_SOURCE_TARBALL_PREFIX)-$(QT6WAYLAND_VERSION).tar.xz
+QT6WAYLAND_INSTALL_STAGING = YES
+QT6WAYLAND_SUPPORTS_IN_SOURCE_BUILD = NO
+
+QT6WAYLAND_LICENSE = \
+    GPL-2.0+ or LGPL-3.0, \
+    GPL-3.0, GFDL-1.3 no invariants (docs)
+
+QT6WAYLAND_LICENSE_FILES = \
+    LICENSES/GPL-2.0-only.txt \
+    LICENSES/GPL-3.0-only.txt \
+    LICENSES/LGPL-3.0-only.txt \
+    LICENSES/GFDL-1.3-no-invariants-only.txt
+
+QT6WAYLAND_CONF_OPTS = \
+    -GNinja \
+    -DQT_HOST_PATH=$(HOST_DIR) \
+    -DBUILD_WITH_PCH=OFF \
+    -DQT_BUILD_EXAMPLES=OFF \
+    -DQT_BUILD_TESTS=OFF
+
+QT6WAYLAND_DEPENDENCIES = qt6base host-qt6wayland
+
+define QT6WAYLAND_BUILD_CMDS
+    $(TARGET_MAKE_ENV) $(BR2_CMAKE) --build $(QT6WAYLAND_BUILDDIR)
+endef
+
+define QT6WAYLAND_INSTALL_STAGING_CMDS
+    $(TARGET_MAKE_ENV) DESTDIR=$(STAGING_DIR) $(BR2_CMAKE) --install $(QT6WAYLAND_BUILDDIR)
+endef
+
+define QT6WAYLAND_INSTALL_TARGET_CMDS
+    $(TARGET_MAKE_ENV) DESTDIR=$(TARGET_DIR) $(BR2_CMAKE) --install $(QT6WAYLAND_BUILDDIR)
+endef
+
+$(eval $(cmake-package))
+$(eval $(host-cmake-package))
