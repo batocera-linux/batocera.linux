72bdc0ca67ee26910a54a3ac8fe4d7d165f09e6c
dmanlfc@gmail.com <dmanlfc@gmail.com>
2023-09-16 14:38:21 +1000

    fix ryujinx audio

--- package/batocera/core/batocera-configgen/configgen/configgen/generators/ryujinx/ryujinxGenerator.py
+++ package/batocera/core/batocera-configgen/configgen/configgen/generators/ryujinx/ryujinxGenerator.py
@@ -107,6 +107,8 @@ class RyujinxGenerator(Generator):
         conf["game_dirs"] = ["/userdata/roms/switch"]
         conf["start_fullscreen"] = True
         conf["docked_mode"] = True
+        conf["audio_backend"] = "OpenAl"
+        conf["audio_volume"] = 1
         # set ryujinx app language
         conf["language_code"] = str(getLangFromEnvironment())
 
