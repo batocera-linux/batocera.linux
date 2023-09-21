7a22354ad9d3223cb4f4b36cded12b72c5bebeac
Romain TISSERAND <romain.tisserand@gmail.com>
2023-09-21 15:11:45 +0200

    Fix lr-same-cdi genie build

--- package/batocera/emulators/retroarch/libretro/libretro-same-cdi/libretro-same-cdi.mk
+++ package/batocera/emulators/retroarch/libretro/libretro-same-cdi/libretro-same-cdi.mk
@@ -9,7 +9,15 @@ LIBRETRO_SAME_CDI_SITE = $(call github,libretro,same_cdi,$(LIBRETRO_SAME_CDI_VER
 LIBRETRO_SAME_CDI_LICENSE = GPL
 
 define LIBRETRO_SAME_CDI_BUILD_CMDS
-	$(HOST_CONFIGURE_OPTS) $(MAKE) -C $(@D)/3rdparty/genie/build/gmake.linux -f genie.make
+	# First, we need to build genie for host
+	cd $(@D); \
+	PATH="$(HOST_DIR)/bin:$$PATH" \
+	$(MAKE) TARGETOS=linux OSD=sdl genie \
+	TARGET=mame SUBTARGET=tiny \
+	NO_USE_PORTAUDIO=1 NO_X11=1 USE_SDL=0 \
+	USE_QTDEBUG=0 DEBUG=0 IGNORE_GIT=1 MPARAM=""
+
+	# Then build lr-same-cdi for target
 	$(TARGET_CONFIGURE_OPTS) $(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" GIT_VERSION="" -C $(@D) -f Makefile.libretro
 endef
 
