8283a5b738cc4e5a8fcb1599536572f6d974fe77
Nicholas S. Castellano <nicholas.s.castellano+github@gmail.com>
2023-09-22 19:07:17 -0400

    Download vf2 firmware from github release instead of in-tree blobs

--- package/batocera/boot/uboot-visionfive2/uboot-visionfive2.mk
+++ package/batocera/boot/uboot-visionfive2/uboot-visionfive2.mk
@@ -5,15 +5,20 @@
 ################################################################################
 
 UBOOT_VISIONFIVE2_VERSION = 3.6.1
-UBOOT_VISIONFIVE2_SOURCE =
+UBOOT_VISIONFIVE2_SITE = https://github.com/starfive-tech/VisionFive2/releases/download/VF2_v$(UBOOT_VISIONFIVE2_VERSION)
+UBOOT_VISIONFIVE2_SOURCE = u-boot-spl.bin.normal.out
+UBOOT_VISIONFIVE2_EXTRA_DOWNLOADS = visionfive2_fw_payload.img
+
+define UBOOT_VISIONFIVE2_EXTRACT_CMDS
+endef
 
 define UBOOT_VISIONFIVE2_BUILD_CMDS
 endef
 
 define UBOOT_VISIONFIVE2_INSTALL_TARGET_CMDS
 	mkdir -p $(BINARIES_DIR)/uboot-visionfive2
-	cp $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/boot/uboot-visionfive2/visionfive2_fw_payload.img $(BINARIES_DIR)/uboot-visionfive2/visionfive2_fw_payload.img
-	cp $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/boot/uboot-visionfive2/u-boot-spl.bin.normal.out $(BINARIES_DIR)/uboot-visionfive2/u-boot-spl.bin.normal.out
+	cp $(UBOOT_VISIONFIVE2_DL_DIR)/u-boot-spl.bin.normal.out $(BINARIES_DIR)/uboot-visionfive2/u-boot-spl.bin.normal.out
+	cp $(UBOOT_VISIONFIVE2_DL_DIR)/visionfive2_fw_payload.img $(BINARIES_DIR)/uboot-visionfive2/visionfive2_fw_payload.img
 endef
 
 $(eval $(generic-package))
