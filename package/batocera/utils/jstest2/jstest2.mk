ca263f7fe321b421b3f9a21c0e97672569d80648
Nicholas S. Castellano <nicholas.s.castellano+github@gmail.com>
2023-09-23 15:16:00 -0400

    Update sdl-jstest repo from gitlab to github

--- package/batocera/utils/jstest2/jstest2.mk
+++ package/batocera/utils/jstest2/jstest2.mk
@@ -5,8 +5,7 @@
 ################################################################################
 
 JSTEST2_VERSION = 95784a322faf66f7af79bc883508c8d827ed77b2
-#JSTEST2_SITE = $(call gitlab,sdl-jstest,sdl-jstest,$(JSTEST2_VERSION))
-JSTEST2_SITE = https://gitlab.com/sdl-jstest/sdl-jstest
+JSTEST2_SITE = https://github.com/Grumbel/sdl-jstest
 JSTEST2_SITE_METHOD = git
 JSTEST2_GIT_SUBMODULES = YES
 JSTEST2_DEPENDENCIES = sdl2
