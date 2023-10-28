51e9f05d33f25203b64d553d829997b12ef527af
dmanlfc@gmail.com <dmanlfc@gmail.com>
2023-10-28 20:39:07 +1000

    bump libretro-minivmac

--- package/batocera/emulators/retroarch/libretro/libretro-minivmac/libretro-minivmac.mk
+++ package/batocera/emulators/retroarch/libretro/libretro-minivmac/libretro-minivmac.mk
@@ -3,8 +3,8 @@
 # libretro-minivmac
 #
 ################################################################################
-# Version: Commits on Apr 5, 2022
-LIBRETRO_MINIVMAC_VERSION = 7fc3e2e7b9234ef2446bc49cf830e4d600123251
+# Version: Commits on Dec 7, 2022
+LIBRETRO_MINIVMAC_VERSION = 45edc82baae906b90b67cce66761557923a6ba75
 LIBRETRO_MINIVMAC_SITE = https://github.com/libretro/libretro-minivmac
 LIBRETRO_MINIVMAC_LICENSE = GPLv2
 LIBRETRO_MINIVMAC_SITE_METHOD=git
@@ -13,7 +13,8 @@ LIBRETRO_MINIVMAC_GIT_SUBMODULES=YES
 LIBRETRO_MINIVMAC_DEPENDENCIES = 
 
 define LIBRETRO_MINIVMAC_BUILD_CMDS
-	$(TARGET_CONFIGURE_OPTS) $(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" -C $(@D)/ -f Makefile platform=unix \
+    $(TARGET_CONFIGURE_OPTS) $(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" \
+        -C $(@D)/ -f Makefile platform=unix \
         GIT_VERSION="-$(shell echo $(LIBRETRO_MINIVMAC_VERSION) | cut -c 1-7)"
 endef
 
