ccbc3922288dcfa02654e5124d00ba184f94b55d
dmanlfc@gmail.com <dmanlfc@gmail.com>
2023-11-04 16:52:53 +1000

    start of dxx-rebirth into v39

--- package/batocera/ports/dxx-rebirth/dxx-rebirth.mk
+++ package/batocera/ports/dxx-rebirth/dxx-rebirth.mk
@@ -3,15 +3,17 @@
 # dxx-rebirth (Descent 1 & 2) engine
 #
 ################################################################################
-
-DXX_REBIRTH_VERSION = 5c900ca7a72398a9d9e46428cb1d684eba0a5cc5
+# Version: Commits on Oct 9, 2023
+DXX_REBIRTH_VERSION = 3ad507fad88f7475751e2ca94d911c97c62cbc65
 DXX_REBIRTH_SITE = https://github.com/dxx-rebirth/dxx-rebirth
 DXX_REBIRTH_SITE_METHOD=git
+DXX_REBIRTH_LICENSE = GPLv3
+DXX_REBIRTH_LICENSE_FILE = COPYING.txt
 DXX_REBIRTH_DEPENDENCIES = host-scons sdl2 sdl2_image sdl2_mixer libpng physfs
 
-DXX_REBIRTH_LDFLAGS = $(TARGET_LDFLAGS)
-DXX_REBIRTH_CFLAGS = $(TARGET_CFLAGS)
-DXX_REBIRTH_CXXFLAGS = $(TARGET_CXXFLAGS)
+DXX_REBIRTH_LDFLAGS   = $(TARGET_LDFLAGS)
+DXX_REBIRTH_CFLAGS    = $(TARGET_CFLAGS)
+DXX_REBIRTH_CXXFLAGS  = $(TARGET_CXXFLAGS)
 DXX_REBIRTH_SCONS_ENV = $(TARGET_CONFIGURE_OPTS)
 
 DXX_REBIRTH_SCONS_OPTS = -j$(PARALLEL_JOBS) sdl2=yes
@@ -22,16 +24,24 @@ DXX_REBIRTH_SCONS_OPTS += opengles=yes
 endif
 
 define DXX_REBIRTH_BUILD_CMDS
-        (cd $(@D); \
-		PKG_CONFIG_PATH="$(STAGING_DIR)/usr/lib/pkgconfig" \
-                $(DXX_REBIRTH_SCONS_ENV) \
-                $(SCONS) \
-                $(DXX_REBIRTH_SCONS_OPTS))
+    (cd $(@D); \
+        PKG_CONFIG_PATH="$(STAGING_DIR)/usr/lib/pkgconfig" \
+        $(DXX_REBIRTH_SCONS_ENV) \
+        $(SCONS) \
+        $(DXX_REBIRTH_SCONS_OPTS))
 endef
 
 define DXX_REBIRTH_INSTALL_TARGET_CMDS
-	cp $(@D)/build/d1x-rebirth/d1x-rebirth $(TARGET_DIR)/usr/bin/
-	cp $(@D)/build/d2x-rebirth/d2x-rebirth $(TARGET_DIR)/usr/bin/
+    cp $(@D)/build/d1x-rebirth/d1x-rebirth $(TARGET_DIR)/usr/bin/
+    cp $(@D)/build/d2x-rebirth/d2x-rebirth $(TARGET_DIR)/usr/bin/
 endef
+
+define DXX_REBIRTH_EVMAPY
+	mkdir -p $(TARGET_DIR)/usr/share/evmapy
+	cp $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/ports/dxx-rebirth/dxx-rebirth.keys \
+	    $(TARGET_DIR)/usr/share/evmapy
+endef
+
+DXX_REBIRTH_POST_INSTALL_TARGET_HOOKS += DXX_REBIRTH_EVMAPY
 
 $(eval $(generic-package))
