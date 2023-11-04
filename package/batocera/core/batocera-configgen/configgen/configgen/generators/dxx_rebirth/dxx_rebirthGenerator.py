a19bc691e61f8126abc20a0496df64c97cbe5c02
dmanlfc@gmail.com <dmanlfc@gmail.com>
2023-11-04 21:55:05 +1000

    add some dxx-rebirth options

--- package/batocera/core/batocera-configgen/configgen/configgen/generators/dxx_rebirth/dxx_rebirthGenerator.py
+++ package/batocera/core/batocera-configgen/configgen/configgen/generators/dxx_rebirth/dxx_rebirthGenerator.py
@@ -2,9 +2,7 @@
 import Command
 from generators.Generator import Generator
 import controllersConfig
-import configparser
 import os
-import shutil
 import batoceraFiles
 
 class DXX_RebirthGenerator(Generator):
@@ -31,29 +29,59 @@ class DXX_RebirthGenerator(Generator):
             with open(rebirthConfigFile, 'r') as file:
                 lines = file.readlines()
             
-            # Modify the values of ResolutionX and ResolutionY
             for i, line in enumerate(lines):
+                # set resolution
                 if line.startswith('ResolutionX='):
                     lines[i] = f'ResolutionX={gameResolution["width"]}\n'
                 elif line.startswith('ResolutionY='):
                     lines[i] = f'ResolutionY={gameResolution["height"]}\n'
+                # fullscreen
+                if line.startswith('WindowMode='):
+                    lines[i] = f'WindowMode=0\n'
+                # vsync
+                if line.startswith('VSync='):
+                    if system.isOptSet("rebirth_vsync"):
+                        lines[i] = f'VSync={system.config["rebirth_vsync"]}\n'
+                    else:
+                        lines[i] = f'VSync=0\n'
+                # texture filtering
+                if line.startswith('TexFilt='):
+                    if system.isOptSet("rebirth_filtering"):
+                        lines[i] = f'TexFilt={system.config["rebirth_filtering"]}\n'
+                    else:
+                        lines[i] = f'TexFilt=0\n'
+                # anisotropy
+                if line.startswith('TexAnisotropy='):
+                    if system.isOptSet("rebirth_anisotropy"):
+                        lines[i] = f'TexAnisotropy={system.config["rebirth_anisotropy"]}\n'
+                    else:
+                        lines[i] = f'TexAnisotropy=0\n'
+                # 4x multisampling
+                if line.startswith('Multisample='):
+                    if system.isOptSet("rebirth_multisample"):
+                        lines[i] = f'Multisample={system.config["rebirth_multisample"]}\n'
+                    else:
+                        lines[i] = f'Multisample=0\n'
             
-            # Write the modified contents back to the file
             with open(rebirthConfigFile, 'w') as file:
                 file.writelines(lines)
         
         else:
-            # File doesn't exist, create it with default values
+            # File doesn't exist, create it with some default values
             with open(rebirthConfigFile, 'w') as file:
                 file.write(f'ResolutionX={gameResolution["width"]}\n')
                 file.write(f'ResolutionY={gameResolution["height"]}\n')
+                file.write(f'WindowMode=0\n')
+                file.write(f'VSync=0\n')
+                file.write(f'TexFilt=0\n')
+                file.write(f'TexAnisotropy=0\n')
+                file.write(f'Multisample=0\n')
         
         commandArray = [dxx_rebirth, "-hogdir", directory]
         
         return Command.Command(
             array=commandArray,
             env={
-                "XDG_CONFIG_HOME":batoceraFiles.CONF,
                 "SDL_GAMECONTROLLERCONFIG":controllersConfig.generateSdlGameControllerConfig(playersControllers)
             }
         )
