e3df2b5f6100536128a9868a98d7e127c668e158
dmanlfc@gmail.com <dmanlfc@gmail.com>
2023-12-11 22:17:39 +1000

    sonic port updates

--- package/batocera/ports/sonic2013/sonic2013.mk
+++ package/batocera/ports/sonic2013/sonic2013.mk
@@ -3,19 +3,14 @@
 # sonic2013
 #
 ################################################################################
-
+# Version: Commits on Dec 2, 2023
+SONIC2013_VERSION = 72de771
 SONIC2013_SITE = https://github.com/Rubberduckycooly/Sonic-1-2-2013-Decompilation.git
 SONIC2013_SITE_METHOD = git
 SONIC2013_GIT_SUBMODULES == YES
-
-SONIC2013_DEPENDENCIES = sdl2 libogg libvorbis
 SONIC2013_LICENSE = Custom
 
-ifeq ($(BR2_PACKAGE_BATOCERA_TARGET_X86_ANY),y)
-	SONIC2013_VERSION = 290cd5f
-else
-	SONIC2013_VERSION = f9718af
-endif
+SONIC2013_DEPENDENCIES = sdl2 libogg libvorbis
 
 define SONIC2013_BUILD_CMDS
 	$(TARGET_CONFIGURE_OPTS) $(MAKE) -C $(@D) -f Makefile VERBOSE=1
@@ -27,7 +22,8 @@ endef
 
 define SONIC2013_POST_PROCESS
 	mkdir -p $(TARGET_DIR)/usr/share/evmapy $(TARGET_DIR)/usr/share/sonic2013
-	cp -f $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/ports/sonic2013/sonicretro.keys $(TARGET_DIR)/usr/share/evmapy
+	cp -f $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/ports/sonic2013/sonicretro.keys \
+	    $(TARGET_DIR)/usr/share/evmapy
 endef
 
 SONIC2013_POST_INSTALL_TARGET_HOOKS += SONIC2013_POST_PROCESS
