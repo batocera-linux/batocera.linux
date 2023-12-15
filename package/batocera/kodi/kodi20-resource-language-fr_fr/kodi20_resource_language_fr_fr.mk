c2f68877806817cfa1de9a947b072ac0f1634c89
dmanlfc <dmanlfc@gmail.com>
2023-12-15 22:03:25 +1000

    update kodi language files

--- package/batocera/kodi/kodi20-resource-language-fr_fr/kodi20_resource_language_fr_fr.mk
+++ package/batocera/kodi/kodi20-resource-language-fr_fr/kodi20_resource_language_fr_fr.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-KODI20_RESOURCE_LANGUAGE_FR_FR_VERSION = 10.0.75
+KODI20_RESOURCE_LANGUAGE_FR_FR_VERSION = 10.0.77
 KODI20_RESOURCE_LANGUAGE_FR_FR_SOURCE = resource.language.fr_fr-$(KODI20_RESOURCE_LANGUAGE_FR_FR_VERSION).zip
 KODI20_RESOURCE_LANGUAGE_FR_FR_SITE = http://mirrors.kodi.tv/addons/nexus/resource.language.fr_fr
 KODI20_RESOURCE_LANGUAGE_FR_FR_PLUGINNAME=resource.language.fr_fr
