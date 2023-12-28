bb6bedaa1aa783dd4643d6ec0017822edcbd810f
Daniel Martin <dmanlfc@gmail.com>
2023-12-28 11:40:44 +1000

    tidy gzdoom

--- package/batocera/ports/gzdoom/gzdoom.mk
+++ package/batocera/ports/gzdoom/gzdoom.mk
@@ -14,11 +14,13 @@ GZDOOM_SUPPORTS_IN_SOURCE_BUILD = NO
  
 # We need the tools from the host package to build the target package
 HOST_GZDOOM_DEPENDENCIES = zlib bzip2 host-webp
-HOST_GZDOOM_CONF_OPTS += -DTOOLS_ONLY=ON -DHAVE_VULKAN=ON -DSKIP_INSTALL_ALL=ON -DCMAKE_BUILD_TYPE=Release
+HOST_GZDOOM_CONF_OPTS += -DCMAKE_BUILD_TYPE=Release
+HOST_GZDOOM_CONF_OPTS += -DSKIP_INSTALL_ALL=ON
+HOST_GZDOOM_CONF_OPTS += -DTOOLS_ONLY=ON
 HOST_GZDOOM_SUPPORTS_IN_SOURCE_BUILD = NO
 
 define HOST_GZDOOM_INSTALL_CMDS
-	# Skipping install, the tools are used directly via `ImportExecutables.cmake` from the build directory.
+	# Skip install as we only need `ImportExecutables.cmake` from the build directory.
 endef
 
 GZDOOM_CONF_OPTS += -DCMAKE_BUILD_TYPE=Release
@@ -26,38 +28,17 @@ GZDOOM_CONF_OPTS += -DFORCE_CROSSCOMPILE=ON
 GZDOOM_CONF_OPTS += -DBUILD_SHARED_LIBS=OFF
 GZDOOM_CONF_OPTS += -DIMPORT_EXECUTABLES="$(HOST_GZDOOM_BUILDDIR)/ImportExecutables.cmake"
 
-# Copy the headers that are usually generated on the target machine
-# but must be provided when cross-compiling.
-ifeq ($(BR2_ARCH_IS_64),y)
-GZDOOM_GENERATED_HEADER_SUFFIX = 64
-else
-GZDOOM_GENERATED_HEADER_SUFFIX = 32
-endif
-
-define GZDOOM_COPY_GENERATED_HEADERS
-	mkdir -p $(GZDOOM_BUILDDIR)/libraries/gdtoa/
-	cp $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/ports/gzdoom/arith_$(GZDOOM_GENERATED_HEADER_SUFFIX).h \
-	    $(GZDOOM_BUILDDIR)/libraries/gdtoa/arith.h
-	cp $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/ports/gzdoom/gd_qnan_$(GZDOOM_GENERATED_HEADER_SUFFIX).h \
-	    $(GZDOOM_BUILDDIR)/libraries/gdtoa/gd_qnan.h
-endef
-
-GZDOOM_PRE_CONFIGURE_HOOKS += GZDOOM_COPY_GENERATED_HEADERS
-
 ifeq ($(BR2_PACKAGE_VULKAN_HEADERS)$(BR2_PACKAGE_VULKAN_LOADER),yy)
     GZDOOM_CONF_OPTS += -DHAVE_VULKAN=ON
     GZDOOM_DEPENDENCIES += vulkan-headers vulkan-loader
-ifeq ($(BR2_PACKAGE_XSERVER_XORG_SERVER),y)
-    GZDOOM_CONF_OPTS += -DVULKAN_USE_XLIB=ON -DVULKAN_USE_WAYLAND=OFF
-else
-ifeq ($(BR2_PACKAGE_WAYLAND)$(BR2_PACKAGE_SWAY),yy)
-    GZDOOM_CONF_OPTS += -DVULKAN_USE_XLIB=OFF -DVULKAN_USE_WAYLAND=ON
-endif
-endif
+    ifeq ($(BR2_PACKAGE_XSERVER_XORG_SERVER),y)
+        GZDOOM_CONF_OPTS += -DVULKAN_USE_XLIB=ON -DVULKAN_USE_WAYLAND=OFF
+    else ifeq ($(BR2_PACKAGE_WAYLAND)$(BR2_PACKAGE_SWAY),yy)
+        GZDOOM_CONF_OPTS += -DVULKAN_USE_XLIB=OFF -DVULKAN_USE_WAYLAND=ON
+    endif
 else
     GZDOOM_CONF_OPTS += -DHAVE_VULKAN=OFF
 endif
-
 
 # This applies the patches to actually use GLES2.
 # By default, gzdoom attempts to use GLES2 with an OpenGL context.
