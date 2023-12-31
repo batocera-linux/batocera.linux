4cda03412f6470aab04b62c4294bba5f36e7aae0
Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>
2023-12-31 14:13:12 +0000

    guncon3 calibration
    Signed-off-by: Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>

--- package/batocera/controllers/guncon3/guncon3.mk
+++ package/batocera/controllers/guncon3/guncon3.mk
@@ -11,10 +11,10 @@ GUNCON3_MODULE_SUBDIRS = src
 GUNCON3_SOURCE_PATH = $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/controllers/guncon3
 
 define GUNCON3_INSTALL_TARGET_CMDS
-    $(INSTALL) -m 0644 -D $(GUNCON3_SOURCE_PATH)/99-guncon3.rules \
-        $(TARGET_DIR)/etc/udev/rules.d/99-guncon3.rules
-    $(INSTALL) -m 0755 -D $(GUNCON3_SOURCE_PATH)/guncon3-add \
-        $(TARGET_DIR)/usr/bin/guncon3-add
+    $(INSTALL) -m 0644 -D $(GUNCON3_SOURCE_PATH)/99-guncon3.rules $(TARGET_DIR)/etc/udev/rules.d/99-guncon3.rules
+    $(INSTALL) -m 0755 -D $(GUNCON3_SOURCE_PATH)/guncon3-add $(TARGET_DIR)/usr/bin/guncon3-add
+    $(INSTALL) -m 0755 -D $(GUNCON3_SOURCE_PATH)/batocera-guncon3-calibrator $(TARGET_DIR)/usr/bin/batocera-guncon3-calibrator
+    $(INSTALL) -m 0755 -D $(GUNCON3_SOURCE_PATH)/batocera-guncon3-calibrator-daemon $(TARGET_DIR)/usr/bin/batocera-guncon3-calibrator-daemon
 endef
 
 $(eval $(kernel-module))
