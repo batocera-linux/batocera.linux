a3e551c49adbd945d418b3d69ad8fddf57231aaa
Daniel Martin <dmanlfc@gmail.com>
2024-01-15 22:48:41 +1000

    slim shady

--- package/batocera/emulators/retroarch/shaders/glsl-shaders/glsl-shaders.mk
+++ package/batocera/emulators/retroarch/shaders/glsl-shaders/glsl-shaders.mk
@@ -3,20 +3,27 @@
 # glsl-shaders
 #
 ################################################################################
-# Version: Commits on July 29, 2023
-GLSL_SHADERS_VERSION = b3e7982c0924658c1dddc47da4ee1270a1cb3187
+# Version: Commits on Jan 7, 2024
+GLSL_SHADERS_VERSION = bc8df92f013a128c3a735a57d3d98c6f087cc291
 GLSL_SHADERS_SITE = $(call github,libretro,glsl-shaders,$(GLSL_SHADERS_VERSION))
 GLSL_SHADERS_LICENSE = GPL
 
 define GLSL_SHADERS_BUILD_CMDS
-	CFLAGS="$(TARGET_CFLAGS)" CXXFLAGS="$(TARGET_CXXFLAGS)" $(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" -C $(@D)/ -f Makefile
+	CFLAGS="$(TARGET_CFLAGS)" \
+	CXXFLAGS="$(TARGET_CXXFLAGS)" \
+	$(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" -C $(@D)/ -f Makefile
 endef
 
 define GLSL_SHADERS_INSTALL_TARGET_CMDS
 	mkdir -p $(TARGET_DIR)/usr/share/batocera/shaders
-	$(MAKE) CXX="$(TARGET_CXX)" -C $(@D) INSTALLDIR=$(TARGET_DIR)/usr/share/batocera/shaders install
-	sed -e "s:^//#define CURVATURE:#define CURVATURE:" $(TARGET_DIR)/usr/share/batocera/shaders/crt/shaders/crt-pi.glsl > $(TARGET_DIR)/usr/share/batocera/shaders/crt/shaders/crt-pi-curvature.glsl
-	sed -e 's:^shader0 = "shaders/crt-pi.glsl":shader0 = "shaders/crt-pi-curvature.glsl":' $(TARGET_DIR)/usr/share/batocera/shaders/crt/crt-pi.glslp > $(TARGET_DIR)/usr/share/batocera/shaders/crt/crt-pi-curvature.glslp
+	$(MAKE) CXX="$(TARGET_CXX)" -C $(@D) \
+	    INSTALLDIR=$(TARGET_DIR)/usr/share/batocera/shaders install
+	sed -e "s:^//#define CURVATURE:#define CURVATURE:" \
+	    $(TARGET_DIR)/usr/share/batocera/shaders/crt/shaders/crt-pi.glsl > \
+		    $(TARGET_DIR)/usr/share/batocera/shaders/crt/shaders/crt-pi-curvature.glsl
+	sed -e 's:^shader0 = "shaders/crt-pi.glsl":shader0 = "shaders/crt-pi-curvature.glsl":' \
+	    $(TARGET_DIR)/usr/share/batocera/shaders/crt/crt-pi.glslp > \
+		    $(TARGET_DIR)/usr/share/batocera/shaders/crt/crt-pi-curvature.glslp
 endef
 
 $(eval $(generic-package))
