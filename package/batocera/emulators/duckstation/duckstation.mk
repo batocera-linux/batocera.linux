1c5f16203c3e0a7dd390b4a5112494c154e9ffc4
Daniel Martin <dmanlfc@gmail.com>
2024-01-19 09:44:55 +1000

    bump duckstation to 10th of Jan build

--- package/batocera/emulators/duckstation/duckstation.mk
+++ package/batocera/emulators/duckstation/duckstation.mk
@@ -3,8 +3,8 @@
 # duckstation
 #
 ################################################################################
-# Version: Commits on Dec 27, 2023
-DUCKSTATION_VERSION = 6d07d709468c25eb92e88bab867080bad1655403
+# Version: Commits on Jan 14, 2024
+DUCKSTATION_VERSION = 572ea80e50f83b81328b7fc1b70d71fb6a74cd90
 DUCKSTATION_SITE = https://github.com/stenzek/duckstation.git
 DUCKSTATION_SITE_METHOD=git
 DUCKSTATION_GIT_SUBMODULES=YES
