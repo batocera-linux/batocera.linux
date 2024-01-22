27565176f746128e377ac7db7dfca59ce40655ea
Daniel Martin <dmanlfc@gmail.com>
2024-01-22 20:30:42 +1000

    fix prime for wow64 & wine tidy-up

--- package/batocera/core/batocera-configgen/configgen/configgen/generators/wine/wineGenerator.py
+++ package/batocera/core/batocera-configgen/configgen/configgen/generators/wine/wineGenerator.py
@@ -45,8 +45,7 @@ class WineGenerator(Generator):
 
                 environment.update(
                     {
-                        'VK_ICD_FILENAMES': '/usr/share/vulkan/icd.d/nvidia_icd.x86_64.json',
-                        'VK_LAYER_PATH': '/usr/share/vulkan/explicit_layer.d'
+                        'VK_ICD_FILENAMES': '/usr/share/vulkan/icd.d/nvidia_icd.x86_64.json:/usr/share/vulkan/icd.d/nvidia_icd.i686.json',
                     }
                 )
             
