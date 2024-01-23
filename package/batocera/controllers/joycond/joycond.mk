bbc54b114c4fa6f97b9af538a6bbf0828d9e1819
Daniel Martin <dmanlfc@gmail.com>
2024-01-23 22:10:12 +1000

    bump joycond

--- package/batocera/controllers/joycond/joycond.mk
+++ package/batocera/controllers/joycond/joycond.mk
@@ -3,8 +3,8 @@
 # joycond
 #
 ################################################################################
-# Version: Commits on Jul 21, 2022
-JOYCOND_VERSION = 5b590ecc9bca181d8bc21377e752126bc9180319
+# Version: Commits on Dec 4, 2023
+JOYCOND_VERSION = 9d1f5098b716681d087cca695ad714218a18d4e8
 JOYCOND_SITE = $(call github,DanielOgorchock,joycond,$(JOYCOND_VERSION))
 JOYCOND_LICENSE = GPL-3.0+
 JOYCOND_LICENSE_FILES = LICENSE
@@ -13,6 +13,8 @@ JOYCOND_DEPENDENCIES = acl libevdev udev
 define JOYCOND_INSTALL_TARGET_CMDS
     $(INSTALL) -D -m 0755 $(@D)/joycond $(TARGET_DIR)/usr/bin
     $(INSTALL) -D -m 0644 $(@D)/udev/*.rules $(TARGET_DIR)/etc/udev/rules.d/
+    $(INSTALL) -D -m 0644 $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/controllers/joycond/99-joycond-ignore.rules \
+        $(TARGET_DIR)/etc/udev/rules.d/
 endef
 
 $(eval $(cmake-package))
