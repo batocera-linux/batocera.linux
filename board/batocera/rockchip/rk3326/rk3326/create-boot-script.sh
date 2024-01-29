ef3e234e103094af78bd6ce61230e09c4a37ac1a
Juliano Dorigão <jdorigao@gmail.com>
2024-01-29 07:25:15 -0400

    rk3326: create-boot-script

--- board/batocera/rockchip/rk3326/rk3326/create-boot-script.sh
+++ board/batocera/rockchip/rk3326/rk3326/create-boot-script.sh
@@ -18,14 +18,13 @@ BATOCERA_BINARIES_DIR=$6
 mkdir -p "${BATOCERA_BINARIES_DIR}/boot/boot" || exit 1
 
 "${HOST_DIR}/bin/mkimage" -A arm64 -O linux -T kernel -C none -a 0x1080000 -e 0x1080000 -n 5.x -d "${BINARIES_DIR}/Image" "${BATOCERA_BINARIES_DIR}/boot/boot/linux" || exit 1
-cp "${BINARIES_DIR}/uInitrd"         "${BATOCERA_BINARIES_DIR}/boot/boot/uInitrd"         || exit 1
-cp "${BINARIES_DIR}/rootfs.squashfs" "${BATOCERA_BINARIES_DIR}/boot/boot/batocera.update" || exit 1
+cp "${BINARIES_DIR}/uInitrd"         "${BATOCERA_BINARIES_DIR}/boot/boot/uInitrd"           || exit 1
+cp "${BINARIES_DIR}/rootfs.squashfs" "${BATOCERA_BINARIES_DIR}/boot/boot/batocera.update"   || exit 1
 
-cp "${BINARIES_DIR}/rk3326-rg351p-linux.dtb"        "${BATOCERA_BINARIES_DIR}/boot/boot/rk3326-rg351p-linux.dtb"        || exit 1
-cp "${BINARIES_DIR}/rk3326-odroidgo2-linux.dtb"     "${BATOCERA_BINARIES_DIR}/boot/boot/rk3326-odroidgo2-linux.dtb"     || exit 1
-cp "${BINARIES_DIR}/rk3326-odroidgo2-linux-v11.dtb" "${BATOCERA_BINARIES_DIR}/boot/boot/rk3326-odroidgo2-linux-v11.dtb" || exit 1
-cp "${BINARIES_DIR}/rk3326-odroidgo3-linux.dtb"     "${BATOCERA_BINARIES_DIR}/boot/boot/rk3326-odroidgo3-linux.dtb"     || exit 1
-
-cp "${BOARD_DIR}/boot/boot.ini"                     "${BATOCERA_BINARIES_DIR}/boot/"                                    || exit 1
+cp "${BINARIES_DIR}/rk3326-anbernic-rg351m.dtb" "${BATOCERA_BINARIES_DIR}/boot/boot/"       || exit 1
+cp "${BINARIES_DIR}/rk3326-odroid-go2.dtb"      "${BATOCERA_BINARIES_DIR}/boot/boot/"       || exit 1
+cp "${BINARIES_DIR}/rk3326-odroid-go2-v11.dtb"  "${BATOCERA_BINARIES_DIR}/boot/boot/"       || exit 1
+cp "${BINARIES_DIR}/rk3326-odroid-go3.dtb"      "${BATOCERA_BINARIES_DIR}/boot/boot/"       || exit 1
+cp "${BOARD_DIR}/boot/boot.ini"                 "${BATOCERA_BINARIES_DIR}/boot/"            || exit 1
 
 exit 0
