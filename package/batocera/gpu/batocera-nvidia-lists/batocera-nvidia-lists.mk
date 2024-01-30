15cb0883ea773686625c8445a5563c860f6b0c0a
Daniel Martin <dmanlfc@gmail.com>
2024-01-30 23:20:48 +1000

    update nv list

--- package/batocera/gpu/batocera-nvidia-lists/batocera-nvidia-lists.mk
+++ package/batocera/gpu/batocera-nvidia-lists/batocera-nvidia-lists.mk
@@ -3,8 +3,8 @@
 # batocera-nvidia-lists
 #
 ################################################################################
-# Version: Commits on Jan 18, 2024
-BATOCERA_NVIDIA_LISTS_VERSION = 1711cbab07b6ac639439adcf75d86d8532afbb47
+# Version: Commits on Jan 30, 2024
+BATOCERA_NVIDIA_LISTS_VERSION = cd010693fd3ddd5d218944f47493925367735b79
 BATOCERA_NVIDIA_LISTS_SITE = $(call github,batocera-linux,nvidia-lists,$(BATOCERA_NVIDIA_LISTS_VERSION))
 BATOCERA_NVIDIA_LISTS_LICENSE = GPL-3.0+
 BATOCERA_NVIDIA_LISTS_LICENSE_FILES = LICENSE
