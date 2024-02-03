a348e6a2f6a984255bb9ea5097e7328aee253466
Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>
2024-02-03 16:29:19 +0100

    0 dz for mupen core wheels by default
    Signed-off-by: Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>

--- package/batocera/core/batocera-configgen/configgen/configgen/generators/libretro/libretroOptions.py
+++ package/batocera/core/batocera-configgen/configgen/configgen/generators/libretro/libretroOptions.py
@@ -1173,7 +1173,10 @@ def generateCoreSettings(coreSettings, system, rom, guns, wheels):
         if system.isOptSet('mupen64plus-deadzone'):
             coreSettings.save('mupen64plus-astick-deadzone', '"' + system.config['mupen64plus-deadzone'] + '"')
         else:
-            coreSettings.save('mupen64plus-astick-deadzone', '"15"')
+            if system.isOptSet('use_wheels') and system.getOptBoolean('use_wheels') and len(wheels) > 0:
+                coreSettings.save('mupen64plus-astick-deadzone', '"0"')
+            else:
+                coreSettings.save('mupen64plus-astick-deadzone', '"15"')
             
         # Joystick sensitivity
         if system.isOptSet('mupen64plus-sensitivity'):
@@ -1275,7 +1278,10 @@ def generateCoreSettings(coreSettings, system, rom, guns, wheels):
         if system.isOptSet('parallel-n64-deadzone'):
             coreSettings.save('parallel-n64-astick-deadzone', '"' + system.config['parallel-n64-deadzone'] + '"')
         else:
-            coreSettings.save('parallel-n64-astick-deadzone', '"15"')
+            if system.isOptSet('use_wheels') and system.getOptBoolean('use_wheels') and len(wheels) > 0:
+                coreSettings.save('parallel-n64-astick-deadzone', '"0"')
+            else:
+                coreSettings.save('parallel-n64-astick-deadzone', '"15"')
             
         # Joystick sensitivity
         if system.isOptSet('parallel-n64-sensitivity'):
