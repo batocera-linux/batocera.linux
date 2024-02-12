e6674b081b480bae8ebd2a25bc0a36e5f87a0b4a
Juliano Dorigão <jdorigao@gmail.com>
2024-02-12 12:56:16 -0400

    libliftoff: bump to 0.4.1
    Fix -Wsign-conversion on 32-bit

--- package/batocera/libraries/libliftoff/libliftoff.mk
+++ package/batocera/libraries/libliftoff/libliftoff.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBLIFTOFF_VERSION = 0.4.0
+LIBLIFTOFF_VERSION = 0.4.1
 LIBLIFTOFF_SITE = https://gitlab.freedesktop.org/emersion/libliftoff/-/releases/v$(LIBLIFTOFF_VERSION)/downloads
 LIBLIFTOFF_LICENSE = MIT
 LIBLIFTOFF_LICENSE_FILES = LICENSE
