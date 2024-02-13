ef74fd5065eb9e16f0832bfaee8c716ecb78608a
Lbrpdx <lionel.bringuier@gmail.com>
2024-02-13 07:25:37 -0800

    Update ES theme Carbon for v39

--- package/batocera/emulationstation/es-theme-carbon/es-theme-carbon.mk
+++ package/batocera/emulationstation/es-theme-carbon/es-theme-carbon.mk
@@ -3,8 +3,8 @@
 # EmulationStation theme "Carbon"
 #
 ################################################################################
-# Version.: Commits on Jan 31, 2024
-ES_THEME_CARBON_VERSION = fa4b29a9a8fb495b33f840d2959ffe34724e7a57
+# Version.: Commits on Feb 13, 2024
+ES_THEME_CARBON_VERSION = 6b868454cad63296a3f36b1d3c5307d60f23ac2b
 ES_THEME_CARBON_SITE = $(call github,fabricecaruso,es-theme-carbon,$(ES_THEME_CARBON_VERSION))
 
 define ES_THEME_CARBON_INSTALL_TARGET_CMDS
