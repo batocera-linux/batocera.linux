9720521ac05e314b050904d1f31373295fe664c2
Juliano Dorigão <jdorigao@gmail.com>
2024-02-23 20:44:31 -0400

    mupen64plus-audio-sdl: bump to Jun 11, 2023 build

--- package/batocera/emulators/mupen64plus/mupen64plus-audio-sdl/mupen64plus-audio-sdl.mk
+++ package/batocera/emulators/mupen64plus/mupen64plus-audio-sdl/mupen64plus-audio-sdl.mk
@@ -3,8 +3,8 @@
 # mupen64plus-audio-sdl
 #
 ################################################################################
-# Version.: Commits on Mar 03, 2023
-MUPEN64PLUS_AUDIO_SDL_VERSION = 8d5df662e91cae8c4ad327d67a2ccaffc486a88c
+# Version.: Commits on Jun 11, 2023
+MUPEN64PLUS_AUDIO_SDL_VERSION = cec70e42b4535ce5bd1be24e7d816c54b3e75673
 MUPEN64PLUS_AUDIO_SDL_SITE = $(call github,mupen64plus,mupen64plus-audio-sdl,$(MUPEN64PLUS_AUDIO_SDL_VERSION))
 MUPEN64PLUS_AUDIO_SDL_LICENSE = GPLv2
 MUPEN64PLUS_AUDIO_SDL_DEPENDENCIES = sdl2 alsa-lib mupen64plus-core
