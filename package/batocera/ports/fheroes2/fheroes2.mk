e12dd935a05fe75a8db853ec16d8b753c942ebcd
Romain Tisserand <romain.tisserand@gmail.com>
2024-03-04 13:10:10 +0100

    Clean and bump experimental cores

--- package/batocera/ports/fheroes2/fheroes2.mk
+++ package/batocera/ports/fheroes2/fheroes2.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-FHEROES2_VERSION = 1.0.9
+FHEROES2_VERSION = 1.0.12
 FHEROES2_SITE = $(call github,ihhub,fheroes2,$(FHEROES2_VERSION))
 FHEROES2_DEPENDENCIES = sdl2 sdl2_image
 
