939ec93121420e7fe7907eaf1ad9f2fdb82b0ed9
Daniel Martin <dmanlfc@gmail.com>
2024-03-13 06:59:39 +1000

    update tests

--- package/batocera/core/batocera-desktopapps/batocera-desktopapps.mk
+++ package/batocera/core/batocera-desktopapps/batocera-desktopapps.mk
@@ -110,11 +110,11 @@ ifeq ($(BR2_PACKAGE_BAUH),y)
   BATOCERA_DESKTOPAPPS_APPS    += flatpak-config.desktop
 endif
 
-# yuzu
-ifeq ($(BR2_PACKAGE_YUZU),y)
-  BATOCERA_DESKTOPAPPS_SCRIPTS += batocera-config-yuzu
-  BATOCERA_DESKTOPAPPS_APPS    += yuzu-config.desktop
-  BATOCERA_DESKTOPAPPS_ICONS   += yuzu.png
+# suyu
+ifeq ($(BR2_PACKAGE_SUYU),y)
+  BATOCERA_DESKTOPAPPS_SCRIPTS += batocera-config-suyu
+  BATOCERA_DESKTOPAPPS_APPS    += suyu-config.desktop
+  BATOCERA_DESKTOPAPPS_ICONS   += suyu.png
 endif
 
 # ryujinx
