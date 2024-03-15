5770edca68b9508550e32c394eb8b221e18267ce
Daniel Martin <dmanlfc@gmail.com>
2024-03-15 14:18:29 +1000

    overwrite when dirty building

--- package/batocera/emulators/wine-x86/wine-x86.mk
+++ package/batocera/emulators/wine-x86/wine-x86.mk
@@ -4,17 +4,17 @@
 #
 ################################################################################
 
-# version 5.18 means binary from 5.18 version (or the last built if the version is not yet out)
 WINE_X86_VERSION = $(BATOCERA_SYSTEM_VERSION)
 WINE_X86_SOURCE = wine-x86-$(WINE_X86_VERSION).tar.lzma
 WINE_X86_SITE = https://github.com/batocera-linux/wine-x86/releases/download/$(WINE_X86_VERSION)
 
 define WINE_X86_EXTRACT_CMDS
-	mkdir -p $(@D)/target && cd $(@D)/target && tar xf $(DL_DIR)/$(WINE_X86_DL_SUBDIR)/$(WINE_X86_SOURCE)
+	mkdir -p $(@D)/target && cd $(@D)/target && \
+	    tar xf $(DL_DIR)/$(WINE_X86_DL_SUBDIR)/$(WINE_X86_SOURCE)
 endef
 
 define WINE_X86_INSTALL_TARGET_CMDS
-	cp -prn $(@D)/target/* $(TARGET_DIR)
+	cp -prf $(@D)/target/* $(TARGET_DIR)
 endef
 
 $(eval $(generic-package))
