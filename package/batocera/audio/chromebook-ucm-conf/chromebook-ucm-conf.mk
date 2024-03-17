b3fa3b11b952979fd0231ed617c5572e1914c968
Daniel Martin <dmanlfc@gmail.com>
2024-03-17 16:42:28 +1000

    changed chrombook ucm source

--- package/batocera/audio/chromebook-ucm-conf/chromebook-ucm-conf.mk
+++ package/batocera/audio/chromebook-ucm-conf/chromebook-ucm-conf.mk
@@ -3,9 +3,9 @@
 # chromebook-ucm-conf
 #
 ################################################################################
-# Version: Commits on Oct 9, 2023
-CHROMEBOOK_UCM_CONF_VERSION = 5fccbeda9f94ebb57f60bdb8fd0bac7e8970711d
-CHROMEBOOK_UCM_CONF_SITE = $(call github,WeirdTreeThing,chromebook-ucm-conf,$(CHROMEBOOK_UCM_CONF_VERSION))
+# Version: Commits on Feb 8, 2024 (use Standalone branch)
+CHROMEBOOK_UCM_CONF_VERSION = 6b395ae73ac63407d8a9892fe1290f191eb0315b
+CHROMEBOOK_UCM_CONF_SITE = $(call github,WeirdTreeThing,alsa-ucm-conf-cros,$(CHROMEBOOK_UCM_CONF_VERSION))
 CHROMEBOOK_UCM_CONF_LICENSE = BSD-3-Clause
 CHROMEBOOK_UCM_CONF_LICENSE_FILES = LICENSE
 
@@ -14,31 +14,7 @@ CHROMEBOOK_UCM_CONF_DEPENDENCIES += alsa-ucm-conf alsa-utils
 CHROMEBOOK_UCM_CONF_DEPENDENCIES += alllinuxfirmwares sound-open-firmware
 
 define CHROMEBOOK_UCM_CONF_INSTALL_TARGET_CMDS
-    ## Intel systems
-    # appollo lake
-    rsync -arv $(@D)/apl/* $(TARGET_DIR)/usr/share/alsa/ucm2/conf.d/
-    # gemini lake
-    rsync -arv $(@D)/glk/* $(TARGET_DIR)/usr/share/alsa/ucm2/conf.d/
-    # comet lake
-    rsync -arv $(@D)/cml/* $(TARGET_DIR)/usr/share/alsa/ucm2/conf.d/
-    # tigerlake
-    rsync -arv $(@D)/tgl/* $(TARGET_DIR)/usr/share/alsa/ucm2/conf.d/
-    # jasper lake
-    rsync -arv $(@D)/jsl/* $(TARGET_DIR)/usr/share/alsa/ucm2/conf.d/
-    # alder lake
-    rsync -arv $(@D)/adl/* $(TARGET_DIR)/usr/share/alsa/ucm2/conf.d/
-    ## AMD systems
-    rsync -arv $(@D)/stoney/* $(TARGET_DIR)/usr/share/alsa/ucm2/conf.d/
-    rsync -arv $(@D)/picasso/* $(TARGET_DIR)/usr/share/alsa/ucm2/conf.d/
-    rsync -arv $(@D)/cezanne/* $(TARGET_DIR)/usr/share/alsa/ucm2/conf.d/
-    rsync -arv $(@D)/mendocino/* $(TARGET_DIR)/usr/share/alsa/ucm2/conf.d/
-    ## AVS
-    rsync -arv $(@D)/avs/* $(TARGET_DIR)/usr/share/alsa/ucm2/conf.d/
-    ## Common files
-    # dmic-common
-    rsync -arv $(@D)/dmic-common/* $(TARGET_DIR)/usr/share/alsa/ucm2/conf.d/dmic-common/
-    # hdmi-common
-    rsync -arv $(@D)/hdmi-common/* $(TARGET_DIR)/usr/share/alsa/ucm2/conf.d/hdmi-common/
+    rsync -arv $(@D)/ucm2/* $(TARGET_DIR)/usr/share/alsa/ucm2/
 endef
 
 $(eval $(generic-package))
