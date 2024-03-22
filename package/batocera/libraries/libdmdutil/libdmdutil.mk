a2e9a8043b1e5fd8ff31014b01038429b228bcaf
Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>
2024-03-22 19:26:55 +0100

    update libdmd
    Signed-off-by: Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>

--- package/batocera/libraries/libdmdutil/libdmdutil.mk
+++ package/batocera/libraries/libdmdutil/libdmdutil.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 # Version: Commits on Mar 11, 2024
-LIBDMDUTIL_VERSION = 988793a60b6d9706999ee6b70b55d95716db9950
+LIBDMDUTIL_VERSION = 8c19c99aa5adecc97af709f534f9ecf624c5cf7c
 LIBDMDUTIL_SITE = $(call github,vpinball,libdmdutil,$(LIBDMDUTIL_VERSION))
 LIBDMDUTIL_LICENSE = BSD-3-Clause
 LIBDMDUTIL_LICENSE_FILES = LICENSE
