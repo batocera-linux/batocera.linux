4e76e9e151fd363a3a4b42a7db5c5a01a3c0f717
Daniel Martin <dmanlfc@gmail.com>
2024-03-29 11:40:11 +1000

    allow multiple custom wine runners

--- package/batocera/utils/batocera-wine/batocera-wine.mk
+++ package/batocera/utils/batocera-wine/batocera-wine.mk
@@ -11,6 +11,8 @@ BATOCERA_WINE_SOURCE=
 define BATOCERA_WINE_INSTALL_TARGET_CMDS
 	install -m 0755 $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/utils/batocera-wine/batocera-wine \
 	    $(TARGET_DIR)/usr/bin/batocera-wine
+	install -m 0755 $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/utils/batocera-wine/batocera-wine-runners \
+	    $(TARGET_DIR)/usr/bin/batocera-wine-runners
 	install -m 0755 $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/utils/batocera-wine/bsod.py \
 	    $(TARGET_DIR)/usr/bin/bsod-wine
 	ln -fs /userdata/system/99-nvidia.conf $(TARGET_DIR)/etc/X11/xorg.conf.d/99-nvidia.conf
