6b67199b1f6514c2d9069abeb33f2d27ca119095
Daniel Martin <dmanlfc@gmail.com>
2024-04-01 17:22:53 +1000

    pcsx2 to v1.7.5663

--- package/batocera/emulators/pcsx2/pcsx2.mk
+++ package/batocera/emulators/pcsx2/pcsx2.mk
@@ -3,8 +3,8 @@
 # pcsx2
 #
 ################################################################################
-#Version: Commits on Jan 29, 2024
-PCSX2_VERSION = v1.7.5531
+#Version: Commits on Apr 1, 2024
+PCSX2_VERSION = v1.7.5663
 PCSX2_SITE = https://github.com/pcsx2/pcsx2.git
 PCSX2_SITE_METHOD = git
 PCSX2_GIT_SUBMODULES = YES
