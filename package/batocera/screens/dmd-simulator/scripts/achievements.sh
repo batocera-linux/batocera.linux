0a9f9cdfb7813ea3c3c8024ec5957a1d10880749
Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>
2024-04-06 10:34:25 +0200

    fix achivement overlay
    Signed-off-by: Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>

--- package/batocera/screens/dmd-simulator/scripts/achievements.sh
+++ package/batocera/screens/dmd-simulator/scripts/achievements.sh
@@ -13,12 +13,12 @@ do
     CUS="/userdata/system/dmd/achievement.${EXT}"
     if test -e "${CUS}"
     then
-	dmd-play ${DMDOPT} -f "${CUS}"
+	dmd-play ${DMDOPT} -f "${CUS}" --once --overlay
 	exit 0
     fi
 done
 
 # fallback : empty
-dmd-play ${DMDOPT} -t "WOUHOU !" --once --moving-text --no-fit
+dmd-play ${DMDOPT} -t "WOUHOU !" --once --moving-text --no-fit --overlay
 
 exit 0
