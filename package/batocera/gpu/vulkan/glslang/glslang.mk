7a34777968b45d0c0ddb60b13ead3f18e404f41d
Daniel Martin <dmanlfc@gmail.com>
2024-04-06 20:03:53 +1000

    glslang update for vulkan

--- package/batocera/gpu/vulkan/glslang/glslang.mk
+++ package/batocera/gpu/vulkan/glslang/glslang.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-GLSLANG_VERSION = 14.0.0
+GLSLANG_VERSION = 14.1.0
 GLSLANG_SITE =  https://github.com/KhronosGroup/glslang
 GLSLANG_SITE_METHOD=git
 GLSLANG_DEPENDENCIES = vulkan-headers vulkan-loader spirv-tools
