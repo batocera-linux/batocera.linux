e4916b2b9583375a6bbdd6b78d2241a3fd0cf72c
Daniel Martin <dmanlfc@gmail.com>
2024-05-09 23:35:59 +1000

    bump xpadneo to Apr 23, 2024 build.

--- package/batocera/controllers/xpadneo/xpadneo.mk
+++ package/batocera/controllers/xpadneo/xpadneo.mk
@@ -3,8 +3,8 @@
 # xpadneo
 #
 ################################################################################
-# Version: Commits on Mar 11, 2024
-XPADNEO_VERSION = be25f7ba836fba99def5d7bc85fcf26696a2fbf9
+# Version: Commits on Apr 23, 2024
+XPADNEO_VERSION = 73be2eb22ee7b3b3f66b01ad8b9aaa4edc9b93b5
 XPADNEO_SITE = $(call github,atar-axis,xpadneo,$(XPADNEO_VERSION))
 XPADNEO_DEPENDENCIES = host-libcurl host-cabextract bluez5_utils
 XPADNEO_MODULE_SUBDIRS = hid-xpadneo/src
@@ -18,6 +18,7 @@ define XPADNEO_INSTALL_TARGET_CMDS
 	cp -v $(@D)/hid-xpadneo/etc-udev-rules.d/*.rules $(TARGET_DIR)/etc/udev/rules.d/
 	cp -v $(@D)/hid-xpadneo/etc-modprobe.d/*.conf $(TARGET_DIR)/etc/modprobe.d/
 	echo "options bluetooth disable_ertm=1" >> $(TARGET_DIR)/etc/modprobe.d/xpadneo.conf
+	echo "options disable_shift_mode=1" >> $(TARGET_DIR)/etc/modprobe.d/xpadneo.conf
 endef
 
 $(eval $(kernel-module))
