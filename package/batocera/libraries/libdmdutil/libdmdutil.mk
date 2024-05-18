5b8d61edca60ae5cd3027943d605cd74eef441a1
Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>
2024-05-18 12:53:24 +0000

    fix pixelcade events
    Signed-off-by: Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>

--- package/batocera/libraries/libdmdutil/libdmdutil.mk
+++ package/batocera/libraries/libdmdutil/libdmdutil.mk
@@ -39,7 +39,9 @@ define LIBDMDUTIL_INSTALL_SERVER
    mkdir -p $(TARGET_DIR)/usr/share/batocera/services
    install -m 0755 $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/libraries/libdmdutil/dmd_server.service $(TARGET_DIR)/usr/share/batocera/services/dmd_real
    install -m 0755 $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/libraries/libdmdutil/dmdserver-config.py $(TARGET_DIR)/usr/bin/dmdserver-config
+   # pixelcade
    install -m 0644 -D $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/libraries/libdmdutil/99-pixelcade.rules $(TARGET_DIR)/etc/udev/rules.d/99-pixelcade.rules
+   $(INSTALL) -m 0755 -D $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/libraries/libdmdutil/pixelcade-add   $(TARGET_DIR)/usr/bin/pixelcade-add
 endef
 
 LIBDMDUTIL_POST_INSTALL_TARGET_HOOKS += LIBDMDUTIL_INSTALL_SERVER
