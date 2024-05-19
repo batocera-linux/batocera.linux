19da2495648f3987cc9dd65e47f52ebc1950e255
Daniel Martin <dmanlfc@gmail.com>
2024-05-19 11:03:42 +1000

    bump lr-fake08 to Apr 17, 2024 build

--- batocera-Changelog.md
+++ batocera-Changelog.md
@@ -141,6 +141,7 @@ Add support for the Ayaneo Air Plus (6800U) model
 - Libretro-WASM4 to v2.7.0
 - Libretro-Tic80 to May 16, 2024 build
 - Libretro-Chailove to May 18, 2024 build
+- Libretro-Fake08 to Apr 17, 2024 build
 ### System
 - Mainline Kernel 6.6.y bump to 6.6.23
 - Nvidia Production driver to 550.78
--- package/batocera/emulators/retroarch/libretro/libretro-fake08/libretro-fake08.mk
+++ package/batocera/emulators/retroarch/libretro/libretro-fake08/libretro-fake08.mk
@@ -3,16 +3,16 @@
 # libretro-fake08
 #
 ################################################################################
-# Version: Commits on Jan 28, 2024
-LIBRETRO_FAKE08_VERSION = 5a480cbf1f8ab1ef435f14d1206009f5fd56e588
+# Version: Commits on Apr 17, 2024
+LIBRETRO_FAKE08_VERSION = d98376fc41c34e3cd6d3ffc25ae9c2970efd5286
 LIBRETRO_FAKE08_SITE_METHOD=git
 LIBRETRO_FAKE08_SITE=https://github.com/jtothebell/fake-08
 LIBRETRO_FAKE08_LICENSE = MIT
 LIBRETRO_FAKE08_GIT_SUBMODULES=YES
 
 define LIBRETRO_FAKE08_BUILD_CMDS
-	# $(TARGET_CONFIGURE_OPTS) $(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" -C $(@D) -f Makefile sdl2
-	$(TARGET_CONFIGURE_OPTS) $(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" -C $(@D)/platform/libretro -f Makefile platform="unix"
+	$(TARGET_CONFIGURE_OPTS) $(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" \
+	    -C $(@D)/platform/libretro -f Makefile platform="unix"
 endef
 
 define LIBRETRO_FAKE08_INSTALL_TARGET_CMDS
