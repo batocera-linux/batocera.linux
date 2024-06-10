0335aeb0cfb4a61ba67ecec4d110014c724e57b7
Jerome Audu <jau@free.fr>
2024-06-10 13:29:04 +0200

    Improve autoreconf process
    Instead of using a manual ./autogen.sh call with FOO_PRE_CONFGIURE_HOOKS
    use the Buildroot configuration variable FOO_AUTORECONF = YES
    
    So build process will add autoconf dependencies and use the correct
    path automagically.

--- package/batocera/utils/re2c/re2c.mk
+++ package/batocera/utils/re2c/re2c.mk
@@ -6,13 +6,7 @@
 
 RE2C_VERSION = 2.2
 RE2C_SITE =  $(call github,skvadrik,re2c,$(RE2C_VERSION))
-
-define RE2C_AUTOGEN
-    cd $(@D); ./autogen.sh
-endef
-
-RE2C_PRE_CONFIGURE_HOOKS += RE2C_AUTOGEN
-HOST_RE2C_PRE_CONFIGURE_HOOKS += RE2C_AUTOGEN
+RE2C_AUTORECONF = YES
 
 $(eval $(autotools-package))
 $(eval $(host-autotools-package))
