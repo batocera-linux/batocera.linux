fd0844a498327aa0af8ad94f21fa992333968898
Daniel Martin <dmanlfc@gmail.com>
2024-06-16 00:30:21 +1000

    fix dependency build issues

--- package/batocera/libraries/libliftoff/libliftoff.mk
+++ package/batocera/libraries/libliftoff/libliftoff.mk
@@ -5,9 +5,13 @@
 ################################################################################
 
 LIBLIFTOFF_VERSION = 0.4.1
-LIBLIFTOFF_SITE = https://gitlab.freedesktop.org/emersion/libliftoff/-/releases/v$(LIBLIFTOFF_VERSION)/downloads
+LIBLIFTOFF_SITE = \
+    https://gitlab.freedesktop.org/emersion/libliftoff/-/releases/v$(LIBLIFTOFF_VERSION)/downloads
 LIBLIFTOFF_LICENSE = MIT
 LIBLIFTOFF_LICENSE_FILES = LICENSE
+
+LIBLIFTOFF_DEPENDENCIES = libdrm
+
 LIBLIFTOFF_INSTALL_STAGING = YES
 
 $(eval $(meson-package))
