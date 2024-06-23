40f8fa976f1f19a402e9b5d312c09efc56a8bbda
Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>
2024-06-23 20:50:03 +0000

    mangohud bump
    Signed-off-by: Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>

--- package/batocera/utils/mangohud/mangohud.mk
+++ package/batocera/utils/mangohud/mangohud.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 # Version: Commits from Oct, 2021
-MANGOHUD_VERSION = a8a0a245e69fbbca5263d2436fd1c04289375498
+MANGOHUD_VERSION = 12620c91eaca0917a7939a92ec33915cadf24475
 MANGOHUD_SITE =  $(call github,flightlessmango,MangoHud,$(MANGOHUD_VERSION))
 
 MANGOHUD_DEPENDENCIES = host-libcurl host-python-mako host-glslang
@@ -43,12 +43,14 @@ endif
 # use submodule vulkan headers - https://github.com/flightlessmango/MangoHud/issues/968
 define MANGOHUD_DWD_DEPENDENCIES
 	mkdir -p $(@D)/subprojects/packagecache
-	$(HOST_DIR)/bin/curl -L https://github.com/ocornut/imgui/archive/v1.81.tar.gz                  -o $(@D)/subprojects/packagecache/imgui-1.81.tar.gz
-	$(HOST_DIR)/bin/curl -L https://wrapdb.mesonbuild.com/v2/imgui_1.81-1/get_patch                -o $(@D)/subprojects/packagecache/imgui-1.81-1-wrap.zip
-	$(HOST_DIR)/bin/curl -L https://github.com/gabime/spdlog/archive/v1.8.5.tar.gz                 -o $(@D)/subprojects/packagecache/v1.8.5.tar.gz
-	$(HOST_DIR)/bin/curl -L https://wrapdb.mesonbuild.com/v2/spdlog_1.8.5-1/get_patch              -o $(@D)/subprojects/packagecache/spdlog-1.8.5-1-wrap.zip
+	$(HOST_DIR)/bin/curl -L https://github.com/ocornut/imgui/archive/refs/tags/v1.89.9.tar.gz      -o $(@D)/subprojects/packagecache/imgui-1.89.9.tar.gz
+	$(HOST_DIR)/bin/curl -L https://wrapdb.mesonbuild.com/v2/imgui_1.89.9-1/get_patch              -o $(@D)/subprojects/packagecache/imgui_1.89.9-1_patch.zip
+	$(HOST_DIR)/bin/curl -L https://github.com/gabime/spdlog/archive/refs/tags/v1.14.1.tar.gz      -o $(@D)/subprojects/packagecache/spdlog-1.14.1.tar.gz
+	$(HOST_DIR)/bin/curl -L https://wrapdb.mesonbuild.com/v2/spdlog_1.14.1-1/get_patch             -o $(@D)/subprojects/packagecache/spdlog_1.14.1-1_patch.zip
 	$(HOST_DIR)/bin/curl -L https://github.com/KhronosGroup/Vulkan-Headers/archive/v1.2.158.tar.gz -o $(@D)/subprojects/packagecache/vulkan-headers-1.2.158.tar.gz
 	$(HOST_DIR)/bin/curl -L https://wrapdb.mesonbuild.com/v2/vulkan-headers_1.2.158-2/get_patch    -o $(@D)/subprojects/packagecache/vulkan-headers-1.2.158-2-wrap.zip
+	$(HOST_DIR)/bin/curl -L https://github.com/epezent/implot/archive/refs/tags/v0.16.zip          -o $(@D)/subprojects/packagecache/implot-0.16.zip
+	$(HOST_DIR)/bin/curl -L https://wrapdb.mesonbuild.com/v2/implot_0.16-1/get_patch               -o $(@D)/subprojects/packagecache/implot_0.16-1_patch.zip
 endef
 MANGOHUD_PRE_CONFIGURE_HOOKS += MANGOHUD_DWD_DEPENDENCIES
 
