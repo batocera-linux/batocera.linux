17c791a207f0013366612754d5fa78c3cb8e590d
cracklejoy <>
2024-08-02 12:16:44 -0400

    replace commas with dots in controller.realName fixing non-working controller configurations

--- package/batocera/core/batocera-configgen/configgen/configgen/controllersConfig.py
+++ package/batocera/core/batocera-configgen/configgen/configgen/controllersConfig.py
@@ -120,7 +120,7 @@ def _generateSdlGameControllerConfig(controller, sdlMapping=_DEFAULT_SDL_MAPPING
     """Returns an SDL_GAMECONTROLLERCONFIG-formatted string for the given configuration."""
     config = []
     config.append(controller.guid)
-    config.append(controller.realName)
+    config.append(controller.realName.replace(",", "."))
     config.append("platform:Linux")
 
     def add_mapping(input):
