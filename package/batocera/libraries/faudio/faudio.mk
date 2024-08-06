62fc1fd1ce0dfa2940657734a69552c1961838b0
Daniel Martin <dmanlfc@gmail.com>
2024-08-06 20:52:55 +1000

    bump faudio to 24.08

--- package/batocera/libraries/faudio/faudio.mk
+++ package/batocera/libraries/faudio/faudio.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-FAUDIO_VERSION = 24.05
+FAUDIO_VERSION = 24.08
 FAUDIO_SITE = $(call github,FNA-XNA,FAudio,$(FAUDIO_VERSION))
 
 FAUDIO_LICENSE = ZLIB
