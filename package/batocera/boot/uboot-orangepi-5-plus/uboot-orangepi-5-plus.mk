1b83c22a76590495729ec5557596ec7c75b33e56
Daniel Martin <dmanlfc@gmail.com>
2024-08-19 21:54:13 +1000

    update the opi5+ uboot files

--- package/batocera/boot/uboot-orangepi-5-plus/uboot-orangepi-5-plus.mk
+++ package/batocera/boot/uboot-orangepi-5-plus/uboot-orangepi-5-plus.mk
@@ -1,19 +1,24 @@
 ################################################################################
 #
-# uboot files for OrangePi 5 Plus
+# uboot-orangepi-5-plus
 #
 ################################################################################
 
-UBOOT_ORANGEPI_5_PLUS_VERSION = 2017.09
+UBOOT_ORANGEPI_5_PLUS_VERSION = 2017.09-1.0.8
 UBOOT_ORANGEPI_5_PLUS_SOURCE =
 
 define UBOOT_ORANGEPI_5_PLUS_BUILD_CMDS
 endef
 
+UBOOT_ORANGEPI_5_PLUS_PATH = \
+    $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/boot/uboot-orangepi-5-plus
+
 define UBOOT_ORANGEPI_5_PLUS_INSTALL_TARGET_CMDS
 	mkdir -p $(BINARIES_DIR)/uboot-orangepi-5-plus
-	cp $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/boot/uboot-orangepi-5-plus/idbloader.img $(BINARIES_DIR)/uboot-orangepi-5-plus/idbloader.img
-	cp $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/boot/uboot-orangepi-5-plus/u-boot.itb $(BINARIES_DIR)/uboot-orangepi-5-plus/u-boot.itb
+	cp -f $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/boot/uboot-orangepi-5-plus/idbloader.img \
+	    $(BINARIES_DIR)/uboot-orangepi-5-plus/idbloader.img
+	cp -f $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/boot/uboot-orangepi-5-plus/u-boot.itb \
+	    $(BINARIES_DIR)/uboot-orangepi-5-plus/u-boot.itb
 endef
 
 $(eval $(generic-package))
