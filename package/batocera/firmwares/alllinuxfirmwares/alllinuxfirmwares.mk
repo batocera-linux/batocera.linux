9af334f3124ad34e260d4742f97b71f3b7a45c6e
Daniel Martin <dmanlfc@gmail.com>
2024-09-03 19:28:01 +1000

    allow intel bluetooth firmware for rk3588 boards

--- package/batocera/firmwares/alllinuxfirmwares/alllinuxfirmwares.mk
+++ package/batocera/firmwares/alllinuxfirmwares/alllinuxfirmwares.mk
@@ -9,10 +9,19 @@ ALLLINUXFIRMWARES_SOURCE = linux-firmware-$(ALLLINUXFIRMWARES_VERSION).tar.gz
 ALLLINUXFIRMWARES_SITE = https://git.kernel.org/pub/scm/linux/kernel/git/firmware/linux-firmware.git/snapshot
 
 # exclude some dirs not required on batocera
-ALLLINUXFIRMWARES_REMOVE_DIRS = $(@D)/liquidio $(@D)/netronome $(@D)/mellanox $(@D)/dpaa2 $(@D)/bnx2x $(@D)/cxgb4 $(@D)/mrvl/prestera
+ALLLINUXFIRMWARES_REMOVE_DIRS = $(@D)/liquidio $(@D)/netronome $(@D)/mellanox \
+    $(@D)/dpaa2 $(@D)/bnx2x $(@D)/cxgb4 $(@D)/mrvl/prestera
 
 ifeq ($(BR2_arm)$(BR2_aarch64),y)
-    ALLLINUXFIRMWARES_REMOVE_DIRS += $(@D)/amd $(@D)/amdgpu $(@D)/intel $(@D)/i915 $(@D)/nvidia $(@D)/radeon $(@D)/s5p-* $(@D)/qat_* $(@D)/ql2*
+    # rk3588 boards can allow for more varied pcie combo adapters
+    ifeq ($(BR2_PACKAGE_BATOCERA_TARGET_RK3588),y)
+        ALLLINUXFIRMWARES_REMOVE_DIRS += $(@D)/amd $(@D)/amdgpu $(@D)/intel/avs \
+            $(@D)/intel/catpt $(@D)/intel/ice $(@D)/intel/ipu $(@D)/intel/ish $(@D)/intel/vsc \
+            $(@D)/i915 $(@D)/nvidia $(@D)/radeon $(@D)/s5p-* $(@D)/qat_* $(@D)/ql2*
+    else
+        ALLLINUXFIRMWARES_REMOVE_DIRS += $(@D)/amd $(@D)/amdgpu $(@D)/intel \
+            $(@D)/i915 $(@D)/nvidia $(@D)/radeon $(@D)/s5p-* $(@D)/qat_* $(@D)/ql2*
+    endif
 endif
 
 ifeq ($(BR2_PACKAGE_BRCMFMAC_SDIO_FIRMWARE_RPI)$(BR2_PACKAGE_EXTRALINUXFIRMWARES),y)
@@ -29,9 +38,14 @@ define ALLLINUXFIRMWARES_INSTALL_TARGET_CMDS
 
     # exclude some dirs not required on batocera
     rm -rf $(ALLLINUXFIRMWARES_REMOVE_DIRS)
+    
+    if [ "$(BR2_PACKAGE_BATOCERA_TARGET_RK3588)" = "y" ]; then \
+        find $(@D)/intel -type f ! -name 'ibt-*' -delete; \
+    fi
 
     # -n is mandatory while some other packages provides firmwares too
-    # this is not ideal, but i don't know how to tell to buildroot to install this package first (and not worry about all packages installing firmwares)
+    # this is not ideal, but i don't know how to tell to buildroot to install this package first
+    # (and not worry about all packages installing firmwares)
     cp --remove-destination -prn $(@D)/* $(TARGET_DIR)/lib/firmware/
 
     # Some firmware are distributed as a symlink, for drivers to load them using a
@@ -41,14 +55,14 @@ define ALLLINUXFIRMWARES_INSTALL_TARGET_CMDS
     # parses the WHENCE file where symlinks are described. We follow the same logic
     # here, adding symlink only for firmwares installed in the target directory.
     cd $(TARGET_DIR)/lib/firmware ; \
-    sed -r -e '/^Link: (.+) -> (.+)$$/!d; s//\1 \2/' $(@D)/WHENCE | \
-	while read f d; do \
-		if test -f $$(readlink -m $$(dirname "$$f")/$$d); then \
-            if test -f $(TARGET_DIR)/lib/firmware/$$(dirname "$$f")/$$d; then \
-                ln -sf $$d "$$f" || exit 1; \
-            fi \
-		fi ; \
-	done
+        sed -r -e '/^Link: (.+) -> (.+)$$/!d; s//\1 \2/' $(@D)/WHENCE | \
+        while read f d; do \
+            if test -f $$(readlink -m $$(dirname "$$f")/$$d); then \
+                if test -f $(TARGET_DIR)/lib/firmware/$$(dirname "$$f")/$$d; then \
+                    ln -sf $$d "$$f" || exit 1; \
+                fi \
+            fi ; \
+        done
 endef
 
 define ALLLINUXFIRMWARES_LINK_QCA_WIFI_BT
@@ -56,12 +70,12 @@ define ALLLINUXFIRMWARES_LINK_QCA_WIFI_BT
     mkdir -p $(TARGET_DIR)/lib/firmware/ath11k/WCN6855/hw2.1
     mkdir -p $(TARGET_DIR)/lib/firmware/ath11k/QCA2066
     cp -rf $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/firmwares/alllinuxfirmwares/hw2.1/* \
-	    $(TARGET_DIR)/lib/firmware/ath11k/WCN6855/hw2.1
+        $(TARGET_DIR)/lib/firmware/ath11k/WCN6855/hw2.1
     cp -rf $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/firmwares/alllinuxfirmwares/QCA206X/* \
-	    $(TARGET_DIR)/lib/firmware/ath11k/QCA2066
+        $(TARGET_DIR)/lib/firmware/ath11k/QCA2066
     # bluetooth
     cp -rf $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/firmwares/alllinuxfirmwares/qca/* \
-	    $(TARGET_DIR)/lib/firmware/qca
+        $(TARGET_DIR)/lib/firmware/qca
 endef
 
 # Copy Qualcomm firmware for Steam Deck OLED
