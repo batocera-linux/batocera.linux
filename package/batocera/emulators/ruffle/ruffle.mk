3e8dbe8010a61dd6fe9bb04f7023146387c8d944
Bryan Forbes <bryan@reigndropsfall.net>
2024-09-07 15:25:02 -0500

    improve ruffle build and add dependencies

--- package/batocera/emulators/ruffle/ruffle.mk
+++ package/batocera/emulators/ruffle/ruffle.mk
@@ -7,25 +7,23 @@
 RUFFLE_VERSION = nightly-2024-03-28
 RUFFLE_SITE = $(call github,ruffle-rs,ruffle,$(RUFFLE_VERSION))
 RUFFLE_LICENSE = GPLv2
-RUFFLE_DEPENDENCIES = host-rustc host-rust-bin openssl
+RUFFLE_DEPENDENCIES = host-rustc host-rust-bin openssl udev nghttp2 alsa-lib
 
-RUFFLE_ARGS_FOR_BUILD = -L $(STAGING_DIR) -Wl,-rpath,$(STAGING_DIR)
-
-RUFFLE_CARGO_ENV = CARGO_HOME=$(HOST_DIR)/usr/share/cargo \
-    RUSTFLAGS='$(addprefix -C linker=$(TARGET_CC) -C link-args=,$(RUFFLE_ARGS_FOR_BUILD))' \
+RUFFLE_CARGO_ENV = CARGO_HOME=$(DL_DIR)/br-cargo-home \
+    CARGO_BUILD_TARGET="$(RUSTC_TARGET_NAME)" \
+    CARGO_HOST_RUSTFLAGS="$(addprefix -C link-args=,$(HOST_LDFLAGS))" \
+    CARGO_TARGET_$(call UPPERCASE,$(RUSTC_TARGET_NAME))_LINKER=$(notdir $(TARGET_CROSS))gcc \
     PKG_CONFIG="$(PKG_CONFIG_HOST_BINARY)"
-
 
 RUFFLE_CARGO_MODE = $(if $(BR2_ENABLE_DEBUG),,release)
 RUFFLE_BIN_DIR = target/$(RUSTC_TARGET_NAME)/$(RUFFLE_CARGO_MODE)
 
 RUFFLE_CARGO_OPTS = \
     --$(RUFFLE_CARGO_MODE) \
-        --target=$(RUSTC_TARGET_NAME) \
-        --manifest-path=$(@D)/Cargo.toml
+    --manifest-path=$(@D)/Cargo.toml
 
 define RUFFLE_BUILD_CMDS
-    $(TARGET_MAKE_ENV) $(RUFFLE_CARGO_ENV) \
+    $(TARGET_MAKE_ENV) $(TARGET_CONFIGURE_OPTS) $(RUFFLE_CARGO_ENV) \
             cargo build $(RUFFLE_CARGO_OPTS)
 endef
 
