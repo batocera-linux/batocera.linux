435a5dad4dad8c2a6a5400e36095e69bcbb1c323
Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>
2024-09-11 19:14:13 +0000

    ultimarc-mini 4p support
    Signed-off-by: Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>

--- package/batocera/controllers/pads/ultimarc-mini-pac/ultimarc-mini-pac.mk
+++ package/batocera/controllers/pads/ultimarc-mini-pac/ultimarc-mini-pac.mk
@@ -11,6 +11,7 @@ ULTIMARC_MINI_PAC_SOURCE =
 define ULTIMARC_MINI_PAC_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0644 -D $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/controllers/pads/ultimarc-mini-pac/99-ultimarc-mini-pac.rules $(TARGET_DIR)/etc/udev/rules.d/99-ultimarc-mini-pac.rules
 	$(INSTALL) -m 0755 -D $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/controllers/pads/ultimarc-mini-pac/ultimarc-mini-pacSplit $(TARGET_DIR)/usr/bin/ultimarc-mini-pacSplit
+	$(INSTALL) -m 0755 -D $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/controllers/pads/ultimarc-mini-pac/ultimarc-ipac4Split    $(TARGET_DIR)/usr/bin/ultimarc-ipac4Split
 endef
 
 $(eval $(generic-package))
