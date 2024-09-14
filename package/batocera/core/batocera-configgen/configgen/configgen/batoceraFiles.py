5484ac2c66d5256dd43a1a1a80c9f40d77c704ec
Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>
2024-09-14 20:05:06 +0000

    did you know that es can have es_input system and local side to allow system update file
    Signed-off-by: Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>

--- package/batocera/core/batocera-configgen/configgen/configgen/batoceraFiles.py
+++ package/batocera/core/batocera-configgen/configgen/configgen/batoceraFiles.py
@@ -13,7 +13,6 @@ OVERLAYS = '/userdata/overlays'
 CACHE = '/userdata/system/cache'
 ROMS = '/userdata/roms'
 
-esInputs = CONF + '/emulationstation/es_input.cfg'
 esSettings = CONF + '/emulationstation/es_settings.cfg'
 esGunsMetadata = "/usr/share/emulationstation/resources/gungames.xml"
 esWheelsMetadata = "/usr/share/emulationstation/resources/wheelgames.xml"
