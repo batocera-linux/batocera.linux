3455ad9f54ce875412701050bbfce8fbbe468b39
Nicholas S. Castellano <nicholas.s.castellano+github@gmail.com>
2024-09-19 18:00:48 -0400

    Bump ia32 EFI shim boot to latest

--- batocera-Changelog.md
+++ batocera-Changelog.md
@@ -117,6 +117,7 @@
 - Mesa3D to 24.2.1
 - Buildroot to the 2024.05.2 release base
 - GStreamer codecs to 1.24.7
+- shim-signed ia32 EFI bootloader to 1.44~1+deb12u1+15.8-1~deb12u1
 
 # 2024/08/11 - batocera.linux 40 - Swallowtail
 ### Special Notes
--- package/batocera/boot/batocera-shim-signed-efi-helpers-ia32/batocera-shim-signed-efi-helpers-ia32.mk
+++ package/batocera/boot/batocera-shim-signed-efi-helpers-ia32/batocera-shim-signed-efi-helpers-ia32.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-BATOCERA_SHIM_SIGNED_EFI_HELPERS_IA32_VERSION = 1+15.7+1
+BATOCERA_SHIM_SIGNED_EFI_HELPERS_IA32_VERSION = 1+15.8+1~deb12u1
 BATOCERA_SHIM_SIGNED_EFI_HELPERS_IA32_SITE = https://ftp.debian.org/debian/pool/main/s/shim-helpers-i386-signed
 BATOCERA_SHIM_SIGNED_EFI_HELPERS_IA32_SOURCE = shim-helpers-i386-signed_$(BATOCERA_SHIM_SIGNED_EFI_HELPERS_IA32_VERSION)_i386.deb
 
