0cb3f52daad52df02af236358ab0ffad49e24769
Bryan Forbes <bryan@reigndropsfall.net>
2024-09-25 13:29:36 -0500

    add typing for `Generator`

--- package/batocera/core/batocera-configgen/configgen/configgen/generators/Generator.py
+++ package/batocera/core/batocera-configgen/configgen/configgen/generators/Generator.py
@@ -1,33 +1,55 @@
+from __future__ import annotations
+
 from abc import ABCMeta, abstractmethod
+from typing import TYPE_CHECKING, Any
+
+from configgen.types import HotkeysContext
+
+if TYPE_CHECKING:
+    from collections.abc import Mapping
+
+    from ..Command import Command
+    from ..controllersConfig import ControllerMapping
+    from ..Emulator import Emulator
+    from ..types import DeviceInfoMapping, GunMapping, Resolution
 
 
 class Generator(metaclass=ABCMeta):
     @abstractmethod
-    def generate(self, system, rom, playersControllers, metadata, guns, wheels, gameResolution):
-        pass
+    def generate(
+        self,
+        system: Emulator,
+        rom: str,
+        playersControllers: ControllerMapping,
+        metadata: Mapping[str, str],
+        guns: GunMapping,
+        wheels: DeviceInfoMapping,
+        gameResolution: Resolution,
+    ) -> Command:
+        ...
 
-    def getResolutionMode(self, config):
+    def getResolutionMode(self, config: dict[str, Any]) -> str:
         return config['videomode']
 
-    def getMouseMode(self, config, rom):
+    def getMouseMode(self, config: dict[str, Any], rom: str) -> bool:
         return False
 
-    def executionDirectory(self, config, rom):
+    def executionDirectory(self, config: dict[str, Any], rom: str) -> str | None:
         return None
 
     # mame or libretro have internal bezels, don't display the one of mangohud
-    def supportsInternalBezels(self):
+    def supportsInternalBezels(self) -> bool:
         return False
 
     # mangohud must be called by the generator itself (wine based emulator for example)
-    def hasInternalMangoHUDCall(self):
+    def hasInternalMangoHUDCall(self) -> bool:
         return False
 
-    def getInGameRatio(self, config, gameResolution, rom):
+    def getInGameRatio(self, config: dict[str, Any], gameResolution: Resolution, rom: str) -> float:
         # put a default value, but it should be overriden by generators
         return 4/3
 
-    def getHotkeysContext(self):
+    def getHotkeysContext(self) -> HotkeysContext:
         return {
             "name": "configgen",
             "keys": {}
