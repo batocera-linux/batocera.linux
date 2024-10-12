0ff9dad1b4a58443a76c0160fcd4941b2f39ef36
Daniel Martin <dmanlfc@gmail.com>
2024-10-12 12:56:51 +1000

    bump pyxel to 2.2.4

--- package/batocera/emulators/python-pyxel/python-pyxel.mk
+++ package/batocera/emulators/python-pyxel/python-pyxel.mk
@@ -4,13 +4,15 @@
 #
 ################################################################################
 
-PYTHON_PYXEL_VERSION = v1.9.18
+PYTHON_PYXEL_VERSION = v2.2.4
 PYTHON_PYXEL_SITE =  $(call github,kitao,pyxel,$(PYTHON_PYXEL_VERSION))
 PYTHON_PYXEL_SETUP_TYPE = setuptools
 PYTHON_PYXEL_LICENSE = MIT
 PYTHON_PYXEL_SETUP_TYPE = maturin
 PYTHON_PYXEL_CARGO_MANIFEST_PATH = crates/pyxel-extension/Cargo.toml
 PYTHON_PYXEL_DEPENDENCIES = host-rust-bin sdl2
+
+PYTHON_PYXEL_SUBDIR = python
 
 ifeq ($(BR2_PACKAGE_BATOCERA_TARGET_X86_64_ANY),y)
 	PYXEL_CARGO_TARGET=x86_64-unknown-linux-gnu
@@ -30,8 +32,16 @@ define PYTHON_PYXEL_SAMPLE_AND_KEYS
 	cp -rf $(@D)/python/pyxel $(TARGET_DIR)/usr/lib/python*/site-packages/
 	rm -rf $(TARGET_DIR)/usr/lib/python*/site-packages/pyxel/examples
 	cd $(TARGET_DIR)/usr/lib/python*/site-packages/pyxel && ln -sf ../pyxel_extension .
-	cp -f $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/emulators/python-pyxel/pyxel.keys $(TARGET_DIR)/usr/share/evmapy/
+	cp -f $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/emulators/python-pyxel/pyxel.keys \
+	    $(TARGET_DIR)/usr/share/evmapy/
 endef
+
+define PYTHON_PYXEL_FIX_BUILD
+    $(SED) "s+-I/usr/include+-I$(STAGING_DIR)/usr/include/SDL2+g" \
+	    $(@D)/rust/pyxel-platform/build.rs
+endef
+
+PYTHON_PYXEL_PRE_CONFIGURE_HOOKS += PYTHON_PYXEL_FIX_BUILD
 
 PYTHON_PYXEL_PRE_INSTALL_TARGET_HOOKS += PYTHON_PYXEL_REMOVE_PREVIOUS
 
