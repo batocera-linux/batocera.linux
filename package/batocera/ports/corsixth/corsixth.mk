a4e29ba3b7c93194fe850b4d738ea7de9cfce1cf
Daniel Martin <dmanlfc@gmail.com>
2024-10-18 00:37:49 +1000

    bump corsixth to v0.68.0

--- package/batocera/ports/corsixth/corsixth.mk
+++ package/batocera/ports/corsixth/corsixth.mk
@@ -1,17 +1,19 @@
 ################################################################################
 #
-# CORSIXTH
+# corsixth
 #
 ################################################################################
 
-CORSIXTH_VERSION = v0.67
+CORSIXTH_VERSION = v0.68.0
 CORSIXTH_SITE = $(call github,CorsixTH,CorsixTH,$(CORSIXTH_VERSION))
-CORSIXTH_DEPENDENCIES = sdl2 sdl2_image lua luafilesystem lpeg luasocket luasec sdl2_mixer ffmpeg
+CORSIXTH_DEPENDENCIES = sdl2 sdl2_image lua luafilesystem lpeg luasocket 
+CORSIXTH_DEPENDENCIES += luasec sdl2_mixer ffmpeg
 
 define CORSIXTH_INSTALL_EVMAPY
-        # evmap config
-        mkdir -p $(TARGET_DIR)/usr/share/evmapy
-        cp $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/ports/corsixth/corsixth.keys $(TARGET_DIR)/usr/share/evmapy
+    # evmap config
+    mkdir -p $(TARGET_DIR)/usr/share/evmapy
+    cp $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/ports/corsixth/corsixth.keys \
+        $(TARGET_DIR)/usr/share/evmapy
 endef
 
 CORSIXTH_POST_INSTALL_TARGET_HOOKS += CORSIXTH_INSTALL_EVMAPY
