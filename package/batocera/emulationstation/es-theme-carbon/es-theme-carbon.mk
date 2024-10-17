19eb57270edc62494e9c540ecbf6018a19eeb80f
Daniel Martin <dmanlfc@gmail.com>
2024-10-17 18:52:05 +1000

    ES theme carbon to Oct 16 build

--- package/batocera/emulationstation/es-theme-carbon/es-theme-carbon.mk
+++ package/batocera/emulationstation/es-theme-carbon/es-theme-carbon.mk
@@ -3,8 +3,8 @@
 # EmulationStation theme "Carbon"
 #
 ################################################################################
-# Version: Commits on Sep 16, 2024
-ES_THEME_CARBON_VERSION = 06db7dc11c1eb618ccaccad6343e861071271dd5
+# Version: Commits on Oct 16, 2024
+ES_THEME_CARBON_VERSION = 421cec038116fe7b6711fa35683470884a28ed55
 ES_THEME_CARBON_SITE = $(call github,fabricecaruso,es-theme-carbon,$(ES_THEME_CARBON_VERSION))
 
 define ES_THEME_CARBON_INSTALL_TARGET_CMDS
