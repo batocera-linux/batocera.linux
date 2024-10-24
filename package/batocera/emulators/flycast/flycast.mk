80e49cc12f275cde9c7207696de6fdb954ba0c25
Daniel Martin <dmanlfc@gmail.com>
2024-10-25 08:55:29 +1000

    update flycast versions to v2.4

--- package/batocera/emulators/flycast/flycast.mk
+++ package/batocera/emulators/flycast/flycast.mk
@@ -3,8 +3,8 @@
 # flycast
 #
 ################################################################################
-# Version: Commits on Sep 29, 2024
-FLYCAST_VERSION = 2dd4cbe33d24dcb9f40ec9b623d788e3291dc4d6
+
+FLYCAST_VERSION = v2.4
 FLYCAST_SITE = https://github.com/flyinghead/flycast.git
 FLYCAST_SITE_METHOD=git
 FLYCAST_GIT_SUBMODULES=YES
--- package/batocera/emulators/retroarch/libretro/libretro-flycast/libretro-flycast.mk
+++ package/batocera/emulators/retroarch/libretro/libretro-flycast/libretro-flycast.mk
@@ -3,8 +3,8 @@
 # libretro-flycast
 #
 ################################################################################
-# Version: Commits on Sep 29, 2024
-LIBRETRO_FLYCAST_VERSION = 2dd4cbe33d24dcb9f40ec9b623d788e3291dc4d6
+
+LIBRETRO_FLYCAST_VERSION = v2.4
 LIBRETRO_FLYCAST_SITE = https://github.com/flyinghead/flycast.git
 LIBRETRO_FLYCAST_SITE_METHOD=git
 LIBRETRO_FLYCAST_GIT_SUBMODULES=YES
