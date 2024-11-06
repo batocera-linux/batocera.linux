c588ffb3c24edb8fbd88fb579dc351b1b59ba7a1
Daniel Martin <dmanlfc@gmail.com>
2024-11-06 12:50:51 +1000

    update the placebo package for rk3399

--- package/batocera/libraries/libplacebo/libplacebo.mk
+++ package/batocera/libraries/libplacebo/libplacebo.mk
@@ -12,4 +12,28 @@ LIBPLACEBO_LICENSE = LGPL-2.1
 LIBPLACEBO_LICENSE_FILES = LICENSE
 LIBPLACEBO_INSTALL_STAGING = YES
 
+LIBPLACEBO_CONF_OPTS = -Ddemos=false
+
+ifeq ($(BR2_PACKAGE_VULKAN_HEADERS)$(BR2_PACKAGE_VULKAN_LOADER),yy)
+LIBPLACEBO_CONF_OPTS += -Dvulkan=enabled
+LIBPLACEBO_DEPENDENCIES += vulkan-headers vulkan-loader
+else
+LIBPLACEBO_CONF_OPTS += -Dvulkan=disabled
+endif
+
+ifeq ($(BR2_PACKAGE_GLSLANG),y)
+LIBPLACEBO_CONF_OPTS += -Dglslang=enabled
+LIBPLACEBO_DEPENDENCIES += glslang
+LIBPLACEBO_LDFLAGS = -lglslang
+else
+LIBPLACEBO_CONF_OPTS += -Dglslang=disabled
+endif
+
+ifeq ($(BR2_PACKAGE_SHADERC),y)
+LIBPLACEBO_CONF_OPTS += -Dshaderc=enabled
+LIBPLACEBO_DEPENDENCIES += shaderc
+else
+LIBPLACEBO_CONF_OPTS += -Dshaderc=disabled
+endif
+
 $(eval $(meson-package))
