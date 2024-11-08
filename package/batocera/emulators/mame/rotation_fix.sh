f08656e4c1f8035234155241b7388fde6e9cb467
Daniel Martin <dmanlfc@gmail.com>
2024-11-08 19:29:37 +1000

    mame rotate for xorg only currently

--- package/batocera/emulators/mame/rotation_fix.sh
+++ package/batocera/emulators/mame/rotation_fix.sh
@@ -2,11 +2,12 @@
 # Sometimes with a rotated screen (typically SteamDeck), MAME's video
 # driver needs to be reinitialized with the right rotation after exit
 case $1 in
-    	gameStop)
-		if [[ "$3" == "mame" ]]; then
-			rotation=$(batocera-resolution getRotation)
-		       	! [ -z "$rotation" ] && batocera-resolution setRotation "$rotation"
-		fi
-	;;
+    gameStop)
+        if [[ "$3" == "mame" ]]; then
+            if [[ "$(batocera-resolution getDisplayMode)" == "xorg" ]]; then
+                rotation=$(batocera-resolution getRotation)
+                [[ -n "$rotation" ]] && batocera-resolution setRotation "$rotation"
+            fi
+        fi
+    ;;
 esac
-
