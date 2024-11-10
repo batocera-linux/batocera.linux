c095db446b1fe1a140f2bf72dd4d39917524f9c2
Daniel Martin <dmanlfc@gmail.com>
2024-11-10 17:48:19 +1000

    fix diablo bezels & option

--- package/batocera/core/batocera-configgen/configgen/configgen/generators/devilutionx/devilutionxGenerator.py
+++ package/batocera/core/batocera-configgen/configgen/configgen/generators/devilutionx/devilutionxGenerator.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import configparser
 from typing import TYPE_CHECKING
 
 from ... import Command
@@ -15,13 +16,34 @@ class DevilutionXGenerator(Generator):
 
     def generate(self, system, rom, playersControllers, metadata, guns, wheels, gameResolution):
         configDir = CONFIGS / 'devilutionx'
+        configFile = configDir / 'diablo.ini'
         saveDir = SAVES / 'devilutionx'
 
         configDir.mkdir(parents=True, exist_ok=True)
         saveDir.mkdir(parents=True, exist_ok=True)
 
-        commandArray = ['devilutionx', '--data-dir', '/userdata/roms/devilutionx',
-                        '--config-dir', configDir, '--save-dir', saveDir]
+        ## Configure
+        config = configparser.ConfigParser()
+
+        if configFile.exists():
+            config.read(configFile)
+        
+        # Ensure the [Graphics] section exists
+        if 'Graphics' not in config:
+            config['Graphics'] = {}
+        
+        if system.isOptSet("devilutionx_stretch") and system.config["devilutionx_stretch"] == "true":
+            config['Graphics']['Fit to Screen'] = '1'
+        else:
+            config['Graphics']['Fit to Screen'] = '0'
+
+        with open(configFile, 'w') as file:
+            config.write(file)
+        
+        commandArray = [
+            'devilutionx', '--data-dir', '/userdata/roms/devilutionx',
+            '--config-dir', configDir, '--save-dir', saveDir
+        ]
         if rom.endswith('hellfire.mpq'):
             commandArray.append('--hellfire')
         elif rom.endswith('spawn.mpq'):
@@ -29,16 +51,33 @@ class DevilutionXGenerator(Generator):
         else:
             commandArray.append('--diablo')
 
-        if system.isOptSet('showFPS') and system.getOptBoolean('showFPS') == True:
+        if system.isOptSet('showFPS') and system.getOptBoolean('showFPS'):
             commandArray.append('-f')
+        
         return Command.Command(
             array=commandArray,
             env={
                 'SDL_GAMECONTROLLERCONFIG': generate_sdl_game_controller_config(playersControllers)
-            })
+            }
+        )
 
     def getHotkeysContext(self) -> HotkeysContext:
         return {
             "name": "devilutionx",
-            "keys": { "exit": ["KEY_LEFTALT", "KEY_F4"], "menu": "KEY_ESC", "pause": "KEY_ESC", "save_state": "KEY_F2", "restore_state": "KEY_F3" }
+            "keys": {
+                "exit": ["KEY_LEFTALT", "KEY_F4"],
+                "menu": "KEY_ESC",
+                "pause": "KEY_ESC",
+                "save_state": "KEY_F2",
+                "restore_state": "KEY_F3"
+            }
         }
+
+    def getInGameRatio(self, config, gameResolution, rom):
+        if "devilutionx_stretch" in config:
+            if config['devilutionx_stretch'] == "true":
+                return 16 / 9
+            else:
+                return 4 / 3
+        else:
+            return 4 / 3
