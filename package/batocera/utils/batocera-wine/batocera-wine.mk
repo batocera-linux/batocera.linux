2dc6a14a96e129dc8fa61307ed42527d396ba75e
Daniel Martin <dmanlfc@gmail.com>
2024-11-27 09:59:26 +1000

    fix wsquashfs not properly unmounting

--- package/batocera/utils/batocera-wine/batocera-wine.mk
+++ package/batocera/utils/batocera-wine/batocera-wine.mk
@@ -4,21 +4,24 @@
 #
 ################################################################################
 
-BATOCERA_WINE_VERSION = 1.1
+BATOCERA_WINE_VERSION = 1.2
 BATOCERA_WINE_LICENSE = GPL
 BATOCERA_WINE_SOURCE=
 
+BATOCERA_WINE_SOURCE_PATH = \
+    $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/utils/batocera-wine
+
 define BATOCERA_WINE_INSTALL_TARGET_CMDS
-	install -m 0755 $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/utils/batocera-wine/batocera-wine \
+	install -m 0755 $(BATOCERA_WINE_SOURCE_PATH)/batocera-wine \
 	    $(TARGET_DIR)/usr/bin/batocera-wine
-	install -m 0755 $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/utils/batocera-wine/batocera-wine-runners \
+	install -m 0755 $(BATOCERA_WINE_SOURCE_PATH)/batocera-wine-runners \
 	    $(TARGET_DIR)/usr/bin/batocera-wine-runners
-	install -m 0755 $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/utils/batocera-wine/bsod.py \
+	install -m 0755 $(BATOCERA_WINE_SOURCE_PATH)/bsod.py \
 	    $(TARGET_DIR)/usr/bin/bsod-wine
 	ln -fs /userdata/system/99-nvidia.conf $(TARGET_DIR)/etc/X11/xorg.conf.d/99-nvidia.conf
 
 	mkdir -p $(TARGET_DIR)/usr/share/evmapy
-	cp $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/utils/batocera-wine/mugen.keys \
+	cp $(BATOCERA_WINE_SOURCE_PATH)/mugen.keys \
 	    $(TARGET_DIR)/usr/share/evmapy
 endef
 
