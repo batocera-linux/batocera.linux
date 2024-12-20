82e9ec1521d4e90071b0559302aa9b24bdf10724
Daniel Martin <dmanlfc@gmail.com>
2024-12-20 16:31:15 +1000

    move riscv to starfive/jh7110 for future riscv boards

--- package/batocera/boot/uboot-visionfive2/uboot-visionfive2.mk
+++ package/batocera/boot/uboot-visionfive2/uboot-visionfive2.mk
@@ -1,11 +1,12 @@
 ################################################################################
 #
-# uboot files for StarFive VisionFive 2
+# uboot-visionfive2
 #
 ################################################################################
 
 UBOOT_VISIONFIVE2_VERSION = 3.6.1
-UBOOT_VISIONFIVE2_SITE = https://github.com/starfive-tech/VisionFive2/releases/download/VF2_v$(UBOOT_VISIONFIVE2_VERSION)
+UBOOT_VISIONFIVE2_SITE = \
+    https://github.com/starfive-tech/VisionFive2/releases/download/VF2_v$(UBOOT_VISIONFIVE2_VERSION)
 UBOOT_VISIONFIVE2_SOURCE = u-boot-spl.bin.normal.out
 UBOOT_VISIONFIVE2_EXTRA_DOWNLOADS = visionfive2_fw_payload.img
 
@@ -17,8 +18,10 @@ endef
 
 define UBOOT_VISIONFIVE2_INSTALL_TARGET_CMDS
 	mkdir -p $(BINARIES_DIR)/uboot-visionfive2
-	cp $(UBOOT_VISIONFIVE2_DL_DIR)/u-boot-spl.bin.normal.out $(BINARIES_DIR)/uboot-visionfive2/u-boot-spl.bin.normal.out
-	cp $(UBOOT_VISIONFIVE2_DL_DIR)/visionfive2_fw_payload.img $(BINARIES_DIR)/uboot-visionfive2/visionfive2_fw_payload.img
+	cp $(UBOOT_VISIONFIVE2_DL_DIR)/u-boot-spl.bin.normal.out \
+	    $(BINARIES_DIR)/uboot-visionfive2/u-boot-spl.bin.normal.out
+	cp $(UBOOT_VISIONFIVE2_DL_DIR)/visionfive2_fw_payload.img \
+	    $(BINARIES_DIR)/uboot-visionfive2/visionfive2_fw_payload.img
 endef
 
 $(eval $(generic-package))
