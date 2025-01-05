45766be411ddc622b8b117cb8cc4b80cda76ce24
m33ts4k0z <dokimi200@gmail.com>
2025-01-05 15:41:16 +0100

    Fixed BT for RK3588 and RTL8852BE

--- package/batocera/firmwares/firmware-rk3588/firmware-rk3588.mk
+++ package/batocera/firmwares/firmware-rk3588/firmware-rk3588.mk
@@ -14,6 +14,9 @@ FIRMWARE_RK3588_TARGET_DIR=$(TARGET_DIR)/lib/firmware
 define FIRMWARE_RK3588_INSTALL_TARGET_CMDS
 	mkdir -p $(FIRMWARE_RK3588_TARGET_DIR)/rtk_bt
 	cp -v $(@D)/rtl8821cs_* $(FIRMWARE_RK3588_TARGET_DIR)/rtk_bt/
+	# RTL8852BE BT & WiFi firmware files for Rockchip-linux 6.1.75
+	$(INSTALL) -m 0644 -D $(FIRMWARE_RK3588_PKGDIR)/rtl8852bu/rtl8852bu_config.bin $(FIRMWARE_RK3588_TARGET_DIR)/rtl_bt/rtl8852bu_config.bin
+	$(INSTALL) -m 0644 -D $(FIRMWARE_RK3588_PKGDIR)/rtl8852bu/rtl8852bu_fw.bin $(FIRMWARE_RK3588_TARGET_DIR)/rtl_bt/rtl8852bu_fw.bin
 endef
 
 $(eval $(generic-package))
