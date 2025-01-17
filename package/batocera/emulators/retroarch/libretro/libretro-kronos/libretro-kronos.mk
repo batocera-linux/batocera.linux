8315d2e83dea78d063b2b956fe76b62f25b77af0
Daniel Martin <dmanlfc@gmail.com>
2025-01-17 21:42:25 +1000

    libretro kronos to 2.7.0

--- package/batocera/emulators/retroarch/libretro/libretro-kronos/libretro-kronos.mk
+++ package/batocera/emulators/retroarch/libretro/libretro-kronos/libretro-kronos.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-LIBRETRO_KRONOS_VERSION = 2.6.1_official_release
+LIBRETRO_KRONOS_VERSION = 2.7.0_official_release
 LIBRETRO_KRONOS_SITE = $(call github,FCare,kronos,$(LIBRETRO_KRONOS_VERSION))
 LIBRETRO_KRONOS_LICENSE = BSD-3-Clause
 
