29a5293d867584ce534e810add19b5ae6aac6549
Nicholas S. Castellano <nicholas.s.castellano+github@gmail.com>
2025-01-24 20:15:17 -0500

    Fix various dependencies and target directories

--- package/batocera/utils/batocera-wine/batocera-wine.mk
+++ package/batocera/utils/batocera-wine/batocera-wine.mk
@@ -12,6 +12,8 @@ BATOCERA_WINE_SOURCE_PATH = \
     $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/utils/batocera-wine
 
 define BATOCERA_WINE_INSTALL_TARGET_CMDS
+	mkdir -p $(TARGET_DIR)/usr/bin
+	mkdir -p $(TARGET_DIR)/etc/X11/xorg.conf.d
 	install -m 0755 $(BATOCERA_WINE_SOURCE_PATH)/batocera-wine \
 	    $(TARGET_DIR)/usr/bin/batocera-wine
 	install -m 0755 $(BATOCERA_WINE_SOURCE_PATH)/batocera-wine-runners \
