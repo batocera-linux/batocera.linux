6cf1175a60a883d60f14c6e8b48b6db8a4789e26
alexandre derumier <aderumier@odiso.com>
2025-01-30 07:55:58 +0100

    scummvm: fix stretch_mode (#13196)
    since https://github.com/batocera-linux/batocera.linux/pull/9630/commits/8cc8b2790a118a56786fff9ef112044aaa3b59de,
    default stretch mode is broken.
    
    The default stretch_mode of scummvm (if not defined in command line) is "fit", not "center".
    (Center don't scale at all, so if you have a 4k screen with a 320x200 games, it'll be displayed too small)
    
    This patch simply don't force it command line
    
    also, es_features.yml is buggy, as "center" value is currently defined in french "centre"
    
    Co-authored-by: Alexandre Derumier <aderumier@gmail.com>

--- package/batocera/core/batocera-configgen/configgen/configgen/generators/scummvm/scummvmGenerator.py
+++ package/batocera/core/batocera-configgen/configgen/configgen/generators/scummvm/scummvmGenerator.py
@@ -87,8 +87,6 @@ class ScummVMGenerator(Generator):
         #  stretch mode
         if system.isOptSet("scumm_stretch"):
             commandArray.append(f"--stretch-mode={system.config['scumm_stretch']}")
-        else:
-            commandArray.append("--stretch-mode=center")
 
         # renderer
         if system.isOptSet("scumm_renderer"):
