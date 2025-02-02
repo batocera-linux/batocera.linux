435afb3ed6ef55a3541a6d1dab77b62b13b3d964
Sam Yeda <samyeda@gmail.com>
2025-02-02 17:49:02 -0500

    vim4: upgrade kernel and gpu
    Notable major changes
    * Upgrade to maintained Kernel version 5.15.x
    * Upgrade Mali GPU (r44)
    * Upgrade Firmware files
    * VIM4N board support

--- package/batocera/gpu/mali-G52/mali-G52.mk
+++ package/batocera/gpu/mali-G52/mali-G52.mk
@@ -4,16 +4,20 @@
 #
 ################################################################################
 
-MALI_G52_VERSION = 6ab9d7ce7a991f997460697013fc54029fcac681
-MALI_G52_SITE = $(call github,rtissera,a311d2-mali-g52,$(MALI_G52_VERSION))
+MALI_G52_VERSION = 1.4-r44p0-202311
+MALI_G52_SITE = https://dl.khadas.com/repos/vim4/pool/main/l/linux-gpu-mali-wayland
+MALI_G52_SOURCE = linux-gpu-mali-wayland_$(MALI_G52_VERSION)_arm64.deb
 MALI_G52_LICENSE = Proprietary
 MALI_G52_LICENSE_FILES = END_USER_LICENCE_AGREEMENT.txt
 MALI_G52_INSTALL_STAGING = YES
 
-MALI_G52_EXTRA_DOWNLOADS = https://gitlab.freedesktop.org/glvnd/libglvnd/-/archive/master/libglvnd-master.tar.gz
-
 MALI_G52_PROVIDES = libegl libgbm libgles libmali
 MALI_G52_DEPENDENCIES = host-patchelf libdrm libglvnd wayland
+
+define MALI_G52_EXTRACT_CMDS
+	$(AR) --output=$(@D) -x $(MALI_G52_DL_DIR)/$(MALI_G52_SOURCE)
+	$(TAR) xf $(@D)/data.tar.xz -C $(@D)
+endef
 
 define MALI_G52_INSTALL_STAGING_CMDS
         mkdir -p $(STAGING_DIR)/usr/lib/pkgconfig
@@ -31,6 +35,7 @@ define MALI_G52_INSTALL_TARGET_CMDS
             $(TARGET_DIR)/usr/share/batocera/configgen/scripts/mali_g52_gpu_launch_hooks.sh
 endef
 
+#MALI_G52_EXTRA_DOWNLOADS = https://gitlab.freedesktop.org/glvnd/libglvnd/-/archive/master/libglvnd-master.tar.gz
 #MALI_G52_GPU = bifrost-g52
 #MALI_G52_VER = g2p0
 #MALI_G52_PLATFORM = wayland-gbm
