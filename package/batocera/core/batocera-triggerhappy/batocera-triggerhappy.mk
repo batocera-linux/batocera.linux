bbc6f51606b1bc202802c0202c707133ce783160
Nicholas S. Castellano <nicholas.s.castellano+github@gmail.com>
2025-02-03 18:29:33 -0500

    Fix batocera-trigerhappy for parallel build

--- package/batocera/core/batocera-triggerhappy/batocera-triggerhappy.mk
+++ package/batocera/core/batocera-triggerhappy/batocera-triggerhappy.mk
@@ -6,15 +6,15 @@
 
 BATOCERA_TRIGGERHAPPY_VERSION = 1.1
 BATOCERA_TRIGGERHAPPY_LICENSE = GPL
-BATOCERA_TRIGGERHAPPY_DEPENDENCIES = triggerhappy # to erase the trigger happy S50 startup script
+BATOCERA_TRIGGERHAPPY_DEPENDENCIES = triggerhappy
 BATOCERA_TRIGGERHAPPY_SOURCE=
 
 define BATOCERA_TRIGGERHAPPY_INSTALL_CONFIG
 	mkdir -p $(TARGET_DIR)/etc/triggerhappy/triggers.d
-	mkdir -p $(TARGET_DIR)/etc/init.d
+	mkdir -p $(BINARIES_DIR)/batocera-target/etc/init.d
 	cp $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/core/batocera-triggerhappy/conf/multimedia_keys.conf          $(TARGET_DIR)/etc/triggerhappy/triggers.d
 	cp $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/core/batocera-triggerhappy/conf/multimedia_keys_disabled.conf $(TARGET_DIR)/etc/triggerhappy/triggers.d
-	install -m 0755 $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/core/batocera-triggerhappy/triggerhappy.service  $(TARGET_DIR)/etc/init.d/S50triggerhappy
+	install -m 0755 $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/core/batocera-triggerhappy/triggerhappy.service  $(BINARIES_DIR)/batocera-target/etc/init.d/S50triggerhappy
 endef
 
 define BATOCERA_TRIGGERHAPPY_INSTALL_RK3326_CONFIG
