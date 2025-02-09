f6fd2d9dd59416678969771d753b2c65111d2270
Daniel Martin <dmanlfc@gmail.com>
2025-02-09 18:59:14 +1000

    ryzenadj to v0.16.0

--- package/batocera/utils/ryzenadj/ryzenadj.mk
+++ package/batocera/utils/ryzenadj/ryzenadj.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RYZENADJ_VERSION = v0.15.0
+RYZENADJ_VERSION = v0.16.0
 RYZENADJ_SITE = $(call github,FlyGoat,RyzenAdj,$(RYZENADJ_VERSION))
 RYZENADJ_LICENSE = GPLv2
 RYZENADJ_DEPENDENCIES = pciutils libpciaccess
@@ -15,7 +15,6 @@ RYZENADJ_CONF_OPTS += -DARCHITECTURE_x86_64=ON
 
 define RYZENADJ_INSTALL_TARGET_CMDS
 	mkdir -p $(TARGET_DIR)/usr/bin
-
 	$(INSTALL) -D $(@D)/ryzenadj $(TARGET_DIR)/usr/bin/ryzenadj
 	$(INSTALL) -D $(@D)/libryzenadj.so $(TARGET_DIR)/usr/lib/libryzenadj.so
 endef
