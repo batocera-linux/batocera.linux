357b2789861e99b5648a59e58b679d11ccd6a5fc
Daniel Martin <dmanlfc@gmail.com>
2025-02-09 15:01:15 +1000

    snes9x to 24Jan

--- package/batocera/emulators/snes9x/snes9x.mk
+++ package/batocera/emulators/snes9x/snes9x.mk
@@ -3,8 +3,8 @@
 # snes9x
 #
 ################################################################################
-# Version: Commits on May 14, 2024
-SNES9X_VERSION = 8f41776532c407744c41e5d08444cb2dbd492c14
+# Version: Commits on Jan 24, 2025
+SNES9X_VERSION = 663738341a43e15f5e00fd8fd36672da6089a694
 SNES9X_SITE = https://github.com/snes9xgit/snes9x.git
 SNES9X_SITE_METHOD = git
 SNES9X_GIT_SUBMODULES = YES
@@ -18,10 +18,18 @@ SNES9X_CONF_OPTS = -DCMAKE_BUILD_TYPE=Release
 SNES9X_CONF_OPTS += -DBUILD_SHARED_LIBS=OFF
 SNES9X_CONF_OPTS += -DBUILD_STATIC_LIBS=ON
 
-ifeq ($(BR2_x86_64),y)
+ifeq ($(BR2_PACKAGE_XORG7),y)
     SNES9X_CONF_OPTS += -DUSE_X11=ON
+    SNES9X_DEPENDENCIES += xlib_libX11
 else
     SNES9X_CONF_OPTS += -DUSE_X11=OFF
+endif
+
+ifeq ($(BR2_PACKAGE_WAYLAND),y)
+    SNES9X_CONF_OPTS += -DUSE_WAYLAND=ON
+    SNES9X_DEPENDENCIES += wayland
+else
+    SNES9X_CONF_OPTS += -DUSE_WAYLAND=OFF
 endif
 
 define SNES9X_INSTALL_TARGET_CMDS
