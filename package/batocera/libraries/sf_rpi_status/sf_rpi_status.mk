9071f87879714970a1f2cc589ca2ace27d71dba0
Daniel Martin <dmanlfc@gmail.com>
2025-02-23 12:27:54 +1000

    update pironman5 to 1.1.3 - fixes disk capacity

--- package/batocera/libraries/sf_rpi_status/sf_rpi_status.mk
+++ package/batocera/libraries/sf_rpi_status/sf_rpi_status.mk
@@ -3,8 +3,8 @@
 # sf_rpi_status
 #
 ################################################################################
-# Version: Commits on Jul 4, 2024
-SF_RPI_STATUS_VERSION = 09b3e252308328b932d678d04d4d7c87798d2748
+# Version: Commits on Jan 16, 2025
+SF_RPI_STATUS_VERSION = 7f7fa9d42f2d62a6b5faf9efc6a01577fc2c38c1
 SF_RPI_STATUS_SITE = $(call github,sunfounder,sf_rpi_status,$(SF_RPI_STATUS_VERSION))
 SF_RPI_STATUS_SETUP_TYPE = setuptools
 SF_RPI_STATUS_LICENSE = GPL-2.0
