b887a962e8a353158f6bdb6343390171d2865337
Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>
2025-03-05 20:09:30 +0000

    some other hotkeys cleaning
    Signed-off-by: Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>

--- package/batocera/ports/sdlpop/sdlpop.mk
+++ package/batocera/ports/sdlpop/sdlpop.mk
@@ -12,14 +12,12 @@ SDLPOP_DEPENDENCIES = sdl2 sdl2_image
 
 define SDLPOP_INSTALL_TARGET_CMDS
 	mkdir -p $(TARGET_DIR)/usr/share/sdlpop
-	mkdir -p $(TARGET_DIR)/usr/share/evmapy
 	mkdir -p $(TARGET_DIR)/usr/share/sdlpop/cfg
 	$(INSTALL) -m 0755 $(@D)/prince -D $(TARGET_DIR)/usr/bin/SDLPoP
 	cp $(@D)/SDLPoP.ini $(TARGET_DIR)/usr/share/sdlpop/cfg/SDLPoP.ini
 	echo "# Blank cfg file for advanced configuration" > \
 	    $(TARGET_DIR)/usr/share/sdlpop/cfg/SDLPoP.cfg
 	cp -pr $(@D)/data $(TARGET_DIR)/usr/share/sdlpop/
-	cp $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/ports/sdlpop/sdlpop.keys $(TARGET_DIR)/usr/share/evmapy
 endef
 
 $(eval $(cmake-package))
