70962d3909ba0ca585f527a534557cd217447205
Daniel Martin <dmanlfc@gmail.com>
2025-03-26 20:48:26 +1000

    add the sdl3 packages in preparation to support updated emulators

---
+++ package/batocera/libraries/sdl3_ttf/sdl3_ttf.mk
@@ -0,0 +1,33 @@
+################################################################################
+#
+# sdl3_ttf
+#
+################################################################################
+
+SDL3_TTF_VERSION = release-3.2.0
+SDL3_TTF_SITE = https://github.com/libsdl-org/SDL_ttf
+SDL3_TTF_SITE_METHOD = git
+SDL3_TTF_GIT_SUBMODULES = yes
+SDL3_TTF_LICENSE = Zlib
+SDL3_TTF_LICENSE_FILES = LICENSE.txt
+SDL3_TTF_INSTALL_STAGING = YES
+
+SDL3_TTF_DEPENDENCIES += sdl3
+
+SDL3_TTF_CONF_OPTS = -DCMAKE_BUILD_TYPE=Release
+
+ifeq ($(BR2_PACKAGE_HARFBUZZ),y)
+SDL3_TTF_DEPENDENCIES += harfbuzz
+SDL3_TTF_CONF_OPTS += -DSDLTTF_HARFBUZZ=ON
+else
+SDL3_TTF_CONF_OPTS += -DSDLTTF_HARFBUZZ=OFF
+endif
+
+ifeq ($(BR2_PACKAGE_PLUTOSVG),y)
+SDL3_TTF_DEPENDENCIES += plutosvg
+SDL3_TTF_CONF_OPTS += -DSDLTTF_PLUTOSVG=ON
+else
+SDL3_TTF_CONF_OPTS += -DSDLTTF_PLUTOSVG=OFF
+endif
+
+$(eval $(cmake-package))
