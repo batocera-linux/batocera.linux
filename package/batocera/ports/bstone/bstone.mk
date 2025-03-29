ac47a699615886c6e1703a2039618d7d21413b25
Daniel Martin <dmanlfc@gmail.com>
2025-03-29 23:05:54 +1000

    start adding bstone options

--- package/batocera/ports/bstone/bstone.mk
+++ package/batocera/ports/bstone/bstone.mk
@@ -1,3 +1,16 @@
+#
+# This file is part of the batocera distribution (https://batocera.org).
+# Copyright (c) 2025+.
+#
+# This program is free software: you can redistribute it and/or modify  
+# it under the terms of the GNU General Public License as published by  
+# the Free Software Foundation, version 3.
+#
+# You should have received a copy of the GNU General Public License 
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+# YOU MUST KEEP THIS HEADER AS IT IS
+#
 ################################################################################
 #
 # bstone
