3c00478d6a151db51e1327aae138fbb173e5309b
Jason Millard <jsm174@gmail.com>
2025-03-31 20:54:18 -0400

    vpinball: bump vpinball to latest standalone

--- package/batocera/emulators/vpinball/vpinball.mk
+++ package/batocera/emulators/vpinball/vpinball.mk
@@ -3,9 +3,9 @@
 # vpinball
 #
 ################################################################################
-# Version: Commits on Mar 6, 2025
+# Version: Commits on Mar 31, 2025
 # uses standalone tree for now
-VPINBALL_VERSION = ececff7d0105d52c18872cbf0a84d0840408fc29
+VPINBALL_VERSION = c3e8134bebae535689c52f8d841f86a970a89acb
 VPINBALL_SITE = $(call github,vpinball,vpinball,$(VPINBALL_VERSION))
 VPINBALL_LICENSE = GPLv3+
 VPINBALL_LICENSE_FILES = LICENSE
