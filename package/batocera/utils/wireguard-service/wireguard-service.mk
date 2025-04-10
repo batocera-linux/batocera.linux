b8aa2c1836e70834ce6a6075b9a59c06e9d11485
Daniel Martin <dmanlfc@gmail.com>
2025-04-10 22:35:18 +1000

    fix packages

--- package/batocera/utils/wireguard-service/wireguard-service.mk
+++ package/batocera/utils/wireguard-service/wireguard-service.mk
@@ -1,6 +1,6 @@
 ################################################################################
 #
-# WIREGUARD_SERVICE
+# wireguard-service
 #
 ################################################################################
 
@@ -8,11 +8,15 @@ WIREGUARD_SERVICE_VERSION = 0.0.1
 WIREGUARD_SERVICE_SOURCE =
 WIREGUARD_SERVICE_SITE =
 
+WIREGUARD_SERVICE_DEPENDENCIES = wireguard-tools
+
 define WIREGUARD_SERVICE_INSTALL_TARGET_CMDS
-	rmdir $(TARGET_DIR)/etc/wireguard || rm $(TARGET_DIR)/etc/wireguard
+    mkdir -p $(TARGET_DIR)/etc
+	rm -rf $(TARGET_DIR)/etc/wireguard 2>/dev/null || true
 	ln -sf /userdata/system/wireguard $(TARGET_DIR)/etc/wireguard
 	mkdir -p $(TARGET_DIR)/usr/share/batocera/services
-	$(INSTALL) -Dm755 $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/utils/wireguard-service/wireguard $(TARGET_DIR)/usr/share/batocera/services/
+	$(INSTALL) -Dm755 $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/utils/wireguard-service/wireguard \
+	    $(TARGET_DIR)/usr/share/batocera/services/
 endef
 
 $(eval $(generic-package))
