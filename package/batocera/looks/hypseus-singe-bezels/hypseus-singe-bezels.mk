65233869337f0a0fc00efc5679860486d577783d
Tovarichtch <firech@gmail.com>
2025-04-20 11:47:42 -0400

    bump hypseus singe bezels

--- package/batocera/looks/hypseus-singe-bezels/hypseus-singe-bezels.mk
+++ package/batocera/looks/hypseus-singe-bezels/hypseus-singe-bezels.mk
@@ -4,7 +4,8 @@
 #
 ################################################################################
 
-HYPSEUS_SINGE_BEZELS_VERSION = b836daf3e294d15dc36a7267f86622b261d8bcba
+# Feb 13, 2025
+HYPSEUS_SINGE_BEZELS_VERSION = 016275c5785873c91adec0bb8397512a7f0a7a46
 HYPSEUS_SINGE_BEZELS_SITE = \
     $(call github,Tovarichtch,hypseus_singe_daphne_bezels,$(HYPSEUS_SINGE_BEZELS_VERSION))
 HYPSEUS_SINGE_BEZELS_LICENSE = LGPL-3.0
@@ -17,7 +18,7 @@ define HYPSEUS_SINGE_BEZELS_INSTALL_TARGET_CMDS
 	cp -f $(@D)/default.png $(TARGET_DIR)/usr/share/hypseus-singe/bezels
 	cp -f $(@D)/Daphne/*.png $(TARGET_DIR)/usr/share/hypseus-singe/bezels
 	cp -f $(@D)/Singe/*.png $(TARGET_DIR)/usr/share/hypseus-singe/bezels
-	cp -f $(@D)/Kimmy/*.png $(TARGET_DIR)/usr/share/hypseus-singe/bezels  
+	cp -f $(@D)/Kimmy/*.png $(TARGET_DIR)/usr/share/hypseus-singe/bezels
 endef
 
 $(eval $(generic-package))
