faee14b59e3c25be916bd87c848f45f1a2e82ffc
Daniel Martin <dmanlfc@gmail.com>
2025-04-27 23:21:41 +1000

    add opi firmware for the rk3588 boards

--- package/batocera/firmwares/firmware-orangepi/firmware-orangepi.mk
+++ package/batocera/firmwares/firmware-orangepi/firmware-orangepi.mk
@@ -3,15 +3,18 @@
 # firmware-orangepi
 #
 ################################################################################
-# Version: Commits on Oct 9, 2024
-FIRMWARE_ORANGEPI_VERSION = 75ea6fc5f3c454861b39b33823cb6876f3eca598
+# Version: Commits on Mar 19, 2025
+FIRMWARE_ORANGEPI_VERSION = db5e86200ae592c467c4cfa50ec0c66cbc40b158
 FIRMWARE_ORANGEPI_SITE = $(call github,orangepi-xunlong,firmware,$(FIRMWARE_ORANGEPI_VERSION))
 
-FIRMWARE_ORANGEPI_TARGET_DIR=$(TARGET_DIR)/lib/firmware/
+# We have newer BT code
+ifeq ($(BR2_PACKAGE_FIRMWARE_RADXA_RKWIFIBT),y)
+FIRMWARE_ORANGEPI_DEPENDENCIES = firmware-radxa-rkwifibt
+endif
 
 define FIRMWARE_ORANGEPI_INSTALL_TARGET_CMDS
-	mkdir -p $(FIRMWARE_ORANGEPI_TARGET_DIR)
-	cp -a $(@D)/* $(FIRMWARE_ORANGEPI_TARGET_DIR)/
+	mkdir -p $(TARGET_DIR)/lib/firmware
+	rsync -a --checksum $(@D)/ $(TARGET_DIR)/lib/firmware/
 endef
 
 $(eval $(generic-package))
