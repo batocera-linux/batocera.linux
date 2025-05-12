d7a27158201a862b452f092fc73529c2158abe0e
Rion <17915121+Redemp@users.noreply.github.com>
2025-05-12 23:47:39 +0200

    Update gamescope.mk
    Small update to mk file and latest release.

--- package/batocera/utils/gamescope/gamescope.mk
+++ package/batocera/utils/gamescope/gamescope.mk
@@ -4,21 +4,17 @@
 #
 ################################################################################
 
-GAMESCOPE_VERSION = 3.15.16
-GAMESCOPE_SITE = https://github.com/Plagman/gamescope.git
-GAMESCOPE_METHOD=git
-GAMESCOPE_SUBMODULES=YES
-GAMESCOPE_LICENSE = BSD-2-Clause
-GAMESCOPE_DEPENDENCIES = sdl2 libdrm libx11 libxdamage libxext libxfixes libxrender libxres libxtst
-
-GAMESCOPE_CONF_OPTS += -DCMAKE_BUILD_TYPE=Release
-GAMESCOPE_CONF_OPTS += -DBUILD_SHARED_LIBS=ON
-GAMESCOPE_CONF_OPTS += -DARCHITECTURE_x86_64=ON
+GAMESCOPE_VERSION = 3.16.4
+GAMESCOPE_SITE = https://github.com/ValveSoftware/gamescope
+GAMESCOPE_SITE_METHOD = git
+GAMESCOPE_GIT_SUBMODULES=YES
+GAMESCOPE_DEPENDENCIES = sdl2 libdrm wayland wayland-protocols glm hwdata pipewire xlib_libXres  xlib_libXmu stb seatd xwayland libdecor
+GAMESCOPE_CONF_OPTS += -Denable_openvr_support=FALSE
 
 define GAMESCOPE_INSTALL_TARGET_CMDS
 	mkdir -p $(TARGET_DIR)/usr/bin
 
-	$(INSTALL) -D $(@D)/gamescope $(TARGET_DIR)/usr/bin/gamescope
+	$(INSTALL) -D $(@D)/build/src/gamescope $(TARGET_DIR)/usr/bin/gamescope
 endef
 
 $(eval $(meson-package))
