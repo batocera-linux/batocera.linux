f44c6373da28a2759d21033dba754a8ee1d6e6ea
Tovarichtch <firech@gmail.com>
2025-06-22 14:07:10 -0400

    bump hypseus singe bezels

--- package/batocera/looks/hypseus-singe-bezels/hypseus-singe-bezels.mk
+++ package/batocera/looks/hypseus-singe-bezels/hypseus-singe-bezels.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-# Feb 13, 2025
-HYPSEUS_SINGE_BEZELS_VERSION = 016275c5785873c91adec0bb8397512a7f0a7a46
+# Jun 22, 2025
+HYPSEUS_SINGE_BEZELS_VERSION = 97a8e2c792d270cdf3b2c5a0d8ba410b9ee8fd50
 HYPSEUS_SINGE_BEZELS_SITE = \
     $(call github,Tovarichtch,hypseus_singe_daphne_bezels,$(HYPSEUS_SINGE_BEZELS_VERSION))
 HYPSEUS_SINGE_BEZELS_LICENSE = LGPL-3.0
