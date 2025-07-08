cdcad35eef83ccbf60e42b3af19c549a6927d6d2
Daniel Martin <dmanlfc@gmail.com>
2025-07-08 23:37:26 +1000

    linux fw to 20250627

--- package/batocera/firmwares/alllinuxfirmwares/alllinuxfirmwares.mk
+++ package/batocera/firmwares/alllinuxfirmwares/alllinuxfirmwares.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-ALLLINUXFIRMWARES_VERSION = 20250613
+ALLLINUXFIRMWARES_VERSION = 20250627
 ALLLINUXFIRMWARES_SOURCE = linux-firmware-$(ALLLINUXFIRMWARES_VERSION).tar.gz
 ALLLINUXFIRMWARES_SITE = https://www.kernel.org/pub/linux/kernel/firmware
 
