4781f5a9500efc29c0ca07efa65bcfce526b9543
Daniel Martin <dmanlfc@gmail.com>
2025-07-31 18:25:52 +1000

    CoolPi 4b, fix wifi & bt

--- package/batocera/network/aic8800/aic8800.mk
+++ package/batocera/network/aic8800/aic8800.mk
@@ -15,17 +15,26 @@ AIC8800_MODULE_SUBDIRS = src/SDIO/driver_fw/driver/aic8800
 # set configs to be sure
 AIC8800_MODULE_MAKE_OPTS = \
 	CONFIG_AIC_WLAN_SUPPORT=y \
-	CONFIG_AIC_FW_PATH="/lib/firmware/aic8800d80/" \
+	CONFIG_AIC_FW_PATH="/lib/firmware/aic8800" \
 	CONFIG_AIC8800_WLAN_SUPPORT=m \
 	CONFIG_AIC8800_BTLPM_SUPPORT=m \
 	USER_EXTRA_CFLAGS="-DCONFIG_$(call qstrip,$(BR2_ENDIAN))_ENDIAN \
 		-Wno-error"
 
-# only copy firmware for the SDIO for now
+# only copy all firmware for the SDIO for now
+# Rock 5c uses aic8800D80 & CoolPi 4b uses aic8800
 define AIC8800_FIRMWARE_ETC
-    mkdir -p $(TARGET_DIR)/lib/firmware/aic8800d80
+    mkdir -p $(TARGET_DIR)/lib/firmware/aic8800
+	cp -f $(@D)/src/SDIO/driver_fw/fw/aic8800/* \
+	    $(TARGET_DIR)/lib/firmware/aic8800
 	cp -f $(@D)/src/SDIO/driver_fw/fw/aic8800D80/* \
-	    $(TARGET_DIR)/lib/firmware/aic8800d80
+	    $(TARGET_DIR)/lib/firmware/aic8800
+	cp -f $(@D)/src/SDIO/driver_fw/fw/aic8800D80X2/* \
+	    $(TARGET_DIR)/lib/firmware/aic8800
+	cp -f $(@D)/src/SDIO/driver_fw/fw/aic8800DC/* \
+	    $(TARGET_DIR)/lib/firmware/aic8800
+	# remove orangepi firmware duplication here...
+	rm -rf $(TARGET_DIR)/lib/firmware/aic8800d80
 endef
 
 AIC8800_POST_INSTALL_TARGET_HOOKS = AIC8800_FIRMWARE_ETC
