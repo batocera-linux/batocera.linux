514464a30c54b72d643bee35a2ee1955eb225a9f
Daniel Martin <dmanlfc@gmail.com>
2025-08-01 21:34:44 +1000

    fix the aic8800 driver & firmware duplication

--- package/batocera/network/aic8800/aic8800.mk
+++ package/batocera/network/aic8800/aic8800.mk
@@ -3,21 +3,29 @@
 # aic8800
 #
 ################################################################################
-# Version: Commits on Jun 25, 2024
-AIC8800_VERSION = ccba7fffed8554fe861bd631ff6f852d2d6eec39
-AIC8800_SITE = $(call github,batocera-linux,aic8800,$(AIC8800_VERSION))
-AIC8800_LICENSE = GPL-2.0
+# Version: Commits on Jul 22, 2025
+AIC8800_VERSION = dd2afa18bc47dfde591ee981b57bd7b22d017a0d
+AIC8800_SITE = $(call github,radxa-pkg,aic8800,$(AIC8800_VERSION))
+AIC8800_LICENSE = GPL-3.0
 AIC8800_LICENSE_FILES = LICENSE
 
+# build SDIO only
+AIC8800_MODULE_SUBDIRS = src/SDIO/driver_fw/driver/aic8800
+
+# set configs to be sure
 AIC8800_MODULE_MAKE_OPTS = \
-	AIC_WLAN_SUPPORT=m \
+	CONFIG_AIC_WLAN_SUPPORT=y \
+	CONFIG_AIC_FW_PATH="/lib/firmware/aic8800d80/" \
+	CONFIG_AIC8800_WLAN_SUPPORT=m \
+	CONFIG_AIC8800_BTLPM_SUPPORT=m \
 	USER_EXTRA_CFLAGS="-DCONFIG_$(call qstrip,$(BR2_ENDIAN))_ENDIAN \
 		-Wno-error"
 
-# only copy firmware for the Rock 5c for now
+# only copy firmware for the SDIO for now
 define AIC8800_FIRMWARE_ETC
-    mkdir -p $(TARGET_DIR)/lib/firmware/aic8800D80
-	cp -f $(@D)/firmware/aic8800D80/* $(TARGET_DIR)/lib/firmware/aic8800D80
+    mkdir -p $(TARGET_DIR)/lib/firmware/aic8800d80
+	cp -f $(@D)/src/SDIO/driver_fw/fw/aic8800D80/* \
+	    $(TARGET_DIR)/lib/firmware/aic8800d80
 endef
 
 AIC8800_POST_INSTALL_TARGET_HOOKS = AIC8800_FIRMWARE_ETC
