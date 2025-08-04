83d243368d7cb29804c2c35868a0dcc690835f1b
Tovarichtch <firech@gmail.com>
2025-08-03 21:10:27 -0400

    bump hypseus bezel
    1 game added (tierras)

--- package/batocera/looks/hypseus-singe-bezels/hypseus-singe-bezels.mk
+++ package/batocera/looks/hypseus-singe-bezels/hypseus-singe-bezels.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-# Jun 22, 2025
-HYPSEUS_SINGE_BEZELS_VERSION = 97a8e2c792d270cdf3b2c5a0d8ba410b9ee8fd50
+# Aug 03, 2025
+HYPSEUS_SINGE_BEZELS_VERSION = 509aed4e75954a918cc34a0ba89efb2c7ae868b3
 HYPSEUS_SINGE_BEZELS_SITE = \
     $(call github,Tovarichtch,hypseus_singe_daphne_bezels,$(HYPSEUS_SINGE_BEZELS_VERSION))
 HYPSEUS_SINGE_BEZELS_LICENSE = LGPL-3.0
