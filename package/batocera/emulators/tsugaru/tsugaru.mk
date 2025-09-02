3792498b3297122d0869ed42c6fe45b6dce4ae0e
Daniel Martin <dmanlfc@gmail.com>
2025-09-02 21:08:23 +1000

    fix no sound with tsugaru

--- package/batocera/emulators/tsugaru/tsugaru.mk
+++ package/batocera/emulators/tsugaru/tsugaru.mk
@@ -6,14 +6,16 @@
 
 TSUGARU_VERSION = v20250513
 TSUGARU_SITE = $(call github,captainys,TOWNSEMU,$(TSUGARU_VERSION))
-TSUGARU_DEPENDENCIES = libglu
 TSUGARU_LICENSE = GPLv2
 TSUGARU_SUPPORTS_IN_SOURCE_BUILD = NO
 # CMakeLists.txt in src subfolder
 TSUGARU_SUBDIR = src
 
+TSUGARU_DEPENDENCIES = alsa-lib libglu
+
 TSUGARU_CONF_OPTS += -DCMAKE_BUILD_TYPE=Release
-TSUGARU_CONF_OPTS += -DBUILD_SHARED_LIBS=FALSE
+TSUGARU_CONF_OPTS += -DBUILD_SHARED_LIBS=OFF
+
 TSUGARU_CONF_ENV += LDFLAGS=-lpthread
 
 define TSUGARU_INSTALL_TARGET_CMDS
@@ -23,7 +25,7 @@ define TSUGARU_INSTALL_TARGET_CMDS
     # evampy
     mkdir -p $(TARGET_DIR)/usr/share/evmapy
     cp $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/emulators/tsugaru/fmtowns.keys \
-    $(TARGET_DIR)/usr/share/evmapy
+        $(TARGET_DIR)/usr/share/evmapy
 endef
 
 $(eval $(cmake-package))
