cd26b5bc7a8c399c43af7c60ac61af4b7ccf6cc1
Daniel Martin <dmanlfc@gmail.com>
2025-10-17 20:30:12 +1000

    update xash3d-fwgs to Oct 17 build

--- batocera-Changelog.md
+++ batocera-Changelog.md
@@ -37,6 +37,7 @@
 - Taradino to 20250816
 - TheXTech to v1.3.7.1-2
 - TheForceEngine to v1.22.420
+- Xash3D FWGS engine to Oct 17 build
 - Xemu to v0.8.106
 - Ymir to 0.2.0
 ### System
--- package/batocera/ports/xash3d/xash3d-fwgs/xash3d-fwgs.mk
+++ package/batocera/ports/xash3d/xash3d-fwgs/xash3d-fwgs.mk
@@ -3,8 +3,8 @@
 # xash3d-fwgs
 #
 ################################################################################
-# Version: Commits on Feb 20, 2025
-XASH3D_FWGS_VERSION = 4c15cceb324cae6d9314b4a7f1ee83c2182ce99b
+# Version: Commits on Oct 17, 2025
+XASH3D_FWGS_VERSION = 7315865d34eba668055f990fca65e43ce66c60e7
 XASH3D_FWGS_SITE = https://github.com/FWGS/xash3d-fwgs.git
 XASH3D_FWGS_SITE_METHOD = git
 XASH3D_FWGS_GIT_SUBMODULES = yes
@@ -15,7 +15,6 @@ XASH3D_FWGS_DEPENDENCIES += freetype fontconfig hlsdk-xash3d
 XASH3D_FWGS_CONF_OPTS += --build-type=release \
   --enable-packaging \
   --sdl2=$(STAGING_DIR)/usr/ \
-  --disable-vgui \
   --disable-menu-changegame
 
 ifeq ($(BR2_ARCH_IS_64),y)
