806236cec54fc92d4145f7ba120d0f0202fb2cfd
Daniel Martin <dmanlfc@gmail.com>
2025-10-17 20:39:20 +1000

    Xemu to v0.8.108

--- package/batocera/emulators/xemu/xemu.mk
+++ package/batocera/emulators/xemu/xemu.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-XEMU_VERSION = v0.8.106
+XEMU_VERSION = v0.8.108
 XEMU_SITE = https://github.com/xemu-project/xemu.git
 XEMU_SITE_METHOD = git
 XEMU_GIT_SUBMODULES = YES
