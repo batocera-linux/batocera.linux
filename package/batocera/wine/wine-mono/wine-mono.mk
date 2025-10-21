cfe003e9a552a390658ede7b347171fe2dcec1ba
Daniel Martin <dmanlfc@gmail.com>
2025-10-21 20:13:10 +1000

    fix wine mono location

--- package/batocera/wine/wine-mono/wine-mono.mk
+++ package/batocera/wine/wine-mono/wine-mono.mk
@@ -6,17 +6,14 @@
 
 WINE_MONO_VERSION = 10.3.0
 WINE_MONO_SOURCE = wine-mono-$(WINE_MONO_VERSION)-x86.tar.xz
-WINE_MONO_SITE = https://github.com/wine-mono/wine-mono/releases/download/wine-mono-$(WINE_MONO_VERSION)
+WINE_MONO_SITE = \
+    https://github.com/wine-mono/wine-mono/releases/download/wine-mono-$(WINE_MONO_VERSION)
 
 define WINE_MONO_INSTALL_TARGET_CMDS
     mkdir -p \
-	    $(TARGET_DIR)/usr/wine/wine-tkg/share/wine/mono/wine-mono-$(WINE_MONO_VERSION)
-	mkdir -p \
-	    $(TARGET_DIR)/usr/wine/wine-proton/share/wine/mono/wine-mono-$(WINE_MONO_VERSION)
+	    $(TARGET_DIR)/usr/share/wine/wine-mono-$(WINE_MONO_VERSION)
 	rsync -a --exclude='.*' $(@D)/ \
-	    $(TARGET_DIR)/usr/wine/wine-tkg/share/wine/mono/wine-mono-$(WINE_MONO_VERSION)
-	rsync -a --exclude='.*' $(@D)/ \
-	    $(TARGET_DIR)/usr/wine/wine-proton/share/wine/mono/wine-mono-$(WINE_MONO_VERSION)
+	    $(TARGET_DIR)/usr/share/wine/wine-mono-$(WINE_MONO_VERSION)
 endef
 
 $(eval $(generic-package))
