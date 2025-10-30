8dea23628d005c0ca70b2193f9ede181420f142a
Daniel Martin <dmanlfc@gmail.com>
2025-10-30 19:18:54 +1000

    powkiddy x55 defaults

--- package/batocera/controllers/pads/anbernic-gpio-pad/anbernic-gpio-pad.mk
+++ package/batocera/controllers/pads/anbernic-gpio-pad/anbernic-gpio-pad.mk
@@ -3,13 +3,18 @@
 # anbernic-gpio-pad
 #
 ################################################################################
-ANBERNIC_GPIO_PAD_VERSION = 1
+ANBERNIC_GPIO_PAD_VERSION = 1.1
 ANBERNIC_GPIO_PAD_LICENSE = GPL
 ANBERNIC_GPIO_PAD_SOURCE=
 
+ANBERNIC_GPIO_PAD_PATH = \
+    $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/controllers/pads/anbernic-gpio-pad
+
 define ANBERNIC_GPIO_PAD_INSTALL_TARGET_CMDS
-	$(INSTALL) -m 0644 -D $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/controllers/pads/anbernic-gpio-pad/99-anbernic-gpio-pad.rules $(TARGET_DIR)/etc/udev/rules.d/99-anbernic-gpio-pad.rules
-	$(INSTALL) -m 0755 -D $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/controllers/pads/anbernic-gpio-pad/anbernic-gpio-pad-add $(TARGET_DIR)/usr/bin/anbernic-gpio-pad-add
+	$(INSTALL) -m 0644 -D $(ANBERNIC_GPIO_PAD_PATH)/99-anbernic-gpio-pad.rules \
+	    $(TARGET_DIR)/etc/udev/rules.d/99-anbernic-gpio-pad.rules
+	$(INSTALL) -m 0755 -D $(ANBERNIC_GPIO_PAD_PATH)/anbernic-gpio-pad-add \
+	    $(TARGET_DIR)/usr/bin/anbernic-gpio-pad-add
 endef
 
 $(eval $(generic-package))
