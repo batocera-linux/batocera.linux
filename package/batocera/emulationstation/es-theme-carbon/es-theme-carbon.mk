f3de106c749134ae8465ccf568c7422bd08a43e2
lbrpdx <lionel.bringuier@gmail.com>
2025-11-05 14:28:27 -0800

    Bump es-carbon theme

--- package/batocera/emulationstation/es-theme-carbon/es-theme-carbon.mk
+++ package/batocera/emulationstation/es-theme-carbon/es-theme-carbon.mk
@@ -3,8 +3,8 @@
 # es-theme-carbon
 #
 ################################################################################
-# Version: Commits on Jul 5, 2025
-ES_THEME_CARBON_VERSION = 6391092f4ee2ee8a1cc7814a710ee1cc4d9be9c0
+# Version: Commits on Nov 5, 2025
+ES_THEME_CARBON_VERSION = d03375dfd6af53ba5f2bcd3ced13df9032afc54e
 ES_THEME_CARBON_SITE = $(call github,fabricecaruso,es-theme-carbon,$(ES_THEME_CARBON_VERSION))
 
 define ES_THEME_CARBON_INSTALL_TARGET_CMDS
