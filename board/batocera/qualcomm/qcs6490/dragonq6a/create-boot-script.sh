18d328dec04b31877cc841d586c373143ffcd184
Ettore Chimenti <ek5.chimenti@gmail.com>
2025-11-17 09:35:05 +0000

    qualcomm: add initial support for Radxa Dragon Q6A
    Adding initial support for QCS6490-based SBC Radxa Dragon Q6A.
    Based on Armbian patches for Linux Kernel 6.17.7.
    
    Working:
     - Boot
     - Serial
     - GPU
     - USB
     - HDMI
     - Audio (Jack/HDMI)
     - Ethernet
     - SDcard
     - Wifi/BT
    
    Not Tested:
     - eMMC/UFS (needs 4K LBA support in genimage, probably fine with manual partitioning)
     - PCIe/NVMe

---
+++ board/batocera/qualcomm/qcs6490/dragonq6a/create-boot-script.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# HOST_DIR = host dir
+# BOARD_DIR = board specific dir
+# BUILD_DIR = base dir/build
+# BINARIES_DIR = images dir
+# TARGET_DIR = target dir
+# BATOCERA_BINARIES_DIR = batocera binaries sub directory
+
+HOST_DIR=$1
+BOARD_DIR=$2
+BUILD_DIR=$3
+BINARIES_DIR=$4
+TARGET_DIR=$5
+BATOCERA_BINARIES_DIR=$6
+
+mkdir -p "${BATOCERA_BINARIES_DIR}/boot/boot"     || exit 1
+
+cp "${BINARIES_DIR}/Image"              "${BATOCERA_BINARIES_DIR}/boot/boot/Image"                      || exit 1
+cp "${BINARIES_DIR}/initrd.lz4"         "${BATOCERA_BINARIES_DIR}/boot/boot/initrd.lz4"                 || exit 1
+cp "${BINARIES_DIR}/rootfs.squashfs"    "${BATOCERA_BINARIES_DIR}/boot/boot/batocera.update"            || exit 1
+
+cp "${BINARIES_DIR}/qcs6490-radxa-dragon-q6a.dtb"       "${BATOCERA_BINARIES_DIR}/boot/boot/"           || exit 1
+cp -f "${BOARD_DIR}/grub.cfg"                           "${BINARIES_DIR}/efi-part/EFI/BOOT/grub.cfg"    || exit 1
+cp -r "${BINARIES_DIR}/efi-part/EFI/"                   "${BATOCERA_BINARIES_DIR}/boot/"                || exit 1
+
+exit 0
