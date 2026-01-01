8e306116f54ae4f2f7d6974b415e280a2ca0d436
WizzardSK <42868978+WizzardSK@users.noreply.github.com>
2026-01-01 12:07:39 +0100

    Update tic80.mk

--- package/batocera/emulators/tic80/tic80.mk
+++ package/batocera/emulators/tic80/tic80.mk
@@ -4,8 +4,8 @@ TIC80_SITE = https://github.com/nesbox/TIC-80
 TIC80_LICENSE = MIT
 TIC80_DEPENDENCIES = libpipewire-0.3-dev libwayland-dev libsdl2-dev ruby-dev libcurl4-openssl-dev libglvnd-dev libglu1-mesa-dev freeglut3-dev
 
-TIC80_CONF_OPTS = -BUILD_SDLGPU=On
-TIC80_CONF_OPTS += -BUILD_WITH_ALL=On
-TIC80_CONF_OPTS += -BUILD_STATIC=On
+TIC80_CONF_OPTS = -DBUILD_SDLGPU=On
+TIC80_CONF_OPTS += -DBUILD_WITH_ALL=On
+TIC80_CONF_OPTS += -DBUILD_STATIC=On
 
 $(eval $(generic-package))
