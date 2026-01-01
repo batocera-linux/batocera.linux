25bc2570e5024ff364dcd7598f65f575c0de4c5c
WizzardSK <42868978+WizzardSK@users.noreply.github.com>
2026-01-01 11:43:51 +0100

    Create tic80.mk

---
+++ package/batocera/emulators/tic80/tic80.mk
@@ -0,0 +1,11 @@
+TIC80_VERSION = 1.1.2837
+TIC80_SOURCE = 	archive/refs/tags/v1.1.2837.tar.gz
+TIC80_SITE = https://github.com/nesbox/TIC-80
+TIC80_LICENSE = MIT
+TIC80_DEPENDENCIES = libpipewire-0.3-dev libwayland-dev libsdl2-dev ruby-dev libcurl4-openssl-dev libglvnd-dev libglu1-mesa-dev freeglut3-dev
+
+TIC80_CONF_OPTS = -BUILD_SDLGPU=On
+TIC80_CONF_OPTS += -BUILD_WITH_ALL=On
+TIC80_CONF_OPTS += -BUILD_STATIC=On
+
+$(eval $(generic-package))
