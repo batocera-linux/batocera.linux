4d77dc25a8e5cf07d8dcb2e4403262cf03862093
WizzardSK <42868978+WizzardSK@users.noreply.github.com>
2026-01-11 20:16:54 +0100

    Update libretro-ep128emu.mk

--- package/batocera/emulators/retroarch/libretro/libretro-ep128emu/libretro-ep128emu.mk
+++ package/batocera/emulators/retroarch/libretro/libretro-ep128emu/libretro-ep128emu.mk
@@ -5,15 +5,16 @@
 ################################################################################
 
 LIBRETRO_EP128EMU_VERSION = master
-LIBRETRO_EP128EMU_SITE = https://github.com/libretro/ep128emu-core.git
+LIBRETRO_EP128EMU_SITE = https://github.com/libretro/ep128emu.git
 LIBRETRO_EP128EMU_SITE_METHOD = git
 LIBRETRO_EP128EMU_LICENSE = GPLv2
 LIBRETRO_EP128EMU_PLATFORM = $(LIBRETRO_PLATFORM)
+LIBRETRO_EP128EMU_DEPENDENCIES = host-git
 
 define LIBRETRO_EP128EMU_BUILD_CMDS
 	$(TARGET_CONFIGURE_OPTS) $(MAKE) \
-		CXX="$(TARGET_CXX)" \
 		CC="$(TARGET_CC)" \
+		CXX="$(TARGET_CXX)" \
 		-C $(@D)/src/libretro \
 		platform="$(LIBRETRO_EP128EMU_PLATFORM)"
 endef
