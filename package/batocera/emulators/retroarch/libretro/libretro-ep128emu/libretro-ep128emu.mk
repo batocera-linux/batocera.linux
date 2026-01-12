22250cda9dd106db81f69268c153d17929ad4f83
WizzardSK <42868978+WizzardSK@users.noreply.github.com>
2026-01-12 18:34:51 +0100

    Update libretro-ep128emu.mk

--- package/batocera/emulators/retroarch/libretro/libretro-ep128emu/libretro-ep128emu.mk
+++ package/batocera/emulators/retroarch/libretro/libretro-ep128emu/libretro-ep128emu.mk
@@ -5,7 +5,8 @@
 ################################################################################
 
 LIBRETRO_EP128EMU_VERSION = 812dd1b35225b64d9392a3e21de28c3a5861ddf7
-LIBRETRO_EP128EMU_SITE = $(call github,libretro,ep128emu-core,$(LIBRETRO_EP128EMU_VERSION))
+LIBRETRO_EP128EMU_SITE = https://github.com/libretro/ep128emu-core/archive/$(LIBRETRO_EP128EMU_VERSION).tar.gz
+LIBRETRO_EP128EMU_SITE_METHOD = tar
 LIBRETRO_EP128EMU_LICENSE = GPL-2.0
 LIBRETRO_EP128EMU_DEPENDENCIES += retroarch
 
@@ -15,13 +16,13 @@ define LIBRETRO_EP128EMU_BUILD_CMDS
 	$(TARGET_CONFIGURE_OPTS) $(MAKE) \
 		CC="$(TARGET_CC)" \
 		CXX="$(TARGET_CXX)" \
-		-C $(@D)/libretro \
+		-C $(@D)/ep128emu-core-$(LIBRETRO_EP128EMU_VERSION)/libretro \
 		platform="$(LIBRETRO_EP128EMU_PLATFORM)"
 endef
 
 define LIBRETRO_EP128EMU_INSTALL_TARGET_CMDS
 	$(INSTALL) -D \
-		$(@D)/src/libretro/ep128emu_libretro.so \
+		$(@D)/ep128emu-core-$(LIBRETRO_EP128EMU_VERSION)/libretro/ep128emu_libretro.so \
 		$(TARGET_DIR)/usr/lib/libretro/ep128emu_libretro.so
 endef
 
