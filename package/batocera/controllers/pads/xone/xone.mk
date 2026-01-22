6bb55e6f5fc889310704933f6cdb28bbb75a3678
Daniel Martin <dmanlfc@gmail.com>
2026-01-22 18:13:13 +1000

    xone v0.5.2

--- package/batocera/controllers/pads/xone/xone.mk
+++ package/batocera/controllers/pads/xone/xone.mk
@@ -8,17 +8,18 @@
 ifeq ($(BR2_PACKAGE_HOST_LINUX_HEADERS_CUSTOM_5_4)$(BR2_PACKAGE_HOST_LINUX_HEADERS_CUSTOM_5_10),y)
     XONE_VERSION = bbf0dcc484c3f5611f4e375da43e0e0ef08f3d18
 else
-    # Version: Commits on Oct 6, 2025
-    XONE_VERSION = 3e53480d835eeb31049ef8a3b397f3180a1db9dc
+    XONE_VERSION = v0.5.2
 endif
 
 XONE_SITE = $(call github,dlundqvist,xone,$(XONE_VERSION))
 XONE_DEPENDENCIES = host-libcurl host-cabextract libusb
 
 define XONE_INSTALL_TARGET_CMDS
+mkdir -p $(TARGET_DIR)/etc/modprobe.d
 $(INSTALL) -D -m 0644 $(@D)/install/modprobe.conf \
     $(TARGET_DIR)/etc/modprobe.d/xone-blacklist.conf
 # copy firmware
+mkdir -p $(TARGET_DIR)/lib/firmware
 cp $(BR2_EXTERNAL_BATOCERA_PATH)/package/batocera/controllers/pads/xone/FW_ACC_00U.bin \
     $(TARGET_DIR)/lib/firmware/xow_dongle.bin
 # create symbolic link
