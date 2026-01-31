3e79db355d927dc053e5fc91915e096cab13a6d2
Tovarichtch <firech@gmail.com>
2026-01-31 09:44:23 -0500

    bump pcsx2 to 2.6.3

--- package/batocera/emulators/pcsx2/pcsx2.mk
+++ package/batocera/emulators/pcsx2/pcsx2.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PCSX2_VERSION = v2.6.2
+PCSX2_VERSION = v2.6.3
 PCSX2_SITE = https://github.com/pcsx2/pcsx2.git
 PCSX2_SITE_METHOD = git
 PCSX2_GIT_SUBMODULES = YES
