8fc8c6fa8aa5d2199c707cae0c2e45a3b22a2cbc
lbrpdx <lionel.bringuier@gmail.com>
2026-03-17 12:41:42 -0700

    Bump notice (refresh with new hotkeys)

--- package/batocera/core/batocera-notice/batocera-notice.mk
+++ package/batocera/core/batocera-notice/batocera-notice.mk
@@ -3,7 +3,7 @@
 # Batocera notice
 #
 ################################################################################
-BATOCERA_NOTICE_VERSION = e6241340d292dee20406a0567262a78ff34d45f7
+BATOCERA_NOTICE_VERSION = d8877e6282868d30e1c2d70c28ec1d1e2491d5b7
 BATOCERA_NOTICE_SITE = $(call github,batocera-linux,batocera-notice,$(BATOCERA_NOTICE_VERSION))
 
 define BATOCERA_NOTICE_INSTALL_TARGET_CMDS
