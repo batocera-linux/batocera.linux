ded934e33bdee59a14cc61474dca50b22ac1684c
Daniel Martin <dmanlfc@gmail.com>
2026-03-24 20:03:50 +1000

    fix lr-gearcoleco

--- package/batocera/emulators/retroarch/libretro/libretro-gearcoleco/libretro-gearcoleco.mk
+++ package/batocera/emulators/retroarch/libretro/libretro-gearcoleco/libretro-gearcoleco.mk
@@ -8,22 +8,12 @@ LIBRETRO_GEARCOLECO_VERSION = 1.5.5
 LIBRETRO_GEARCOLECO_SITE = $(call github,drhelius,Gearcoleco,$(LIBRETRO_GEARCOLECO_VERSION))
 LIBRETRO_GEARCOLECO_LICENSE = GPLv3
 
-ifeq ($(BR2_PACKAGE_BATOCERA_TARGET_BCM2835),y)
-GEARCOLECO_PLATFORM = rpi1
-else ifeq ($(BR2_PACKAGE_BATOCERA_TARGET_BCM2836),y)
-GEARCOLECO_PLATFORM = rpi2
-else ifeq ($(BR2_PACKAGE_BATOCERA_TARGET_BCM2837),y)
-GEARCOLECO_PLATFORM = rpi3
-else
-GEARCOLECO_PLATFORM = unix
-endif
-
 define LIBRETRO_GEARCOLECO_BUILD_CMDS
-    $(TARGET_CONFIGURE_OPTS) $(MAKE) -C $(@D)/platforms/libretro \
-        platform=$(GEARCOLECO_PLATFORM)
+    $(TARGET_CONFIGURE_OPTS) $(MAKE) -C $(@D)/platforms/libretro platform=unix
 endef
 
 define LIBRETRO_GEARCOLECO_INSTALL_TARGET_CMDS
+    mkdir -p $(TARGET_DIR)/usr/lib/libretro
     $(INSTALL) -D -m 0755 \
         $(@D)/platforms/libretro/gearcoleco_libretro.so \
         $(TARGET_DIR)/usr/lib/libretro/gearcoleco_libretro.so
