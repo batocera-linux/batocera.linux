bf795a8a2ea0415d9c6086c00bc83f7a506ca9c8
Mikhailzrick <90733003+Mikhailzrick@users.noreply.github.com>
2026-05-21 07:41:40 -0500

    batocera-cpu-temp

--- package/batocera/core/batocera-scripts/batocera-scripts.mk
+++ package/batocera/core/batocera-scripts/batocera-scripts.mk
@@ -88,6 +88,7 @@ define BATOCERA_SCRIPTS_INSTALL_TARGET_CMDS
     install -m 0755 $(BATOCERA_SCRIPTS_PATH)/scripts/batocera-storage-manager           $(TARGET_DIR)/usr/bin/
     install -m 0755 $(BATOCERA_SCRIPTS_PATH)/scripts/batocera-storage-udev              $(TARGET_DIR)/usr/bin/
     install -m 0755 $(BATOCERA_SCRIPTS_PATH)/scripts/batocera-keyboard                  $(TARGET_DIR)/usr/bin/
+    install -m 0755 $(BATOCERA_SCRIPTS_PATH)/scripts/batocera-cpu-temp                  $(TARGET_DIR)/usr/bin/
 endef
 
 define BATOCERA_SCRIPTS_INSTALL_MOUSE
