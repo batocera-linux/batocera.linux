a0ba31b175a2f9f740d8899d8dc7e84d5090836a
Simon Daniel <simondaniel321@gmail.com>
2026-06-16 18:12:00 +0200

    wait for es startup in nfc-on-connect default script

--- package/batocera/utils/batocera-nfc/scripts/on-nfc-connect/default.sh
+++ package/batocera/utils/batocera-nfc/scripts/on-nfc-connect/default.sh
@@ -2,6 +2,18 @@
 
 TAG=$1
 
+# wait for ES webserver to be ready (up to 60 seconds)
+TRIES=0
+while [ $TRIES -lt 60 ]; do
+    curl -s http://127.0.0.1:1234/runningGame > /dev/null 2>&1
+    if [ $? -eq 0 ]; then
+        break
+    fi
+    sleep 1
+    TRIES=$((TRIES + 1))
+done
+
+
 # start the game only if none is already running
 CURRENT=$(curl -s http://127.0.0.1:1234/runningGame | jq -r '.name')
 if test "${CURRENT}" = null
