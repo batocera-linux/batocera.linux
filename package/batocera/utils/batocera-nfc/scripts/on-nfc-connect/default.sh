3d8522c064a75ded5bc829699b325667fcf2295e
crcerror <git@duftpirat.de>
2026-06-17 20:42:45 +0200

    nfc: wait for es startup if a nfc token is on a reader at boot, the default nfc-on-connect script fails to start a game because emulationstation is not ready yet. This fix waits 60s to detect emulationstation.ready-file with inode tool This is a yet more solid solution than https://github.com/batocera-linux/batocera.linux/pull/15889
    edit: clean ready-flag on stop-command

--- package/batocera/utils/batocera-nfc/scripts/on-nfc-connect/default.sh
+++ package/batocera/utils/batocera-nfc/scripts/on-nfc-connect/default.sh
@@ -2,6 +2,10 @@
 
 TAG=$1
 
+# check for emulationstation.ready-file: https://stackoverflow.com/questions/70132359
+# watch dir tmp for creation of any file --include means only emulationstation.ready - regex-include always searches for full path so /file.name$ is valid
+! [ -e /tmp/emulationstation.ready ] && inotifywait -t 60 -e create --include '/emulationstation\.ready$' /tmp
+
 # start the game only if none is already running
 CURRENT=$(curl -s http://127.0.0.1:1234/runningGame | jq -r '.name')
 if test "${CURRENT}" = null
