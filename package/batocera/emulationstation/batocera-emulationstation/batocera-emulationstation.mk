47bfd1ecde9ddd8974661678fb156a9f490b4319
Imanol-Mikel Barba Sabariego <imanol@imanolbarba.net>
2026-06-17 23:35:16 +0200

    Bump ES version again
    Previous one didn't build

--- package/batocera/emulationstation/batocera-emulationstation/batocera-emulationstation.mk
+++ package/batocera/emulationstation/batocera-emulationstation/batocera-emulationstation.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 # Last update: Commits on June 13, 2026
-BATOCERA_EMULATIONSTATION_VERSION = 683556d923962845fca409c8f633e459734a3541
+BATOCERA_EMULATIONSTATION_VERSION = 74856dff26a9631628b6e7105152f13294043032
 BATOCERA_EMULATIONSTATION_SITE = https://github.com/batocera-linux/batocera-emulationstation
 BATOCERA_EMULATIONSTATION_SITE_METHOD = git
 BATOCERA_EMULATIONSTATION_LICENSE = MIT
