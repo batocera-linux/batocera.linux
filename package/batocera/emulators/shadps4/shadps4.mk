67a0f4636becc9a33868eba7dfb7c19156ff7b39
Bryan Forbes <bryan@reigndropsfall.net>
2026-06-19 20:17:08 -0500

    fix(shadps4): pre-build `Dear_ImGui_FontEmbed` on the host (#15901)
    This not only avoids cross-compilation issues but is the proper way to
    use a build-time code generator.

--- package/batocera/emulators/shadps4/shadps4.mk
+++ package/batocera/emulators/shadps4/shadps4.mk
@@ -43,5 +43,18 @@ SHADPS4_CONF_OPTS += -DENABLE_DISCORD_RPC=OFF
 SHADPS4_CONF_OPTS += -DENABLE_UPDATER=OFF
 SHADPS4_CONF_OPTS += -DVMA_ENABLE_INSTALL=ON
 
+# Dear_ImGui_FontEmbed is a build-time code generator that runs on the host.
+# The following builds the binary to run on the host rather than adding a
+# host- prefixed package just to compile Dear_ImGui_FontEmbed. After compiling
+# the binary, we tell CMake to use that executable via -DDear_ImGui_FontEmbed_EXECUTABLE
+# which is added in 0001-imgui-renderer-allow-prebuilt-fontembed.patch.
+define SHADPS4_BUILD_HOST_FONTEMBED
+	$(HOSTCXX) -O2 -o $(@D)/host-dear-imgui-fontembed \
+		$(@D)/externals/dear_imgui/misc/fonts/binary_to_compressed_c.cpp
+endef
+SHADPS4_PRE_CONFIGURE_HOOKS += SHADPS4_BUILD_HOST_FONTEMBED
+
+SHADPS4_CONF_OPTS += -DDear_ImGui_FontEmbed_EXECUTABLE=$(BUILD_DIR)/shadps4-$(SHADPS4_VERSION)/host-dear-imgui-fontembed
+
 $(eval $(cmake-package))
 $(eval $(emulator-info-package))
