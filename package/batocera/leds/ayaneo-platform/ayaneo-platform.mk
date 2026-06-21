327463dc68201b97af06653cb8c12afda077f7d7
Daniel Martin <dmanlfc@gmail.com>
2026-06-21 17:00:34 +1000

    update & modify ayaneo-platform module to get rainbow & chroma functionality - add the ayaneo ec module which replaces ayaneo-platform module for some devices - add asus armoury too

--- package/batocera/leds/ayaneo-platform/ayaneo-platform.mk
+++ package/batocera/leds/ayaneo-platform/ayaneo-platform.mk
@@ -3,8 +3,8 @@
 # ayaneo-platform
 #
 ################################################################################
-# Version: Commits on May 22, 2025
-AYANEO_PLATFORM_VERSION = 8ccdf707e7dd7a7c97307b078122b80e92a4ca62
+# Version: Commits on Feb 9, 2026
+AYANEO_PLATFORM_VERSION = eb849d5deabe70fde004f67349fa281af322a499
 AYANEO_PLATFORM_SITE = \
     $(call github,ShadowBlip,ayaneo-platform,$(AYANEO_PLATFORM_VERSION))
 AYANEO_PLATFORM_LICENSE = GPL-3.0
