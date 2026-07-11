00da7726258a869951c81615b3a40c7e4ec1e645
Daniel Martin <dmanlfc@gmail.com>
2026-07-11 11:24:07 +1000

    rpi kernel to 6.18.37

--- package/batocera/utils/rpi-utils/rpi-utils.mk
+++ package/batocera/utils/rpi-utils/rpi-utils.mk
@@ -3,13 +3,13 @@
 # rpi-utils
 #
 ################################################################################
-# Version: Commits on Dec 18, 2025
-RPI_UTILS_VERSION = 230d67ad28e74b17a42064453b2163991cb51a5e
+# Version: Commits on Jul 8, 2026
+RPI_UTILS_VERSION = 5edd399260b5081f9c1c96fc7f369b920d6732d1	
 RPI_UTILS_SITE = $(call github,raspberrypi,utils,$(RPI_UTILS_VERSION))
 RPI_UTILS_LICENSE = BSD-3-Clause
 RPI_UTILS_LICENSE_FILES = LICENCE
 
-RPI_UTILS_DEPENDENCIES = dtc
+RPI_UTILS_DEPENDENCIES = dtc ncurses
 
 RPI_UTILS_CONF_OPTS += -DCMAKE_BUILD_TYPE=Release
 RPI_UTILS_CONF_OPTS += -DBUILD_SHARED_LIBS=OFF
