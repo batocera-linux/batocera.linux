9f80164d48ec18138cd887394b2d6c4c37ec7a90
Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>
2026-07-12 15:58:07 +0000

    feat: update es for torrent updates
    Signed-off-by: Nicolas Adenis-Lamarre <nicolas.adenis.lamarre@gmail.com>

--- package/batocera/emulationstation/batocera-emulationstation/batocera-emulationstation.mk
+++ package/batocera/emulationstation/batocera-emulationstation/batocera-emulationstation.mk
@@ -3,8 +3,8 @@
 # batocera-emulationstation
 #
 ################################################################################
-# Last update: Commits on June 21, 2026
-BATOCERA_EMULATIONSTATION_VERSION = 7351f65717caf046cebcb4b14c48c166e15a6edf
+# Last update: Commits on Jul 12, 2026
+BATOCERA_EMULATIONSTATION_VERSION = cb0061588a8cbccbd47268283539cb0b95360074
 BATOCERA_EMULATIONSTATION_SITE = https://github.com/batocera-linux/batocera-emulationstation
 BATOCERA_EMULATIONSTATION_SITE_METHOD = git
 BATOCERA_EMULATIONSTATION_LICENSE = MIT
