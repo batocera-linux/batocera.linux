5e0e8ad6ef9b62bf3308f308748d82551ea58f0c
Bryan Forbes <bryan@reigndropsfall.net>
2026-08-17 20:29:36 -0500

    feat(python): improve local python package management
    - Make root project a uv "virtual" project, removing the need for a
      "batocera-linux" package and to maintain the list of dependencies in
      multiple places.
    - Add `local-python-package` and `host-local-python-package` make
      functions to simplify the creation of local python packages, and to
      avoid having to maintain the source directory and rsync exclusions in
      multiple places.
    - Move `batocera-common` to `python-batocera-common` to appease
      buildroot's package naming conventions.

--- python-src/batocera-bezel-overlay/batocera-bezel-overlay.mk
+++ python-src/batocera-bezel-overlay/batocera-bezel-overlay.mk
@@ -6,10 +6,7 @@
 
 BATOCERA_BEZEL_OVERLAY_VERSION=44.0
 BATOCERA_BEZEL_OVERLAY_LICENSE=GPL
-BATOCERA_BEZEL_OVERLAY_SOURCE=
-BATOCERA_BEZEL_OVERLAY_OVERRIDE_SRCDIR=$(BR2_EXTERNAL_BATOCERA_PATH)/python-src/batocera-bezel-overlay
-BATOCERA_BEZEL_OVERLAY_OVERRIDE_SRCDIR_RSYNC_EXCLUSIONS=--exclude=".*" --exclude="**/__pycache__/" --exclude="dist"
 BATOCERA_BEZEL_OVERLAY_SETUP_TYPE=hatch
 BATOCERA_BEZEL_OVERLAY_DEPENDENCIES=python3 python-gobject libgtk3 gtk-layer-shell
 
-$(eval $(python-package))
+$(eval $(local-python-package))
