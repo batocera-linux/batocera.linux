24ecf727a6634ce8d80b7615e3b36c8801491f13
Bryan Forbes <bryan@reigndropsfall.net>
2026-08-25 19:47:42 -0500

    fix(libxmp): download using tag

--- package/batocera/libraries/libxmp/libxmp.mk
+++ package/batocera/libraries/libxmp/libxmp.mk
@@ -5,8 +5,7 @@
 ################################################################################
 
 LIBXMP_VERSION = 4.7.2
-LIBXMP_SOURCE = enet-libxmp-${LIBXMP_VERSION}.tar.gz
-LIBXMP_SITE =  $(call github,libxmp,libxmp,$(LIBXMP_VERSION))
+LIBXMP_SITE =  $(call github,libxmp,libxmp,libxmp-$(LIBXMP_VERSION))
 LIBXMP_INSTALL_STAGING = YES
 LIBXMP_AUTORECONF = YES
 LIBXMP_DEPENDENCIES = host-pkgconf
