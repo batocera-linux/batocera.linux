87552e0d7ed7d43f105cae74e0729c01130f445e
Daniel Martin <dmanlfc@gmail.com>
2026-08-27 16:17:07 +1000

    bump supermodel, remove custom patches that are now included

--- package/batocera/emulators/supermodel/supermodel.mk
+++ package/batocera/emulators/supermodel/supermodel.mk
@@ -3,10 +3,10 @@
 # supermodel
 #
 ################################################################################
-# Version: based on v0.3a-20260726 (Commits on Aug 26, 2026)
-SUPERMODEL_VERSION = 10d26c4469891c78c490a845e356fb296f5cb805
+# Version: based on v0.3a-20260726 (Commits on Aug 27, 2026)
+SUPERMODEL_VERSION = 39beed2b6f8eeb2081093ad205580c743e53d4ec
 SUPERMODEL_SITE = $(call github,dmanlfc,Supermodel,$(SUPERMODEL_VERSION))
-SUPERMODEL_DEPENDENCIES = sdl2 zlib libzip sdl2_net
+SUPERMODEL_DEPENDENCIES = sdl2 zlib libzip sdl2_net udev
 SUPERMODEL_LICENSE = GPLv3
 SUPERMODEL_EMULATOR_INFO = supermodel.emulator.yml
 
@@ -33,11 +33,6 @@ SUPERMODEL_CONF_OPTS += VULKAN=1
 endif
 
 define SUPERMODEL_BUILD_CMDS
-	cp $(@D)/Makefiles/Makefile.UNIX $(@D)/Makefile
-	$(SED) "s|CC = gcc|CC = $(TARGET_CC)|g" $(@D)/Makefile
-	$(SED) "s|CXX = g++|CXX = $(TARGET_CXX)|g" $(@D)/Makefile
-	$(SED) "s|LD = gcc|LD = $(TARGET_CC)|g" $(@D)/Makefile
-	$(SED) "s|sdl2-config|$(STAGING_DIR)/usr/bin/sdl2-config|g" $(@D)/Makefile
 	$(TARGET_CONFIGURE_OPTS) $(MAKE) -C $(@D) -f Makefile \
 	    ARCH=$(BR2_TARGET_OPTIMIZATION) $(SUPERMODEL_CONF_OPTS)
 endef
@@ -52,23 +47,12 @@ define SUPERMODEL_INSTALL_TARGET_CMDS
 	$(INSTALL) -D -m 0644 $(@D)/Assets/* $(TARGET_DIR)/usr/share/supermodel/Assets/
 endef
 
-define SUPERMODEL_LINE_ENDINGS_FIXUP
-	# DOS2UNIX Supermodel.ini and Main.cpp - patch system does not support different line endings
-	sed -i -E -e "s|\r$$||g" $(@D)/Src/OSD/SDL/Main.cpp
-	sed -i -E -e "s|\r$$||g" $(@D)/Src/Model3/Model3.cpp
-	sed -i -E -e "s|\r$$||g" $(@D)/Src/Inputs/Inputs.cpp
-	sed -i -E -e "s|\r$$||g" $(@D)/Src/Graphics/New3D/R3DShaderTriangles.h
-	sed -i -E -e "s|\r$$||g" $(@D)/Src/OSD/Unix/FileSystemPath.cpp
-endef
-
 define SUPERMODEL_POST_PROCESS
 	mkdir -p $(TARGET_DIR)/usr/share/evmapy $(TARGET_DIR)/usr/share/supermodel
 	cp -pr $(SUPERMODEL_PKGDIR)/NVRAM $(TARGET_DIR)/usr/share/supermodel
 	cp -p $(SUPERMODEL_PKGDIR)/Supermodel.ini.template \
 	    $(TARGET_DIR)/usr/share/supermodel/Supermodel.ini.template
 endef
-
-SUPERMODEL_PRE_PATCH_HOOKS += SUPERMODEL_LINE_ENDINGS_FIXUP
 
 SUPERMODEL_POST_INSTALL_TARGET_HOOKS += SUPERMODEL_POST_PROCESS
 
