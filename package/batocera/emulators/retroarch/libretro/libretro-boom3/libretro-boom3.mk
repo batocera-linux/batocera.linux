c4d1851f83f33d90a715b1f43e5fd0181dd0d574
Daniel Martin <dmanlfc@gmail.com>
2026-08-30 13:57:11 +1000

    fix lr-boom3, only 1 combined core now

--- package/batocera/emulators/retroarch/libretro/libretro-boom3/libretro-boom3.mk
+++ package/batocera/emulators/retroarch/libretro/libretro-boom3/libretro-boom3.mk
@@ -17,19 +17,13 @@ LIBRETRO_BOOM3_PLATFORM = $(LIBRETRO_PLATFORM)
 
 define LIBRETRO_BOOM3_BUILD_CMDS
     $(TARGET_CONFIGURE_OPTS) $(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" \
-	    -C $(@D)/neo -f Makefile platform="$(LIBRETRO_BOOM3_PLATFORM)" \
-		GIT_VERSION="-$(shell echo $(LIBRETRO_BOOM3_VERSION) | cut -c 1-7)"
-	# boom3 XP core
-    $(TARGET_CONFIGURE_OPTS) $(MAKE) CXX="$(TARGET_CXX)" CC="$(TARGET_CC)" \
-	    -C $(@D)/neo -f Makefile D3XP=1 platform="$(LIBRETRO_BOOM3_PLATFORM)" \
-		GIT_VERSION="-$(shell echo $(LIBRETRO_BOOM3_VERSION) | cut -c 1-7)"
+        -C $(@D)/neo -f Makefile platform="$(LIBRETRO_BOOM3_PLATFORM)" \
+        GIT_VERSION="-$(shell echo $(LIBRETRO_BOOM3_VERSION) | cut -c 1-7)"
 endef
 
 define LIBRETRO_BOOM3_INSTALL_TARGET_CMDS
-	$(INSTALL) -D $(@D)/neo/boom3_libretro.so \
-		$(TARGET_DIR)/usr/lib/libretro/boom3_libretro.so
-	$(INSTALL) -D $(@D)/neo/boom3_xp_libretro.so \
-		$(TARGET_DIR)/usr/lib/libretro/boom3_xp_libretro.so
+    $(INSTALL) -D $(@D)/neo/boom3_libretro.so \
+        $(TARGET_DIR)/usr/lib/libretro/boom3_libretro.so
 endef
 
 $(eval $(generic-package))
