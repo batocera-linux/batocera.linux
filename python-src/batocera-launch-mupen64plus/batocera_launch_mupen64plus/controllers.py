bb2cb07078a9c9c52af28d24f601ee3473f44181
Bryan Forbes <bryan@reigndropsfall.net>
2026-09-10 16:16:04 -0500

    refactor(mupen64plus): Emulator-owned paths and controllers
    - Inline historical `config_dir` and `saves_dir` paths
    - Move controller config onto `Mupen64PlusControllersMixin`
    - Cache user input mappings as properties

--- python-src/batocera-launch-mupen64plus/batocera_launch_mupen64plus/controllers.py
+++ python-src/batocera-launch-mupen64plus/batocera_launch_mupen64plus/controllers.py
@@ -3,14 +3,17 @@ from __future__ import annotations
 from typing import TYPE_CHECKING, Final
 from xml.dom import minidom
 
-from .paths import MUPEN64PLUS_SYSTEM_MAPPING, MUPEN64PLUS_USER_MAPPING
+from batocera_common.dataclasses import cached_dataclass, cached_property
+from batocera_launch import Controller, Emulator, Input, InputMapping
+from batocera_launch.paths import DATAINIT_DIR
 
 if TYPE_CHECKING:
     from collections.abc import Mapping
+    from pathlib import Path
 
     from batocera_common.configparser import CaseSensitiveConfigParser
-    from batocera_launch import Controller, Controllers, Input, InputMapping, SystemConfig
-    from batocera_launch.devices.device import DeviceInfoMapping
+
+_MUPEN64PLUS_SYSTEM_MAPPING: Final = DATAINIT_DIR / 'system' / 'configs' / 'mupen64' / 'input.xml'
 
 # Must read :
 # http://mupen64plus.org/wiki/index.php?title=Mupen64Plus_Plugin_Parameters
@@ -37,131 +40,12 @@ _VALID_N64_CONTROLLER_NAMES: Final = [
 ]
 
 
-def _get_mupen_mapping(use_n64_inputs: bool, /) -> dict[str, str]:
-    # load system values and override by user values in case some user values are missing
-    mapping: dict[str, str] = {}
-    for file in (MUPEN64PLUS_SYSTEM_MAPPING, MUPEN64PLUS_USER_MAPPING):
-        if file.exists():
-            dom = minidom.parse(str(file))
-            list_name = 'n64InputList' if use_n64_inputs else 'defaultInputList'
-            for inputs in dom.getElementsByTagName(list_name):
-                for input_node in inputs.childNodes:
-                    if input_node.attributes and input_node.attributes['name'] and input_node.attributes['value']:
-                        mapping[input_node.attributes['name'].value] = input_node.attributes['value'].value
-    return mapping
-
-
-def set_controllers_config(
-    ini_config: CaseSensitiveConfigParser,
-    controllers: Controllers,
-    config: SystemConfig,
-    wheels: DeviceInfoMapping,
+def _get_controller_line(
+    mupen_mapping: Mapping[str, str],
+    input_: Input,
+    mupen_setting_name: str,
+    all_inputs: InputMapping,
     /,
-) -> None:
-    for pad in controllers:
-        is_wheel = pad.device_path in wheels and wheels[pad.device_path].is_wheel
-        pad_config = _define_controller_keys(pad.player_number, pad, config, is_wheel)
-        _fill_ini_player(pad.player_number, ini_config, pad, pad_config)
-
-    # remove section with no player
-    for x in range(len(controllers) + 1, 4):
-        section = f'Input-SDL-Control{x}'
-        if ini_config.has_section(section):
-            _clean_player(x, ini_config)
-
-
-def _get_joystick_peak(start_value: str, config_value: str, config: SystemConfig, /) -> str:
-    default_value = int(start_value.split(',')[0])
-    multiplier = config.get_float(config_value, 1)
-
-    # This is needed because higher peak value lowers sensitivity and vice versa
-    if multiplier != 1.0:
-        adjusted_value = default_value * multiplier
-        difference = abs(adjusted_value - default_value)
-
-        # Figure out if we need to add or subtract the starting peak value
-        if adjusted_value < default_value:
-            peak = round(default_value + difference)
-        else:
-            peak = round(default_value - difference)
-    else:
-        peak = default_value
-
-    return f'{peak},{peak}'
-
-
-def _get_joystick_deadzone(default_peak: str, config_value: str, config: SystemConfig, /) -> str:
-    default_value = int(default_peak.split(',')[0])
-    deadzone_multiplier = config.get_float(config_value, 0.01)
-
-    deadzone = round(default_value * deadzone_multiplier)
-
-    return f'{deadzone},{deadzone}'
-
-
-def _define_controller_keys(
-    nplayer: int, controller: Controller, config: SystemConfig, is_wheel: bool, /
-) -> dict[str, str]:
-    # check for auto-config inputs by guid and name, or es settings
-    if (controller.guid in _VALID_N64_CONTROLLER_GUIDS and controller.name in _VALID_N64_CONTROLLER_NAMES) or (
-        config.get(f'mupen64-controller{nplayer}', 'retropad') != 'retropad'
-    ):
-        mupen_mapping = _get_mupen_mapping(True)
-    else:
-        mupen_mapping = _get_mupen_mapping(False)
-
-    # pad_config holds the final pad configuration in the mupen style
-    # ex: pad_config['DPad U'] = "button(1)"
-    pad_config: dict[str, str] = {}
-
-    # determine joystick deadzone and peak
-    pad_config['AnalogPeak'] = _get_joystick_peak(mupen_mapping['AnalogPeak'], f'mupen64-sensitivity{nplayer}', config)
-
-    # Analog Deadzone
-    if is_wheel:
-        pad_config['AnalogDeadzone'] = '0,0'
-    else:
-        pad_config['AnalogDeadzone'] = _get_joystick_deadzone(
-            mupen_mapping['AnalogPeak'], f'mupen64-deadzone{nplayer}', config
-        )
-
-    # z is important, in case l2 is not available for this pad, use l1
-    # assume that l2 is for "Z Trig" in the mapping
-    if 'l2' not in controller.inputs:
-        mupen_mapping['pageup'] = mupen_mapping['l2']
-
-    # if joystick1up is not available, use up/left while these keys are more used
-    if 'joystick1up' not in controller.inputs:
-        mupen_mapping['up'] = mupen_mapping['joystick1up']
-        mupen_mapping['down'] = mupen_mapping['joystick1down']
-        mupen_mapping['left'] = mupen_mapping['joystick1left']
-        mupen_mapping['right'] = mupen_mapping['joystick1right']
-
-    # the input.xml adds 2 directions per joystick, ES handles just 1
-    fake_sticks = {'joystick2up': 'joystick2down', 'joystick2left': 'joystick2right'}
-    # Cheat on the controller
-    for real_stick, fake_stick in fake_sticks.items():
-        if real_stick in controller.inputs and controller.inputs[real_stick].type == 'axis':
-            controller.inputs[fake_stick] = controller.inputs[real_stick].replace(
-                name=fake_stick,
-                value=str(-int(controller.inputs[real_stick].value)),
-            )
-
-    for input_idx in controller.inputs:
-        pad_input = controller.inputs[input_idx]
-        if pad_input.name in mupen_mapping and mupen_mapping[pad_input.name] != '':
-            value = _set_controller_line(mupen_mapping, pad_input, mupen_mapping[pad_input.name], controller.inputs)
-            # Handle multiple inputs for a single N64 Pad input
-            if value != '':
-                if mupen_mapping[pad_input.name] not in pad_config:
-                    pad_config[mupen_mapping[pad_input.name]] = value
-                else:
-                    pad_config[mupen_mapping[pad_input.name]] += f' {value}'
-    return pad_config
-
-
-def _set_controller_line(
-    mupen_mapping: Mapping[str, str], input_: Input, mupen_setting_name: str, all_inputs: InputMapping, /
 ) -> str:
     value = ''
     input_type = input_.type
@@ -213,9 +97,9 @@ def _set_controller_line(
 
 
 def _fill_ini_player(
-    nplayer: int, ini_config: CaseSensitiveConfigParser, controller: Controller, pad_config: dict[str, str], /
+    controller: Controller, ini_config: CaseSensitiveConfigParser, pad_config: dict[str, str], /
 ) -> None:
-    section = f'Input-SDL-Control{nplayer}'
+    section = f'Input-SDL-Control{controller.player_number}'
 
     # set static config
     if not ini_config.has_section(section):
@@ -267,3 +151,130 @@ def _clean_player(nplayer: int, ini_config: CaseSensitiveConfigParser, /) -> Non
         ini_config.add_section(section)
     ini_config.set(section, 'Version', '2')
     ini_config.set(section, 'plugged', 'False')
+
+
+@cached_dataclass
+class Mupen64PlusControllersMixin(Emulator):
+    @cached_property
+    def user_input_mapping_file(self) -> Path:
+        return self.config_dir / 'input.xml'
+
+    @cached_property
+    def user_n64_input_mapping(self) -> dict[str, str]:
+        return self._get_mupen_mapping(True)
+
+    @cached_property
+    def user_input_mapping(self) -> dict[str, str]:
+        return self._get_mupen_mapping(False)
+
+    def _get_mupen_mapping(self, use_n64_inputs: bool, /) -> dict[str, str]:
+        # load system values and override by user values in case some user values are missing
+        mapping: dict[str, str] = {}
+        for file in (_MUPEN64PLUS_SYSTEM_MAPPING, self.user_input_mapping_file):
+            if file.exists():
+                dom = minidom.parse(str(file))
+                list_name = 'n64InputList' if use_n64_inputs else 'defaultInputList'
+                for inputs in dom.getElementsByTagName(list_name):
+                    for input_node in inputs.childNodes:
+                        if input_node.attributes and input_node.attributes['name'] and input_node.attributes['value']:
+                            mapping[input_node.attributes['name'].value] = input_node.attributes['value'].value
+        return mapping
+
+    def _define_controller_keys(self, controller: Controller, is_wheel: bool, /) -> dict[str, str]:
+        # check for auto-config inputs by guid and name, or es settings
+        if (controller.guid in _VALID_N64_CONTROLLER_GUIDS and controller.name in _VALID_N64_CONTROLLER_NAMES) or (
+            self.config.get(f'mupen64-controller{controller.player_number}', 'retropad') != 'retropad'
+        ):
+            mupen_mapping = dict(self.user_n64_input_mapping)
+        else:
+            mupen_mapping = dict(self.user_input_mapping)
+
+        # pad_config holds the final pad configuration in the mupen style
+        # ex: pad_config['DPad U'] = "button(1)"
+        pad_config: dict[str, str] = {}
+
+        # determine joystick deadzone and peak
+        pad_config['AnalogPeak'] = self._get_joystick_peak(
+            mupen_mapping['AnalogPeak'], f'mupen64-sensitivity{controller.player_number}'
+        )
+
+        # Analog Deadzone
+        if is_wheel:
+            pad_config['AnalogDeadzone'] = '0,0'
+        else:
+            pad_config['AnalogDeadzone'] = self._get_joystick_deadzone(
+                mupen_mapping['AnalogPeak'], f'mupen64-deadzone{controller.player_number}'
+            )
+
+        # z is important, in case l2 is not available for this pad, use l1
+        # assume that l2 is for "Z Trig" in the mapping
+        if 'l2' not in controller.inputs:
+            mupen_mapping['pageup'] = mupen_mapping['l2']
+
+        # if joystick1up is not available, use up/left while these keys are more used
+        if 'joystick1up' not in controller.inputs:
+            mupen_mapping['up'] = mupen_mapping['joystick1up']
+            mupen_mapping['down'] = mupen_mapping['joystick1down']
+            mupen_mapping['left'] = mupen_mapping['joystick1left']
+            mupen_mapping['right'] = mupen_mapping['joystick1right']
+
+        # the input.xml adds 2 directions per joystick, ES handles just 1
+        fake_sticks = {'joystick2up': 'joystick2down', 'joystick2left': 'joystick2right'}
+        # Cheat on the controller
+        for real_stick, fake_stick in fake_sticks.items():
+            if real_stick in controller.inputs and controller.inputs[real_stick].type == 'axis':
+                controller.inputs[fake_stick] = controller.inputs[real_stick].replace(
+                    name=fake_stick,
+                    value=str(-int(controller.inputs[real_stick].value)),
+                )
+
+        for input_idx in controller.inputs:
+            pad_input = controller.inputs[input_idx]
+            if pad_input.name in mupen_mapping and mupen_mapping[pad_input.name] != '':
+                value = _get_controller_line(mupen_mapping, pad_input, mupen_mapping[pad_input.name], controller.inputs)
+                # Handle multiple inputs for a single N64 Pad input
+                if value != '':
+                    if mupen_mapping[pad_input.name] not in pad_config:
+                        pad_config[mupen_mapping[pad_input.name]] = value
+                    else:
+                        pad_config[mupen_mapping[pad_input.name]] += f' {value}'
+        return pad_config
+
+    def _get_joystick_peak(self, start_value: str, config_value: str, /) -> str:
+        default_value = int(start_value.split(',')[0])
+        multiplier = self.config.get_float(config_value, 1)
+
+        # This is needed because higher peak value lowers sensitivity and vice versa
+        if multiplier != 1.0:
+            adjusted_value = default_value * multiplier
+            difference = abs(adjusted_value - default_value)
+
+            # Figure out if we need to add or subtract the starting peak value
+            if adjusted_value < default_value:
+                peak = round(default_value + difference)
+            else:
+                peak = round(default_value - difference)
+        else:
+            peak = default_value
+
+        return f'{peak},{peak}'
+
+    def _get_joystick_deadzone(self, default_peak: str, config_value: str, /) -> str:
+        default_value = int(default_peak.split(',')[0])
+        deadzone_multiplier = self.config.get_float(config_value, 0.01)
+
+        deadzone = round(default_value * deadzone_multiplier)
+
+        return f'{deadzone},{deadzone}'
+
+    def set_controllers_config(self, ini_config: CaseSensitiveConfigParser, /) -> None:
+        for pad in self.controllers:
+            is_wheel = pad.device_path in self.wheels and self.wheels[pad.device_path].is_wheel
+            pad_config = self._define_controller_keys(pad, is_wheel)
+            _fill_ini_player(pad, ini_config, pad_config)
+
+        # remove section with no player
+        for x in range(len(self.controllers) + 1, 4):
+            section = f'Input-SDL-Control{x}'
+            if ini_config.has_section(section):
+                _clean_player(x, ini_config)
