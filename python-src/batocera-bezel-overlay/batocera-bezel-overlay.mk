e936590d888c42d8ffda70f615142cdea89daa05
Therand90 <135005486+Therand90@users.noreply.github.com>
2026-09-12 00:12:26 +0200

    fix: make Wayland bezel overlays pointer-transparent

--- python-src/batocera-bezel-overlay/batocera-bezel-overlay.mk
+++ python-src/batocera-bezel-overlay/batocera-bezel-overlay.mk
@@ -7,6 +7,6 @@
 BATOCERA_BEZEL_OVERLAY_VERSION=44.0
 BATOCERA_BEZEL_OVERLAY_LICENSE=GPL
 BATOCERA_BEZEL_OVERLAY_SETUP_TYPE=hatch
-BATOCERA_BEZEL_OVERLAY_DEPENDENCIES=python3 python-gobject libgtk3 gtk-layer-shell
+BATOCERA_BEZEL_OVERLAY_DEPENDENCIES=python3 python-pycairo python-gobject libgtk3 gtk-layer-shell
 
 $(eval $(local-python-package))
