bef466132db132fb272ff57a0bdcf1b12d059c6e
Daniel Martin <dmanlfc@gmail.com>
2026-09-18 00:56:28 +1000

    ruffle: bump to nightly-2026-09-17

--- package/batocera/emulators/ruffle/ruffle.mk
+++ package/batocera/emulators/ruffle/ruffle.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RUFFLE_VERSION = nightly-2026-09-12
+RUFFLE_VERSION = nightly-2026-09-17
 RUFFLE_SITE = $(call github,ruffle-rs,ruffle,$(RUFFLE_VERSION))
 RUFFLE_LICENSE = GPLv2
 RUFFLE_EMULATOR_INFO = ruffle.emulator.yml
